d5522a39134e6a9cfc6e6f5a550ee2022459ae37
Robbie Paul <robbie.paul@dxw.com>
2017-08-23 11:32:03 +0100

    (feature) Factory Girl gem has been added
    This is a replacement for the default rails fixtures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,9 +17,6 @@ Capybara.javascript_driver = :poltergeist
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
