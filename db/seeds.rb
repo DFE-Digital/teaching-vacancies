d38721450e828f1cd989832b62fe0acafb854b1b
Robbie Paul <robbie.paul@dxw.com>
2017-09-01 14:51:45 +0100

    (feature) Database seeds now exist
    Also modified the existing model factories in preparation for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,67 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+raise if Rails.env.production?
+
+require 'faker'
+require 'factory_girl_rails'
+
+london = Region.create(name: 'London')
+Region.create(name: 'South East England')
+Region.create(name: 'South West England')
+Region.create(name: 'Yorkshire and the Humber')
+Region.create(name: 'North West England')
+Region.create(name: 'West Midlands')
+Region.create(name: 'East Midlands')
+Region.create(name: 'North East England')
+
+PayScale.create(label: 'Main pay range 1')
+PayScale.create(label: 'Main pay range 2')
+PayScale.create(label: 'Main pay range 3')
+PayScale.create(label: 'Main pay range 4')
+PayScale.create(label: 'Main pay range 5')
+PayScale.create(label: 'Main pay range 6')
+PayScale.create(label: 'Upper pay range 1')
+PayScale.create(label: 'Upper pay range 2')
+PayScale.create(label: 'Upper pay range 3')
+
+Leadership.create(title: 'Middle Leader')
+Leadership.create(title: 'Senior Leader')
+Leadership.create(title: 'Headteacher')
+Leadership.create(title: 'Executive Head')
+Leadership.create(title: 'Multi-Academy Trust')
+
+academy = SchoolType.create(label: 'Academy')
+SchoolType.create(label: 'Independent School')
+SchoolType.create(label: 'Free School')
+SchoolType.create(label: 'LA Maintained School')
+SchoolType.create(label: 'Special School')
+
+Subject.create(name: 'English')
+Subject.create(name: 'Mathematics')
+science = Subject.create(name: 'Science')
+Subject.create(name: 'Art and design')
+Subject.create(name: 'Citizenship')
+Subject.create(name: 'Computing')
+Subject.create(name: 'Design and technology')
+Subject.create(name: 'Geography')
+Subject.create(name: 'History')
+Subject.create(name: 'Languages')
+Subject.create(name: 'Music')
+Subject.create(name: 'Physical education')
+
+school = FactoryGirl.create(:school,
+  name: 'Acme Secondary School',
+  school_type: academy,
+  urn: 1234567890,
+  address: '22 High Street',
+  town: 'Ealing',
+  county: 'Middlesex',
+  postcode: 'EA1 1NG',
+  region: london
+)
+
+FactoryGirl.create(:vacancy,
+  job_title: 'Physics Teacher',
+  subject: science,
+  school: school,
+  minimum_salary: 25000,
+  maximum_salary: 30000
+)
--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -1,13 +1,14 @@
 FactoryGirl.define do
   factory :school do
     association :school_type
+    association :region
 
-    name { Faker::Company.name }
+    name { Faker::Educator.secondary_school }
     description { Faker::Lorem.paragraph(1) }
     urn { Faker::Number.number(10) }
     address { Faker::Address.street_name }
     town { Faker::Address.city }
-    county { Faker::Address.state }
+    county { Faker::Address.state_abbr }
     postcode { Faker::Address.postcode }
   end
 end
