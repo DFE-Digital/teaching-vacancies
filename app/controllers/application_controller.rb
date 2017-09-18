a23a820180d66628cc1eeb126d8c4c1a7b956b09
Robbie Paul <robbie.paul@dxw.com>
2017-09-18 15:21:07 +0100

    (feature) Now responds to unknown pages with a 404
    Any routes that aren't matched will display a 404 error.
    
    If html is requested a 404 page template will be displayed
    If json is requested a json formatted error will return
    If anything else is requested a 404 will be returned with an empty body
    
    Any ActiveRecord::NotFound errors are rescued and forwarded to the
    application's not_found method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, except: :not_found
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    respond_to do |format|
+      format.html { render 'errors/not_found', status: 404 }
+      format.json { render json: { errors: 'Resource not found' }, status: 404 }
+      format.all { render status: 404, body: nil }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'vacancies#index'
-  resources :vacancies, only: [:index]
+
+  resources :vacancies, only: %i[index show]
+
+  match '*path', to: 'application#not_found', via: %i[get post patch put delete]
 end
