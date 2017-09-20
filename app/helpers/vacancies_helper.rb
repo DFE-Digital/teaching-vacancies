8b237be2ff7cc2b4b8de0c0609f9956bbbe97df1
Robbie Paul <robbie.paul@dxw.com>
2017-09-20 11:36:14 +0100

    (feature) Vacancies list now has filters side panel

--- app/helpers/vacancies_helper.rb
+++ app/helpers/vacancies_helper.rb
@@ -1,2 +1,13 @@
 module VacanciesHelper
+  def salary_options
+    (20000..70000).step(10000).map { |num| [number_to_currency(num), num] }.to_h
+  end
+
+  def working_pattern_options
+    Vacancy.working_patterns.keys.map { |key| [key.humanize, key] }
+  end
+
+  def school_phase_options
+    School.phases.keys.map { |key| [key.humanize, key] }
+  end
 end
