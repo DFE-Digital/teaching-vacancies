1578dde03e48d91d1b8bddc463cc9d6df37a3f33
Despo Pentara <despo@despo.me>
2017-10-15 20:16:59 +0100

    (fix) extract date format error message
    … to reduce line length and make method easier to read

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -4,8 +4,13 @@ module DateHelper
   def format_date(date, format = :default)
     return if date.nil?
     date_formats = Date::DATE_FORMATS.keys.join(' ')
-    raise FormatDateError, "Unknown format: #{format} should be one of #{date_formats}" \
-      unless Date::DATE_FORMATS.include?(format)
+    unless Date::DATE_FORMATS.include?(format)
+      raise FormatDateError, date_format_error_message(format, date_formats)
+    end
     date.to_s(format)
+  end
+
+  def date_format_error_message(format, date_formats)
+    "Unknown format: #{format} should be one of #{date_formats}"
   end
 end
