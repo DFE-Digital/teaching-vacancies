cbd54226050a1edbe5b64b108a462e0216958a30
Despo Pentara <despo@despo.me>
2017-10-20 18:34:59 +0100

    (feature) Remove JobSchema references from index
    As per guidelines from https://developers.google.com/search/docs/data-types/job-postings
    
    `Don’t add structured data to listing pages`

--- spec/features/users_can_view_vacancies_spec.rb
+++ spec/features/users_can_view_vacancies_spec.rb
@@ -39,38 +39,4 @@ RSpec.feature 'Viewing vacancies' do
 
     expect(page).to have_link('2')
   end
-
-  scenario 'Vacancies should contain JobPosting schema.org mark up', elasticsearch: true do
-    valid_vacancy = create(:vacancy, :job_schema)
-
-    Vacancy.__elasticsearch__.client.indices.flush
-    visit vacancies_path
-
-    within '.vacancies' do
-      expect(page).to have_selector('li[itemscope][itemtype="http://schema.org/JobPosting"]')
-
-      within 'li[itemscope][itemtype="http://schema.org/JobPosting"]' do
-        expect_schema_property_to_match_value('title', valid_vacancy.job_title)
-        expect_schema_property_to_match_value('description', valid_vacancy.headline)
-        expect_schema_property_to_match_value('responsibilities', valid_vacancy.job_description)
-        expect_schema_property_to_match_value('industry', 'Education')
-        expect_schema_property_to_match_value('employmentType', valid_vacancy.working_pattern&.titleize)
-        expect_schema_property_to_match_value('url', vacancy_url(valid_vacancy))
-        expect_schema_property_to_match_value('streetAddress', valid_vacancy.school.address)
-        expect_schema_property_to_match_value('postalCode', valid_vacancy.school.postcode)
-        expect_schema_property_to_match_value('addressLocality', valid_vacancy.school.town)
-        expect_schema_property_to_match_value('addressRegion', valid_vacancy.school.county)
-        expect_schema_property_to_match_value('hiringOrganization', valid_vacancy.school.name)
-        expect_schema_property_to_match_value('datePosted', valid_vacancy.publish_on.to_s(:db))
-        expect_schema_property_to_match_value('validThrough', valid_vacancy.expires_on.to_s(:db))
-        expect_schema_property_to_match_value('workHours', valid_vacancy.weekly_hours)
-        expect_schema_property_to_match_value('currency', 'GBP')
-        expect_schema_property_to_match_value('minValue', valid_vacancy.minimum_salary)
-        expect_schema_property_to_match_value('maxValue', valid_vacancy.maximum_salary)
-        expect_schema_property_to_match_value('educationRequirements', valid_vacancy.education)
-        expect_schema_property_to_match_value('experienceRequirements', valid_vacancy.essential_requirements)
-        expect_schema_property_to_match_value('jobBenefits', valid_vacancy.benefits)
-      end
-    end
-  end
 end
