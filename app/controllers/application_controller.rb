8b625e29ef235bea538a4c099e368327bddc47de
Robbie Paul <robbie.paul@dxw.com>
2017-11-01 11:29:33 +0000

    (feature) Basic auth credentials are required if env vars set
    If HTTP_USER and HTTP_PASS environment variablesare set the user will be
    asked to provide basic auth credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,20 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
+  before_action :authenticate
+
   def not_found
     respond_to do |format|
       format.html { render 'errors/not_found', status: 404 }
       format.json { render json: { errors: 'Resource not found' }, status: 404 }
       format.all { render status: 404, body: nil }
+    end
+  end
+
+  def authenticate
+    return unless Figaro.env.http_user? && Figaro.env.http_pass?
+    authenticate_or_request_with_http_basic do |name, password|
+      name == Figaro.env.http_user && password == Figaro.env.http_pass
     end
   end
 end
---
+++ spec/requests/basic_auth_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe 'basic auth', type: :request do
+  context 'when basic auth credentials are set' do
+    it 'asks for the basic auth credentials' do
+      fake_env = double.as_null_object
+      allow(Figaro).to receive(:env).and_return(fake_env)
+      allow(fake_env).to receive(:http_user?).and_return(true)
+      allow(fake_env).to receive(:http_pass?).and_return(true)
+      allow(fake_env).to receive(:http_user).and_return('username')
+      allow(fake_env).to receive(:http_pass).and_return('password')
+
+      get '/'
+      expect(response).to have_http_status(401)
+    end
+
+    context 'when the correct basic auth credentials are given' do
+      it 'returns a 200' do
+        username = 'username'
+        password = 'foobar'
+
+        fake_env = double.as_null_object
+        allow(Figaro).to receive(:env).and_return(fake_env)
+        allow(fake_env).to receive(:http_user?).and_return(true)
+        allow(fake_env).to receive(:http_pass?).and_return(true)
+        allow(fake_env).to receive(:http_user).and_return(username)
+        allow(fake_env).to receive(:http_pass).and_return(password)
+
+        encoded_credentials = ActionController::HttpAuthentication::Basic.encode_credentials(username, password)
+        get '/', env: { 'HTTP_AUTHORIZATION': encoded_credentials }
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  context 'when basic auth credentials are not set' do
+    it 'does not ask for the basic auth credentials' do
+      fake_env = double.as_null_object
+      allow(Figaro).to receive(:env).and_return(fake_env)
+      allow(fake_env).to receive(:http_user?).and_return(false)
+      allow(fake_env).to receive(:http_pass?).and_return(false)
+      allow(fake_env).to receive(:http_user).and_return(nil)
+      allow(fake_env).to receive(:http_pass).and_return(nil)
+
+      get '/'
+      expect(response).to have_http_status(200)
+    end
+  end
+end
