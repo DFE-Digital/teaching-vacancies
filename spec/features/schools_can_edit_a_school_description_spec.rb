7c03c9a5723c5e1e8e477b3adf7ae7ee903a1a7e
Frankie Roberto <frankie@frankieroberto.com>
2017-11-14 10:11:14 +0000

    Adding a scenario for removing a description

--- spec/features/schools_can_edit_a_school_description_spec.rb
+++ spec/features/schools_can_edit_a_school_description_spec.rb
@@ -16,4 +16,17 @@ RSpec.feature 'Editing a School’s description' do
 
     expect(page).to have_content('Our school prides itself on excellence.')
   end
+
+  scenario 'removing a description' do
+    visit school_path(@school.urn)
+
+    expect(page).to have_content('Salisbury school')
+    expect(page).to have_content('We are a good school')
+
+    click_on 'Change description'
+    fill_in 'Description', with: ''
+    click_on 'Save'
+
+    expect(page).to have_content('Description Not provided')
+  end
 end
