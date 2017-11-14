d83c034b43cabd607c1ce3a58ba6b220127b11a5
Frankie Roberto <frankie@frankieroberto.com>
2017-11-14 10:08:56 +0000

    code style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'vacancies#index'
 
-
   resources :vacancies, only: %i[index show new create edit] do
     get 'review', on: :member
     put 'publish', on: :member
@@ -13,7 +12,6 @@ Rails.application.routes.draw do
   end
 
   resources :schools, only: %i[show edit update]
-
 
   match '*path', to: 'application#not_found', via: %i[get post patch put delete]
 end
