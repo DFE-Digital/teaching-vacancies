21c999c3775f3ac778c709ad0202b8fc1d1e5deb
Tom Hipkin <tomhipkin@gmail.com>
2018-02-21 10:59:20 +0000

    (chore): Project test suite is no longer dependant on state of ENV
    * On getting started with this project it became immediately clear that the presence or omission of env changed the behaviour of the app in development and affected the test suite nastily with many request spec failures.
    * This change introduces a new `authenticate?` method into the controller which we can stub in our tests that want to simulate a staging or production environment. The alternative would be to stub Rails.env and return a `ActiveSupport::StringInquirer.new('production’)` but this has far reaching consquences as other code also uses this and fails. In my case Elastic Search already fails as it cannot find content when the env changes and it expects `test`.
    * The long term vision is not to have basic auth for production but to be safe until we have a more permenant solution (SSO?) I am treating it the same as staging - locked down.
    * The presence or absence of basic auth environment varialbes no longer dictates behaviour in an obfuscated way. We always expect it to be set, if it’s forgotten then basic auth will try to match any empty string against nil and log a warning to create a value.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,31 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate
-    return unless Figaro.env.http_user? && Figaro.env.http_pass?
+    return unless authenticate?
     authenticate_or_request_with_http_basic do |name, password|
-      name == Figaro.env.http_user && password == Figaro.env.http_pass
+      name == http_user && password == http_pass
+    end
+  end
+
+  def authenticate?
+    !(Rails.env.development? || Rails.env.test?)
+  end
+
+  private def http_user
+    if Figaro.env.http_user?
+      Figaro.env.http_user
+    else
+      Rails.logger.warn('Basic auth failed: ENV["HTTP_USER"] expected but not found.')
+      nil
+    end
+  end
+
+  private def http_pass
+    if Figaro.env.http_pass?
+      Figaro.env.http_pass
+    else
+      Rails.logger.warn('Basic auth failed: ENV["HTTP_PASS"] expected but not found.')
+      nil
     end
   end
 end
