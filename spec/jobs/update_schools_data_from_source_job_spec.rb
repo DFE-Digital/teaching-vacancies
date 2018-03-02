0899f62999de866031293f9100953108487128ea
Despo Pentara <despo@extractmethod.com>
2018-03-02 10:02:34 +0000

    (Chore) imporove testing of school data import Don’t use instance variables in tests exctract and reuse variables

--- spec/jobs/update_schools_data_from_source_job_spec.rb
+++ spec/jobs/update_schools_data_from_source_job_spec.rb
@@ -2,16 +2,32 @@ require 'rails_helper'
 require 'open-uri'
 
 RSpec.describe UpdateSchoolsDataFromSourceJob, type: :job do
-  before do
-    # Associated records that already exist
-    @la_maintained_school_type = SchoolType.create!(label: 'LA maintained school', code: '4')
-    @voluntary_aided_school = DetailedSchoolType.create!(label: 'Voluntary aided school', code: '02')
-    @london = Region.create(name: 'London', code: 'H')
+  let(:datestring) { Time.zone.now.strftime('%Y%m%d') }
+  let!(:school) do
+    School.create!(urn: '100000',
+                   name: '?',
+                   address: '?',
+                   locality: '?',
+                   address3: '?',
+                   town: '?',
+                   county: '?',
+                   postcode: 'PO1 1QW',
+                   minimum_age: 0,
+                   maximum_age: 99,
+                   url: nil,
+                   school_type: SchoolType.new(label: 'Previous school type', code: '999'),
+                   detailed_school_type: DetailedSchoolType.new(label: 'Previous detailed school type',
+                                                                code: '999'),
+                   region: Region.new(name: 'Previous region', code: 'ZZZ'),
+                   phase: 'all_through')
   end
+
+  let!(:la_maintained_school_type) { SchoolType.create!(label: 'LA maintained school', code: '4') }
+  let!(:voluntary_aided_school) { DetailedSchoolType.create!(label: 'Voluntary aided school', code: '02') }
+  let!(:london) { Region.create(name: 'London', code: 'H') }
 
   context 'When the CSV is unavailable' do
     before do
-      datestring = Time.zone.now.strftime('%Y%m%d')
       stub_request(:get, "http://ea-edubase-api-prod.azurewebsites.net/edubase/edubasealldata#{datestring}.csv")
         .to_return(body: 'Not Found', status: 404)
     end
@@ -23,47 +39,25 @@ RSpec.describe UpdateSchoolsDataFromSourceJob, type: :job do
 
   context 'where the CSV contains smart-quotes using Windows 1252 encoding' do
     before do
-      datestring = Time.zone.now.strftime('%Y%m%d')
-
       stub_request(:get, "http://ea-edubase-api-prod.azurewebsites.net/edubase/edubasealldata#{datestring}.csv")
         .to_return(body:
-          'URN,EstablishmentName,EstablishmentTypeGroup (code),' \
-          'TypeOfEstablishment (code),GOR (code),SchoolWebsite,Street,' \
-          "Town,Postcode\n" \
-          "100000,St John\x92s School,999,999,ZZZ,http://test.com,?,?,?")
-
-      @school = School.create!(
-        urn: '100000',
-        name: '?',
-        address: '?',
-        locality: '?',
-        address3: '?',
-        town: '?',
-        county: '?',
-        postcode: 'PO1 1QW',
-        minimum_age: 0,
-        maximum_age: 99,
-        url: nil,
-        school_type: SchoolType.create!(label: 'Previous school type', code: '999'),
-        detailed_school_type: DetailedSchoolType.create!(label: 'Previous detailed school type', code: '999'),
-        region: Region.create!(name: 'Previous region', code: 'ZZZ'),
-        phase: 'all_through'
-      )
+                   'URN,EstablishmentName,EstablishmentTypeGroup (code),' \
+                   'TypeOfEstablishment (code),GOR (code),SchoolWebsite,Street,' \
+                   "Town,Postcode\n" \
+                   "100000,St John\x92s School,999,999,ZZZ,http://test.com,?,?,?")
     end
 
     it 'should correct convert the file to UTF-8' do
       UpdateSchoolsDataFromSourceJob.new.perform
 
-      @school.reload
+      school.reload
 
-      expect(@school.name).to eql('St John’s School')
+      expect(school.name).to eql('St John’s School')
     end
   end
 
   context 'when the CSV is available' do
     before do
-      datestring = Time.zone.now.strftime('%Y%m%d')
-
       csv = File.read(Rails.root.join('spec', 'fixtures', 'example_schools_data.csv'))
       stub_request(:get, "http://ea-edubase-api-prod.azurewebsites.net/edubase/edubasealldata#{datestring}.csv")
         .to_return(body: csv)
@@ -84,9 +78,9 @@ RSpec.describe UpdateSchoolsDataFromSourceJob, type: :job do
         expect(school.minimum_age).to eql(3)
         expect(school.maximum_age).to eql(11)
         expect(school.url).to eql('http://www.sirjohncassprimary.org')
-        expect(school.school_type).to eql(@la_maintained_school_type)
-        expect(school.detailed_school_type).to eql(@voluntary_aided_school)
-        expect(school.region).to eql(@london)
+        expect(school.school_type).to eql(la_maintained_school_type)
+        expect(school.detailed_school_type).to eql(voluntary_aided_school)
+        expect(school.region).to eql(london)
         expect(school.phase).to eql('primary')
         expect(school.easting).to eql('533498')
         expect(school.northing).to eql('181201')
@@ -108,7 +102,7 @@ RSpec.describe UpdateSchoolsDataFromSourceJob, type: :job do
         expect(school.school_type.code).to eql('3')
         expect(school.detailed_school_type.label).to eql('Other independent school')
         expect(school.detailed_school_type.code).to eql('11')
-        expect(school.region).to eql(@london)
+        expect(school.region).to eql(london)
         expect(school.phase).to eql('not_applicable')
         expect(school.easting).to eql('532301')
         expect(school.northing).to eql('181746')
@@ -126,45 +120,25 @@ RSpec.describe UpdateSchoolsDataFromSourceJob, type: :job do
     end
 
     context 'and a school was already in the database' do
-      before do
-        @school = School.create!(
-          urn: '100000',
-          name: '?',
-          address: '?',
-          locality: '?',
-          address3: '?',
-          town: '?',
-          county: '?',
-          postcode: 'PO1 1QW',
-          minimum_age: 0,
-          maximum_age: 99,
-          url: nil,
-          school_type: SchoolType.create!(label: 'Previous school type', code: '999'),
-          detailed_school_type: DetailedSchoolType.create!(label: 'Previous detailed school type', code: '999'),
-          region: Region.create!(name: 'Previous region', code: 'ZZZ'),
-          phase: 'all_through'
-        )
-      end
-
       it 'should update the school details' do
         UpdateSchoolsDataFromSourceJob.new.perform
 
-        @school.reload # Re-fetch from the database
+        school.reload # Re-fetch from the database
 
-        expect(@school.name).to eql("Sir John Cass's Foundation Primary School")
-        expect(@school.address).to eql("St James's Passage")
-        expect(@school.locality).to eql("Duke's Place")
-        expect(@school.address3).to eql(nil)
-        expect(@school.town).to eql('London')
-        expect(@school.county).to eql(nil)
-        expect(@school.postcode).to eql('EC3A 5DE')
-        expect(@school.minimum_age).to eql(3)
-        expect(@school.maximum_age).to eql(11)
-        expect(@school.url).to eql('http://www.sirjohncassprimary.org')
-        expect(@school.school_type).to eql(@la_maintained_school_type)
-        expect(@school.detailed_school_type).to eql(@voluntary_aided_school)
-        expect(@school.region).to eql(@london)
-        expect(@school.phase).to eql('primary')
+        expect(school.name).to eql("Sir John Cass's Foundation Primary School")
+        expect(school.address).to eql("St James's Passage")
+        expect(school.locality).to eql("Duke's Place")
+        expect(school.address3).to eql(nil)
+        expect(school.town).to eql('London')
+        expect(school.county).to eql(nil)
+        expect(school.postcode).to eql('EC3A 5DE')
+        expect(school.minimum_age).to eql(3)
+        expect(school.maximum_age).to eql(11)
+        expect(school.url).to eql('http://www.sirjohncassprimary.org')
+        expect(school.school_type).to eql(la_maintained_school_type)
+        expect(school.detailed_school_type).to eql(voluntary_aided_school)
+        expect(school.region).to eql(london)
+        expect(school.phase).to eql('primary')
       end
     end
   end
