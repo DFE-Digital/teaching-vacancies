f8fab0187c6157145975d8f1c4d4b088c255d9e7
Despo Pentara <despo@extractmethod.com>
2018-03-02 10:00:48 +0000

    (chore) wrap school data import in single transaction dont save associated objects, school creation handles that

--- app/jobs/update_schools_data_from_source_job.rb
+++ app/jobs/update_schools_data_from_source_job.rb
@@ -14,45 +14,48 @@ class UpdateSchoolsDataFromSourceJob < ApplicationJob
     # Convert from Windows-1251 encoding to UTF-8
     file.encode!('UTF-8', 'windows-1251', invalid: :replace)
 
-    CSV.parse(file, headers: true).each do |row|
-      school_type = SchoolType.find_or_initialize_by(code: row['EstablishmentTypeGroup (code)'])
-      school_type.label ||= row['EstablishmentTypeGroup (name)']
-      school_type.save!
+    School.transaction do
+      CSV.parse(file, headers: true).each do |row|
+        school_type = SchoolType.find_or_initialize_by(code: row['EstablishmentTypeGroup (code)'])
+        school_type.label = row['EstablishmentTypeGroup (name)']
 
-      detailed_school_type = DetailedSchoolType.find_or_initialize_by(code: row['TypeOfEstablishment (code)'])
-      detailed_school_type.label ||= row['TypeOfEstablishment (name)']
-      detailed_school_type.save!
+        detailed_school_type = DetailedSchoolType.find_or_initialize_by(code: row['TypeOfEstablishment (code)'])
+        detailed_school_type.label = row['TypeOfEstablishment (name)']
 
-      region = Region.find_or_initialize_by(code: row['GOR (code)'])
-      region.name ||= row['GOR (name)']
-      region.save!
+        region = Region.find_or_initialize_by(code: row['GOR (code)'])
+        region.name = row['GOR (name)']
 
-      school = School.find_or_initialize_by(urn: row['URN'])
-      school.name = row['EstablishmentName']
-      school.address = row['Street']
-      school.locality = row['Locality'].presence
-      school.address3 = row['Address3'].presence
-      school.town = row['Town']
-      school.county = row['County (name)'].presence
-      school.postcode = row['Postcode']
-      school.minimum_age = row['StatutoryLowAge']
-      school.maximum_age = row['StatutoryHighAge']
+        school = School.find_or_initialize_by(urn: row['URN'])
+        school.name = row['EstablishmentName']
+        school.address = row['Street']
+        school.locality = row['Locality'].presence
+        school.address3 = row['Address3'].presence
+        school.town = row['Town']
+        school.county = row['County (name)'].presence
+        school.postcode = row['Postcode']
+        school.minimum_age = row['StatutoryLowAge']
+        school.maximum_age = row['StatutoryHighAge']
 
-      school.easting = row['Easting']
-      school.northing = row['Northing']
+        school.easting = row['Easting']
+        school.northing = row['Northing']
 
-      website = row['SchoolWebsite']
-      website = "http://#{website}" unless website.start_with? 'http'
+        school.url = valid_website(row['SchoolWebsite'])
 
-      school.url = website.presence
+        school.phase = row['PhaseOfEducation (code)'].to_i
 
-      school.phase = row['PhaseOfEducation (code)'].to_i
+        school.school_type = school_type
+        school.detailed_school_type = detailed_school_type
+        school.region = region
 
-      school.school_type = school_type
-      school.detailed_school_type = detailed_school_type
-      school.region = region
-      school.save!
+        school.save
+      end
     end
     # rubocop:enable Metrics/AbcSize
+  end
+
+  private
+
+  def valid_website(url)
+    url.match?(/^http/) ? url : "http://#{url}"
   end
 end
