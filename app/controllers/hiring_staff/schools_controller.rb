bcc7da45cd0420197db28691d273b9975de21baf
Tom Hipkin <tomhipkin@gmail.com>
2018-03-22 16:48:24 +0000

    (chore): Remove School search
    * School search has been useful for demonstrating the publishing flow but now we are limiting what users can do when logged into publishing this is no longer appropriate: a member of hiring staff for one school should never be able to manage other schools

--- app/controllers/hiring_staff/schools_controller.rb
+++ app/controllers/hiring_staff/schools_controller.rb
@@ -26,9 +26,4 @@ class HiringStaff::SchoolsController < HiringStaff::BaseController
   def index
     # Nothing to do here yet
   end
-
-  def search
-    @schools = School.where(['name ILIKE ?', "%#{params[:name]}%"])
-    render 'search_results'
-  end
 end
--- spec/controllers/hiring_staff/schools_controller_spec.rb
+++ spec/controllers/hiring_staff/schools_controller_spec.rb
@@ -1,28 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe HiringStaff::SchoolsController, type: :controller do
-  describe 'index' do
-    before { get :index }
-
-    it 'should render the page' do
-      expect(response.status).to eql(200)
-    end
-  end
-
-  describe 'search' do
-    before do
-      @salisbury_school = create(:school, name: 'Salisbury School')
-      create(:school, name: 'Canterbury School')
-
-      get :search, params: { name: 'salisbury' }
-    end
-
-    it 'should render the page' do
-      expect(response.status).to eql(200)
-    end
-
-    it 'should assign the matching school to @schools' do
-      expect(assigns(:schools)).to match_array([@salisbury_school])
-    end
-  end
 end
