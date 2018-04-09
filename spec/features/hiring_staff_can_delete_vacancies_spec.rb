4e2b64250d4720e2d801bab5080c1df7ec39d2bf
Despo Pentara <despo@extractmethod.com>
2018-04-09 18:02:26 +0100

    (feature) track vacancy deletion

--- spec/features/hiring_staff_can_delete_vacancies_spec.rb
+++ spec/features/hiring_staff_can_delete_vacancies_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 RSpec.feature 'School deleting vacancies' do
   let(:school) { create(:school) }
+  let(:session_id) { SecureRandom.uuid }
+
   before(:each) do
-    stub_hiring_staff_auth(urn: school.urn)
+    stub_hiring_staff_auth(urn: school.urn, session_id: session_id)
   end
 
   scenario 'Hiring staff should see a delete button for a vacancy' do
@@ -27,5 +29,19 @@ RSpec.feature 'School deleting vacancies' do
     expect(page).not_to have_content(vacancy1.job_title)
     expect(page).to have_content(vacancy2.job_title)
     expect(page).to have_content('The vacancy has been deleted')
+  end
+
+  scenario 'Audits the vacancy deletion' do
+    vacancy = create(:vacancy, school: school)
+
+    visit school_path(school)
+
+    within("tr#vacancy_#{vacancy.id}") do
+      click_on 'Delete'
+    end
+
+    activity = vacancy.activities.last
+    expect(activity.session_id).to eq(session_id)
+    expect(activity.key).to eq('vacancy.delete')
   end
 end
