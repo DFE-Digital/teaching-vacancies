76352804b62014230e2ab5206f86f55ac43b5e3e
Tom Hipkin <tomhipkin@gmail.com>
2018-04-17 10:28:39 +0100

    (chore): Add onboarding test user

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -4,6 +4,7 @@ class Permission
     '0535c532-31e2-4170-9846-225f5b1347aa' => '110627', # benwick@
     '3e2728aa-958c-42fe-9890-39f6d8b8496a' => '137138', # bexleyheath@
     '7d842528-52c8-49db-9deb-6bd4d9aaf7c8' => '111692', # fiona@
+    'd486e1aa-180c-4871-8c26-f12a590cdac3' => '137060', # tolworthtest@
   }.freeze
 
   def initialize(identifier:)
