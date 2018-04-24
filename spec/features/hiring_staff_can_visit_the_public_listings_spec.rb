ff6bc3f52f7d2c2b2f7568d72c0b1aee78bcd536
Gaz Aston <gaz@dxw.com>
2018-04-24 15:38:07 +0100

    (feature) hiring staff can link back to school page
    As a temporary workaround, the “My dashboard” link in the header uses the `new_sessions_path` variable. We should revisit this and make a tighter solution.

---
+++ spec/features/hiring_staff_can_visit_the_public_listings_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+require 'permission'
+
+RSpec.feature 'School viewing public listings' do
+  before do
+    OmniAuth.config.test_mode = true
+  end
+
+  after(:each) do
+    OmniAuth.config.mock_auth[:default] = nil
+  end
+
+  let!(:school) { create(:school, urn: '110627') }
+  before(:each) do
+    stub_const('Permission::USER_TO_SCHOOL_MAPPING', 'a-valid-oid' => school.urn)
+  end
+
+  scenario 'A signed in school should see a link back to their own dashboard when viewing public listings' do
+    OmniAuth.config.mock_auth[:default] = OmniAuth::AuthHash.new(
+      provider: 'default',
+      extra: {
+        raw_info: {
+          id_token_claims: {
+            oid: 'a-valid-oid'
+          }
+        }
+      }
+    )
+
+    visit root_path
+
+    click_on(I18n.t('nav.sign_in'))
+    expect(page).to have_content("Vacancies at #{school.name}")
+    within('#proposition-links') { expect(page).to have_content(I18n.t('nav.school_page_link')) }
+
+    click_on(I18n.t('app.title'))
+    expect(page).to have_content(I18n.t('vacancies.heading'))
+
+    click_on(I18n.t('nav.school_page_link'))
+    expect(page).to have_content("Vacancies at #{school.name}")
+  end
+end
