4bb1ecfcc2156d3182583e25eb7d3baae60dd262
Tom Hipkin <tomhipkin@gmail.com>
2018-04-30 12:46:04 +0100

    (chore): Database Cleaner config isolated
    * Inline with the previous commit that extracted the Capybara setup to make the Rails helper easier to read and work with, this was another obvious candidate which was reinforced by the need for a comment.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'factory_girl_rails'
+require 'database_cleaner_helper'
 require 'browser_test_helper'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
@@ -17,27 +18,6 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-
-  # Database cleaner
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 
   config.include FactoryGirl::Syntax::Methods
   config.include ActionView::Helpers::NumberHelper
