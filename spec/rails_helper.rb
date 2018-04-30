e83d266109d91e11bc4d8f6e67ce524f12085051
Tom Hipkin <tomhipkin@gmail.com>
2018-04-30 12:43:42 +0100

    (chore): Capybara setup extracted
    * During this story I chose to experiement with Puffing Billy to see if I could test GA events (I couldn’t) but in doing so it was a bit tricky to unpick what was capybara related. Even though Puffing Billy isn’t going to be committed I think this decoupling makes things easier to work with.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,20 +1,16 @@
 require 'spec_helper'
 require 'es_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'capybara/rspec'
-require 'capybara/poltergeist'
 require 'factory_girl_rails'
+require 'browser_test_helper'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ['--load-images=false'])
-end
-Capybara.javascript_driver = :poltergeist
 
 ActiveRecord::Migration.maintain_test_schema!
 
