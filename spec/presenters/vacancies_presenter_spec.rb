222d976ff3cf748d728307a8719d46ec10954e3e
Gaz Aston <gaz@dxw.com>
2018-05-01 10:29:22 +0100

    (feature) job count text is more contextual
    Previously, the initial view of the vacancies#index page would say ‘there are n vacancies that match your search’ even though as far as the user is concerned, they hadn’t ‘searched’ yet.
    
    - the `VacanciesController` has a new private method `searched?` that checks for the presence of the `:commit` key in the query params.
    - this method is passed into new instances of the `VacanciesPresenter` where it’s used to conditionally return the correct text via the `total_count` method

--- spec/presenters/vacancies_presenter_spec.rb
+++ spec/presenters/vacancies_presenter_spec.rb
@@ -3,9 +3,9 @@ RSpec.describe VacanciesPresenter do
   describe '#each' do
     it 'is delegated to the decorated collection' do
       vacancies = create_list(:vacancy, 3)
-
+      searched = true
       decorated_vacancies = vacancies.map { |v| VacancyPresenter.new(v) }
-      vacancies_presenter = VacanciesPresenter.new(vacancies)
+      vacancies_presenter = VacanciesPresenter.new(vacancies, searched: searched)
       allow(vacancies_presenter).to receive(:decorated_collection).and_return(decorated_vacancies)
 
       expect(decorated_vacancies).to receive(:each)
@@ -20,8 +20,8 @@ RSpec.describe VacanciesPresenter do
       Vacancy.__elasticsearch__.client.indices.flush
 
       vacancies = Vacancy.search('Teacher').records
-
-      vacancies_presenter = VacanciesPresenter.new(vacancies)
+      searched = true
+      vacancies_presenter = VacanciesPresenter.new(vacancies, searched: searched)
       expect(vacancies_presenter.total_count).to eq(I18n.t('vacancies.vacancy_count', count: 1))
     end
 
@@ -32,9 +32,31 @@ RSpec.describe VacanciesPresenter do
       Vacancy.__elasticsearch__.client.indices.flush
 
       vacancies = Vacancy.search('Teacher').records
-
-      vacancies_presenter = VacanciesPresenter.new(vacancies)
+      searched = true
+      vacancies_presenter = VacanciesPresenter.new(vacancies, searched: searched)
       expect(vacancies_presenter.total_count).to eq(I18n.t('vacancies.vacancy_count_plural', count: 3))
+    end
+
+    it 'returns the correct number for a single vacancy without a search', elasticsearch: true do
+      create(:vacancy, job_title: 'School teacher')
+      Vacancy.__elasticsearch__.client.indices.flush
+
+      vacancies = Vacancy.search('Teacher').records
+      searched = false
+      vacancies_presenter = VacanciesPresenter.new(vacancies, searched: searched)
+      expect(vacancies_presenter.total_count).to eq(I18n.t('vacancies.vacancy_count_without_search', count: 1))
+    end
+
+    it 'returns the correct number for multiple vacancies without a search', elasticsearch: true do
+      create(:vacancy, job_title: 'School teacher')
+      create(:vacancy, job_title: 'Math teacher')
+      create(:vacancy, job_title: 'English teacher')
+      Vacancy.__elasticsearch__.client.indices.flush
+
+      vacancies = Vacancy.search('Teacher').records
+      searched = false
+      vacancies_presenter = VacanciesPresenter.new(vacancies, searched: searched)
+      expect(vacancies_presenter.total_count).to eq(I18n.t('vacancies.vacancy_count_plural_without_search', count: 3))
     end
   end
 end
