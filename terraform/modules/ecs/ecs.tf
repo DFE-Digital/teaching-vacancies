b307c624195131ddb4e81992940f081716ecd4a1
Chris Wright <chris@dxw.com>
2018-05-02 23:19:43 +0100

    (Chore) Add logspout service
    * Uses `aws_task_definition.logspout` as the task definition
    * Runs in the created ecs cluster
    * `ecs_logspout_task_count` should be set to the autoscaling group
    minimum instance count and step increased as the instances autoscale up
    * Places one container per instance
    * Subsequent `desired_count` changes in terraform are ignored with the
    use of `ignore_changes` in `lifecycle`, ensuring it will not be set back
    to it's initial value causing containers to be destroyed

--- terraform/modules/ecs/ecs.tf
+++ terraform/modules/ecs/ecs.tf
@@ -34,6 +34,23 @@ resource "aws_ecs_service" "web" {
   depends_on = ["aws_iam_role.ecs_role"]
 }
 
+resource "aws_ecs_service" "logspout" {
+  name            = "logspout-${var.environment}"
+  cluster         = "${aws_ecs_cluster.cluster.id}"
+  task_definition = "${aws_ecs_task_definition.logspout.arn}"
+  desired_count   = "${var.ecs_logspout_task_count}"
+
+  deployment_minimum_healthy_percent = 50
+
+  placement_constraints {
+    type = "distinctInstance"
+  }
+
+  lifecycle {
+    ignore_changes = ["desired_count"]
+  }
+}
+
 /*====
 ECS task definitions
 ======*/
--- terraform/modules/ecs/input.tf
+++ terraform/modules/ecs/input.tf
@@ -34,6 +34,7 @@ variable "ecs_service_logspout_container_definition_file_path" {}
 variable "logspout_command" {
   type = "list"
 }
+variable "ecs_logspout_task_count" {}
 
 variable "import_schools_task_command" {
   type = "list"
