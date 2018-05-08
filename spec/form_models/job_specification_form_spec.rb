45207bd524dfcb357406d277a990e2c7c221421f
Gaz Aston <gaz@dxw.com>
2018-05-08 13:37:36 +0100

    (chore) change minimum salary error message

--- spec/form_models/job_specification_form_spec.rb
+++ spec/form_models/job_specification_form_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe JobSpecificationForm, type: :model do
           create(:pay_scale, salary: 3000)
           expect(job_specification.valid?). to be false
           expect(job_specification.errors.messages[:minimum_salary][0])
-            . to eq('must be at least equal to the minimum pay range of £3,000')
+            . to eq('must be at least £3,000')
         end
       end
     end
