9fa73f3f81db91047bee72c5033c6ce2600243ae
Despo Pentara <despo@extractmethod.com>
2018-05-09 18:53:24 +0100

    (chore) updates to latest vacancies
    & extend script to reset working_pattern

--- spec/lib/tasks/vacancies_rake_spec.rb
+++ spec/lib/tasks/vacancies_rake_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe ':vacancies' do
       create(:vacancy, slug: 'teacher-of-french', min_pay_scale: nil)
       create(:vacancy, slug: 'teacher-of-maths-maternity-cover', job_title: 'Another french teacher',
                        updated_at: Time.zone.now + 2.minutes)
+      create(:vacancy, slug: 'teaching-assistants', working_pattern: 'part_time')
 
       Rake::Task['vacancies:data:update'].invoke
       index = 20 # position in data file
@@ -28,6 +29,7 @@ RSpec.describe ':vacancies' do
       other_vacancy = Vacancy.find_by(slug: 'teacher-of-technology-and-art-part-time-temporary')
       pay_scale_vacancy = Vacancy.find_by(slug: 'teacher-of-french')
       edited_vacancy = Vacancy.find_by(slug: 'teacher-of-maths-maternity-cover')
+      reset_working_pattern = Vacancy.find_by(slug: 'teaching-assistants')
 
       expect(vacancy.experience).to eq(sanitize(data[index]['experience']))
       expect(vacancy.job_title).to eq(data[index]['job_title'])
@@ -39,6 +41,7 @@ RSpec.describe ':vacancies' do
       expect(pay_scale_vacancy.min_pay_scale).to eq(mps)
       expect(pay_scale_vacancy.max_pay_scale).to eq(ups)
       expect(edited_vacancy.job_title).to eq('Another french teacher')
+      expect(reset_working_pattern.working_pattern).to eq(nil)
     end
   end
 
