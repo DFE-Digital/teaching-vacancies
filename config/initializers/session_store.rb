fd044b5856ca6f58514d81a8d0c4769c8bdb9683
Despo Pentara <despo@extractmethod.com>
2018-05-28 13:48:57 +0100

    (chore) expire `_tvs_session` cookie after an hour and rename to `_teachingjobs_sesssion`

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
-Rails.application.config.session_store :active_record_store, key: '_tvs_session'
+Rails.application.config.session_store :active_record_store,
+                                       key: '_teachingjobs_session',
+                                       expire_after: 1.hour
