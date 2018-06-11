aa01c5375e351f5b48f17f5ff3a0c93bcc903e45
Gaz Aston <gaz@dxw.com>
2018-06-11 14:02:46 +0100

    (fix) make capybara helper .find more specific

--- spec/support/capybara_helper.rb
+++ spec/support/capybara_helper.rb
@@ -1,6 +1,6 @@
 module CapybaraHelper
   def click_link_in_container_with_text(text)
-    find(:xpath, "//tr[td[contains(text(), '#{text}')]]").find('a').click
+    find(:xpath, "//tr[td[contains(text(), '#{text}')]]").find('td.cya-change a').click
   end
 
   def within_row_for(element: 'label', text:, &block)
