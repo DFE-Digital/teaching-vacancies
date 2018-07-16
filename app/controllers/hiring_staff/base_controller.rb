fc6c46d9172466854ce65f6fdfa4755bd957417c
Tom Hipkin <tomhipkin@gmail.com>
2018-07-16 18:11:54 +0100

    (fix): My jobs nav link doesn’t go via Sign In
    * This needed to change as the way we sign in is about to include a new step so this original quick hack to root ‘My Jobs’ to the sign in action to figure out can no longer be used. Good opportunity to make it better.

--- app/controllers/hiring_staff/base_controller.rb
+++ app/controllers/hiring_staff/base_controller.rb
@@ -7,10 +7,6 @@ class HiringStaff::BaseController < ApplicationController
     redirect_to new_sessions_path unless session.key?(:urn)
   end
 
-  def current_school
-    @current_school ||= School.find_by! urn: session[:urn]
-  end
-
   def current_session_id
     session[:session_id]
   end
