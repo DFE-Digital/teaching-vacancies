db5d699dcc779bdc56d30d3b4249f2045072ec8f
Tom Hipkin <tomhipkin@gmail.com>
2018-07-17 14:01:14 +0100

    (feature): Other identification option informs the user they cannot conintue yet

--- app/controllers/hiring_staff/identifications_controller.rb
+++ app/controllers/hiring_staff/identifications_controller.rb
@@ -2,11 +2,19 @@ class HiringStaff::IdentificationsController < HiringStaff::BaseController
   skip_before_action :check_session, only: %i[new create]
   skip_before_action :verify_authenticity_token, only: [:create]
 
-  DFE_SIGN_IN_OPTIONS = [
-    OpenStruct.new(name: 'Central Bedfordshire', to_radio: ['Central Bedfordshire', 'Central Bedfordshire']),
+  before_action :halt_other_regions, only: [:create]
+
+  OTHER_SIGN_IN_OPTION = [
     OpenStruct.new(
-      name: 'Any other local authority',
-      to_radio: ['Any other local authority', 'Any other local authority']
+      name: 'Other',
+      to_radio: ['Other', 'Other']
+    )
+  ].freeze
+
+  DFE_SIGN_IN_OPTIONS = [
+    OpenStruct.new(
+      name: 'Milton Keynes',
+      to_radio: ['Milton Keynes', 'Milton Keynes']
     )
   ].freeze
 
@@ -17,18 +25,27 @@ class HiringStaff::IdentificationsController < HiringStaff::BaseController
 
   def new
     @identification_options =
-      AZURE_SIGN_IN_OPTIONS.map(&:to_radio) + DFE_SIGN_IN_OPTIONS.map(&:to_radio)
+      AZURE_SIGN_IN_OPTIONS.map(&:to_radio) +
+      DFE_SIGN_IN_OPTIONS.map(&:to_radio) +
+      OTHER_SIGN_IN_OPTION.map(&:to_radio)
   end
 
   def create
     sign_in_path = new_azure_path
     sign_in_path = new_dfe_path if DFE_SIGN_IN_OPTIONS.map(&:name).include?(choice)
 
-    logger.debug("Hiring staff identified as from the #{choice} local authority during sign in.")
+    logger.debug("Hiring staff identified as from the #{choice} district during sign in.")
     redirect_to sign_in_path
   end
 
   def choice
     params.require('identifications').permit('name')['name']
+  end
+
+  private def halt_other_regions
+    return unless choice.eql?('Other')
+    flash[:notice] = "Other areas have not been invited yet, \
+      please register your interest by emailing us at #{I18n.t('help.email')}."
+    redirect_to root_path
   end
 end
--- spec/controllers/hiring_staff/identifications_controller_spec.rb
+++ spec/controllers/hiring_staff/identifications_controller_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe HiringStaff::IdentificationsController, type: :controller do
   describe '#create' do
+    context 'when the area is other' do
+      it 'redirects to the root path with an explanation' do
+        choice = HiringStaff::IdentificationsController::OTHER_SIGN_IN_OPTION.first.name
+        post :create, params: { identifications: { name: choice } }
+        expect(controller).to set_flash[:notice]
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
     context 'when the area is being serviced by Azure' do
       HiringStaff::IdentificationsController::AZURE_SIGN_IN_OPTIONS.each do |option|
         context "and the region is #{option.name}" do
