17b764b2d40b8b76f5eb0ae295da1ddefa0d3fc6
Tom Hipkin <tomhipkin@gmail.com>
2018-07-17 17:37:41 +0100

    (fix) test description typo

--- spec/features/hiring_staff_can_only_see_their_school_spec.rb
+++ spec/features/hiring_staff_can_only_see_their_school_spec.rb
@@ -12,7 +12,7 @@ RSpec.feature 'Hiring staff can only see their school' do
     end
   end
 
-  context 'when the session in is NOT connected to a known school' do
+  context 'when the session is NOT connected to a known school' do
     scenario 'returns a 404' do
       create(:school)
       stub_hiring_staff_auth(urn: 'foo')
