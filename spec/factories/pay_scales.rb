d2ed9dcce65f2ee3ac4bae1e41266523a71f864a
Robbie Paul <robbie.paul@dxw.com>
2018-07-19 12:35:28 +0100

    (refactor) Force `code` in PayScale factory to be unique
    As we have a unique index on `code` & `expires_at` our factory will
    quickly create invalid records causing our tests to fail.
    
    First attempted to use `Faker::Code.unqiue.asin` however I ran in to the
    `Faker::UniqueGenerator::RetryLimitExceeded` error. By using a sequence
    we can avoid duplication in our factories.

--- spec/factories/pay_scales.rb
+++ spec/factories/pay_scales.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :pay_scale do
     label { Faker::Lorem.unique.words(2).join(' ') }
-    code { Faker::Code.asin }
+    sequence(:code) { |n| "MPS#{n}" }
     salary { Faker::Number.number(4) }
     starts_at { Time.zone.today - 5.months }
     expires_at { Time.zone.today + 5.months }
