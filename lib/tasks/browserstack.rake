afe6bdb47915a2edf1461da903509fe763d86504
Robbie Paul <robbie.paul@dxw.com>
2018-08-01 15:43:30 +0100

    (feature) Add browserstack:all rake task
    We need to be able to test all our browsers with one task so we can make
    it part of our CI process.
    
    This task loops through our browser_caps config and runs our main task
    each time with a different TASK_ID.

--- lib/tasks/browserstack.rake
+++ lib/tasks/browserstack.rake
@@ -1,3 +1,5 @@
+CONFIG = YAML.safe_load(File.read(File.join(File.dirname(__FILE__), '../../config/browserstack.yml')))
+
 namespace :browserstack do
   RSpec::Core::RakeTask.new(:local) do |t|
     ENV['TEST_BROWSER'] = 'browserstack'
@@ -5,5 +7,13 @@ namespace :browserstack do
     t.rspec_opts = '--format documentation --tag browserstack'
     t.verbose = false
   end
+
+  task :all do
+    CONFIG['browser_caps'].each_with_index do |_browser, i|
+      ENV['TASK_ID'] = i.to_s
+      Rake::Task['browserstack:local'].execute
+    end
+  end
+
   task default: :local
 end
--- spec/browser_test_helper.rb
+++ spec/browser_test_helper.rb
@@ -8,13 +8,11 @@ if ENV['TEST_BROWSER'] == 'browserstack'
   # monkey patch to avoid reset sessions
   class Capybara::Selenium::Driver < Capybara::Driver::Base
     def reset!
-      @browser.navigate.to('about:blank') if @browser
+      @browser&.navigate&.to('about:blank')
     end
   end
 
-  CONFIG = YAML.safe_load(File.read(File.join(File.dirname(__FILE__), '../config/browserstack.yml')))
-  CONFIG['user'] = ENV['BROWSERSTACK_USERNAME']
-  CONFIG['key'] = ENV['BROWSERSTACK_ACCESS_KEY']
+  CONFIG ||= YAML.safe_load(File.read(File.join(File.dirname(__FILE__), '../config/browserstack.yml')))
   TASK_ID = ENV['TASK_ID'] ||= '0'
 
   Capybara.register_driver :browserstack do |app|
@@ -22,11 +20,12 @@ if ENV['TEST_BROWSER'] == 'browserstack'
     caps['browserstack.local'] = true
 
     @browserstack_local = BrowserStack::Local.new
-    @browserstack_local.start('key' => (CONFIG['key']).to_s, 'forcelocal' => true)
+    @browserstack_local.start('key' => (ENV['BROWSERSTACK_ACCESS_KEY']).to_s, 'forcelocal' => true)
 
+    url = "http://#{ENV['BROWSERSTACK_USERNAME']}:#{ENV['BROWSERSTACK_ACCESS_KEY']}@hub-cloud.browserstack.com/wd/hub"
     Capybara::Selenium::Driver.new(app,
                                    browser: :remote,
-                                   url: "http://#{CONFIG['user']}:#{CONFIG['key']}@hub-cloud.browserstack.com/wd/hub",
+                                   url: url,
                                    desired_capabilities: caps)
   end
 
@@ -34,13 +33,13 @@ if ENV['TEST_BROWSER'] == 'browserstack'
   Capybara.javascript_driver = :browserstack
   Capybara.current_driver = :browserstack
   Capybara.run_server = true
-  Capybara.ignore_hidden_elements = true
+  Capybara.ignore_hidden_elements = false
 
   at_exit do
     @browserstack_local&.stop
   end
 
-  puts 'Running specs with BrowserStack'
+  puts "Running specs with BrowserStack using #{CONFIG['browser_caps'][TASK_ID.to_i]['browser']}"
 
 else
 
