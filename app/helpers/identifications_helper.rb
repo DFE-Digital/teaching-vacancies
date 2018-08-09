28c1d93167ef5ce35847afe04caa92c488d7abc2
Robbie Paul <robbie.paul@dxw.com>
2018-08-09 15:25:43 +0100

    (chore) Remove Milton Keynes as a sign in option
    We're no longer using this area to test DfE Sign-in instead we'll use
    the two areas we're rolled out in

--- app/helpers/identifications_helper.rb
+++ app/helpers/identifications_helper.rb
@@ -1,6 +1,5 @@
 module IdentificationsHelper
   DFE_SIGN_IN_OPTIONS = [
-    OpenStruct.new(name: 'milton_keynes', to_radio: ['milton_keynes', 'Milton Keynes']),
     OpenStruct.new(name: 'cambridgeshire', to_radio: ['cambridgeshire', 'Cambridgeshire']),
     OpenStruct.new(name: 'the_north_east', to_radio: ['the_north_east', 'The North East'])
   ].freeze
