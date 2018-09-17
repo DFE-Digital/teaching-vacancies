ec557d9b63ce1a85d918a9900a958cc6052a9ff2
Esther Olatunde <olatundeesther@gmail.com>
2018-09-17 12:41:46 +0100

    (feature) Modified test specs for job seekers filtering by NQTs, and change elasticsearch index type to boolean

--- app/services/vacancy_filters.rb
+++ app/services/vacancy_filters.rb
@@ -10,9 +10,9 @@ class VacancyFilters
     @keyword = args[:keyword]
     @minimum_salary = args[:minimum_salary]
     @maximum_salary = args[:maximum_salary]
+    @newly_qualified_teacher = args[:newly_qualified_teacher]
     @working_pattern = extract_working_pattern(args)
     @phase = School.phases.include?(args[:phase]) ? args[:phase] : nil
-    @newly_qualified_teacher = args[:newly_qualified_teacher]
   end
 
   def to_hash
--- spec/features/job_seekers_can_filter_vacancies_spec.rb
+++ spec/features/job_seekers_can_filter_vacancies_spec.rb
@@ -201,19 +201,37 @@ RSpec.feature 'Filtering vacancies' do
     end
   end
 
-  scenario 'Filterable by newly qualified teacher', elasticsearch: true do
-    nqt_suitable_vacancy = create(:vacancy, :published, newly_qualified_teacher: true)
-    nqt_not_suitable_vacancy = create(:vacancy, :published, newly_qualified_teacher: false)
+  context 'Filterable by newly qualified teacher', elasticsearch: true do
+    scenario 'Suitable for NQTs', elasticsearch: true do
+      nqt_suitable_vacancy = create(:vacancy, :published, newly_qualified_teacher: true)
+      not_nqt_suitable_vacancy = create(:vacancy, :published, newly_qualified_teacher: false)
 
-    Vacancy.__elasticsearch__.client.indices.flush
-    visit jobs_path
+      Vacancy.__elasticsearch__.client.indices.flush
+      visit jobs_path
 
-    within '.filters-form' do
-      select 'Suitable', from: 'newly_qualified_teacher'
-      page.find('.button[type=submit]').click
+      within '.filters-form' do
+        select 'Suitable', from: 'newly_qualified_teacher'
+        page.find('.button[type=submit]').click
+      end
+
+      expect(page).to have_content(nqt_suitable_vacancy.job_title)
+      expect(page).not_to have_content(not_nqt_suitable_vacancy.job_title)
     end
 
-    expect(page).to have_content(nqt_suitable_vacancy.job_title)
-    expect(page).not_to have_content(nqt_not_suitable_vacancy.job_title)
+    scenario 'Not suitable for NQTs', elasticsearch: true do
+      nqt_suitable_vacancy = create(:vacancy, :published, newly_qualified_teacher: true)
+      not_nqt_suitable_vacancy = create(:vacancy, :published, newly_qualified_teacher: false)
+
+      Vacancy.__elasticsearch__.client.indices.flush
+      visit jobs_path
+
+      within '.filters-form' do
+        select 'Not suitable', from: 'newly_qualified_teacher'
+        page.find('.button[type=submit]').click
+      end
+
+      expect(page).to have_content(not_nqt_suitable_vacancy.job_title)
+      expect(page).not_to have_content(nqt_suitable_vacancy.job_title)
+    end
   end
 end
