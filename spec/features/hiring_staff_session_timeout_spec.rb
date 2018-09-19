d4a31df8866c1cfe0022978b021d19a7e783b6ff
Esther Olatunde <olatundeesther@gmail.com>
2018-09-19 17:30:47 +0100

    (chore) Increases session timeout to 8 hours and added test spec for that

---
+++ spec/features/hiring_staff_session_timeout_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature 'Hiring staff session' do
+  let(:school) { create(:school) }
+  let(:session_id) { 'session_id' }
+  let(:current_user) { User.find_by(oid: session_id) }
+  before do
+    stub_hiring_staff_auth(urn: school.urn, session_id: session_id)
+  end
+
+  after do
+    Timecop.return
+  end
+
+  it 'expires after 8 hours and redirects to login page' do
+    visit new_school_job_path
+
+    Timecop.travel(8.hours)
+
+    click_on 'Save and continue'
+
+    expect(page.current_path).to eq new_identifications_path
+  end
+
+  it 'does\'nt expire before 8 hours' do
+    visit new_school_job_path
+
+    Timecop.travel(1.hour)
+
+    click_on 'Save and continue'
+
+    expect(page.current_path).to eq job_specification_school_job_path
+  end
+end
