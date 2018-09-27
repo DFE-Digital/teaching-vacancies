a33b89d9f114cdc06c6f57f4ac11546e74e38722
Gaz Aston <gaz@dxw.com>
2018-09-27 15:34:00 +0100

    (feature) jobs#show PT salaries state pro rata
    # Conflicts:
    #	app/views/vacancies/_show.html+phone.haml

--- spec/features/hiring_staff_can_specify_a_part_time_role.rb
+++ spec/features/hiring_staff_can_specify_a_part_time_role.rb
@@ -23,18 +23,65 @@ RSpec.feature 'Hiring staff can specify a part time role' do
   end
 
   scenario 'the list page should indicate a pro rata salary if the role is part time', elasticsearch: true do
-    create(:vacancy, working_pattern: :full_time)
     create(:vacancy, working_pattern: :part_time)
 
     Vacancy.__elasticsearch__.client.indices.flush
 
     visit jobs_path
 
-    expect(page.find('.vacancy:eq(2)')).to have_content('Full time')
-    expect(page.find('.vacancy:eq(2)')).to have_content(I18n.t('jobs.per_year'))
-    expect(page.find('.vacancy:eq(2)')).not_to have_content(I18n.t('jobs.per_year_pro_rata'))
-
     expect(page.find('.vacancy:eq(1)')).to have_content('Part time')
     expect(page.find('.vacancy:eq(1)')).to have_content(I18n.t('jobs.per_year_pro_rata'))
+  end
+
+  scenario 'the list page should not indicate a pro rata salary if the role is full time', elasticsearch: true do
+    create(:vacancy, working_pattern: :full_time)
+
+    Vacancy.__elasticsearch__.client.indices.flush
+
+    visit jobs_path
+
+    expect(page.find('.vacancy:eq(1)')).to have_content('Full time')
+    expect(page.find('.vacancy:eq(1)')).to have_content(I18n.t('jobs.per_year'))
+    expect(page.find('.vacancy:eq(1)')).not_to have_content(I18n.t('jobs.per_year_pro_rata'))
+  end
+
+  scenario 'a job page should indicate a pro rata salary if the role is part time' do
+    school = create(:school)
+    vacancy = create(:vacancy, :published, school: school, working_pattern: :part_time)
+
+    visit job_path(vacancy.id)
+
+    within('.vacancy--metadata') { expect(page).to have_content('pro rata') }
+  end
+
+  scenario 'a job page should not indicate a pro rata salary if the role is full time' do
+    school = create(:school)
+    vacancy = create(:vacancy, :published, school: school, working_pattern: :full_time)
+
+    visit job_path(vacancy.id)
+
+    within('.vacancy--metadata') { expect(page).not_to have_content('pro rata') }
+  end
+
+  context 'when the user is on mobile' do
+    scenario 'a job page should indicate a pro rata salary if the role is part time' do
+      page.driver.header('User-Agent', USER_AGENTS['MOBILE_CHROME'])
+      school = create(:school)
+      vacancy = create(:vacancy, :published, school: school, working_pattern: :part_time)
+
+      visit job_path(vacancy.id)
+
+      within('.vacancy table.check-your-answers') { expect(page).to have_content('pro rata') }
+    end
+
+    scenario 'a job page should not indicate a pro rata salary if the role is full time' do
+      page.driver.header('User-Agent', USER_AGENTS['MOBILE_CHROME'])
+      school = create(:school)
+      vacancy = create(:vacancy, :published, school: school, working_pattern: :full_time)
+
+      visit job_path(vacancy.id)
+
+      within('.vacancy table.check-your-answers') { expect(page).not_to have_content('pro rata') }
+    end
   end
 end
