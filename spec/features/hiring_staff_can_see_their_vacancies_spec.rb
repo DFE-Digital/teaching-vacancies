e1787b74b3ee65a3dfb40b755b38900281d30596
Despo Pentara <despo@extractmethod.com>
2018-10-26 12:49:04 +0100

    Revert "[Feature] Render job views on school and school/vacancy pages"

--- spec/features/hiring_staff_can_see_their_vacancies_spec.rb
+++ spec/features/hiring_staff_can_see_their_vacancies_spec.rb
@@ -1,33 +1,17 @@
 require 'rails_helper'
 
-RSpec.feature 'Hiring staff viewing their vacancies' do
-  let(:school) { create(:school) }
-  let!(:vacancy) { create(:vacancy, school: school, status: 'published') }
+RSpec.feature 'Hiring staff can see their vacancies' do
+  scenario 'school with geolocation' do
+    school = create(:school, northing: '1', easting: '2')
 
-  before(:each) do
     stub_hiring_staff_auth(urn: school.urn)
-  end
+    vacancy = create(:vacancy, school: school, status: 'published')
 
-  scenario 'can get to the vacancy preview page' do
     visit school_path
 
     click_on(vacancy.job_title)
 
     expect(page).to have_content(vacancy.job_title)
     expect(page).to have_content(vacancy.job_description)
-  end
-
-  scenario 'can see stats for a vacancy' do
-    get_information_count = 4
-    get_information_count.times do
-      Auditor::Audit.new(vacancy, 'vacancy.get_more_information', nil).log
-    end
-    visit school_job_path(vacancy.id)
-
-    within '#statistics' do
-      expect(page).to have_content(vacancy.weekly_pageviews)
-      expect(page).to have_content(vacancy.total_pageviews)
-      expect(page).to have_content(get_information_count)
-    end
   end
 end
