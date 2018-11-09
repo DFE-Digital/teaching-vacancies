cc4e8861465b575fe544c0664df39a259317d513
Despo Pentara <despo@extractmethod.com>
2018-11-09 14:26:10 +0000

    (chore) add Message Encryptor

---
+++ spec/lib/message_encryptor_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'message_encryptor'
+
+RSpec.describe 'MessageEnryptor' do
+  describe 'it can #encrypt and #decrypt data' do
+    it 'an array' do
+      data = ['an', 'array', 'of', 'data']
+      encrypted_data = MessageEncryptor.new(data).encrypt
+
+      expect(MessageEncryptor.new(encrypted_data).decrypt).to eq(data)
+    end
+
+    it 'a string' do
+      data = 'The quick brown fox'
+      encrypted_data = MessageEncryptor.new(data).encrypt
+
+      expect(MessageEncryptor.new(encrypted_data).decrypt).to eq(data)
+    end
+  end
+end
