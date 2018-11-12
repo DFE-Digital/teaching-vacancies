8bd4aeb8a4d428e14e91b358f6003dd8e5ff7bbe
Laura Porter <laura@dxw.com>
2018-11-12 10:53:55 +0000

    (fix) Only show these warnings in Rails development env
    I wanted to test this, perhaps in spec/services/teacher_vacancy_authorisation_spec.rb but it's hard to test initializers with rspec. I can keep plugging away at it if it's important?
    
    We decided to only show these warnings in development. In production this warning will be handled by Rollbar (because the app as a whole just won't function wihout the authorisation service)

--- config/initializers/authorisation_service.rb
+++ config/initializers/authorisation_service.rb
@@ -1,10 +1,10 @@
 AUTHORISATION_SERVICE_URL = ENV['AUTHORISATION_SERVICE_URL']
 AUTHORISATION_SERVICE_TOKEN = ENV['AUTHORISATION_SERVICE_TOKEN']
 
-if AUTHORISATION_SERVICE_URL.nil?
+if AUTHORISATION_SERVICE_URL.nil? && Rails.env.development?
   Rails.logger.error('***No authorisation service url configured. To configure set AUTHORISATION_SERVICE_URL')
 end
 
-if AUTHORISATION_SERVICE_TOKEN.nil?
+if AUTHORISATION_SERVICE_TOKEN.nil? && Rails.env.development?
   Rails.logger.error('***No authorisation service token configured. To configure set AUTHORISATION_SERVICE_TOKEN')
 end
