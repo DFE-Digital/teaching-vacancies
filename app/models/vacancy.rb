6f5cf08f55abdc07bcfeecbe54283a72eb5d46a4
Laura Porter <laura@dxw.com>
2018-12-10 10:21:41 +0000

    Revert "(wip) Extremely basic list of published, draft, pending and expired vacancies"
    This reverts commit 02cbe85c2fe6ab4a7fe13e413fcbdd7dcfee5af3.

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -95,6 +95,7 @@ class Vacancy < ApplicationRecord
 
   scope :applicable, (-> { where('expires_on >= ?', Time.zone.today) })
   scope :active, (-> { where(status: %i[published draft]) })
+  scope :draft, (-> { where(status: %i[draft]) })
   scope :listed, (-> { published.where('publish_on <= ?', Time.zone.today) })
   scope :published_on_count, (->(date) { published.where('date(publish_on) = ?', date).count })
   scope :pending, (-> { published.where('publish_on > ?', Time.zone.today) })
