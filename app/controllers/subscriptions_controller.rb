7b0b63853408068f7ed2ce2c52fd7a7f85c1cde1
Despo Pentara <despo@extractmethod.com>
2018-12-12 14:15:47 +0000

    (feature) Email alerts: Create a subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,16 +5,31 @@ class SubscriptionsController < ApplicationController
   end
 
   def create
-    subscription = Subscription.new(subscription_params)
+    flash.clear
+    subscription = Subscription.new(daily_subscription_params)
     @subscription = SubscriptionPresenter.new(subscription)
 
-    render 'new' unless @subscription.valid?
+    if Subscription.ongoing.exists?(email: daily_subscription_params[:email],
+                                    search_criteria: daily_subscription_params[:search_criteria],
+                                    frequency: daily_subscription_params[:frequency])
+      flash[:error] = I18n.t('errors.subscriptions.already_exists')
+    elsif @subscription.save
+      flash[:notice] = I18n.t('messages.subscriptions.created')
+      return render 'confirm'
+    end
+
+    render 'new'
   end
 
   private
 
   def subscription_params
     params.require(:subscription).permit(:email, :reference, :search_criteria)
+  end
+
+  def daily_subscription_params
+    subscription_params.merge(expires_on: 3.months.from_now,
+                              frequency: :daily)
   end
 
   def search_criteria
