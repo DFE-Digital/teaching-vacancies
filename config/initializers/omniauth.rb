fb57d209bc5796fe4a8f941460fdf38ea35bc8b4
Tom Hipkin <tomhipkin@gmail.com>
2018-12-12 14:51:38 +0000

    (refactor) Omniauth configuration simplified
    * We didn’t need to use a root `issuer` option, this appears to be being put together from the client_options.
    * Instead of having lots of ENV examples being set for Docker build, explain explicitly why we need it and switch to a simpler guard statement that checks for the most important ENV variable. This is probably good enough as if it is the only variable available, our deployments won’t mark a new container that is full of ruby errors as healthy.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -45,30 +45,29 @@ end
 
 class OmniAuth::Strategies::Dfe < OmniAuth::Strategies::OpenIDConnect; end
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  dfe_sign_in_issuer_uri    = URI(ENV.fetch('DFE_SIGN_IN_ISSUER', 'example'))
-  dfe_sign_in_identifier    = ENV.fetch('DFE_SIGN_IN_IDENTIFIER', 'example')
-  dfe_sign_in_secret        = ENV.fetch('DFE_SIGN_IN_SECRET', 'example')
-  dfe_sign_in_redirect_uri  = ENV.fetch('DFE_SIGN_IN_REDIRECT_URL', 'example')
+# Docker initialises the application (during `docker build`) in a context where
+# there are no environment variables. This guard clause is cleaner than multiple
+# `ENV.fetch` that sets up defaults.
+if ENV['DFE_SIGN_IN_ISSUER'].present?
+  dfe_sign_in_issuer_uri = URI.parse(ENV['DFE_SIGN_IN_ISSUER'])
+  options = {
+    name: :dfe,
+    discovery: true,
+    response_type: :code,
+    client_signing_alg: :RS256,
+    scope: %i[openid profile email organisation],
+    client_options: {
+      port: dfe_sign_in_issuer_uri.port,
+      scheme: dfe_sign_in_issuer_uri.scheme,
+      host: dfe_sign_in_issuer_uri.host,
+      identifier: ENV['DFE_SIGN_IN_IDENTIFIER'],
+      secret: ENV['DFE_SIGN_IN_SECRET'],
+      redirect_uri: ENV['DFE_SIGN_IN_REDIRECT_URL'],
+      authorization_endpoint: '/auth',
+      jwks_uri: '/certs',
+      userinfo_endpoint: '/me'
+    }
+  }
 
-  dfe_sign_in_issuer_url = "#{dfe_sign_in_issuer_uri}:#{dfe_sign_in_issuer_uri.port}" if dfe_sign_in_issuer_uri.port
-
-  provider :dfe,
-           name: :dfe,
-           discovery: true,
-           response_type: :code,
-           issuer: dfe_sign_in_issuer_url,
-           client_signing_alg: :RS256,
-           scope: %i[openid profile email organisation],
-           client_options: {
-             port: dfe_sign_in_issuer_uri.port,
-             scheme: dfe_sign_in_issuer_uri.scheme,
-             host: dfe_sign_in_issuer_uri.host,
-             identifier: dfe_sign_in_identifier,
-             secret: dfe_sign_in_secret,
-             redirect_uri: dfe_sign_in_redirect_uri,
-             authorization_endpoint: '/auth',
-             jwks_uri: '/certs',
-             userinfo_endpoint: '/me'
-           }
+  Rails.application.config.middleware.use OmniAuth::Strategies::OpenIDConnect, options
 end
