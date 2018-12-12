eca44818d3029382ecbe7096dd09cf6f7fa6ccea
Despo Pentara <despo@extractmethod.com>
2018-12-12 14:15:47 +0000

    (feature) Email alerts: Create a subscription

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :subscription do
     expires_on { Time.zone.today.strftime('%Y-%m-%d') }
     email { Faker::Internet.email }
+
+    factory :daily_subscription do
+      frequency { :daily }
+      expires_on { 3.months.from_now }
+    end
   end
 end
--- spec/features/job_seekers_can_subscribe_to_a_job_alert_spec.rb
+++ spec/features/job_seekers_can_subscribe_to_a_job_alert_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'A job seeker can subscribe to a job alert' do
+RSpec.feature 'A job seeker can subscribe to a job alert', wip: true do
   context 'A job seeker' do
     scenario 'can access the new subscription page when search criteria have been specified' do
       expect { visit(new_subscription_path) }.to raise_error(ActionController::ParameterMissing)
@@ -24,6 +24,63 @@ RSpec.feature 'A job seeker can subscribe to a job alert' do
       expect(page).to have_content('Working pattern: Full time')
       expect(page).to have_content('Minimum salary: £20,000')
       expect(page).to have_content('Maximum salary: £30,000')
+    end
+
+    context 'can create a new subscription' do
+      scenario 'when the email address is valid' do
+        visit new_subscription_path(search_criteria: { keyword: 'test' })
+        fill_in 'subscription[email]', with: 'jane.doe@example.com'
+        click_on 'Subscribe'
+
+        expect(page).to have_content('Email subscription created successfully')
+      end
+
+      scenario 'when the email address is associated with other active subscriptions' do
+        create(:daily_subscription, email: 'jane.doe@example.com',
+                                    search_criteria: { keyword: 'teacher' }.to_json)
+
+        visit new_subscription_path(search_criteria: { keyword: 'math teacher' })
+        fill_in 'subscription[email]', with: 'jane.doe@example.com'
+        click_on 'Subscribe'
+
+        expect(page).to have_content('Email subscription created successfully')
+      end
+
+      scenario 'when the email address is associated with the same inactive subscriptions' do
+        create(:daily_subscription, email: 'jane.doe@example.com',
+                                    expires_on: 1.day.ago,
+                                    search_criteria: { keyword: 'teacher' }.to_json)
+
+        visit new_subscription_path(search_criteria: { keyword: 'teacher' })
+        fill_in 'subscription[email]', with: 'jane.doe@example.com'
+        click_on 'Subscribe'
+
+        expect(page).to have_content('Email subscription created successfully')
+      end
+    end
+
+    context 'is not able to create a new subscription' do
+      scenario 'when the email address is invalid' do
+        visit new_subscription_path(search_criteria: { keyword: 'test' })
+        fill_in 'subscription[email]', with: 'jane.doe@example'
+        click_on 'Subscribe'
+
+        expect(page).to have_content('Please correct the following error')
+        expect(page).to have_content('Email is not a valid email address')
+      end
+
+      scenario 'when an active subcsription with the same search_criteria exists' do
+        search_criteria = { location: 'EC2 9AN', radius: '10' }
+
+        create(:daily_subscription, email: 'jane.doe@example.com',
+                                    search_criteria: search_criteria.to_json)
+
+        visit new_subscription_path(search_criteria: search_criteria)
+        fill_in 'subscription[email]', with: 'jane.doe@example.com'
+        click_on 'Subscribe'
+
+        expect(page).to have_content('You are already subscribed to a daily email with these search criteria')
+      end
     end
   end
 end
