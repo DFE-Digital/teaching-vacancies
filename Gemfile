b05ed1bf3744ff61a283e821fb481202d95c74ec
Despo Pentara <despo@extractmethod.com>
2018-12-17 17:21:17 +0000

    (feature) Email alerts: Subscription confirmation
    - integrates Notify
    - adds subscription confirmation email
    - adds Sidekiq job that triggers email
    - triggers Sidekiq job from subscription#create

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ gem 'high_voltage', '~> 3.1'
 gem 'google_drive', require: false
 gem 'google-api-client'
 gem 'browser'
+gem 'notifications-ruby-client'
 
 gem 'sidekiq'
 
---
+++ lib/services/notify.rb
@@ -0,0 +1,36 @@
+require 'notifications/client'
+
+class Notify
+  def initialize(email, personalisation, template_id, reference)
+    @email = email
+    @template_id = template_id
+    @reference = reference
+    @personalisation = personalisation
+  end
+
+  def call
+    raise 'Notify: NOTIFY_KEY is not set' if NOTIFY_KEY.blank?
+    send_email
+  end
+
+  private
+
+  attr_reader :email, :template_id, :reference, :personalisation
+
+  def client
+    @client ||= Notifications::Client.new(NOTIFY_KEY)
+  end
+
+  def email_params
+    {
+      email_address: email,
+      template_id: template_id,
+      personalisation: personalisation,
+      reference: reference
+    }
+  end
+
+  def send_email
+    client.send_email(email_params)
+  end
+end
