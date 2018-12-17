bc85db504596ab0ac949df760bf40874c5c9de6e
Laura Porter <laura@dxw.com>
2018-12-17 17:34:02 +0000

    (refactor) Slim down expensive feature tests

--- spec/features/hiring_staff_can_copy_a_vacancy_spec.rb
+++ spec/features/hiring_staff_can_copy_a_vacancy_spec.rb
@@ -6,31 +6,43 @@ RSpec.feature 'Copying a vacancy' do
     stub_hiring_staff_auth(urn: school.urn)
   end
 
-  scenario 'hiring staff can see a Duplicate link on published and pending jobs' do
-    create(:vacancy, school: school, status: 'published')
-    create(:vacancy,
-           school: school,
-           status: 'published',
-           expires_on: Time.zone.today + 4.days,
-           publish_on: Time.zone.tomorrow)
+  scenario 'a job can be successfully copied and published' do
+    FactoryBot.create(:vacancy, school: school)
 
     visit school_path
 
-    published_table = page.find('section#published-jobs table')
-    pending_table = page.find('section#pending-jobs table')
+    click_on I18n.t('jobs.duplicate_link')
+    click_on I18n.t('jobs.submit')
 
-    expect(published_table).to have_selector('td', text: I18n.t('jobs.duplicate_link'))
-    expect(pending_table).to have_selector('td', text: I18n.t('jobs.duplicate_link'))
+    expect(page).to have_content(I18n.t('jobs.confirmation_page.submitted'))
+  end
+
+  scenario 'hiring staff can see a Duplicate link on published jobs' do
+    FactoryBot.create(:vacancy, school: school)
+
+    visit school_path
+
+    expect(page).to have_selector('td', text: I18n.t('jobs.duplicate_link'))
+  end
+
+  scenario 'hiring staff can see a Duplicate link on pending jobs' do
+    vacancy = FactoryBot.build(:vacancy, :future_publish)
+    vacancy.school = school
+    vacancy.save
+
+    visit school_path
+
+    expect(page).to have_selector('td', text: I18n.t('jobs.duplicate_link'))
   end
 
   scenario 'hiring staff can NOT see a Duplicate link on draft jobs' do
-    create(:vacancy, school: school, status: 'draft')
+    vacancy = FactoryBot.build(:vacancy, :draft)
+    vacancy.school = school
+    vacancy.save
 
     visit school_path
 
-    draft_table = page.find('section#draft-jobs table')
-
-    expect(draft_table).to_not have_selector('td', text: I18n.t('jobs.duplicate_link'))
+    expect(page).to_not have_selector('td', text: I18n.t('jobs.duplicate_link'))
   end
 
   context 'review page' do
@@ -71,46 +83,6 @@ RSpec.feature 'Copying a vacancy' do
 
         expect(page).to have_content("#{I18n.t('jobs.copy_of')} #{pending.job_title}")
       end
-
-      scenario 'the publish_on date is the same as the pending job it was copied from' do
-        pending = create(:vacancy,
-                         school: school,
-                         status: 'published',
-                         expires_on: Time.zone.today + 4.days,
-                         publish_on: Time.zone.tomorrow)
-
-        visit school_path
-        click_on I18n.t('jobs.duplicate_link')
-
-        dt_publish_on = page.find('dt#publish_on')
-        expect(dt_publish_on.sibling('dd.app-check-your-answers__answer')).to have_content(pending.publish_on)
-      end
-    end
-  end
-
-  scenario 'a job can be successfully copied and published' do
-    create(:vacancy, school: school, status: 'published')
-
-    visit school_path
-    click_on I18n.t('jobs.duplicate_link')
-    click_on I18n.t('jobs.submit')
-
-    expect(page).to have_content(I18n.t('jobs.confirmation_page.submitted'))
-  end
-
-  context 'an error copying a job' do
-    before do
-      copy = build(:vacancy, school: school, status: 'draft')
-      allow_any_instance_of(Vacancy).to receive(:dup).and_return(copy)
-      allow(copy).to receive(:save).and_return(false)
-    end
-
-    scenario 'hiring staff see a message when there is a problem copying a job' do
-      create(:vacancy, school: school, status: 'published')
-
-      visit school_path
-      click_on I18n.t('jobs.duplicate_link')
-      expect(page).to have_content(I18n.t('errors.jobs.unable_to_copy'))
     end
   end
 end
