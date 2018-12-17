ceb42058e3c838538474abf7d3e5eb8e2cd229f1
Laura Porter <laura@dxw.com>
2018-12-17 17:06:26 +0000

    (feature) Log event when a vacancy is copied

--- app/controllers/hiring_staff/vacancies/copy_controller.rb
+++ app/controllers/hiring_staff/vacancies/copy_controller.rb
@@ -4,6 +4,7 @@ class HiringStaff::Vacancies::CopyController < HiringStaff::Vacancies::Applicati
     vacancy_copy = vacancy.dup
     update_fields(vacancy_copy)
     if vacancy_copy.save
+      Auditor::Audit.new(vacancy_copy, 'vacancy.copy', current_session_id).log
       redirect_to review_path(vacancy_copy)
     else
       redirect_to school_path, notice: I18n.t('errors.jobs.unable_to_copy')
