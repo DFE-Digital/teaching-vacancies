d6642b260e6c2734b014519fbb2283683ce82624
Tom Hipkin <tomhipkin@gmail.com>
2018-12-18 16:23:44 +0000

    (feature): Subscription checker isn’t passed raw user params
    * It is unclear if `exists?` is safe to use with a hash however we want to be safe and remove the possibility of any member of the public being able to inject into our subscription table. https://rails-sqli.org/#exists. We know we can trust `where` to do so, and 1 extra call is worth the confidence we gain.

--- app/services/subscription_finder.rb
+++ app/services/subscription_finder.rb
@@ -1,13 +1,16 @@
 class SubscriptionFinder
   def initialize(params = {})
-    @email = params[:email]
-    @search_criteria = params[:search_criteria]
-    @frequency = params[:frequency]
+    @sanitised_params = params.each_pair do |key, value|
+      params[key] = Sanitize.fragment(value)
+    end
   end
 
   def exists?
-    Subscription.ongoing.exists?(email: @email,
-                                 search_criteria: @search_criteria,
-                                 frequency: @frequency)
+    Subscription
+      .ongoing
+      .where(email: @sanitised_params[:email],
+             search_criteria: @sanitised_params[:search_criteria],
+             frequency: @sanitised_params[:frequency])
+      .exists?
   end
 end
