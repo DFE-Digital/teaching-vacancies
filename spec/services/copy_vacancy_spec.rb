1a28185ed1a47f1e3c8361e3a0bef610079fac7b
Tom Hipkin <tomhipkin@gmail.com>
2019-01-31 13:08:03 +0000

    (refactor) CopyVacancy service ready to accept user form input
    * Adding ‘Copy of’ infront of the job title was risky as when we tested it we accidentally copied and published a new listing without notice the automatic and hard to see change of the ‘Copy of’ being added in. It would be better to remove this risk from hiring staff accidentally publishing a job with an unexpected title.
    * As part of the user journey being refactored to address this, we’re adding in a new form object that will pass on a validated vacancy to this service for us in a copy process that allows for the important information like title and dates to be updated.

--- spec/services/copy_vacancy_spec.rb
+++ spec/services/copy_vacancy_spec.rb
@@ -7,24 +7,62 @@ RSpec.describe CopyVacancy do
       original_vacancy.save
       new_vacancy = original_vacancy.dup
 
-        expect(vacancy_copy.publish_on).to eq(Time.zone.today)
+      result = described_class.new(original: original_vacancy, new: new_vacancy)
+                              .call
+
+      expect(result).to be_kind_of(Vacancy)
+      expect(Vacancy.count).to eq(2)
+      expect(Vacancy.find(result.id).status).to eq('draft')
+    end
+
+    it 'does not change the original vacancy' do
+      # Needed to compare a FactoryBot object fields for updated_at and created_at
+      # and against the record it creates in Postgres.
+      Timecop.freeze(Time.zone.local(2008, 9, 1, 12, 0, 0))
+
+      original_vacancy = FactoryBot.create(:vacancy, job_title: 'Maths teacher')
+      new_vacancy = original_vacancy.dup
+
+      described_class.new(original: original_vacancy, new: new_vacancy)
+                     .call
+
+      expect(Vacancy.find(original_vacancy.id).attributes == original_vacancy.attributes)
+        .to eq(true)
+
+      Timecop.return
+    end
+
+    context 'when a new job_title is provided' do
+      it 'creates a new vacancy with a new job title' do
+        original_vacancy = FactoryBot.create(:vacancy, job_title: 'Maths teacher')
+        new_vacancy = original_vacancy.dup
+        new_vacancy.job_title = 'English teacher'
+
+        result = described_class.new(original: original_vacancy, new: new_vacancy)
+                                .call
+
+        expect(Vacancy.find(result.id).job_title).to eq('English teacher')
       end
     end
 
-    context 'a published vacancy with a publish_on date in the future' do
-      it 'does not update the vacancy\'s publish_on field' do
-        vacancy = FactoryBot.build(:vacancy, :future_publish)
-        vacancy_copy = CopyVacancy.new(vacancy: vacancy).copy
+    context 'when new dates are provided' do
+      it 'creates a new vacancy with the new dates' do
+        original_vacancy = FactoryBot.create(:vacancy, job_title: 'Maths teacher')
+        new_vacancy = original_vacancy.dup
+        new_vacancy.starts_on = 60.days.from_now
+        new_vacancy.ends_on = 100.days.from_now
+        new_vacancy.publish_on = 0.days.from_now
+        new_vacancy.expires_on = 50.days.from_now
 
-        expect(vacancy_copy.publish_on).to eq(vacancy.publish_on)
+        result = described_class.new(original: original_vacancy, new: new_vacancy)
+                                .call
+
+        created_vacancy = Vacancy.find(result.id)
+        expect(created_vacancy.starts_on).to eq(new_vacancy.starts_on)
+        expect(created_vacancy.ends_on).to eq(new_vacancy.ends_on)
+        expect(created_vacancy.publish_on).to eq(new_vacancy.publish_on)
+        expect(created_vacancy.expires_on).to eq(new_vacancy.expires_on)
       end
-    end
-
-    it 'sets the copied vacancy\'s status to draft' do
-      vacancy = FactoryBot.build(:vacancy, :published)
-      vacancy_copy = CopyVacancy.new(vacancy: vacancy).copy
-
-      expect(vacancy_copy.status).to eq('draft')
     end
   end
 end
