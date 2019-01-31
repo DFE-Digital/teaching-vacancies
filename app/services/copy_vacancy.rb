dc75567c6ab902d42edee9c654c64bc6c4829fb9
Tom Hipkin <tomhipkin@gmail.com>
2019-01-31 13:08:03 +0000

    (refactor) CopyVacancy service ready to accept user form input
    * Adding ‘Copy of’ infront of the job title was risky as when we tested it we accidentally copied and published a new listing without notice the automatic and hard to see change of the ‘Copy of’ being added in. It would be better to remove this risk from hiring staff accidentally publishing a job with an unexpected title.
    * As part of the user journey being refactored to address this, we’re adding in a new form object that will pass on a validated vacancy to this service for us in a copy process that allows for the important information like title and dates to be updated.

--- app/services/copy_vacancy.rb
+++ app/services/copy_vacancy.rb
@@ -1,19 +1,18 @@
 class CopyVacancy
-  def initialize(vacancy:)
-    @vacancy = vacancy
+  def initialize(original:, new:)
+    @original = original
+    @new = new
   end
 
-  def copy
-    @vacancy_copy = @vacancy.dup
-    update_fields
-    @vacancy_copy
-  end
-
-  private
-
-  def update_fields
-    @vacancy_copy.job_title = "#{I18n.t('jobs.copy_of')} #{@vacancy.job_title}"
-    @vacancy_copy.status = :draft
-    @vacancy_copy.publish_on = Time.zone.today if @vacancy_copy.publish_on <= Time.zone.today
+  def call
+    new_vacancy = @original.dup
+    new_vacancy.job_title = @new.job_title
+    new_vacancy.starts_on = @new.starts_on
+    new_vacancy.ends_on = @new.ends_on
+    new_vacancy.expires_on = @new.expires_on
+    new_vacancy.publish_on = @new.publish_on
+    new_vacancy.status = :draft
+    new_vacancy.save
+    new_vacancy
   end
 end
