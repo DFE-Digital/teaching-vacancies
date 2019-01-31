6cac2905569f3084406945778b4c3cecf5c63ac9
Stuart Harrison <pezholio@gmail.com>
2019-01-31 10:30:09 +0000

    (feature) Use adapter to send mails with Notify
    I’ve built [a gem](https://github.com/pezholio/mail-notify) to handle this, so we can send mails in a more standard way.

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,10 @@ module TeacherVacancyService
     config.autoload_paths += Dir[Rails.root.join('lib', 'modules')]
 
     config.active_job.queue_adapter = :sidekiq
+
+    config.action_mailer.delivery_method = :notify
+    config.action_mailer.notify_settings = {
+      api_key: ENV['NOTIFY_KEY']
+    }
   end
 end
