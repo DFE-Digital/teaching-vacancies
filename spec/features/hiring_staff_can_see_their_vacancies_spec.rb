52f99a2073bb521eaddf9bf60c06b5e2e412a1b1
Tom Hipkin <tomhipkin@gmail.com>
2019-01-31 18:37:12 +0000

    (feature): Draft jobs are shown with their last updated at
    * The locale file is getting very long and hard to find relevant copy. Nesting draft vacancies under the `jobs.draft.` namespace. Would be good to keep picking away at this file.

--- spec/features/hiring_staff_can_see_their_vacancies_spec.rb
+++ spec/features/hiring_staff_can_see_their_vacancies_spec.rb
@@ -78,8 +78,8 @@ RSpec.feature 'Hiring staff can see their vacancies' do
       end
 
       within('table.vacancies') do
-        expect(page).to have_content(I18n.t('jobs.date_drafted'))
-        expect(page).to have_content(I18n.t('jobs.date_drafted'))
+        expect(page).to have_content(I18n.t('jobs.draft.time_created'))
+        expect(page).to have_content(format_date(draft_vacancy.created_at))
         expect(page).to have_content(format_date(draft_vacancy.updated_at))
         expect(page).to have_content(draft_vacancy.job_title)
         expect(page).to have_css('tbody tr', count: 1)
@@ -115,6 +115,26 @@ RSpec.feature 'Hiring staff can see their vacancies' do
         expect(page).to have_content(format_date(expired_vacancy.expires_on))
         expect(page).to have_content(format_date(expired_vacancy.publish_on))
         expect(page).to have_css('tbody tr', count: 1)
+      end
+    end
+
+    context 'when a draft vacancy has been updated' do
+      let!(:draft_vacancy) do
+        create(:vacancy, :draft, school: school, created_at: 3.days.ago, updated_at: 1.day.ago)
+      end
+
+      scenario 'shows the last updated at' do
+        draft_vacancy
+        visit school_path
+
+        within('.tab-list') do
+          click_on(I18n.t('jobs.draft_jobs'))
+        end
+
+        within('table.vacancies') do
+          expect(page).to have_content(format_date(draft_vacancy.created_at))
+          expect(page).to have_content(format_date(draft_vacancy.updated_at))
+        end
       end
     end
   end
