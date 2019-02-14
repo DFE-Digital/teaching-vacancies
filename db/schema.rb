ca038302d13f94ac39a4ca2d51f80b1c831864d0
Stuart Harrison <pezholio@gmail.com>
2019-02-14 15:57:20 +0000

    (feature) add status to alert runs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_120218) do
+ActiveRecord::Schema.define(version: 2019_02_14_150420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_02_13_120218) do
     t.string "job_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["subscription_id"], name: "index_alert_runs_on_subscription_id"
   end
 
