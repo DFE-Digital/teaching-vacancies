3a0f65d3e9808a917bdc3129e97c19346f39e7f6
Stuart Harrison <pezholio@gmail.com>
2019-02-14 16:19:54 +0000

    (feature) use callbacks to check job age
    Don’t run a daily alert email if it’s over 4 hours old, as it’s more than likely going to be irrelevant for a user by then

--- app/jobs/daily_alert_mailer_job.rb
+++ app/jobs/daily_alert_mailer_job.rb
@@ -1,4 +1,6 @@
 class DailyAlertMailerJob < ActionMailer::DeliveryJob
+  EXPIRES_IN = 4.hours
+
   around_perform do |job, block|
     subscription = Subscription.find(job.arguments[3])
     alert_run = subscription.alert_run_today
@@ -8,6 +10,6 @@ class DailyAlertMailerJob < ActionMailer::DeliveryJob
   private
 
   def job_expired?(alert_run)
-    (alert_run.created_at + 4.hours) < Time.zone.now
+    (alert_run.created_at + EXPIRES_IN) < Time.zone.now
   end
 end
