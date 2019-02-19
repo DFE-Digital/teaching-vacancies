e6e3bc26153cde4fb5934f8424aca7d766d7ed54
Robbie Paul <robbie.paul@dxw.com>
2019-02-19 10:08:28 +0000

    (feature) Add background job for tracking vacancy page views

--- app/jobs/track_vacancy_page_view.rb
+++
@@ -1,7 +0,0 @@
-class TrackVacancyPageView < ApplicationJob
-  queue_as :low_priority
-
-  def perform(vacancy_id)
-    vacancy = Vacancy.find(vacancy_id)
-  end
-end
---
+++ spec/jobs/track_vacancy_page_view_job_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe TrackVacancyPageViewJob, type: :job do
+  include ActiveJob::TestHelper
+
+  let(:id) { SecureRandom.uuid }
+  subject(:job) { described_class.perform_later(id) }
+
+  it 'queues the job' do
+    expect { job }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'is in the page view collector queue' do
+    expect(job.queue_name).to eq('page_view_collector')
+  end
+
+  it 'executes perform' do
+    vacancy_page_view = double(:vacancy_page_view)
+    vacancy = double(:vacancy)
+    allow(Vacancy).to receive(:find).with(id).and_return(vacancy)
+
+    expect(VacancyPageView).to receive(:new).with(vacancy).and_return(vacancy_page_view)
+    expect(vacancy_page_view).to receive(:track)
+
+    perform_enqueued_jobs { job }
+  end
+end
