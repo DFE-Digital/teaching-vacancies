0dc155e1fb8d911cbd97d42d40dfcf2255b9ccfd
Tom Hipkin <tomhipkin@gmail.com>
2019-02-20 16:51:46 +0000

    (chore) Define browser caching rules for public job listings
    * Instruct the the user’s browser to only fetch new content a second time from the server after a maximum of 5 minutes for both the root path and individual job listings [1]
    * Allow the default cloudfront behaviour to respect the origin’s headers rather than it’s own configuration. Apparatently this is done by removing these 3 variables! [2]
    
    [1] https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/header-caching.html
    [2] https://angristan.xyz/terraform-enable-origin-cache-headers-aws-cloudfront/

--- terraform/modules/cloudfront/cloudfront.tf
+++ terraform/modules/cloudfront/cloudfront.tf
@@ -122,6 +122,28 @@ resource "aws_cloudfront_distribution" "default" {
     viewer_protocol_policy = "redirect-to-https"
   }
 
+  cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${var.project_name}-${var.environment}-default-origin"
+
+    path_pattern = "/*"
+
+    forwarded_values = {
+      query_string = true
+      headers      = ["Host", "Origin", "Authorization"]
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+    # The absense of `ttl` configuration here means caching is deferred to the origin
+    # https://angristan.xyz/terraform-enable-origin-cache-headers-aws-cloudfront/
+    
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
   price_class = "PriceClass_100"
 
   restrictions {
