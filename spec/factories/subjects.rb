f3455bdb444eb1a5447d46e5d456e885d7b2eb35
Tom Hipkin <tomhipkin@gmail.com>
2019-02-26 09:41:00 +0000

    (fix) update fake course method from deprecation warning
    ```
    NOTE: Faker::Educator.course is deprecated; use course_name instead. It will be removed on or after 2018-10-01.
    ```

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :subject do
-    name { Faker::Educator.course }
+    name { Faker::Educator.course_name }
     initialize_with { Subject.find_or_create_by(name: name) }
   end
 end
