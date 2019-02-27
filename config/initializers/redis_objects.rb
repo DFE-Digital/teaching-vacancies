c512b3bdbd705a48b293937fd9574bb140948340
Robbie Paul <robbie.paul@dxw.com>
2019-02-27 10:30:19 +0000

    (chore) Use separate Redis database for caching counters

--- config/initializers/redis_objects.rb
+++ config/initializers/redis_objects.rb
@@ -1,6 +1,6 @@
 require 'connection_pool'
 
-redis_url = "#{ENV['REDIS_CACHE_URL']}/0"
+redis_url = "#{ENV['REDIS_CACHE_URL']}/1"
 
 Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) do
   Redis.new(url: redis_url)
