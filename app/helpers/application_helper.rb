48f8593dbdc2ebfe567cf0b86f0680b74704377d
Tom Hipkin <tomhipkin@gmail.com>
2019-03-04 13:28:38 +0000

    (refactor) Decrease memory allocations of DateHelper
    * I don’t know why we have this method as it doesn’t do anything but pass on the params to super. Maybe it did in the past and the extra bit has been factored away.
    * Before this change I used derailed_benchmarks with 1000 requests. The old code used the third highest amount of memory allocations: 7744000 bytes. After this change it isn’t in the report which means it’s below 392000!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,6 @@ module ApplicationHelper
   include ActionView::Helpers::NumberHelper
   include ActionView::Helpers::SanitizeHelper
 
-  def number_to_currency(number, options = {})
-    super(number, options)
-  end
-
   def sanitize(text, options = {})
     super(text, options)&.gsub('&amp;', '&')
   end
