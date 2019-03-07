448bc5a10b38b3e1d48634e6e14594e82062abf8
Tom Hipkin <tomhipkin@gmail.com>
2019-03-07 18:33:07 +0000

    (fix) Only update vacancies that have page view increments
    * This will also avoid additional callbacks to elastic search to reindex for all records
    * Running this on Staging resulted in a single Vacancy update for every active listing which can be in the thousands.

--- app/services/vacancy_page_view.rb
+++ app/services/vacancy_page_view.rb
@@ -8,6 +8,8 @@ class VacancyPageView
   end
 
   def persist!
+    return if @vacancy.page_view_counter.to_i.zero?
+
     @vacancy.total_pageviews = @vacancy.total_pageviews.to_i + @vacancy.page_view_counter.to_i
     @vacancy.total_pageviews_updated_at = Time.zone.now
     reset_counter if @vacancy.save
