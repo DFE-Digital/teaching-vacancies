514fe128699cbe02404082fd163a112b9120847a
Tom Hipkin <tomhipkin@gmail.com>
2019-03-21 17:29:41 +0000

    (refactor) stats live in a generic variable for reuse
    * I’m about to add stats that don’t come from the Audit Table, instead of adding another variable and have to change all the views with new loops, or do funky merges which aren’t easily extendable I’m opting to make it understandable via a new generic name

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,7 @@
 class StatsController < ApplicationController
   def index
-    @audit_summary = PublicActivity::Activity.order(:key)
-                                             .group(:key).count
+    @stats = PublicActivity::Activity.order(:key)
+                                     .group(:key).count
 
     expires_in 60.minutes, public: true
   end
