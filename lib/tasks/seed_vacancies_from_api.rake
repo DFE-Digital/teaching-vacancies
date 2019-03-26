c005e1b5e63cd3adb7c38f7aaf6275055af5045f
Robbie Paul <robbie.paul@dxw.com>
2019-03-26 12:26:49 +0000

    (feature) Only allow importing vacancies to run on whitelisted DB
    We already have some guards against running this rake task in
    production. However there is a low chance that the rake task could run
    in a production environment and having another guard in place will
    give us more confidence in it.
    
    The databases that are on ECS are prefixed with `tvs2_` instead of
    `tvs_` as that is what is defined in the Terraform variable
    `project_name`. For local development it should always be
    `tvs_development`.

--- lib/tasks/seed_vacancies_from_api.rake
+++ lib/tasks/seed_vacancies_from_api.rake
@@ -6,6 +6,7 @@ namespace :data do
     task vacancies: :environment do
       next if Rails.env.production?
       next unless ImportVacanciesFeature.enabled?
+      next unless database_name_in_whitelist?
 
       Rails.logger.debug("Seeding vacancies from Teaching Vacancies API in #{Rails.env}")
       job_postings = TeachingVacancies::API.new.jobs(limit: 50)
@@ -16,4 +17,10 @@ namespace :data do
       Rails.logger.warn("Teaching Vacancies API response error: #{e.message}")
     end
   end
+end
+
+DB_ALLOWING_IMPORT_VACANCIES = %w[tvs_development tvs2_staging tvs2_edge tvs2_testing].freeze
+
+def database_name_in_whitelist?
+  DB_ALLOWING_IMPORT_VACANCIES.include?(Vacancy.connection.current_database)
 end
--- spec/lib/tasks/seed_vacancies_from_api_rake_spec.rb
+++ spec/lib/tasks/seed_vacancies_from_api_rake_spec.rb
@@ -4,9 +4,11 @@ require 'teaching_vacancies_api'
 RSpec.describe 'rake data:seed_from_api:vacancies', type: :task do
   let(:teaching_vacancies_api) { instance_double(TeachingVacancies::API) }
   let(:feature_enabled?) { true }
+  let(:current_database) { 'tvs_development' }
   before do
     allow(TeachingVacancies::API).to receive(:new).and_return(teaching_vacancies_api)
     allow(ImportVacanciesFeature).to receive(:enabled?).and_return(feature_enabled?)
+    allow(Vacancy.connection).to receive(:current_database).and_return(current_database)
   end
 
   it 'queues jobs to add vacancies from the Teaching Vacancies API' do
@@ -33,6 +35,17 @@ RSpec.describe 'rake data:seed_from_api:vacancies', type: :task do
 
   context 'when the import vacancies feature is NOT enabled' do
     let(:feature_enabled?) { false }
+
+    it 'returns early and doesn’t call the API at all' do
+      expect(teaching_vacancies_api).not_to receive(:jobs)
+      expect(SaveJobPostingToVacancyJob).not_to receive(:perform_later)
+
+      task.execute
+    end
+  end
+
+  context 'when the database name is NOT in the whitelist' do
+    let(:current_database) { 'tvs2_production' }
 
     it 'returns early and doesn’t call the API at all' do
       expect(teaching_vacancies_api).not_to receive(:jobs)
