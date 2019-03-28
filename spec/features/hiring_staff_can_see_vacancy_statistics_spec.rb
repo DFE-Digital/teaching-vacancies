82d62ec97d80ed0b57ceeabe06e26f48aaa6b7b4
Stuart Harrison <pezholio@gmail.com>
2019-03-28 10:58:38 +0000

    (feature) add get more info clicks to hiring staff tables

---
+++ spec/features/hiring_staff_can_see_vacancy_statistics_spec.rb
@@ -0,0 +1,113 @@
+require 'rails_helper'
+
+RSpec.feature 'Hiring staff can see vacancy statistics' do
+  let(:school) { create(:school) }
+  let(:total_pageviews) { nil }
+  let(:total_get_more_info_clicks) { nil }
+
+  before { stub_hiring_staff_auth(urn: school.urn) }
+
+  context 'when vacancy is published' do
+    let(:status) { 'published' }
+
+    let!(:vacancy) do
+      create(:vacancy,
+             school: school,
+             status: status,
+             total_pageviews: total_pageviews,
+             total_get_more_info_clicks: total_get_more_info_clicks)
+    end
+
+    before do
+      visit school_path(school)
+    end
+
+    context 'page views are nil' do
+      scenario 'page views show zero' do
+        within("tr#school_vacancy_presenter_#{vacancy.id}") do
+          expect(page.find('td[4]')).to have_content('0')
+        end
+      end
+    end
+
+    context 'page views are present' do
+      let(:total_pageviews) { 100 }
+
+      scenario 'page views show the page view count' do
+        within("tr#school_vacancy_presenter_#{vacancy.id}") do
+          expect(page.find('td[4]')).to have_content(total_pageviews)
+        end
+      end
+    end
+
+    context 'get more info clicks are nil' do
+      scenario 'page views show zero' do
+        within("tr#school_vacancy_presenter_#{vacancy.id}") do
+          expect(page.find('td[5]')).to have_content('0')
+        end
+      end
+    end
+
+    context 'get more info clicks are present' do
+      let(:total_get_more_info_clicks) { 100 }
+
+      scenario 'page views show the page view count' do
+        within("tr#school_vacancy_presenter_#{vacancy.id}") do
+          expect(page.find('td[5]')).to have_content(total_get_more_info_clicks)
+        end
+      end
+    end
+  end
+
+  context 'when vacancy is expired' do
+    let!(:vacancy) do
+      expired_vacancy = build(:vacancy,
+                              :expired,
+                              school: school,
+                              total_pageviews: total_pageviews,
+                              total_get_more_info_clicks: total_get_more_info_clicks)
+      expired_vacancy.save(validate: false)
+      expired_vacancy
+    end
+
+    before do
+      visit jobs_with_type_school_path(:expired)
+    end
+
+    context 'page views are nil' do
+      scenario 'page views show zero' do
+        within("tr#school_vacancy_presenter_#{vacancy.id}") do
+          expect(page.find('td[4]')).to have_content('0')
+        end
+      end
+    end
+
+    context 'page views are present' do
+      let(:total_pageviews) { 100 }
+
+      scenario 'page views show the page view count' do
+        within("tr#school_vacancy_presenter_#{vacancy.id}") do
+          expect(page.find('td[4]')).to have_content(total_pageviews)
+        end
+      end
+    end
+
+    context 'get more info clicks are nil' do
+      scenario 'page views show zero' do
+        within("tr#school_vacancy_presenter_#{vacancy.id}") do
+          expect(page.find('td[5]')).to have_content('0')
+        end
+      end
+    end
+
+    context 'get more info clicks are present' do
+      let(:total_get_more_info_clicks) { 100 }
+
+      scenario 'page views show the page view count' do
+        within("tr#school_vacancy_presenter_#{vacancy.id}") do
+          expect(page.find('td[5]')).to have_content(total_get_more_info_clicks)
+        end
+      end
+    end
+  end
+end
