8835c99cc5f8ec225c29372be55d0730cc4f2172
F <f@dxw.com>
2019-04-01 11:17:20 +0100

    (feature) Make published vacancies sortable
    - Creates a new `HiringStaff::SchoolsHelper` with a method to
      generate a link for sorting by a header
    - Makes the published vacancies view use that link as headers

---
+++ app/helpers/hiring_staff/schools_helper.rb
@@ -0,0 +1,23 @@
+module HiringStaff::SchoolsHelper
+  def table_header_sort_by(title, column:, sort:)
+    if column == sort.column
+      order = sort.reverse_order
+      active_class = ' active'
+    else
+      order = sort.order
+    end
+
+    link_to title,
+            jobs_with_type_school_path(school_vacancy_params(sort_column: column, sort_order: order)),
+            class: "govuk-link sortby--#{order}#{active_class || ''}",
+            'aria-label': "Sort jobs by #{title} in #{order}ending order"
+  end
+
+  def school_vacancy_params_whitelist
+    %i[sort_column sort_order]
+  end
+
+  def school_vacancy_params(overwrite = {})
+    params.merge(overwrite).permit(school_vacancy_params_whitelist)
+  end
+end
---
+++ spec/helpers/hiring_staff/schools_helper_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe HiringStaff::SchoolsHelper, type: :helper do
+  describe '#table_header_sort_by' do
+    it 'returns a link to the hiring staff index with the new parameters' do
+      sort = VacancySort.new
+      result = helper.table_header_sort_by('foo', column: 'job_title', sort: sort)
+
+      expect(result).to eq(
+        '<a class="govuk-link sortby--asc" aria-label="Sort jobs by foo in ascending order" '\
+        'href="/school/jobs?sort_column=job_title&amp;sort_order=asc">foo</a>'
+      )
+    end
+
+    context 'the current sort column is the same as the given column' do
+      it 'outputs an active class and reverses the sort order' do
+        sort = VacancySort.new(default_column: 'job_title', default_order: 'asc')
+        result = helper.table_header_sort_by('foo', column: 'job_title', sort: sort)
+
+        expect(result).to eq(
+          '<a class="govuk-link sortby--desc active" aria-label="Sort jobs by foo in descending order" '\
+          'href="/school/jobs?sort_column=job_title&amp;sort_order=desc">foo</a>'
+        )
+      end
+    end
+  end
+end
