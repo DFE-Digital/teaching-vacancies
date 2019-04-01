ecb2266e6c01663051268321c4216841a92dc147
Robbie Paul <robbie.paul@dxw.com>
2019-04-01 13:02:22 +0100

    (chore) Add working patterns array enum column to vacancies

---
+++ db/migrate/20190401103821_add_working_patterns_to_vacancy.rb
@@ -0,0 +1,5 @@
+class AddWorkingPatternsToVacancy < ActiveRecord::Migration[5.2]
+  def change
+    add_column :vacancies, :working_patterns, :integer, array: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_142008) do
+ActiveRecord::Schema.define(version: 2019_04_01_103821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -211,6 +211,7 @@ ActiveRecord::Schema.define(version: 2019_03_21_142008) do
     t.uuid "second_supporting_subject_id"
     t.integer "total_get_more_info_clicks"
     t.datetime "total_get_more_info_clicks_updated_at"
+    t.integer "working_patterns", array: true
     t.index ["expires_on"], name: "index_vacancies_on_expires_on"
     t.index ["first_supporting_subject_id"], name: "index_vacancies_on_first_supporting_subject_id"
     t.index ["leadership_id"], name: "index_vacancies_on_leadership_id"
