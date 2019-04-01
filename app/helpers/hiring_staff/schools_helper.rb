c50084bc821c5b63473532b36f8d632d92798b1d
F <f@dxw.com>
2019-04-01 14:55:08 +0100

    (refactor) Refactor `table_header_sort_by` for readability

--- app/helpers/hiring_staff/schools_helper.rb
+++ app/helpers/hiring_staff/schools_helper.rb
@@ -1,15 +1,12 @@
 module HiringStaff::SchoolsHelper
   def table_header_sort_by(title, type, column:, sort:)
-    if column == sort.column
-      order = sort.reverse_order
-      active_class = ' active'
-    else
-      order = sort.order
-    end
+    order = column == sort.column ? sort.reverse_order : sort.order
+    extra_classes = "sortby--#{order}"
+    extra_classes += ' active' if column == sort.column
 
     link_to title,
             jobs_with_type_school_path(type, school_vacancy_params(sort_column: column, sort_order: order)),
-            class: "govuk-link sortable-link sortby--#{order}#{active_class || ''}",
+            class: "govuk-link sortable-link #{extra_classes}",
             'aria-label': t('jobs.aria_labels.sort_by_link', column: title, order: order)
   end
 
