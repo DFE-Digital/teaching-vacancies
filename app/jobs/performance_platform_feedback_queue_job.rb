af7a05457158ae67320ca9062a8ad1571ec5f3fb
David Winter <i@djw.me>
2019-04-15 10:50:33 +0100

    (refactor) Make performance platform jobs more consistent

--- app/jobs/performance_platform_feedback_queue_job.rb
+++ app/jobs/performance_platform_feedback_queue_job.rb
@@ -2,16 +2,20 @@ require 'performance_platform'
 class PerformancePlatformFeedbackQueueJob < ApplicationJob
   queue_as :performance_platform
 
+  LOG_SOURCE = 'performance_platform:submit_user_satisfaction'.freeze
+
   def perform(time_to_s)
     date = Time.zone.parse(time_to_s)
-    return if TransactionAuditor::Logger.new('performance_platform:submit_user_satisfaction', date).performed?
 
-    data = { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0 }.merge!(Feedback.published_on(date).group(:rating).count)
+    return if TransactionAuditor::Logger.new(LOG_SOURCE, date).performed?
+
+    feedback = { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0 }.merge!(Feedback.published_on(date).group(:rating).count)
     PerformancePlatform::UserSatisfaction.new(PP_USER_SATISFACTION_TOKEN)
-                                         .submit(data, date.iso8601)
-    TransactionAuditor::Logger.new('performance_platform:submit_user_satisfaction', date).log_success
+                                         .submit(feedback, date.iso8601)
+
+    TransactionAuditor::Logger.new(LOG_SOURCE, date).log_success
   rescue StandardError => e
-    TransactionAuditor::Logger.new('performance_platform:submit_user_satisfaction', date).log_failure
+    TransactionAuditor::Logger.new(LOG_SOURCE, date).log_failure
     Rails.logger.error("Sidekiq: Something went wrong and user satisfaction was not \
                        submitted to the Performance Platform': #{e.message}")
     raise
--- app/jobs/performance_platform_transactions_queue_job.rb
+++ app/jobs/performance_platform_transactions_queue_job.rb
@@ -2,19 +2,20 @@ require 'performance_platform'
 class PerformancePlatformTransactionsQueueJob < ApplicationJob
   queue_as :performance_platform
 
-  def perform(time_to_s)
-    time = Time.zone.parse(time_to_s)
-    date = time.to_date
+  LOG_SOURCE = 'performance_platform:submit_transactions'.freeze
 
-    return if TransactionAuditor::Logger.new('performance_platform:submit_transactions', date).performed?
+  def perform(time_to_s)
+    date = Time.zone.parse(time_to_s)
+
+    return if TransactionAuditor::Logger.new(LOG_SOURCE, date).performed?
 
     no_of_transactions = Vacancy.published_on_count(date)
     PerformancePlatform::TransactionsByChannel.new(PP_TRANSACTIONS_BY_CHANNEL_TOKEN)
-                                              .submit_transactions(no_of_transactions, time.iso8601)
+                                              .submit_transactions(no_of_transactions, date.iso8601)
 
-    TransactionAuditor::Logger.new('performance_platform:submit_transactions', date).log_success
+    TransactionAuditor::Logger.new(LOG_SOURCE, date).log_success
   rescue StandardError => e
-    TransactionAuditor::Logger.new('performance_platform:submit_transactions', date).log_failure
+    TransactionAuditor::Logger.new(LOG_SOURCE, date).log_failure
     Rails.logger.error("Sidekiq: Something went wrong and transactions were not submitted \
                         to the Performance Platform: #{e.message}")
     raise
