bed2568d4821911543689e2cf7b0d7b98f6da7c8
Stuart Harrison <pezholio@gmail.com>
2019-04-23 16:49:02 +0100

    (fix) ignore whitespace when ordering in tests

--- spec/presenters/school_vacancies_presenter_spec.rb
+++ spec/presenters/school_vacancies_presenter_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 RSpec.describe SchoolVacanciesPresenter do
   let(:school) { create(:school) }
-  let(:sort) { VacancySort.new }
   let(:type) { 'published' }
+  let(:sort) { VacancySort.new }
   let(:presenter) { described_class.new(school, sort, type) }
 
   let!(:draft_vacancies) { create_list(:vacancy, 5, :draft, school: school) }
@@ -22,6 +22,56 @@ RSpec.describe SchoolVacanciesPresenter do
     expect(presenter.school).to eq(school)
   end
 
+  describe 'sorting' do
+    let(:vacancy_ids) do
+      vacancies.sort_by { |vacancy| vacancy['job_title'].delete(' ') }.reverse.pluck(:id)
+    end
+
+    context 'when type is draft' do
+      let(:type) { 'draft' }
+      let(:vacancies) { draft_vacancies }
+
+      it 'returns sorted draft vacancies' do
+        sort.update(column: 'job_title', order: 'desc')
+
+        expect(presenter.vacancies.pluck(:id)).to eq(vacancy_ids)
+      end
+    end
+
+    context 'when type is pending' do
+      let(:type) { 'pending' }
+      let(:vacancies) { pending_vacancies }
+
+      it 'returns sorted pending vacancies' do
+        sort.update(column: 'job_title', order: 'desc')
+
+        expect(presenter.vacancies.pluck(:id)).to eq(vacancy_ids)
+      end
+    end
+
+    context 'when type is expired' do
+      let(:type) { 'expired' }
+      let(:vacancies) { expired_vacancies }
+
+      it 'returns sorted expired vacancies' do
+        sort.update(column: 'job_title', order: 'desc')
+
+        expect(presenter.vacancies.pluck(:id)).to eq(vacancy_ids)
+      end
+    end
+
+    context 'when type is published' do
+      let(:type) { 'published' }
+      let(:vacancies) { published_vacancies }
+
+      it 'returns sorted published vacancies' do
+        sort.update(column: 'job_title', order: 'desc')
+
+        expect(presenter.vacancies.pluck(:id)).to match_array(vacancy_ids)
+      end
+    end
+  end
+
   context 'when type is draft' do
     let(:type) { 'draft' }
 
@@ -31,12 +81,6 @@ RSpec.describe SchoolVacanciesPresenter do
 
     it 'returns draft vacancies' do
       expect(presenter.vacancies.count).to eq(draft_vacancies.count)
-    end
-
-    it 'returns sorted draft vacancies' do
-      sort.update(column: 'job_title', order: 'desc')
-      vacancy_ids = draft_vacancies.sort_by { |vacancy| vacancy['job_title'] }.reverse.pluck(:id)
-      expect(presenter.vacancies.pluck(:id)).to eq(vacancy_ids)
     end
   end
 
@@ -50,12 +94,6 @@ RSpec.describe SchoolVacanciesPresenter do
     it 'returns pending vacancies' do
       expect(presenter.vacancies.count).to eq(pending_vacancies.count)
     end
-
-    it 'returns sorted pending vacancies' do
-      sort.update(column: 'job_title', order: 'desc')
-      vacancy_ids = pending_vacancies.sort_by { |vacancy| vacancy['job_title'] }.reverse.pluck(:id)
-      expect(presenter.vacancies.pluck(:id)).to eq(vacancy_ids)
-    end
   end
 
   context 'when type is expired' do
@@ -68,12 +106,6 @@ RSpec.describe SchoolVacanciesPresenter do
     it 'returns expired vacancies' do
       expect(presenter.vacancies.count).to eq(expired_vacancies.count)
     end
-
-    it 'returns sorted expired vacancies' do
-      sort.update(column: 'job_title', order: 'desc')
-      vacancy_ids = expired_vacancies.sort_by { |vacancy| vacancy['job_title'] }.reverse.pluck(:id)
-      expect(presenter.vacancies.pluck(:id)).to eq(vacancy_ids)
-    end
   end
 
   context 'when type is published' do
@@ -85,12 +117,6 @@ RSpec.describe SchoolVacanciesPresenter do
 
     it 'returns published vacancies' do
       expect(presenter.vacancies.count).to eq(published_vacancies.count)
-    end
-
-    it 'returns sorted published vacancies' do
-      sort.update(column: 'job_title', order: 'desc')
-      vacancy_ids = published_vacancies.sort_by { |vacancy| vacancy['job_title'] }.reverse.pluck(:id)
-      expect(presenter.vacancies.pluck(:id)).to match_array(vacancy_ids)
     end
   end
 
