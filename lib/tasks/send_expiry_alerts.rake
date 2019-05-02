ae919da15905e2dd1216d4ab94006fa66cac7d5b
Stuart Harrison <pezholio@gmail.com>
2019-05-02 09:35:58 +0100

    (feature) add final alerts job to rake task

--- lib/tasks/send_expiry_alerts.rake
+++ lib/tasks/send_expiry_alerts.rake
@@ -2,6 +2,7 @@ namespace :subscription do
   namespace :expiry_alerts do
     task send: :environment do
       SendFirstSubscriptionExpiryAlertsJob.perform_later
+      SendFinalSubscriptionExpiryAlertsJob.perform_later
     end
   end
 end
--- spec/lib/tasks/send_expiry_alerts_rake_spec.rb
+++ spec/lib/tasks/send_expiry_alerts_rake_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe 'rake subscription:expiry_alerts:send', type: :task do
     ActiveJob::Base.queue_adapter = :test
   end
 
-  it 'queues the vacancy job' do
+  it 'queues first reminder job' do
     expect { task.execute }.to have_enqueued_job(SendFirstSubscriptionExpiryAlertsJob)
+  end
+
+  it 'queues the final reminder job' do
+    expect { task.execute }.to have_enqueued_job(SendFinalSubscriptionExpiryAlertsJob)
   end
 end
