3f8cd60dc3ba6c09e27eec2f9ef85454d50f87f2
Stuart Harrison <pezholio@gmail.com>
2019-05-09 16:14:57 +0100

    (feature) add view for awaiting feedback route
    I'm not 100% happy with this as in order to get forms in table rows, I've has to use HTML5 form input attributes (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#form). This is not supported in IE <= 11. We may be able to work around it with polyfills or JS

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -101,7 +101,6 @@ class Vacancy < ApplicationRecord
     hired_tvs
     hired_other_free
     hired_paid
-    hired_free
     not_filled_ongoing
     not_filled_readvertised
     not_filled_not_looking
