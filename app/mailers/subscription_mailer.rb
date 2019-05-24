31efd9aeaa090262a697c3b64f2626a786d74937
Stuart Harrison <pezholio@gmail.com>
2019-05-24 08:30:11 +0100

    (chore) tidy up mailers

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,11 +1,7 @@
 class SubscriptionMailer < ApplicationMailer
   def confirmation(subscription_id)
-    @subscription = Subscription.find(subscription_id)
-
-    @subscription_reference = @subscription.reference
-    @search_criteria = SubscriptionPresenter.new(@subscription).filtered_search_criteria
-    @expires_on = @subscription.expires_on
-    @unsubscribe_token = @subscription.token
+    subscription = Subscription.find(subscription_id)
+    @subscription = SubscriptionPresenter.new(subscription)
 
     view_mail(
       NOTIFY_SUBSCRIPTION_CONFIRMATION_TEMPLATE,
