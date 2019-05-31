a8c6b65a2f7b52e6ce4d2e3ed9a30e1ccaed2aab
Renny Fadoju <rennyvbirds@yahoo.com>
2019-05-31 12:53:10 +0100

    (Refactor) Edit test to reflect multple vacancies

--- spec/features/hiring_staff_can_add_stats_to_a_vacancy_spec.rb
+++ spec/features/hiring_staff_can_add_stats_to_a_vacancy_spec.rb
@@ -13,30 +13,41 @@ RSpec.feature 'Adding feedback to a vacancy' do
       vacancy
     end
 
+    let!(:another_vacancy) do
+      vacancy = build(:vacancy, :expired, school: school)
+      vacancy.save(validate: false)
+      vacancy
+    end
+
     scenario 'hiring staff can see notification badge' do
       visit school_path
 
-      expect(page).to have_selector('span.notification', text: 1)
+      expect(page).to have_selector('span.notification', text: 2)
     end
 
     scenario 'hiring staff can see notice of vacancies awaiting feedback' do
       visit school_path
 
-      expect(page).to have_selector('#awaiting_notice .count', text: 1)
+      expect(page).to have_selector('#awaiting_notice .count', text: '2 jobs')
     end
 
     scenario 'feedback can be added to a vacancy' do
       visit jobs_with_type_school_path(type: :awaiting_feedback)
 
-      expect(page).to have_content(vacancy.job_title)
+      expect(page).to have_selector('#job-title.view-vacancy-link', count: 2)
+      expect(page).to have_selector('#job-title.view-vacancy-link', text: vacancy.job_title)
+      expect(page).to have_selector('#job-title.view-vacancy-link', text: another_vacancy.job_title)
 
-      select I18n.t('jobs.feedback.hired_status.hired_tvs'), from: 'vacancy_hired_status'
-      select I18n.t('jobs.feedback.listed_elsewhere.listed_paid'), from: 'vacancy_listed_elsewhere'
-
-      click_on I18n.t('buttons.submit')
+      within('tr', text: vacancy.job_title) do
+        select I18n.t('jobs.feedback.hired_status.hired_tvs'), from: 'vacancy_hired_status'
+        select I18n.t('jobs.feedback.listed_elsewhere.listed_paid'), from: 'vacancy_listed_elsewhere'
+        click_on I18n.t('buttons.submit')
+      end
 
       expect(page).to have_content(I18n.t('jobs.feedback_submitted'))
       expect(page).to_not have_content(vacancy.job_title)
+
+      expect(page).to have_selector('#awaiting_notice .count', text: '1 job')
 
       vacancy.reload
 
@@ -47,9 +58,10 @@ RSpec.feature 'Adding feedback to a vacancy' do
     scenario 'when an option is not selected' do
       visit jobs_with_type_school_path(type: :awaiting_feedback)
 
-      select I18n.t('jobs.feedback.hired_status.hired_tvs'), from: 'vacancy_hired_status'
-
-      click_on I18n.t('buttons.submit')
+      within('tr', text: vacancy.job_title) do
+        select I18n.t('jobs.feedback.hired_status.hired_tvs'), from: 'vacancy_hired_status'
+        click_on I18n.t('buttons.submit')
+      end
 
       expect(page).to have_content(I18n.t('jobs.feedback_error'))
       expect(page).to have_content(vacancy.job_title)
