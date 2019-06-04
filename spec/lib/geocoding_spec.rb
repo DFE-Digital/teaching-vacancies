217ffb0a03de5f1cebcf7f85e3468f509e743039
Adrian Clay <adrian@madetech.com>
2019-06-04 12:14:26 +0100

    (fix) Make Geocoder use AutoExpiryCacheRedis for caching items
    We found that the cache was filling up with Geocoder cache entries
    because Geocoder doesn't set an expiry time on items by default.
    
    By adding a TTL to items, they should be deleted periodically
    preventing the cache from filling up.

--- spec/lib/geocoding_spec.rb
+++ spec/lib/geocoding_spec.rb
@@ -2,31 +2,74 @@ require 'rails_helper'
 require 'geocoding'
 
 RSpec.describe Geocoding do
-  before do
-    require 'geocoder'
-    Geocoder.configure(lookup: :test)
-  end
-
   context 'Retrieving the coordinates for a postcode' do
-    it 'returns the correct value when the input is a valid postcode' do
-      Geocoder::Lookup::Test.add_stub(
-        'TS14 6RD', [
-          {
-            'coordinates' => [-1.04577, 54.541098],
-            'longitude' => '-1.04577',
-            'latitude' => '54.541098',
-          }
-        ]
-      )
-      geocoding = Geocoding.new('TS14 6RD')
-      expect(geocoding.coordinates).to eq([-1.04577, 54.541098])
+    context 'When using the Redis cache with network calls' do
+      let(:request_url) do
+        'https://api.ordnancesurvey.co.uk/opennames/v1/find?fq=local_type%3ACity+local_type%3AHamlet+local_type%3AOther_Settlement+local_type%3ATown+local_type%3AVillage+local_type%3APostcode&key=&query=TS14+6RD'
+      end
+
+      let(:cache_key) do
+        request_url_without_key = request_url.sub('&key=', '')
+        "geocoder:#{request_url_without_key}"
+      end
+
+      let(:os_api_response) { { header: { totalresults: 0 } }.to_json }
+      let(:key_doesnt_exist) { -2 }
+
+      before do
+        Geocoder.configure(api_key: '')
+        stub_request(:get, request_url)
+          .to_return(status: 200, body: os_api_response, headers: {})
+      end
+
+      after do
+        Redis::Objects.redis.flushdb
+      end
+
+      let(:geocoding) do
+        Geocoding.new('TS14 6RD')
+      end
+
+      subject do
+        geocoding.coordinates
+      end
+
+      it 'caches the API response with a TTL of 26 hours' do
+        expect { subject }.to change { Redis::Objects.redis.ttl(cache_key) }.from(key_doesnt_exist).to(26.hours.to_i)
+      end
+
+      it 'caches the API response' do
+        expect { subject }.to change { Redis::Objects.redis.get(cache_key) }.from(nil).to(os_api_response)
+      end
     end
 
-    it 'returns [0,0] when the input is invalid' do
-      Geocoder::Lookup::Test.add_stub('TS14', [{}])
+    context 'When not using the Redis cache' do
+      around(:all) do |example|
+        Geocoder.configure(lookup: :test)
+        example.run
+        Geocoder.configure(lookup: :os_names)
+      end
 
-      geocoding = Geocoding.new('TS14')
-      expect(geocoding.coordinates).to eq([0, 0])
+      it 'returns the correct value when the input is a valid postcode' do
+        Geocoder::Lookup::Test.add_stub(
+          'TS14 6RD', [
+            {
+              'coordinates' => [-1.04577, 54.541098],
+              'longitude' => '-1.04577',
+              'latitude' => '54.541098',
+            }
+          ]
+        )
+        geocoding = Geocoding.new('TS14 6RD')
+        expect(geocoding.coordinates).to eq([-1.04577, 54.541098])
+      end
+
+      it 'returns [0,0] when the input is invalid' do
+        Geocoder::Lookup::Test.add_stub('TS14', [{}])
+
+        geocoding = Geocoding.new('TS14')
+        expect(geocoding.coordinates).to eq([0, 0])
+      end
     end
   end
 end
