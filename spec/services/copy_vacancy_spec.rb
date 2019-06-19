023e56b14972e80ac615eb104ffe584963cf76cb
MadeTech Dushan <dushan@madetech.com>
2019-06-19 09:49:43 +0100

    (chore) add test to check vacancy fields copied
    Not all fields of a vacancy should be copied when a user uses the copy
    vacancy functionality e.g. the slug, page view and more info clicks
    should all be unique. The test added checks that these fields which
    should be uniqued are copied.

--- spec/services/copy_vacancy_spec.rb
+++ spec/services/copy_vacancy_spec.rb
@@ -26,5 +26,30 @@ RSpec.describe CopyVacancy do
 
       Timecop.return
     end
+
+    context "not all fields are copied" do
+      
+      it "should not copy fields that should be unique" do
+        vacancy = create(:vacancy, 
+          job_title: 'Maths teacher',
+          slug: 'maths-teacher', 
+          weekly_pageviews: 4,
+          total_pageviews: 4,
+          weekly_pageviews_updated_at: Time.zone.today - 5.days,
+          total_pageviews_updated_at: Time.zone.today - 5.days,
+          total_get_more_info_clicks: 6,
+          total_get_more_info_clicks_updated_at: Time.zone.today - 5.days
+        )
+        
+        result = described_class.new(vacancy).call
+        expect(Vacancy.find(result.id).slug).to_not eq('maths_teacher')
+        expect(Vacancy.find(result.id).weekly_pageviews).to eq(0)
+        expect(Vacancy.find(result.id).weekly_pageviews_updated_at).to_not eq(Time.zone.today - 5.days)
+        expect(Vacancy.find(result.id).total_pageviews).to eq(0)
+        expect(Vacancy.find(result.id).total_pageviews_updated_at).to_not eq(Time.zone.today - 5.days)
+        expect(Vacancy.find(result.id).total_get_more_info_clicks).to eq(0)
+        expect(Vacancy.find(result.id).total_get_more_info_clicks_updated_at).to_not eq(Time.zone.today - 5.days)
+      end
+    end
   end
 end
