d5be8ebb4d06dcd7fb546fe8e651d3d9b97e3a69
MadeTech Dushan <dushan@madetech.com>
2019-06-19 09:55:39 +0100

    (fix) fix vacancy page view and info click copying
    This change passes the test added by the previous commit by fixing the
    bug that meant page views and get more info clicks were copied when a
    vacancy was copied using the copy functionality.

--- app/services/copy_vacancy.rb
+++ app/services/copy_vacancy.rb
@@ -6,6 +6,9 @@ class CopyVacancy
   def call
     new_vacancy = @vacancy.dup
     new_vacancy.status = :draft
+    new_vacancy.weekly_pageviews = 0
+    new_vacancy.total_pageviews = 0
+    new_vacancy.total_get_more_info_clicks = 0
     new_vacancy.save
     new_vacancy
   end
