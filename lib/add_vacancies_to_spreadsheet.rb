d872b0b87481be4e4f91f17b53ebddbe6dd4131b
Renny Fadoju <rennyvbirds@yahoo.com>
2019-07-01 15:36:13 +0100

    (Refactor) Remove values.unshift method use

--- lib/add_vacancies_to_spreadsheet.rb
+++ lib/add_vacancies_to_spreadsheet.rb
@@ -12,24 +12,24 @@ class AddVacanciesToSpreadsheet < ExportToSpreadsheet
   end
 
   def to_row(vacancy)
-    {
-      id: vacancy.id,
-      slug: vacancy.slug,
-      created_at: vacancy.created_at.to_s,
-      status: vacancy.status,
-      publish_on: vacancy.publish_on,
-      expires_on: vacancy.expires_on,
-      starts_on: vacancy.starts_on,
-      ends_on: vacancy.ends_on,
-      weekly_hours: vacancy.weekly_hours,
-      flexible_working: vacancy.working_patterns.join(','),
-      school_urn: vacancy.school.urn,
-      school_county: vacancy.school.county
-    }
+    [
+      Time.zone.now.to_s,
+      vacancy.id,
+      vacancy.slug,
+      vacancy.created_at.to_s,
+      vacancy.status,
+      vacancy.publish_on,
+      vacancy.expires_on,
+      vacancy.starts_on,
+      vacancy.ends_on,
+      vacancy.weekly_hours,
+      vacancy.working_patterns.join(','),
+      vacancy.school.urn,
+      vacancy.school.county
+    ]
   end
 
   def present(vacancy)
-    row = to_row(vacancy)
-    row.values.unshift(Time.zone.now.to_s)
+    to_row(vacancy)
   end
 end
