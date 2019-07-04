b058b8ff4e1fc637d284c68bd76650451f745216
csabagyorfi <csaba@madetech.com>
2019-07-04 16:04:08 +0100

    (chore) add context in specs for toggling feature
    We added an extra context block to describe the hiring staff sign in
    feature working without using DFE sign_in authorisation service.
    Did this by enabling and disabling the DFE authorisation feature flag
    within the relevant context blocks.

--- spec/features/hiring_staff_can_visit_the_public_listings_spec.rb
+++ spec/features/hiring_staff_can_visit_the_public_listings_spec.rb
@@ -1,43 +1,99 @@
 require 'rails_helper'
 
 RSpec.feature 'School viewing public listings' do
-  before do
-    OmniAuth.config.test_mode = true
-  end
-
-  after(:each) do
-    OmniAuth.config.test_mode = false
-    OmniAuth.config.mock_auth[:default] = nil
-    OmniAuth.config.mock_auth[:dfe] = nil
-  end
-
-  let!(:school) { create(:school, urn: '110627') }
-
-  context 'when signed in with DfE Sign In' do
-    before(:each) do
-      stub_accepted_terms_and_condition
-      stub_authentication_step(school_urn: '110627')
-      stub_authorisation_step
+  context 'when the Dfe Sign In Authorisation feature flag is enabled' do
+    before do
+      allow(DfeSignInAuthorisationFeature).to receive(:enabled?) { true }
+      OmniAuth.config.test_mode = true
     end
 
-    scenario 'A signed in school should see a link back to their own dashboard when viewing public listings' do
-      visit root_path
+    after(:each) do
+      OmniAuth.config.test_mode = false
+      OmniAuth.config.mock_auth[:default] = nil
+      OmniAuth.config.mock_auth[:dfe] = nil
+    end
 
-      click_on(I18n.t('nav.sign_in'))
-      click_on(I18n.t('sign_in.link'))
+    let!(:school) { create(:school, urn: '110627') }
 
-      expect(page).to have_content("Jobs at #{school.name}")
-      within('.app-navigation') { expect(page).to have_content(I18n.t('nav.school_page_link')) }
+    context 'when signed in with DfE Sign In' do
+      before(:each) do
+        stub_accepted_terms_and_condition
+        stub_authentication_step(school_urn: '110627')
+        stub_authorisation_step
+      end
 
-      click_on(I18n.t('app.title'))
-      expect(page).to have_content(I18n.t('jobs.heading'))
+      scenario 'A signed in school should see a link back to their own dashboard when viewing public listings' do
+        visit root_path
 
-      click_on(I18n.t('nav.school_page_link'))
-      expect(page).to have_content("Jobs at #{school.name}")
+        click_on(I18n.t('nav.sign_in'))
+        click_on(I18n.t('sign_in.link'))
+
+        expect(page).to have_content("Jobs at #{school.name}")
+        within('.app-navigation') { expect(page).to have_content(I18n.t('nav.school_page_link')) }
+
+        click_on(I18n.t('app.title'))
+        expect(page).to have_content(I18n.t('jobs.heading'))
+
+        click_on(I18n.t('nav.school_page_link'))
+        expect(page).to have_content("Jobs at #{school.name}")
+      end
+    end
+
+    def stub_accepted_terms_and_condition
+      create(:user, oid: '161d1f6a-44f1-4a1a-940d-d1088c439da7', accepted_terms_at: 1.day.ago)
     end
   end
 
-  def stub_accepted_terms_and_condition
-    create(:user, oid: '161d1f6a-44f1-4a1a-940d-d1088c439da7', accepted_terms_at: 1.day.ago)
+  context 'when the Dfe Sign In Authorisation feature flag is disabled' do
+    before do
+      allow(DfeSignInAuthorisationFeature).to receive(:enabled?) { false }
+      OmniAuth.config.test_mode = true
+    end
+
+    after(:each) do
+      OmniAuth.config.test_mode = false
+      OmniAuth.config.mock_auth[:default] = nil
+      OmniAuth.config.mock_auth[:dfe] = nil
+    end
+
+    let!(:school) { create(:school, urn: '110627') }
+    let!(:user) { create(:user, oid: 'a-valid-oid') }
+
+    context 'when signed in with DfE Sign In' do
+      before(:each) do
+        OmniAuth.config.mock_auth[:dfe] = OmniAuth::AuthHash.new(
+          provider: 'dfe',
+          uid: 'a-valid-oid',
+          info: {
+            email: 'an-email@example.com',
+          },
+          extra: {
+            raw_info: {
+              organisation: { urn: '110627' }
+            }
+          }
+        )
+
+        mock_response = double(code: '200', body: { user: { permissions: [{ school_urn: '110627' }] } }.to_json)
+        allow(TeacherVacancyAuthorisation::Permissions).to receive(:new)
+          .and_return(AuthHelpers::MockPermissions.new(mock_response))
+      end
+
+      scenario 'A signed in school should see a link back to their own dashboard when viewing public listings' do
+        visit root_path
+
+        click_on(I18n.t('nav.sign_in'))
+        click_on(I18n.t('sign_in.link'))
+
+        expect(page).to have_content("Jobs at #{school.name}")
+        within('.app-navigation') { expect(page).to have_content(I18n.t('nav.school_page_link')) }
+
+        click_on(I18n.t('app.title'))
+        expect(page).to have_content(I18n.t('jobs.heading'))
+
+        click_on(I18n.t('nav.school_page_link'))
+        expect(page).to have_content("Jobs at #{school.name}")
+      end
+    end
   end
 end
