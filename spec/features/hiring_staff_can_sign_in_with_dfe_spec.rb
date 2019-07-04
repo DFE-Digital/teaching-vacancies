632eaff0f2cf3b9f64eb0fc57a34eae8b3d016b1
csabagyorfi <csaba@madetech.com>
2019-07-04 15:16:36 +0100

    (chore) update specs to use feature flag
    as part of the feature flag implementation it is necessary to add
    context blocks to separate tests based on whether DFE sign-in feature
    flag is toggled this is because the system will behave differently
    based on the flag is toggled or not. We want to add the old behaviour
    if the feature flag is disabled

--- spec/features/hiring_staff_can_sign_in_with_dfe_spec.rb
+++ spec/features/hiring_staff_can_sign_in_with_dfe_spec.rb
@@ -141,9 +141,176 @@ RSpec.feature 'Hiring staff signing-in with DfE Sign In' do
         expect(page).to have_content(I18n.t('error_pages.external_server_error.dfe_sign_in'))
       end
     end
+  end
 
-    def stub_accepted_terms_and_condition
-      create(:user, oid: '161d1f6a-44f1-4a1a-940d-d1088c439da7', accepted_terms_at: 1.day.ago)
+  context 'when the dfe sign-in authorisation feature flag is disabled' do
+    before(:each) do
+      allow(DfeSignInAuthorisationFeature).to receive(:enabled?) { false }
+      stub_accepted_terms_and_condition
+      OmniAuth.config.test_mode = true
     end
+
+    after(:each) do
+      OmniAuth.config.mock_auth[:default] = nil
+      OmniAuth.config.mock_auth[:dfe] = nil
+      OmniAuth.config.test_mode = false
+    end
+
+    let!(:school) { create(:school, urn: '110627') }
+    let!(:other_school) { create(:school, urn: '101010') }
+    let!(:user) { create(:user, oid: 'an-unknown-oid') }
+
+    context 'with valid credentials that do match a school' do
+      let(:mock_response) do
+        double(code: '200', body: {
+          user:
+          {
+            permissions:
+            [
+              {
+                user_token: 'an-email@example.com',
+                school_urn: '110627'
+              },
+              {
+                user_token: 'an-email@example.com',
+                school_urn: '101010'
+              }
+            ]
+          }
+        }.to_json)
+      end
+      let(:mock_permissions) { AuthHelpers::MockPermissions.new(mock_response) }
+
+      before(:each) do
+        OmniAuth.config.mock_auth[:dfe] = OmniAuth::AuthHash.new(
+          provider: 'dfe',
+          uid: 'an-unknown-oid',
+          info: {
+            email: 'an-email@example.com',
+          },
+          extra: {
+            raw_info: {
+              organisation: { urn: '110627' }
+            }
+          }
+        )
+        expect(TeacherVacancyAuthorisation::Permissions).to receive(:new)
+          .and_return(mock_permissions)
+      end
+
+      context 'successful events' do
+        before(:each) do
+          visit root_path
+          click_on(I18n.t('nav.sign_in'))
+          # binding.pry
+          click_on(I18n.t('sign_in.link'))
+        end
+
+        it_behaves_like 'a successful sign in'
+
+        scenario 'it redirects the sign in page to the school page' do
+          visit new_identifications_path
+          expect(page).to have_content("Jobs at #{school.name}")
+          expect(current_path).to eql(school_path)
+        end
+
+        context 'the user can switch between organisations' do
+          scenario 'allows the user to switch between organisations' do
+            expect(page).to have_content("Jobs at #{school.name}")
+
+            # Mock switching organisations on DfE Sign In
+            OmniAuth.config.mock_auth[:dfe] = OmniAuth::AuthHash.new(
+              provider: 'dfe',
+              uid: 'an-unknown-oid',
+              info: {
+                email: 'an-email@example.com',
+              },
+              extra: {
+                raw_info: {
+                  organisation: { urn: '101010' }
+                }
+              }
+            )
+            expect(TeacherVacancyAuthorisation::Permissions).to receive(:new)
+              .and_return(mock_permissions)
+            click_on 'Change organisation'
+
+            expect(page).to have_content("Jobs at #{other_school.name}")
+          end
+        end
+      end
+    end
+
+    context 'with valid credentials but no authorisation' do
+      before(:each) do
+        OmniAuth.config.mock_auth[:dfe] = OmniAuth::AuthHash.new(
+          provider: 'dfe',
+          uid: 'an-unknown-oid',
+          info: {
+            email: 'another_email@example.com',
+          },
+          extra: {
+            raw_info: {
+              organisation: { urn: '110627' }
+            }
+          }
+        )
+
+        mock_authorisation_response = double(code: '200', body: {
+          user:
+          {
+            permissions: []
+          }
+        }.to_json)
+
+        expect(TeacherVacancyAuthorisation::Permissions).to receive(:new)
+          .and_return(AuthHelpers::MockPermissions.new(mock_authorisation_response))
+      end
+
+      it_behaves_like 'a failed sign in', user_id: 'an-unknown-oid',
+                                          school_urn: '110627',
+                                          email: 'another_email@example.com'
+    end
+
+    context 'with valid credentials and no organisation in DfE Sign In but existing permissions' do
+      before(:each) do
+        OmniAuth.config.mock_auth[:dfe] = OmniAuth::AuthHash.new(
+          provider: 'dfe',
+          uid: 'an-unknown-oid',
+          info: {
+            email: 'an-email@example.com',
+          },
+          extra: {
+            raw_info: {
+              organisation: {}
+            }
+          }
+        )
+
+        mock_authorisation_response = double(code: '200', body: {
+          user:
+          {
+            permissions:
+            [
+              {
+                user_token: 'an-email@example.com',
+                school_urn: '110627'
+              }
+            ]
+          }
+        }.to_json)
+
+        expect(TeacherVacancyAuthorisation::Permissions).to receive(:new)
+          .and_return(AuthHelpers::MockPermissions.new(mock_authorisation_response))
+      end
+
+      it_behaves_like 'a failed sign in', user_id: 'an-unknown-oid',
+                                          school_urn: '',
+                                          email: 'an-email@example.com'
+    end
+  end
+
+  def stub_accepted_terms_and_condition
+    create(:user, oid: '161d1f6a-44f1-4a1a-940d-d1088c439da7', accepted_terms_at: 1.day.ago)
   end
 end
