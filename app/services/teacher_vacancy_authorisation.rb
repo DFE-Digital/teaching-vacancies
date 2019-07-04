492bd93b01c18c92988df6e1120efe0e6ca0c51b
csabagyorfi <csaba@madetech.com>
2019-07-04 16:26:25 +0100

    (chore) Add non dsi auth behaviour back with specs
    We want to keep the previous sign in behaviour which did not use
    DFE sign in when the feature flag is disabled, this commit adds that
    back along with the spec file that tests it.

---
+++ app/services/teacher_vacancy_authorisation.rb
@@ -0,0 +1,55 @@
+require 'net/http'
+module TeacherVacancyAuthorisation
+  class Permissions
+    attr_reader :response, :headers, :http
+
+    def initialize
+      uri = URI.parse(AUTHORISATION_SERVICE_URL)
+      @http = Net::HTTP.new(uri.host, uri.port)
+      @http.use_ssl = true
+      @headers = { 'Authorization' => "Token token=#{AUTHORISATION_SERVICE_TOKEN}",
+                   'Content-Type' => 'application/json' }
+    end
+
+    def authorise(user_token, school_urn = nil)
+      request = Net::HTTP::Get.new("/users/#{user_token}", headers)
+      @response = http.request(request)
+      @school_urn = school_urn
+
+      user_permissions
+    end
+
+    def all_permissions
+      user_permissions
+    end
+
+    def school_urn
+      return nil unless user_permissions.any?
+      return user_permissions_for_school.first['school_urn'] if @school_urn.present?
+
+      user_permissions.first['school_urn']
+    end
+
+    def many?
+      user_permissions && user_permissions.count > 1
+    end
+
+    def authorised?
+      user_permissions_for_school.any?
+    end
+
+    private
+
+    def parsed_response
+      response.code == '200' ? JSON.parse(response.body) : nil
+    end
+
+    def user_permissions
+      @user_permissions ||= parsed_response.present? ? parsed_response['user']['permissions'] : []
+    end
+
+    def user_permissions_for_school
+      user_permissions.select { |permission| permission['school_urn'] == @school_urn }
+    end
+  end
+end
