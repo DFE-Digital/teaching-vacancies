91023ce37be05cb7fcd9c2a0d019fcb7878bc1a6
csabagyorfi <csaba@madetech.com>
2019-07-05 09:59:05 +0100

    (feature) Add smoke test for vacancies page
    Add smoke test so we can schedule these and gain confidence of the
    website availability. The test covers a user journey on the vacancies
    page, such as visit the page, perform a search and open first vacancy.
    Edited rspec config file to exclude smoke test from a full run.

---
+++ spec/smoke_test/job_seekers_can_view_homepage_spec.rb
@@ -0,0 +1,27 @@
+require 'browser_test_helper'
+require 'i18n_helper'
+
+RSpec.describe 'Page availability', js: true, smoke_test: true do
+  context 'Job seeker visits vacancy page' do
+    it 'should ensure users can search and view a job vacancy page' do
+      page = Capybara::Session.new(:poltergeist)
+      page.driver.set_cookie('smoke_test', '1', domain: 'teaching-vacancies.service.gov.uk')
+
+      page.visit 'https://teaching-vacancies.service.gov.uk/'
+      expect(page).to have_content(I18n.t('jobs.heading'))
+
+      page.within '.filters-form' do
+        page.fill_in I18n.t('jobs.filters.subject'), with: 'Maths'
+        page.first('.govuk-button[type=submit]').click
+      end
+      expect(page).to have_content(I18n.t('subscriptions.button'))
+
+      vacancy_page = page.first('.view-vacancy-link')
+      unless vacancy_page.nil?
+        vacancy_page.click
+        expect(page).to have_content(I18n.t('jobs.description'))
+        expect(page.current_url).to include('https://teaching-vacancies.service.gov.uk/jobs/')
+      end
+    end
+  end
+end
