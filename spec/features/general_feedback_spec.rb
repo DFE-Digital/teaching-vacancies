463abf54e3f19cfd6675d42d48420ffc2ec73f05
Renny Fadoju <rennyvbirds@yahoo.com>
2019-07-25 10:32:07 +0100

    Remove unnecessary tests for content
    Testing static content could give confidence that the test setup has successfully navigated to the page of choice. e.g if the route was to change, then the tests would fail.
    However, this can be done by just checking that the page heading is for the page we want.
    The extra content tests are unnecessary and in this change, removed.

--- spec/features/general_feedback_spec.rb
+++ spec/features/general_feedback_spec.rb
@@ -2,22 +2,8 @@ require 'rails_helper'
 RSpec.feature 'Giving general feedback for the service' do
   before(:each) { visit new_feedback_path }
 
-  context 'when viewing the visiting purpose section' do
-    it 'displays the section heading' do
-      expect(page).to have_content(I18n.t('general_feedback.visit_purpose_legend'))
-    end
-
-    it 'displays the visiting purpose options' do
-      expect(page).to have_content(I18n.t('general_feedback.visit_purpose_options.find_teaching_job'))
-      expect(page).to have_content(I18n.t('general_feedback.visit_purpose_options.list_teaching_job'))
-      expect(page).to have_content(I18n.t('general_feedback.visit_purpose_options.other_purpose'))
-    end
-  end
-
-  context 'when viewing the service rating comment section' do
-    it 'displays the section heading' do
-      expect(page).to have_content(I18n.t('feedback.comment_label'))
-    end
+  it 'displays the general feedback page' do
+    expect(page).to have_content(I18n.t('feedback.heading'))
   end
 
   scenario 'successfully submitting feedback for the service' do
