6c095d74a9ec8570a146948fbc116776a648899e
csabagyorfi <csaba@madetech.com>
2019-09-09 11:44:26 +0100

    (chore) Update en.yml file and views where application deadline time used
    Update the above to show the right content when application deadline time
    is available. Rename application details controller error methods.

--- app/form_models/application_details_form.rb
+++ app/form_models/application_details_form.rb
@@ -27,10 +27,10 @@ class ApplicationDetailsForm < VacancyForm
   end
 
   def validate_expiry_time
-    return create_blank_error if expiry_time_hh.blank? || expiry_time_mm.blank?
-    return create_wrong_format_error unless in_range?(expiry_time_hh, 1, 12)
-    return create_wrong_format_error unless in_range?(expiry_time_mm, 0, 59)
-    return create_meridiem_error if expiry_time_meridian.blank?
+    return blank_error if expiry_time_hh.blank? || expiry_time_mm.blank?
+    return wrong_format_error unless in_range?(expiry_time_hh, 1, 12)
+    return wrong_format_error unless in_range?(expiry_time_mm, 0, 59)
+    return meridiem_error if expiry_time_meridian.blank?
   end
 
   def attributes
@@ -67,15 +67,15 @@ class ApplicationDetailsForm < VacancyForm
     /\A[+-]?\d+\z/.match?(value)
   end
 
-  def create_wrong_format_error
+  def wrong_format_error
     errors.add(:expiry_time, I18n.t('activerecord.errors.models.vacancy.attributes.expiry_time.wrong_format'))
   end
 
-  def create_blank_error
+  def blank_error
     errors.add(:expiry_time, I18n.t('activerecord.errors.models.vacancy.attributes.expiry_time.blank'))
   end
 
-  def create_meridiem_error
+  def meridiem_error
     errors.add(:expiry_time, I18n.t('activerecord.errors.models.vacancy.attributes.expiry_time.must_be_am_pm'))
   end
 end
