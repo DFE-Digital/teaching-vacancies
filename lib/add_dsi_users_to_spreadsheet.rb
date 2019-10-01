c389cfd6e5ed4ffe86a08611eb12019b6bf0663d
csabagyorfi <csaba@madetech.com>
2019-10-01 17:24:32 +0100

    Add user data to spreadsheet
    We want to map json response to an array, so that we can write it into a spreadsheet.

--- lib/add_dsi_users_to_spreadsheet.rb
+++ lib/add_dsi_users_to_spreadsheet.rb
@@ -10,13 +10,39 @@ class AddDSIUsersToSpreadsheet
     @worksheet.clear_all_rows
     total_page_num = total_page_number
     (1..total_page_num).each do |page|
-      DFESignIn::API.new.users(page: page)
+      response = DFESignIn::API.new.users(page: page)
+      unless response.with_indifferent_access[:users].first.empty?
+        rows = response_to_rows(response)
+        @worksheet.append_rows(rows)
+      end
     rescue StandardError => e
       Rails.logger.warn("DSI API failed to respond at page #{page} with error: #{e.message}")
     end
   end
 
   def total_page_number
-    DFESignIn::API.new.users[:numberOfPages]
+    DFESignIn::API.new.users.with_indifferent_access[:numberOfPages]
+  end
+
+  def response_to_rows(users_response)
+    users_response['users'].map do |user|
+      [
+        user['roleName'],
+        user['userId'],
+        user['approvedAt'],
+        user['updatedAt'],
+        user['givenName'],
+        user['familyName'],
+        user['email'],
+        user['organisation']['URN'],
+        user['organisation']['name'],
+        user['organisation']['Status'],
+        user['organisation']['phaseOfEducation'],
+        user['organisation']['telephone'],
+        user['organisation']['regionCode'],
+        user['organisation']['createdAt'],
+        user['organisation']['updatedAt']
+      ]
+    end
   end
 end
--- spec/lib/add_dsi_users_to_spreadsheet_spec.rb
+++ spec/lib/add_dsi_users_to_spreadsheet_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe AddDSIUsersToSpreadsheet do
 
   describe '#total_page_number' do
     it 'returns the number of pages value from the API response' do
-      stub_dsi_user_api_response_with(response_json_2)
+      stub_dsi_user_api_initial_call(response_json_2)
 
       total_page_num = AddDSIUsersToSpreadsheet.new.total_page_number
 
@@ -56,7 +56,7 @@ RSpec.describe AddDSIUsersToSpreadsheet do
   describe '#all_service_users' do
     context 'when making multiple requests' do
       scenario 'makes one request if there is only one page' do
-        stub_dsi_user_api_response_with(response_json_1)
+        stub_dsi_user_api_initial_call(response_json_1)
 
         expect(dfe_sign_in_api).to receive(:users).with(page: 1)
 
@@ -64,7 +64,7 @@ RSpec.describe AddDSIUsersToSpreadsheet do
       end
 
       scenario 'makes two requests if there are two pages' do
-        stub_dsi_user_api_response_with(response_json_2)
+        stub_dsi_user_api_initial_call(response_json_2)
 
         expect(dfe_sign_in_api).to receive(:users).with(page: 1)
         expect(dfe_sign_in_api).to receive(:users).with(page: 2)
@@ -73,7 +73,7 @@ RSpec.describe AddDSIUsersToSpreadsheet do
       end
 
       scenario 'make three requests if there are three pages' do
-        stub_dsi_user_api_response_with(response_json_3)
+        stub_dsi_user_api_initial_call(response_json_3)
 
         expect(dfe_sign_in_api).to receive(:users).with(page: 1)
         expect(dfe_sign_in_api).to receive(:users).with(page: 2)
@@ -83,7 +83,7 @@ RSpec.describe AddDSIUsersToSpreadsheet do
       end
 
       scenario 'when an error is raised, it continues with next page' do
-        stub_dsi_user_api_response_with(response_json_3)
+        stub_dsi_user_api_initial_call(response_json_3)
 
         stub_dsi_user_api_response_error_for_page(2)
 
@@ -94,7 +94,7 @@ RSpec.describe AddDSIUsersToSpreadsheet do
       end
 
       scenario 'when there is a error response, logs the error message' do
-        stub_dsi_user_api_response_with(response_json_3)
+        stub_dsi_user_api_initial_call(response_json_3)
 
         stub_dsi_user_api_response_error_for_page(2)
 
@@ -106,7 +106,7 @@ RSpec.describe AddDSIUsersToSpreadsheet do
 
     context 'when recieving the user data' do
       it 'initializes the spreadsheet writer with a spreadsheet' do
-        stub_dsi_user_api_response_with(response_json_1)
+        stub_dsi_user_api_initial_call(response_json_1)
 
         expect(Spreadsheet::Writer).to receive(:new)
           .with(DSI_USER_SPREADSHEET_ID, DSI_USER_WORKSHEET_GID, true) { worksheet }
@@ -114,7 +114,7 @@ RSpec.describe AddDSIUsersToSpreadsheet do
       end
 
       it 'clears the spreadsheet' do
-        stub_dsi_user_api_response_with(response_json_1)
+        stub_dsi_user_api_initial_call(response_json_1)
 
         allow(Spreadsheet::Writer).to receive(:new)
           .with(DSI_USER_SPREADSHEET_ID, DSI_USER_WORKSHEET_GID, true) { worksheet }
@@ -123,12 +123,35 @@ RSpec.describe AddDSIUsersToSpreadsheet do
 
         AddDSIUsersToSpreadsheet.new.all_service_users
       end
+
+      it 'adds user data to spreadsheet' do
+        response_json_page_one = response_file_to_json(1)
+        response_json_page_two = response_file_to_json(2)
+
+        expected_spreadsheet_rows_one = map_json_to_rows(response_json_page_one)
+        expected_spreadsheet_rows_two = map_json_to_rows(response_json_page_two)
+
+        stub_dsi_user_api_initial_call(response_json_page_one)
+        stub_dsi_user_api_response_for_page(1, response_json_page_one)
+        stub_dsi_user_api_response_for_page(2, response_json_page_two)
+
+        allow(Spreadsheet::Writer).to receive(:new)
+          .with(DSI_USER_SPREADSHEET_ID, DSI_USER_WORKSHEET_GID, true) { worksheet }
+
+        expect(worksheet).to receive(:append_rows).with(expected_spreadsheet_rows_one)
+        expect(worksheet).to receive(:append_rows).with(expected_spreadsheet_rows_two)
+        AddDSIUsersToSpreadsheet.new.all_service_users
+      end
     end
   end
 
-  def stub_dsi_user_api_response_with(response)
+  def stub_dsi_user_api_initial_call(response)
     allow(DFESignIn::API).to receive(:new).and_return(dfe_sign_in_api)
     allow(dfe_sign_in_api).to receive(:users).and_return(response)
+  end
+
+  def stub_dsi_user_api_response_for_page(page, response)
+    allow(dfe_sign_in_api).to receive(:users).with(page: page).and_return(response)
   end
 
   def stub_dsi_user_api_response_error_for_page(page)
@@ -141,5 +164,34 @@ RSpec.describe AddDSIUsersToSpreadsheet do
     allow(spreadsheet).to receive(:worksheet_by_gid).with(DSI_USER_WORKSHEET_GID) { worksheet }
     allow(worksheet).to receive(:delete_rows)
     allow(worksheet).to receive(:clear_all_rows)
+  end
+
+  def map_json_to_rows(json_response)
+    json_response['users'].map do |user|
+      [
+        user['roleName'],
+        user['userId'],
+        user['approvedAt'],
+        user['updatedAt'],
+        user['givenName'],
+        user['familyName'],
+        user['email'],
+        user['organisation']['URN'],
+        user['organisation']['name'],
+        user['organisation']['Status'],
+        user['organisation']['phaseOfEducation'],
+        user['organisation']['telephone'],
+        user['organisation']['regionCode'],
+        user['organisation']['createdAt'],
+        user['organisation']['updatedAt']
+      ]
+    end
+  end
+
+  def response_file_to_json(page)
+    response_file = File.read(Rails.root.join(
+                                'spec', 'fixtures', "dfe_sign_in_service_users_response_page_#{page}.json"
+                              ))
+    JSON.parse(response_file)
   end
 end
