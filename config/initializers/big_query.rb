b29e86f6795242b8e5c21c7a57765792a7c7d159
MadeTech Dushan <dushan@madetech.com>
2019-10-04 11:07:00 +0100

    (chore) don't initialize bigquery api if key nil

--- config/initializers/big_query.rb
+++ config/initializers/big_query.rb
@@ -1,10 +1,14 @@
-require "google/cloud/bigquery"
+require 'google/cloud/bigquery'
 
 BIG_QUERY_API_JSON_KEY = ENV['BIG_QUERY_API_JSON_KEY']
 
-Google::Cloud::Bigquery.configure do |config|
-  config.project_id  = "teacher-vacancy-service"
-  config.credentials = JSON.parse(BIG_QUERY_API_JSON_KEY)
-end
+if BIG_QUERY_API_JSON_KEY
+  Google::Cloud::Bigquery.configure do |config|
+    config.project_id  = 'teacher-vacancy-service'
+    config.credentials = JSON.parse(BIG_QUERY_API_JSON_KEY)
+  end
 
-bigquery = Google::Cloud::Bigquery.new
+  # rubocop:disable Lint/UselessAssignment
+  bigquery = Google::Cloud::Bigquery.new
+  # rubocop:enable Lint/UselessAssignment
+end
