88bf6aea727a830ba4d21f648ba300beb1b9d22a
Renny Fadoju <rennyvbirds@yahoo.com>
2019-10-31 12:24:33 +0000

    Move error messages into the translation file

--- app/models/concerns/feedback_validations.rb
+++ app/models/concerns/feedback_validations.rb
@@ -4,18 +4,18 @@ module FeedbackValidations
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
 
   included do
-    validates :comment, presence: { message: 'Enter your feedback' }, length: \
-      { maximum: 1200, message: 'Feedback must not be more than 1,200 characters' }
+    validates :comment, presence: { message: I18n.t('feedback.comment_errors.blank') }, length: \
+      { maximum: 1200, message: I18n.t('feedback.comment_errors.over_max_length') }
 
     validates :user_participation_response, presence: \
-      { message: "Please indicate if you'd like to participate in user research" }
+      { message: I18n.t('general_feedback.user_participation_errors.blank') }
 
     validate :email_address_given?, if: :user_is_interested?
   end
 
   def email_address_given?
-    return errors.add(:email, 'Enter your email address') if email.blank?
-    errors.add(:email, 'Enter an email address in the correct format, like name@example.com') \
+    return errors.add(:email, I18n.t('general_feedback.user_interested_email_errors.blank')) if email.blank?
+    errors.add(:email, I18n.t('general_feedback.user_interested_email_errors.incorrect_format')) \
       unless email.match(EMAIL_REGEX)
   end
 
--- app/models/general_feedback.rb
+++ app/models/general_feedback.rb
@@ -5,10 +5,10 @@ class GeneralFeedback < ApplicationRecord
   enum visit_purpose: { other_purpose: 0, find_teaching_job: 1, list_teaching_job: 2 }
   enum user_participation_response: { interested: 0, not_interested: 1 }
 
-  validates :visit_purpose, presence: { message: 'Enter the reason for your visit' }
+  validates :visit_purpose, presence: { message: I18n.t('general_feedback.visit_purpose_errors.blank') }
 
-  validates :visit_purpose_comment, length: \
-    { maximum: 1200, message: 'Purpose of visit must not be more than 1,200 characters' }, if: :visit_purpose_comment?
+  validates :visit_purpose_comment, length: { maximum: 1200, \
+    message: I18n.t('general_feedback.visit_purpose_errors.over_max_length') }, if: :visit_purpose_comment?
 
   scope :published_on, (->(date) { where(created_at: date.all_day) })
 
