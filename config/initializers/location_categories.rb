054b5b108849e06e1d06c6864085dac2034bbee7
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2019-12-09 15:29:42 +0000

    Load counties, boroughs and regions at startup time

--- config/initializers/location_categories.rb
+++ config/initializers/location_categories.rb
@@ -1,2 +1,7 @@
-ALL_LOCATION_CATEGORIES =
-  YAML.load_file(Rails.root.join('lib', 'tasks', 'data', 'location_categories.yml'))
+base_path = Rails.root.join('lib', 'tasks', 'data')
+
+REGIONS = YAML.load_file(base_path.join('regions.yml'))
+COUNTIES = YAML.load_file(base_path.join('counties.yml'))
+BOROUGHS = YAML.load_file(base_path.join('boroughs.yml'))
+
+ALL_LOCATION_CATEGORIES = (REGIONS + COUNTIES + BOROUGHS).map(&:downcase)
