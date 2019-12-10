0593a46082147704824cffb6be0f5dbf7bb1851c
Todd Tyree <tatyree@gmail.com>
2019-12-10 13:32:10 +0000

    Silence the capybara/puma startup message
    It's just noise that gets in the way of reading the rspec output.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,7 +18,8 @@ ActiveRecord::Migration.maintain_test_schema!
 user_agents ||= YAML.load_file(Browser.root.join('test/ua.yml')).freeze
 USER_AGENTS = user_agents
 
-Capybara.server = :puma, { Threads: '0:1' }
+# This keeps the startup message from appearing in the stdout of the test run.
+Capybara.server = :puma, { Silent: true, Threads: '0:1' }
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
