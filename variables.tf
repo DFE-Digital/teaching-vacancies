342c5f0cbf2213e5404b0045f8995f51b3b69be4
Renny Fadoju <rennyvbirds@yahoo.com>
2019-12-23 13:55:29 +0000

    Add `update_database_records_in_big_query` task command and schedule

--- variables.tf
+++ variables.tf
@@ -204,6 +204,16 @@ variable "update_dsi_spreadsheets_task_schedule" {
   default     = "cron(0 02 * * ? *)"
 }
 
+variable "update_database_records_in_big_query_task_command" {
+  description = "The Entrypoint for the update_database_records_in_big_query task"
+  default     = ["rake", "verbose", "database_records:in_big_query:update"]
+}
+
+variable "update_database_records_in_big_query_task_schedule" {
+  description = "update_database_records_in_big_query schedule expression - https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html"
+  default     = "cron(0 03 * * ? *)"
+}
+
 variable "sessions_trim_task_command" {
   description = "The Entrypoint for trimming old sessions"
   default     = ["rake", "verbose", "db:sessions:trim"]
