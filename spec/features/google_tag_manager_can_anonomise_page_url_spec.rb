6132092dc2915ce076e6c258c0a744b91832f5ad
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2020-01-03 11:26:09 +0000

    Fix google_tag_manager flaky specs

--- spec/features/google_tag_manager_can_anonomise_page_url_spec.rb
+++ spec/features/google_tag_manager_can_anonomise_page_url_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Google Tag Manager' do
   context 'When searching for jobs with home postcode filter' do
-    scenario 'parameters are removed from URL before sending URL to dataLayer', js: true do
+    scenario 'parameters are removed from URL before sending URL to dataLayer', js: true, elasticsearch: true do
       expect(Geocoder).to receive(:coordinates).with('CH52DD')
                                                .and_return([51.6622925, -0.1180655])
 
@@ -14,6 +14,7 @@ RSpec.feature 'Google Tag Manager' do
         page.find('.govuk-button[type=submit]').click
       end
 
+      expect(page).to have_content 'jobs match your search'
       expect(evaluate_script('dataLayer')).to include(include('event' => 'parametersRemoved', 'dePIIedURL' => '/jobs'))
     end
   end
