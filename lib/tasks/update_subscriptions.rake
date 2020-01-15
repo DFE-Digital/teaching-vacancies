91f5c344595076cfa36c7e1e91eea9bed35a9092
MadeTech Dushan <dushan@madetech.com>
2020-01-15 15:12:29 +0000

    (chore) add documentation for task
    The commit message gives context into why the script was needed and what
    its purpose was, but added some documentation around this as it can get
    buried

--- lib/tasks/update_subscriptions.rake
+++ lib/tasks/update_subscriptions.rake
@@ -2,6 +2,8 @@ class UpdateSubscriptionsWithNewWorkingPatterns
   def self.run!
     Subscription.all.each do |s|
       search_criteria_hash = s.search_criteria_to_h
+      # Old format hash = {"subject"=>"english", "working_pattern"=>"full_time"}
+      # New format hash = {"subject"=>"english", "working_patterns"=>["full_time"]}
       search_criteria_hash.transform_keys! { |k| k.gsub(/working_pattern\b/, 'working_patterns') }
                           .transform_values! {
                             |v| search_criteria_hash.key(v) == 'working_patterns' && v.is_a?(String) ? [v] : v
--- spec/lib/tasks/update_subscriptions_rake_spec.rb
+++ spec/lib/tasks/update_subscriptions_rake_spec.rb
@@ -2,15 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'subscription_records:convert_subscriptions', type: :task do
   let!(:old_style_subscription) do
-    create(:subscription, frequency: :daily, search_criteria: { subject: 'english', working_pattern: 'full_time' }.to_json)
+    create(:subscription, frequency: :daily,
+      search_criteria: { subject: 'english', working_pattern: 'full_time' }.to_json
+    )
   end
-  
+
   let!(:new_style_subscription) do
-    create(:subscription, frequency: :daily, search_criteria: { subject: 'english', working_patterns: ['full_time', 'part_time'] }.to_json)
+    create(:subscription, frequency: :daily,
+      search_criteria: { subject: 'english', working_patterns: ['full_time', 'part_time'] }.to_json
+    )
   end
 
   let!(:no_working_pattern_subscription) do
-    create(:subscription, frequency: :daily, search_criteria: { subject: 'english' }.to_json)    
+    create(:subscription, frequency: :daily, search_criteria: { subject: 'english' }.to_json)
   end
 
   let!(:nil_criteria_subscription) do
@@ -19,21 +23,21 @@ RSpec.describe 'subscription_records:convert_subscriptions', type: :task do
 
   before do
     UpdateSubscriptionsWithNewWorkingPatterns.run!
-  end 
+  end
 
   it 'Converts a string working pattern to an array working pattern in search criteria' do
     search_criteria_hash = old_style_subscription.reload.search_criteria_to_h
-    expect(search_criteria_hash).to eq({"subject"=>"english", "working_patterns"=>["full_time"]})
+    expect(search_criteria_hash).to eq({ 'subject'=>'english', 'working_patterns'=>['full_time'] })
   end
 
   it 'Does not make any changes to the search criteria of a new style working pattern subscription' do
     search_criteria_hash = new_style_subscription.reload.search_criteria_to_h
-    expect(search_criteria_hash).to eq({"subject"=>"english", "working_patterns"=>["full_time","part_time"]})
+    expect(search_criteria_hash).to eq({ 'subject'=>'english', 'working_patterns'=>['full_time', 'part_time'] })
   end
 
   it 'Does not make any changes to the search criteria of a subscription with no working pattern' do
     search_criteria_hash = no_working_pattern_subscription.reload.search_criteria_to_h
-    expect(search_criteria_hash).to eq({"subject"=>"english"})
+    expect(search_criteria_hash).to eq({ 'subject'=>'english' })
   end
 
   it 'Does not make any changes to a subscription with nil search criteria' do
