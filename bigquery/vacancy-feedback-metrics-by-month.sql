af32734f6aa096358a8184a07c474f2fac9d12b4
Todd Tyree <tatyree@gmail.com>
2020-01-21 11:11:35 +0000

    Create vacancy-feedback-metrics-by-month.sql
    To ensure we have a copy of this that is accessible by everyone and is under version control.

---
+++ bigquery/vacancy-feedback-metrics-by-month.sql
@@ -0,0 +1,39 @@
+SELECT
+  *,
+  SAFE_DIVIDE(feedback_available, #calculate various metrics which are proportions of other metrics. SAFE_DIVIDE handles the legitimate cases where the denominator is zero, passing null instead of failing in these cases.
+    vacancies_published) AS response_rate,
+  SAFE_DIVIDE(hires_through_tv,
+    feedback_available) AS hires_rate_through_tv,
+  SAFE_DIVIDE(hires_through_tv_upperbound,
+    feedback_available) AS hires_rate_through_tv_upperbound,
+  SAFE_DIVIDE(exclusive_hires,
+    feedback_available) AS exclusive_hires_rate,
+  SAFE_DIVIDE(exclusive_hires_upperbound,
+    feedback_available) AS exclusive_hires_rate_upperbound
+FROM (
+  SELECT
+    CAST(TIMESTAMP_TRUNC(publish_on,MONTH) AS DATE) AS month,
+    COUNT(*) AS vacancies_published,
+    COUNTIF(hired_status IS NOT NULL
+      AND listed_elsewhere IS NOT NULL) AS feedback_available,
+    COUNTIF(hired_status="hired_tvs") AS hires_through_tv,
+    COUNTIF(hired_status IN ("hired_dont_know",
+        "hired_tvs")) AS hires_through_tv_upperbound,
+    COUNTIF(hired_status IN ("hired_tvs")
+      AND listed_elsewhere IN ("not_listed",
+        "listed_free")) AS exclusive_hires,
+    COUNTIF(hired_status IN ("hired_dont_know",
+        "hired_tvs")
+      AND listed_elsewhere IN ("not_listed",
+        "listed_free",
+        "listed_dont_know")) AS exclusive_hires_upperbound
+  FROM
+    `teacher-vacancy-service.production_dataset.vacancies`
+  WHERE
+    status NOT IN ("trashed",
+      "deleted",
+      "draft")
+  GROUP BY
+    month)
+ORDER BY
+  month ASC
