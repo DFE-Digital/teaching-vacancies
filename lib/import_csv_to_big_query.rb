c7f8c4d174209eea6e48206247139d7220bb8166
MadeTech Dushan <dushan@madetech.com>
2020-01-27 16:27:58 +0000

    (chore) rubocop changes, fixes and use env variable

--- lib/import_csv_to_big_query.rb
+++ lib/import_csv_to_big_query.rb
@@ -1,28 +1,25 @@
 require 'google/cloud/bigquery'
 require 'rollbar'
 require 'export_tables_to_cloud_storage'
-require 'active_support'
-include ActiveSupport::Inflector
 
 class ImportCSVToBigQuery
   def load(bigquery: Google::Cloud::Bigquery.new)
-    dataset  = bigquery.dataset ENV['BIG_QUERY_DATASET']
+    dataset = bigquery.dataset ENV['BIG_QUERY_DATASET']
 
     ExportTablesToCloudStorage::TABLES.each do |table_csv|
-      import_csv_uri  = "gs://tvs_staging/csv_export/#{underscore(table_csv)}.csv"
+      import_csv_uri = "gs://#{ENV['CLOUD_STORAGE_BUCKET']}/csv_export/#{underscore(table_csv)}.csv"
       table_id = import_csv_uri.split('/').last.sub('.csv', '')
 
       load_job = dataset.load_job table_id, import_csv_uri, skip_leading: 1, autodetect: true, write: 'truncate'
 
-      load_job.wait_until_done!  # Waits for table load to complete.
+      load_job.wait_until_done! # Waits for table load to complete.
 
       table = dataset.table(table_id)
       if table.nil?
         Rollbar.log(:error, "The #{table_id} table failed to load to Big Query")
       else
         Rollbar.log(:info, "Loaded #{table.rows_count} rows to table #{table.id}")
-      end 
+      end
     end
   end
-
 end
