295e424be114259e4136f04a7fc0e5a5af4a2ca6
cpjmcquillan <connor.mcquillan17@gmail.com>
2020-02-12 13:49:35 +0000

    Ignore JS errors in Capybara feature tests

--- spec/browser_test_helper.rb
+++ spec/browser_test_helper.rb
@@ -2,7 +2,7 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ['--load-images=false'])
+  Capybara::Poltergeist::Driver.new(app, { phantomjs_options: ['--load-images=false'], js_errors: false })
 end
 Capybara.javascript_driver = :poltergeist
 
