63f569a632d75350df2bf80b07e6d694cd0964bd
cpjmcquillan <connor.mcquillan17@gmail.com>
2020-02-12 14:29:36 +0000

    Wait for DOM to load in uploadDocuments.js

--- app/frontend/src/uploadDocuments.js
+++ app/frontend/src/uploadDocuments.js
@@ -1,6 +1,8 @@
 // This triggers a file input when the button is clicked
 
-document.getElementById('file-upload').addEventListener("click", function(e) {
-  e.preventDefault();
-  document.getElementById('upload').click();
-});
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('file-upload').addEventListener("click", function(e) {
+    e.preventDefault();
+    document.getElementById('upload').click();
+  });
+})
