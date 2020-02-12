b99966eda4d71357760d0236d07050302638d5a2
cpjmcquillan <connor.mcquillan17@gmail.com>
2020-02-12 14:29:36 +0000

    Wait for DOM to load in uploadDocuments.js

--- spec/browser_test_helper.rb
+++ spec/browser_test_helper.rb
@@ -1,6 +1,8 @@
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 
+# Setting js:errors to false because unrelated tests failed due to vanilla javascript code
+
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, { phantomjs_options: ['--load-images=false'], js_errors: false })
 end
