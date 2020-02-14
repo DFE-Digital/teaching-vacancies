667b7c22859830d0ef28bd9f2e45178246fd45f9
cpjmcquillan <connor.mcquillan17@gmail.com>
2020-02-14 14:15:15 +0000

    Disabled save and continue after file upload

--- app/frontend/src/uploadDocuments.js
+++ app/frontend/src/uploadDocuments.js
@@ -2,14 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
   const inputFileUpload = document.getElementsByClassName('govuk-file-upload')[0];
   const selectFileButton = document.getElementsByClassName('govuk-button--secondary')[0];
   const uploadFileButton = document.getElementsByClassName('govuk-button--secondary')[1];
+  const saveContinueButton = document.getElementsByName('commit')[1];
 
   selectFileButton.addEventListener('click', function(e) {
     e.preventDefault();
-
     inputFileUpload.click();
   });
 
   inputFileUpload.addEventListener('change', function(e) {
+    saveContinueButton.disabled = true;
     inputFileUpload.form.submit();
   });
 
