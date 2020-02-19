1d6221f5bd85c121e06e44b41b6424f8e15affe4
David Mears <david.mears@digital.education.gov.uk>
2020-02-19 17:27:51 +0000

    Refactor schools factory and update database seeds.rb
    Attributes were alphabetized. I removed rsc_region because we
    don't use it.
    The seeded schools now define some religious characters.
    
    When you define no gias_data, the factory populates the fields.
    
    TODO: Fix the problem where defining more than one gias_data field
    prevents the undefined gias_data fields from being populated.

--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -1,16 +1,6 @@
 FactoryBot.define do
-  OFSTED_RATINGS = ["Outstanding", "Good", "Requires Improvement", "Inadequate"]
-  RELIGIOUS_CHARACTERS = ["Church of England", "Romanic Catholic", "None", "Does not apply"]
-  RSC_REGIONS = [
-    "North-West London and South-Central England",
-    "South-East England and South London",
-    "East of England and North-East London",
-    "East Midlands and the Humber",
-    "Lancashire and West Yorkshire",
-    "North of England",
-    "South West England",
-    "West Midlands"
-  ]
+  OFSTED_RATINGS = ['Outstanding', 'Good', 'Requires Improvement', 'Inadequate']
+  RELIGIOUS_CHARACTERS = ['Church of England', 'Roman Catholic', 'None', 'Does not apply']
 
   factory :school do
     association :school_type
@@ -24,16 +14,15 @@ FactoryBot.define do
       close_date: nil,
       head_first_name: Faker::Name.first_name,
       head_last_name: Faker::Name.last_name,
-      head_title: Faker::Name.prefix.gsub('.',''),
+      head_title: Faker::Name.prefix.gsub('.', ''),
       last_ofsted_inspection_date: Faker::Date.between(from: 999.days.ago, to: 5.days.ago),
       number_of_pupils: Faker::Number.number(digits: 3),
       oftsed_rating: OFSTED_RATINGS.sample,
       open_date: Faker::Date.between(from: 10000.days.ago, to: 1000.days.ago),
       religious_character: RELIGIOUS_CHARACTERS.sample,
-      rsc_region: RSC_REGIONS.sample,
       school_capacity: Faker::Number.number(digits: 4),
       telephone: Faker::Number.number(digits: 11).to_s,
-      trust_name: Faker::Company.name + " Trust"
+      trust_name: Faker::Company.name + ' Trust'
       } }
     local_authority { Faker::Address.state_abbr }
     name { Faker::Educator.secondary_school.strip }
