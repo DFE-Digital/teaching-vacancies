5d32063b36de889faea0d9e996b55ea28ace5704
David Mears <david.mears@digital.education.gov.uk>
2020-02-19 12:13:22 +0000

    Update FactoryBot school creator to have gias_data attribute
    FactoryBot creates schools for testing which are used to seed the database
    For us to query the gias_data attribute, it must exist.

--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -1,19 +1,47 @@
 FactoryBot.define do
+  OFSTED_RATINGS = ["Outstanding", "Good", "Requires Improvement", "Inadequate"]
+  RELIGIOUS_CHARACTERS = ["Church of England", "Romanic Catholic", "None", "Does not apply"]
+  RSC_REGIONS = [
+    "North-West London and South-Central England",
+    "South-East England and South London",
+    "East of England and North-East London",
+    "East Midlands and the Humber",
+    "Lancashire and West Yorkshire",
+    "North of England",
+    "South West England",
+    "West Midlands"
+  ]
+
   factory :school do
     association :school_type
     association :region
 
-    name { Faker::Educator.secondary_school.strip }
-    description { Faker::Lorem.paragraph(sentence_count: 1) }
-    urn { Faker::Number.number(digits: 10) }
     address { Faker::Address.street_name }
-    town { Faker::Address.city }
     county { Faker::Address.state_abbr }
-    local_authority { Faker::Address.state_abbr }
-    postcode { Faker::Address.postcode }
-    phase { :secondary }
+    description { Faker::Lorem.paragraph(sentence_count: 1) }
     easting { '1' }
+    gias_data { {
+      close_date: nil,
+      head_first_name: Faker::Name.first_name,
+      head_last_name: Faker::Name.last_name,
+      head_title: Faker::Name.prefix.gsub('.',''),
+      last_ofsted_inspection_date: Faker::Date.between(from: 999.days.ago, to: 5.days.ago),
+      number_of_pupils: Faker::Number.number(digits: 3),
+      oftsed_rating: OFSTED_RATINGS.sample,
+      open_date: Faker::Date.between(from: 10000.days.ago, to: 1000.days.ago),
+      religious_character: RELIGIOUS_CHARACTERS.sample,
+      rsc_region: RSC_REGIONS.sample,
+      school_capacity: Faker::Number.number(digits: 4),
+      telephone: Faker::Number.number(digits: 11).to_s,
+      trust_name: Faker::Company.name + " Trust"
+      } }
+    local_authority { Faker::Address.state_abbr }
+    name { Faker::Educator.secondary_school.strip }
     northing { '1' }
+    phase { :secondary }
+    postcode { Faker::Address.postcode }
+    town { Faker::Address.city }
+    urn { Faker::Number.number(digits: 6) }
 
     trait :nursery do
       phase { :nursery }
