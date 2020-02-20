dc7db49136ec171a09f4cda8691c3eca16b92560
MadeTech Dushan <dushan@madetech.com>
2020-02-20 15:53:02 +0000

    Create clear key info sections
    This commit is an initial attempt at recreating the design given in
    https://www.figma.com/file/cUXdMUunAsT8NRvEH2sElQ/Sprint-47?node-id=0%3A1
    with the aim of displaying the key information job seekers will wish to
    see before applying to a job at a school in an easy to navigate way.

--- app/presenters/school_presenter.rb
+++ app/presenters/school_presenter.rb
@@ -9,6 +9,20 @@ class SchoolPresenter < BasePresenter
     [model.name, model.town, model.county].reject(&:blank?).join(', ')
   end
 
+  def school_size
+    if model.gias_data.present?
+      return model.gias_data['NumberOfPupils'] + ' pupils enrolled' if model.gias_data['NumberOfPupils'].present?
+      return 'Up to ' + model.gias_data['SchoolCapacity'] + 'pupils' if model.gias_data['SchoolCapacity'].present?
+    end
+    'No data available'
+  end
+
+  def ofsted_report
+    if model.gias_data.present?
+      "https://www.ofsted.gov.uk/oxedu_providers/full/(urn)/#{model.gias_data['URN']}"
+    end
+  end
+
   def full_address
     [model.address, model.town, model.county, model.postcode].compact.join(', ')
   end
