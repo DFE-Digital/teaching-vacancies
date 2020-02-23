c81ad93eaf1aeac708216050f833a2279af70c23
Todd Tyree <tatyree@gmail.com>
2020-02-23 08:25:45 +0000

    Alter the processor to use JSON instead of CSV
    This is a large change and will necessitate changing or removing some of
    the exiting tests.
    
    BigQuery works best if it has a flat, clean data structure to ingest.
    The two-tierd nature of the exiting `data` attributes, combined with
    their tendency to contain incomplete records, makes the import process
    fragile and prone to errors. By normalizing the structure of the json
    data across all related records, hoisting it to the same level as the
    attributes and prefixing with a `data_` identifier, it becomes simpler
    to import to BigQuery and easier to maintain. BigQuery can also
    intelligently cast the column types, which saves Performance Analytics
    from having to work it out in their BigQuery SQL.
    
    The commit is less broken down than I would have liked, but I took this
    on after a late delivery and needed to get the changes live ASAP to
    unblock Performance Analytics. It sensible TODO would be to refactor it
    when we have time to look at the overall data structuring.

--- lib/export_tables_to_cloud_storage.rb
+++ lib/export_tables_to_cloud_storage.rb
@@ -1,74 +1,171 @@
-require 'google/cloud/storage'
 require 'fileutils'
+require 'google/cloud/storage'
 
 class ExportTablesToCloudStorage
+  # Skip attributes that cannot be queried, we do not report on or that frequently break the import.
+  # Dropping gias_data as it is aliased in to data to permit handling records without associated gias data.
+  DROP_THESE_ATTRIBUTES = %w[
+    education
+    experience
+    gias_data
+    job_description
+    qualifications
+    supporting_documents
+    weekly_hours
+  ].freeze
+
   TABLES = %w[
-      Vacancy
-      AlertRun
-      AuditData
-      DetailedSchoolType
-      GeneralFeedback
-      Leadership
-      PayScale
-      Region
-      School
-      SchoolType
-      Subject
-      Subscription
-      TransactionAuditor
-      User
-      VacancyPublishFeedback
-  ]
+    AlertRun
+    AuditData
+    DetailedSchoolType
+    GeneralFeedback
+    Leadership
+    PayScale
+    Region
+    School
+    SchoolType
+    Subject
+    Subscription
+    TransactionAuditor
+    User
+    Vacancy
+    VacancyPublishFeedback
+  ].freeze
 
   BATCH_SIZE = 1000
 
-  attr_reader :bucket
+  attr_reader :bucket, :tmpdir, :data_field_normalizer
 
   def initialize(storage: Google::Cloud::Storage.new)
-    @bucket = storage.bucket ENV.fetch('CLOUD_STORAGE_BUCKET')
+    # @bucket = storage.bucket ENV.fetch('CLOUD_STORAGE_BUCKET')
+    @data_field_normalizer = {}
+    @tmpdir = Dir.mktmpdir
   end
 
   def run!
-    export_csv_files
-    upload_to_google_cloud_storage
-    remove_csv_folder
+    export
+    # upload_to_google_cloud_storage
+    # FileUtils.rm_rf(Rails.root.join(tmpdir))
   end
 
   private
 
-  def export_csv_files
-    Dir.mkdir(Rails.root.join('tmp/csv_export')) unless File.exist?(Rails.root.join('tmp/csv_export'))
-
+  def export
+    logging_details = { status: 'starting' }
     TABLES.each do |table|
-      file = Rails.root.join("tmp/csv_export/#{table.underscore}.csv")
-      table == 'Vacancy' ? records = vacancy_records_with_removed_fields : records = table.constantize.all
+      file = Rails.root.join(tmpdir, "#{table.underscore}.json")
+      logging_details[:table] = table
+      logging_details[:outfile] = file
+      Rails.logger.info(logging_details.to_json)
+
+      # This permits selective re-runs without needing to overwrite everything.
+      # Mostly to be used from the REPL or scripts
+      if File.exist?(file)
+        logging_details[:exists] = 'skipping'
+        Rails.logger.info(logging_details.to_json)
+        next
+      end
+
+      fh = File.new(file, 'w')
+
+      records = table.constantize
+      logging_details[:record_count] = records.count
+      logging_details[:records_processed] = 0
 
       next unless records.any?
 
-      CSV.open(file, 'w') do |csv|
-        csv << records.first.attributes.keys
-        records.find_in_batches batch_size: BATCH_SIZE do |batch|
-          batch.each do |row|
-            csv << row.attributes.values
-          end
+      analyze_data_field(records, table.to_s) if records.all.first.respond_to?(:data)
+
+      records.find_in_batches(batch_size: BATCH_SIZE) do |batch|
+        batch.each do |row|
+          fh.puts clean_record(row, table.to_s).to_json
+        end
+        logging_details[:records_processed] += batch.size
+        Rails.logger.info(logging_details.to_json)
+      end
+      fh.close
+    end
+
+    logging_details[:status] = 'finished'
+    Rails.logger.info(logging_details.to_json)
+  end
+
+  # Ensure that all records have a consistent map of the data keys. This is to prevnet BigQuery breaking when importing
+  # records with flattened json columns that have differing numbers of attributes. This is a tradeoff between runtime
+  # and maintainability.
+  #
+  # TODO: This would be much more sensibly done in an `#as_json` method on each model. However, we have a lot of models
+  # and some dependencies that are not immediately obvious-like the RESTFUL API endpoints. It would be easy to break
+  # something by doing that so, until we've got more time to refactor, this is the safer approach. That said, even that
+  # approach would have to traverse all the records to make sure it had captured the whole schema.
+  def analyze_data_field(records, table_name)
+    logging_details = { table: table_name, phase: 'json_analysis', status: 'starting' }
+    Rails.logger.info(logging_details.to_json)
+    bad_records = []
+    data_field_normalizer[table_name] = SortedSet.new()
+    records.find_in_batches(batch_size: BATCH_SIZE) do |batch|
+      batch.each do |row|
+        if row.data.respond_to?(:keys)
+          data_field_normalizer[table_name].merge(row.data.keys)
+        else
+          bad_records << row.id
         end
       end
     end
+    logging_details[:status] = 'finished'
+    logging_details[:bad_records] = bad_records
+    logging_details[:bad_record_count] = bad_records.size
+    Rails.logger.info(logging_details.to_json)
+  end
+
+  def clean_record(record, table_name)
+    scratch = {}
+    record.as_json.keys.each { |key|
+      if record.class.columns_hash[key].type == :datetime || record.class.columns_hash[key].type == :date
+        # BigQuery uses the same standard format for dates and datetimes that `.to_s(:db)` outputs.
+        if record[key].blank?
+          scratch[key] = nil
+        else
+          scratch[key] = record[key].to_s(:db)
+        end
+      elsif key.match?(/data/) # Flatten the data structure into one level
+        # These are 'bad' records. They normally have arrays instead of hashes and they break processing. The list was
+        # recorded in `analyze_data_field` so does not need to recorded again.
+        if record.data.respond_to?(:keys)
+          data_field_normalizer[table_name].each do |normalized_key|
+            value = record.data[normalized_key]
+            # This is the only type in gias data that can be cast without running in to trouble with variations in the
+            # data. For example, trying to cast numbers reveals a wide variety of value types for DioceseCode.
+            value = Date.parse(value).to_s(:db) if value.match?(/^\d{2}\-\d{2}\-\d{4}$/)
+            # I didn't use `#parameterize(separator: '_')` here because it is **SLOW** in contrast to this.
+            scratch["data_#{normalized_key.chomp(')').gsub(/\W+/, '_')}"] = value.presence
+          end
+        else
+          data_field_normalizer[table_name].each do |normalized_key|
+            scratch["data_#{normalized_key.chomp(')').gsub(/\W+/, '_')}"] = nil
+          end
+        end
+      elsif DROP_THESE_ATTRIBUTES.include?(key)
+        # noop - skip attributes that cannot be queried, we do not report on or that frequently break the import.
+      else
+        value = record[key]
+        value = value.to_f if value.is_a?(String) && value.match(/^(\d|\.)+$/)
+        value = value.to_i if value.is_a?(String) && value.match(/^(\d)+$/)
+        scratch[key] = value.presence
+      end
+    }
+    scratch
   end
 
   def upload_to_google_cloud_storage
-    Dir.children('tmp/csv_export').each do |csv_file|
-      local_file_path   = "tmp/csv_export/#{csv_file}"
-      storage_file_path = "csv_export/#{csv_file}"
-      bucket.create_file local_file_path, storage_file_path
+    Dir.children(tmpdir).each do |file|
+      logging_details = { file: file, status: 'uploading' }
+      Rails.logger.info(logging_details.to_json)
+      local_path = Rails.root.join(tmpdir, "#{file}.json")
+      bucket_path = "json_export/#{file}.json"
+      bucket.create_file(local_path, bucket_path)
+      logging_details = { file: file, status: 'uploaded' }
+      Rails.logger.info(logging_details.to_json)
     end
-  end
-
-  def remove_csv_folder
-    FileUtils.rm_rf(Rails.root.join('tmp/csv_export'))
-  end
-
-  def vacancy_records_with_removed_fields
-    Vacancy.select(Vacancy.column_names - ['job_description', 'weekly_hours'].map(&:to_s))
   end
 end
