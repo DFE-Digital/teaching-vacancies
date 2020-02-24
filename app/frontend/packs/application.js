4c906c1587f4c0fbab673fb17cdef2abf309da7d
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2020-02-24 11:14:44 +0000

    Remove unused javascript library

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,7 +7,6 @@ import Rails from 'rails-ujs';
 
 import 'src/addTitleToGoogleMapsIframe';
 import 'src/deleteAccordionControl';
-import 'src/deleteDocument';
 import 'src/details';
 import 'src/enableRadiusFilter';
 import 'src/fetchLocation';
