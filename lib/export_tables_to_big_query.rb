471f759badd0a2dd9d29687a931843d9b45293ec
Todd Tyree <tatyree@gmail.com>
2020-03-03 10:39:06 +0000

    Enable a reporting table
    This will allow us to track and monitor BQ building runs directly in BQ
    itself.

--- lib/export_tables_to_big_query.rb
+++ lib/export_tables_to_big_query.rb
@@ -90,6 +90,12 @@ class ExportTablesToBigQuery
 
       bad_records["#{table.underscore}.json"] = (records_count * BAD_RECORD_RATE).to_i
       total_records["#{table.underscore}.json"] = records_count
+      reporting = {
+        permitted_number_of_bad_records: bad_records["#{table.underscore}.json"],
+        records_expected: records_count,
+        started_at: DateTime.now.to_s(:db),
+        table: [BIGQUERY_TABLE_PREFIX, table].join('_'),
+      }
 
       Rails.logger.info(logging_details.to_json)
       next if records.none?
@@ -104,9 +110,31 @@ class ExportTablesToBigQuery
         end
         logging_details[:records_processed] += batch.size
       end
+
+      reporting = reporting.merge({
+        finished_at: DateTime.now.to_s(:db),
+        records_processed: logging_details[:records_processed]
+      })
+
+      create_or_update_reporting_table(reporting)
+
       fh.close
       Rails.logger.info(logging_details.to_json)
     end
+  end
+
+  def create_or_update_reporting_table(data)
+    table = dataset.table('reporting')
+    table = dataset.create_table 'reporting' do |schema|
+      schema.timestamp 'finished_at'
+      schema.integer 'permitted_number_of_bad_records', mode: :required
+      schema.integer 'records_expected', mode: :required
+      schema.integer 'records_processed'
+      schema.timestamp 'started_at', mode: :required
+      schema.string 'table', mode: :required
+    end if table.nil?
+
+    table.insert(data)
   end
 
   # Ensure that all records have a consistent map of the data keys. This is to prevent BigQuery breaking when importing
@@ -234,11 +262,11 @@ class ExportTablesToBigQuery
 
       if dataset.load(
         table_id,
-          endpoint,
-          autodetect: true,
-          format: 'json',
-          max_bad_records: bad_records[file],
-          write: 'truncate'
+        endpoint,
+        autodetect: true,
+        format: 'json',
+        max_bad_records: bad_records[file],
+        write: 'truncate'
       )
         table = dataset.table(table_id)
         logging_details = logging_details.merge({
