dd05d65543ec7b5e8f5a18800bf8b079dd224a2b
David Mears <david.mears@digital.education.gov.uk>
2020-03-04 16:27:39 +0000

    Update text and style for key dates timeline sidebar

--- spec/features/hiring_staff_can_publish_a_vacancy_spec.rb
+++ spec/features/hiring_staff_can_publish_a_vacancy_spec.rb
@@ -793,7 +793,7 @@ RSpec.feature 'Creating a vacancy' do
 
         expect(page).to have_content("Your job listing will be posted on #{format_date(vacancy.publish_on)}.")
         visit school_job_path(vacancy.id)
-        expect(page).to have_content('Date posted')
+        expect(page).to have_content('Date listed')
         expect(page).to have_content("#{format_date(vacancy.publish_on)}")
       end
 
