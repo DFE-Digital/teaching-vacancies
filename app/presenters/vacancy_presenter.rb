7d309b31802de3fa51c4bfe8ac8874d4e994835d
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2020-03-06 20:33:54 +0000

    Remove flexible working pattern messages

--- app/presenters/vacancy_presenter.rb
+++ app/presenters/vacancy_presenter.rb
@@ -119,16 +119,6 @@ class VacancyPresenter < BasePresenter
     model.newly_qualified_teacher? ? 'Suitable' : 'Not suitable'
   end
 
-  def flexible_working
-    return unless model.flexible_working?
-
-    mailto = mail_to(model.contact_email, model.school.name, class: 'govuk-link')
-
-    # rubocop:disable Rails/OutputSafety
-    I18n.t('jobs.flexible_working_info', mailto: mailto).html_safe
-    # rubocop:enable Rails/OutputSafety
-  end
-
   def working_patterns?
     model_working_patterns.present?
   end
