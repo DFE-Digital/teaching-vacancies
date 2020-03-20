5bc4c8eefc38b79ddc98eb245b64db066b9dd9e4
Todd Tyree <tatyree@gmail.com>
2020-03-20 11:39:00 +0000

    Provide soted array of arrays of step number/title
    This allows the sidebar to be automatically built using details from the
    routes.

--- app/helpers/hiring_staff/job_creation_helper.rb
+++ app/helpers/hiring_staff/job_creation_helper.rb
@@ -1,9 +1,17 @@
 module HiringStaff::JobCreationHelper
-  def total_steps
-    Rails.application.routes.routes.select { |r| r.defaults.has_key?(:create_step) }.size
-  end
-
   def current_step
     params[:create_step]
+  end
+
+  def steps_to_display
+    Rails.application.routes.routes.select { |r|
+      r.defaults.has_key?(:create_step) && r.defaults.has_key?(:step_title)
+    }.map{ |r|
+      [r.defaults[:create_step], r.defaults[:step_title]]
+    }.sort
+  end
+
+  def total_steps
+    Rails.application.routes.routes.select { |r| r.defaults.has_key?(:create_step) }.size
   end
 end
--- spec/helpers/hiring_staff/job_creation_helper_spec.rb
+++ spec/helpers/hiring_staff/job_creation_helper_spec.rb
@@ -1,23 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe HiringStaff::JobCreationHelper do
-  describe '#total_steps' do
-    around do |example|
-      Rails.application.routes.draw do
-        get :step_one, params: { to: 'job#step_one', defaults: { create_step: 1 } }
-        get :step_two, params: { to: 'job#step_two', defaults: { create_step: 2 } }
-      end
-
-      example.run
-
-      Rails.application.routes_reloader.reload!
-    end
-
-    it 'counts the number of routes with { defaults: { create_step: /\d+/ }} and returns the total' do
-      expect(helper.total_steps).to eql(2)
-    end
-  end
-
   describe '#current_step' do
     let(:params) do
       double('params')
@@ -27,6 +10,72 @@ RSpec.describe HiringStaff::JobCreationHelper do
       allow(params).to receive(:[]).with(:create_step).and_return(1)
       allow(helper).to receive(:params).and_return(params)
       expect(helper.current_step).to eql(1)
+    end
+  end
+
+  describe '#steps_to_display' do
+    context 'steps are correctly ordered in `routes.rb`' do
+      around do |example|
+        # Rubocop mistakes the verb-based route definitions for the identically named commands used to interact with
+        # environment.
+        # rubocop:disable Rails/HttpPositionalArguments
+        Rails.application.routes.draw do
+          get :step_one, to: 'job#step_one', defaults: { create_step: 1, step_title: 'Step one' }
+          get :step_two, to: 'job#step_two', defaults: { create_step: 2, step_title: 'Step two' }
+        end
+        # rubocop:enable Rails/HttpPositionalArguments
+
+        example.run
+
+        Rails.application.routes_reloader.reload!
+      end
+
+      it 'returns an array of arrays with the step number and step title to be displayed' do
+        expect(helper.steps_to_display).to eql([[1, 'Step one'], [2, 'Step two']])
+      end
+    end
+
+    context 'steps are not ordered in `routes.rb`' do
+      around do |example|
+        # Rubocop mistakes the verb-based route definitions for the identically named commands used to interact with
+        # environment.
+        # rubocop:disable Rails/HttpPositionalArguments
+        Rails.application.routes.draw do
+          get :step_three, to: 'job#step_three', defaults: { create_step: 3, step_title: 'Step three' }
+          get :step_one, to: 'job#step_one', defaults: { create_step: 1, step_title: 'Step one' }
+          get :step_two, to: 'job#step_two', defaults: { create_step: 2, step_title: 'Step two' }
+        end
+        # rubocop:enable Rails/HttpPositionalArguments
+
+        example.run
+
+        Rails.application.routes_reloader.reload!
+      end
+
+      it 'returns an array of arrays with the step number and step title to be displayed' do
+        expect(helper.steps_to_display).to eql([[1, 'Step one'], [2, 'Step two'], [3, 'Step three']])
+      end
+    end
+  end
+
+  describe '#total_steps' do
+    around do |example|
+      # Rubocop mistakes the verb-based route definitions for the identically named commands used to interact with
+      # environment.
+      # rubocop:disable Rails/HttpPositionalArguments
+      Rails.application.routes.draw do
+        get :step_one, to: 'job#step_one', defaults: { create_step: 1, step_title: 'Step one' }
+        get :step_two, to: 'job#step_two', defaults: { create_step: 2, step_title: 'Step two' }
+      end
+      # rubocop:enable Rails/HttpPositionalArguments
+
+      example.run
+
+      Rails.application.routes_reloader.reload!
+    end
+
+    it 'counts the number of routes with { defaults: { create_step: /\d+/ }} and returns the total' do
+      expect(helper.total_steps).to eql(2)
     end
   end
 end
