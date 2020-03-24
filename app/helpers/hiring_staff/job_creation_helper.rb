d0889c9517a56753f74a00bdf36515abd21e90ba
Todd Tyree <tatyree@gmail.com>
2020-03-24 17:32:43 +0000

    Apply classes to the sidebar circles based on current step

--- app/helpers/hiring_staff/job_creation_helper.rb
+++ app/helpers/hiring_staff/job_creation_helper.rb
@@ -6,12 +6,20 @@ module HiringStaff::JobCreationHelper
   def steps_to_display
     Rails.application.routes.routes.select { |r|
       r.defaults.has_key?(:create_step) && r.defaults.has_key?(:step_title)
-    }.map{ |r|
-      [r.defaults[:create_step], r.defaults[:step_title]]
-    }.sort.uniq
+    }.map { |r|
+      { number: r.defaults[:create_step], title: r.defaults[:step_title] }
+    }.sort_by { |r| r[:number] }.uniq { |r| r[:number] }
   end
 
   def total_steps
     steps_to_display.size
+  end
+
+  def set_active_step_class(step_number)
+    return 'app-step-nav__step--active' if current_step == step_number
+  end
+
+  def set_visited_step_class(step_number)
+    return 'app-step-nav__step--visited' if current_step > step_number
   end
 end
