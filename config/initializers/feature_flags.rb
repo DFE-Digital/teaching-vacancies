75bd8598beae83e6ad5f580825362473a69978d0
David Mears <david.mears@digital.education.gov.uk>
2020-03-31 10:29:17 +0100

    Delete remaining key-info-search-snippet feature flag code
    This removes two lines of left over code about a flag we don't use any more.

--- config/initializers/feature_flags.rb
+++ config/initializers/feature_flags.rb
@@ -2,7 +2,6 @@ require 'feature_flag'
 
 FEATURE_EMAIL_ALERTS = ENV['FEATURE_EMAIL_ALERTS']
 FEATURE_IMPORT_VACANCIES = ENV['FEATURE_IMPORT_VACANCIES']
-FEATURE_KEY_INFO_SEARCH_SNIPPET = ENV['FEATURE_KEY_INFO_SEARCH_SNIPPET']
 
 EmailAlertsFeature = FeatureFlag.new('email_alerts')
 ImportVacanciesFeature = FeatureFlag.new('import_vacancies')
