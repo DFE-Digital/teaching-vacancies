798ad148d1afed9e05e48363c82ad0e227061478
cpjmcquillan <connor.mcquillan17@gmail.com>
2020-04-02 10:00:07 +0100

    Update specs for editing draft vacancy to ensure correct redirects

--- spec/features/hiring_staff_can_edit_a_draft_vacancy_spec.rb
+++ spec/features/hiring_staff_can_edit_a_draft_vacancy_spec.rb
@@ -1,57 +1,124 @@
 require 'rails_helper'
 RSpec.feature 'Hiring staff can edit a draft vacancy' do
   let(:school) { create(:school) }
+  let!(:vacancy) do
+    VacancyPresenter.new(build(:vacancy, :complete,
+                               job_title: 'Draft vacancy',
+                               school: school,
+                               working_patterns: ['full_time', 'part_time']))
+  end
+  let(:draft_vacancy) { Vacancy.find_by(job_title: vacancy.job_title) }
 
   before do
     stub_hiring_staff_auth(urn: school.urn)
+    visit new_school_job_path
+    fill_in_job_specification_form_fields(vacancy)
+    click_on I18n.t('buttons.save_and_continue')
   end
 
-  context 'with job specification completed' do
-    let!(:vacancy) do
-      VacancyPresenter.new(build(:vacancy, :complete,
-                                 job_title: 'Draft vacancy',
-                                 school: school,
-                                 working_patterns: ['full_time', 'part_time']))
-    end
-    let(:draft_vacancy) { Vacancy.find_by(job_title: vacancy.job_title) }
-
-    before do
-      visit new_school_job_path
-      fill_in_job_specification_form_fields(vacancy)
-      click_on I18n.t('buttons.save_and_continue')
-    end
-
-    scenario 'redirects to incomplete pay package step, with fields pre-populated' do
-      draft_vacancy.benefits = 'Lots of benefits'
-      draft_vacancy.save(validate: false)
-
+  context '#redirects_to' do
+    scenario 'incomplete pay package step' do
       visit edit_school_job_path(id: draft_vacancy.id)
 
       expect(page).to have_content(I18n.t('jobs.current_step', step: 2, total: 5))
-      expect(page).to have_content(draft_vacancy.benefits)
+      expect(page).to have_content(I18n.t('jobs.pay_package'))
     end
 
-    context 'when editing a different vacancy' do
-      # We use the session to store vacancy attributes, make sure it doesn't leak between edits.
-      before do
-        edit_a_published_vacancy
-      end
+    scenario 'incomplete supporting documents step' do
+      visit edit_school_job_path(id: draft_vacancy.id)
 
-      scenario 'then editing the draft redirects to incomplete step' do
-        visit school_job_path(id: draft_vacancy.id)
-        expect(page).to have_content(I18n.t('jobs.current_step', step: 2, total: 5))
-      end
+      draft_vacancy.salary = 'Pay scale 1 to Pay scale 2'
+      draft_vacancy.benefits = 'Gym, health insurance'
 
-      def edit_a_published_vacancy
-        published_vacancy = create(:vacancy, :published, school: school)
-        visit edit_school_job_path(published_vacancy.id)
-        click_link_in_container_with_text(I18n.t('jobs.application_link'))
+      fill_in_pay_package_form_fields(draft_vacancy)
+      click_on I18n.t('buttons.save_and_continue')
 
-        fill_in 'application_details_form[application_link]', with: 'https://example.com'
-        click_on I18n.t('buttons.update_job')
+      expect(page).to have_content(I18n.t('jobs.current_step', step: 3, total: 5))
+      expect(page).to have_content(I18n.t('jobs.supporting_documents'))
+    end
 
-        expect(page).to have_content(I18n.t('messages.jobs.updated'))
-      end
+    scenario 'documents step if YES selected for supporting documents' do
+      visit edit_school_job_path(id: draft_vacancy.id)
+
+      draft_vacancy.salary = 'Pay scale 1 to Pay scale 2'
+      draft_vacancy.benefits = 'Gym, health insurance'
+
+      fill_in_pay_package_form_fields(draft_vacancy)
+      click_on I18n.t('buttons.save_and_continue')
+
+      find('label[for="supporting-documents-form-supporting-documents-field-error"]').click
+      click_on I18n.t('buttons.save_and_continue')
+
+      expect(page).to have_content(I18n.t('jobs.current_step', step: 3, total: 5))
+      expect(page).to have_content(I18n.t('jobs.supporting_documents'))
+    end
+
+    scenario 'application details step if NO selected for supporting documents' do
+      visit edit_school_job_path(id: draft_vacancy.id)
+
+      draft_vacancy.salary = 'Pay scale 1 to Pay scale 2'
+      draft_vacancy.benefits = 'Gym, health insurance'
+
+      fill_in_pay_package_form_fields(draft_vacancy)
+      click_on I18n.t('buttons.save_and_continue')
+
+      select_no_for_supporting_documents
+      click_on I18n.t('buttons.save_and_continue')
+
+      expect(page).to have_content(I18n.t('jobs.current_step', step: 4, total: 5))
+      expect(page).to have_content(I18n.t('jobs.application_details'))
+    end
+
+    scenario 'incomplete application details step' do
+      visit edit_school_job_path(id: draft_vacancy.id)
+
+      draft_vacancy.salary = 'Pay scale 1 to Pay scale 2'
+      draft_vacancy.benefits = 'Gym, health insurance'
+
+      fill_in_pay_package_form_fields(draft_vacancy)
+      click_on I18n.t('buttons.save_and_continue')
+
+      find('label[for="supporting-documents-form-supporting-documents-field-error"]').click
+      click_on I18n.t('buttons.save_and_continue')
+
+      click_on I18n.t('buttons.save_and_continue')
+
+      expect(page).to have_content(I18n.t('jobs.current_step', step: 4, total: 5))
+      expect(page).to have_content(I18n.t('jobs.application_details'))
+    end
+  end
+
+  context 'after editing a different vacancy' do
+    # We use the session to store vacancy attributes, make sure it doesn't leak between edits.
+    before do
+      visit edit_school_job_path(id: draft_vacancy.id)
+
+      draft_vacancy.salary = 'Pay scale 1 to Pay scale 2'
+      draft_vacancy.benefits = 'Gym, health insurance'
+
+      fill_in_pay_package_form_fields(draft_vacancy)
+      click_on I18n.t('buttons.save_and_continue')
+
+      select_no_for_supporting_documents
+      click_on I18n.t('buttons.save_and_continue')
+
+      edit_a_published_vacancy
+    end
+
+    scenario 'then editing the draft redirects to incomplete step' do
+      visit school_job_path(id: draft_vacancy.id)
+      expect(page).to have_content(I18n.t('jobs.current_step', step: 4, total: 5))
+    end
+
+    def edit_a_published_vacancy
+      published_vacancy = create(:vacancy, :published, school: school)
+      visit edit_school_job_path(published_vacancy.id)
+      click_link_in_container_with_text(I18n.t('jobs.application_link'))
+
+      fill_in 'application_details_form[application_link]', with: 'https://example.com'
+      click_on I18n.t('buttons.update_job')
+
+      expect(page).to have_content(I18n.t('messages.jobs.updated'))
     end
   end
 end
