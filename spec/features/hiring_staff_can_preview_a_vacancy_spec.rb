71a4b819aa5fff954fac04d2066f94dcdbaea6bf
cpjmcquillan <connor.mcquillan17@gmail.com>
2020-04-04 10:57:59 +0100

    Hiring staff preview page feature spec

---
+++ spec/features/hiring_staff_can_preview_a_vacancy_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.feature 'Hiring staff can preview a vacancy' do
+  let(:school) { create(:school) }
+  let(:session_id) { SecureRandom.uuid }
+  let(:vacancy) { create(:vacancy, :draft, school: school) }
+
+  before(:each) do
+    stub_hiring_staff_auth(urn: school.urn, session_id: session_id)
+  end
+
+  context 'when reviewing a draft vacancy' do
+    before do
+      visit school_job_review_path(vacancy.id)
+    end
+
+    scenario 'review page shows preview, submit and save calls to action' do
+      expect(page).to have_content(I18n.t('jobs.preview_listing.button'))
+      expect(page).to have_content(I18n.t('jobs.submit_listing.button'))
+      expect(page).to have_content(I18n.t('buttons.save_and_return'))
+    end
+
+    scenario 'users can preview the listing' do
+      click_on I18n.t('jobs.preview_listing.button')
+      expect(page).to have_current_path(school_job_preview_path(vacancy.id))
+      expect(page).to have_content('This is the preview page')
+    end
+
+    scenario 'users can submit the listing' do
+      click_on I18n.t('jobs.submit_listing.button')
+      expect(page).to have_current_path(school_job_summary_path(vacancy.id))
+      expect(page).to have_content(I18n.t('jobs.confirmation_page.submitted'))
+    end
+
+    scenario 'users can save and return later' do
+      click_on I18n.t('buttons.save_and_return')
+      expect(page).to have_current_path(school_path)
+      expect(page).to have_content(I18n.t('schools.jobs.index', school: school.name))
+      expect(page).to have_content(I18n.t('buttons.create_job'))
+    end
+  end
+end
