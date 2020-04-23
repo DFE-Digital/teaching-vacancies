d0559e22bc9301a1e4cef597e1ba51027473b4e5
cpjmcquillan <connor.mcquillan17@gmail.com>
2020-04-23 09:45:46 +0100

    Update copy vacancy controller

--- app/controllers/hiring_staff/vacancies/copy_controller.rb
+++ app/controllers/hiring_staff/vacancies/copy_controller.rb
@@ -4,39 +4,40 @@ class HiringStaff::Vacancies::CopyController < HiringStaff::Vacancies::Applicati
   include PersistNQTJobRole
 
   def new
-    vacancy = Vacancy.find(vacancy_id)
-    @copy_form = CopyVacancyForm.new(vacancy: vacancy)
+    @copy_form = CopyVacancyForm.new(vacancy: @vacancy)
   end
 
   def create
-    old_vacancy = Vacancy.find(vacancy_id)
-    @copy_form = CopyVacancyForm.new(vacancy: old_vacancy)
-    proposed_vacancy = @copy_form.apply_changes!(copy_form_params)
+    @copy_form = CopyVacancyForm.new(vacancy: @vacancy)
+    date_errors = convert_multiparameter_attributes_to_dates(
+      :copy_vacancy_form, [:starts_on, :ends_on, :publish_on, :expires_on]
+    )
+    @proposed_vacancy = @copy_form.apply_changes!(copy_form_params)
+    @copy_form.update_expiry_time(@proposed_vacancy, params.require(:copy_vacancy_form))
+    add_errors_to_form(date_errors, @copy_form)
 
-    if proposed_vacancy.valid? && @copy_form.valid?
-      @copy_form.update_expiry_time(proposed_vacancy, copy_form_params)
-      new_vacancy = CopyVacancy.new(proposed_vacancy).call
+    if valid_copy_form?
+      new_vacancy = CopyVacancy.new(@proposed_vacancy).call
       Auditor::Audit.new(new_vacancy, 'vacancy.copy', current_session_id).log
-      redirect_to review_path(new_vacancy)
-    else
-      render 'new'
+      return redirect_to review_path(new_vacancy)
     end
+
+    render :new
   end
 
   private
 
-  def copy_form_params
-    persist_nqt_job_role_to_nqt_attribute(:copy_vacancy_form)
-    params.require(:copy_vacancy_form).permit(:job_title, :about_school,
-                                              :starts_on_dd, :starts_on_mm, :starts_on_yyyy,
-                                              :ends_on_dd, :ends_on_mm, :ends_on_yyyy,
-                                              :expires_on_dd, :expires_on_mm, :expires_on_yyyy,
-                                              :expiry_time_hh, :expiry_time_mm, :expiry_time_meridiem,
-                                              :publish_on_dd, :publish_on_mm, :publish_on_yyyy,
-                                              :newly_qualified_teacher, job_roles: [])
+  def valid_copy_form?
+    @copy_form.complete_and_valid? && @proposed_vacancy.valid?
   end
 
-  def vacancy_id
-    params.permit(:job_id)[:job_id]
+  def copy_form_params
+    persist_nqt_job_role_to_nqt_attribute(:copy_vacancy_form)
+    strip_empty_checkboxes(:copy_vacancy_form, [:job_roles])
+    params.require(:copy_vacancy_form).permit(:job_title, :about_school,
+                                              :starts_on, :ends_on,
+                                              :publish_on, :expires_on,
+                                              :expiry_time_hh, :expiry_time_mm, :expiry_time_meridiem,
+                                              :newly_qualified_teacher, job_roles: [])
   end
 end
