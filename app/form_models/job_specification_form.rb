14334ad2899d78bd2b70a1e6ff384e84bf50fe27
cpjmcquillan <connor.mcquillan17@gmail.com>
2020-04-23 09:32:17 +0100

    Update job specification form to reflect date field changes

--- app/form_models/job_specification_form.rb
+++ app/form_models/job_specification_form.rb
@@ -1,7 +1,5 @@
 class JobSpecificationForm < VacancyForm
-  delegate(*['starts_on_yyyy', 'starts_on_mm', 'starts_on_dd',
-             'ends_on_dd', 'ends_on_mm', 'ends_on_yyyy',
-             'expires_on'].map { |attr| [attr, "#{attr}=", "#{attr}?"] }.flatten, to: :vacancy)
+  delegate :expires_on, to: :vacancy
 
   include VacancyJobSpecificationValidations
 end
