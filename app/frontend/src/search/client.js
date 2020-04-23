7526d77bd5bfa526c0a84946006aa8dc15922fe8
alexbowen <alexbowen@Alexs-MBP.lan>
2020-04-23 10:14:07 +0100

    add algolia api keys (with safety msg from algolia docs)

--- app/frontend/src/search/client.js
+++ app/frontend/src/search/client.js
@@ -1,4 +1,6 @@
-export const search = algoliasearch('', '')
+// This is the public API key which can be safely used in your frontend code.
+// This key is usable for search queries and list the indices you've got access to.
+export const search = algoliasearch('QM2YE0HRBW', '20b88d28047d5e3d60437993ad3d9c50')
 
 // TODO remove all this global scope
 export const client = indexName => instantsearch({
