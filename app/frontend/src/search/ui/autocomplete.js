53d1b862bbca46229e50623f58168ab345c57337
alexbowen <alexbowen@Alexs-MBP.lan>
2020-04-23 10:46:34 +0100

    add autocomplete click away behaviour

--- app/frontend/src/search/ui/autocomplete.js
+++ app/frontend/src/search/ui/autocomplete.js
@@ -2,23 +2,36 @@ export const renderAutocomplete = (renderOptions, isFirstRender) => {
     const { indices, currentRefinement, refine, widgetParams } = renderOptions;
 
     if (isFirstRender) {
-        const ul = document.createElement('ul');
+        const ul = document.createElement('ul')
 
-        ul.classList.add('app-site-search__menu--overlay');
-        ul.classList.add('app-site-search__menu');
+        ul.classList.add('app-site-search__menu')
 
-        widgetParams.container.appendChild(ul);
+        widgetParams.container.appendChild(ul)
+
+        document.addEventListener('click', () => hide(ul))
     }
+
+    show(widgetParams.container.querySelector('.app-site-search__menu'))
 
     widgetParams.container.querySelector('#location').value = currentRefinement;
 
     widgetParams.container.querySelector('ul').innerHTML = indices
         .map(renderIndexListItem)
-        .join('');
-};
+        .join('')
+}
+
+export const show = element => {
+    element.classList.add('app-site-search__menu--visible');
+    element.classList.remove('app-site-search__menu--hidden');
+}
+
+export const hide = element => {
+    element.classList.add('app-site-search__menu--hidden');
+    element.classList.remove('app-site-search__menu--visible')
+}
 
 export const renderIndexListItem = ({ indexId, hits }) => `
     ${hits
-    .map(hit => `<li class="app-site-search__option">${hit.school.town} (${hit.school.county})</li>`)
-    .join('')}
+        .map(hit => `<li class="app-site-search__option">${hit.school.town} (${hit.school.county})</li>`)
+        .join('')}
 `;
