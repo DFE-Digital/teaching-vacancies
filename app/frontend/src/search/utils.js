6a41d13c0f2014bc34402a47e5355938e440bda3
alexbowen <alexbowen@Alexs-MBP.lan>
2020-05-05 03:12:40 +0100

    add utility method for checking postcode validity

--- app/frontend/src/search/utils.js
+++ app/frontend/src/search/utils.js
@@ -6,3 +6,9 @@ export const constructNewUrlWithParam = (key, value, url) => {
 export const updateUrlQueryParams = (key, value, url) => {
     history.replaceState({}, null, constructNewUrlWithParam(key, value, url));
 };
+
+export const stringMatchesPostcode = postcode => {
+    postcode = postcode.replace(/\s/g, "");
+    var regex = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i;
+    return regex.test(postcode);
+};
--- app/frontend/src/search/utils.test.js
+++ app/frontend/src/search/utils.test.js
@@ -1,4 +1,4 @@
-import { constructNewUrlWithParam } from './utils';
+import { constructNewUrlWithParam, stringMatchesPostcode } from './utils';
 
 describe('constructNewUrlWithParams', () => {
     test('should activate autocomplete if threshold has been met', () => {
@@ -17,5 +17,25 @@ describe('constructNewUrlWithParams', () => {
         )).toBe(
             'https://jobs?radius=20&location=so&job_title=#vacancy-results'
         );
+    });
+});
+
+describe('stringMatchesPostcode', () => {
+    test('should test that string matching correct postcode is true', () => {
+        expect(stringMatchesPostcode('CT9 5ST')).toBe(true);
+        expect(stringMatchesPostcode('CT95ST')).toBe(true);
+        expect(stringMatchesPostcode('SE18 2BT')).toBe(true);
+        expect(stringMatchesPostcode('SE182BT')).toBe(true);
+        expect(stringMatchesPostcode('B2 5ST')).toBe(true);
+        expect(stringMatchesPostcode('B25ST')).toBe(true);
+    });
+
+    test('should test that string matching incorrect postcode is false', () => {
+        expect(stringMatchesPostcode('CT 5ST')).toBe(false);
+        expect(stringMatchesPostcode('CT5ST')).toBe(false);
+        expect(stringMatchesPostcode('SEF8 2BT')).toBe(false);
+        expect(stringMatchesPostcode('SEF82BT')).toBe(false);
+        expect(stringMatchesPostcode('B2 %ST')).toBe(false);
+        expect(stringMatchesPostcode('B2%ST')).toBe(false);
     });
 });
