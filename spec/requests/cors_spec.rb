23c3fee34f6024354545dde07f85eabae57631ae
David Mears <david.mears@digital.education.gov.uk>
2020-05-06 12:12:28 +0100

    Rename tests

--- spec/requests/cors_spec.rb
+++ spec/requests/cors_spec.rb
@@ -5,14 +5,6 @@ RSpec.describe 'CORS', type: :request do
   describe '/api/v1/coordinates/:location.json' do
     let(:location_query) { 'ct9' }
 
-    scenario 'does not allow a request from a different domain' do
-      params = { api_version: 1, location: location_query, format: 'json' }
-      headers = { 'HTTP_ORIGIN': 'https://www.test.com' }
-      get api_path(params), headers: headers
-
-      expect(response).to have_http_status(:not_found)
-    end
-
     scenario 'allows a request from a domain defined by configuration' do
       geocoding = Geocoding.new(location_query)
       allow(Geocoding).to receive(:new).with(location_query).and_return(geocoding)
@@ -25,10 +17,18 @@ RSpec.describe 'CORS', type: :request do
       expect(response).to have_http_status(:ok)
       expect(response.headers['Access-Control-Allow-Origin']).to eq(Rails.application.config.allowed_cors_origin)
     end
+
+    scenario 'does not allow a request from a different domain' do
+      params = { api_version: 1, location: location_query, format: 'json' }
+      headers = { 'HTTP_ORIGIN': 'https://www.test.com' }
+      get api_path(params), headers: headers
+
+      expect(response).to have_http_status(:not_found)
+    end
   end
 
   describe '/api/v1/jobs.json' do
-    scenario 'allows a request from a different domain' do
+    scenario 'is configured to allow a request from any domain' do
       params = { api_version: 1, format: 'json' }
       headers = { 'HTTP_ORIGIN': 'https://www.test.com' }
       get api_jobs_path(params), headers: headers
@@ -41,7 +41,7 @@ RSpec.describe 'CORS', type: :request do
   describe '/api/v1/job/:id.json' do
     let(:vacancy) { create(:vacancy) }
 
-    scenario 'allows a request from a different domain' do
+    scenario 'is configured to allow a request from any domain' do
       params = { id: vacancy.slug, api_version: 1, format: 'json' }
       headers = { 'HTTP_ORIGIN': 'https://www.test.com' }
       get api_job_path(params), headers: headers
