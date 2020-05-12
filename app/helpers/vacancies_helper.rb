ca3a6524f4564122f4e3577eaff360f2e51f92ec
cpjmcquillan <connor.mcquillan17@gmail.com>
2020-05-12 14:29:11 +0100

    Pluralize the radius filter options using I18n

--- app/helpers/vacancies_helper.rb
+++ app/helpers/vacancies_helper.rb
@@ -25,7 +25,7 @@ module VacanciesHelper
 
   def radius_filter_options
     [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 70, 80, 90, 100, 200].inject([]) do |radii, radius|
-      radii << ["#{radius} miles", radius]
+      radii << [I18n.t('jobs.filters.number_of_miles', count: radius), radius]
     end
   end
 
