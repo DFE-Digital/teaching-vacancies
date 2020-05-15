d9d13e1a1d96c9e2e37b2337a309c48f1060fee5
alexbowen <alexbowen@alexs-mbp.home>
2020-05-15 12:29:37 +0100

    fixes to autocomplete so it is hidden/shown appropriately

--- app/frontend/src/search/ui/autocomplete.js
+++ app/frontend/src/search/ui/autocomplete.js
@@ -4,55 +4,59 @@ export const renderAutocomplete = (renderOptions, isFirstRender) => {
     const { currentRefinement, widgetParams } = renderOptions;
 
     if (isFirstRender) {
-        const ul = document.createElement('ul');
-        ul.setAttribute('id', 'location__listbox');
-        ul.setAttribute('role', 'listbox');
-        ul.setAttribute('tabindex', 0);
-
-        ul.classList.add('app-site-search__menu');
-        ul.classList.add('app-site-search__menu--overlay');
-
-        widgetParams.container.appendChild(ul);
-
-        document.addEventListener('click', () => {
-            hide(ul);
-            widgetParams.input.setAttribute('aria-expanded', false);
-        });
-
-        ul.addEventListener('click', (e) => {
-            widgetParams.onSelection(e.target.innerHTML);
-        });
+        create(widgetParams.container, widgetParams.input, widgetParams.onSelection);
     }
 
-    const handleFocus = (e) => {
+    const handleFocus = e => {
         widgetParams.input.value = e.target.innerHTML;
         widgetParams.onSelection(e.target.innerHTML);
     };
 
-    show(widgetParams.container.querySelector('.app-site-search__menu'));
-
     if (isActive(widgetParams.threshold, currentRefinement)) {
         const options = getOptions(widgetParams.dataset, currentRefinement);
-        const active = options.length ? true : false;
-
-        widgetParams.input.setAttribute('aria-expanded', active);
 
         widgetParams.container.querySelector('ul').innerHTML = options.map(renderIndexListItem).join('');
 
+        show(widgetParams.container.querySelector('ul'), widgetParams.input);
+
         Array.from(widgetParams.container.querySelectorAll('.app-site-search__option'))
             .forEach(element => element.addEventListener('focus', (e) => handleFocus(e), true));
+    } else {
+        hide(widgetParams.container.querySelector('ul'), widgetParams.input);
     }
 };
 
-export const show = element => {
+export const show = (element, inputElement) => {
     element.classList.add('app-site-search__menu--visible');
     element.classList.remove('app-site-search__menu--hidden');
+    inputElement.setAttribute('aria-expanded', true);
 };
 
-export const hide = element => {
+export const hide = (element, inputElement) => {
     element.classList.add('app-site-search__menu--hidden');
     element.classList.remove('app-site-search__menu--visible');
+    inputElement.setAttribute('aria-expanded', false);
 };
+
+export const create = (container, input, onSelect) => {
+    const ul = document.createElement('ul');
+    ul.setAttribute('id', 'location__listbox');
+    ul.setAttribute('role', 'listbox');
+    ul.setAttribute('tabindex', 0);
+
+    ul.classList.add('app-site-search__menu');
+    ul.classList.add('app-site-search__menu--overlay');
+
+    container.appendChild(ul);
+
+    document.addEventListener('click', () => {
+        hide(ul, input);
+    });
+
+    ul.addEventListener('click', (e) => {
+        onSelect(e.target.innerHTML);
+    });
+}
 
 export const getOptions = (dataset, query) => dataset.filter((result) => result.toLowerCase().indexOf(query) >= 0);
 
