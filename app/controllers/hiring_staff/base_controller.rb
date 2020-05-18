b931997396076fdf3ea58870ba6f651a53eee4c4
David Mears <david.mears@digital.education.gov.uk>
2020-05-18 12:50:52 +0100

    Sign out user after inactivity server-side.

--- app/controllers/hiring_staff/base_controller.rb
+++ app/controllers/hiring_staff/base_controller.rb
@@ -1,7 +1,11 @@
 class HiringStaff::BaseController < ApplicationController
-  before_action :redirect_to_root_if_read_only
-  before_action :check_session
-  before_action :check_terms_and_conditions
+  TIMEOUT_PERIOD = 60.minutes
+
+  before_action :check_user_last_activity_at,
+    :update_user_last_activity_at,
+    :redirect_to_root_if_read_only,
+    :check_session,
+    :check_terms_and_conditions
 
   helper_method :current_school
 
@@ -27,5 +31,34 @@ class HiringStaff::BaseController < ApplicationController
     return if current_session_id.blank?
 
     @current_user ||= User.find_or_create_by(oid: current_session_id)
+  end
+
+  def check_user_last_activity_at
+    # config/session_store.rb expires sessions after 8 hours of inactivity,
+    # resulting in current_user == nil.
+
+    # TODO: remove config/session_store.rb when it has become redundant.
+
+    # The session_store.rb behaviour does not meet security requirements
+    # as it does not call the dsi_logout_url. Rather it only deletes our session,
+    # so the user may still be logged in on DSI, so signing in
+    # will skip the requirement to enter a password.
+    if current_user&.last_activity_at.blank?
+      redirect_to dsi_logout_url
+    elsif Time.zone.now > (current_user.last_activity_at + TIMEOUT_PERIOD)
+      session[:signing_out_for_inactivity] = true
+      redirect_to dsi_logout_url
+    end
+  end
+
+  def update_user_last_activity_at
+    return if current_user.blank?
+    current_user.update(last_activity_at: Time.zone.now)
+  end
+
+  def dsi_logout_url
+    url = URI.parse("#{ENV['DFE_SIGN_IN_ISSUER']}/session/end")
+    url.query = { post_logout_redirect_uri: auth_dfe_signout_url, id_token_hint: session[:id_token] }.to_query
+    url.to_s
   end
 end
--- spec/features/hiring_staff_session_timeout_spec.rb
+++ spec/features/hiring_staff_session_timeout_spec.rb
@@ -12,20 +12,42 @@ RSpec.feature 'Hiring staff session' do
     Timecop.return
   end
 
-  it 'expires after 8 hours and redirects to login page' do
+  def keep_user_active(length_of_time, path: new_school_job_path)
+    time_between_visits = HiringStaff::BaseController::TIMEOUT_PERIOD / 2
+    number_of_times_to_visit = length_of_time / time_between_visits
+
+    number_of_times_to_visit.times do
+      visit path
+      Timecop.travel(time_between_visits)
+    end
+  end
+
+  # TODO: add tests for TIMEOUT_PERIOD sign out
+
+  it 'expires after inactivity and redirects to login page' do
+    dsi_signout_path = '/session/end'
+
     visit new_school_job_path
 
-    Timecop.travel(8.hours)
+    Timecop.travel(HiringStaff::BaseController::TIMEOUT_PERIOD + 1.second)
 
     click_on I18n.t('buttons.save_and_continue')
 
+    expect(page.current_path).to eq dsi_signout_path
+
+    # A request to logout is sent to DfE Sign-in system. On success DSI comes back at auth_dfe_signout_path
+    visit auth_dfe_signout_path
+
     expect(page.current_path).to eq new_identifications_path
+
+    within('.govuk-header__navigation') { expect(page).to have_content(I18n.t('nav.sign_in')) }
+    expect(page).to have_content(I18n.t('messages.access.signed_out_for_inactivity'))
   end
 
-  it 'doesn\'t expire before 8 hours' do
+  it 'doesn\'t expire while user is active' do
     visit new_school_job_path
 
-    Timecop.travel(1.hour)
+    keep_user_active(10.hours)
 
     click_on I18n.t('buttons.save_and_continue')
 
