cdd9898d2b4316997262b276827edb3b3dd23128
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-05-21 08:54:54 +0100

    Add subjects column to vacancies table as a string array

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_151545) do
+ActiveRecord::Schema.define(version: 2020_05_21_075215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -229,6 +229,7 @@ ActiveRecord::Schema.define(version: 2020_05_19_151545) do
     t.integer "completed_step"
     t.text "about_school"
     t.string "state", default: "create"
+    t.string "subjects", array: true
     t.index ["expires_on"], name: "index_vacancies_on_expires_on"
     t.index ["expiry_time"], name: "index_vacancies_on_expiry_time"
     t.index ["first_supporting_subject_id"], name: "index_vacancies_on_first_supporting_subject_id"
