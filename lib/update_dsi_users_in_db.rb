2be9e5a06906ffce857b711c47fe851e81f1e7f4
David Mears <david.mears@digital.education.gov.uk>
2020-05-21 18:37:42 +0100

    Update user URN/UID arrays in nightly task

--- lib/update_dsi_users_in_db.rb
+++ lib/update_dsi_users_in_db.rb
@@ -4,26 +4,22 @@ class UpdateDfeSignInUsers
   include DFESignIn
 
   def run
-    get_data
-  end
-
-  private
-
-  def get_data
     (1..number_of_pages).each do |page|
       response = api_response(page: page)
 
       if users_nil_or_empty?(response)
         Rollbar.log(:error,
-          "DfE Sign In API responded with zero users during task #{self.class.name}")
+          "DfE Sign In API responded with nil users during job #{self.class.name}")
         raise error_message_for(response)
       end
 
-      update_or_convert_to_users(response['users'])
+      convert_to_users(response['users'])
     end
   end
 
-  def update_or_convert_to_users(dsi_users)
+  private
+
+  def convert_to_users(dsi_users)
     dsi_users.each do |dsi_user|
       User.transaction do
         user = User.find_or_initialize_by(oid: dsi_user['userId'])
@@ -32,9 +28,10 @@ class UpdateDfeSignInUsers
         # DfE Sign In returns 1 user object per organisation.
         # Each of these user objects has the same userId.
         organisation = dsi_user['organisation']
-        # Pending migrations:
-        # user.school_urns += organisation['URN']
-        # user.organisation_uids += organisation['UID']
+        user.dsi_data = {
+          school_urns: Set.new(user.dsi_data['school_urns']).add(organisation['URN']),
+          school_group_uids: Set.new(user.dsi_data['school_group_uids']).add(organisation['UID'])
+        }
         user.save
       end
     end
