296f995e25106664d1d655688122a0be7b2b122c
David Mears <david.mears@digital.education.gov.uk>
2020-05-21 19:14:51 +0100

    Initialize email sign-in feature flag

--- config/initializers/feature_flags.rb
+++ config/initializers/feature_flags.rb
@@ -2,8 +2,11 @@ require 'feature_flag'
 
 FEATURE_READ_ONLY = ENV['FEATURE_READ_ONLY']
 FEATURE_EMAIL_ALERTS = ENV['FEATURE_EMAIL_ALERTS']
+FEATURE_EMAIL_SIGN_IN = ENV['FEATURE_EMAIL_SIGN_IN']
 FEATURE_IMPORT_VACANCIES = ENV['FEATURE_IMPORT_VACANCIES']
+
 
 ReadOnlyFeature = FeatureFlag.new('read_only')
 EmailAlertsFeature = FeatureFlag.new('email_alerts')
+EmailSignInFeature = FeatureFlag.new('email_sign_in')
 ImportVacanciesFeature = FeatureFlag.new('import_vacancies')
