74a090add9329e373949f20f58cd606dbcfa4186
alexbowen <alexbowen@Alexs-MBP.lan>
2020-05-22 15:25:01 +0100

    add autocomplete functionality to homepage search

---
+++ app/frontend/src/locationAutocomplete.js
@@ -0,0 +1,16 @@
+import { renderAutocomplete } from './search/ui/autocomplete';
+import { locations } from './search/data/locations';
+
+$(document).ready(function () {
+    if ($('#new_').length) {
+        renderAutocomplete({
+            widgetParams: {
+                container: document.querySelector('#new_'),
+                input: document.querySelector('#location'),
+                dataset: locations,
+                threshold: 3,
+                onSelection: () => { }
+            }
+        })
+    }
+});
