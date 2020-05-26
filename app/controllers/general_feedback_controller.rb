02d29288add3e6c67e01be94f9bd89aaa7c38b81
Todd Tyree <tatyree@gmail.com>
2020-05-26 16:53:00 +0100

    Enable recaptcha scoring, remove Auditor::Audit
    As I noted in an earlier commit, all we are doing at this point is
    collecting a score. Once we have some data, we can decide how we are
    going to deal with failing scores.
    
    I checked with Performance and we are not using the audit log at all as
    it relates to feedback so I removed it.

--- app/controllers/general_feedback_controller.rb
+++ app/controllers/general_feedback_controller.rb
@@ -4,11 +4,17 @@ class GeneralFeedbackController < ApplicationController
   end
 
   def create
-    @feedback = GeneralFeedback.create(general_feedback_params)
+    @feedback = GeneralFeedback.new(general_feedback_params)
+    return render 'new' unless @feedback.valid?
+    recaptcha_valid = verify_recaptcha(model: @feedback, action: 'feedback')
 
-    return render 'new' unless @feedback.save
-
-    Auditor::Audit.new(@feedback, 'feedback.create', current_session_id).log
+    # v3_recaptcha does not interact with the user in any way. All it does is give them a score based on a number of
+    # heurisitics. For now, all we will do is attach the score to the submitted feedback. Once we have a representative
+    # data sample, we can decide how we will act on it.
+    #
+    # https://developers.google.com/recaptcha/docs/v3
+    @feedback.recaptcha_score = recaptcha_reply['score'] if recaptcha_valid
+    @feedback.save
 
     redirect_to root_path, notice: I18n.t('messages.feedback.submitted')
   end
@@ -17,6 +23,6 @@ class GeneralFeedbackController < ApplicationController
 
   def general_feedback_params
     params.require(:general_feedback)
-          .permit(:visit_purpose, :visit_purpose_comment, :comment, :user_participation_response, :email)
+      .permit(:visit_purpose, :visit_purpose_comment, :comment, :user_participation_response, :email)
   end
 end
