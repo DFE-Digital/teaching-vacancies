80175a689fbaa5e79c71e9db7f5e25dbe8188c18
Todd Tyree <tatyree@gmail.com>
2020-05-26 13:14:12 +0100

    Fix typo

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = ENV.fetch('RECAPTCHA_SITE_KEYA')
+  config.site_key = ENV.fetch('RECAPTCHA_SITE_KEY')
   config.secret_key = ENV.fetch('RECAPTCHA_SECRET_KEY')
 end
