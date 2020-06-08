3ea62c81982b1e3e82090e0b46f72d2090f4910d
alexbowen <alexbowen@Alexs-MBP.lan>
2020-06-08 08:42:51 +0100

    relocate and restyle job alert cta

--- app/frontend/src/search/hits.js
+++ app/frontend/src/search/hits.js
@@ -6,9 +6,14 @@ export const renderContent = (renderOptions) => {
   const { results, widgetParams } = renderOptions;
 
   if (results) {
-    widgetParams.container.innerHTML = createHeadingMarkup(results.nbHits, document.getElementById('keyword').value, document.getElementById('location').value);
+    widgetParams.container.innerHTML = createHeadingMarkup(
+      results.nbHits,
+      document.getElementById('keyword').value,
+      document.getElementById('location').value
+    );
+
     if (results.query.length >= widgetParams.threshold) {
-      addJobAlertMarkup(widgetParams.alert);
+      addJobAlertMarkup(widgetParams.container);
       document.querySelector('#job-alert-link').href = getJobAlertLink(window.location.href);
     } else {
       removeJobAlertMarkup();
@@ -96,18 +101,15 @@ export const templates = {
 </dl>
 </article>
 `,
-empty: '<h4 class="govuk-heading-m">Try another search</h4><ul class="govuk-list govuk-list--bullet"><li>with different keywords</li><li>and/or a wider radius</li></ul>',};
+empty: '<h4 class="govuk-heading-m">Try another search</h4><ul class="govuk-list govuk-list--bullet"><li>with different keywords</li><li>and/or a wider radius</li></ul>'};
 
 export const customTemplates = {
   alert: `
-  <div class="govuk-inset-text" id="job-alert-cta">
+  <div id="job-alert-cta" class="govuk-heading-s govuk-!-font-weight-regular govuk-!-margin-0">
 <span class="job-seeker-alert-icon">
-<svg height="16" viewBox="0 0 20 16" width="20" xmlns="http://www.w3.org/2000/svg">
-<path d="M18 0H2C.9 0 0 .9 0 2v12a2 2 0 0 0 2 2h16c1.1 0 2-.9 2-2V2a2 2 0 0 0-2-2zm-.4 4.25l-7.07 4.42c-.32.2-.74.2-1.06 0L2.4 4.25a.85.85 0 1 1 .9-1.44L10 7l6.7-4.19a.85.85 0 1 1 .9 1.44zm0 0" fill="#0b0c0c"></path>
-<image src="/packs/media/images/job-seeker-alert-icon-4ad035e860f39781ba7b99c29ccb47ff.png"></image>
-</svg>
+<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="bell" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="20" height="16"><path fill="#1d70b8" d="M439.39 362.29c-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84C118.56 68.1 64.08 130.3 64.08 208c0 102.3-36.15 133.53-55.47 154.29-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h383.8c19.12 0 32-15.6 32.1-32 .05-7.55-2.61-15.27-8.61-21.71zM67.53 368c21.22-27.97 44.42-74.33 44.53-159.42 0-.2-.06-.38-.06-.58 0-61.86 50.14-112 112-112s112 50.14 112 112c0 .2-.06.38-.06.58.11 85.1 23.31 131.46 44.53 159.42H67.53zM224 512c35.32 0 63.97-28.65 63.97-64H160.03c0 35.35 28.65 64 63.97 64z" class=""/></svg>
 </span>
-<a class="govuk-link" id="job-alert-link" href="/">Subscribe to a job alert.</a>
-We’ll email you whenever a job matching this search is published.
+<a class="govuk-link" id="job-alert-link" href="/">Receive a job alert</a>
+whenever a job matching this search is published
 </div>
 `};
--- app/frontend/src/search/index.js
+++ app/frontend/src/search/index.js
@@ -108,7 +108,6 @@ if (document.querySelector('#vacancies-hits')) {
         }),
         heading({
             container: document.querySelector('#job-count'),
-            alert: document.querySelector('.vacancies-count'),
             threshold: SEARCH_THRESHOLD,
         }),
         hits({
