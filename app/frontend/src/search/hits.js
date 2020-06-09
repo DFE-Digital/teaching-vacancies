f5cb9283cc21ec18994c1bd8c9c42daed20dda6e
alexbowen <alexbowen@Alexs-MBP.lan>
2020-06-09 22:08:25 +0100

    make Js search heading construction use more intuitive variable names

--- app/frontend/src/search/hits.js
+++ app/frontend/src/search/hits.js
@@ -55,10 +55,10 @@ export const getJobAlertLinkParam = (key, value) => {
 export const createHeadingMarkup = (options) => {
   const { count, keyword = '', location = '' } = options;
 
-  const postfix = `${getSearchTermsPrefix(location, keyword, count)}${createHeadingHTMLForSearchTerm('', keyword, false)} ${createHeadingHTMLForSearchTerm('near', location, true)}`;
-  const hits = keyword || location ? `<span class="govuk-!-font-weight-bold">${count}</span>` : `There ${count > 1 ? 'are' : 'is'} <span class="govuk-!-font-weight-bold">${count}</span>`;
+  const searchTermsHTML = `${getSearchTermsPrefix(location, keyword, count)}${createHeadingHTMLForSearchTerm('', keyword, false)} ${createHeadingHTMLForSearchTerm('near', location, true)}`;
+  const countHTML = keyword || location ? `<span class="govuk-!-font-weight-bold">${count}</span>` : `There ${count > 1 ? 'are' : 'is'} <span class="govuk-!-font-weight-bold">${count}</span>`;
 
-  return `${hits} ${count > 1 ? 'jobs' : 'job'} ${postfix}`;
+  return `${countHTML} ${count > 1 ? 'jobs' : 'job'} ${searchTermsHTML}`;
 };
 
 export const getSearchTermsPrefix = (location, keyword, count) => {
