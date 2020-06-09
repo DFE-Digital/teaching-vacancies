3182fc1bd767d2d9f5769ea15ccb1b56b9c3940d
David Mears <david.mears@digital.education.gov.uk>
2020-06-09 15:55:31 +0100

    :seedling: Update Factory User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,21 @@
 FactoryBot.define do
   factory :user do
-    oid { Faker::Crypto.md5 }
     accepted_terms_at { Time.zone.today - 5.months }
-    email { Faker::Internet.email }
     dsi_data { {
       school_urns: proc {
         set = Set.new
         (0..4).to_a.sample.times {
-          set.add(Faker::Number.number(digits: 6))
+          number = Faker::Number.number(digits: 6)
+          set.add(number.to_s)
         }
         set
-      }.call,
-      school_group_uids: [Faker::Number.number(digits: [4, 5].sample), nil].sample
-    } }
+        }.call,
+      school_group_uids: proc {
+        number = Faker::Number.number(digits: [4, 5].sample)
+        [[number.to_s, nil].sample]
+        }.call
+      } }
+    email { Faker::Internet.email }
+    oid { Faker::Crypto.md5 }
   end
 end
