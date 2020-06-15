43edee7b2d9c5d40bad178feff3feb0998c879a4
David Mears <david.mears@digital.education.gov.uk>
2020-06-15 03:45:34 +0100

    :bug: Fix bug caused by FEATURE_SIGN_IN_ALERT=false

--- config/routes.rb
+++ config/routes.rb
@@ -40,14 +40,20 @@ Rails.application.routes.draw do
 
   # Authentication fallback with emailed magic link
   if AuthenticationFallback.enabled?
-    get 'auth/email/sessions/new', to: 'hiring_staff/sign_in/email/sessions#new', as: 'new_auth_email'
-    post 'auth/email/sessions/check-your-email', to: 'hiring_staff/sign_in/email/sessions#check_your_email', as: 'auth_email_check_your_email'
-    get 'auth/email/sessions/choose-organisation', to: 'hiring_staff/sign_in/email/sessions#choose_organisation', as: 'auth_email_choose_organisation'
-    get 'auth/email/sessions/sign-in', to: 'hiring_staff/sign_in/email/sessions#create', as: 'auth_email_sign_in'
-    get 'auth/email/sessions/sign-out', to: 'hiring_staff/sign_in/email/sessions#destroy', as: 'auth_email_sign_out'
-    get 'auth/email/sessions/change-organisation', to: 'hiring_staff/sign_in/email/sessions#change_organisation', as: 'auth_email_change_organisation'
+    get 'auth/email/sessions/new', to: 'hiring_staff/sign_in/email/sessions#new',
+      as: 'new_auth_email'
+    post 'auth/email/sessions/check-your-email', to: 'hiring_staff/sign_in/email/sessions#check_your_email',
+      as: 'auth_email_check_your_email'
+    get 'auth/email/sessions/choose-organisation', to: 'hiring_staff/sign_in/email/sessions#choose_organisation',
+      as: 'auth_email_choose_organisation'
+    get 'auth/email/sessions/sign-in', to: 'hiring_staff/sign_in/email/sessions#create',
+      as: 'auth_email_sign_in'
+    get 'auth/email/sessions/sign-out', to: 'hiring_staff/sign_in/email/sessions#destroy',
+      as: 'auth_email_sign_out'
+    get 'auth/email/sessions/change-organisation', to: 'hiring_staff/sign_in/email/sessions#change_organisation',
+      as: 'auth_email_change_organisation'
   else
-  # DfE Sign In
+    # DfE Sign In
     resource :sessions,
             only: %i[create new],
             as: :dfe,
