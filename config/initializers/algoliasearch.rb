c4c47c18eb4900bc7be5069d7e6d2a0738cb38c0
Todd Tyree <tatyree@gmail.com>
2020-06-18 10:48:23 +0100

    Disallow access to prod index from non-prod envs
    All things being equal, allowing direct access to the production Algolia
    index from non-production environments is high-risk at best. This change
    ensure that the app will error if this is attempted.
    
    It will require a change in working practices, but this should not be
    too odious after a bit of practice.
    
    TODO: Add the README section on how to reuse or create free tier indexes
    for development and testing.

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,3 +1,19 @@
+ALGOLIA_PRODUCTION_APP_ID = 'QM2YE0HRBW'
+
+class AlgoliaEnvironmentError < StandardError
+  def initialize
+    msg=<<-HERE.chomp
+Do not use the production index #{ALGOLIA_PRODUCTION_APP_ID} in a non-production environment.
+See main README.md for instructions how to create new dev indexes.
+    HERE
+    super(msg)
+  end
+end
+
+if !Rails.env.production? && ENV.fetch('ALGOLIA_APP_ID') == ALGOLIA_PRODUCTION_APP_ID
+  raise AlgoliaEnvironmentError.new
+end
+
 AlgoliaSearch.configuration = {
   application_id: ENV.fetch('ALGOLIA_APP_ID'),
   api_key: ENV.fetch('ALGOLIA_WRITE_API_KEY'),
