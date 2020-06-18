d27a84ee4a6d603d1f82f70e5d5231a938713d0e
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-06-18 13:53:39 +0100

    Refactor general feedback form to use GOVUKDesignSystemFormBuilder

--- spec/features/general_feedback_spec.rb
+++ spec/features/general_feedback_spec.rb
@@ -10,11 +10,11 @@ RSpec.feature 'Giving general feedback for the service' do
   end
 
   context 'when submitting feedback on the service' do
-    let(:choose_yes_to_participation) { choose('general_feedback_user_participation_response_interested') }
-    let(:choose_no_to_participation) { choose('general_feedback_user_participation_response_not_interested') }
+    let(:choose_yes_to_participation) { choose('general-feedback-user-participation-response-interested-field') }
+    let(:choose_no_to_participation) { choose('general-feedback-user-participation-response-not-interested-field') }
 
     scenario 'must have a visit purpose' do
-      fill_in 'general_feedback_comment', with: 'Keep going!'
+      fill_in 'general_feedback[comment]', with: 'Keep going!'
       choose_no_to_participation
 
       click_on 'Submit feedback'
@@ -24,7 +24,7 @@ RSpec.feature 'Giving general feedback for the service' do
 
     scenario 'must have a participation response' do
       choose 'Find a job in teaching'
-      fill_in 'general_feedback_comment', with: 'Keep going!'
+      fill_in 'general_feedback[comment]', with: 'Keep going!'
 
       click_on 'Submit feedback'
 
@@ -33,7 +33,7 @@ RSpec.feature 'Giving general feedback for the service' do
 
     scenario 'successfully submitting feedback for the service' do
       choose 'Find a job in teaching'
-      fill_in 'general_feedback_comment', with: 'Keep going!'
+      fill_in 'general_feedback[comment]', with: 'Keep going!'
       choose_no_to_participation
 
       click_on 'Submit feedback'
@@ -43,7 +43,7 @@ RSpec.feature 'Giving general feedback for the service' do
 
     scenario 'must have an email when participation response is Yes' do
       choose 'Find a job in teaching'
-      fill_in 'general_feedback_comment', with: 'Keep going!'
+      fill_in 'general_feedback[comment]', with: 'Keep going!'
       choose_yes_to_participation
 
       click_on 'Submit feedback'
@@ -53,7 +53,7 @@ RSpec.feature 'Giving general feedback for the service' do
 
     scenario 'successfully submitting feedback and interest in user research' do
       choose 'Find a job in teaching'
-      fill_in 'general_feedback_comment', with: 'Keep going!'
+      fill_in 'general_feedback[comment]', with: 'Keep going!'
 
       choose_yes_to_participation
       fill_in 'email', with: 'test@test.com'
