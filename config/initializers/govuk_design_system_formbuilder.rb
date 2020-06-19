e5ed2abfcc69ac32dafeaa459dc4d7444367eb99
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-06-19 09:09:46 +0100

    Add config initializer file

---
+++ config/initializers/govuk_design_system_formbuilder.rb
@@ -0,0 +1,5 @@
+GOVUKDesignSystemFormBuilder.configure do |conf|
+  conf.default_error_summary_title  = 'Please correct the following errors'
+  conf.default_legend_tag           = 'h3'
+  conf.default_legend_size          = 's'
+end
