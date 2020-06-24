05d8cd94eacea0abf4dcb11604aeeb35ecc70899
David Mears <david.mears@digital.education.gov.uk>
2020-06-24 22:46:01 +0100

    :white_check_mark: Update tests touching DfE Sign In to mock feature flag

--- spec/controllers/hiring_staff/identifications_controller_spec.rb
+++ spec/controllers/hiring_staff/identifications_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe HiringStaff::IdentificationsController, type: :controller do
   describe '#create' do
+    before { allow(AuthenticationFallback).to receive(:enabled?).and_return(false) }
     it 'redirects to DfE Sign in' do
       post :create
       expect(response).to redirect_to(new_dfe_path)
--- spec/controllers/hiring_staff/sign_in/dfe/sessions_controller_spec.rb
+++ spec/controllers/hiring_staff/sign_in/dfe/sessions_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe HiringStaff::SignIn::Dfe::SessionsController, type: :controller do
   describe '#new' do
+    before { allow(AuthenticationFallback).to receive(:enabled?).and_return(false) }
     it 'redirects to Dfe' do
       get :new
       expect(response).to redirect_to('/auth/dfe') # From here we trust Omniauth
