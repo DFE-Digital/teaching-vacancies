5e698e86e5ff2edd174bf0a177d1226b22e7fe54
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-06-24 13:22:58 +0100

    Update kaminari templates for new pagination design

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Kaminari.configure do |config|
-  config.default_per_page = 25
+  config.default_per_page = 10
   config.max_per_page = nil
-  config.window = 4
+  config.window = 2
   config.outer_window = 0
   config.left = 0
   config.right = 0
