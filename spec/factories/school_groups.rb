052c904367a430dc55592def83c8f066f3b178b1
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-07-03 11:18:31 +0100

    Add basic SchoolGroup factory

---
+++ spec/factories/school_groups.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :school_group do
+    uid { Faker::Number.number(digits: 5).to_s }
+    gias_data { {
+      "Group UID": uid,
+      "Group Name": 'Trust name'
+    } }
+  end
+end
