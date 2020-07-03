58592c2eb4c191036c187309404ed8f4e09df7a3
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-07-03 11:34:09 +0100

    Add a name method to the SchoolGroup model

--- app/models/school_group.rb
+++ app/models/school_group.rb
@@ -3,4 +3,9 @@ class SchoolGroup < ApplicationRecord
   has_many :schools, through: :school_group_memberships
 
   has_many :vacancies
+
+  # TODO: This should ideally be a stored as column in the database table
+  def name
+    gias_data['Group Name']
+  end
 end
