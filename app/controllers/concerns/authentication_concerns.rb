02786e00fca71788f8b522cacc04f725fc37f208
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-07-06 12:24:22 +0100

    Add current_organisation helper method

--- app/controllers/concerns/authentication_concerns.rb
+++ app/controllers/concerns/authentication_concerns.rb
@@ -3,14 +3,28 @@ module AuthenticationConcerns
 
   included do
     helper_method :authenticated?
+    helper_method :current_organisation
     helper_method :current_school
+    helper_method :current_school_group
   end
 
   def authenticated?
     session[:session_id].present?
   end
 
+  def current_organisation
+    if SchoolGroupJobsFeature.enabled?
+      current_school_group || current_school
+    else
+      current_school
+    end
+  end
+
   def current_school
-    School.find_by!(urn: session[:urn])
+    @current_school ||= School.find_by!(urn: session[:urn])
+  end
+
+  def current_school_group
+    @current_school_group ||= SchoolGroup.find_by!(uid: session[:uid])
   end
 end
