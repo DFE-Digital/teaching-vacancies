836627c131bd6dd9bd75bb1cb49dedc17fee44c7
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-07-21 18:16:53 +0100

    Refactor JobSpecificationController to reuse methods

--- app/controllers/hiring_staff/vacancies/job_specification_controller.rb
+++ app/controllers/hiring_staff/vacancies/job_specification_controller.rb
@@ -4,61 +4,53 @@ class HiringStaff::Vacancies::JobSpecificationController < HiringStaff::Vacancie
   include GetSubjectName
 
   before_action :set_up_url
-  before_action :set_up_job_specification_form, only: %i[create update]
-
+  before_action only: %i[create update] do
+    set_up_form(JobSpecificationForm)
+  end
   before_action only: %i[update] do
-    save_vacancy_as_draft_if_save_and_return_later(job_specification_form_params, @vacancy)
+    save_vacancy_as_draft_if_save_and_return_later(form_params, @vacancy)
   end
 
   def show
-    if @vacancy.present?
-      @job_specification_form = JobSpecificationForm.new(@vacancy.attributes)
-    elsif session[:vacancy_attributes].present?
-      @job_specification_form = JobSpecificationForm.new(session[:vacancy_attributes])
+    attributes = @vacancy.present? ? @vacancy.attributes : session[:vacancy_attributes]
+    if attributes.present?
+      @form = JobSpecificationForm.new(attributes)
     else
-      @job_specification_form = JobSpecificationForm.new(school_id: current_school.id)
+      @form = JobSpecificationForm.new(school_id: current_school.id)
     end
   end
 
   def create
-    store_vacancy_attributes(@job_specification_form.vacancy.attributes)
+    store_vacancy_attributes(@form.vacancy.attributes)
 
     if params[:commit] == I18n.t('buttons.save_and_return_later')
-      return save_vacancy_as_draft
-    elsif @job_specification_form.complete_and_valid?
-      session_vacancy_id ? update_vacancy(job_specification_form_params) : save_vacancy_without_validation
-      store_vacancy_attributes(@job_specification_form.vacancy.attributes)
-      return redirect_to_next_step_if_continue(@vacancy&.id.present? ? @vacancy.id : session_vacancy_id)
+      save_vacancy_as_draft
+    elsif @form.valid?
+      session_vacancy_id ? update_vacancy(form_params) : save_vacancy_without_validation
+      redirect_to_next_step_if_continue(@vacancy&.id.present? ? @vacancy.id : session_vacancy_id)
+    else
+      render :show
     end
-
-    render :show
   end
 
   def update
-    if @job_specification_form.valid?
+    if @form.valid?
       remove_subject_fields(@vacancy) unless @vacancy.subjects.nil?
-      update_vacancy(job_specification_form_params, @vacancy)
+      update_vacancy(form_params, @vacancy)
       update_google_index(@vacancy) if @vacancy.listed?
-      return redirect_to_next_step_if_continue(@vacancy.id, @vacancy.job_title)
+      redirect_to_next_step_if_continue(@vacancy.id, @vacancy.job_title)
+    else
+      render :show
     end
-
-    render :show
   end
 
   private
 
-  def set_up_url
-    @job_specification_url_method = @vacancy&.id.present? ? 'patch' : 'post'
-    @job_specification_url = @vacancy&.id.present? ?
-      organisation_job_job_specification_path(@vacancy.id) :
-      job_specification_organisation_job_path(school_id: current_school.id)
+  def form_submission_path(vacancy_id)
+    vacancy_id.present? ? organisation_job_job_specification_path(vacancy_id) : job_specification_organisation_job_path
   end
 
-  def set_up_job_specification_form
-    @job_specification_form = JobSpecificationForm.new(job_specification_form_params)
-  end
-
-  def job_specification_form_params
+  def form_params
     strip_empty_checkboxes(:job_specification_form, [:working_patterns, :job_roles, :subjects])
     params.require(:job_specification_form)
           .permit(:state, :job_title,
@@ -73,25 +65,32 @@ class HiringStaff::Vacancies::JobSpecificationController < HiringStaff::Vacancie
   end
 
   def save_vacancy_as_draft
-    if @job_specification_form.vacancy.job_title.present?
+    if @form.vacancy.job_title.present?
       save_vacancy_without_validation
-      redirect_to_draft(
-        @job_specification_form.vacancy.id,
-        @job_specification_form.vacancy.job_title
-      )
+      redirect_to_draft(@form.vacancy.id, @form.vacancy.job_title)
     else
       redirect_to jobs_with_type_organisation_path('draft')
     end
   end
 
   def save_vacancy_without_validation
-    @job_specification_form.vacancy.school_id = current_school.id
-    @job_specification_form.vacancy.send :set_slug
-    @job_specification_form.vacancy.status = :draft
-    Auditor::Audit.new(@job_specification_form.vacancy, 'vacancy.create', current_session_id).log do
-      @job_specification_form.vacancy.save(validate: false)
+    set_school_or_school_group_id
+    @form.vacancy.send :set_slug
+    @form.vacancy.status = :draft
+    @form.vacancy.assign_attributes(session[:vacancy_attributes])
+    Auditor::Audit.new(@form.vacancy, 'vacancy.create', current_session_id).log do
+      @form.vacancy.save(validate: false)
     end
-    @job_specification_form.vacancy
+    store_vacancy_attributes(@form.vacancy.attributes)
+    @form.vacancy
+  end
+
+  def set_school_or_school_group_id
+    if current_organisation.is_a?(School)
+      @form.vacancy.school_id = current_organisation.id
+    elsif current_organisation.is_a?(SchoolGroup)
+      @form.vacancy.school_group_id = current_organisation.id
+    end
   end
 
   def next_step
