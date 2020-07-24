02ea9479dce283ffff9152035d68bf94eec62ad0
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2020-07-24 14:06:37 +0100

    terraform files to deploy to GOV.UK PaaS
    terraform resources files with AWS S3 backend.

---
+++ terraform/paas/variables.tf
@@ -0,0 +1,27 @@
+variable cf_user {}
+
+variable cf_password {}
+
+variable cf_space_name {}
+
+variable postgres_service_name {}
+
+variable redis_service_name {}
+
+variable papertrail_service_name {}
+
+variable web_app_name {}
+
+variable worker_app_name {}
+
+variable app_stopped { default = false }
+
+variable app_docker_image {}
+
+variable no_of_instances { default = 2 }
+
+variable app_memory { default = 512 }
+
+variable app_start_timeout { default = 300 }
+
+variable service_recursive_delete { default = false }
