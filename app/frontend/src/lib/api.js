ad8ca734217fe78c48d8b674e56d4742008635f7
David Mears <david.mears@digital.education.gov.uk>
2020-07-28 11:41:00 +0100

    Refactor to use axios and api.js

--- app/frontend/src/lib/api.js
+++ app/frontend/src/lib/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
+import Rollbar from './logging';
 
 export const getGeolocatedCoordinates = (query) => axios.get(`/api/v1/coordinates/${query}?format=json`).then((response) => response.data);
 
 export const getPostcodeFromCoordinates = (latitude, longitude) => axios.get('https://api.postcodes.io/postcodes', {
   params: { latitude, longitude },
 }).then((response) => response.data);
+
+export const getPlaceOptionsFromSearchQuery = (query) => axios.get(`/api/v1/location_suggestion/${query}.json`)
+  .then((response) => response.data)
+  .then((data) => data.suggestions)
+  .catch((error) => {
+    Rollbar.log(`${error} Search query: ${query}`);
+  });
