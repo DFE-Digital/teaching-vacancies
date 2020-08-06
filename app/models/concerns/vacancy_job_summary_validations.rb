a66d70ee7513bd86957f7026fb0b7c66baccfcfb
josephhull676 <josephhull676@yahoo.co.uk>
2020-08-06 17:09:24 +0100

    Change about school error messages to change depending on job location

--- app/models/concerns/vacancy_job_summary_validations.rb
+++ app/models/concerns/vacancy_job_summary_validations.rb
@@ -3,6 +3,18 @@ module VacancyJobSummaryValidations
 
   included do
     validates :job_summary, presence: true
-    validates :about_school, presence: true
+    validate :about_school_must_not_be_blank
+  end
+
+  def about_school_must_not_be_blank
+    if defined?(vacancy) && vacancy.job_location == 'central_office'
+      organisation = 'trust'
+    else
+      organisation = 'school'
+    end
+    errors.add(:about_school,
+      I18n.t('activerecord.errors.models.vacancy.attributes.about_school.blank',
+      organisation: organisation)
+    ) if about_school.blank?
   end
 end
