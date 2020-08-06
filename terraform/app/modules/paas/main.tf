c48e2aa214b0886a70b711b81daedd14d83f23ee
Nick Romney <nick.romney@digital.education.gov.uk>
2020-08-06 16:00:41 +0100

    Move Redis service to TF module with var for plan size

--- terraform/app/modules/paas/main.tf
+++ terraform/app/modules/paas/main.tf
@@ -4,3 +4,9 @@ resource cloudfoundry_service_instance postgres_instance {
   service_plan = data.cloudfoundry_service.postgres.service_plans["${var.postgres_service_plan}"]
   json_params  = "{\"enable_extensions\": [\"pgcrypto\", \"fuzzystrmatch\", \"plpgsql\"]}"
 }
+
+resource cloudfoundry_service_instance redis_instance {
+  name         = local.redis_service_name
+  space        = data.cloudfoundry_space.space.id
+  service_plan = data.cloudfoundry_service.redis.service_plans["${var.redis_service_plan}"]
+}
--- terraform/app/modules/paas/variables.tf
+++ terraform/app/modules/paas/variables.tf
@@ -7,9 +7,13 @@ variable postgres_service_plan {
 variable project_name {
 }
 
+variable redis_service_plan {
+}
+
 variable space_name {
 }
 
 locals {
   postgres_service_name = "${var.project_name}-postgres-${var.environment}"
+  redis_service_name    = "${var.project_name}-redis-${var.environment}"
 }
