86a2e4b51f8724e3d29642c9534eb20de68f97c2
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2020-08-12 14:40:21 +0100

    Create organisations from local database

---
+++ app/models/organisation.rb
@@ -0,0 +1,12 @@
+class Organisation < ApplicationRecord
+  enum phase: {
+    not_applicable: 0,
+    nursery: 1,
+    primary: 2,
+    middle_deemed_primary: 3,
+    secondary: 4,
+    middle_deemed_secondary: 5,
+    sixteen_plus: 6,
+    all_through: 7,
+  }
+end
