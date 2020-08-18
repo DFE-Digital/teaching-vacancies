7f1abb6ac8786ffae786f5ca26bb0ca8bb185e79
josephhull676 <josephhull676@yahoo.co.uk>
2020-08-18 16:10:46 +0100

    Create MULTI_SCHOOL_JOB feature flag

--- config/initializers/flags.rb
+++ config/initializers/flags.rb
@@ -5,9 +5,11 @@ FEATURE_READ_ONLY = ENV['FEATURE_READ_ONLY']
 FEATURE_EMAIL_ALERTS = ENV['FEATURE_EMAIL_ALERTS']
 FEATURE_IMPORT_VACANCIES = ENV['FEATURE_IMPORT_VACANCIES']
 FEATURE_SCHOOL_GROUP_JOBS = ENV['FEATURE_SCHOOL_GROUP_JOBS']
+FEATURE_MULTI_SCHOOL_JOBS = ENV['FEATURE_MULTI_SCHOOL_JOBS']
 
 AuthenticationFallback = Flag.new('authentication_fallback', is_feature: false)
 ReadOnlyFeature = Flag.new('read_only')
 EmailAlertsFeature = Flag.new('email_alerts')
 ImportVacanciesFeature = Flag.new('import_vacancies')
 SchoolGroupJobsFeature = Flag.new('school_group_jobs')
+MultiSchoolJobsFeature = Flag.new('multi_school_jobs')
