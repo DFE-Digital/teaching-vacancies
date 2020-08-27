7f840ce8da35744802ec0443d1953a3e4a11a662
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2020-08-27 12:53:00 +0100

    Update from Poltergeist to Selenium
    This also changes the driver from phantomjs to headless chrome

--- spec/browser_test_helper.rb
+++ spec/browser_test_helper.rb
@@ -1,15 +1,3 @@
 require 'capybara/rspec'
-require 'capybara/poltergeist'
 
-# Setting js:errors to false because unrelated tests failed due to vanilla javascript code
-
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, { phantomjs_options: ['--load-images=false'], js_errors: false })
-end
-Capybara.javascript_driver = :poltergeist
-
-RSpec.configure do |config|
-  config.before(:each, js: true, type: ->(v) { v != :smoke_test }) do
-    page.driver.clear_memory_cache
-  end
-end
+Capybara.javascript_driver = :selenium_chrome_headless
