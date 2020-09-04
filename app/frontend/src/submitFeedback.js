ba0ed2f84d94f6bddadb34f991a3eb644a442e73
alexbowen <alexbowen123@googlemail.com>
2020-09-04 14:50:25 +0100

    fix minor sonarcloud warnings

--- app/frontend/src/submitFeedback.js
+++ app/frontend/src/submitFeedback.js
@@ -11,7 +11,7 @@ $(document).ready(() => {
         if (!$(this).hasClass('govuk-input--error')) {
           $(this).addClass('govuk-input--error');
           $(this).wrap('<div class=\'govuk-form-group--error\'></div>');
-          text = form.data('optionNotSelectedMessage');
+          const text = form.data('optionNotSelectedMessage');
           $(this).parent('div').prepend(`${'<span class="govuk-error-message">' + '<span class="govuk-visually-hidden">' + 'Error:' + '</span>'}${text}</span>`);
         }
       } else {
