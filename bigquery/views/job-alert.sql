a47299e58c6e5d34cee5e07bf24e9d7b3f23bc0b
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2020-09-10 09:36:14 +0100

    Create job-alert.sql
    - Convert scheduled query that extracts job alerts from JSONs in feb20_subscription to a view
    - Add recaptcha_score and human boolean to job alert view

---
+++ bigquery/views/job-alert.sql
@@ -0,0 +1,54 @@
+SELECT
+  CAST(created_at AS date) AS created_date,
+  CAST(updated_at AS date) AS updated_date,
+  expires_on AS expires_date,
+  recaptcha_score,
+  IF(recaptcha_score IS NULL,NULL,recaptcha_score > 0.5) AS human,
+  ARRAY_TO_STRING(ARRAY(   #extract all search criteria from the JSON, except radius (because this is meaningless without location)
+    SELECT
+      *
+    FROM
+      UNNEST(REGEXP_EXTRACT_ALL(search_criteria, r'\"([a-z_]+)\":')) AS criteria #we have to use REGEXP_EXTRACT_ALL here rather than JSON_EXTRACT... because the latter only supports value rather than key extraction
+    WHERE
+      criteria NOT IN ("radius")
+    ORDER BY
+      criteria ASC )," and ") AS search_criteria,
+IF
+  (REGEXP_CONTAINS(JSON_EXTRACT_SCALAR(search_criteria,
+        '$.location'), r"[0-9]+"),
+    #anonymise postcodes
+    "postcode",
+    TRIM(LOWER(JSON_EXTRACT_SCALAR(search_criteria,
+        '$.location'))," ")) AS location,
+  JSON_EXTRACT_SCALAR(search_criteria,
+    '$.radius') AS radius,
+  ARRAY_TO_STRING( (
+    SELECT
+      ARRAY(
+      SELECT
+        JSON_EXTRACT_SCALAR(working_patterns_string,
+          '$') AS pattern
+      FROM
+        UNNEST(JSON_EXTRACT_ARRAY(search_criteria,
+            '$.working_patterns')) AS working_patterns_string
+      ORDER BY
+        pattern ASC))," or ") AS working_patterns,
+  JSON_EXTRACT_SCALAR(search_criteria,
+    '$.newly_qualified_teacher') AS newly_qualified_teacher,
+  ARRAY_TO_STRING( (
+    SELECT
+      ARRAY(
+      SELECT
+        JSON_EXTRACT_SCALAR(education_phases_string,
+          '$') AS phase
+      FROM
+        UNNEST(JSON_EXTRACT_ARRAY(search_criteria,
+            '$.phases')) AS education_phases_string
+      ORDER BY
+        phase ASC ))," or ") AS education_phases,
+  TRIM(LOWER(JSON_EXTRACT_SCALAR(search_criteria,
+    '$.subject'))," ") AS subject,
+  TRIM(LOWER(JSON_EXTRACT_SCALAR(search_criteria,
+    '$.job_title'))," ") AS job_title
+FROM
+  `teacher-vacancy-service.production_dataset.feb20_subscription`
