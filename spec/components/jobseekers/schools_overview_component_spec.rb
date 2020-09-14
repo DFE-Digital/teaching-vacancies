d17a72f4c661bca7bf9135df5490797e05427ebf
David Mears <david.mears@digital.education.gov.uk>
2020-09-14 10:30:29 +0100

    Linting

--- spec/components/jobseekers/schools_overview_component_spec.rb
+++ spec/components/jobseekers/schools_overview_component_spec.rb
@@ -4,14 +4,20 @@ require 'pry'
 RSpec.describe Jobseekers::SchoolsOverviewComponent, type: :component do
   let(:school_group) { create(:school_group) }
   let(:geolocation_trait) { nil }
-  let(:school_1) { create(:school, geolocation_trait, name: 'Oxford Uni',
-                          gias_data: { 'URN' => Faker::Number.number(digits: 6) },
-                          website: 'https://this-is-a-test-url.tvs') }
-  let(:school_2) { create(:school, geolocation_trait, name: 'Cambridge Uni',
-                          gias_data: { 'URN' => Faker::Number.number(digits: 6) }) }
-  let(:school_3) { create(:school, geolocation_trait, name: 'London LSE',
-                          gias_data: { 'URN' => Faker::Number.number(digits: 6) }) }
-  
+  let(:school_1) do
+    create(:school, geolocation_trait, name: 'Oxford Uni',
+                                       gias_data: { 'URN' => Faker::Number.number(digits: 6) },
+                                       website: 'https://this-is-a-test-url.tvs')
+  end
+  let(:school_2) do
+    create(:school, geolocation_trait, name: 'Cambridge Uni',
+                                       gias_data: { 'URN' => Faker::Number.number(digits: 6) })
+  end
+  let(:school_3) do
+    create(:school, geolocation_trait, name: 'London LSE',
+                                       gias_data: { 'URN' => Faker::Number.number(digits: 6) })
+  end
+
   let(:vacancy) do
     create(:vacancy, :at_multiple_schools, organisation_vacancies_attributes: [
       { organisation: school_1 }, { organisation: school_2 }, { organisation: school_3 }
