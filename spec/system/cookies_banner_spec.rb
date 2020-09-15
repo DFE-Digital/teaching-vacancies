d49197cc113d9f136276de8a506eca01b699e0dc
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-09-15 10:25:17 +0100

    Render cookies banner if feature is enabled

---
+++ spec/system/cookies_banner_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Cookies banner' do
+  before do
+    allow(CookiesBannerFeature).to receive(:enabled?).and_return(cookies_banner_enabled)
+    visit root_path
+  end
+
+  context 'when CookiesBannerFeature is enabled' do
+    let(:cookies_banner_enabled) { true }
+
+    scenario 'displays cookies banner' do
+      within '.cookies-banner' do
+        expect(page).to have_content(I18n.t('cookies_banner.heading'))
+      end
+    end
+  end
+
+  context 'when CookiesBannerFeature is disabled' do
+    let(:cookies_banner_enabled) { false }
+
+    scenario 'does not display cookies banner' do
+      expect(page).to_not have_content(I18n.t('cookies_banner.heading'))
+    end
+  end
+end
