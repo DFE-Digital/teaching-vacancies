e3a513c78411738deb7c4a2372a74f1782c73d92
josephhull676 <josephhull676@yahoo.co.uk>
2020-09-15 12:29:25 +0100

    Add notification for multi-school jobs

--- spec/components/notification_component_spec.rb
+++ spec/components/notification_component_spec.rb
@@ -6,108 +6,116 @@ RSpec.describe NotificationComponent, type: :component do
   let(:dismiss) { true }
   let(:background) { false }
   let(:alert) { false }
-  let!(:inline_component) { render_inline(NotificationComponent.new(content: content, style: style, dismiss: dismiss, background: background, alert: alert)) }
-
-  context 'when content is a string' do
-    it 'renders content in the body' do
-      expect(inline_component.css('.govuk-notification__body').to_html).to include(content)
-    end
+  let!(:inline_component) do
+    render_inline(NotificationComponent.new(
+                    content: content,
+                    style: style,
+                    dismiss: dismiss,
+                    background: background,
+                    alert: alert,
+                  ))
   end
 
-  context 'when content is a hash' do
-    let(:content) { { title: 'Title', body: 'This is the body' } }
-
-    it 'renders the title' do
-      expect(inline_component.css('.govuk-notification__title').to_html).to include(content[:title])
-    end
-
-    it 'renders the body' do
-      expect(inline_component.css('.govuk-notification__body').to_html).to include(content[:body])
-    end
-  end
-
-  context 'when dismiss is true' do
-    it 'renders the dismiss link' do
-      expect(inline_component.css('.dismiss-link').to_html).to include(I18n.t('buttons.dismiss'))
-    end
-  end
-
-  context 'when dismiss is false' do
-    let(:dismiss) { false }
-
-    it 'does not render the dismiss link' do
-      expect(rendered_component).to_not include(I18n.t('buttons.dismiss'))
-    end
-  end
-
-  context 'when style is notice' do
-    it 'applies correct style' do
-      expect(inline_component.css('.govuk-notification--notice')).to_not be_blank
-    end
-  end
-
-  context 'when style is success' do
-    let(:style) { 'success' }
-
-    it 'applies correct style' do
-      expect(inline_component.css('.govuk-notification--success')).to_not be_blank
-    end
-  end
-
-  context 'when style is danger' do
-    let(:style) { 'danger' }
-
-    it 'does not render the dismiss link' do
-      expect(rendered_component).to_not include(I18n.t('buttons.dismiss'))
-    end
-
-    it 'applies correct style' do
-      expect(inline_component.css('.govuk-notification--danger')).to_not be_blank
-    end
-  end
-
-  context 'when background is true' do
-    let(:background) { true }
-
-    it 'applies the background style' do
-      expect(inline_component.css('.govuk-notification__background')).to_not be_blank
-    end
-  end
-
-  context 'when background is false' do
-    it 'does not apply the background style' do
-      expect(inline_component.css('.govuk-notification__background')).to be_blank
-    end
-  end
-
-  context 'when style is success' do
-    let(:style) { 'success' }
-
-    it 'does not apply the alert style' do
-      expect(inline_component.css('.alert')).to be_blank
-    end
-  end
-
-  context 'when style is danger' do
-    let(:style) { 'danger' }
-
-    it 'does not apply the alert style' do
-      expect(inline_component.css('.alert')).to be_blank
-    end
-  end
-
-  context 'when style is notice' do
-    context 'when alert is true' do
-      let(:alert) { true }
-
-      it 'applies the alert style' do
-        expect(inline_component.css('.alert')).to_not be_blank
+  describe '#content' do
+    context 'when content is a string' do
+      it 'renders content in the body' do
+        expect(inline_component.css('.govuk-notification__body').to_html).to include('This is content')
       end
     end
 
-    context 'when alert is false' do
-      it 'does not apply the alert style' do
+    context 'when content is a hash' do
+      let(:content) { { title: 'Title', body: 'This is the body' } }
+
+      it 'renders the title' do
+        expect(inline_component.css('.govuk-notification__title').to_html).to include('Title')
+      end
+
+      it 'renders the body' do
+        expect(inline_component.css('.govuk-notification__body').to_html).to include('This is the body')
+      end
+    end
+  end
+
+  describe '#dismiss' do
+    context 'when dismiss is true' do
+      it 'renders the dismiss link' do
+        expect(inline_component.css('.dismiss-link').to_html).to include(I18n.t('buttons.dismiss'))
+      end
+    end
+
+    context 'when dismiss is false' do
+      let(:dismiss) { false }
+
+      it 'does not render the dismiss link' do
+        expect(rendered_component).to_not include(I18n.t('buttons.dismiss'))
+      end
+    end
+  end
+
+  describe '#style' do
+    context 'when style is notice' do
+      it 'applies correct class' do
+        expect(inline_component.css('.govuk-notification--notice')).to_not be_blank
+      end
+
+      context 'when alert is true' do
+        let(:alert) { true }
+
+        it 'applies the alert class' do
+          expect(inline_component.css('.alert')).to_not be_blank
+        end
+      end
+
+      context 'when alert is false' do
+        it 'does not apply the alert class' do
+          expect(inline_component.css('.alert')).to be_blank
+        end
+      end
+    end
+
+    context 'when style is success' do
+      let(:style) { 'success' }
+      let(:alert) { true }
+
+      it 'applies correct class' do
+        expect(inline_component.css('.govuk-notification--success')).to_not be_blank
+      end
+
+      it 'does not apply the alert class' do
         expect(inline_component.css('.alert')).to be_blank
+      end
+    end
+
+    context 'when style is danger' do
+      let(:style) { 'danger' }
+      let(:alert) { true }
+
+      it 'does not render the dismiss link' do
+        expect(rendered_component).to_not include(I18n.t('buttons.dismiss'))
+      end
+
+      it 'applies correct class' do
+        expect(inline_component.css('.govuk-notification--danger')).to_not be_blank
+      end
+
+      it 'does not apply the alert class' do
+        expect(inline_component.css('.alert')).to be_blank
+      end
+    end
+  end
+
+  describe '#background' do
+    context 'when background is true' do
+      let(:background) { true }
+
+      it 'applies the background class' do
+        expect(inline_component.css('.govuk-notification__background')).to_not be_blank
+      end
+    end
+
+    context 'when background is false' do
+      it 'does not apply the background class' do
+        expect(inline_component.css('.govuk-notification__background')).to be_blank
       end
     end
   end
