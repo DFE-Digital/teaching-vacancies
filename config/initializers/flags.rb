14ee6a1dd1cc4a5769a316cb550d76e78194eb5b
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-09-15 10:24:41 +0100

    Add cookies banner feature flag

--- config/initializers/flags.rb
+++ config/initializers/flags.rb
@@ -2,16 +2,20 @@ require 'flag'
 
 AUTHENTICATION_FALLBACK = ENV['AUTHENTICATION_FALLBACK']
 DISABLE_EXPENSIVE_JOBS = ENV['DISABLE_EXPENSIVE_JOBS']
-FEATURE_READ_ONLY = ENV['FEATURE_READ_ONLY']
+
+FEATURE_COOKIES_BANNER = ENV['FEATURE_COOKIES_BANNER']
 FEATURE_EMAIL_ALERTS = ENV['FEATURE_EMAIL_ALERTS']
 FEATURE_IMPORT_VACANCIES = ENV['FEATURE_IMPORT_VACANCIES']
-FEATURE_SCHOOL_GROUP_JOBS = ENV['FEATURE_SCHOOL_GROUP_JOBS']
 FEATURE_MULTI_SCHOOL_JOBS = ENV['FEATURE_MULTI_SCHOOL_JOBS']
+FEATURE_READ_ONLY = ENV['FEATURE_READ_ONLY']
+FEATURE_SCHOOL_GROUP_JOBS = ENV['FEATURE_SCHOOL_GROUP_JOBS']
 
 AuthenticationFallback = Flag.new('authentication_fallback', is_feature: false)
 DisableExpensiveJobs = Flag.new('disable_expensive_jobs', is_feature: false)
-ReadOnlyFeature = Flag.new('read_only')
+
+CookiesBannerFeature = Flag.new('cookies_banner')
 EmailAlertsFeature = Flag.new('email_alerts')
 ImportVacanciesFeature = Flag.new('import_vacancies')
-SchoolGroupJobsFeature = Flag.new('school_group_jobs')
 MultiSchoolJobsFeature = Flag.new('multi_school_jobs')
+ReadOnlyFeature = Flag.new('read_only')
+SchoolGroupJobsFeature = Flag.new('school_group_jobs')
