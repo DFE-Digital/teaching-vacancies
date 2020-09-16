5c108ea5e99d380a557e57ded45b707087dacba9
Nick Romney <nick.romney@digital.education.gov.uk>
2020-09-16 09:44:54 +0100

    Try without data resource to S3

--- terraform/app/modules/cloudfront/data.tf
+++ terraform/app/modules/cloudfront/data.tf
@@ -1,5 +1,5 @@
 data aws_caller_identity current {}
 
-data aws_s3_bucket cloudfront-logs {
-  bucket = "${data.aws_caller_identity.current.account_id}-tv-cloudfront-logs-spike"
-}
+# data aws_s3_bucket cloudfront-logs {
+#   bucket = "${data.aws_caller_identity.current.account_id}-tv-cloudfront-logs-spike"
+# }
