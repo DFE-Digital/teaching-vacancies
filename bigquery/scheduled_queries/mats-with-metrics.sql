15709c6d4170929959f3e71eba19ffe1ca8f3446
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2020-09-22 17:11:59 +0100

    Update mats-with-metrics.sql
    - Add count of multischool_vacancies_published to table of MATs

--- bigquery/scheduled_queries/mats-with-metrics.sql
+++ bigquery/scheduled_queries/mats-with-metrics.sql
@@ -15,6 +15,7 @@ FROM (
     trust_name,
     trust.id AS id,
     COUNT(school.id) AS trust_size,
+    #count trust-level vacancies published by this trust
     (
     SELECT
       COUNTIF(schoolgroup_level)
@@ -26,6 +27,26 @@ FROM (
       vacancy.id=organisationvacancy.vacancy_id
     WHERE
       trust.id=organisationvacancy.organisation_id) AS trust_level_vacancies_published,
+    #count multi-school vacancies where at least 1 of the schools is part of this trust
+    (
+    SELECT
+      COUNTIF(vacancy.number_of_organisations > 1)
+    FROM
+      `teacher-vacancy-service.production_dataset.vacancies_published` AS vacancy
+    LEFT JOIN
+      `teacher-vacancy-service.production_dataset.feb20_organisationvacancy` AS organisationvacancy
+    ON
+      vacancy.id=organisationvacancy.vacancy_id
+    LEFT JOIN
+      `teacher-vacancy-service.production_dataset.feb20_organisation` AS organisation
+    ON
+      organisation.id=organisationvacancy.organisation_id
+    LEFT JOIN
+      `teacher-vacancy-service.production_dataset.feb20_schoolgroupmembership` AS schoolgroupmembership
+    ON
+      organisation.id=schoolgroupmembership.school_id
+    WHERE
+      trust.id=schoolgroupmembership.school_group_id) AS multischool_vacancies_published,
     COUNTIF(signed_up IS TRUE) AS schools_signed_up,
     COUNTIF(vacancies_published_in_the_last_year>0) AS schools_that_published_vacancies_in_the_last_year,
     COUNTIF(vacancies_published_in_the_last_quarter>0) AS schools_that_published_vacancies_in_the_last_quarter,
