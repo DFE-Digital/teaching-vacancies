605540e2efbe718b1a29c90dc2f51695c79a4817
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-09-24 13:41:12 +0100

    Use TableComponent in VacancyDetailsComponent

--- app/components/jobseekers/vacancy_details_component.rb
+++ app/components/jobseekers/vacancy_details_component.rb
@@ -1,5 +1,16 @@
 class Jobseekers::VacancyDetailsComponent < ViewComponent::Base
+  attr_accessor :vacancy
+
   def initialize(vacancy:)
     @vacancy = vacancy
+  end
+
+  def rows
+    [{ present: vacancy.job_roles&.any?, th: I18n.t('jobs.job_roles'), td: vacancy.show_job_roles },
+     { present: vacancy.subjects&.any?,
+       th: I18n.t('jobs.subject', count: vacancy.subjects&.count),
+       td: vacancy.show_subjects },
+     { present: true, th: I18n.t('jobs.working_patterns'), td: vacancy.working_patterns },
+     { present: true, th: I18n.t('jobs.salary'), td: vacancy.salary }]
   end
 end
