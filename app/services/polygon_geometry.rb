98b4713f28c34499c7b3f7364a6e6f9d99f8a52f
David Mears <david.mears@digital.education.gov.uk>
2020-09-24 13:27:52 +0100

    WIP. Add service for converting polygons to a buffered polygon
    A buffer adds a constant 'radius' to the edge of a polygon.
    This does not preserve the original shape; it ends up with the
    corners rounded off, as if you had used a string and a pencil
    to trace x metres away from the boundary all the way around.

---
+++ app/services/polygon_geometry.rb
@@ -0,0 +1,66 @@
+class PolygonGeometry
+  def get_buffer_polygon(location_polygon:, buffer_in_metres: 30_000)
+    start_time = Time.zone.now
+    @buffer_in_metres = buffer_in_metres
+
+    # convert 1D array of lat, lng to 2D array of coordinates
+    coords = location_polygon.boundary.each_slice(2).to_a
+
+    # JSON of polygon for ArcGIS to consume
+    buffer_polygon = HTTParty.get(construct_buffer_api(coords))
+
+    # 1D array of polygon for Algolia to consume
+    buffer_boundary = buffer_polygon['geometries'].first['rings'].flatten
+
+    puts Time.zone.now - start_time
+    [buffer_polygon, buffer_boundary]
+  end
+
+# binding.pry
+# buffered_polygon = LocationPolygon.first.boundary
+
+# # https://sampleserver6.arcgisonline.com/arcgis/rest/services/Utilities/Geometry/GeometryServer/relation?sr=4326&geometries1={"geometryType":"esriGeometryPoint","geometries":[{"x":-104.53,"y":34.74},{"x":-63.53,"y":10.23}]}&geometries2={"geometryType":"esriGeometryPolygon","geometries":[{"rings":[[[-105,34],[-104,34],[-104,35],[-105,35],[-105,34]]]}]}&relation=esriGeometryRelationWithin&relationParam=&f=html
+
+# mid_time = Time.zone.now
+
+# LocationPolygon.each do |polygon|
+#   HTTParty.get(http://sampleserver6.arcgisonline.com/arcgis/rest/services/Utilities/Geometry/GeometryServer/relation?sr=4326&geometries1=%7B%22geometryType%22%3A%22esriGeometryPolygon%22%2C%22geometries%22%3A%5B%7B%22rings%22%3A%5B%5B%5B0%2C0%5D%2C%5B0%2C4%5D%2C%5B2%2C2%5D%2C%5B3%2C4%5D%2C%5B5%2C0%5D%2C%5B0%2C0%5D%5D%5D%7D%2C%7B%22rings%22%3A%5B%5B%5B0%2C4%5D%2C%5B3%2C4%5D%2C%5B6.72%2C3.42%5D%2C%5B4.34%2C2%5D%2C%5B0%2C4%5D%5D%5D%7D%5D%7D&geometries2=%7B%22geometryType%22%3A%22esriGeometryPolygon%22%2C%22geometries%22%3A%5B%7B%22rings%22%3A%5B%5B%5B3%2C4%5D%2C%5B6%2C72%2C3.42%5D%2C%5B4.34%2C2%5D%2C%5B3%2C4%5D%5D%5D%7D%2C%7B%22rings%22%3A%5B%5B%5B5%2C1%5D%2C%5B7%2C1%5D%2C%5B7%2C0%5D%2C%5B5%2C0%5D%2C%5B5%2C1%5D%5D%5D%7D%2C%7B%22rings%22%3A%5B%5B%5B1%2C1%5D%2C%5B1%2C2%5D%2C%5B2%2C1%5D%2C%5B1%2C1%5D%5D%5D%7D%2C%7B%22rings%22%3A%5B%5B%5B3%2C1%5D%2C%5B5%2C1%5D%2C%5B7%2C1%5D%2C%5B7%2C0%5D%2C%5B3%2C0%5D%2C%5B3%2C1%5D%5D%5D%7D%0D%0A%5D%7D&relation=esriGeometryRelationIntersection&relationParam=&f=html)
+# end
+
+# end_time = Time.zone.now
+# duration = end_time - start_time
+# puts duration
+
+private
+
+  def construct_buffer_api(coords)
+    geometries_param = {
+      'geometryType' => 'esriGeometryPolygon',
+      'geometries' => [{ 'rings' => [coords] }]
+    }
+
+    params = { 'geometries' => geometries_param.to_s,
+               'inSR' => '4326',
+               'outSR' => '4326',
+               'bufferSR' => '3857',
+               'distances' => @buffer_in_metres.to_s,
+               'unit' => '',
+               'unionResults' => 'true',
+               'geodesic' => 'false',
+               'f' => 'json' }.to_param
+
+    buffer_endpoint = 'https://ons-inspire.esriuk.com/arcgis/rest/services/Utilities/Geometry/GeometryServer/buffer?'
+
+    api = buffer_endpoint + params
+
+    if api.length > 2000
+      # Reduce number of coordinates in order to have fewer than 2000 characters in request url
+      coords = coords.each_with_index
+      .map { |item, index| item if (index % 5).zero? }
+      .compact
+      api = construct_buffer_api(coords)
+    end
+
+    api
+  end
+end
