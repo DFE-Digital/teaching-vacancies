6ce3710497af84beeebd8beb9f08884cd70b5f1f
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-09-25 10:06:05 +0100

    Update allowed origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   # config/application.rb.
 
   # Configure the domains permitted to access coordinates API
-  config.allowed_cors_origin = proc { "https://#{DOMAIN}" }
+  config.allowed_cors_origin = ["https://#{DOMAIN}", "https://www.#{DOMAIN}"]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
