94caef894e5c44fb123880432d42a35554afdeb6
Nick Romney <nick.romney@digital.education.gov.uk>
2020-09-25 15:07:16 +0100

    [TEVA-1330] - Add DNS records to Terraform common

--- terraform/common/route53.tf
+++ terraform/common/route53.tf
@@ -4,3 +4,12 @@ resource aws_route53_zone zones {
   comment       = "DNS Zone for Teaching Vacancies"
   force_destroy = false
 }
+
+resource aws_route53_record route53_records {
+  for_each = var.route53_records
+  zone_id  = aws_route53_zone.zones[each.value.zone_name].zone_id
+  name     = each.value.record_name
+  records  = [each.value.record_value]
+  ttl      = each.value.record_ttl
+  type     = each.value.record_type
+}
