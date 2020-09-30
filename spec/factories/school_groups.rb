f059a364fe8bd49f7650ab7dd810c9c8f3c87876
David Mears <david.mears@digital.education.gov.uk>
2020-09-30 17:58:28 +0100

    For school groups, save gias_data to an attribute on organisation
    As opposed to sourcing it from inside the gias_data json attribute
    
    For example, this makes the method organisation.postcode work for both Schools and SchoolGroups, which is needed for encapsulation.

--- spec/factories/school_groups.rb
+++ spec/factories/school_groups.rb
@@ -1,13 +1,23 @@
 FactoryBot.define do
   factory :school_group do
-    uid { Faker::Number.number(digits: 5).to_s }
+    address { Faker::Address.street_name.gsub("'", '') }
+    county { Faker::Address.state_abbr }
     gias_data do
       {
         "Group UID": uid,
-        "Group Name": 'Trust name',
-        "Group Type": 'Trust type'
+        "Group Name": name,
+        "Group Type": 'Trust type',
+        "Group Postcode": postcode,
+        "Group Type (code)": '06',
+        "Group Locality": address,
+        "Group Town": town,
+        "Group County": county
       }
     end
+    name { Faker::Company.name.gsub("'", '') + ' Trust' }
+    postcode { Faker::Address.postcode }
+    town { Faker::Address.city.gsub("'", '') }
+    uid { Faker::Number.number(digits: 5).to_s }
     website { Faker::Internet.url }
   end
 end
