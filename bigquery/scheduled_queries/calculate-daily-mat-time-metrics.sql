0292429e9a68cbdf67c301db46bb7b82d28ab8f4
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2020-10-01 09:56:10 +0100

    Update calculate-daily-mat-time-metrics.sql
    - Cut loads of unnecessary repetition out

--- bigquery/scheduled_queries/calculate-daily-mat-time-metrics.sql
+++ bigquery/scheduled_queries/calculate-daily-mat-time-metrics.sql
@@ -4,267 +4,95 @@ WITH
   SELECT
     date
   FROM
-    UNNEST(GENERATE_DATE_ARRAY('2020-09-01', DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY), INTERVAL 1 DAY)) AS date
+    UNNEST(GENERATE_DATE_ARRAY('2020-09-01', DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY), INTERVAL 1 DAY)) AS date #all dates between 1/9/20 and yesterday
   WHERE
     #only calculate these metrics for dates we don't already have within this range
     date NOT IN (
     SELECT
       DISTINCT date
     FROM
-      `teacher-vacancy-service.production_dataset.CALCULATED_daily_MAT_time_metrics`) ),
+      `teacher-vacancy-service.production_dataset.CALCULATED_daily_MAT_time_metrics` ) ),
   trust_metrics AS (
   SELECT
     date,
     size_bracket,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(in_scope)) AS trusts_in_scope,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(signed_up)) AS trusts_signed_up,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(using_mat_access_workaround)) AS trusts_using_mat_access_workaround,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(has_published_so_far)) AS trusts_which_published_so_far,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(has_published_in_the_last_year)) AS trusts_which_published_in_the_last_year,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(has_published_in_the_last_quarter)) AS trusts_which_published_in_the_last_quarter,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(had_live_vacancies)) AS trusts_which_had_vacancies_live,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(has_published_multischool_vacancies_so_far)) AS trusts_which_published_multischool_vacancies_so_far,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(has_published_multischool_vacancies_in_the_last_year)) AS trusts_which_published_multischool_vacancies_in_the_last_year,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(has_published_multischool_vacancies_in_the_last_quarter)) AS trusts_which_published_multischool_vacancies_in_the_last_quarter,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(had_live_multischool_vacancies)) AS trusts_which_had_multischool_vacancies_live,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(has_published_trust_level_vacancies_so_far)) AS trusts_which_published_trust_level_vacancies_so_far,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(has_published_trust_level_vacancies_in_the_last_year)) AS trusts_which_published_trust_level_vacancies_in_the_last_year,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(has_published_trust_level_vacancies_in_the_last_quarter)) AS trusts_which_published_trust_level_vacancies_in_the_last_quarter,
-  IF
-    (date > CURRENT_DATE(),
-      NULL,
-      COUNTIF(had_live_trust_level_vacancies)) AS trusts_which_had_trust_level_vacancies_live
+    COUNT(uid) AS trusts_in_scope,
+    COUNTIF(signed_up) AS trusts_signed_up,
+    COUNTIF(using_mat_access_workaround) AS trusts_using_mat_access_workaround,
+    COUNTIF(has_published_so_far) AS trusts_which_published_so_far,
+    COUNTIF(has_published_in_the_last_year) AS trusts_which_published_in_the_last_year,
+    COUNTIF(has_published_in_the_last_quarter) AS trusts_which_published_in_the_last_quarter,
+    COUNTIF(had_live_vacancies) AS trusts_which_had_vacancies_live,
+    COUNTIF(has_published_multischool_vacancies_so_far) AS trusts_which_published_multischool_vacancies_so_far,
+    COUNTIF(has_published_multischool_vacancies_in_the_last_year) AS trusts_which_published_multischool_vacancies_in_the_last_year,
+    COUNTIF(has_published_multischool_vacancies_in_the_last_quarter) AS trusts_which_published_multischool_vacancies_in_the_last_quarter,
+    COUNTIF(had_live_multischool_vacancies) AS trusts_which_had_multischool_vacancies_live,
+    COUNTIF(has_published_trust_level_vacancies_so_far) AS trusts_which_published_trust_level_vacancies_so_far,
+    COUNTIF(has_published_trust_level_vacancies_in_the_last_year) AS trusts_which_published_trust_level_vacancies_in_the_last_year,
+    COUNTIF(has_published_trust_level_vacancies_in_the_last_quarter) AS trusts_which_published_trust_level_vacancies_in_the_last_quarter,
+    COUNTIF(had_live_trust_level_vacancies) AS trusts_which_had_trust_level_vacancies_live
   FROM (
     SELECT
-      trusts.uid AS uid,
-      dates.date AS date,
-      trusts.size_bracket AS size_bracket,
-      trusts.using_mat_access_workaround AS using_mat_access_workaround,
-    IF
-      ((trusts.status != "Closed"
-          OR trusts.date_closed > dates.date) #if the trust is not currently closed or if it closed after the date we're calculating for
-        AND (trusts.status != "Proposed to open" #and if the trust is not currently proposed to open
-          OR trusts.date_opened <= dates.date #or if the trust opened before the date we're calculating for
-          )
-        AND trusts.date_created <= dates.date,
-        #and if the trust was first listed on GIAS before or on the date we're calculating for
-        TRUE,
-        FALSE) AS in_scope,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not signed up
-      IF
-        (COUNTIF(CAST(users.approval_datetime AS DATE) <= dates.date) >= 1,
-          #Count the number of users who had access, see if it is 1 or more, and if so count the school as signed up
-          TRUE,
-          FALSE)) AS signed_up,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having published
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date) > 1,
-          TRUE,
-          FALSE)) AS has_published_so_far,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having published in the last year
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND vacancies.publish_on >= DATE_SUB(dates.date,INTERVAL 1 YEAR)) > 1,
-          TRUE,
-          FALSE)) AS has_published_in_the_last_year,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having published in the last quarter
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND vacancies.publish_on >= DATE_SUB(dates.date,INTERVAL 3 MONTH)) > 1,
-          TRUE,
-          FALSE)) AS has_published_in_the_last_quarter,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having had live vacancies
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND vacancies.expires_on > dates.date) > 1,
-          TRUE,
-          FALSE)) AS had_live_vacancies,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having published
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND number_of_organisations > 1) > 1,
-          TRUE,
-          FALSE)) AS has_published_multischool_vacancies_so_far,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having published in the last year
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND vacancies.publish_on >= DATE_SUB(dates.date,INTERVAL 1 YEAR)
-            AND number_of_organisations > 1) > 1,
-          TRUE,
-          FALSE)) AS has_published_multischool_vacancies_in_the_last_year,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having published in the last quarter
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND vacancies.publish_on >= DATE_SUB(dates.date,INTERVAL 3 MONTH)
-            AND number_of_organisations > 1) > 1,
-          TRUE,
-          FALSE)) AS has_published_multischool_vacancies_in_the_last_quarter,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having had live vacancies
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND vacancies.expires_on > dates.date
-            AND number_of_organisations > 1) > 1,
-          TRUE,
-          FALSE)) AS had_live_multischool_vacancies,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having published
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND schoolgroup_level) > 1,
-          TRUE,
-          FALSE)) AS has_published_trust_level_vacancies_so_far,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having published in the last year
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND vacancies.publish_on >= DATE_SUB(dates.date,INTERVAL 1 YEAR)
-            AND schoolgroup_level) > 1,
-          TRUE,
-          FALSE)) AS has_published_trust_level_vacancies_in_the_last_year,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having published in the last quarter
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND vacancies.publish_on >= DATE_SUB(dates.date,INTERVAL 3 MONTH)
-            AND schoolgroup_level) > 1,
-          TRUE,
-          FALSE)) AS has_published_trust_level_vacancies_in_the_last_quarter,
-    IF
-      ((trusts.status = "Closed"
-          AND trusts.date_closed <= dates.date)
-        OR trusts.date_opened > dates.date,
-        FALSE,
-        #mark trusts which had closed or had not yet opened as not having had live vacancies
-      IF
-        (COUNTIF(vacancies.publish_on <= dates.date
-            AND vacancies.expires_on > dates.date
-            AND schoolgroup_level) > 1,
-          TRUE,
-          FALSE)) AS had_live_trust_level_vacancies
+      uid,
+      date,
+      size_bracket,
+      using_mat_access_workaround,
+      COUNTIF(CAST(user.approval_datetime AS DATE) <= date) >= 1 AS signed_up,
+      #Count the number of users who had access, see if it is 1 or more, and if so count the school as signed up
+      COUNTIF(vacancy.id IS NOT NULL) > 1 AS has_published_so_far,
+      COUNTIF(vacancy.publish_on >= DATE_SUB(date,INTERVAL 1 YEAR)) > 1 AS has_published_in_the_last_year,
+      COUNTIF(vacancy.publish_on >= DATE_SUB(date,INTERVAL 3 MONTH)) > 1 AS has_published_in_the_last_quarter,
+      COUNTIF(vacancy.expires_on > date) > 1 AS had_live_vacancies,
+      COUNTIF(number_of_organisations > 1) > 1 AS has_published_multischool_vacancies_so_far,
+      COUNTIF(vacancy.publish_on >= DATE_SUB(date,INTERVAL 1 YEAR)
+        AND number_of_organisations > 1) > 1 AS has_published_multischool_vacancies_in_the_last_year,
+      COUNTIF(vacancy.publish_on >= DATE_SUB(date,INTERVAL 3 MONTH)
+        AND number_of_organisations > 1) > 1 AS has_published_multischool_vacancies_in_the_last_quarter,
+      COUNTIF(vacancy.expires_on > date
+        AND number_of_organisations > 1) > 1 AS had_live_multischool_vacancies,
+      COUNTIF(schoolgroup_level) > 1 AS has_published_trust_level_vacancies_so_far,
+      COUNTIF(vacancy.publish_on >= DATE_SUB(date,INTERVAL 1 YEAR)
+        AND schoolgroup_level) > 1 AS has_published_trust_level_vacancies_in_the_last_year,
+      COUNTIF(vacancy.publish_on >= DATE_SUB(date,INTERVAL 3 MONTH)
+        AND schoolgroup_level) > 1 AS has_published_trust_level_vacancies_in_the_last_quarter,
+      COUNTIF(vacancy.expires_on > date
+        AND schoolgroup_level) > 1 AS had_live_trust_level_vacancies
     FROM
-      `teacher-vacancy-service.production_dataset.CALCULATED_MATs_with_metrics` AS trusts
+      `teacher-vacancy-service.production_dataset.CALCULATED_MATs_with_metrics` AS trust
     CROSS JOIN
       dates
     LEFT JOIN
-      `teacher-vacancy-service.production_dataset.dsi_users` AS users
+      `teacher-vacancy-service.production_dataset.dsi_users` AS user
     ON
-      CAST(users.organisation_uid AS STRING) = trusts.uid
+      CAST(user.organisation_uid AS STRING) = trust.uid
     LEFT JOIN
-      `teacher-vacancy-service.production_dataset.feb20_userpreference` AS tv_users_logged_in
+      `teacher-vacancy-service.production_dataset.feb20_userpreference` AS tv_user_logged_in
     ON
-      tv_users_logged_in.school_group_id=trusts.id
+      tv_user_logged_in.school_group_id=trust.id
     LEFT JOIN
-      `teacher-vacancy-service.production_dataset.vacancies_published` AS vacancies
+      `teacher-vacancy-service.production_dataset.vacancies_published` AS vacancy
     ON
-      vacancies.publisher_user_id=tv_users_logged_in.user_id
+      vacancy.publisher_user_id=tv_user_logged_in.user_id
+    WHERE
+      #only include trusts that were open on each date we're calculating for in the counts above
+      (trust.status != "Closed"
+        OR trust.date_closed > dates.date) #if the trust is not currently closed or if it closed after the date we're calculating for
+      AND (trust.status != "Proposed to open" #and if the trust is not currently proposed to open
+        OR trust.date_opened <= dates.date #or if the trust opened before the date we're calculating for
+        )
+      AND trust.date_created <= dates.date
+      #only include vacancies which were published before the date we're calculating for in the counts above - or blank vacancies (so that we keep rows for the trusts, dates and users where there are no published vacancies)
+      AND (vacancy.publish_on<date
+        OR vacancy.publish_on IS NULL)
     GROUP BY
       uid,
       date,
       date_opened,
       date_created,
       date_closed,
-      trusts.status,
-      trusts.size_bracket,
-      trusts.using_mat_access_workaround)
+      trust.status,
+      trust.size_bracket,
+      trust.using_mat_access_workaround)
   GROUP BY
     date,
     size_bracket )
