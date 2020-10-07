1977846fef5267222975c2ca87825253e6c09e3f
Nick Romney <nick.romney@digital.education.gov.uk>
2020-10-07 13:00:18 +0100

    [TEVA-1365] Add lifecycle to S3 Cloudfront logs

--- terraform/common/s3.tf
+++ terraform/common/s3.tf
@@ -6,4 +6,27 @@ resource aws_s3_bucket db_backups {
 
 resource aws_s3_bucket cloudfront_logs {
   bucket = "${data.aws_caller_identity.current.account_id}-tv-cloudfront-logs"
+
+  lifecycle_rule {
+    id      = "staging"
+    enabled = true
+
+    prefix = "staging/"
+
+    expiration {
+      days = 3
+    }
+  }
+
+  lifecycle_rule {
+    id      = "production"
+    enabled = true
+
+    prefix = "production/"
+
+    expiration {
+      days = 35
+    }
+  }
+
 }
