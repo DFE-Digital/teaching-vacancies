ea17d6c3e838bbe95c657c53e26c517bfd8e93f8
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-10-12 09:38:57 +0100

    Convert region fields in migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_074625) do
+ActiveRecord::Schema.define(version: 2020_10_12_083509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
