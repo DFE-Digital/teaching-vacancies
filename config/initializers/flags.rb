a76aa65caa909c06dd28608b4893974b0ca358ad
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-10-16 10:28:29 +0100

    Add LA access feature flag

--- config/initializers/flags.rb
+++ config/initializers/flags.rb
@@ -5,6 +5,7 @@ DISABLE_EXPENSIVE_JOBS = ENV['DISABLE_EXPENSIVE_JOBS']
 
 FEATURE_COOKIES_BANNER = ENV['FEATURE_COOKIES_BANNER']
 FEATURE_IMPORT_VACANCIES = ENV['FEATURE_IMPORT_VACANCIES']
+FEATURE_LOCAL_AUTHORITY_ACCESS = ENV['FEATURE_LOCAL_AUTHORITY_ACCESS']
 FEATURE_MULTI_SCHOOL_JOBS = ENV['FEATURE_MULTI_SCHOOL_JOBS']
 FEATURE_READ_ONLY = ENV['FEATURE_READ_ONLY']
 
@@ -13,5 +14,6 @@ DisableExpensiveJobs = Flag.new('disable_expensive_jobs', is_feature: false)
 
 CookiesBannerFeature = Flag.new('cookies_banner')
 ImportVacanciesFeature = Flag.new('import_vacancies')
+LocalAuthorityAccessFeature = Flag.new('local_authority_access')
 MultiSchoolJobsFeature = Flag.new('multi_school_jobs')
 ReadOnlyFeature = Flag.new('read_only')
