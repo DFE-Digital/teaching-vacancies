734299ab513aefd718eb5998fbdcd4dc6bfc6ddf
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-10-21 10:38:36 +0100

    Update managed organisations page

--- app/helpers/organisation_helper.rb
+++ app/helpers/organisation_helper.rb
@@ -3,6 +3,30 @@ module OrganisationHelper
 
   OFSTED_REPORT_ENDPOINT = 'https://reports.ofsted.gov.uk/oxedu_providers/full/(urn)/'.freeze
 
+  def age_range(school)
+    return I18n.t('schools.not_given') unless school.minimum_age? && school.maximum_age?
+
+    "#{school.minimum_age} to #{school.maximum_age}"
+  end
+
+  def edit_vacancy_section_number(section, organisation)
+    sections = {
+      job_location: 1,
+      job_details: 2,
+      pay_package: 3,
+      important_dates: 4,
+      supporting_documents: 5,
+      application_details: 6,
+      job_summary: 7,
+    }
+    section_number = organisation.is_a?(SchoolGroup) ? sections[section] : sections[section] - 1
+    "#{section_number}."
+  end
+
+  def full_address(organisation)
+    address_join([organisation.address, organisation.town, organisation.county, organisation.postcode])
+  end
+
   def location(organisation, job_location: nil)
     return "#{I18n.t('hiring_staff.organisations.readable_job_location.at_multiple_schools')}, #{organisation.name}" if
       job_location.presence == 'at_multiple_schools'
@@ -10,8 +34,16 @@ module OrganisationHelper
     address_join([organisation.name, organisation.town, organisation.county])
   end
 
-  def full_address(organisation)
-    address_join([organisation.address, organisation.town, organisation.county, organisation.postcode])
+  def managed_organisations_all_label(organisation)
+    if organisation.group_type == 'local_authority'
+      I18n.t('hiring_staff.managed_organisations.options.all_schools')
+    else
+      I18n.t('hiring_staff.managed_organisations.options.all')
+    end
+  end
+
+  def ofsted_report(school)
+    OFSTED_REPORT_ENDPOINT + school.urn
   end
 
   def organisation_type(organisation:, with_age_range: false)
@@ -47,30 +79,6 @@ module OrganisationHelper
       return school_capacity(school) if school.gias_data['SchoolCapacity'].present?
     end
     I18n.t('schools.no_information')
-  end
-
-  def ofsted_report(school)
-    OFSTED_REPORT_ENDPOINT + school.urn
-  end
-
-  def age_range(school)
-    return I18n.t('schools.not_given') unless school.minimum_age? && school.maximum_age?
-
-    "#{school.minimum_age} to #{school.maximum_age}"
-  end
-
-  def edit_vacancy_section_number(section, organisation)
-    sections = {
-      job_location: 1,
-      job_details: 2,
-      pay_package: 3,
-      important_dates: 4,
-      supporting_documents: 5,
-      application_details: 6,
-      job_summary: 7,
-    }
-    section_number = organisation.is_a?(SchoolGroup) ? sections[section] : sections[section] - 1
-    "#{section_number}."
   end
 
 private
