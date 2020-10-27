9bbbec011228cf64f910508d23f26b1903ef8437
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-10-27 14:50:47 +0000

    Allow BannerLinkComponent to accept custom id

--- app/components/shared/banner_link_component.rb
+++ app/components/shared/banner_link_component.rb
@@ -1,15 +1,16 @@
 class Shared::BannerLinkComponent < ViewComponent::Base
-  attr_accessor :icon_class, :link_method, :link_path, :link_text
+  attr_accessor :icon_class, :link_id, :link_method, :link_path, :link_text
 
-  def initialize(icon_class:, link_method:, link_path:, link_text:)
+  def initialize(icon_class:, link_id:, link_method:, link_path:, link_text:)
     @icon_class = icon_class
+    @link_id = link_id
     @link_method = link_method
     @link_path = link_path
     @link_text = link_text
   end
 
   def call
-    link_to link_path, method: link_method, class: "banner-link banner-link--#{icon_class}" do
+    link_to link_path, method: link_method, class: "banner-link banner-link--#{icon_class}", id: link_id do
       content_tag(:div, link_text, class: 'banner-link__text')
     end
   end
