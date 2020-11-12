bcc972fc2826ee33e625b25655c1ac792e11e0de
Nick Romney <nick.romney@digital.education.gov.uk>
2020-11-12 18:07:38 +0000

    [TEVA-1536] Add Docker tag as TF output

---
+++ terraform/app/modules/paas/outputs.tf
@@ -0,0 +1,4 @@
+output docker_image {
+  value       = cloudfoundry_app.web_app.docker_image
+  description = "Docker image - repository:tag"
+}
--- terraform/app/output.tf
+++ terraform/app/output.tf
@@ -1,4 +1,8 @@
+output docker_image {
+  value       = module.paas.docker_image
+  description = "Docker image - repository:tag"
+}
+
 output workspace {
   value = terraform.workspace
 }
-
