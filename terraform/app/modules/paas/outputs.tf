9d1482104708e99bef02e1b4a2dd4c1e7a975954
Nick Romney <nick.romney@digital.education.gov.uk>
2020-11-13 15:40:25 +0000

    [TEVA-1536] GH Actions refresh - set docker tag output
    Extract Docker tag from image using regex, in the context of a terraform
    output.
    Disable `terraform_wrapper` functionality of `setup-terraform` GH
    Action, which allows setting of tag as output, for input to workflow
    triggered by workflow dispatch

--- terraform/app/modules/paas/outputs.tf
+++ terraform/app/modules/paas/outputs.tf
@@ -2,3 +2,7 @@ output docker_image {
   value       = cloudfoundry_app.web_app.docker_image
   description = "Docker image - repository:tag"
 }
+
+output docker_tag {
+  value = regex(":(.*)$", cloudfoundry_app.web_app.docker_image)[0]
+}
