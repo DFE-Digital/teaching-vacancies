d8a3b669f8bc584163d0225a8e8e5f87932635a3
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-11-19 13:18:00 +0000

    Add invalid recaptcha score check to general feedback page

--- spec/system/general_feedback_spec.rb
+++ spec/system/general_feedback_spec.rb
@@ -1,66 +1,42 @@
 require "rails_helper"
 
 RSpec.describe "Giving general feedback for the service" do
-  before(:each) do
-    visit new_feedback_path
-  end
+  context "when all required fields are complete" do
+    scenario "can submit feedback" do
+      visit new_feedback_path
+      expect(page).to have_content(I18n.t("feedback.heading"))
 
-  it "displays the general feedback page" do
-    expect(page).to have_content(I18n.t("feedback.heading"))
-  end
-
-  context "when submitting feedback on the service" do
-    let(:choose_yes_to_participation) { choose("general-feedback-user-participation-response-interested-field") }
-    let(:choose_no_to_participation) { choose("general-feedback-user-participation-response-not-interested-field") }
-
-    scenario "must have a visit purpose" do
-      fill_in "general_feedback[comment]", with: "Keep going!"
-      choose_no_to_participation
-
-      click_on I18n.t("buttons.submit_feedback")
-
-      expect(page).to have_content("Enter the reason for your visit")
-    end
-
-    scenario "must have a participation response" do
-      choose "Find a job in teaching"
+      choose("general-feedback-visit-purpose-find-teaching-job-field")
       fill_in "general_feedback[comment]", with: "Keep going!"
 
-      click_on I18n.t("buttons.submit_feedback")
-
-      expect(page).to have_content("Please indicate if you'd like to participate in user research")
-    end
-
-    scenario "successfully submitting feedback for the service" do
-      choose "Find a job in teaching"
-      fill_in "general_feedback[comment]", with: "Keep going!"
-      choose_no_to_participation
-
-      click_on I18n.t("buttons.submit_feedback")
-
-      expect(page).to have_content("Your feedback has been successfully submitted")
-    end
-
-    scenario "must have an email when participation response is Yes" do
-      choose "Find a job in teaching"
-      fill_in "general_feedback[comment]", with: "Keep going!"
-      choose_yes_to_participation
-
-      click_on I18n.t("buttons.submit_feedback")
-
-      expect(page).to have_content("Enter your email address")
-    end
-
-    scenario "successfully submitting feedback and interest in user research" do
-      choose "Find a job in teaching"
-      fill_in "general_feedback[comment]", with: "Keep going!"
-
-      choose_yes_to_participation
+      choose("general-feedback-user-participation-response-interested-field")
       fill_in "email", with: "test@test.com"
 
       click_on I18n.t("buttons.submit_feedback")
 
-      expect(page).to have_content("Your feedback has been successfully submitted")
+      expect(page).to have_content(I18n.t("messages.feedback.submitted"))
+    end
+  end
+
+  context "when all required fields are not complete" do
+    scenario "can not submit feedback" do
+      visit new_feedback_path
+      expect(page).to have_content(I18n.t("feedback.heading"))
+      click_on I18n.t("buttons.submit_feedback")
+      expect(page).to have_content("There is a problem")
+    end
+  end
+
+  context "when recaptcha score is invalid" do
+    before do
+      allow_any_instance_of(ApplicationController).to receive(:verify_recaptcha).and_return(true)
+      allow_any_instance_of(ApplicationController).to receive(:recaptcha_reply).and_return({ "score" => 0.1 })
+    end
+
+    scenario "redirects to invalid_recaptcha path" do
+      visit new_feedback_path
+      click_on I18n.t("buttons.submit_feedback")
+      expect(page).to have_current_path(invalid_recaptcha_path(form_name: "General feedback"))
     end
   end
 end
