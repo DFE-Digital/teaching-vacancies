65632cc55332ab606c8a02d0297c3651934b172f
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2020-11-25 14:25:44 +0000

    Update vacancies_published.sql
    - Refactor vacancy categoriser into UDF

--- bigquery/views/vacancies_published.sql
+++ bigquery/views/vacancies_published.sql
@@ -40,27 +40,7 @@ SELECT
   contact_number,
   publisher_organisation_id,
   #categorises each vacancy as leadership,teaching_assistant,teacher or NULL
-IF
-  (LOWER(job_title) LIKE '%head%'
-    OR LOWER(job_title) LIKE '%ordinat%'
-    OR LOWER(job_title) LIKE '%principal%',
-    "leadership",
-  IF
-    ((job_title LIKE '%TA%'
-        OR job_title LIKE '%TAs%'
-        OR LOWER(job_title) LIKE '% assistant%' #picks up teaching assistant, learning support assistant etc.
-        OR LOWER(job_title) LIKE '%intervention %')
-      AND LOWER(job_title) NOT LIKE '%admin%'
-      AND LOWER(job_title) NOT LIKE '%account%'
-      AND LOWER(job_title) NOT LIKE '%marketing%'
-      AND LOWER(job_title) NOT LIKE '%admission%'
-      AND LOWER(job_title) NOT LIKE '%care%',
-      "teaching_assistant",
-    IF
-      (LOWER(job_title) LIKE '%teacher%'
-        OR LOWER(job_title) LIKE '%lecturer%',
-        "teacher",
-        NULL))) AS category,
+  `teacher-vacancy-service.production_dataset.categorise_vacancy_job_title`(job_title) AS category,
   STRING_AGG(DISTINCT employer_organisation.phase, " and ") AS education_phase,
   STRING_AGG(DISTINCT employer_organisation.detailed_school_type, " and ") AS detailed_school_type,
   STRING_AGG(DISTINCT employer_organisation.school_type, " and ") AS school_type,
@@ -86,12 +66,12 @@ IF
 IF
   (publisher_organisation.group_type = "Multi-academy trust",
     MAX((
-    SELECT
-      COUNT(DISTINCT schoolgroupmembership.school_id)
-    FROM
-      `teacher-vacancy-service.production_dataset.feb20_schoolgroupmembership` AS schoolgroupmembership
-    WHERE
-      schoolgroupmembership.school_group_id=publisher_organisation.id)),
+      SELECT
+        COUNT(DISTINCT schoolgroupmembership.school_id)
+      FROM
+        `teacher-vacancy-service.production_dataset.feb20_schoolgroupmembership` AS schoolgroupmembership
+      WHERE
+        schoolgroupmembership.school_group_id=publisher_organisation.id)),
     MAX((
       SELECT
         COUNT(DISTINCT schoolgroupmembership.school_id)
