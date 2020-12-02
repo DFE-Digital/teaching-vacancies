1dcc03e8ee53b5cf6bf87c7c577c7ac906f48289
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-12-02 14:52:07 +0000

    Store jobseeker location This is used to redirect successfully after sign in [with Devise](https://github.com/heartcombo/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update)

--- app/controllers/jobseekers/application_controller.rb
+++ app/controllers/jobseekers/application_controller.rb
@@ -1,3 +1,14 @@
 class Jobseekers::ApplicationController < ApplicationController
+  before_action :store_jobseeker_location!, if: :storable_location?
   before_action :authenticate_jobseeker!
+
+private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_jobseeker_location!
+    store_location_for(:jobseeker, request.fullpath)
+  end
 end
