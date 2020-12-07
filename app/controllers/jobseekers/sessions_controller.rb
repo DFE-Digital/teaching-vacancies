d3decc41d252057c33b11293ab7f779cc05d0068
David Mears <david.mears@digital.education.gov.uk>
2020-12-07 13:32:20 +0000

    Render Devise sign in error in error summary component on form
    In order to reduce code complexity by avoiding extending/customizing Devise too heavily, we agreed with UX only to have a single, generic error message for all jobseeker sign in errors.

--- app/controllers/jobseekers/sessions_controller.rb
+++ app/controllers/jobseekers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class Jobseekers::SessionsController < Devise::SessionsController
   after_action :replace_devise_notice_flash_with_success!, only: %i[create destroy]
   before_action :sign_out_publisher!, only: %i[create]
+
+  def new
+    if [I18n.t("devise.failure.invalid"), I18n.t("devise.failure.not_found_in_database")].include? flash[:alert]
+      self.resource = resource_class.new(sign_in_params)
+      resource.errors.add(:email, flash[:alert])
+      flash.clear
+      render :new and return
+    else
+      super
+    end
+  end
 end
