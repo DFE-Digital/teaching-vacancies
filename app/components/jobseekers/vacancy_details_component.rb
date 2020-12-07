c941284c96101f851a9ba72342e5dbdb299177e3
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-12-07 16:36:51 +0000

    Remove table component and use GovukComponent::SummaryList

--- app/components/jobseekers/vacancy_details_component.rb
+++ app/components/jobseekers/vacancy_details_component.rb
@@ -4,13 +4,4 @@ class Jobseekers::VacancyDetailsComponent < ViewComponent::Base
   def initialize(vacancy:)
     @vacancy = vacancy
   end
-
-  def rows
-    [{ present: vacancy.job_roles&.any?, th: I18n.t("jobs.job_roles"), td: vacancy.show_job_roles },
-     { present: vacancy.subjects&.any?,
-       th: I18n.t("jobs.subject", count: vacancy.subjects&.count),
-       td: vacancy.show_subjects },
-     { present: true, th: I18n.t("jobs.working_patterns"), td: vacancy.working_patterns },
-     { present: true, th: I18n.t("jobs.salary"), td: vacancy.salary }]
-  end
 end
