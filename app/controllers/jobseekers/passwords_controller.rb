43c2f2f25857eff86c6a5a9d3f76522cdee152cf
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2020-12-09 16:54:21 +0000

    Refactor passwords_controller

--- app/controllers/jobseekers/passwords_controller.rb
+++ app/controllers/jobseekers/passwords_controller.rb
@@ -1,6 +1,6 @@
 class Jobseekers::PasswordsController < Devise::PasswordsController
-  before_action :set_jobseeker_from_token, only: %i[edit update]
-  before_action :check_reset_password_token, only: %i[edit]
+  before_action :set_resource, only: %i[edit update]
+  before_action :check_reset_password_token, only: %i[edit update]
   after_action :remove_devise_notice_flash!, only: %i[create]
 
 protected
@@ -9,12 +9,12 @@ protected
     Devise.token_generator.digest(resource_class, :reset_password_token, params[:reset_password_token] || params[resource_name][:reset_password_token])
   end
 
-  def set_jobseeker_from_token
-    @jobseeker_from_token = Jobseeker.find_by(reset_password_token: reset_password_token)
+  def set_resource
+    self.resource = Jobseeker.find_by(reset_password_token: reset_password_token)
   end
 
   def check_reset_password_token
-    render :expired_token if @jobseeker_from_token&.reset_password_sent_at&.before?(2.hours.ago)
+    render :expired_token if resource&.reset_password_sent_at&.before?(2.hours.ago)
   end
 
   def after_sending_reset_password_instructions_path_for(_resource)
