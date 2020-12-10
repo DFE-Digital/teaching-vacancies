1e602c110406f9e29c3ab1aa206cbce4be1bade8
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2020-12-10 11:50:07 +0000

    Fix flaky reset password spec
    The way we parsed the token in the reset password email sent was not 
    considering the fact that there could be non characters signs

--- spec/support/mailer_helpers.rb
+++ spec/support/mailer_helpers.rb
@@ -8,6 +8,6 @@ module MailerHelpers
   end
 
   def devise_token_from_last_mail(token_name)
-    last_email.body.to_s[/#{token_name}_token=(\w*)/, 1]
+    last_email.body.to_s[/#{token_name}_token=([^)]*)/, 1]
   end
 end
