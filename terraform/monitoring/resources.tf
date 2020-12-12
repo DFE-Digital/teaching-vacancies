9e2f714433aaa1f4ffc79b8eefe539d86ad76a46
Nick Romney <nick.romney@digital.education.gov.uk>
2020-12-12 07:43:09 +0000

    [TEVA-859] Add Slack config to alertmanager

--- terraform/monitoring/resources.tf
+++ terraform/monitoring/resources.tf
@@ -1,13 +1,17 @@
 module prometheus_all {
-  source = "git::https://github.com/DFE-Digital/bat-platform-building-blocks.git//terraform/modules/prometheus_all"
+  source = "git::https://github.com/DFE-Digital/bat-platform-building-blocks.git//terraform/modules/prometheus_all?ref=teva-859-add-slack-to-prometheus-all"
 
-  monitoring_instance_name = local.monitoring_instance_name
-  monitoring_org_name      = local.monitoring_org_name
-  monitoring_space_name    = local.monitoring_space_name
-  paas_exporter_username   = local.secrets["paas_exporter_username"]
-  paas_exporter_password   = local.secrets["paas_exporter_password"]
-  alertmanager_config      = file("${path.module}/files/alertmanager.yml")
-  grafana_admin_password   = local.secrets["grafana_admin_password"]
-  grafana_json_dashboards  = [file("${path.module}/files/paas_dashboard.json")]
-  alert_rules              = file("${path.module}/files/alert.rules")
+  monitoring_instance_name   = local.monitoring_instance_name
+  monitoring_org_name        = local.monitoring_org_name
+  monitoring_space_name      = local.monitoring_space_name
+  paas_exporter_username     = local.secrets["paas_exporter_username"]
+  paas_exporter_password     = local.secrets["paas_exporter_password"]
+  alertmanager_config        = file("${path.module}/files/alertmanager.yml")
+  alertmanager_slack_url     = local.secrets["alertmanager_slack_url"]
+  alertmanager_slack_channel = local.alertmanager_slack_channel
+  grafana_admin_password     = local.secrets["grafana_admin_password"]
+  grafana_json_dashboards    = [file("${path.module}/files/paas_dashboard.json")]
+  alert_rules                = file("${path.module}/files/alert.rules")
 }
+
+# alert_rules are to configure the alerts within Prometheus
