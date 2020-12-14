e64e3117cec30fa1818eb2515993ba47d8aa07a0
alexbowen <alexbowen123@googlemail.com>
2020-12-14 02:47:48 +0000

    fix review comments on ruby style

--- app/helpers/jobseekers/accounts_helper.rb
+++ app/helpers/jobseekers/accounts_helper.rb
@@ -1,5 +1,5 @@
 module Jobseekers::AccountsHelper
-  def account_link(link_text, link_path)
+  def account_navigation_link(link_text, link_path)
     link_to link_text, link_path, class: "moj-primary-navigation__link", aria: { current: ("page" if current_page?(link_path)) }
   end
 end
