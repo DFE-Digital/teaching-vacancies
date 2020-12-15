cef8c3cf45a2c11478672b36e8f5f0c0651d0674
alexbowen <alexbowen123@googlemail.com>
2020-12-15 09:16:04 +0000

    move filters hide button out of dashboard component to vacancies componenet

--- app/components/shared/dashboard_component.rb
+++ app/components/shared/dashboard_component.rb
@@ -1,14 +1,9 @@
 class Shared::DashboardComponent < ViewComponent::Base
   attr_accessor :heading, :navigation_items, :panel, :link
 
-  def initialize(heading:, navigation_items:, panel:, link:)
+  def initialize(heading:, navigation_items:, link:)
     @heading = heading
     @navigation_items = navigation_items
-    @panel = panel
     @link = link
-  end
-
-  def display_actions
-    @panel || @link
   end
 end
