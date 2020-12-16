afc006ab9a565ce8431ad22e34a1d0c023f471b6
David Mears <david.mears@digital.education.gov.uk>
2020-12-16 16:17:38 +0000

    Refactor mailer helper to make tests terser

--- spec/support/mailer_helpers.rb
+++ spec/support/mailer_helpers.rb
@@ -7,7 +7,7 @@ module MailerHelpers
     ActionMailer::Base.deliveries
   end
 
-  def devise_token_from_last_mail(token_name)
-    last_email.body.to_s[/#{token_name}_token=([^)]*)/, 1]
+  def first_link_from_last_mail
+    last_email.body.to_s[/\]\((.*)\)/, 1].delete_suffix(")")
   end
 end
