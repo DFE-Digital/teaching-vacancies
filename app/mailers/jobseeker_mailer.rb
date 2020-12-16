5412fe5f54e560f26f545fc677ee9c73ccb652c1
Connor McQuillan <connor.mcquillan17@gmail.com>
2020-12-16 15:11:31 +0000

    Add email changed mailer and update confirmation mailer to account for updated emails

--- app/mailers/jobseeker_mailer.rb
+++ app/mailers/jobseeker_mailer.rb
@@ -9,8 +9,18 @@ class JobseekerMailer < Devise::Mailer
 
     view_mail(
       NOTIFY_JOBSEEKER_CONFIRMATION_TEMPLATE,
-      to: @jobseeker.email,
+      to: @jobseeker.pending_reconfirmation? ? @jobseeker.unconfirmed_email : @jobseeker.email,
       subject: I18n.t("jobseeker_mailer.confirmation_instructions.subject"),
+    )
+  end
+
+  def email_changed(record, _opts = {})
+    @jobseeker = record
+
+    view_mail(
+      NOTIFY_JOBSEEKER_EMAIL_CHANGED_TEMPLATE,
+      to: @jobseeker.email,
+      subject: I18n.t("jobseeker_mailer.email_changed.subject"),
     )
   end
 
