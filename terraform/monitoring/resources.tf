3d47cfeb28f0da871fee332e43aedeeb05eaac28
Nick Romney <nick.romney@digital.education.gov.uk>
2020-12-17 09:14:08 +0000

    [TEVA-859] Send config values to prometheus_all module
    Send Slack URL, channel, or composite alertmanager config to upstream
    prometheus_all module

--- terraform/monitoring/resources.tf
+++ terraform/monitoring/resources.tf
@@ -6,8 +6,12 @@ module prometheus_all {
   monitoring_space_name    = local.monitoring_space_name
   paas_exporter_username   = local.secrets["paas_exporter_username"]
   paas_exporter_password   = local.secrets["paas_exporter_password"]
-  alertmanager_config      = file("${path.module}/config/alertmanager.yml.tmpl")
   grafana_admin_password   = local.secrets["grafana_admin_password"]
   grafana_json_dashboards  = [file("${path.module}/config/paas_dashboard.json")]
   alert_rules              = file("${path.module}/config/alert.rules.yml")
+  # Send alertmanager_slack_channel and alertmanager_slack_url to prometheus_all to use default notifications
+  alertmanager_slack_url     = local.alertmanager_slack_url
+  alertmanager_slack_channel = local.alertmanager_slack_channel
+  # Send alertmanager_config to prometheus_all to use notifications defined in config/alertmanager.yml.tmpl
+  #alertmanager_config = local.alertmanager_config
 }
