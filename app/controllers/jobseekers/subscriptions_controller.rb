b324c82fe43e66b0a030394a4395aedb1ef9ec9a
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2020-12-17 10:47:33 +0000

    Add ordering to jobseeker manage job alerts page

--- app/controllers/jobseekers/subscriptions_controller.rb
+++ app/controllers/jobseekers/subscriptions_controller.rb
@@ -1,6 +1,7 @@
 class Jobseekers::SubscriptionsController < Jobseekers::ApplicationController
   def index
     @subscriptions = Subscription.active.where(email: current_jobseeker.email)
+                                        .order(created_at: :desc)
                                         .map { |subscription| SubscriptionPresenter.new(subscription) }
   end
 end
