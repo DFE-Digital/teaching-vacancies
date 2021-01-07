ad8a413e87858ba1175cc3e412b826a7f885387b
Christian Sutter <git@doublesignal.com>
2021-01-07 10:16:49 +0000

    Make auth not clear rest of session
    We will occasionally want to persist parts of the session that are
    unrelated to authentication when a user signs in/out, such as A/B tests.
    
    - Change publisher auth to only clear the parts of the session it owns
    - Disable Devise's `sign_out_all_scopes` to prevent Warden from nuking
      the session on signing out
    - Add tests and comments to make sure it doesn't get accidentally turned
      off

---
+++ spec/controllers/jobseekers/sessions_controller_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe Jobseekers::SessionsController, type: :controller do
+  context "unrelated session contents" do
+    let(:unrelated_session_contents) { { keep_me: { foo: "bar" } }.stringify_keys }
+
+    let(:credentials) { { email: "test@example.com", password: "password" } }
+    let!(:jobseeker) { Jobseeker.create(credentials) }
+
+    before do
+      # Required to test Devise controller independently of routing
+      @request.env["devise.mapping"] = Devise.mappings[:jobseeker]
+
+      jobseeker.confirm
+
+      session.merge!(unrelated_session_contents)
+    end
+
+    describe "#create" do
+      it "does not clear unrelated session contents" do
+        post :create, params: { jobseeker: credentials }
+
+        expect(session).to include(*unrelated_session_contents.keys)
+      end
+    end
+
+    describe "#destroy" do
+      before do
+        sign_in jobseeker
+      end
+
+      it "does not clear unrelated session contents" do
+        delete :destroy
+
+        expect(session).to include(*unrelated_session_contents.keys)
+      end
+    end
+  end
+end
