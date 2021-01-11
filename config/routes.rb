d4eacf435bada2877693684343256038826f5fe5
Nick Romney <nick.romney@digital.education.gov.uk>
2021-01-11 15:26:55 +0000

    [TEVA-1762] Add Sidekiq dashboard with basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end
+  mount Sidekiq::Web, at: "/sidekiq"
+
   constraints(-> { JobseekerAccountsFeature.enabled? }) do
     devise_for :jobseekers, controllers: {
       confirmations: "jobseekers/confirmations",
