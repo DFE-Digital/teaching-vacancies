320f80f8bf20dd3cc0176bf28afc641e8b38a590
Connor McQuillan <connor.mcquillan17@gmail.com>
2021-01-12 16:38:37 +0000

    Add subscriptions sorting

---
+++ app/services/jobseekers/subscription_sort.rb
@@ -0,0 +1,19 @@
+class Jobseekers::SubscriptionSort < RecordSort
+  def initialize
+    @column = options.first.column
+    @order = options.first.order
+  end
+
+  def options
+    [
+      SortOption.new("created_at", "desc", I18n.t("jobs.sort_by.created_at.descending.subscription")),
+      SortOption.new("frequency", "asc", I18n.t("jobs.sort_by.frequency.ascending")),
+    ]
+  end
+
+private
+
+  def valid_sort_columns
+    %w[created_at frequency].freeze
+  end
+end
