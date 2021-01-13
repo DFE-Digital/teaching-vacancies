3ad4a68664694a7d19f894f1ff3f4eed34062001
josephhull676 <josephhull676@yahoo.co.uk>
2021-01-13 16:08:21 +0000

    Create jobseeker applications feature flag

--- config/initializers/flags.rb
+++ config/initializers/flags.rb
@@ -4,6 +4,7 @@ AUTHENTICATION_FALLBACK = ENV["AUTHENTICATION_FALLBACK"]
 DISABLE_EXPENSIVE_JOBS = ENV["DISABLE_EXPENSIVE_JOBS"]
 
 FEATURE_JOBSEEKER_ACCOUNTS = ENV["FEATURE_JOBSEEKER_ACCOUNTS"]
+FEATURE_JOBSEEKER_APPLICATIONS = ENV["FEATURE_JOBSEEKER_APPLICATIONS"]
 FEATURE_LOCAL_AUTHORITY_ACCESS = ENV["FEATURE_LOCAL_AUTHORITY_ACCESS"]
 FEATURE_READ_ONLY = ENV["FEATURE_READ_ONLY"]
 
@@ -11,5 +12,6 @@ AuthenticationFallback = Flag.new("authentication_fallback", is_feature: false)
 DisableExpensiveJobs = Flag.new("disable_expensive_jobs", is_feature: false)
 
 JobseekerAccountsFeature = Flag.new("jobseeker_accounts")
+JobseekerApplicationsFeature = Flag.new("jobseeker_applications")
 LocalAuthorityAccessFeature = Flag.new("local_authority_access")
 ReadOnlyFeature = Flag.new("read_only")
