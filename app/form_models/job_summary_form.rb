4bdaa05df808bbe4e7d36511a6eabcfaad2e1066
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-01-15 14:51:31 +0000

    Remove validation concerns that are not shared

--- app/form_models/job_summary_form.rb
+++ app/form_models/job_summary_form.rb
@@ -1,3 +1,21 @@
 class JobSummaryForm < VacancyForm
-  include VacancyJobSummaryValidations
+  validates :job_summary, presence: true
+  validate :about_school_must_not_be_blank
+
+  def about_school_must_not_be_blank
+    return if about_school.present?
+
+    # Since vacancy is set by VacancyForm.initialize, it can be undefined here.
+    case job_location
+    when "central_office"
+      organisation = "trust"
+    when "at_one_school"
+      organisation = "school"
+    when "at_multiple_schools"
+      organisation = "schools"
+    end
+    errors.add(:about_school,
+               I18n.t("job_summary_errors.about_school.blank",
+                      organisation: organisation))
+  end
 end
--- app/form_models/pay_package_form.rb
+++ app/form_models/pay_package_form.rb
@@ -1,3 +1,13 @@
 class PayPackageForm < VacancyForm
-  include VacancyPayPackageValidations
+  include ActionView::Helpers::SanitizeHelper
+
+  validates :salary, presence: true
+  validates :salary, length: { minimum: 1, maximum: 256 }, if: :salary?
+  validate :salary_has_no_tags?, if: :salary?
+
+  def salary_has_no_tags?
+    return if salary == sanitize(salary, tags: [])
+
+    errors.add(:salary, I18n.t("activemodel.errors.models.pay_package_form.attributes.salary.invalid_characters"))
+  end
 end
