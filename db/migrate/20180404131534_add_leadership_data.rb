982b6f217cec24786a5f2d1ef1b2cd27e4dd4c73
David Mears <60350599+david-mears-dfe@users.noreply.github.com>
2021-01-18 16:38:48 +0000

    Comment out unusable migration
    Commented out this method as it relies on code that no longer exists. We removed the class Leadership.

--- db/migrate/20180404131534_add_leadership_data.rb
+++ db/migrate/20180404131534_add_leadership_data.rb
@@ -1,9 +1,11 @@
 class AddLeadershipData < ActiveRecord::Migration[5.1]
   def change
-    ['Executive Head', 'Headteacher',
-     'Middle Leader', 'Multi-Academy Trust',
-     'Senior Leader'].each do |leadership|
-       Leadership.find_or_create_by(title: leadership)
-     end
+    # Commented out this method as it relies on code that no longer exists. We removed the class Leadership.
+    
+#     ['Executive Head', 'Headteacher',
+#      'Middle Leader', 'Multi-Academy Trust',
+#      'Senior Leader'].each do |leadership|
+#        Leadership.find_or_create_by(title: leadership)
+#      end
   end
 end
