0b8e5dbd7ca8e634a4c1092171a367aee55add94
Connor McQuillan <connor.mcquillan17@gmail.com>
2021-01-20 09:17:28 +0000

    Remove redundant coordinates api endpoint

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,13 +8,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: Rails.en
              headers: :any,
              methods: :get
   end
-
-  allow do
-    # Only allow our domains access to coordinates API with HTTP GET
-    origins Rails.application.config.allowed_cors_origin.call
-
-    resource "/api/v1/coordinates/*",
-             headers: :any,
-             methods: :get
-  end
 end
--- spec/requests/cors_spec.rb
+++ spec/requests/cors_spec.rb
@@ -2,28 +2,6 @@ require "rails_helper"
 require "geocoding"
 
 RSpec.describe "CORS", type: :request do
-  describe "/api/v1/coordinates/:location.json" do
-    let(:location_query) { "abingdon" }
-
-    scenario "allows a request from a domain defined by configuration" do
-      params = { api_version: 1, location: location_query, format: "json" }
-      headers = { 'HTTP_ORIGIN': Rails.application.config.allowed_cors_origin.call }
-      get api_path(params), headers: headers
-
-      expect(response.headers["X-Rack-CORS"]).to eq("hit")
-      expect(response.headers["Access-Control-Allow-Origin"]).to eq(Rails.application.config.allowed_cors_origin.call)
-    end
-
-    scenario "does not allow a request from a different domain" do
-      params = { api_version: 1, location: location_query, format: "json" }
-      headers = { 'HTTP_ORIGIN': "https://www.test.com" }
-      get api_path(params), headers: headers
-
-      expect(response.headers["X-Rack-CORS"]).to include("miss")
-      expect(response.headers["Access-Control-Allow-Origin"]).to be_blank
-    end
-  end
-
   describe "/api/v1/jobs.json" do
     scenario "is configured to allow a request from any domain" do
       params = { api_version: 1, format: "json" }
