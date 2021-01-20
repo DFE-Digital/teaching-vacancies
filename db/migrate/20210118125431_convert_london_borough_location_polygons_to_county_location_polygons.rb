98749b61679b524f0da4e2b458ac9553bc18c549
David Mears <david.mears@digital.education.gov.uk>
2021-01-20 10:25:31 +0000

    Remove unnecessary migration
    Also allow some London boroughs to be considered as cities for the purposes
    of VacancyFacets (going on whether they're described as towns in a Google
    search).

--- db/migrate/20210118125431_convert_london_borough_location_polygons_to_county_location_polygons.rb
+++
@@ -1,7 +0,0 @@
-class ConvertLondonBoroughLocationPolygonsToCountyLocationPolygons < ActiveRecord::Migration[6.1]
-  def change
-    LocationPolygon.where(location_type: "london_boroughs").each do |borough|
-      borough.update!(location_type: 'counties')
-    end
-  end
-end
