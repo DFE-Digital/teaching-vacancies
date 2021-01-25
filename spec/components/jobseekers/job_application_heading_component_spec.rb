1646e6ab84eb0596dc666292fd2b88708dfbdea7
Connor McQuillan <connor.mcquillan17@gmail.com>
2021-01-25 17:38:28 +0000

    Update before you start application page

--- spec/components/jobseekers/job_application_heading_component_spec.rb
+++ spec/components/jobseekers/job_application_heading_component_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe Jobseekers::JobApplicationHeadingComponent, type: :component do
   let(:school) { build_stubbed(:school, name: "Test school") }
   let(:vacancy) do
-    build_stubbed(:vacancy, :at_one_school, job_title: "Test job",
-                                            organisation_vacancies_attributes: [{ organisation: school }])
+    build_stubbed(:vacancy, :at_one_school, :published_slugged, job_title: "Test job",
+                                                                organisation_vacancies_attributes: [{ organisation: school }])
   end
 
   before do
