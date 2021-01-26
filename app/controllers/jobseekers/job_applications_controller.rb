8a5cd36d54b1e6a38e769921372e91663ba14af5
Connor McQuillan <connor.mcquillan17@gmail.com>
2021-01-26 14:45:55 +0000

    Refactor job application controllers

--- app/controllers/jobseekers/job_applications_controller.rb
+++ app/controllers/jobseekers/job_applications_controller.rb
@@ -1,23 +1,23 @@
 class Jobseekers::JobApplicationsController < Jobseekers::ApplicationController
-  before_action :set_up_vacancy, only: %i[new create]
-  before_action :set_up_job_application, only: %i[review submit]
+  helper_method :job_application, :vacancy
 
   def create
-    job_application = current_jobseeker.job_applications.create(status: :draft, vacancy: @vacancy)
-    redirect_to jobseekers_job_application_build_path(job_application, :personal_details)
+    new_job_application = current_jobseeker.job_applications.find_or_initialize_by(vacancy: vacancy)
+    new_job_application.draft!
+    redirect_to jobseekers_job_application_build_path(new_job_application, :personal_details)
   end
 
   def submit
-    @job_application.update(status: :submitted)
+    job_application.update(status: :submitted)
   end
 
   private
 
-  def set_up_vacancy
-    @vacancy = Vacancy.live.find(params[:job_id])
+  def job_application
+    @job_application ||= current_jobseeker.job_applications.find(params[:job_application_id])
   end
 
-  def set_up_job_application
-    @job_application = current_jobseeker.job_applications.find(params[:job_application_id])
+  def vacancy
+    @vacancy ||= Vacancy.live.find(params[:job_id])
   end
 end
