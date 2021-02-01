9bf95cc55c6dcd2e7395a2b06e2c6da89a129d11
Nick Romney <nick.romney@digital.education.gov.uk>
2021-02-01 16:38:43 +0000

    [TEVA-1897] Use placeholders in initializers
    For Algolia and RECAPTCAH which both require values to be present,
    fallback to a placeholder value in code if ENV var is not present.
    
    In `test.yml` workflow, remove the ENV vars

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,7 +1,5 @@
-if ENV["ALGOLIA_APP_ID"]
-  AlgoliaSearch.configuration = {
-    application_id: ENV["ALGOLIA_APP_ID"],
-    api_key: ENV["ALGOLIA_WRITE_API_KEY"],
-    pagination_backend: :kaminari,
-  }
-end
+AlgoliaSearch.configuration = {
+  application_id: ENV.fetch("ALGOLIA_APP_ID", "Placeholder"),
+  api_key: ENV.fetch("ALGOLIA_WRITE_API_KEY", "Placeholder"),
+  pagination_backend: :kaminari,
+}
