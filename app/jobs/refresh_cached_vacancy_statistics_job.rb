8c45b155e941884d0eb2cef7e5e42c9e2493cd53
Christian Sutter <git@doublesignal.com>
2021-02-02 18:27:40 +0000

    Remove RefreshCachedVacancyStatistics job
    This job and its Rake task are no longer needed as of #2725.

--- app/jobs/refresh_cached_vacancy_statistics_job.rb
+++
@@ -1,8 +0,0 @@
-class RefreshCachedVacancyStatisticsJob < ApplicationJob
-  queue_as :low
-
-  def perform
-    Rails.application.load_tasks
-    Rake::Task["vacancies:statistics:refresh_cache"].invoke
-  end
-end
