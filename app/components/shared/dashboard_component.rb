15a75633e64553f9141e71795c514ecb3693a569
alexbowen <alexbowen123@googlemail.com>
2021-02-05 22:01:33 +0000

    add correct style and markup for no listings publisher view

--- app/components/shared/dashboard_component.rb
+++ app/components/shared/dashboard_component.rb
@@ -1,7 +1,7 @@
 class Shared::DashboardComponent < ViewComponent::Base
   attr_reader :heading, :navigation_items, :panel, :link
 
-  def initialize(heading:, navigation_items:, link: nil)
+  def initialize(heading:, navigation_items: [], link: nil)
     @heading = heading
     @navigation_items = navigation_items
     @link = link
