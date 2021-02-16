f6cbd5024d2cb9eb1afadffdb75c2361a7e53e9e
Nick Romney <nick.romney@digital.education.gov.uk>
2021-02-16 12:27:19 +0000

    AWS provider full namespace

--- terraform/app/versions.tf
+++ terraform/app/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.13.1"
   required_providers {
     aws = {
-      source  = "-/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.28.0"
     }
     cloudfoundry = {
