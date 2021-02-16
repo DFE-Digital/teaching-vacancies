6512bca95bca0ecdeaf8bf3d960f6e030c02d850
Connor McQuillan <connor.mcquillan17@gmail.com>
2021-02-16 15:48:55 +0000

    Trigger email events after emails are delivered

--- app/mailers/jobseeker_mailer.rb
+++ app/mailers/jobseeker_mailer.rb
@@ -3,32 +3,61 @@ class JobseekerMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers
   default template_path: "jobseeker_mailer"
 
+  after_action :trigger_email_event
+
   def confirmation_instructions(record, token, _opts = {})
+    @template = NOTIFY_JOBSEEKER_CONFIRMATION_TEMPLATE
     @jobseeker = record
-    @token = token
-    @confirmation_type = @jobseeker.pending_reconfirmation? ? ".reconfirmation" : ""
     @to = @jobseeker.pending_reconfirmation? ? @jobseeker.unconfirmed_email : @jobseeker.email
 
-    view_mail(NOTIFY_JOBSEEKER_CONFIRMATION_TEMPLATE, to: @to, subject: t("#{@confirmation_type}.subject"))
+    @token = token
+    @confirmation_type = @jobseeker.pending_reconfirmation? ? ".reconfirmation" : ""
+
+    view_mail(@template, to: @to, subject: t("#{@confirmation_type}.subject"))
   end
 
   def email_changed(record, _opts = {})
+    @template = NOTIFY_JOBSEEKER_EMAIL_CHANGED_TEMPLATE
     @jobseeker = record
+    @to = @jobseeker.email
 
-    view_mail(NOTIFY_JOBSEEKER_EMAIL_CHANGED_TEMPLATE, to: @jobseeker.email, subject: t(".subject"))
+    view_mail(@template, to: @to, subject: t(".subject"))
   end
 
   def reset_password_instructions(record, token, _opts = {})
+    @template = NOTIFY_JOBSEEKER_RESET_PASSWORD_TEMPLATE
     @jobseeker = record
+    @to = @jobseeker.email
+
     @token = token
 
-    view_mail(NOTIFY_JOBSEEKER_RESET_PASSWORD_TEMPLATE, to: @jobseeker.email, subject: t(".subject"))
+    view_mail(@template, to: @to, subject: t(".subject"))
   end
 
   def unlock_instructions(record, token, _opts = {})
+    @template = NOTIFY_JOBSEEKER_LOCKED_ACCOUNT_TEMPLATE
     @jobseeker = record
+    @to = @jobseeker.email
+
     @token = token
 
-    view_mail(NOTIFY_JOBSEEKER_LOCKED_ACCOUNT_TEMPLATE, to: @jobseeker.email, subject: t(".subject"))
+    view_mail(@template, to: @to, subject: t(".subject"))
+  end
+
+  private
+
+  def email_event
+    @email_event ||= EmailEvent.new(@template, @to, jobseeker: @jobseeker)
+  end
+
+  def trigger_email_event
+    data = case action_name
+           when "confirmation_instructions"
+             { previous_email_identifier: StringAnonymiser.new(@jobseeker.email) }
+           else
+             {}
+           end
+
+    email_event.trigger("jobseeker_#{action_name}".to_sym, data)
   end
 end
