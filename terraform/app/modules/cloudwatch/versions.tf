7faccdd454f9178b41d08454b1212c896b9cf73d
Nick Romney <nick.romney@digital.education.gov.uk>
2021-02-18 15:36:01 +0000

    Remove version constraints from TF modules

--- terraform/app/modules/cloudwatch/versions.tf
+++ terraform/app/modules/cloudwatch/versions.tf
@@ -1,5 +1,4 @@
 terraform {
-  required_version = ">= 0.13.1"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
--- terraform/app/modules/statuscake/versions.tf
+++ terraform/app/modules/statuscake/versions.tf
@@ -1,5 +1,4 @@
 terraform {
-  required_version = ">= 0.13.1"
   required_providers {
     statuscake = {
       source  = "StatusCakeDev/statuscake"
