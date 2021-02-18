692cd3b10bd328feb79361695dcfbdf8664d3935
David Mears <david.mears@digital.education.gov.uk>
2021-02-18 15:06:39 +0000

    Remove unused attribute from account feedback form

--- app/form_models/jobseekers/account_feedback_form.rb
+++ app/form_models/jobseekers/account_feedback_form.rb
@@ -1,7 +1,7 @@
 class Jobseekers::AccountFeedbackForm
   include ActiveModel::Model
 
-  attr_accessor :origin, :comment, :jobseeker_id, :rating
+  attr_accessor :origin, :comment, :rating
 
   validates :comment, length: { maximum: 1200 }, if: -> { comment.present? }
   validates :rating, inclusion: { in: Feedback.ratings.keys }
