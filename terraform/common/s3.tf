80678fc6352019d13a3639456e579a11ad967040
Nick Romney <nick.romney@digital.education.gov.uk>
2021-02-18 17:51:19 +0000

    [TEVA-2004] Restrict S3 policy further

--- terraform/common/s3.tf
+++ terraform/common/s3.tf
@@ -16,7 +16,8 @@ resource "aws_s3_bucket" "db_backups" {
 resource "aws_s3_bucket_public_access_block" "db_backups" {
   bucket = aws_s3_bucket.db_backups.id
 
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
