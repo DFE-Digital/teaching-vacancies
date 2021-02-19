e6ee73fda69470fc90f181d0037f2f7b96dc3d73
Christian Sutter <git@doublesignal.com>
2021-02-19 13:42:31 +0000

    Remove GDS Performance Platform reporting
    The Performance Platform has been broken for a while and is now
    officially deprecated:
    https://www.gov.uk/service-manual/measuring-success/data-you-must-publish
    
    This removes all our code related to exporting data to it.

--- app/jobs/submit_performance_platform_job.rb
+++
@@ -1,8 +0,0 @@
-class SubmitPerformancePlatformJob < ApplicationJob
-  queue_as :low
-
-  def perform
-    Rails.application.load_tasks
-    Rake::Task["performance_platform:submit_transactions"].invoke
-  end
-end
