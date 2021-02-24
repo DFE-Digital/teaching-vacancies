db545701b3b00f6d6167b2b8478cdd3c50d48323
Christian Sutter <git@doublesignal.com>
2021-02-24 11:06:46 +0000

    Cache similar jobs for 12 hours
    Store vacancy IDs for similar jobs in the Rails cache for 12 hours so
    we don't keep hitting Algolia.

--- app/services/search/similar_jobs.rb
+++ app/services/search/similar_jobs.rb
@@ -1,11 +1,31 @@
 class Search::SimilarJobs
-  attr_reader :similar_jobs
+  attr_reader :vacancy
 
   NUMBER_OF_SIMILAR_JOBS = 4
+  CACHE_DURATION = 12.hours
 
   def initialize(vacancy)
+    @vacancy = vacancy
+  end
+
+  def criteria
     # For now, similar jobs are retrieved based on the same set of rules that define similar job alerts
-    criteria = Search::CriteriaDeviser.new(vacancy).criteria
-    @similar_jobs = Search::VacancySearch.new(criteria).vacancies.reject { |job| job.id == vacancy.id }.take(NUMBER_OF_SIMILAR_JOBS)
+    @criteria ||= Search::CriteriaDeviser.new(vacancy).criteria
+  end
+
+  def similar_jobs
+    @similar_jobs ||= Vacancy.live.where(id: similar_job_ids).first(NUMBER_OF_SIMILAR_JOBS)
+  end
+
+  private
+
+  def similar_job_ids
+    Rails.cache.fetch([:similar_job_ids, vacancy.id], expires_in: CACHE_DURATION) do
+      Search::VacancySearch
+        .new(criteria, per_page: NUMBER_OF_SIMILAR_JOBS * 3) # Fetch more than we need in case some expire while being cached
+        .vacancies
+        .reject { |job| job.id == vacancy.id }
+        .map(&:id)
+    end
   end
 end
