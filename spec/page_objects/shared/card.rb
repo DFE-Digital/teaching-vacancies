376c153289631bfc95e40e012d068a73a8f2c8fd
Connor McQuillan <connor.mcquillan17@gmail.com>
2021-03-02 18:55:03 +0000

    Jobseeker job applications dashboard

---
+++ spec/page_objects/shared/card.rb
@@ -0,0 +1,11 @@
+module PageObjects
+  module Shared
+    class Card < SitePrism::Section
+      element :header, ".card-component__header"
+      element :body, ".card-component__body"
+      section :actions, ".card-component__actions" do
+        elements :links, "a.govuk-link"
+      end
+    end
+  end
+end
