55e17dd42d80fbf2e14b2f9fcc8766a0096aece4
Connor McQuillan <connor.mcquillan17@gmail.com>
2021-03-03 15:53:57 +0000

    Add no QTS explanation field to professional status page

--- spec/form_models/jobseekers/job_application/professional_status_form_spec.rb
+++ spec/form_models/jobseekers/job_application/professional_status_form_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Jobseekers::JobApplication::ProfessionalStatusForm, type: :model 
 
     it { is_expected.to validate_numericality_of(:qualified_teacher_status_year).is_less_than_or_equal_to(Time.current.year) }
   end
+
+  context "when qualified_teacher_status is no" do
+    before { allow(subject).to receive(:qualified_teacher_status).and_return("no") }
+
+    it { is_expected.to validate_presence_of(:no_qualified_teacher_status_details) }
+  end
 end
