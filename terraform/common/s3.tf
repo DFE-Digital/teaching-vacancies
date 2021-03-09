b0550c8b464ec580c6ecaeca0fc0573d6e5ea2ce
Nick Romney <nick.romney@digital.education.gov.uk>
2021-03-09 11:29:48 +0000

    S3 policy tweaks for offline bucket
    - Specify bucket resource ARN for GetBucketAcl, GetBucketLocation,
    ListBucket, PutBucketAcl
    - Move policy into iam.tf file

--- terraform/common/s3.tf
+++ terraform/common/s3.tf
@@ -35,32 +35,3 @@ resource "aws_s3_bucket_public_access_block" "db_backups" {
 resource "aws_s3_bucket" "offline_site" {
   bucket = "${data.aws_caller_identity.current.account_id}-offline-site"
 }
-
-data "aws_iam_policy_document" "offline_site_full_access" {
-  statement {
-    actions = [
-      "s3:GetBucketAcl",
-      "s3:GetBucketLocation",
-      "s3:GetObject",
-      "s3:GetObjectAcl",
-      "s3:ListBucket",
-      "s3:PutBucketAcl",
-      "s3:PutObject"
-    ]
-    resources = [
-      "arn:aws:s3:::${aws_s3_bucket.offline_site.bucket}/",
-      "arn:aws:s3:::${aws_s3_bucket.offline_site.bucket}/*",
-      "arn:aws:s3:::${aws_s3_bucket.offline_site.bucket}/teaching-vacancies-offline/*"
-    ]
-  }
-}
-
-resource "aws_iam_policy" "offline_site_full_access" {
-  name   = "offline_site_full_access"
-  policy = data.aws_iam_policy_document.offline_site_full_access.json
-}
-
-resource "aws_iam_user_policy_attachment" "offline_site_full_access" {
-  user       = aws_iam_user.deploy.name
-  policy_arn = aws_iam_policy.offline_site_full_access.arn
-}
