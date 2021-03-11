bce932cb829a570755388bfd463f023866394f4f
David Mears <david.mears@digital.education.gov.uk>
2021-03-11 17:43:22 +0000

    Replace vacancy statistics form js with GDS form and add missing form labels

---
+++ app/form_models/publishers/vacancy_statistics_form.rb
@@ -0,0 +1,13 @@
+class Publishers::VacancyStatisticsForm
+  include ActiveModel::Model
+
+  attr_accessor :hired_status, :listed_elsewhere
+
+  validate :all_questions_completed
+
+  def all_questions_completed
+    return if hired_status.present? && listed_elsewhere.present?
+
+    errors.add(:base, I18n.t("errors.publishers.job_statistics.base_error"))
+  end
+end
--- app/frontend/src/application/publishers/init.js
+++ app/frontend/src/application/publishers/init.js
@@ -1,4 +1,3 @@
-import './feedbackForm';
 import './uploadDocuments/uploadDocuments';
 import './deleteDocument';
 import '../../components/form/form';
