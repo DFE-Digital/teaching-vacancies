6cafc79f0e5429dabf2d558606326751c5328593
Connor McQuillan <connor.mcquillan17@gmail.com>
2021-03-12 09:05:18 +0000

    Add job application shortlisted and rejected emails

--- app/mailers/jobseekers/job_application_mailer.rb
+++ app/mailers/jobseekers/job_application_mailer.rb
@@ -1,12 +1,38 @@
 class Jobseekers::JobApplicationMailer < Jobseekers::BaseMailer
+  def application_rejected(job_application)
+    @job_application = job_application
+    @vacancy = @job_application.vacancy
+    @organisation_name = @vacancy.parent_organisation.name
+    @contact_email = @vacancy.contact_email
+    @jobseeker = @job_application.jobseeker
+
+    @template = NOTIFY_JOBSEEKER_APPLICATION_REJECTED_TEMPLATE
+    @to = @jobseeker.email
+
+    view_mail(@template, to: @to, subject: I18n.t("jobseekers.job_application_mailer.application_rejected.subject"))
+  end
+
+  def application_shortlisted(job_application)
+    @job_application = job_application
+    @vacancy = @job_application.vacancy
+    @organisation_name = @vacancy.parent_organisation.name
+    @contact_email = @vacancy.contact_email
+    @jobseeker = @job_application.jobseeker
+
+    @template = NOTIFY_JOBSEEKER_APPLICATION_SHORTLISTED_TEMPLATE
+    @to = @jobseeker.email
+
+    view_mail(@template, to: @to, subject: I18n.t("jobseekers.job_application_mailer.application_shortlisted.subject"))
+  end
+
   def application_submitted(job_application)
     @vacancy = job_application.vacancy
     @organisation_name = @vacancy.parent_organisation.name
     @contact_email = @vacancy.contact_email
     @jobseeker = job_application.jobseeker
 
-    @template = NOTIFY_JOBSEEKER_APPLICATION_SUBMITTED_CONFIRMATION_TEMPLATE
-    @to = job_application.jobseeker.email
+    @template = NOTIFY_JOBSEEKER_APPLICATION_SUBMITTED_TEMPLATE
+    @to = @jobseeker.email
 
     view_mail(@template, to: @to, subject: I18n.t("jobseekers.job_application_mailer.application_submitted.subject"))
   end
