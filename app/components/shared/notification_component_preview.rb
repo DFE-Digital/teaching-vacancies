cc6a8b78bd09fae25abb44f1ec6dd52702f1a07e
alexbowen <alexbowen123@googlemail.com>
2021-03-16 11:51:03 +0000

    add basic files preview component files for notification component

---
+++ app/components/shared/notification_component_preview.rb
@@ -0,0 +1,16 @@
+class Shared::NotificationComponentPreview < ViewComponent::Preview
+  layout "preview"
+
+  attr_accessor :variants, :form
+
+  def initialize
+    @variants = [
+      { id: "success", name: "Success", title: "Success alert", description: "Sometimes dismissable." },
+      { id: "notice", name: "Notice", title: "General information alert", description: "Sometimes dismissable." },
+      { id: "danger", name: "Danger", title: "Warnings to user", description: "Never dismissable. This is a warning message or something potentially bad for the user." },
+      { id: "empty", name: "Empty", title: "Inline emphasis", description: "Basic inline emphasis." },
+    ]
+
+    @form = Shared::NotificationComponentPreview::PreviewOptionsForm
+  end
+end
---
+++ app/components/shared/notification_component_preview/preview_options_form.rb
@@ -0,0 +1,20 @@
+class Shared::NotificationComponentPreview::PreviewOptionsForm
+  include ActiveModel::Model
+
+  attr_reader :background, :dismiss, :title, :body, :variant, :icon
+
+  def initialize(params = {})
+    preview_criteria = if params[:shared_notification_component_preview_form].present?
+                         JSON.parse(params[:shared_notification_component_preview_form].to_json).symbolize_keys
+                       else
+                         {}
+                       end
+
+    @icon = preview_criteria[:icon] || false
+    @background = preview_criteria[:background] || false
+    @dismiss = preview_criteria[:dismiss] || false
+    @title = preview_criteria[:title] || "title text"
+    @body = preview_criteria[:body] || "body text"
+    @variant = preview_criteria[:variant] || "success"
+  end
+end
