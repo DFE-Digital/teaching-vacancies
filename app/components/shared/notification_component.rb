39232af4c377bc6da1facb1e65e31226b1ab481e
alexbowen <alexbowen123@googlemail.com>
2021-03-16 02:45:24 +0000

    refactor notification component

--- app/components/shared/notification_component.rb
+++ app/components/shared/notification_component.rb
@@ -16,14 +16,27 @@ class Shared::NotificationComponent < GovukComponent::Base
     @title = title
     @variant = variant
     @links = links
+<<<<<<< HEAD
     @dismiss = variant == "warning" ? false : dismiss
+=======
+    @dismiss = variant == "danger" ? false : dismiss
+>>>>>>> 72a5268ff... refactor notification component
     @background = background
     @icon = icon
 
     super(classes: classes, html_attributes: html_attributes.merge(default_html_attributes))
+<<<<<<< HEAD
+=======
   end
   # rubocop:enable Metrics/ParameterLists
 
+  def variant_class
+    "govuk-notification--#{@variant}"
+>>>>>>> 72a5268ff... refactor notification component
+  end
+  # rubocop:enable Metrics/ParameterLists
+
+<<<<<<< HEAD
   def variant_class
     "govuk-notification--#{@variant}"
   end
@@ -32,6 +45,12 @@ class Shared::NotificationComponent < GovukComponent::Base
     "govuk-notification__background"
   end
 
+=======
+  def background_class
+    "govuk-notification__background"
+  end
+
+>>>>>>> 72a5268ff... refactor notification component
   def icon_class
     "icon icon--left icon--#{@variant}"
   end
@@ -51,10 +70,19 @@ class Shared::NotificationComponent < GovukComponent::Base
   private
 
   def default_classes
+<<<<<<< HEAD
     ["notification-component", "govuk-notification", variant_class].tap do |applied_classes|
       applied_classes.push(background_class) if @background
       applied_classes.push(icon_class) if @icon
       applied_classes.push(dismissable_class) if @dismiss
     end
+=======
+    applied_classes = %w[notification-component govuk-notification]
+    applied_classes.push(variant_class)
+    applied_classes.push(background_class) if @background
+    applied_classes.push(icon_class) if @icon
+    applied_classes.push(dismissable_class) if @dismiss
+    applied_classes
+>>>>>>> 72a5268ff... refactor notification component
   end
 end
