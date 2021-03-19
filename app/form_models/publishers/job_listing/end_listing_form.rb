526c9f158bfd0e4ac4055eed912958319717e0e1
Connor McQuillan <connor.mcquillan17@gmail.com>
2021-03-19 11:54:26 +0000

    Implement end job listing form

---
+++ app/form_models/publishers/job_listing/end_listing_form.rb
@@ -0,0 +1,8 @@
+class Publishers::JobListing::EndListingForm
+  include ActiveModel::Model
+
+  attr_accessor :end_listing_reason, :candidate_hired_from
+
+  validates :end_listing_reason, inclusion: { in: Vacancy.end_listing_reasons.keys }
+  validates :candidate_hired_from, inclusion: { in: Vacancy.candidate_hired_froms.keys }, if: -> { end_listing_reason == "suitable_candidate_found" }
+end
