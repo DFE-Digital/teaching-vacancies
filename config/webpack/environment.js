d31c7fece41665cd0b67a26711646b151ae4cf84
alexbowen <alexbowen123@googlemail.com>
2021-03-19 14:33:31 +0000

    test

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,17 @@
 const { environment } = require('@rails/webpacker');
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-environment.plugins.append(
-  'MiniCssExtractPlugin', // arbitrary name
-   new MiniCssExtractPlugin({
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  }),
-);
+// environment.plugins.append(
+//   'MiniCssExtractPlugin', // arbitrary name
+//    new MiniCssExtractPlugin({
+//     rules: [
+//       {
+//         test: /\.s[ac]ss$/i,
+//         use: [MiniCssExtractPlugin.loader, 'css-loader'],
+//       },
+//     ],
+//   }),
+// );
 
 module.exports = environment;
