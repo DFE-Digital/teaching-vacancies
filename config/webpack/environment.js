df52f1b74ddfe0308f521b3c9b157412e4e33c85
alexbowen <alexbowen123@googlemail.com>
2021-03-19 16:48:01 +0000

    remove redundant webpack plugin that solves dissapearance of cache bust value from css bundle

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,3 @@
 const { environment } = require('@rails/webpacker');
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-environment.plugins.append(
-  'MiniCssExtractPlugin', // arbitrary name
-   new MiniCssExtractPlugin({
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  }),
-);
-
 module.exports = environment;
