19fe2975c4b1b7bd17ea56410a11d0b82eea6ca0
Nick Romney <nick.romney@digital.education.gov.uk>
2021-03-27 09:32:08 +0000

    Set StatusCake node_locations to UK and Ireland

--- terraform/app/modules/statuscake/main.tf
+++ terraform/app/modules/statuscake/main.tf
@@ -1,12 +1,13 @@
 resource "statuscake_test" "alert" {
-  for_each      = var.statuscake_alerts
-  website_name  = each.value.website_name
-  website_url   = each.value.website_url
-  test_type     = "HTTP"
-  check_rate    = 30
-  contact_group = each.value.contact_group
-  trigger_rate  = 0
-  find_string   = lookup(each.value, "find_string", null)
-  do_not_find   = lookup(each.value, "do_not_find", false)
-  confirmations = 1
+  for_each       = var.statuscake_alerts
+  website_name   = each.value.website_name
+  website_url    = each.value.website_url
+  test_type      = "HTTP"
+  check_rate     = 30
+  contact_group  = each.value.contact_group
+  trigger_rate   = 0
+  find_string    = lookup(each.value, "find_string", null)
+  do_not_find    = lookup(each.value, "do_not_find", false)
+  confirmations  = 1
+  node_locations = ["EC1", "MAN1", "DUB2"]
 }
