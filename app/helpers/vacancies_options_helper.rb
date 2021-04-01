d6a8c7008480e7df8ee466ada10afc9c616eeef5
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-04-01 16:14:07 +0100

    Fix copy changes in LA dashboard

--- app/helpers/vacancies_options_helper.rb
+++ app/helpers/vacancies_options_helper.rb
@@ -7,18 +7,6 @@ module VacanciesOptionsHelper
     Vacancy.listed_elsewheres.keys.map { |k| [t("jobs.feedback.listed_elsewhere.#{k}"), k] }.unshift(["--", ""])
   end
 
-  def job_location_options(organisation)
-    mapped_job_location_options(organisation)
-      .delete_if { |_k, v| organisation.group_type == "local_authority" && v == "central_office" }
-      .reject(&:blank?)
-  end
-
-  def mapped_job_location_options(organisation)
-    Vacancy.job_locations.keys.map do |job_location|
-      [t("helpers.options.publishers_job_listing_job_location_form.job_location.#{job_location}", organisation_type: organisation_type_basic(organisation)), job_location]
-    end
-  end
-
   def radius_filter_options
     Search::RadiusSuggestionsBuilder::RADIUS_OPTIONS.inject([]) do |radii, radius|
       radii << [t("jobs.filters.number_of_miles", count: radius), radius]
