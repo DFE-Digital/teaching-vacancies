2018f186c5e1c108547ba58800a57fdbcb3b737b
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-04-01 11:23:32 +0100

    Send job application received emails to publishers

---
+++ app/jobs/send_applications_received_yesterday_job.rb
@@ -0,0 +1,20 @@
+class SendApplicationsReceivedYesterdayJob < ApplicationJob
+  queue_as :low
+
+  def perform
+    publishers_with_vacancies_with_applications_submitted_yesterday.each do |publisher|
+      next unless publisher.email?
+
+      Publishers::JobApplicationMailer.applications_received(publisher: publisher).deliver_later
+      Rails.logger.info("Sidekiq: Sending job applications received yesterday for publisher id: #{publisher.id}")
+    end
+  end
+
+  private
+
+  def publishers_with_vacancies_with_applications_submitted_yesterday
+    Publisher.distinct
+             .joins(vacancies: :job_applications)
+             .where("DATE(job_applications.submitted_at) = ? AND job_applications.status = ?", Date.yesterday, 1)
+  end
+end
---
+++ app/mailers/publishers/job_application_mailer.rb
@@ -0,0 +1,28 @@
+class Publishers::JobApplicationMailer < Publishers::BaseMailer
+  helper VacanciesHelper
+
+  def applications_received(publisher:)
+    @template = NOTIFY_PUBLISHER_JOB_APPLICATIONS_RECEIVED_TEMPLATE
+    @publisher = publisher
+    @vacancies = publisher.vacancies_with_job_applications_submitted_yesterday
+    @to = publisher.email
+
+    @job_applications_count = @vacancies.sum { |vacancy| vacancy.job_applications.submitted_yesterday.count }
+    @subject = I18n.t("publishers.job_application_mailer.applications_received.subject", count: @job_applications_count)
+
+    view_mail(@template, to: @to, subject: @subject)
+  end
+
+  private
+
+  def email_event_data
+    { vacancies_job_applications: vacancies_job_applications }
+  end
+
+  def vacancies_job_applications
+    @vacancies.each_with_object({}) do |vacancy, hash|
+      hash[StringAnonymiser.new(vacancy.id).to_s] =
+        vacancy.job_applications.submitted_yesterday.pluck(:id).map { |job_application_id| StringAnonymiser.new(job_application_id).to_s }
+    end
+  end
+end
