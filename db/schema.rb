c24d2cf63e441c347bb349c47b95e4952c3710ae
Connor McQuillan <cpjmcquillan@gmail.com>
2021-04-16 15:24:46 +0100

    Add notifications using noticed gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_164134) do
+ActiveRecord::Schema.define(version: 2021_04_15_172816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -226,6 +226,18 @@ ActiveRecord::Schema.define(version: 2021_04_12_164134) do
     t.datetime "updated_at", null: false
     t.jsonb "buffers"
     t.jsonb "polygons"
+  end
+
+  create_table "notifications", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "recipient_type", null: false
+    t.uuid "recipient_id", null: false
+    t.string "type", null: false
+    t.jsonb "params"
+    t.datetime "read_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["read_at"], name: "index_notifications_on_read_at"
+    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
   end
 
   create_table "organisation_publisher_preferences", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
---
+++ spec/factories/notifications.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :notification do
+    recipient { nil }
+    type { "" }
+    params { "" }
+    read_at { "2021-04-15 18:28:16" }
+  end
+end
