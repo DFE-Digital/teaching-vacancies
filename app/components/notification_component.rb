e558b8e8f6cfd089418ebe6f481891f4c41afc3a
Connor McQuillan <cpjmcquillan@gmail.com>
2021-04-19 11:09:16 +0100

    Add a notification component

---
+++ app/components/notification_component.rb
@@ -0,0 +1,19 @@
+class NotificationComponent < GovukComponent::Base
+  attr_reader :notification
+
+  def initialize(notification:, classes: [], html_attributes: {})
+    super(classes: classes, html_attributes: html_attributes)
+
+    @notification = notification
+  end
+
+  private
+
+  def default_classes
+    %w[notification-component]
+  end
+
+  def unread_tag
+    govuk_tag text: "new", colour: "blue"
+  end
+end
