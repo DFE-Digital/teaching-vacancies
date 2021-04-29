44510371b4e5b16a6ab4acad99aeeea8da6017cc
Connor McQuillan <cpjmcquillan@gmail.com>
2021-04-29 11:29:02 +0100

    Send uid with all email events and add a utm_campaign method for mailers

--- spec/services/email_event_spec.rb
+++ spec/services/email_event_spec.rb
@@ -1,12 +1,13 @@
 require "rails_helper"
 
 RSpec.describe EmailEvent do
-  subject { described_class.new(notify_template, email, jobseeker: jobseeker, publisher: publisher) }
+  subject { described_class.new(notify_template, email, uid, jobseeker: jobseeker, publisher: publisher) }
 
   let(:notify_template) { "test_template" }
   let(:email) { "test@email.com" }
   let(:jobseeker) { instance_double(Jobseeker, id: 1234, email: "test@email.com") }
   let(:publisher) { instance_double(Publisher, oid: 4321) }
+  let(:uid) { "#{DateTime.current.strftime('%Q')}-#{StringAnonymiser.new(email)}" }
 
   describe "#trigger" do
     let(:expected_data) do
@@ -14,6 +15,7 @@ RSpec.describe EmailEvent do
         type: :best_ever_email_event,
         occurred_at: "1999-12-31T23:59:59.000000Z",
         data: [
+          { key: "uid", value: uid },
           { key: "notify_template", value: notify_template },
           { key: "email_identifier", value: anonymised_form_of("test@email.com") },
           { key: "user_anonymised_jobseeker_id", value: anonymised_form_of("1234") },
