cb6873fb71aadd0350cd03bb133154d23715f80d
alexbowen <alexbowen123@googlemail.com>
2021-05-05 11:04:26 +0100

    do not apply links for expired vacancy

--- spec/system/jobseekers_can_apply_for_a_vacancy_spec.rb
+++ spec/system/jobseekers_can_apply_for_a_vacancy_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe "Jobseekers can apply for a vacancy" do
   scenario "it increments the get_more_info_counter in the background" do
     expect { click_on I18n.t("jobs.apply") }.to have_enqueued_job(PersistVacancyGetMoreInfoClickJob).with(vacancy.id)
   end
+
+  let(:expired_vacancy) do
+    create(:vacancy, :expired, :no_tv_applications,
+           application_link: "www.google.com", organisation_vacancies_attributes: [{ organisation: build(:school) }], expires_on: Date.current)
+  end
+
+  scenario "the application links are not present for expired vacancy" do
+    visit job_path(expired_vacancy)
+    expect(page).not_to have_link(I18n.t("jobs.apply", href: "http://www.google.com"))
+  end
 end
