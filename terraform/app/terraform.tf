f79c0faf662252e55d630861dc460d26e3ca3828
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2021-05-11 17:39:59 +0100

    temp

--- terraform/app/terraform.tf
+++ terraform/app/terraform.tf
@@ -8,13 +8,11 @@ For SSO authentication
 */
 
 provider "cloudfoundry" {
-  api_url           = var.paas_api_url
-  password          = var.paas_password != null ? var.paas_password : local.infra_secrets.cf_password
-  sso_passcode      = var.paas_sso_passcode != "" ? var.paas_sso_passcode : null
   store_tokens_path = "./tokens"
-  user              = var.paas_user != null ? var.paas_user : local.infra_secrets.cf_username
-
-
+  api_url           = var.paas_api_url
+  user              = var.paas_sso_passcode == null ? local.infra_secrets.cf_username : null
+  password          = var.paas_sso_passcode == null ? local.infra_secrets.cf_password : null
+  sso_passcode      = var.paas_sso_passcode
 }
 
 provider "statuscake" {
