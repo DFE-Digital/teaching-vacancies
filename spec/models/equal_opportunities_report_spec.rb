0ef9d5d368d96d649d7f195e42389a1331a531dd
David Mears <david.mears@digital.education.gov.uk>
2021-05-11 10:01:22 +0100

    Send equal opportunities report publication events to BigQuery
    As this data is sensitive, and could be deduced from matching up timestamps of the updated equal_opportunities_reports and job_applications, we will need to handle it separately and differently than we do normal entity create/update/destroy events, rather than sending equal_opportunities_reports to BigQuery whenever the report is updated (which is the same thing as 'whenever a job application is submitted').
    
    A new job runs every night after midnight. This works by triggering an event for every job listing that expired yesterday, but only if the listing has more than [threshold] applications, to preserve anonymity in the streamed events db.

--- spec/models/equal_opportunities_report_spec.rb
+++ spec/models/equal_opportunities_report_spec.rb
@@ -2,4 +2,10 @@ require "rails_helper"
 
 RSpec.describe EqualOpportunitiesReport do
   it { is_expected.to belong_to(:vacancy) }
+
+  describe "#trigger_event" do
+    it "triggers an event of the correct type" do
+      expect { described_class.new.trigger_event }.to have_triggered_event(:equal_opportunities_report_published).with_data({ table_name: "equal_opportunities_reports" })
+    end
+  end
 end
