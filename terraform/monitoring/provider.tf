bf49bb150d9d21aee38079dfc2893f639563646c
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2021-05-11 17:08:38 +0100

    Use new PaaS credentials

--- terraform/monitoring/provider.tf
+++ terraform/monitoring/provider.tf
@@ -1,8 +1,8 @@
 provider "cloudfoundry" {
   store_tokens_path = "./tokens"
   api_url           = local.paas_api_url
-  user              = var.paas_user
-  password          = var.paas_password
+  user              = var.paas_sso_passcode == null ? local.secrets.cf_username : null
+  password          = var.paas_sso_passcode == null ? local.secrets.cf_password : null
   sso_passcode      = var.paas_sso_passcode
 }
 
