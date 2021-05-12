4509f98779c2247a8381a771e43138d86359f1e1
alexbowen <alexbowen123@googlemail.com>
2021-05-12 12:12:04 +0100

    add variants for round 3 of cookie banner ab test

--- spec/components/ab_test/cookies_banner_component_spec.rb
+++ spec/components/ab_test/cookies_banner_component_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe AbTest::CookiesBannerComponent, type: :component do
   let(:create_path) { "/cp" }
   let(:reject_path) { "/rp" }
 
-  let!(:inline_component) { render_inline(described_class.new(create_path: create_path, reject_path: reject_path, preferences_path: preferences_path).with_variant(:default)) }
+  let!(:inline_component) { render_inline(described_class.new(create_path: create_path, reject_path: reject_path, preferences_path: preferences_path).with_variant(:top_static_default)) }
 
   it "renders the cookies banner component" do
     expect(inline_component.css(".cookies-banner-component").count).to eq(1)
