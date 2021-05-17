6dc748ed229777fc0aee07436dbc2960ed677ca0
alexbowen <alexbowen123@googlemail.com>
2021-05-17 17:34:09 +0100

    use gds print hide class where possible

--- app/components/review_component.rb
+++ app/components/review_component.rb
@@ -48,7 +48,7 @@ class ReviewComponent < GovukComponent::Base
     end
 
     def edit_link
-      govuk_link_to text, href, aria: { label: "#{text} #{title}" } if text && href
+      govuk_link_to text, href, aria: { label: "#{text} #{title}" }, classes: "govuk-!-display-none-print" if text && href
     end
   end
 end
