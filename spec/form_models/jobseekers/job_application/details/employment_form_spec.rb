f92fe9f51fee44029ebd4629efb6435766eb572e
Connor McQuillan <cpjmcquillan@gmail.com>
2021-05-20 13:52:42 +0100

    Rethink how we validate dates that use multiparameter attribute assignment

--- spec/form_models/jobseekers/job_application/details/employment_form_spec.rb
+++ spec/form_models/jobseekers/job_application/details/employment_form_spec.rb
@@ -7,40 +7,55 @@ RSpec.describe Jobseekers::JobApplication::Details::EmploymentForm, type: :model
   it { is_expected.to validate_presence_of(:organisation) }
   it { is_expected.to validate_presence_of(:job_title) }
   it { is_expected.to validate_presence_of(:main_duties) }
-  it { is_expected.to validate_presence_of(:started_on) }
   it { is_expected.to validate_inclusion_of(:current_role).in_array(%w[yes no]) }
 
-  context "when started_on is an invalid date" do
-    let(:params) { { "started_on(1i)" => "2021", "started_on(2i)" => "01", "started_on(3i)" => "100" } }
+  describe "#started_on" do
+    context "when started_on is blank" do
+      let(:params) { {} }
 
-    it "is invalid" do
-      expect(subject).not_to be_valid
-      expect(subject.errors.of_kind?(:started_on, :invalid)).to be true
+      it "is invalid" do
+        expect(subject).not_to be_valid
+        expect(subject.errors.of_kind?(:started_on, :blank)).to be true
+      end
+    end
+
+    context "when started_on is an invalid date" do
+      let(:params) { { "started_on(1i)" => "2021", "started_on(2i)" => "01", "started_on(3i)" => "100" } }
+
+      it "is invalid" do
+        expect(subject).not_to be_valid
+        expect(subject.errors.of_kind?(:started_on, :invalid)).to be true
+      end
+    end
+
+    context "when started_on is an incomplete date" do
+      let(:params) { { "started_on(3i)" => "1" } }
+
+      it "is invalid" do
+        expect(subject).not_to be_valid
+        expect(subject.errors.of_kind?(:started_on, :invalid)).to be true
+      end
+    end
+
+    context "when started_on is after today" do
+      let(:params) { { "started_on(1i)" => "2121", "started_on(2i)" => "01", "started_on(3i)" => "01" } }
+
+      it "is invalid" do
+        expect(subject).not_to be_valid
+        expect(subject.errors.of_kind?(:started_on, :before)).to be true
+      end
     end
   end
 
-  context "when started_on is an incomplete date" do
-    let(:params) { { "started_on(3i)" => "1" } }
+  describe "#ended_on" do
+    context "when ended_on is blank" do
+      let(:params) { { current_role: "no" } }
 
-    it "is invalid" do
-      expect(subject).not_to be_valid
-      expect(subject.errors.of_kind?(:started_on, :invalid)).to be true
+      it "is invalid" do
+        expect(subject).not_to be_valid
+        expect(subject.errors.of_kind?(:ended_on, :blank)).to be true
+      end
     end
-  end
-
-  context "when started_on is not in the past" do
-    let(:params) { { "started_on(1i)" => "2121", "started_on(2i)" => "01", "started_on(3i)" => "01" } }
-
-    it "is invalid" do
-      expect(subject).not_to be_valid
-      expect(subject.errors.of_kind?(:started_on, :not_in_the_past)).to be true
-    end
-  end
-
-  context "when current_role is no" do
-    let(:params) { { current_role: "no" } }
-
-    it { is_expected.to validate_presence_of(:ended_on) }
 
     context "when ended_on is invalid" do
       let(:params) { { current_role: "no", "ended_on(1i)" => "2021", "ended_on(2i)" => "01", "ended_on(3i)" => "100" } }
@@ -64,7 +79,7 @@ RSpec.describe Jobseekers::JobApplication::Details::EmploymentForm, type: :model
       end
     end
 
-    context "when ended_on is not in the past" do
+    context "when ended_on is after today" do
       let(:params) do
         { current_role: "no",
           "started_on(1i)" => "2021", "started_on(2i)" => "01", "started_on(3i)" => "01",
@@ -73,7 +88,7 @@ RSpec.describe Jobseekers::JobApplication::Details::EmploymentForm, type: :model
 
       it "is invalid" do
         expect(subject).not_to be_valid
-        expect(subject.errors.of_kind?(:ended_on, :not_in_the_past)).to be true
+        expect(subject.errors.of_kind?(:ended_on, :before)).to be true
       end
     end
 
@@ -86,7 +101,7 @@ RSpec.describe Jobseekers::JobApplication::Details::EmploymentForm, type: :model
 
       it "is invalid" do
         expect(subject).not_to be_valid
-        expect(subject.errors.of_kind?(:ended_on, :before_started_on)).to be true
+        expect(subject.errors.of_kind?(:ended_on, :after)).to be true
       end
     end
   end
