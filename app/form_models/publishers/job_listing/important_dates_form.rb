ba0078147d1813f355a40f881576528c14244797
Connor McQuillan <cpjmcquillan@gmail.com>
2021-05-24 08:40:42 +0100

    Refactor vacancy dates.
    - Use multiparameter attribute assignment for dates
    - Use improved date validation
    - Remove redundant expires_on field

--- app/form_models/publishers/job_listing/important_dates_form.rb
+++ app/form_models/publishers/job_listing/important_dates_form.rb
@@ -1,18 +1,23 @@
 class Publishers::JobListing::ImportantDatesForm < Publishers::JobListing::VacancyForm
-  delegate :published?, :status, to: :vacancy
+  include ActiveRecord::AttributeAssignment
+  include DateAttributeAssignment
 
-  attr_accessor :expires_at_hh, :expires_at_mm, :expires_at_meridiem, :starts_asap, :starts_on, :publish_on, :expires_on, :expires_at
+  EXPIRY_TIME_OPTIONS = %w[9:00 12:00 17:00 23:59].freeze
 
-  include DatesHelper
+  delegate :published?, to: :vacancy
 
-  include VacancyImportantDateValidations
-  include VacancyExpiresAtFieldValidations
+  attr_accessor :expiry_time, :starts_asap
+  attr_reader :expires_at, :publish_on, :starts_on
+
+  validates :publish_on, date: { on_or_after: :today }, unless: proc { published? }
+  validates :expires_at, date: { on_or_after: :now, after: :publish_on }
+  validates :expiry_time, inclusion: { in: EXPIRY_TIME_OPTIONS }
+  validates :starts_on, date: { on_or_after: :today, after: :expires_at }, allow_blank: true,
+                        if: proc { starts_asap == "0" }
+  validate :starts_on_and_starts_asap_not_present
 
   def initialize(params, vacancy)
-    @params = params
-    @expires_at_hh = params.delete(:expires_at_hh) || params[:expires_at]&.strftime("%-l")
-    @expires_at_mm = params.delete(:expires_at_mm) || params[:expires_at]&.strftime("%-M")
-    @expires_at_meridiem = params.delete(:expires_at_meridiem) || params[:expires_at]&.strftime("%P")
+    @expiry_time = params[:expiry_time] || vacancy.expires_at&.strftime("%k:%M")&.strip
 
     super(params, vacancy)
   end
@@ -22,19 +27,31 @@ class Publishers::JobListing::ImportantDatesForm < Publishers::JobListing::Vacan
   end
 
   def params_to_save
-    params_with_expires_at = @params
+    {
+      publish_on: publish_on,
+      expires_at: expires_at,
+      starts_on: (starts_on unless starts_asap == "true"),
+      starts_asap: starts_asap,
+      completed_step: completed_step,
+    }.compact
+  end
 
-    expires_at_attr = {
-      day: expires_on&.day,
-      month: expires_on&.month,
-      year: expires_on&.year,
-      hour: expires_at_hh,
-      min: expires_at_mm,
-      meridiem: expires_at_meridiem,
-    }
-    expires_at = compose_expires_at(expires_at_attr)
-    params_with_expires_at[:expires_at] = expires_at unless expires_at.nil?
+  def expires_at=(value)
+    expires_on = date_from_multiparameter_hash(value)
+    @expires_at = datetime_from_date_and_time(expires_on, expiry_time)
+  end
 
-    params_with_expires_at
+  def publish_on=(value)
+    @publish_on = date_from_multiparameter_hash(value)
+  end
+
+  def starts_on=(value)
+    @starts_on = date_from_multiparameter_hash(value)
+  end
+
+  private
+
+  def starts_on_and_starts_asap_not_present
+    errors.add(:starts_on, :date_and_asap) if starts_on.present? && starts_asap == "true"
   end
 end
--- app/form_models/publishers/job_listing/vacancy_form.rb
+++ app/form_models/publishers/job_listing/vacancy_form.rb
@@ -14,16 +14,4 @@ class Publishers::JobListing::VacancyForm
   def params_to_save
     params.except(:current_organisation)
   end
-
-  # This method is only necessary for forms with specific error messages for date inputs.
-  def complete_and_valid?
-    existing_errors = errors.dup
-    is_valid = valid?
-    existing_errors.messages.each do |field, error_array|
-      error_array.each do |error|
-        errors.add(field, error)
-      end
-    end
-    errors.none? && is_valid
-  end
 end
