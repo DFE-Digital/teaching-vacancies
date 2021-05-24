0ce866f7a4ef9ffa841e5d72b2e51d310b898b15
Connor McQuillan <cpjmcquillan@gmail.com>
2021-05-24 08:40:42 +0100

    Refactor vacancy dates.
    - Use multiparameter attribute assignment for dates
    - Use improved date validation
    - Remove redundant expires_on field

--- app/helpers/dates_helper.rb
+++ app/helpers/dates_helper.rb
@@ -4,10 +4,6 @@ module DatesHelper
   def format_date(date, format = :default)
     return "No date given" if date.nil?
 
-    unless Date::DATE_FORMATS.include?(format)
-      raise FormatDateError, date_format_error_message(format, Date::DATE_FORMATS.keys.join(" "))
-    end
-
     date.to_s(format).lstrip
   end
 
@@ -25,40 +21,5 @@ module DatesHelper
     else
       date.strftime("%-d %B")
     end
-  end
-
-  def date_format_error_message(format, date_formats)
-    "Unknown format: #{format} should be one of #{date_formats}"
-  end
-
-  def compose_expires_at(args)
-    return nil if [args[:hour],
-                   args[:min],
-                   args[:meridiem]].any? { |attr| attr.to_s.empty? }
-
-    return nil unless in_range?(args[:hour], 1, 12)
-    return nil unless in_range?(args[:min], 0, 59)
-
-    expires_at_string = "#{args[:day]}-#{args[:month]}-#{args[:year]}" \
-                         " #{args[:hour]}:#{args[:min]} #{args[:meridiem]}"
-
-    Time.zone.parse(expires_at_string)
-  end
-
-  def format_datetime_with_seconds(str)
-    return nil if str.nil?
-
-    date_time = Time.zone.parse(str)
-    "#{date_time}:#{date_time.strftime('%S')}"
-  end
-
-  private
-
-  def in_range?(value, min, max)
-    number?(value) && value.to_i >= min && value.to_i <= max
-  end
-
-  def number?(value)
-    /\A[+-]?\d+\z/.match?(value.to_s)
   end
 end
