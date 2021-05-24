2afa4efd2b91e966d59fd1726e9e3c184fbfde42
Connor McQuillan <cpjmcquillan@gmail.com>
2021-05-24 08:40:42 +0100

    Refactor vacancy dates.
    - Use multiparameter attribute assignment for dates
    - Use improved date validation
    - Remove redundant expires_on field

--- spec/controllers/publishers/vacancies/base_controller_spec.rb
+++ spec/controllers/publishers/vacancies/base_controller_spec.rb
@@ -44,46 +44,4 @@ RSpec.describe Publishers::Vacancies::BaseController do
       end
     end
   end
-
-  describe "#convert_multiparameter_attributes_to_dates" do
-    let(:params) do
-      { test_form: dates }
-    end
-
-    before do
-      allow(controller).to receive(:params).and_return(params)
-    end
-
-    context "date present in params" do
-      let(:dates) { { 'starts_on(3i)': "01", 'starts_on(2i)': "01", 'starts_on(1i)': "2020" } }
-
-      it "converts date params to a Date object" do
-        subject.convert_multiparameter_attributes_to_dates(:test_form, [:starts_on])
-        expect(controller.params[:test_form][:starts_on]).to eq(Date.parse("2020-01-01"))
-      end
-    end
-
-    context "invalid date in params" do
-      let(:dates) { { 'starts_on(3i)': "100", 'starts_on(2i)': "", 'starts_on(1i)': "2020" } }
-
-      it "the form object has an invalid date error" do
-        expect(subject.convert_multiparameter_attributes_to_dates(:test_form, [:starts_on])[:starts_on]).to eq(
-          I18n.t("activerecord.errors.models.vacancy.attributes.starts_on.invalid"),
-        )
-      end
-
-      it "does not convert date params to a Date object" do
-        expect(controller.params[:test_form][:starts_on]).to eq(nil)
-      end
-    end
-
-    context "date not present in params" do
-      let(:dates) { { 'starts_on(3i)': "", 'starts_on(2i)': "", 'starts_on(1i)': "" } }
-
-      it "does not convert date params to a Date object" do
-        subject.convert_multiparameter_attributes_to_dates(:test_form, [:starts_on])
-        expect(controller.params[:test_form][:starts_on]).to eq(nil)
-      end
-    end
-  end
 end
