aaac51b2240b3bd8832bac085ce489c73e055885
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-05-25 08:02:13 +0100

    Ask age in equal opportunities form

--- app/form_models/jobseekers/job_application/equal_opportunities_form.rb
+++ app/form_models/jobseekers/job_application/equal_opportunities_form.rb
@@ -1,12 +1,13 @@
 class Jobseekers::JobApplication::EqualOpportunitiesForm
   include ActiveModel::Model
 
-  ATTRIBUTES = %i[disability gender gender_description orientation orientation_description
+  ATTRIBUTES = %i[disability age gender gender_description orientation orientation_description
                   ethnicity ethnicity_description religion religion_description].freeze
 
   attr_accessor(*ATTRIBUTES)
 
   validates :disability, inclusion: { in: %w[no prefer_not_to_say yes] }
+  validates :age, inclusion: { in: %w[under_twenty_five twenty_five_to_twenty_nine thirty_to_thirty_nine forty_to_forty_nine fifty_to_fifty_nine sixty_and_over prefer_not_to_say] }
   validates :gender, inclusion: { in: %w[man other prefer_not_to_say woman] }
   validates :orientation, inclusion: { in: %w[bisexual gay_or_lesbian heterosexual other prefer_not_to_say] }
   validates :ethnicity, inclusion: { in: %w[asian black mixed other prefer_not_to_say white] }
