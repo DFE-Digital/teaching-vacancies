fef598d154582c9d84905643206034828f34bb23
Connor McQuillan <cpjmcquillan@gmail.com>
2021-05-27 13:17:39 +0100

    Implement a link helper for links that open in new tab
    - Append "(opens in new tab)" to link text
    - Add target="_blank" to link
    - Add rel="noreferrer noopener" to link

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -4,4 +4,8 @@ module LinkHelper
 
     "govuk-header__navigation-item"
   end
+
+  def open_in_new_tab_link_to(text, href, **kwargs)
+    govuk_link_to("#{text} (opens in new tab)", href, target: "_blank", rel: "noreferrer noopener", **kwargs)
+  end
 end
