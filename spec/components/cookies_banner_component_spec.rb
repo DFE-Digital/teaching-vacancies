77a510e668e4b232cb47b9133e6d799a05b2727c
alexbowen <alexbowen123@googlemail.com>
2021-06-03 09:58:43 +0100

    use gds bottom sticky variant of cookies vanner

---
+++ spec/components/cookies_banner_component_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe CookiesBannerComponent, type: :component do
+  let(:preferences_path) { "/pp" }
+  let(:create_path) { "/cp" }
+  let(:reject_path) { "/rp" }
+
+  let(:kwargs) { { create_path: create_path, reject_path: reject_path, preferences_path: preferences_path } }
+
+  let!(:inline_component) { render_inline(described_class.new(**kwargs)) }
+
+  it_behaves_like "a component that accepts custom classes"
+  it_behaves_like "a component that accepts custom HTML attributes"
+  
+  it "renders the cookies banner component" do
+    expect(inline_component.css(".cookies-banner-component").count).to eq(1)
+  end
+
+  it "renders 3 actions for user" do
+    expect(inline_component.css(".button_to").count).to eq(2)
+    expect(inline_component.css('.button_to:first-child').attribute("action").value).to eq(create_path)
+    expect(inline_component.css('.button_to:nth-child(2)').attribute("action").value).to eq(reject_path)
+
+    expect(inline_component.css(".govuk-link").count).to eq(1)
+    expect(inline_component.css('.govuk-link').attribute("href").value).to eq(preferences_path)
+  end
+end
