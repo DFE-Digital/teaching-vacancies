8d247fdb087eba967006f4d0cdb1481aa1b57177
David Mears <david.mears@digital.education.gov.uk>
2021-06-09 17:53:08 +0100

    Anonymise vacancy ids in request events
    In order to match up the events with records, we need to use the anonymised version.
    
    - Allow click_events to have further event data (from a click_event_data param)
    to include in the event. The use case here is to match up a 'save vacancy' click
    event with the vacancy in question. We have to explicitly permit such parameters
    in order to convert the click_event_data param into a hash for consumption by
    Event.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,7 +1,10 @@
 class DocumentsController < ApplicationController
   def show
     document = Document.find(params[:id])
-    request_event.trigger(:vacancy_document_downloaded, vacancy_id: document.vacancy.id, document_id: document.id, filename: document.name)
+    request_event.trigger(:vacancy_document_downloaded,
+                          vacancy_id: StringAnonymiser.new(document.vacancy.id),
+                          document_id: StringAnonymiser.new(document.id),
+                          filename: document.name)
     redirect_to(document.download_url)
   end
 end
