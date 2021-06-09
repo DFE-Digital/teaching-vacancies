a034dcc8d329e13487aac7db844b3c59a4775024
josephhull676 <josephhull676@yahoo.co.uk>
2021-06-09 12:23:00 +0100

    Add user participation question to further feedback form

--- app/form_models/jobseekers/job_alert_further_feedback_form.rb
+++ app/form_models/jobseekers/job_alert_further_feedback_form.rb
@@ -1,7 +1,10 @@
 class Jobseekers::JobAlertFurtherFeedbackForm
   include ActiveModel::Model
 
-  attr_accessor :comment
+  attr_accessor :comment, :email, :user_participation_response
 
   validates :comment, presence: true, length: { maximum: 1200 }
+  validates :email, presence: true, if: -> { user_participation_response == "interested" }
+  validates :email, format: { with: Devise.email_regexp }, if: -> { email.present? }
+  validates :user_participation_response, inclusion: { in: Feedback.user_participation_responses.keys }
 end
