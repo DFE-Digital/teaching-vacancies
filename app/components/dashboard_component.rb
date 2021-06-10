aca6d1c5f7e14d374f425790defced88bf568ea3
alexbowen <alexbowen123@googlemail.com>
2021-06-10 17:55:47 +0100

    update dashboard component structure and add test

--- app/components/dashboard_component.rb
+++ app/components/dashboard_component.rb
@@ -1,9 +1,22 @@
-class DashboardComponent < ViewComponent::Base
-  attr_reader :heading, :navigation_items, :panel, :link
+class DashboardComponent < GovukComponent::Base
+  attr_reader :heading, :link
 
-  def initialize(heading:, navigation_items: [], link: nil)
+  def initialize(heading:, link: nil, classes: [], html_attributes: {})
+    super(classes: classes, html_attributes: html_attributes)
+
     @heading = heading
-    @navigation_items = navigation_items
     @link = link
+  end
+
+  renders_many :navigation_items, lambda { |item:|
+    tag.li class: "dashboard-component-navigation__item" do
+      item
+    end
+  }
+
+  private
+
+  def default_classes
+    %w[dashboard-component]
   end
 end
