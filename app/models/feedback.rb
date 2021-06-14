97f0a3f5625df21822f213013dcc87d5cc3c5793
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-06-14 09:23:48 +0100

    Add close jobseeker account feature

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,9 +1,10 @@
 class Feedback < ApplicationRecord
-  enum feedback_type: { jobseeker_account: 0, general: 1, job_alert: 2, unsubscribe: 3, vacancy_publisher: 4, application: 5 }
+  enum feedback_type: { jobseeker_account: 0, general: 1, job_alert: 2, unsubscribe: 3, vacancy_publisher: 4, application: 5, close_account: 6 }
   enum user_participation_response: { interested: 0, uninterested: 1 }
   enum rating: { highly_satisfied: 0, somewhat_satisfied: 1, neither: 2, somewhat_dissatisfied: 3, highly_dissatisfied: 4 }
   enum unsubscribe_reason: { not_relevant: 0, job_found: 1, circumstances_change: 2, other_reason: 3 }
   enum visit_purpose: { find_teaching_job: 1, list_teaching_job: 2, other_purpose: 0 }
+  enum close_account_reason: { too_many_emails: 0, not_getting_any_value: 1, not_looking_for_job: 2, other_close_account_reason: 3 }
 
   validate :feedback_type, :presence
 end
---
+++ spec/system/jobseekers_can_close_and_reactivate_account_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Jobseekers can close and reactivate their account" do
+  let(:jobseeker) { create(:jobseeker) }
+  let(:organisation) { create(:school) }
+  let(:vacancy) { create(:vacancy, organisation_vacancies_attributes: [{ organisation: organisation }]) }
+  let!(:job_application) { create(:job_application, :reviewed, jobseeker: jobseeker, vacancy: vacancy) }
+  let!(:subscription) { create(:subscription, email: jobseeker.email, active: true) }
+
+  before { login_as(jobseeker, scope: :jobseeker) }
+
+  it "allows closing and reactivating a jobseeker account" do
+    visit jobseekers_account_path
+
+    click_on I18n.t("jobseekers.accounts.show.close_account")
+
+    choose "Other"
+    fill_in "jobseekers_close_account_feedback_form[close_account_reason_comment]", with: "Worst service ever!!"
+
+    click_on I18n.t("buttons.continue")
+
+    expect(page).to have_content(I18n.t("jobseekers.registrations.destroy.success"))
+
+    within(".navbar-component") do
+      click_link I18n.t("buttons.sign_in")
+    end
+
+    sign_in_jobseeker(email: jobseeker.email, password: jobseeker.password)
+
+    expect(page).to have_content(I18n.t("devise.sessions.signed_in"))
+  end
+end
