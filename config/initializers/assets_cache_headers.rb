d40e9e6071c12d39d41e1abf3c2af0f3eb921aef
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2021-06-14 10:56:08 +0100

    rails - cloudfront

---
+++ config/initializers/assets_cache_headers.rb
@@ -0,0 +1,7 @@
+# This will affect assets in /public, /packs e.g. webpacker assets.
+# We set cache headers to instruct Cloudfront to cache assets for one year
+
+Rails.application.config.public_file_server.headers = {
+  "Cache-Control" => "public, max-age=31536000",
+  "Expires" => 1.year.from_now.to_formatted_s(:rfc822),
+}
