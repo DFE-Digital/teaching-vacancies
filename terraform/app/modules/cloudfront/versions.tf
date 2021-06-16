6e4c4642cba8faec27846cb1331938baaf2c83c0
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2021-06-16 17:39:03 +0100

    Update terraform AWS provider
    Required for the new cloudfront behavior to avoid error:
    InvalidArgument: The parameter ForwardedValues cannot be used when a cache policy is associated to the cache behavior.
    
    The provider in common and monitoring was updated as well for
    consistency

--- terraform/app/modules/cloudfront/versions.tf
+++ terraform/app/modules/cloudfront/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.28.0"
+      source                = "hashicorp/aws"
+      version               = ">= 3.45.0"
       configuration_aliases = [aws.aws_us_east_1]
     }
   }
