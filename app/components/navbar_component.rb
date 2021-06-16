d36662a973ed22c17703d4775ca9014a4aae81ea
alexbowen <alexbowen123@googlemail.com>
2021-06-16 18:55:06 +0100

    refactor navbar component to partial

--- app/components/navbar_component.rb
+++ app/components/navbar_component.rb
@@ -1,8 +1,21 @@
-class NavbarComponent < ViewComponent::Base
+class NavbarComponent < GovukComponent::Base
   delegate :active_link_class, to: :helpers
-  delegate :current_organisation, to: :helpers
-  delegate :current_publisher, to: :helpers
   delegate :organisation_type_basic, to: :helpers
-  delegate :jobseeker_signed_in?, to: :helpers
-  delegate :publisher_signed_in?, to: :helpers
+
+  attr_accessor :publisher_signed_in, :jobseeker_signed_in, :current_organisation, :current_publisher
+
+  def initialize(publisher_signed_in:, jobseeker_signed_in:, current_organisation:, current_publisher:, classes: [], html_attributes: {})
+    super(classes: classes, html_attributes: html_attributes)
+
+    @publisher_signed_in = publisher_signed_in
+    @jobseeker_signed_in = jobseeker_signed_in
+    @current_organisation = current_organisation
+    @current_publisher = current_publisher
+  end
+
+  private
+
+  def default_classes
+    %w[navbar-component]
+  end
 end
