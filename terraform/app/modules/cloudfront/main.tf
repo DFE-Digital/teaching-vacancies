c8603c930a3ab28727eb784dfb4aff17ba33778a
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2021-06-16 17:27:12 +0100

    Remove /api and /assets cloudfront behaviors
    They are not used

--- terraform/app/modules/cloudfront/main.tf
+++ terraform/app/modules/cloudfront/main.tf
@@ -86,53 +86,6 @@ resource "aws_cloudfront_distribution" "default" {
   }
 
   ordered_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${var.service_name}-${var.environment}-default-origin"
-
-    path_pattern = "/assets/*"
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Host"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl     = 0
-    default_ttl = 3600
-    max_ttl     = 86400
-
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  ordered_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${var.service_name}-${var.environment}-default-origin"
-
-    path_pattern = "/api/*"
-
-    forwarded_values {
-      query_string = true
-      headers      = ["Authorization", "Host"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl     = 900
-    default_ttl = 3600
-    max_ttl     = 86400
-
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-
-  ordered_cache_behavior {
     allowed_methods = [
       "GET",
       "HEAD",
