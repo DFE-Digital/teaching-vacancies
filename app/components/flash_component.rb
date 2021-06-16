d7ca64806f0896f67048c4155b224eca7acddfc1
alexbowen <alexbowen123@googlemail.com>
2021-06-16 16:37:02 +0100

    refactor searchable collection to Govuk standard

--- app/components/flash_component.rb
+++ app/components/flash_component.rb
@@ -1,25 +1,25 @@
 class FlashComponent < GovukComponent::Base
-  attr_reader :variant, :message
+  attr_reader :variant_name, :message
 
   VARIANTS = %w[notice success warning alert].freeze
 
-  def initialize(variant:, message:, classes: [], html_attributes: {})
+  def initialize(variant_name:, message:, classes: [], html_attributes: {})
     super(classes: classes, html_attributes: html_attributes.merge(default_html_attributes))
 
-    @variant = variant
+    @variant_name = variant_name
     @message = message
   end
 
   def render?
-    variant.in?(VARIANTS)
+    variant_name.in?(VARIANTS)
   end
 
   def variant_class
-    "flash-component--#{variant}"
+    "flash-component--#{variant_name}"
   end
 
   def icon_class
-    "icon icon--left icon--#{variant}"
+    "icon icon--left icon--#{variant_name}"
   end
 
   private
