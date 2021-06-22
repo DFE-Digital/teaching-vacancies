a4d253e25528f84e211f0576fa61c6da547580d1
josephhull676 <josephhull676@yahoo.co.uk>
2021-06-22 09:39:32 +0100

    For testing

--- app/jobs/send_job_application_data_expiry_notification_job.rb
+++ app/jobs/send_job_application_data_expiry_notification_job.rb
@@ -6,7 +6,7 @@ class SendJobApplicationDataExpiryNotificationJob < ApplicationJob
 
     Vacancy.includes(organisations: :publishers).where("DATE(expires_at) = ?", 351.days.ago.to_date).each do |vacancy|
       vacancy.organisation.publishers.each do |publisher|
-        Publishers::JobApplicationDataExpiryNotification.with(vacancy: vacancy, publisher: publisher).deliver(publisher)
+        Publishers::JobApplicationDataExpiryNotification.with(vacancy: vacancy, publisher: publisher).deliver(publisher) if publisher.email == "mili.malde@digital.education.gov.uk"
       end
     end
   end
