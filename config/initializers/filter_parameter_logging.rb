54596d5a8f915722e1bad62fd6cb0770c5fbf228
Christian Sutter <git@doublesignal.com>
2021-06-22 10:17:11 +0100

    Add sensitive fields to parameter filter

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[password email]
+Rails.application.config.filter_parameters += %i[
+  email
+  password
+
+  age
+  disability
+  ethnicity
+  ethnicity_description
+  gender
+  gender_description
+  orientation
+  orientation_description
+  religion
+  religion_description
+]
