c355ff93eea693b529232fda99a5aa61963abc2f
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2021-06-23 11:43:56 +0100

    Change Cloudfront cached path for attachment
    This changed effect the cached pattern from `/attachment` to `/attachment/*`
    It’s to make it more accurate and also more consistent with /packs/*

--- terraform/app/modules/cloudfront/variables.tf
+++ terraform/app/modules/cloudfront/variables.tf
@@ -30,7 +30,7 @@ locals {
   cloudfront_aliases_cnames                              = [for zone in var.route53_zones : "${var.route53_cname_record}.${zone}"]
   cloudfront_aliases                                     = concat(var.route53_a_records, local.cloudfront_aliases_cnames)
   cloudfront_viewer_certificate_minimum_protocol_version = "TLSv1.2_2018"
-  cloudfront_cached_paths                                = ["/packs/*", "/attachment*"]
+  cloudfront_cached_paths                                = ["/packs/*", "/attachment/*"]
   cloudfront_custom_response = {
     404 = { ttl = "10" },
     500 = { ttl = "60", response_code = "500", page_path = "${var.offline_bucket_origin_path}/index.html" },
