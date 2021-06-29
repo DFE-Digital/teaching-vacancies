f957f0a83decb1b1367880a79c65d6df40493cfe
Christian Sutter <git@doublesignal.com>
2021-06-29 11:40:30 +0100

    Improve application caching behaviour
    - Stop sending overly restrictive `Cache-Control` header
    - Fix attachments path pattern in Cloudfront TF config
    - Move file server cache configuration from initializer into
      production environment config

--- config/initializers/assets_cache_headers.rb
+++
@@ -1,6 +0,0 @@
-# This will affect assets in /public, /packs e.g. webpacker assets.
-# We set cache headers to instruct Cloudfront to cache assets for one year
-
-Rails.application.config.public_file_server.headers = {
-  "Cache-Control" => "public, max-age=31536000",
-}
