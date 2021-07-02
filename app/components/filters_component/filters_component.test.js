b85bd060fb4957b4332db08800233d2354f7b10d
alexbowen <alexbowen123@googlemail.com>
2021-07-02 15:16:00 +0100

    rework of filters ui mainly to remove accordions

--- app/components/filters_component/filters_component.test.js
+++ app/components/filters_component/filters_component.test.js
@@ -1,6 +1,5 @@
 import filterGroup, {
   init,
-  isFormAutoSubmitEnabled,
   addRemoveFilterEvent,
   addRemoveAllFiltersEvent,
   removeFilterHandler,
@@ -10,20 +9,9 @@ import filterGroup, {
   getFilterCheckboxesInGroup,
   unCheckCheckbox,
   getFilterGroups,
-  addFilterChangeEvent,
-  getSubmitButton,
-  displayOpenOrCloseText,
-  openOrCloseAllSectionsHandler,
-  openOrCloseAllSections,
-  filterChangeHandler,
   CHECKBOX_CLASS_SELECTOR,
   CHECKBOX_GROUP_CLASS_SELECTOR,
-  CLOSE_ALL_TEXT,
-  OPEN_ALL_TEXT,
-  ACCORDION_SECTION_CLASS_SELECTOR,
-  ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR,
   REMOVE_FILTER_CLASS_SELECTOR,
-  addUpdateOpenOrCloseEvent,
 } from './filters_component';
 
 describe('filterGroup', () => {
@@ -31,7 +19,6 @@ describe('filterGroup', () => {
 
   beforeEach(() => {
     jest.resetAllMocks();
-
     onRemove = jest.fn();
   });
 
@@ -43,49 +30,24 @@ describe('filterGroup', () => {
       filterGroup.addRemoveAllFiltersEvent = jest.fn();
       const addRemoveAllFiltersEventMock = jest.spyOn(filterGroup, 'addRemoveAllFiltersEvent');
 
-      filterGroup.addUpdateOpenOrCloseEvent = jest.fn();
-      const addUpdateOpenOrCloseEventMock = jest.spyOn(filterGroup, 'addUpdateOpenOrCloseEvent');
-
-      document.body.innerHTML = `<form data-auto-submit="true"><div>
+      document.body.innerHTML = `<form data-auto-submit="true"><div class='filters-component'>
 <h2 id="filters-component-show-mobile"></h2>
 <button class="${REMOVE_FILTER_CLASS_SELECTOR}">remove</button>
 <button class="${REMOVE_FILTER_CLASS_SELECTOR}">remove</button>
 <button id="filters-component-clear-all">remove</button>
 <button id="filters-component-close-all">remove</button>
 </div>
-<div class="${ACCORDION_SECTION_CLASS_SELECTOR}"><div class="govuk-accordion__section-header"><h3 class="heading"><button class="govuk-accordion__section-button"></button></h3></div></div>
-<div class="${ACCORDION_SECTION_CLASS_SELECTOR}"><div class="govuk-accordion__section-header"><h3 class="heading"><button class="govuk-accordion__section-button"></button></h3></div></div>
 </form>`;
 
       init(
-        ACCORDION_SECTION_CLASS_SELECTOR,
         REMOVE_FILTER_CLASS_SELECTOR,
         'filters-component-clear-all',
-        'filters-component-close-all',
+        'filters-component-close-panel',
         'filters-component-show-mobile',
-        'govuk-accordion__section-header',
       );
 
       expect(addRemoveFilterEventMock).toHaveBeenCalledTimes(2);
       expect(addRemoveAllFiltersEventMock).toHaveBeenCalledTimes(1);
-      expect(addUpdateOpenOrCloseEventMock).toHaveBeenCalledTimes(2);
-    });
-  });
-
-  describe('isFormAutoSubmitEnabled', () => {
-    test('returns true if the auto submit data attribute has been added to the form', () => {
-      document.body.innerHTML = '<form data-auto-submit="true"><div class="filter-group"></div></form>';
-      expect(isFormAutoSubmitEnabled('filter-group')).toBe('true');
-    });
-
-    test('returns false if the auto submit data attribute has NOT been added to the form', () => {
-      document.body.innerHTML = '<form><div class="filter-group"></div></form>';
-      expect(isFormAutoSubmitEnabled('filter-group')).toBeFalsy();
-    });
-
-    test('returns false if the element in form doesnt exist', () => {
-      document.body.innerHTML = '<form><div></div></form>';
-      expect(isFormAutoSubmitEnabled('filter-group')).toBeFalsy();
     });
   });
 
@@ -128,11 +90,6 @@ describe('filterGroup', () => {
       removeFilterHandler(group, 'filter-key', onRemove);
       expect(checkbox.checked).toBe(false);
     });
-
-    test('calls the supplied callback', () => {
-      removeFilterHandler(group, 'filter-key', onRemove);
-      expect(onRemove).toHaveBeenCalled();
-    });
   });
 
   describe('removeAllFiltersHandler', () => {
@@ -146,11 +103,6 @@ describe('filterGroup', () => {
     test('removes checked attribute for all filter checkboxes', () => {
       removeAllFiltersHandler(onRemove);
       Array.from(document.getElementsByClassName(CHECKBOX_CLASS_SELECTOR)).forEach((checkbox) => expect(checkbox.checked).toBe(false));
-    });
-
-    test('calls the supplied callback', () => {
-      removeAllFiltersHandler(onRemove);
-      expect(onRemove).toHaveBeenCalled();
     });
   });
 
@@ -201,167 +153,6 @@ describe('filterGroup', () => {
       document.body.innerHTML = `<div id="group"><input class="${CHECKBOX_CLASS_SELECTOR}" /><input value="y" /><input class="${CHECKBOX_CLASS_SELECTOR}" /></div>`;
       const group = document.getElementById('group');
       expect(getFilterCheckboxesInGroup(group).length).toBe(2);
-    });
-  });
-
-  describe('addFilterChangeEvent', () => {
-    test('adds event listener to element that calls handler with correct arguments', () => {
-      filterGroup.filterChangeHandler = jest.fn();
-      const filterChangeHandlerMock = jest.spyOn(filterGroup, 'filterChangeHandler');
-
-      document.body.innerHTML = '<div id="test-group" class="group"></div><div class="group"></div>';
-      const groups = document.getElementsByClassName('group');
-      const group = document.getElementById('test-group');
-
-      addFilterChangeEvent(groups);
-      group.dispatchEvent(new Event('click'));
-
-      expect(filterChangeHandlerMock).toHaveBeenCalledWith(group);
-    });
-  });
-
-  describe('filterChangeHandler', () => {
-    test('submits the form only if a filter checkbox is clicked', () => {
-      document.body.innerHTML = `<form data-auto-submit="true">
-  <div class="${ACCORDION_SECTION_CLASS_SELECTOR}">
-  <input type="text" id="should-submit" class="${CHECKBOX_CLASS_SELECTOR}" />
-  <span id="should-not-submit">abc</span>
-  </div>
-  <input type="submit" id="submit-button" />
-  </form>`;
-
-      const submitButton = document.getElementById('submit-button');
-      const shouldNotSubmit = document.getElementById('should-not-submit');
-      const shouldSubmit = document.getElementById('should-submit');
-
-      submitButton.click = jest.fn();
-      const submitMock = jest.spyOn(submitButton, 'click');
-
-      filterChangeHandler(shouldNotSubmit);
-      expect(submitMock).not.toHaveBeenCalled();
-
-      filterChangeHandler(shouldSubmit);
-      expect(submitMock).toHaveBeenCalled();
-    });
-  });
-
-  describe('getSubmitButton', () => {
-    test('finds and returns the DOM element of the submit button for the form thst supplied element is in', () => {
-      document.body.innerHTML = '<form><div><input type="checkbox" id="test-element" /></div><div><input type="submit" id="find-this" /></div></form>';
-
-      const submitButton = document.getElementById('find-this');
-      const found = getSubmitButton(document.getElementById('test-element'));
-
-      expect(found).toBe(submitButton);
-    });
-  });
-
-  describe('openOrCloseAllSectionsHandler', () => {
-    test('removes the class selector from all filter groups that makes them visible', () => {
-      document.body.innerHTML = `<div class="govuk-accordion__section ${ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR}"></div>
-      <div class="govuk-accordion__section ${ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR}"></div>
-      <div class="${ACCORDION_SECTION_CLASS_SELECTOR}"></div>`;
-
-      const event = { preventDefault: jest.fn(), target: { innerText: CLOSE_ALL_TEXT } };
-      const dontFollowLinkMock = jest.spyOn(event, 'preventDefault');
-
-      openOrCloseAllSectionsHandler(event);
-
-      expect(dontFollowLinkMock).toHaveBeenCalled();
-      expect(document.getElementsByClassName(ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR).length).toEqual(0);
-    });
-
-    test('adds the class selector from all filter groups that makes them visible', () => {
-      document.body.innerHTML = `<div class="govuk-accordion__section ${ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR}"></div>
-      <div class="govuk-accordion__section ${ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR}"></div>
-      <div class="${ACCORDION_SECTION_CLASS_SELECTOR}"></div>`;
-
-      const event = { preventDefault: jest.fn(), target: { innerText: OPEN_ALL_TEXT } };
-      const dontFollowLinkMock = jest.spyOn(event, 'preventDefault');
-
-      openOrCloseAllSectionsHandler(event);
-
-      expect(dontFollowLinkMock).toHaveBeenCalled();
-      expect(document.getElementsByClassName(ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR).length).toEqual(3);
-    });
-  });
-
-  describe('displayOpenOrCloseText', () => {
-    test('displays open text when all elements are closed', () => {
-      const targetElement = { innerText: CLOSE_ALL_TEXT };
-      const expandedElements = 0;
-      const maxElements = 2;
-
-      displayOpenOrCloseText(targetElement, expandedElements, maxElements);
-      expect(targetElement.innerText).toEqual(OPEN_ALL_TEXT);
-    });
-
-    test('displays close text when all elements are open', () => {
-      const targetElement = { innerText: OPEN_ALL_TEXT };
-      const expandedElements = 2;
-      const maxElements = 2;
-
-      displayOpenOrCloseText(targetElement, expandedElements, maxElements);
-      expect(targetElement.innerText).toEqual(CLOSE_ALL_TEXT);
-    });
-  });
-
-  describe('openOrCloseAllSections', () => {
-    test('closes all elements when called', () => {
-      document.body.innerHTML = `<div class="govuk-accordion__section ${ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR}"></div>
-      <div class="govuk-accordion__section ${ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR}"></div>
-      <div class="${ACCORDION_SECTION_CLASS_SELECTOR}"></div>`;
-      const targetElement = { innerText: CLOSE_ALL_TEXT };
-
-      openOrCloseAllSections(targetElement);
-      expect(targetElement.innerText).toEqual(OPEN_ALL_TEXT);
-      expect(document.getElementsByClassName(ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR).length).toEqual(0);
-    });
-
-    test('opens all elements when called', () => {
-      document.body.innerHTML = `<div class="govuk-accordion__section ${ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR}"></div>
-      <div class="govuk-accordion__section ${ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR}"></div>
-      <div class="${ACCORDION_SECTION_CLASS_SELECTOR}"></div>`;
-      const targetElement = { innerText: OPEN_ALL_TEXT };
-
-      openOrCloseAllSections(targetElement);
-      expect(targetElement.innerText).toEqual(CLOSE_ALL_TEXT);
-      expect(document.getElementsByClassName(ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR).length).toEqual(3);
-    });
-  });
-
-  describe('addUpdateOpenOrCloseEvent', () => {
-    const accordionTemplate = (allOpen) => {
-      const expandedClass = allOpen ? ACCORDION_SECTION_EXPANDED_CLASS_SELECTOR : '';
-      return `
-      <div><button id="filters-component-close-all"></button></div>
-      <div class="${ACCORDION_SECTION_CLASS_SELECTOR} ${expandedClass}"><div></div></div>
-      <div class="${ACCORDION_SECTION_CLASS_SELECTOR} ${expandedClass}"><div id="click-this-header"></div></div>
-      `;
-    };
-
-    const openOrCloseAllSelector = 'filters-component-close-all';
-
-    test('displays close all when all elements are open', () => {
-      document.body.innerHTML = accordionTemplate(true);
-
-      const sectionHeaderElement = document.getElementById('click-this-header');
-
-      addUpdateOpenOrCloseEvent(sectionHeaderElement, openOrCloseAllSelector);
-      sectionHeaderElement.dispatchEvent(new Event('click'));
-
-      expect(document.getElementById(openOrCloseAllSelector).innerText).toEqual(CLOSE_ALL_TEXT);
-    });
-
-    test('displays open all when all elements are closed', () => {
-      document.body.innerHTML = accordionTemplate(false);
-
-      const sectionHeaderElement = document.getElementById('click-this-header');
-
-      addUpdateOpenOrCloseEvent(sectionHeaderElement, openOrCloseAllSelector);
-      sectionHeaderElement.dispatchEvent(new Event('click'));
-
-      expect(document.getElementById(openOrCloseAllSelector).innerText).toEqual(OPEN_ALL_TEXT);
     });
   });
 });
