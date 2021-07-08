c595e6fe1c4a7ab10c6d2c5dba018d61497f20c3
alexbowen <alexbowen123@googlemail.com>
2021-07-08 14:45:56 +0100

    add copy to clip board functionality

--- app/frontend/src/application/init.js
+++ app/frontend/src/application/init.js
@@ -1,15 +1,33 @@
-import ClipboardJS from 'clipboard';
+/* eslint-disable */
 import './jobseekers/init';
 import './publishers/init';
 import '../components/form/form';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // lint disabled as would require changes to library script
-  new ClipboardJS('.copy-to-clipboard'); // eslint-disable-line
+  const writeText = (str) => {
+    return new Promise((resolve, reject) => {
+      let success = false;
+      const listener = (e) => {
+        e.clipboardData.setData('text/plain', str);
+        e.preventDefault();
+        success = true;
+      };
+      document.addEventListener('copy', listener);
+      document.execCommand('copy');
+      document.removeEventListener('copy', listener);
+      success ? resolve() : reject();
+    });
+  };
 
   Array.from(document.getElementsByClassName('copy-to-clipboard')).forEach((el) => {
     el.addEventListener('click', (e) => {
-      e.preventDefault();
+      const copyEl = e.target.dataset.targetId;
+      const copyText = document.getElementById(copyEl).innerHTML;
+
+      writeText(copyText).then(() => {
+        e.target.innerHTML = 'Copied';
+      });
     });
   });
 });
+/* eslint-enable */
