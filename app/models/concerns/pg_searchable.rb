3ff4fd62e3ae9200c08d0a74cb81e532bc8bd818
Christian Sutter <git@doublesignal.com>
2021-07-12 15:47:31 +0100

    WIP search that searches

--- app/models/concerns/pg_searchable.rb
+++ app/models/concerns/pg_searchable.rb
@@ -4,7 +4,36 @@ module PgSearchable
   included do
     include PgSearch::Model
 
+    before_save :update_searchable
+
     pg_search_scope :pg_search,
-                    against: %i[job_title subjects]
+                    against: :searchable,
+                    using: {
+                      tsearch: {
+                        negation: true,
+                        tsvector_column: :searchable,
+                      },
+                    }
+
+    private
+
+    def update_searchable
+      ts_vector_value = [job_title, *subjects].join(" ")
+
+      to_tsvector = Arel::Nodes::NamedFunction.new(
+        "TO_TSVECTOR", [
+          Arel::Nodes::Quoted.new("pg_catalog.simple"),
+          Arel::Nodes::Quoted.new(ts_vector_value),
+        ]
+      )
+
+      self.searchable =
+        ActiveRecord::Base
+          .connection
+          .execute(Arel::SelectManager.new.project(to_tsvector).to_sql)
+          .first
+          .values
+          .first
+    end
   end
 end
