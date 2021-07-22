5030bed2f214d3af9f5a14ed42cdf1167fbf8114
David Mears <david.mears@digital.education.gov.uk>
2021-07-22 14:50:13 +0100

    Remove 'awaiting feedback' flash message

--- db/scripts/sanitise.sql
+++ db/scripts/sanitise.sql
@@ -2,6 +2,17 @@ TRUNCATE TABLE alert_runs;
 TRUNCATE TABLE emergency_login_keys;
 TRUNCATE TABLE sessions;
 
+-- Columns encrypted with lockbox should be set to values that can be decrypted by the target environment.
+-- These values are encrypted with the staging lockbox master key.
+-- Change them if you are using this script for a different environment, as the key will be different.
+-- There are multiple values in order to pass the different validations on each attribute.
+SET lorem_string_ciphertext = 'London'
+SET lorem_phone_number_ciphertext
+SET lorem_email_ciphertext
+SET lorem_teacher_reference_number_ciphertext
+SET lorem_national_insurance_number_ciphertext
+SET empty_string_ciphertext
+
 UPDATE employments
        SET organisation='Example Organisation',
            job_title='Example Job Title',
@@ -42,26 +53,26 @@ UPDATE equal_opportunities_reports
 
 UPDATE feedbacks
        SET comment=NULL,
-           email=concat('anonymised-feedback-',id,'@example.org'),
+           email=concat('anonymised-feedback-',id,'@example.org'),  -- encrypt. doesn't need id
            other_unsubscribe_reason_comment=NULL,
            visit_purpose_comment=NULL;
 
 UPDATE job_applications
-       SET first_name='Anonymous',
-           last_name='Anon',
-           previous_names='',
-           street_address='1 Example Street',
-           city='Anonymised City',
-           postcode='P05T C0DE',
-           phone_number='01234567890',
-           teacher_reference_number='1234567',
-           national_insurance_number='QQ 12 34 56 C',
-           personal_statement='Lorem ipsum dolor sit amet',
-           support_needed_details='',
-           close_relationships_details='',
-           further_instructions='',
-           rejection_reasons='',
-           gaps_in_employment_details='',
+       SET first_name='Anonymous', -- encrypt
+           last_name='Anon', -- encrypt
+           previous_names='', -- encrypt
+           street_address='1 Example Street', -- encrypt
+           city='Anonymised City', -- encrypt
+           postcode='P05T C0DE', -- encrypt
+           phone_number='01234567890',  -- encrypt
+           teacher_reference_number='1234567',  -- encrypt
+           national_insurance_number='QQ 12 34 56 C', -- encrypt
+           personal_statement='Lorem ipsum dolor sit amet', -- encrypt
+           support_needed_details='', -- encrypt
+           close_relationships_details='', -- encrypt
+           further_instructions='', -- encrypt
+           rejection_reasons='', -- encrypt
+           gaps_in_employment_details='', -- encrypt
            disability='prefer_not_to_say',
            gender='prefer_not_to_say',
            gender_description='',
@@ -73,20 +84,21 @@ UPDATE job_applications
            religion_description='';
 
 UPDATE jobseekers
-       SET email=concat('anonymised-jobseeker-',id,'@example.org'),
+       SET email=concat('anonymised-jobseeker-',id,'@example.org'), -- encrypt, and account for email LIKE '%education'
            encrypted_password='ABCDEFGH12345',
            reset_password_token=concat('anonymised-reset-password-token-',id),
            current_sign_in_ip='8.8.8.8',
            last_sign_in_ip='8.8.4.4',
            confirmation_token=concat('anonymised-confirmation-token-',id),
-           unconfirmed_email='',
+           unconfirmed_email='', -- encrypt
            unlock_token=concat('anonymised-unlock-token-',id)
        WHERE email NOT LIKE '%education.gov.uk';
 
 UPDATE publishers
-       SET email=concat('anonymised-publisher-',id,'@example.org'),
-           family_name='anon',
-           given_name='anon';
+       SET email=concat('anonymised-publisher-',id,'@example.org'), -- encrypt. no need for unique
+           family_name='anon', -- encrypt
+           given_name='anon'; -- encrypt
+           oid=;  -- encrypt (and add to sanitise.sql) (must match the original record)
 
 UPDATE qualifications
        SET finished_studying_details='';
