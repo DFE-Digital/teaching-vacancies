dc176a50b2f5a4f0398e48cdb11cb132b6e30a24
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-07-28 12:33:10 +0100

    Automatically highlight gaps in employment

--- app/form_models/jobseekers/job_application/employment_history_form.rb
+++ app/form_models/jobseekers/job_application/employment_history_form.rb
@@ -1,7 +1,3 @@
 class Jobseekers::JobApplication::EmploymentHistoryForm
   include ActiveModel::Model
-
-  attr_accessor :gaps_in_employment, :gaps_in_employment_details
-
-  validates :gaps_in_employment, inclusion: { in: %w[yes no] }
 end
