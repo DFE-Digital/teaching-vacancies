d2bbb1fa8e5c50aeaa37e33758ab57ffd9d7b85e
alexbowen <alexbowen123@googlemail.com>
2021-07-29 16:11:21 +0100

    use govuk link compoenent for ciorrect focus styles

--- app/components/jobseekers/account_survey_link_component.rb
+++ app/components/jobseekers/account_survey_link_component.rb
@@ -4,7 +4,7 @@ class Jobseekers::AccountSurveyLinkComponent < ViewComponent::Base
   end
 
   def link_to_survey
-    link_to(
+    govuk_link_to(
       t(".survey_link"),
       new_jobseekers_account_feedback_path(origin: @origin),
       id: "account-survey-link-sticky-gtm",
