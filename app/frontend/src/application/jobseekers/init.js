674d758787b33439c1df949fdcb0b58e2966e368
alexbowen <alexbowen123@googlemail.com>
2021-08-04 08:49:15 +0100

    replace with cuurent autocomplete with alphagov accessible version

--- app/frontend/src/application/jobseekers/init.js
+++ app/frontend/src/application/jobseekers/init.js
@@ -1,20 +1,11 @@
-import '../../components/locationFinder/locationFinder';
-import { renderAutocomplete } from '../../components/autocomplete/autocomplete';
-import { getLocationSuggestions } from '../../lib/api';
 
+import { getLocationSuggestions } from '../../lib/api';
+import '../../components/locationFinder/locationFinder';
+import autocomplete from '../../components/autocomplete/autocomplete';
 import './manageQualifications';
 import './map';
 
-const AUTOCOMPLETE_THRESHOLD = 3;
-
 window.addEventListener('DOMContentLoaded', () => {
-  Array.from(document.querySelectorAll('.js-location-finder__input')).forEach((fieldEl) => {
-    renderAutocomplete({
-      container: fieldEl.parentNode,
-      input: fieldEl,
-      threshold: AUTOCOMPLETE_THRESHOLD,
-      getOptions: getLocationSuggestions,
-      key: 'location',
-    });
-  });
+  const locationAutocompleteInputs = ['jobseekers-subscription-form-location-field', 'location-field'];
+  autocomplete(locationAutocompleteInputs, getLocationSuggestions);
 });
