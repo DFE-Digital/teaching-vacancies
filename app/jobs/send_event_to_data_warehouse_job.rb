a59bff562e0f82b65952a2c7a6e6902fce655dd9
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2021-09-03 21:07:14 +0100

    Remove lograge config and add Semantic_logger config

--- app/jobs/send_event_to_data_warehouse_job.rb
+++ app/jobs/send_event_to_data_warehouse_job.rb
@@ -1,9 +1,6 @@
 class SendEventToDataWarehouseJob < ApplicationJob
   queue_as :low
 
-  # Do not log that this job has been enqueued (as it produces at least one superfluous log per request)
-  self.logger = nil
-
   def perform(table_name, data)
     bq = Google::Cloud::Bigquery.new
     dataset = bq.dataset(Rails.configuration.big_query_dataset, skip_lookup: true)
