8fd1550724fe121117a5a190d0ea58245d014201
Christian Sutter <git@doublesignal.com>
2021-09-09 14:14:41 +0100

    Add Ife to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -320,6 +320,12 @@ Publisher.create(oid: "C20E1526-FEB0-4A7C-9651-2ADF03FC57BF",
                  family_name: "Chadburn",
                  given_name: "Danny")
 
+Publisher.create(oid: "C20E1526-FEB0-4A7C-9651-2ADF03FC57EE",
+                 email: "ife.akinbolaji@digital.education.gov.uk",
+                 organisation_publishers_attributes: organisation_publishers_attributes,
+                 family_name: "Akinbolaji",
+                 given_name: "Ife")
+
 publishers = Publisher.all
 
 # Vacancies
