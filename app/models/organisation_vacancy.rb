f599ebd3f1f1761d76e4895c0c2e6b39715f8665
David Mears <david.mears@digital.education.gov.uk>
2021-09-10 16:31:15 +0100

    Use after_commit rather than after_create
    after_create resulted in two duplicate OrganisationVacancies being created by
    the seed for each required OrgVac.

--- app/models/organisation_vacancy.rb
+++ app/models/organisation_vacancy.rb
@@ -2,8 +2,7 @@ class OrganisationVacancy < ApplicationRecord
   belongs_to :organisation
   belongs_to :vacancy
 
-  after_create :set_mean_geolocation
-  after_destroy :set_mean_geolocation
+  after_commit :set_mean_geolocation
 
   def set_mean_geolocation
     vacancy.set_mean_geolocation!
