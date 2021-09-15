b702097a03f4d981df887416f96e5903d6b4c6d8
alexbowen <alexbowen123@googlemail.com>
2021-09-15 12:39:18 +0100

    convert school overview table->summary list

--- app/components/publishers/school_overview_component.rb
+++ app/components/publishers/school_overview_component.rb
@@ -10,4 +10,12 @@ class Publishers::SchoolOverviewComponent < ViewComponent::Base
   def render?
     @organisation.school?
   end
+
+  def link_to_organisation
+    if organisation_url(@organisation)
+      open_in_new_tab_link_to(organisation_url(@organisation), organisation_url(@organisation), class: "wordwrap")
+    else
+      t("jobs.not_defined")
+    end
+  end
 end
