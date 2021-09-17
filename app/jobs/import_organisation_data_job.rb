bd9f0ff1697bd4d490be0bcb299140f46a8ba3bb
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-09-17 11:25:07 +0100

    Add logging for the import organisation task

--- app/jobs/import_organisation_data_job.rb
+++ app/jobs/import_organisation_data_job.rb
@@ -7,9 +7,13 @@ class ImportOrganisationDataJob < ApplicationJob
   def perform
     return if DisableExpensiveJobs.enabled?
 
+    Rollbar.log(:info, "Mark all school_group_memberships to be deleted")
     ImportOrganisationData.mark_all_school_group_memberships_to_be_deleted!
+    Rollbar.log(:info, "Start importing school and local autorities")
     ImportSchoolData.new.run!
+    Rollbar.log(:info, "Start importing trusts")
     ImportTrustData.new.run!
+    Rollbar.log(:info, "Delete marked school_group_memberships")
     ImportOrganisationData.delete_marked_school_group_memberships!
   end
 end
