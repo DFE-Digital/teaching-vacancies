30a2248b6562d64d0944d459e00279be47ed0e3d
Christian Sutter <git@doublesignal.com>
2021-09-17 14:46:04 +0100

    Import polygon data from ONS into PostGIS
    - Add a new geographic `area` column on `LocationPolygon` model to
      supersede legacy raw JSON `buffer` data
    - Rewrite import of ONS data to be more efficient and import data
      directly as GeoJSON into the geographic column
    - Configure ActiveRecord PostGIS adapter to use a factory with
      lenient assertions to work around coordinate precision errors
      for particularly dense polygons
    - Add new fixtures for ONS GeoJSON responses
    
    This keeps the legacy task and column around for now until we have
    verified everything works as intended and we can do buffer calculation
    directly in PostGIS.

---
+++ spec/services/ons_data_import/import_cities_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe OnsDataImport::ImportCities do
+  subject { described_class.new }
+
+  let(:response1) { double(success?: true, to_s: file_fixture("ons_cities_geojson.json").read) }
+  let(:response2) { double(success?: true, to_s: { features: [] }.to_json) }
+
+  before do
+    allow(HTTParty).to receive(:get)
+      .with(/Major_Towns_and_Cities_December_2015_Boundaries/)
+      .and_return(response1, response2)
+  end
+
+  describe "#call" do
+    it "creates a LocationPolygon for Lincoln" do
+      subject.call
+      lincoln = LocationPolygon.find_by(name: "lincoln")
+      expect(lincoln.area.to_s).to eq("POLYGON ((0.0 0.0, 1.0 1.0, 1.0 -1.0, 0.0 0.0))")
+    end
+
+    it "does not create a LocationPolygon for Atlantis" do
+      expect(LocationPolygon.where(name: "atlantis")).to be_empty
+    end
+  end
+end
