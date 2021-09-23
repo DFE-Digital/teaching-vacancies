a1311e91c540b0d573fe5dce1f54014f8861ce86
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-09-23 17:20:38 +0100

    Use govuk_header component

--- spec/system/publishers_can_request_account_spec.rb
+++ spec/system/publishers_can_request_account_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Publishers can request an account" do
   it "shows a success message when filling the form" do
     visit root_path
-    within(".navbar-component") { click_on I18n.t("buttons.sign_in") }
+    within("nav") { click_on I18n.t("buttons.sign_in") }
     click_on I18n.t("buttons.sign_in_publisher")
     click_on I18n.t("publishers.sessions.new.no_account.link_text")
     click_on I18n.t("buttons.request_account")
