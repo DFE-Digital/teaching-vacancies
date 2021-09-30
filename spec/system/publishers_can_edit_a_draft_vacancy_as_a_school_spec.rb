0e16e6c5ef9cc00a6bff031e49bb560504f40c71
josephhull676 <josephhull676@yahoo.co.uk>
2021-09-30 15:30:05 +0100

    Add status tags to job listing edit sections

--- spec/system/publishers_can_edit_a_draft_vacancy_as_a_school_spec.rb
+++ spec/system/publishers_can_edit_a_draft_vacancy_as_a_school_spec.rb
@@ -7,48 +7,21 @@ RSpec.describe "Publishers can edit a draft vacancy" do
   before { login_publisher(publisher: publisher, organisation: school) }
 
   context "editing an incomplete draft vacancy" do
-    context "when there are steps with errors" do
-      let(:vacancy_with_errors) do
-        create(:vacancy, :draft, job_roles: %w[teacher], salary: nil)
-      end
+    let(:incomplete_vacancy) { create(:vacancy, :draft, job_roles: %w[teacher], salary: nil) }
 
-      before do
-        vacancy_with_errors.organisation_vacancies.create(organisation: school)
-        visit organisation_job_path(vacancy_with_errors.id)
-      end
-
-      scenario "cannot submit a incomplete draft from the manage job listing page" do
-        expect(page).to have_content(I18n.t("pay_package_errors.salary.blank"))
-        expect(page).to_not have_content(I18n.t("buttons.submit_job_listing"))
-      end
-
-      scenario "correct status tag is displayed" do
-        within(".review-component#pay_package") do
-          expect(page).to have_content(I18n.t("shared.status_tags.action_required"))
-        end
-      end
+    before do
+      incomplete_vacancy.organisation_vacancies.create(organisation: school)
+      visit organisation_job_path(incomplete_vacancy.id)
     end
 
-    context "when no questions have been answered in a step" do
-      let(:incomplete_vacancy) { create(:vacancy, :draft, job_roles: %w[teacher], salary: nil, completed_steps: completed_steps) }
-      let(:completed_steps) do
-        %w[job_role job_details pay_package important_dates documents applying_for_the_job job_summary]
-      end
+    scenario "cannot submit a incomplete draft from the manage job listing page" do
+      expect(page).to have_content(I18n.t("pay_package_errors.salary.blank"))
+      expect(page).to_not have_content(I18n.t("buttons.submit_job_listing"))
+    end
 
-      before do
-        incomplete_vacancy.organisation_vacancies.create(organisation: school)
-        visit organisation_job_path(incomplete_vacancy.id)
-      end
-
-      scenario "cannot submit a incomplete draft from the manage job listing page" do
-        expect(page).to have_content(I18n.t("pay_package_errors.salary.blank"))
-        expect(page).to_not have_content(I18n.t("buttons.submit_job_listing"))
-      end
-
-      scenario "correct status tag is displayed" do
-        within(".review-component#working_patterns") do
-          expect(page).to have_content(I18n.t("shared.status_tags.not_started"))
-        end
+    scenario "correct status tag is displayed" do
+      within(".review-component#pay_package") do
+        expect(page).to have_content(I18n.t("shared.status_tags.action_required"))
       end
     end
   end
@@ -56,12 +29,13 @@ RSpec.describe "Publishers can edit a draft vacancy" do
   context "editing a complete draft vacancy" do
     let(:complete_vacancy) { create(:vacancy, :draft, job_roles: %w[teacher]) }
 
-    before { complete_vacancy.organisation_vacancies.create(organisation: school) }
+    before do
+      complete_vacancy.organisation_vacancies.create(organisation: school)
+      visit organisation_job_path(complete_vacancy.id)
+    end
 
     describe "cancel_and_return_later" do
       xscenario "can cancel and return from job details page" do
-        visit organisation_job_path(complete_vacancy.id)
-
         click_header_link(I18n.t("publishers.vacancies.steps.job_details"))
         expect(page).to have_content(I18n.t("buttons.cancel_and_return"))
 
@@ -74,8 +48,6 @@ RSpec.describe "Publishers can edit a draft vacancy" do
       let(:completed_steps) do
         %w[job_role job_details pay_package working_patterns important_dates documents applying_for_the_job job_summary]
       end
-
-      before { visit organisation_job_path(complete_vacancy.id) }
 
       scenario "can submit a completed draft from the manage job listing page" do
         click_on I18n.t("buttons.submit_job_listing")
