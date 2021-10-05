855bab6cc9e5ab55ac998ae75c9730af9315d8b7
josephhull676 <josephhull676@yahoo.co.uk>
2021-10-05 09:29:50 +0100

    Update hiring staff account request journey

--- spec/factories/account_requests.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :account_request do
-    full_name { Faker::Name.last_name.delete("'") }
-    email { Faker::Internet.email(domain: "example.com") }
-    organisation_name { Faker::Company.name }
-    organisation_identifier { Faker::Number.number(digits: 3).to_s }
-  end
-end
