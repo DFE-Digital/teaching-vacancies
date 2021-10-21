ea917730ce707991bd5f5efa9f741d8c29a3e4e1
josephhull676 <josephhull676@yahoo.co.uk>
2021-10-21 15:20:58 +0100

    Add callback to update subscription emails

--- app/models/jobseeker.rb
+++ app/models/jobseeker.rb
@@ -7,4 +7,12 @@ class Jobseeker < ApplicationRecord
   has_many :feedbacks
   has_many :job_applications, dependent: :destroy
   has_many :saved_jobs, dependent: :destroy
+
+  after_update :update_subscription_emails
+
+  def update_subscription_emails
+    return unless saved_change_to_attribute?(:email)
+
+    Subscription.where(email: email_previously_was).update(email: email)
+  end
 end
--- spec/models/jobseeker_spec.rb
+++ spec/models/jobseeker_spec.rb
@@ -3,4 +3,17 @@ require "rails_helper"
 RSpec.describe Jobseeker do
   it { is_expected.to have_many(:saved_jobs) }
   it { is_expected.to have_many(:job_applications) }
+
+  describe "update_subscription_emails" do
+    let(:jobseeker) { create(:jobseeker) }
+    let!(:subscription) { create(:subscription, email: jobseeker.email) }
+    let(:new_email_address) { "new_email@example.com" }
+
+    it "updates the email address of every subscription associated with their previous email address" do
+      expect {
+        jobseeker.update(email: new_email_address)
+        jobseeker.confirm
+      }.to change { subscription.reload.email }.to(new_email_address)
+    end
+  end
 end
