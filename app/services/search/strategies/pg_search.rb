0bc6cb5b028888dae5106f0685be072ea24eb210
Christian Sutter <git@doublesignal.com>
2021-10-22 14:40:09 +0100

    Add `VacancyFilterQuery` to handle search filters
    - Add `VacancyFilterQuery` to handle filters with same API as legacy
      `FiltersBuilder` for now
    - Add `search_by_filter` scope to `Vacancy` to use new query object
    - Sort all `PgSearch` results by `updated_at` by default so we have a
      better baseline comparison against Algolia (see #4198)
    - Use our DfE fork of `array_enum` to enable support for querying
      array enum overlap

--- app/services/search/strategies/pg_search.rb
+++ app/services/search/strategies/pg_search.rb
@@ -1,11 +1,12 @@
 class Search::Strategies::PgSearch
-  attr_reader :keyword, :location, :radius, :page, :per_page, :sort_by
+  attr_reader :keyword, :location, :radius, :filters, :page, :per_page, :sort_by
 
-  def initialize(keyword, location:, radius:, page:, per_page:, sort_by:)
+  def initialize(keyword, location:, radius:, filters:, page:, per_page:, sort_by:)
     @keyword = keyword
 
     @location = location
     @radius = radius
+    @filters = filters
 
     @page = page
     @per_page = per_page
@@ -29,7 +30,12 @@ class Search::Strategies::PgSearch
 
     scope = Vacancy.live
     scope = scope.search_by_location(location, radius) if location
+    scope = scope.search_by_filter(filters) if filters.any?
     scope = scope.order(sort_by.column => sort_by.order) if sort_by&.column
-    scope
+
+    # Adds an additional order by updated at for searches so a non-deterministic order column
+    # (e.g. date instead of datetime) will still result in the same order as Algolia for
+    # comparison. Can probably be removed post-migration.
+    scope.order(updated_at: :desc)
   end
 end
