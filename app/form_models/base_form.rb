10824e814523d5c88d9f729f41382c183dc84814
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-10-25 10:00:09 +0100

    Stream validation errors to BigQuery

---
+++ app/form_models/base_form.rb
@@ -0,0 +1,16 @@
+class BaseForm
+  include ActiveModel::Model
+  include ActiveModel::Validations::Callbacks
+
+  after_validation :send_errors_to_big_query
+
+  def send_errors_to_big_query
+    EventContext.trigger_event(:form_validated, data) if errors.any?
+  end
+
+  private
+
+  def data
+    errors.each_with_object({ form_name: self.class.name.underscore }) { |error, data| data[error.attribute] = error.type }
+  end
+end
