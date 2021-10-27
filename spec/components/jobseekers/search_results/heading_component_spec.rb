decb4a40508f518f13b9abfb2d3f335a6da2a898
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-10-27 16:49:30 +0100

    Optimise job role landing pages

--- spec/components/jobseekers/search_results/heading_component_spec.rb
+++ spec/components/jobseekers/search_results/heading_component_spec.rb
@@ -1,9 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Jobseekers::SearchResults::HeadingComponent, type: :component do
-  subject { described_class.new(vacancies_search: vacancies_search) }
+  subject { described_class.new(vacancies_search: vacancies_search, landing_page: landing_page) }
 
   let(:vacancies_search) { instance_double(Search::VacancySearch) }
+  let(:landing_page) { nil }
   let(:keyword) { "maths" }
   let(:location) { "London" }
   let(:polygon_boundaries) { [[51.0, 51.0]] }
@@ -17,6 +18,24 @@ RSpec.describe Jobseekers::SearchResults::HeadingComponent, type: :component do
     allow(vacancies_search).to receive_message_chain(:total_count).and_return(count)
     allow(vacancies_search).to receive_message_chain(:search_criteria, :[]).and_return(radius)
     render_inline(subject)
+  end
+
+  context "when landing_page is a job role" do
+    let(:landing_page) { "teaching-assistant" }
+
+    it "renders correct heading" do
+      expect(rendered_component).to include(
+        I18n.t("jobs.search_result_heading.landing_page_html", jobs_count: count, landing_page: landing_page.titleize.downcase, count: count),
+      )
+    end
+  end
+
+  context "when keyword and search polygon boundaries are present" do
+    it "renders correct heading" do
+      expect(rendered_component).to include(
+        I18n.t("jobs.search_result_heading.keyword_location_polygon_html", jobs_count: count, keyword: keyword, location: location, count: count, radius: radius, units: I18n.t("jobs.search_result_heading.unit_of_length").pluralize(radius.to_i)),
+      )
+    end
   end
 
   context "when keyword and search polygon boundaries are present" do
