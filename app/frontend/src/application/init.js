7e483af4158c47456b105cd0dcc66bcde6fa8743
alexbowen <alexbowen123@googlemail.com>
2021-10-30 03:27:39 +0100

    introduce stimulus for new panel component

--- app/frontend/src/application/init.js
+++ app/frontend/src/application/init.js
@@ -1,4 +1,6 @@
-import './jobseekers/init';
-import './publishers/init';
 import '../components/form/form';
 import '../components/clipboard/clipboard';
+import '../components/uploadDocuments/uploadDocuments';
+import '../components/locationFinder/locationFinder';
+import '../components/map';
+import '../components/manageQualifications/manageQualifications';
