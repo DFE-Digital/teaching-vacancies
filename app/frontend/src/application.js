7be48ab61e25c150c41aa318aed7b6b82f32d80a
alexbowen <alexbowen123@googlemail.com>
2021-10-30 03:27:39 +0100

    introduce stimulus for new panel component

---
+++ app/frontend/src/application.js
@@ -0,0 +1,16 @@
+import { Application } from '@hotwired/stimulus';
+
+import PanelController from '../../components/panel_component/panel';
+import UtilsController from './components/utils';
+import AutocompleteController from './components/autocomplete/autocomplete';
+
+const application = Application.start();
+
+// Configure Stimulus development experience
+application.warnings = true;
+application.debug = false;
+window.Stimulus = application;
+
+application.register('panel', PanelController);
+application.register('utils', UtilsController);
+application.register('autocomplete', AutocompleteController);
