436c1cd63a4e471ee3dfd6c6b78e3eb9110c8c6b
josephhull676 <josephhull676@yahoo.co.uk>
2021-11-02 17:39:41 +0000

    Fix new features page loop bug

--- spec/system/publishers_are_shown_new_features_page_spec.rb
+++ spec/system/publishers_are_shown_new_features_page_spec.rb
@@ -7,22 +7,29 @@ RSpec.describe "Publishers are shown the new features page" do
   before do
     allow(PublisherPreference).to receive(:find_by).and_return(instance_double(PublisherPreference))
     login_publisher(publisher: publisher, organisation: organisation)
-    visit organisation_path
   end
 
-  it "redirects them to the new features page" do
-    expect(current_path).to eq(new_features_path)
+  context "when the publisher has not dismissed the new features page" do
+    before { visit organisation_path }
 
-    check I18n.t("new_features.label")
-    click_on I18n.t("buttons.continue_to_account")
+    it "redirects them to the new features page after logging in" do
+      expect(current_path).to eq(new_features_path)
+      expect(page.get_rack_session_key("visited_new_features_page")).to eq(true)
 
-    publisher.reload
-    expect(current_path).to eq(organisation_path)
-    expect(publisher).to be_dismissed_new_features_page_at
+      visit new_features_path
+      check I18n.t("helpers.label.publishers_new_features_form.dismiss_options.true")
+      click_on I18n.t("buttons.continue_to_account")
+
+      publisher.reload
+      expect(current_path).to eq(organisation_path)
+      expect(publisher).to be_dismissed_new_features_page_at
+    end
   end
 
   context "when logged in as a local authority" do
     let(:organisation) { create(:local_authority) }
+
+    before { visit organisation_path }
 
     it "does not redirect them to the new features page" do
       expect(current_path).to eq(organisation_path)
@@ -33,6 +40,8 @@ RSpec.describe "Publishers are shown the new features page" do
     let(:organisation) { create(:school) }
     let!(:vacancy) { create(:vacancy, publisher: publisher, organisations: [organisation], enable_job_applications: true) }
 
+    before { visit organisation_path }
+
     it "does not redirect them to the new features page" do
       expect(current_path).to eq(organisation_path)
     end
@@ -41,6 +50,8 @@ RSpec.describe "Publishers are shown the new features page" do
   context "when they have previously dismissed the new features page" do
     let(:organisation) { create(:school) }
     let(:publisher) { create(:publisher, dismissed_new_features_page_at: 2.days.ago) }
+
+    before { visit organisation_path }
 
     it "does not redirect them to the new features page" do
       expect(current_path).to eq(organisation_path)
