cef2fee746a1db1bf50eab818ff7901ed4b360ea
David Mears <david.mears@digital.education.gov.uk>
2021-11-04 10:06:54 +0000

    Enable logging for thing I'm writing

--- app/jobs/send_event_to_data_warehouse_job.rb
+++ app/jobs/send_event_to_data_warehouse_job.rb
@@ -1,8 +1,6 @@
 class SendEventToDataWarehouseJob < ApplicationJob
   queue_as :low
-
-  self.logger = ActiveSupport::TaggedLogging.new(Logger.new(IO::NULL))
-
+  
   def perform(table_name, data)
     bq = Google::Cloud::Bigquery.new
     dataset = bq.dataset(Rails.configuration.big_query_dataset, skip_lookup: true)
