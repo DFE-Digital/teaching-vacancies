bde0e71f5b2a041ba40bd2b35d74b084d1e99848
josephhull676 <josephhull676@yahoo.co.uk>
2021-11-04 16:24:31 +0000

    Remove expired job urls from Google index

---
+++ app/jobs/remove_expired_job_urls_from_google_index_job.rb
@@ -0,0 +1,13 @@
+include Rails.application.routes.url_helpers
+
+class RemoveExpiredJobUrlsFromGoogleIndexJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    Rails.logger.info("Removing expired jobs from Google index")
+    Vacancy.expired.find_each do |vacancy|
+      RemoveGoogleIndexQueueJob.perform_now(job_url(vacancy))
+    end
+    Rails.logger.info("Finished removing expired jobs from Google index")
+  end
+end
