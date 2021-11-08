0dede673f9eada48c9d56ab4d8a18c4a16a7d4b0
alexbowen <alexbowen123@googlemail.com>
2021-11-08 15:57:43 +0000

    use stimulus targets

--- app/frontend/src/application.js
+++ app/frontend/src/application.js
@@ -2,7 +2,6 @@ import { Application } from '@hotwired/stimulus';
 
 import PanelController from '../../components/panel_component/panel';
 import UtilsController from './components/utils';
-import AutocompleteController from './components/autocomplete/autocomplete';
 
 const application = Application.start();
 
@@ -12,4 +11,3 @@ window.Stimulus = application;
 
 application.register('panel', PanelController);
 application.register('utils', UtilsController);
-application.register('autocomplete', AutocompleteController);
--- app/frontend/src/application/init.js
+++ app/frontend/src/application/init.js
@@ -1,6 +1,14 @@
+import { getLocationSuggestions } from '../lib/api';
+import autocomplete from '../components/autocomplete/autocomplete';
+
 import '../components/form/form';
 import '../components/clipboard/clipboard';
 import '../components/uploadDocuments/uploadDocuments';
 import '../components/locationFinder/locationFinder';
 import './jobseekers/map';
 import '../components/manageQualifications/manageQualifications';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const locationAutocompleteInputs = ['jobseekers-subscription-form-location-field', 'location-field'];
+  autocomplete(locationAutocompleteInputs, getLocationSuggestions);
+});
