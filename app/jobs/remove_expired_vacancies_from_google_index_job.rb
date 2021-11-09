e7167cfde382b63f2d209989f307e1d6e1892ace
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-11-09 16:35:28 +0000

    Perform later remove from Google index

--- app/jobs/remove_expired_vacancies_from_google_index_job.rb
+++ app/jobs/remove_expired_vacancies_from_google_index_job.rb
@@ -4,7 +4,7 @@ class RemoveExpiredVacanciesFromGoogleIndexJob < ApplicationJob
   def perform
     Rails.logger.info("Removing expired jobs from Google index")
     Vacancy.expired.find_each do |vacancy|
-      RemoveGoogleIndexQueueJob.perform_now(Rails.application.routes.url_helpers.job_url(vacancy))
+      RemoveGoogleIndexQueueJob.perform_later(Rails.application.routes.url_helpers.job_url(vacancy))
     end
     Rails.logger.info("Finished removing expired jobs from Google index")
   end
