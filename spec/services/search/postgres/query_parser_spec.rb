f9e5fc510db4137cac9c604e7259b10cfe04feeb
Christian Sutter <git@doublesignal.com>
2021-11-10 18:01:03 +0000

    Implement query parser for Postgres search
    We use a managed Postgres database through GOV.UK PaaS and it does not
    support custom synonym dictionaries for full-text search (because we do
    not get filesystem access on the database server which Postgres requires
    for loading dictionaries). This means we can't just add a synonym
    dictionary/thesaurus to Postgres, use `websearch_to_tsquery`, and call
    it a day - instead we need to parse our own queries and take synonyms
    into account when doing so.
    
    This implements a basic query parser with synonym support, using the
    [`parslet` gem](http://kschiess.github.io/parslet) to parse user input
    into an abstract "syntax" (well, ish) tree that can then be transformed
    into an Arel AST which ActiveRecord can use to query.
    
    - Add basic synonym dictionaries from Algolia as YAML files and load
      them into Rails configuration
    - Add `Search::Postgres::QueryParser` to parse user search queries into
      a tree
    - Add `Search::Postgres::QueryTransformer` to translate that tree into
      an Arel AST

---
+++ spec/services/search/postgres/query_parser_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe Search::Postgres::QueryParser do
+  let(:search_configuration) { Rails.application.config.x.search }
+
+  before do
+    allow(search_configuration).to receive(:terms_with_synonyms).and_return(%w[hello ciao])
+    allow(search_configuration).to receive(:terms_with_oneway_synonyms)
+      .and_return(["beautiful world"])
+  end
+
+  describe "#parse" do
+    it "parses search queries into their components" do
+      expect(subject.parse("Hello oh beautiful World")).to eq(
+        {
+          query: [
+            { synonym_term: "hello" },
+            { plain_term: "oh" },
+            { oneway_synonym_term: "beautiful world" },
+          ],
+        },
+      )
+    end
+  end
+end
