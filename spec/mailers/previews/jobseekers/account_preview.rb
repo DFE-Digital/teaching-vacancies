6debc955428ca174c41cf99cfa70faf394d39c7e
David Mears <david.mears@digital.education.gov.uk>
2021-11-17 17:44:58 +0000

    Update the email previews

--- spec/mailers/previews/jobseekers/account_preview.rb
+++ spec/mailers/previews/jobseekers/account_preview.rb
@@ -1,11 +1,19 @@
 # Documentation: app/mailers/previewing_emails.md
 class Jobseekers::AccountPreview < ActionMailer::Preview
+  def account_closed
+    Jobseekers::AccountMailer.account_closed(Jobseeker.first)
+  end
+
   def confirmation_instructions
     Jobseekers::AccountMailer.confirmation_instructions(Jobseeker.first, "fake_token")
   end
 
   def email_changed
     Jobseekers::AccountMailer.email_changed(Jobseeker.first)
+  end
+
+  def inactive_account
+    Jobseekers::AccountMailer.inactive_account(Jobseeker.first)
   end
 
   def reset_password_instructions
---
+++ spec/mailers/previews/publishers/job_application_preview.rb
@@ -0,0 +1,6 @@
+# Documentation: app/mailers/previewing_emails.md
+class Publishers::JobApplicationPreview < ActionMailer::Preview
+  def applications_received
+    # TODO
+  end
+end
