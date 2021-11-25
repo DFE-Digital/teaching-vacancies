a2184970b79adae024dee75ce5cfecec8a1d8823
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-11-25 11:41:10 +0000

    Convert uploadDocuments.js to stimulus component

--- app/frontend/src/components/uploadDocuments/uploadDocuments.js
+++ app/frontend/src/components/uploadDocuments/uploadDocuments.js
@@ -1,51 +1,42 @@
 import 'classlist-polyfill';
 import './uploadDocuments.scss';
+import { Controller } from '@hotwired/stimulus';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const inputFileUpload = document.getElementById('publishers-job-listing-documents-form-documents-field');
-  const selectFileButton = document.getElementById('select-files-button');
-  const uploadFileButton = document.getElementsByClassName('upload-files-button')[0];
-  const continueButton = document.getElementsByClassName('save-listing-gtm')[0];
-  const updateButton = document.getElementsByClassName('save-listing-gtm')[0];
-  const saveButton = document.getElementsByClassName('save-and-return-listing-gtm')[0];
+export default class extends Controller {
+  static targets = ['inputFileUpload', 'uploadFilesButton', 'selectFileButton'];
 
-  if (inputFileUpload && selectFileButton && uploadFileButton) {
-    selectFileButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      inputFileUpload.click();
-    });
+  connect() {
+    const continueButton = document.getElementsByClassName('save-listing-gtm')[0];
 
-    inputFileUpload.addEventListener('change', () => {
-      if (continueButton) {
+    if (this.inputFileUploadTarget && this.uploadFilesButtonTarget && this.selectFileButtonTarget) {
+      this.selectFileButtonTarget.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.inputFileUploadTarget.click();
+      });
+
+      this.inputFileUploadTarget.addEventListener('change', () => {
         continueButton.disabled = true;
-      }
-      if (updateButton) {
-        updateButton.disabled = true;
-      }
-      if (saveButton) {
-        saveButton.disabled = true;
-      }
-      injectDocumentsTable(inputFileUpload);
-      inputFileUpload.form.submit();
-    });
+        this.injectDocumentsTable();
+        this.inputFileUploadTarget.form.submit();
+      });
 
-    inputFileUpload.classList.add('display-none');
-    uploadFileButton.classList.add('display-none');
-    selectFileButton.classList.remove('display-none');
+      this.inputFileUploadTarget.classList.add('display-none');
+      this.uploadFilesButtonTarget.classList.add('display-none');
+      this.selectFileButtonTarget.classList.remove('display-none');
+    }
   }
-});
 
-const injectDocumentsTable = (documentsInput) => {
-  const filesList = documentsInput.files;
+  injectDocumentsTable() {
+    const filesList = this.inputFileUploadTarget.files;
 
-  if (filesList && filesList.length) {
-    const documentsContainerElement = document.querySelector('.js-documents');
-    const tableBodyElement = documentsContainerElement.querySelector('#js-documents__table-body');
+    if (filesList && filesList.length) {
+      const documentsContainerElement = document.querySelector('.js-documents');
+      const tableBodyElement = documentsContainerElement.querySelector('#js-documents__table-body');
 
-    documentsContainerElement.classList.remove('js-documents--empty');
+      documentsContainerElement.classList.remove('js-documents--empty');
 
-    Array.from(filesList).forEach((file) => {
-      const rowHTML = `
+      Array.from(filesList).forEach((file) => {
+        const rowHTML = `
 <tr class='govuk-table__row'>
 <td class='govuk-table__cell' scope='row'>${file.name}</td>
 <td class='govuk-table__cell'>
@@ -54,7 +45,8 @@ Uploading<span class='upload-progress'><div class='upload-progress-spinner'></di
 <td class='govuk-table__cell' scope='row'></td>
 <td class='govuk-table__cell' scope='row'></td>
 </tr> `;
-      tableBodyElement.insertAdjacentHTML('beforeend', rowHTML);
-    });
+        tableBodyElement.insertAdjacentHTML('beforeend', rowHTML);
+      });
+    }
   }
-};
+}
