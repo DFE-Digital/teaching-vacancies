da3718321fc9324dc473abf27d998db4882d8b36
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-11-25 11:41:10 +0000

    Convert uploadDocuments.js to stimulus component

--- app/frontend/src/application/init.js
+++ app/frontend/src/application/init.js
@@ -1,5 +1,4 @@
 import '../components/form/form';
-import '../components/uploadDocuments/uploadDocuments';
 import '../components/locationFinder/locationFinder';
 import './jobseekers/map';
 import '../components/manageQualifications/manageQualifications';
