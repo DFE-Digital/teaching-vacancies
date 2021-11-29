b92b0034509e792dfffed2b7aa826436f7641bbb
Rob Young <robertmatthewyoung@gmail.com>
2021-11-29 17:15:58 +0000

    Convert form.js to use stimulus

--- app/frontend/src/components/form/form.js
+++ app/frontend/src/components/form/form.js
@@ -1,73 +1,42 @@
-export const CHECKBOX_CLASS = 'govuk-checkboxes__input';
-export const RADIO_CLASS = 'govuk-radios__input';
-export const SELECT_CLASS = 'govuk-select';
-export const CLEARFORM_CLASS = 'clear-form';
-export const AUTOSUBMIT_ATTR_KEY = 'auto-submit';
+import { Controller } from '@hotwired/stimulus';
 
-document.addEventListener('DOMContentLoaded', () => {
-  initClearForm();
-  initAutoSubmit();
-});
+export default class extends Controller {
+  static targets = ['inputText'];
 
-export const initClearForm = () => {
-  Array.from(document.getElementsByClassName(CLEARFORM_CLASS)).forEach((clearFormEl) => {
-    Array.from(clearFormEl.querySelectorAll(`.${RADIO_CLASS}, .${CHECKBOX_CLASS}`)).forEach((controlEl) => {
-      controlEl.addEventListener('click', (event) => {
-        form.checkboxClickHandler(clearFormEl, event.target.checked);
-      });
-    });
-  });
-};
-
-export const initAutoSubmit = () => {
-  Array.from(document.querySelectorAll(`[data-${AUTOSUBMIT_ATTR_KEY}="true"]`)).forEach((formEl) => {
-    Array.from(formEl.querySelectorAll(`.${SELECT_CLASS}, .${CHECKBOX_CLASS}`)).forEach((el) => {
-      el.addEventListener('change', (e) => {
-        if (e.target.dataset.changeSubmit !== 'false') {
-          form.formSubmit(e.target.closest('form'));
-        }
-      });
-    });
-  });
-};
-
-export const formSubmit = (formEl) => {
-  if (formEl.dataset.autoSubmit) {
-    formEl.submit();
+  submitListener(e) {
+    this.formEl = e.target;
+    this.submitHandler();
   }
-};
 
-export const disableInputs = (inputs) => {
-  inputs.forEach((input) => {
-    input.value = '';
-  });
-};
-
-export const enableInputs = (inputs) => {
-  inputs.forEach((input) => {
-    input.disabled = false;
-    input.value = input.getAttribute('value');
-  });
-};
-
-export const checkboxClickHandler = (element, checked) => {
-  const fields = Array.from(element.querySelectorAll('input[type="text"]'));
-  if (checked) {
-    form.disableInputs(fields);
-  } else {
-    form.enableInputs(fields);
+  submitHandler() {
+    this.formEl.closest('form').submit();
   }
-};
 
-const form = {
-  disableInputs,
-  enableInputs,
-  checkboxClickHandler,
-  formSubmit,
-  CHECKBOX_CLASS,
-  SELECT_CLASS,
-  CLEARFORM_CLASS,
-  AUTOSUBMIT_ATTR_KEY,
-};
+  clearListener(e) {
+    this.inputTextTargets.forEach((inputTextTarget) => {
+      this.clearHandler(inputTextTarget, e.target.checked);
+    });
+  }
 
-export default form;
+  clearHandler(el, checked) {
+    this.fields = Array.from(el.querySelectorAll('input[type="text"]'));
+    if (checked) {
+      this.disableInputs();
+    } else {
+      this.enableInputs();
+    }
+  }
+
+  enableInputs() {
+    this.fields.forEach((input) => {
+      input.disabled = false;
+      input.value = input.getAttribute('value');
+    });
+  }
+
+  disableInputs() {
+    this.fields.forEach((input) => {
+      input.value = '';
+    });
+  }
+}
