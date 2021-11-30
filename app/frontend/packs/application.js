1c1928afa8f463d2479d5aa3df606b3c709f2930
alexbowen <alexbowen123@googlemail.com>
2021-11-30 22:45:50 +0000

    stimulus components asap to 95% remove page load flicker

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,11 +5,10 @@ import Rails from 'rails-ujs';
 
 import { initAll } from 'govuk-frontend';
 
+import 'src/application';
 import 'src/components';
 
 import 'src/styles/application.scss';
-
-import 'src/application';
 import 'src/application/init';
 
 Rails.start();
