08ca49dbe38cc6cbd3aa9ceac1dde63f506979b3
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2021-12-01 11:00:32 +0000

    Fix smoke test

--- spec/smoke_tests/jobseekers_can_view_homepage_spec.rb
+++ spec/smoke_tests/jobseekers_can_view_homepage_spec.rb
@@ -30,8 +30,7 @@ RSpec.describe "Page availability", js: true, smoke_test: true do
       page.visit "https://#{smoke_test_domain}/"
       expect(page).to have_content(I18n.t("jobs.heading"))
 
-      page.fill_in I18n.t("jobs.search.keyword"), with: "Maths"
-      page.click_on I18n.t("buttons.search")
+      page.visit "https://#{smoke_test_domain}/jobs?keyword=Maths"
 
       if page.has_css?(".search-results__header-stats")
         expect(page).to have_content(I18n.t("subscriptions.link.text"))
