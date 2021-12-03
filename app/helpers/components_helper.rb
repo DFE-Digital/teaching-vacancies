c3c101cf6a042eedbbbb996eabbed0ad865c57ff
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2021-12-03 10:46:32 +0000

    Use helper methods to render components

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -1,5 +1,12 @@
 module ComponentsHelper
   {
+    card: "CardComponent",
+    dashboard: "DashboardComponent",
+    empty_section: "EmptySectionComponent",
+    filters: "FiltersComponent",
+    publishers_no_vacancies: "Publishers::NoVacanciesComponent",
+    publishers_school_overview: "Publishers::SchoolOverviewComponent",
+    publishers_vacancies: "Publishers::VacanciesComponent",
     validatable_summary_list: "ValidatableSummaryListComponent",
   }.each do |name, klass|
     define_method(name) do |*args, **kwargs, &block|
