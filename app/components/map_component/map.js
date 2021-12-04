4e29c5607594e9c5342a05de4b6ceb67771df0b5
alexbowen <alexbowen123@googlemail.com>
2021-12-04 13:52:22 +0000

    FE tweaks/tidy

--- app/components/map_component/map.js
+++ app/components/map_component/map.js
@@ -7,18 +7,17 @@ import './map.scss';
 
 import api from '../../frontend/src/lib/api';
 
-export default class extends Controller {
+const MapController = class extends Controller {
   connect() {
     const config = JSON.parse(this.element.dataset.config);
 
-    api.getMapData(config).then((data) => {
+    api.getMapData(config).then((items) => {
       if (!this.map) {
-        // this is a bit knarly but all i had time for, just gets the first map item that has a point to use as map center.
-        const center = data.filter((item) => item[0].data.point)[0][0].data.point;
-        this.create(center, this.element.dataset.zoom);
+        const [[mapCenter]] = items.filter((mapItem) => mapItem[0].data.point);
+        this.create(mapCenter.data.point, this.element.dataset.zoom);
       }
 
-      data.forEach((mapItem) => {
+      items.forEach((mapItem) => {
         mapItem.forEach((item) => {
           this[item.type](item.data);
         });
@@ -37,8 +36,14 @@ export default class extends Controller {
   }
 
   marker({ point, meta }) {
-    const icon = L.divIcon({ className: 'map-component__map__icon', iconSize: [20, 20] });
+    const icon = L.divIcon({ className: 'map-component__map__marker--default', iconSize: [20, 20] });
 
-    L.marker(point, { icon }).addTo(this.map).bindPopup(`<h4 class="govuk-body-m"><a href="${meta.name_link}">${meta.name}</a></h4><p>${meta.school_type}</p><p>${meta.address}</p>`).openPopup();
+    L.marker(point, { icon }).addTo(this.map).bindPopup(MapController.markerHTML(meta)).openPopup();
   }
-}
+
+  static markerHTML(data) {
+    return `<h4 class="govuk-body-m"><a href="${data.name_link}">${data.name}</a></h4><p>${data.school_type}</p><p>${data.address}</p>`;
+  }
+};
+
+export default MapController;
