1f95aa2ccb083708d41f68857149dcadb1cc2cd3
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2022-01-06 16:20:43 +0000

    Update date constant

--- app/controllers/publishers/new_features_controller.rb
+++ app/controllers/publishers/new_features_controller.rb
@@ -1,9 +1,7 @@
 class Publishers::NewFeaturesController < Publishers::BaseController
   skip_before_action :check_terms_and_conditions, only: %i[show update reminder]
 
-  # TODO: update the date here before merging this PR.
-  NEW_FEATURES_PAGE_UPDATED_AT = DateTime.new(2021, 12, 21).freeze # This constant lives here so that we remember to update it.
-  raise "Do not merge until you have updated the constant to the date of merging"
+  NEW_FEATURES_PAGE_UPDATED_AT = DateTime.new(2022, 1, 7).freeze # This constant lives here so that we remember to update it.
 
   def show
     @new_features_form = Publishers::NewFeaturesForm.new
