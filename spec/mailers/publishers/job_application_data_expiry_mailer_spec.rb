9ccdc7578b2df9d67361971e2dfcd35ff33cea5a
Christian Sutter <git@doublesignal.com>
2022-01-12 15:52:42 +0000

    Update application to Rails 7
    - Update all dependencies in advance of Rails update
    - Update Rails to 7.0.1
    - Run `rails app:update` and integrate meaningful config changes
    - Generate and run ActiveStorage migrations
    - Update Spring to 4.0 (and temporarily disable `spring-watcher-listen`
      due to incompatibility)
    - Disable `config.cache_classes` in test environment (should no longer
      be enabled when using Spring)
    - Set test values for ActiveRecord encryption in test environment
    - Fix deprecated use of `to_s` on `Date`/`Time`/`TimeWithZone`
    - Fix use of renamed `ActionMailer::DeliveryJob`

--- spec/mailers/publishers/job_application_data_expiry_mailer_spec.rb
+++ spec/mailers/publishers/job_application_data_expiry_mailer_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Publishers::JobApplicationDataExpiryMailer do
       expect(mail.to).to eq(["test@example.com"])
       expect(mail.body.encoded).to include(vacancy.job_title)
                                .and include(I18n.t("publishers.job_application_data_expiry_mailer.job_application_data_expiry.title", job_title: vacancy.job_title,
-                                                                                                                                      publish_date: vacancy.publish_on.to_s(:month_year),
+                                                                                                                                      publish_date: vacancy.publish_on.to_formatted_s(:month_year),
                                                                                                                                       expiration_date: vacancy_data_expiration_date))
                                .and include(I18n.t("publishers.job_application_data_expiry_mailer.job_application_data_expiry.expiry", job_title: vacancy.job_title,
                                                                                                                                        expiration_date: vacancy_data_expiration_date))
