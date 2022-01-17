228d83114cfe49d96fdb660ed879903534aa4726
josephhull676 <josephhull676@yahoo.co.uk>
2022-01-17 14:10:02 +0000

    Update end listing form

--- spec/form_models/publishers/job_listing/end_listing_form_spec.rb
+++ spec/form_models/publishers/job_listing/end_listing_form_spec.rb
@@ -1,12 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Publishers::JobListing::EndListingForm, type: :model do
-  it { is_expected.to validate_inclusion_of(:end_listing_reason).in_array(Vacancy.end_listing_reasons.keys) }
-  it { is_expected.not_to validate_inclusion_of(:candidate_hired_from).in_array(Vacancy.candidate_hired_froms.keys) }
-
-  context "when `end_listing_reason` is `suitable_candidate_found`" do
-    before { allow(subject).to receive(:end_listing_reason).and_return("suitable_candidate_found") }
-
-    it { is_expected.to validate_inclusion_of(:candidate_hired_from).in_array(Vacancy.candidate_hired_froms.keys) }
-  end
+  it { is_expected.to validate_inclusion_of(:hired_status).in_array(Vacancy.hired_statuses.keys) }
+  it { is_expected.to validate_inclusion_of(:listed_elsewhere).in_array(Vacancy.listed_elsewheres.keys) }
 end
