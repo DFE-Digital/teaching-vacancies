00bdbafa62ee441699d11aab8a55b38e6356b3a2
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2022-01-20 16:21:13 +0000

    Add ability to add blog posts

---
+++ app/services/markdown_document.rb
@@ -0,0 +1,55 @@
+class MarkdownDocument
+  def initialize(section, filename)
+    @section = section
+    @file_name = filename
+    parse
+  end
+
+  def title
+    @front_matter["title"]
+  end
+
+  def category_tags
+    @front_matter["category_tags"]&.split(",")&.map(&:strip)
+  end
+
+  def date_posted
+    @front_matter["date_posted"]
+  end
+
+  def meta_description
+    @front_matter["meta_description"]
+  end
+
+  def content
+    @kramdown_document.to_html
+  end
+
+  def h2_headings
+    @kramdown_document.root
+                      .children
+                      .select { |element| element.type == :header && element.options[:level] == 2 }
+                      .map { |element| element.options[:raw_text] }
+  end
+
+  def exist?
+    File.exist?(file_path)
+  end
+
+  private
+
+  def parse
+    return unless exist?
+
+    @front_matter = FrontMatterParser::Parser.new(:md).call(file_content)
+    @kramdown_document = Kramdown::Document.new(@front_matter.content)
+  end
+
+  def file_content
+    File.read(file_path)
+  end
+
+  def file_path
+    Rails.root.join("app", "views", "content", @section, "#{@file_name}.md")
+  end
+end
