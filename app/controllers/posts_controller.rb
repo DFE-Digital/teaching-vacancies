e3ea0062c79b49d737552cbf626a88173c5f62cc
Rob Young <robertmatthewyoung@gmail.com>
2022-01-24 16:27:34 +0000

    Add landing pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,10 @@
 class PostsController < ApplicationController
-  before_action :render_not_found_unless_post_exist, only: :show
-
-  helper_method :post
-
-  private
-
-  def render_not_found_unless_post_exist
-    not_found unless post.exist?
+  def index
+    @posts = MarkdownDocument.all(params[:section])
   end
 
-  def post
-    @post ||= MarkdownDocument.new(params[:section], params[:post_name])
+  def show
+    @post = MarkdownDocument.new(params[:section], params[:post_name])
+    not_found unless @post.exist?
   end
 end
