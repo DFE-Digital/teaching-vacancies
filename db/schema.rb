d552375dcd89dfe6e00b494e4600489631e270f5
Christian Sutter <git@doublesignal.com>
2022-01-27 12:13:45 +0000

    PoC: Import of feed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_101037) do
+ActiveRecord::Schema.define(version: 2022_02_03_105405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -468,6 +468,9 @@ ActiveRecord::Schema.define(version: 2022_02_01_101037) do
     t.boolean "google_index_removed", default: false
     t.string "parental_leave_cover_contract_duration"
     t.datetime "expired_vacancy_feedback_email_sent_at"
+    t.string "external_feed_id"
+    t.string "external_reference"
+    t.string "external_advert_url"
     t.index ["expires_at"], name: "index_vacancies_on_expires_at"
     t.index ["geolocation"], name: "index_vacancies_on_geolocation", using: :gist
     t.index ["publisher_id"], name: "index_vacancies_on_publisher_id"
