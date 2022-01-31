f32f0936aa9a6eb32d42f92c565167c32d50751c
Christian Sutter <git@doublesignal.com>
2022-01-31 09:43:46 +0000

    Update gems
    - Pin `rspec-rails` pending Rails 6.1/Ruby 3.1 combination bug
    - Make `SendEventToDataWarehouseJob` expectation more lenient

--- spec/jobs/send_event_to_data_warehouse_job_spec.rb
+++ spec/jobs/send_event_to_data_warehouse_job_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe SendEventToDataWarehouseJob do
     it "inserts the data into the BigQuery table" do
       expect(big_query).to receive(:dataset).with("test_dataset", skip_lookup: true).and_return(dataset)
       expect(dataset).to receive(:table).with("a_fancy_table", skip_lookup: true).and_return(table)
-      expect(table).to receive(:insert).with(foo: "bar")
+      expect(table).to receive(:insert).with(hash_including(foo: "bar"))
 
       subject.perform("a_fancy_table", { foo: "bar" })
     end
