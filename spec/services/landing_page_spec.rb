5628f85c64a875906f4ad713fe9234845e65522a
Christian Sutter <git@doublesignal.com>
2022-02-11 11:08:57 +0000

    Introduce manually configured landing pages
    Up until now we've had dynamic landing pages based on the enum fields
    in `Vacancy`, with specific routes per field and some very gnarly
    manual overrides for edge cases.
    
    We now want to move towards static, manually specified (and manually
    curated on the homepage) landing pages based on expert SEO advice and
    insights from our data.
    
    - Introduce `LandingPage` service object to encapsulate a landing page
      and handle I18n and facet counting
    - Create landing pages configuration file to specify search criteria
      for static landing pages
    - Change landing pages i18n translations file to explicitly include
      translations for heading, title, name, and meta description for all
      landing pages
    - Move location landing page code to be in line with the new explicit
      non-location landing pages (while still being dynamic for all
      `LocationPolygons`) by introducing `LocationLandingPage`
    - Change `LandingPageLinkGroupComponent` to render landing page objects
    - Introduce `LandingPageLinkComponent` to render a link to a
      `LandingPage` object
    - Add numerous expertly curated landing pages from our resident SEO
      wizard, and change the homepage links to point to them
    - Amend sitemap to use new landing pages
    - Add legacy redirects for old landing pages
    - Temporarily remove breadcrumb override for teaching assistant jobs
      until we find a better way of implementing it
    - Remove legacy routes, `VacancyFacets`, lots of edge case translation
      handling, and merge `VacanciesController#index_landing` back into
      `#index` (turns out that previous change overcomplicated things)

---
+++ spec/services/landing_page_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+# This spec relies on a fake landing page set up in the test section of `config/landing_pages.yml`
+# and the translation file.
+RSpec.describe LandingPage do
+  subject { described_class["part-time-potions-and-sorcery-teacher-jobs"] }
+
+  let(:search) { instance_double(Search::VacancySearch, total_count: 42) }
+
+  before do
+    allow(Search::VacancySearch)
+      .to receive(:new)
+      .with(hash_including(working_patterns: %w[part_time], subjects: %w[Potions Sorcery]))
+      .and_return(search)
+  end
+
+  describe ".exists?" do
+    it "returns whether a landing page has been set up" do
+      expect(described_class.exists?("part-time-potions-and-sorcery-teacher-jobs")).to be(true)
+      expect(described_class.exists?("i-do-not-exist")).to be(false)
+    end
+  end
+
+  describe ".[]" do
+    it "returns a configured landing page instance if a landing page with the given slug exists" do
+      expect(described_class["part-time-potions-and-sorcery-teacher-jobs"].slug)
+        .to eq("part-time-potions-and-sorcery-teacher-jobs")
+      expect(described_class["part-time-potions-and-sorcery-teacher-jobs"].criteria)
+        .to eq({ working_patterns: %w[part_time], subjects: %w[Potions Sorcery] })
+    end
+
+    it "raises an error if no landing page with the given slug has been configured" do
+      expect { described_class["i-do-not-exist"] }
+        .to raise_error("No such landing page: 'i-do-not-exist'")
+    end
+  end
+
+  describe "#count" do
+    it "performs a search and returns its total count" do
+      expect(subject.count).to eq(42)
+    end
+  end
+
+  describe "i18n methods" do
+    specify { expect(subject.heading).to eq("<span class=\"govuk-!-font-weight-bold\">42</span> amazing jobs APPLY NOW") }
+    specify { expect(subject.meta_description).to eq("Lorem ipsum dolor sit jobs, vacancies adipiscing elit.") }
+    specify { expect(subject.name).to eq("Potions and Sorcery") }
+    specify { expect(subject.title).to eq("Spiffy Part Time Potions and Sorcery Jobs") }
+  end
+end
