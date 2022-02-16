05e64d730e986b5d92dac3a895f156215a5cb79d
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2022-02-16 09:52:17 +0000

    Setup and configure Sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,8 @@
+Sentry.init do |config|
+  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+  config.before_send = lambda do |event, _hint|
+    filter.filter(event.to_hash)
+  end
+
+  config.release = ENV["COMMIT_SHA"]
+end
