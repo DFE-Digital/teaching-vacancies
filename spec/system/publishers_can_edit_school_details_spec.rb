012e35548b46eeb02f144a43876314e006ebd0c1
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2022-02-20 09:21:13 +0000

    Publishers Manage settings

--- spec/system/publishers_can_edit_school_details_spec.rb
+++ spec/system/publishers_can_edit_school_details_spec.rb
@@ -10,17 +10,16 @@ RSpec.describe "Editing a School’s details" do
     visit organisation_path
 
     expect(page).to have_content(school.name)
-    expect(page).to have_content(school.description)
-    expect(page).to have_content(school.url)
 
+    click_link(I18n.t("nav.manage_settings"))
     click_link("Change", match: :first)
+
     expect(find_field("publishers_organisation_form[website]").value).to eq(school.url)
+
     fill_in "publishers_organisation_form[description]", with: "Our school prides itself on excellence."
     fill_in "publishers_organisation_form[website]", with: "https://www.this-is-a-test-url.example.com"
     click_on I18n.t("buttons.save_changes")
 
-    expect(page).to have_content("Our school prides itself on excellence.")
-    expect(page).to have_content("https://www.this-is-a-test-url.example.com")
     expect(page).to have_content("Details updated for #{school.name}")
     expect(page.current_path).to eq(organisation_path)
   end
