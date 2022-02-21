87a7f4f32ff0265884e90d3ea6a020b9755841a2
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2022-02-21 12:49:53 +0000

    Inject secrets from parameterstore

--- config/backstop/engine_scripts/tasks/signInJobseeker.js
+++ config/backstop/engine_scripts/tasks/signInJobseeker.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
+const UI_TEST_USERNAME=process.env.UI_TEST_USERNAME
+const UI_TEST_PASSWORD=process.env.UI_TEST_PASSWORD
+
 module.exports = async (page, scenario, viewport, isReference, browserContext) => {
   console.log('JOBSEEKER SIGN IN > ', scenario.label, viewport.label);
 
@@ -8,8 +11,8 @@ module.exports = async (page, scenario, viewport, isReference, browserContext) =
   await page.waitForSelector('button[type="submit"]');
 
   await page.evaluate(() => {
-    document.querySelector('#jobseeker-email-field').value = 'email';
-    document.querySelector('#jobseeker-password-field').value = 'pass';
+    document.querySelector('#jobseeker-email-field').value = `${UI_TEST_USERNAME}`;
+    document.querySelector('#jobseeker-password-field').value = `${UI_TEST_PASSWORD}`;
     document.cookie = "consented-to-cookies=yes path=/";
   });
 
