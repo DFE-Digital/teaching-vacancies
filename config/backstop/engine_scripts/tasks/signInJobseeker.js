19f9f5740f83797c02c536e2814b1c8d42974b6c
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2022-02-21 15:00:55 +0000

    Inject secrets from parameterstore

--- config/backstop/engine_scripts/tasks/signInJobseeker.js
+++ config/backstop/engine_scripts/tasks/signInJobseeker.js
@@ -7,6 +7,9 @@ console.log(`hello world - fake username: ${UI_TEST_USERNAME}`)
 
 module.exports = async (page, scenario, viewport, isReference, browserContext) => {
 
+  const UI_TEST_USERNAME = process.env.UI_TEST_USERNAME
+  const UI_TEST_PASSWORD = process.env.UI_TEST_PASSWORD
+
   console.log('JOBSEEKER SIGN IN > ', scenario.label, viewport.label);
 
   await require('../playwright/clickAndHoverHelper')(page, scenario);
