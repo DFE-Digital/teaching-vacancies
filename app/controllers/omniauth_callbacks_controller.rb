e835595e362699adba3be14b86efd1dbc14b6ecc
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2022-02-21 16:20:24 +0000

    Add SupportUser and dashboard

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,17 +1,15 @@
-class Publishers::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :dfe
-
-  def new
-    # This is defined by the class name of our Omniauth strategy
-    redirect_to "/auth/dfe"
-  end
 
   def dfe
     authorisation = Authorisation.new(organisation_id: organisation_id, user_id: user_id).call
-    organisation = organisation_from_request
 
-    if authorisation.authorised? && organisation
-      sign_in_publisher(organisation)
+    if authorisation.authorised_support_user?
+      sign_in_support_user
+      trigger_support_user_sign_in_event(:success, :dsi)
+      redirect_to after_sign_in_path_for(:support_user)
+    elsif authorisation.authorised_publisher?
+      sign_in_publisher(organisation_from_request)
       trigger_publisher_sign_in_event(:success, :dsi)
       redirect_to after_sign_in_path_for(:publisher)
     else
@@ -26,6 +24,16 @@ class Publishers::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
     Rails.logger.error("DSI failure - strategy: #{failed_strategy.name}, reason: #{omniauth_error.inspect}")
 
     redirect_to new_publisher_session_path, warning: t(".message")
+  end
+
+  def fake
+    raise "Not permitted outside development" unless Rails.env.development?
+
+    support_user = SupportUser.last
+    sign_in(support_user)
+    sign_out_except(:support_user)
+
+    redirect_to after_sign_in_path_for(:support_user)
   end
 
   private
@@ -48,27 +56,44 @@ class Publishers::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
 
   def organisation_from_request
     # https://github.com/DFE-Digital/login.dfe.public-api#how-do-ids-map-to-categories-and-types
-    case auth_hash.dig("extra", "raw_info", "organisation", "category", "id")
+    case (cat_id = auth_hash.dig("extra", "raw_info", "organisation", "category", "id"))
     when "001"
       School.find_by!(urn: auth_hash.dig("extra", "raw_info", "organisation", "urn"))
     when "002"
       SchoolGroup.find_by!(local_authority_code: auth_hash.dig("extra", "raw_info", "organisation", "establishmentNumber"))
     when "010"
       SchoolGroup.find_by!(uid: auth_hash.dig("extra", "raw_info", "organisation", "uid"))
+    else
+      raise OrganisationCategoryNotFound, "Organisation category ID `#{cat_id}`"
     end
   end
 
   def sign_in_publisher(organisation)
-    publisher = Publisher.find_or_create_by(oid: user_id)
-    publisher.update(email: auth_hash["info"]["email"],
-                     given_name: auth_hash["info"]["first_name"],
-                     family_name: auth_hash["info"]["last_name"])
+    publisher = find_or_create(Publisher)
+
     OrganisationPublisher.find_or_create_by(organisation_id: organisation.id, publisher_id: publisher.id)
 
     sign_in(publisher)
-    sign_out(:jobseeker)
+    sign_out_except(:publisher)
     session.update(publisher_dsi_token: id_token, publisher_organisation_id: organisation.id)
     use_school_group_if_available(publisher, organisation)
+  end
+
+  def sign_in_support_user
+    support_user = find_or_create(SupportUser)
+    sign_in(support_user)
+    sign_out_except(:support_user)
+  end
+
+  def find_or_create(klass)
+    klass.find_or_create_by(oid: user_id).tap do |record|
+      info = auth_hash.fetch("info")
+      record.update(
+        email: info["email"],
+        given_name: info["first_name"],
+        family_name: info["last_name"],
+      )
+    end
   end
 
   def use_school_group_if_available(publisher, organisation)
@@ -77,4 +102,6 @@ class Publishers::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
     publisher_organisation = publisher.organisations.school_groups.find { |school_group| school_group.schools.include?(organisation) }
     session.update(publisher_organisation_id: publisher_organisation.id) if publisher_organisation
   end
+
+  class OrganisationCategoryNotFound < StandardError; end
 end
