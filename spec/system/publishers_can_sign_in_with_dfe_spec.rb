cad4ec75c58de781546f1903e8c6a38f4debd70c
Rob Young <robertmatthewyoung@gmail.com>
2022-02-28 12:46:00 +0000

    Add support user tests

--- spec/system/publishers_can_sign_in_with_dfe_spec.rb
+++ spec/system/publishers_can_sign_in_with_dfe_spec.rb
@@ -89,6 +89,15 @@ RSpec.describe "Publishers can sign in with DfE Sign In" do
       click_on I18n.t("buttons.create_job")
       expect(current_path).to eq(create_or_copy_organisation_jobs_path)
     end
+
+    context "when navigating to support user login page" do
+      it "does not redirect to support user dashboard" do
+        sign_in_publisher(navigate: true)
+        visit new_support_user_session_path
+
+        expect(current_path).not_to eq(support_user_root_path)
+      end
+    end
   end
 
   context "with DSI data including a school group (trust or local authority) that the school belongs to" do
---
+++ spec/system/support_users_can_sign_in_with_dfe_spec.rb
@@ -0,0 +1,111 @@
+require "rails_helper"
+require "message_encryptor"
+
+RSpec.shared_examples "a successful sign in" do
+  before do
+    visit new_support_user_session_path
+  end
+
+  scenario "it signs in the user successfully" do
+    expect { sign_in_support_user }
+      .to have_triggered_event(:support_user_sign_in_attempt)
+      .with_base_data(user_anonymised_support_user_id: anonymised_form_of(user_oid))
+      .with_data(success: "true", sign_in_type: "dsi")
+
+    within("nav") { expect(page).to have_selector(:link_or_button, I18n.t("nav.sign_out")) }
+    within("nav") { expect(page).to have_selector(:link_or_button, I18n.t("nav.support_user_dashboard")) }
+  end
+end
+
+RSpec.shared_examples "a failed sign in" do |options|
+  scenario "it does not sign-in the user, and tells the user what to do" do
+    visit new_support_user_session_path
+
+    expect { sign_in_support_user }
+      .to have_triggered_event(:publisher_sign_in_attempt)
+      .with_data(success: "false", sign_in_type: "dsi", user_anonymised_publisher_id: anonymised_form_of(user_oid))
+
+    expect(page).to have_content(/The email you're signed in with isn't authorised to list jobs for this school/i)
+    expect(page).to have_content(options[:email])
+    within("nav") { expect(page).not_to have_content(I18n.t("nav.school_page_link")) }
+  end
+end
+
+RSpec.describe "Support users can sign in with DfE Sign In" do
+  let(:user_oid) { "161d1f6a-44f1-4a1a-940d-d1088c439da7" }
+
+  before do
+    OmniAuth.config.test_mode = true
+  end
+
+  after do
+    OmniAuth.config.mock_auth[:default] = nil
+    OmniAuth.config.mock_auth[:dfe] = nil
+    OmniAuth.config.test_mode = false
+  end
+
+  context "with valid credentials" do
+    before do
+      stub_support_user_authentication_step
+      stub_support_user_authorisation_step
+    end
+
+    it_behaves_like "a successful sign in"
+
+    scenario "it redirects the sign in page to the support user dashboard" do
+      sign_in_support_user(navigate: true)
+      visit new_support_user_session_path
+
+      expect(current_path).to eq(support_user_root_path)
+      #TODO fill in with content translation
+      expect(page).to have_content("Hello support user")
+    end
+
+    context "when navigating to publisher login page" do
+      it "does not redirect to publisher dashboard" do
+        sign_in_support_user(navigate: true)
+        visit new_publisher_session_path
+
+        expect(current_path).not_to eq(organisation_path)
+      end
+    end
+  end
+
+  context "with valid credentials but no authorisation" do
+    before do
+      create(:school, urn: "110627")
+      stub_support_user_authentication_step(email: "another_email@example.com")
+      stub_authorisation_step_with_not_found
+    end
+
+    it_behaves_like "a failed sign in", email: "another_email@example.com"
+  end
+
+  context "when there is was an error with DfE Sign-in" do
+    before do
+      stub_support_user_authentication_step
+      stub_authorisation_step_with_external_error
+    end
+
+    it "raises an error" do
+      visit new_support_user_session_path
+
+      expect { sign_in_support_user }.to raise_error(Authorisation::ExternalServerError)
+    end
+  end
+
+  context "when there is an Omniauth error" do
+    before do
+      OmniAuth.config.mock_auth[:dfe] = :invalid_client
+    end
+
+    it "logs an error to Sentry and takes the user to the sign in page with an error message" do
+      expect(Sentry).to receive(:capture_message)
+
+      sign_in_support_user(navigate: true)
+
+      expect(current_path).to eq(new_publisher_session_path)
+      expect(page).to have_content(I18n.t("omniauth_callbacks.failure.message"))
+    end
+  end
+end
