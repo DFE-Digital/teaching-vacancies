9a41c1aa58289741651929cc7dee39c386f20ac3
Christian Sutter <git@doublesignal.com>
2022-03-11 14:35:39 +0000

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ Gias::ImportTrusts.new.call
 
 ImportPolygonDataJob.perform_now
 
-bexleyheath_school = School.find_by!(urn: "137138")
-weydon_trust = SchoolGroup.find_by!(uid: "16644")
-southampton_la = SchoolGroup.find_by!(local_authority_code: "852")
+# Change default organisation for publishers
+# TODO: Undo all changes in this file!!
+org = SchoolGroup.find_by!(uid: "5143")
 
 # Users
 users = [
@@ -34,9 +34,29 @@ users = [
 ]
 
 users.each do |user|
-  Publisher.create(organisations: [bexleyheath_school, weydon_trust, southampton_la], **user)
+  Publisher.create(organisations: [org], **user)
   SupportUser.create(user)
 end
+# Publishers
+attrs = { organisations: [org] }
+Publisher.create(email: "alex.bowen@digital.education.gov.uk", family_name: "Bowen", given_name: "Alex", **attrs)
+Publisher.create(email: "alex.wiskar@digital.education.gov.uk", family_name: "Wiskar", given_name: "Alex", **attrs)
+Publisher.create(email: "ben.mitchell@digital.education.gov.uk", family_name: "Mitchell", given_name: "Ben", **attrs)
+Publisher.create(email: "cesidio.dilanda@digital.education.gov.uk", family_name: "Di Landa", given_name: "Cesidio", **attrs)
+Publisher.create(email: "christian.sutter@digital.education.gov.uk", family_name: "Sutter", given_name: "Christian", **attrs)
+Publisher.create(email: "colin.saliceti@digital.education.gov.uk", family_name: "Saliceti", given_name: "Colin", **attrs)
+Publisher.create(email: "danny.chadburn@digital.education.gov.uk", family_name: "Chadburn", given_name: "Danny", **attrs)
+Publisher.create(email: "elliot.crosby-mccullough@digital.education.gov.uk", family_name: "Crosby-McCullough", given_name: "Elliot", **attrs)
+Publisher.create(email: "ife.akinbolaji@digital.education.gov.uk", family_name: "Akinbolaji", given_name: "Ife", **attrs)
+Publisher.create(email: "joseph.hull@digital.education.gov.uk", family_name: "Hull", given_name: "Joseph", **attrs)
+Publisher.create(email: "leonie.shanks@digital.education.gov.uk", family_name: "Shanks", given_name: "Leonie", **attrs)
+Publisher.create(email: "molly.capstick@digital.education.gov.uk", family_name: "Capstick", given_name: "Molly", **attrs)
+Publisher.create(email: "rachael.harvey@digital.education.gov.uk", family_name: "Harvey", given_name: "Rachael", **attrs)
+Publisher.create(email: "rishil.patel@digital.education.gov.uk", family_name: "Patel", given_name: "Rishil", **attrs)
+Publisher.create(email: "rob.young@digital.education.gov.uk", family_name: "Young", given_name: "Rob", **attrs)
+Publisher.create(email: "rose.mackworth-young@digital.education.gov.uk", family_name: "Mackworth-Young", given_name: "Rose", **attrs)
+Publisher.create(email: "sabrina.altieri@education.gov.uk", family_name: "Altieri", given_name: "Sabrina", **attrs)
+Publisher.create(email: "shahad.rahman@digital.education.gov.uk", family_name: "Rahman", given_name: "Shahad", **attrs)
 
 # Do not run vacancy seeds for this branch (as we're only interested in imported ones)
 # TODO: Undo this
