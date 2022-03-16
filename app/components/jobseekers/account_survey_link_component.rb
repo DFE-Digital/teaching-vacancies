0dbc6751c8effc1b2b84efe171f2a4696d606e48
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2022-03-16 11:18:48 +0000

    Remove Google Tag Manager unused classes

--- app/components/jobseekers/account_survey_link_component.rb
+++ app/components/jobseekers/account_survey_link_component.rb
@@ -4,10 +4,6 @@ class Jobseekers::AccountSurveyLinkComponent < ViewComponent::Base
   end
 
   def link_to_survey
-    govuk_button_link_to(
-      t(".survey_link_text"),
-      new_jobseekers_account_feedback_path(origin: @origin),
-      id: "account-survey-link-sticky-gtm",
-    )
+    govuk_button_link_to t(".survey_link_text"), new_jobseekers_account_feedback_path(origin: @origin)
   end
 end
