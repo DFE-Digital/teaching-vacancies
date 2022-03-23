4ad87cd2892acd1d18e6edde9b7ee78a0a9e8e71
alexbowen <alexbowen123@googlemail.com>
2022-03-23 16:42:47 +0000

    remove global element styles from application.scss and relocate where they belong\'

--- app/frontend/backstop/scenarios/public.js
+++ app/frontend/backstop/scenarios/public.js
@@ -67,14 +67,15 @@ module.exports = [
     ]
   },
   //having an issue with this test atm
-  // {
-  //   "label": "Vacancy",
-  //   "url": "/jobs",
-  //   "readySelector": ".govuk-main-wrapper",
-  //   "clickSelector": ".vacancies .govuk-link",
-  //   "removeSelectors": [".govuk-footer", ".environment-banner-component", "#school-location"],
-  //   "selectors": [
-  //     ".govuk-main-wrapper"
-  //   ]
-  // }
+  {
+    "label": "Vacancy",
+    "url": "/jobs",
+    "delay": 1000,
+    "readySelector": ".govuk-main-wrapper",
+    "clickSelector": ".vacancies .govuk-link",
+    "removeSelectors": [".govuk-footer", ".environment-banner-component", "#school-location"],
+    "selectors": [
+      ".govuk-main-wrapper"
+    ]
+  }
 ];
