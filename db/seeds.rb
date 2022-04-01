1e29500ffe378d4c927852933f8a4b204f5027e9
Christian Sutter <git@doublesignal.com>
2022-04-01 08:50:06 +0000

    Remove Elliot from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,6 @@ users = [
   { email: "christian.sutter@digital.education.gov.uk", family_name: "Sutter", given_name: "Christian" },
   { email: "colin.saliceti@digital.education.gov.uk", family_name: "Saliceti", given_name: "Colin" },
   { email: "danny.chadburn@digital.education.gov.uk", family_name: "Chadburn", given_name: "Danny" },
-  { email: "elliot.crosby-mccullough@digital.education.gov.uk", family_name: "Crosby-McCullough", given_name: "Elliot" },
   { email: "ife.akinbolaji@digital.education.gov.uk", family_name: "Akinbolaji", given_name: "Ife" },
   { email: "joseph.hull@digital.education.gov.uk", family_name: "Hull", given_name: "Joseph" },
   { email: "leonie.shanks@digital.education.gov.uk", family_name: "Shanks", given_name: "Leonie" },
