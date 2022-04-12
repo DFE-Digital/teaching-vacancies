670338e32adcb02928f0828e94125e2979fbee3c
Christian Sutter <git@doublesignal.com>
2022-04-12 11:53:38 +0000

    Add error tracking for post-sign in 404 issues
    We are experiencing a surprising level of publishers encountering "page
    not found" errors after signing in with DfE Sign In. This adds some
    tracking to help us identify the root cause.
    
    - Override `not_found` in `OmniauthCallbacksController` to send errors
      to Sentry before its default behaviour
    - Modify `ApplicationController#not_found` to have the same method
      signature as the overridden version for consistency
    - Stop Sentry from automatically discarding `RecordNotFound` errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,6 +6,13 @@ Sentry.init do |config|
 
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
+  # Modify default excluded exceptions
+  # We do not want to exclude `ActiveRecord::RecordNotFound` - most of the time, this is
+  # automatically called (and disregarded) by the `rescue_from` in `ApplicationController`
+  # anyway, but on occasion we want to perform a query that we expect *never* to fail, and
+  # send the error through to Sentry if it does fail so that we have visibility into it.
+  config.excluded_exceptions -= ["ActiveRecord::RecordNotFound"]
+
   config.environment = Rails.application.config.app_role
   config.release = ENV["COMMIT_SHA"]
 end
