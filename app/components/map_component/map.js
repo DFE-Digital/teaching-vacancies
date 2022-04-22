d2f16ca32de4d27a0a2888fa08255c493c9c5667
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2022-04-22 09:35:52 +0100

    Render map results + clustering
    add radius circle for non polygon locations
    
    Show map results only for support users
    
    add search location map center icon
    
    add gesture handling plugin

--- app/components/map_component/map.js
+++ app/components/map_component/map.js
@@ -1,4 +1,6 @@
 import 'leaflet';
+import 'leaflet.markercluster/dist/leaflet.markercluster';
+import { GestureHandling } from 'leaflet-gesture-handling';
 
 import { Controller } from '@hotwired/stimulus';
 
@@ -8,11 +10,12 @@ const MapController = class extends Controller {
   static targets = ['markersTextList', 'marker'];
 
   connect() {
-    if (!this.element.querySelector('#map')) {
-      return;
-    }
+    const singleMarker = this.markerTargets.length <= 1;
+    const polygonData = this.element.dataset.polygon ? JSON.parse(this.element.dataset.polygon) : [];
 
-    const singleMarker = this.markerTargets.length === 1;
+    if (!singleMarker) {
+      this.createCluster();
+    }
 
     this.markerTargets.forEach((marker, index) => {
       const point = {
@@ -22,19 +25,43 @@ const MapController = class extends Controller {
 
       if (index === 0) {
         this.create(point, this.element.dataset.zoom);
+
+        polygonData.forEach((polygon) => {
+          const polygonLayer = MapController.createPolygon({ coordinates: polygon });
+          this.addMapLayer(polygonLayer);
+        });
+
+        if (this.element.dataset.radius && this.element.dataset.point) {
+          MapController.createMarker({
+            lat: JSON.parse(this.element.dataset.point)[0],
+            lon: JSON.parse(this.element.dataset.point)[1],
+          }, 'location', 'location').addTo(this.map);
+          this.addMapLayer(MapController.createCircle(this.element.dataset.radius, JSON.parse(this.element.dataset.point)));
+        }
       }
 
-      this.addMarker(point, marker, index, singleMarker);
+      const leafletMarker = MapController.createPopupMarker(point, marker, 'pin');
+
+      if (!singleMarker) {
+        this.addMarkerToCluster(leafletMarker);
+      } else if (leafletMarker) {
+        leafletMarker.addTo(this.map);
+        leafletMarker.openPopup();
+      }
     });
 
     if (!singleMarker) {
-      this.markersTextListTarget.classList.add('govuk-list--number');
+      this.addMapLayer(this.clusterGroup);
+    }
+
+    if (this.markerTargets.length > 1) {
       this.setMapBounds();
     }
   }
 
   create(point, zoom) {
-    this.map = L.map('map', { tap: false }).setView(point, zoom);
+    L.Map.addInitHook('addHandler', 'gestureHandling', GestureHandling);
+    this.map = L.map('map', { tap: false, gestureHandling: true }).setView(point, zoom);
 
     L.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -42,32 +69,46 @@ const MapController = class extends Controller {
     ).addTo(this.map);
   }
 
-  addMarker(point, marker, index, singleMarker) {
-    const originalLink = marker.querySelector('a');
+  createCluster() {
+    this.clusterGroup = L.markerClusterGroup({
+      iconCreateFunction: (cluster) => {
+        const properties = MapController.clusterIconProperties(cluster.getChildCount(), [5, 20]);
 
-    const leafletMarker = L.marker(point, {
-      icon: MapController.markerIcon(singleMarker ? '' : `<span aria-label="${originalLink.innerText}">${index + 1}</span>`),
-      riseOnHover: true,
+        return L.divIcon({
+          className: `map-component__map__cluster map-component__map__cluster--${properties.style}`,
+          iconSize: [properties.size, properties.size],
+          html: `<span>${properties.text}<span class="govuk-visually-hidden">vacancies</span></span>`,
+        });
+      },
+      maxClusterRadius: 32,
+    });
+  }
+
+  static clusterIconProperties(numberMarkers, thresholds = []) {
+    const properties = {
+      text: numberMarkers,
+      size: 30,
+      style: 'default',
+    };
+
+    const styles = ['medium', 'large'];
+
+    thresholds.forEach((threshold, i) => {
+      if (numberMarkers >= threshold) {
+        properties.text = `${threshold}+`;
+        properties.style = styles[i];
+      }
     });
 
-    const addressBlock = marker.querySelector('.pop-up');
-    addressBlock.parentNode.removeChild(addressBlock);
-    addressBlock.hidden = false;
+    return properties;
+  }
 
-    leafletMarker.addTo(this.map).bindPopup(
-      addressBlock.outerHTML,
-      { className: 'map-component__map__popup' },
-    );
+  addMapLayer(layer) {
+    this.map.addLayer(layer);
+  }
 
-    // use keydown event as leaflet doesnt recognise focus
-    leafletMarker.on('keydown', MapController.openMarkerPopup);
-
-    if (singleMarker) {
-      originalLink.remove();
-      leafletMarker.openPopup();
-    } else {
-      originalLink.replaceWith(originalLink.innerText);
-    }
+  addMarkerToCluster(marker) {
+    this.clusterGroup.addLayer(marker);
   }
 
   setMapBounds() {
@@ -76,16 +117,50 @@ const MapController = class extends Controller {
     );
   }
 
-  static markerIcon(html) {
-    return L.divIcon({
-      className: 'map-component__map__marker--default',
-      iconSize: [20, 20],
-      html,
+  static createPolygon({ coordinates }) {
+    return L.polygon(coordinates.map((point) => point.reverse()), { color: '#0b0c0c', weight: 1, smoothFactor: 2 });
+  }
+
+  static createCircle(radius, point) {
+    return L.circle(point, { radius, color: '#0b0c0c', weight: 1 });
+  }
+
+  static createPopupMarker(point, marker, variant) {
+    const popupHTML = marker.querySelector('.pop-up');
+    popupHTML.parentNode.removeChild(popupHTML);
+    popupHTML.hidden = false;
+
+    const markerTitle = popupHTML.querySelector('.marker-title').textContent;
+
+    const leafletMarker = MapController.createMarker(point, markerTitle, variant);
+
+    leafletMarker.bindPopup(
+      popupHTML.outerHTML,
+      { className: 'map-component__map__popup' },
+    );
+
+    leafletMarker.on('keydown', MapController.closeMarkerPopup);
+
+    return leafletMarker;
+  }
+
+  static createMarker(point, markerTitle, variant) {
+    return L.marker(point, {
+      icon: MapController.markerIcon(markerTitle, variant),
+      riseOnHover: true,
     });
   }
 
-  static openMarkerPopup(e) {
-    e.target.openPopup();
+  static markerIcon(title, variant) {
+    return L.divIcon({
+      className: `icon icon--map-${variant} map-component__map__marker`,
+      iconSize: [22, 30],
+      html: `<span class="govuk-visually-hidden">${title}</span>`,
+    });
+  }
+
+  static closeMarkerPopup(e) {
+    e.target.closePopup();
   }
 };
 
--- app/components/map_component/map.test.js
+++ app/components/map_component/map.test.js
@@ -12,55 +12,102 @@ const initialiseStimulus = () => {
 };
 
 jest.mock('../../frontend/src/lib/api');
+const mockCreate = jest.fn();
+const mockCluster = jest.fn();
+const mockCreatePolygon = jest.fn();
+const mockAddMarkerToCluster = jest.fn();
+const mockSetMapBounds = jest.fn();
+const mockcreatePopupMarker = jest.fn();
+let spies;
 
-let createSpy;
-let markerSpy;
-let mapBoundsSpy;
+const getMarkerHTML = (count) => {
+  let markerHTML = '';
+  for (let step = 0; step < count; step += 1) {
+    markerHTML += '<div data-map-target="marker" data-lat="51" data-lon="0.14"><div class="pop-up"></div></div>';
+  }
+  return markerHTML;
+};
 
-describe('map', () => {
-  beforeAll(() => {
+beforeEach(() => {
+  document.body.innerHTML = `<div class="map-component" id="map-component" data-controller="map" data-zoom="12">
+  <div data-map-target="markersTextList" id="markers"></div>
+  <div class="map-component__map" id="map"></div>
+  </div>`;
+  jest.resetAllMocks();
+});
+
+beforeAll(() => {
+  MapController.prototype.addMapLayer = jest.fn();
+
+  jest.mock('./map', () => jest.fn().mockImplementation(() => ({
+    create: mockCreate,
+    createCluster: mockCluster,
+    createPopupMarker: mockcreatePopupMarker,
+    createPolygon: mockCreatePolygon,
+    addMarkerToCluster: mockAddMarkerToCluster,
+    setMapBounds: mockSetMapBounds,
+  })));
+
+  spies = {
+    create: jest.spyOn(MapController.prototype, 'create'),
+    createPopupMarker: jest.spyOn(MapController, 'createPopupMarker'),
+    createPolygon: jest.spyOn(MapController, 'createPolygon'),
+    cluster: jest.spyOn(MapController.prototype, 'createCluster'),
+    addMarkerToCluster: jest.spyOn(MapController.prototype, 'addMarkerToCluster'),
+    setMapBounds: jest.spyOn(MapController.prototype, 'setMapBounds'),
+    addMapLayer: jest.spyOn(MapController.prototype, 'addMapLayer'),
+  };
+});
+
+describe('when map is initialised with one item', () => {
+  beforeEach(() => {
+    document.getElementById('markers').insertAdjacentHTML('afterbegin', getMarkerHTML(1));
     initialiseStimulus();
-
-    document.body.innerHTML = `<div class="map-component" data-controller="map" data-zoom="12">
-    <ol data-map-target="markersTextList">
-      <li data-map-target="marker" data-lat="51" data-lon="0.14">
-        <div class="pop-up" hidden>
-          <a href="https://school.example.com">Test School</a>
-        </div>
-      </li>
-      <li data-map-target="marker" data-lat="52" data-lon="0.15">
-        <div class="pop-up" hidden>
-          <a href="https://school2.example.com">Test School 2</a>
-        </div>
-      </li>
-      <li data-map-target="marker" data-lat="53" data-lon="0.16">
-        <div class="pop-up" hidden>
-          <a href="https://school3.example.com">Test School 3</a>
-        </div>
-      </li>
-    </ol>
-    <div class="map-component__map" id="map" role="presentation"></div>
-    </div>`;
-
-    MapController.prototype.create = jest.fn();
-    createSpy = jest.spyOn(MapController.prototype, 'create');
-
-    MapController.prototype.addMarker = jest.fn();
-    markerSpy = jest.spyOn(MapController.prototype, 'addMarker');
-
-    MapController.prototype.setMapBounds = jest.fn();
-    mapBoundsSpy = jest.spyOn(MapController.prototype, 'setMapBounds');
   });
 
-  describe('when map is initialised with several items', () => {
-    test('a map object is created once', () => {
-      expect(createSpy).toHaveBeenCalledTimes(1);
-      expect(createSpy).toHaveBeenCalledWith({ lat: '51', lon: '0.14' }, '12');
-      expect(mapBoundsSpy).toHaveBeenCalledTimes(1);
-    });
+  test('a map object is created with one marker', () => {
+    expect(spies.create).toHaveBeenCalledTimes(1);
+    expect(spies.create).toHaveBeenCalledWith({ lat: '51', lon: '0.14' }, '12');
+    expect(spies.cluster).not.toHaveBeenCalled();
+    expect(spies.createPopupMarker).toHaveBeenCalledTimes(1);
+    expect(spies.addMarkerToCluster).not.toHaveBeenCalled();
+    expect(spies.addMapLayer).not.toHaveBeenCalled();
+    expect(spies.setMapBounds).not.toHaveBeenCalled();
+  });
+});
 
-    test('correct number of items are added to map', () => {
-      expect(markerSpy).toHaveBeenCalledTimes(3);
-    });
+describe('when map is initialised with a polygon', () => {
+  beforeEach(() => {
+    document.getElementById('markers').insertAdjacentHTML('afterbegin', getMarkerHTML(1));
+    document.getElementById('map-component').setAttribute('data-polygon', '[[[0, 0],[0, 1],[1, 1],[1, 0]]]');
+    initialiseStimulus();
+  });
+
+  test('a map object is created with one marker', () => {
+    expect(spies.create).toHaveBeenCalledTimes(1);
+    expect(spies.create).toHaveBeenCalledWith({ lat: '51', lon: '0.14' }, '12');
+    expect(spies.cluster).not.toHaveBeenCalled();
+    expect(spies.createPopupMarker).toHaveBeenCalledTimes(1);
+    expect(spies.createPolygon).toHaveBeenCalledTimes(1);
+    expect(spies.addMarkerToCluster).not.toHaveBeenCalled();
+    expect(spies.addMapLayer).toHaveBeenCalledTimes(1);
+    expect(spies.setMapBounds).not.toHaveBeenCalled();
+  });
+});
+
+describe('when map is initialised with more than one marker', () => {
+  beforeEach(() => {
+    document.getElementById('markers').insertAdjacentHTML('afterbegin', getMarkerHTML(3));
+    initialiseStimulus();
+  });
+
+  test('a map object is created with markers added to cluster', () => {
+    expect(spies.create).toHaveBeenCalledTimes(1);
+    expect(spies.create).toHaveBeenCalledWith({ lat: '51', lon: '0.14' }, '12');
+    expect(spies.cluster).toHaveBeenCalledTimes(1);
+    expect(spies.createPopupMarker).toHaveBeenCalledTimes(3);
+    expect(spies.addMarkerToCluster).toHaveBeenCalledTimes(3);
+    expect(spies.addMapLayer).toHaveBeenCalledTimes(1);
+    expect(spies.setMapBounds).toHaveBeenCalledTimes(1);
   });
 });
