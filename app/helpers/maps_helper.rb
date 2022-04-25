053d6b4ca2e450af896e4f694c0ea2a102226469
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2022-04-25 10:49:35 +0100

    Add vacancy_map_markers helper

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,4 +1,22 @@
 module MapsHelper
+  def vacancy_map_markers(vacancies)
+    vacancies.map { |vacancy|
+      vacancy.organisations.map do |organisation|
+        {
+          geopoint: organisation.geopoint,
+          heading: govuk_link_to(vacancy.job_title, job_path(vacancy)),
+          address: full_address(organisation),
+          details: [
+            { label: t("jobs.salary"), value: salary_value(vacancy) },
+            { label: organisation_type_label(vacancy), value: organisation_type(organisation) },
+            { label: t("jobs.working_patterns"), value: vacancy.readable_working_patterns },
+            { label: t("jobs.expires_at"), value: format_time_to_datetime_at(vacancy.expires_at) },
+          ],
+        }
+      end
+    }.flatten
+  end
+
   def organisation_map_markers(vacancy)
     vacancy.organisations.map do |organisation|
       {
