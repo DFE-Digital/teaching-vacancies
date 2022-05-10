ca012d0add3785dae212b97d7a9429f17df7d334
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2022-05-10 12:32:26 +0100

    Support job listing show page markers

--- app/controllers/api/markers_controller.rb
+++ app/controllers/api/markers_controller.rb
@@ -4,18 +4,15 @@ class Api::MarkersController < Api::ApplicationController
   include DatesHelper
 
   before_action :verify_json_request, only: %i[show]
+  before_action :check_valid_params, only: %i[show]
 
   def show
     render json: {
-      heading_text: vacancy.job_title,
-      heading_url: job_path(vacancy),
+      heading_text: heading_text,
+      heading_url: heading_url,
       address: full_address(organisation),
-      details: [
-        { label: t("jobs.salary"), value: vacancy.salary },
-        { label: organisation_type_label(vacancy), value: organisation_type(organisation) },
-        { label: t("jobs.working_patterns"), value: working_patterns(vacancy) },
-        { label: t("jobs.expires_at"), value: format_time_to_datetime_at(vacancy.expires_at) },
-      ],
+      description: description,
+      details: details,
     }
   end
 
@@ -27,5 +24,34 @@ class Api::MarkersController < Api::ApplicationController
 
   def organisation
     @organisation ||= Organisation.find(params[:parent_id])
+  end
+
+  def heading_text
+    params[:marker_type] == "vacancy" ? vacancy.job_title : organisation.name
+  end
+
+  def heading_url
+    params[:marker_type] == "vacancy" ? job_path(vacancy) : organisation.url
+  end
+
+  def description
+    return if params[:marker_type] == "vacancy"
+
+    organisation_type(organisation)
+  end
+
+  def details
+    return if params[:marker_type] == "organisation"
+
+    [
+      { label: t("jobs.salary"), value: vacancy.salary },
+      { label: organisation_type_label(vacancy), value: organisation_type(organisation) },
+      { label: t("jobs.working_patterns"), value: working_patterns(vacancy) },
+      { label: t("jobs.expires_at"), value: format_time_to_datetime_at(vacancy.expires_at) },
+    ]
+  end
+
+  def check_valid_params
+    return render(json: { error: "Missing params" }, status: :bad_request) unless params[:parent_id] && params[:marker_type]
   end
 end
--- spec/requests/api/markers_spec.rb
+++ spec/requests/api/markers_spec.rb
@@ -4,14 +4,15 @@ RSpec.describe "Api::Markers" do
   let(:json) { JSON.parse(response.body, symbolize_names: true) }
   let(:organisation) { create(:school) }
   let(:vacancy) { create(:vacancy, organisations: [organisation]) }
+  let(:marker_type) { "vacancy" }
 
   describe "GET /api/v1/markers/:id.json?parent_id=:parent_id", json: true do
     subject do
-      get api_marker_path(vacancy.id, api_version: 1), params: { parent_id: organisation.id, format: :json }
+      get api_marker_path(vacancy.id, api_version: 1), params: { parent_id: organisation.id, marker_type: marker_type, format: :json }
     end
 
     it "returns status :not_found if the request format is not JSON" do
-      get api_marker_path(vacancy.id, api_version: 1), params: { parent_id: organisation.id, format: :html }
+      get api_marker_path(vacancy.id, api_version: 1), params: { parent_id: organisation.id, marker_type: marker_type, format: :html }
 
       expect(response.status).to eq(Rack::Utils.status_code(:not_found))
     end
@@ -28,11 +29,37 @@ RSpec.describe "Api::Markers" do
       expect(response.status).to eq(Rack::Utils.status_code(:ok))
     end
 
-    it "returns the JSON marker" do
-      subject
-      expect(json).to include(heading_text: vacancy.job_title)
-      expect(json).to include(heading_url: job_path(vacancy))
-      expect(json).to include(address: full_address(organisation))
+    context "when marker_type is vacancy" do
+      it "returns the JSON marker" do
+        subject
+        expect(json).to include(heading_text: vacancy.job_title)
+        expect(json).to include(heading_url: job_path(vacancy))
+        expect(json).to include(address: full_address(organisation))
+        expect(json).to include(description: nil)
+        expect(json[:details].size).to be 4
+      end
+    end
+
+    context "when marker_type is organisation" do
+      let(:marker_type) { "organisation" }
+
+      it "returns the JSON marker" do
+        subject
+        expect(json).to include(heading_text: organisation.name)
+        expect(json).to include(heading_url: organisation.url)
+        expect(json).to include(address: full_address(organisation))
+        expect(json).to include(description: organisation_type(organisation))
+        expect(json).to include(details: nil)
+      end
+    end
+
+    context "when missing params" do
+      it "returns status :bad_request" do
+        get api_marker_path(vacancy.id, api_version: 1), params: { format: :json }
+
+        expect(response).to have_http_status(:bad_request)
+        expect(json[:error]).to eq("Missing params")
+      end
     end
   end
 end
