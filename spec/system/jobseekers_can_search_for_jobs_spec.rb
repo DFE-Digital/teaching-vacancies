e7272c2105470dbcbe197d298e3b3e5f1a3b50ac
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2022-06-23 11:16:42 +0100

    Remove old IDs for algolia

--- spec/system/jobseekers_can_search_for_jobs_spec.rb
+++ spec/system/jobseekers_can_search_for_jobs_spec.rb
@@ -65,13 +65,13 @@ end
 
 RSpec.describe "Jobseekers can search for jobs on the jobs index page" do
   let(:school) { create(:school) }
-  let!(:maths_job1) { create(:vacancy, :past_publish, publish_on: Date.current - 1, id: "67991ea9-431d-4d9d-9c99-a78b80108fe1", job_title: "Maths 1", job_roles: %i[teacher], subjects: %w[Mathematics], organisations: [school]) }
-  let!(:maths_job2) { create(:vacancy, :past_publish, publish_on: Date.current - 2, id: "7bfadb84-cf30-4121-88bd-a9f958440cc9", job_title: "Maths Teacher 2", job_roles: %i[teacher], subjects: %w[Mathematics], organisations: [school]) }
-  let!(:job1) { create(:vacancy, :past_publish, id: "20cc99ff-4fdb-4637-851a-68cf5f8fea9f", job_title: "Physics Teacher", job_roles: %i[teacher], subjects: [], organisations: [school]) }
-  let!(:job2) { create(:vacancy, :past_publish, id: "9910d184-5686-4ffc-9322-69aa150c19d3", job_title: "PE Teacher", job_roles: %i[teacher], subjects: [], organisations: [school]) }
-  let!(:job3) { create(:vacancy, :past_publish, id: "3bf67da6-039c-4ee1-bf59-8475672a0d2b", job_title: "Chemistry Teacher", job_roles: %i[teacher], subjects: [], organisations: [school]) }
-  let!(:job4) { create(:vacancy, :past_publish, id: "e750baf6-cc9a-4b93-84cf-ee4e5f8a7ee4", job_title: "Geography Teacher", job_roles: %i[teacher], subjects: [], organisations: [school]) }
-  let!(:expired_job) { create(:vacancy, :expired, id: "0f86d38c-56d4-48d3-b8a2-474f19d4908e", job_title: "Maths Teacher", job_roles: %i[teacher], subjects: [], organisations: [school]) }
+  let!(:maths_job1) { create(:vacancy, :past_publish, publish_on: Date.current - 1, job_title: "Maths 1", job_roles: %i[teacher], subjects: %w[Mathematics], organisations: [school]) }
+  let!(:maths_job2) { create(:vacancy, :past_publish, publish_on: Date.current - 2, job_title: "Maths Teacher 2", job_roles: %i[teacher], subjects: %w[Mathematics], organisations: [school]) }
+  let!(:job1) { create(:vacancy, :past_publish, job_title: "Physics Teacher", job_roles: %i[teacher], subjects: [], organisations: [school]) }
+  let!(:job2) { create(:vacancy, :past_publish, job_title: "PE Teacher", job_roles: %i[teacher], subjects: [], organisations: [school]) }
+  let!(:job3) { create(:vacancy, :past_publish, job_title: "Chemistry Teacher", job_roles: %i[teacher], subjects: [], organisations: [school]) }
+  let!(:job4) { create(:vacancy, :past_publish, job_title: "Geography Teacher", job_roles: %i[teacher], subjects: [], organisations: [school]) }
+  let!(:expired_job) { create(:vacancy, :expired, job_title: "Maths Teacher", job_roles: %i[teacher], subjects: [], organisations: [school]) }
   let(:per_page) { 2 }
 
   context "when searching using the mobile search fields" do
