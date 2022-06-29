61576f3a2b85e5a062e3bf2b0aaf2680968c4ca4
Christian Sutter <git@doublesignal.com>
2022-06-29 09:53:25 +0000

    Move Backstop folder to root directory
    This isn't really part of our application code, so shouldn't live in
    `app/`, especially now that we've gotten rid of the `app/frontend`
    folder.

--- app/frontend/backstop/lib/reset.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.rmdir('app/frontend/backstop/lib/.tmp', { recursive: true, force: true }, (error) => {
-  if (!error) {
-    console.log('\nExisting cookies removed\n');
-
-    fs.mkdirSync('app/frontend/backstop/lib/.tmp');
-  }
-});
