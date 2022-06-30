c5c1c45d47a79509059ddca83287ebbcd9f6a917
Christian Sutter <git@doublesignal.com>
2022-06-30 08:49:49 +0000

    Make `esbuild` use Babel for transpilation
    We still want to provide functional (but not necessarily 100% compliant)
    support for IE11 for now, so need to add Babel back in to our Javascript
    pipeline as `esbuild` is only able to transpile down to ES6 (which isn't
    fully supported in IE11).
    
    This doesn't add much code for now as we still use Babel for Jest anyway
    which doesn't support ESM properly, so all we need to do is hook it up
    and configure it.
    
    - Add `esbuild-plugin-babel`
    - Add custom `esbuild` configuration including Babel plugin and
      configure with IE11 target

---
+++ esbuild.config.js
@@ -0,0 +1,31 @@
+import esbuild from 'esbuild';
+import babel from 'esbuild-plugin-babel';
+
+const watch = process.argv[process.argv.length - 1] == '--watch';
+
+// Custom `esbuild` configuration to enable Babel plugin for IE11-compatible transpilation
+// TODO: When we decide we are happy to target ES6 and don't need Babel anymore, this file can be
+//       removed and the `build` task in `package.json` can be replaced with a simple command
+//       line invocation of `esbuild`, e.g.:
+//
+//          esbuild app/assets/javascript/*.* --bundle --sourcemap --outdir=app/assets/builds \
+//          --public-path=assets --target=es6
+
+esbuild
+    .build({
+        entryPoints: ['app/assets/javascript/application.js'],
+        bundle: true,
+        outdir: 'app/assets/builds',
+        publicPath: 'assets',
+        watch: watch,
+        plugins: [
+          babel({
+            config: {
+              presets: ['@babel/preset-env'],
+              targets: '> 0.25%, not dead, IE 11'
+            }
+          })
+        ],
+        target: ['ie11']
+    })
+    .catch(() => process.exit(1));
