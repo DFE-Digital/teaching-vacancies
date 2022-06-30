315f0a4196c2d5958d90c0770d3ba97b12e3fe92
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2022-06-30 15:03:48 +0100

    Refactor vacancy presenter spec

--- spec/presenters/vacancy_presenter_spec.rb
+++ spec/presenters/vacancy_presenter_spec.rb
@@ -1,139 +1,149 @@
 require "rails_helper"
 
+RSpec.shared_examples "a fields that outputs the correct HTML" do |field|
+  context "when the field has undesired tags" do
+    let(:vacancy) { build_stubbed(:vacancy, field => unsanitized_text) }
+    let(:unsanitized_text) { "<script> call();</script>Sanitized content" }
+    let(:sanitized_text) { "<p> call();Sanitized content</p>" }
+
+    it "sanitizes and transforms the field into safe HTML" do
+      expect(subject.public_send(field)).to eq(sanitized_text)
+    end
+  end
+
+  context "when the field is well-formatted (has line breaks between list items)" do
+    let(:vacancy) { build_stubbed(:vacancy, field => well_formatted_text) }
+    let(:well_formatted_text) do
+      "Some list\n" \
+      "Some itmes to follow:\n" \
+      "·   Item number one\n" \
+      "·   Item number two\n" \
+      "·   Item number three\n" \
+      "Apply now. "
+    end
+    let(:well_formatted_html) do
+      "<p>Some list\n<br />" \
+      "Some itmes to follow:\n<br />" \
+      "•   Item number one\n<br />" \
+      "•   Item number two\n<br />" \
+      "•   Item number three\n<br />" \
+      "Apply now. </p>"
+    end
+
+    it "does not reformat text" do
+      expect(subject.public_send(field)).to eq(well_formatted_html)
+    end
+  end
+
+  context "when the field is badly formatted (no line breaks between list items)" do
+    let(:vacancy) { build_stubbed(:vacancy, field => badly_formatted_text) }
+
+    let(:badly_formatted_text) do
+      " Sentence one. Sentence two.\n" \
+      "Paragraph two. Qualifications:\n" \
+      "•    Skill one." \
+      "•    Skill two." \
+      "•    Skill three.\n" \
+      "Penultimate paragraph.\n" \
+      "Last paragraph \n"
+    end
+
+    let(:well_formatted_html) do
+      "<p> Sentence one. Sentence two.\n<br />" \
+      "Paragraph two. Qualifications:\n<br />" \
+      "<ul>\n<br /><li>    Skill one.</li>\n<br />" \
+      "<li>    Skill two.</li>\n<br />" \
+      "<li>    Skill three.</li>\n<br /></ul>" \
+      "\n<br />Penultimate paragraph.\n<br />" \
+      "Last paragraph </p>"
+    end
+
+    it "transforms badly formatted inline bullet point symbols into validly formatted <li> tags" do
+      expect(subject.public_send(field)).to eq(well_formatted_html)
+    end
+  end
+
+  # For backwards compatibility. Rich-text editing was removed 16th August 2021.
+  context "when the field was made using a rich text editor" do
+    context "when the field is well-formatted (has line breaks between list items)" do
+      let(:vacancy) { build_stubbed(:vacancy, field => well_formatted_text) }
+
+      let(:well_formatted_text) do
+        "<div><!--block-->&nbsp;</div><div><!--block--><strong>Some list</strong>&nbsp;<br><br></div>" \
+        "<div><!--block-->Some itmes to follow:&nbsp;<br><br></div>" \
+        "<div><!--block-->· &nbsp; Item number one;&nbsp;<br><br></div>" \
+        "<div><!--block-->· &nbsp; Item number two;&nbsp;<br><br></div>" \
+        "<div><!--block-->· &nbsp; Item number three;&nbsp;<br><br></div>" \
+        "<div><!--block-->Apply now.&nbsp;</div>"
+      end
+
+      let(:well_formatted_html) do
+        "<p> <strong>Some list</strong> </p>\n\n" \
+        "<p>Some itmes to follow: </p>\n\n" \
+        "<p>•   Item number one; </p>\n\n" \
+        "<p>•   Item number two; </p>\n\n" \
+        "<p>•   Item number three; </p>\n\n" \
+        "<p>Apply now. </p>"
+      end
+
+      it "does not reformat text" do
+        expect(subject.public_send(field)).to eq(well_formatted_html)
+      end
+    end
+
+    context "when the field is badly formatted" do
+      let(:vacancy) { build_stubbed(:vacancy, field => badly_formatted_text) }
+
+      let(:badly_formatted_text) do
+        "<div><!--block-->&nbsp;</div><div><!--block-->Sentence one. Sentence two.&nbsp;<br><br></div>" \
+        "<div><!--block-->Paragraph two. Qualifications:&nbsp;<br><br></div>" \
+        "<div><!--block-->•&nbsp; &nbsp; Skill one.&nbsp;</div>" \
+        "<div><!--block-->•&nbsp; &nbsp; Skill two.&nbsp;</div>" \
+        "<div><!--block-->•&nbsp; &nbsp; Skill three.&nbsp;<br><br></div>" \
+        "<div><!--block-->Penultimate paragraph.&nbsp;<br><br>" \
+        "Last paragraph &nbsp;<br><br></div><div>"
+      end
+
+      let(:well_formatted_html) do
+        "<p> Sentence one. Sentence two. </p>\n\n" \
+        "<p>Paragraph two. Qualifications: </p>\n\n" \
+        "<p><ul>\n<br /><li>  Skill one.</li>\n<br />" \
+        "<li>  Skill two.</li>\n<br />" \
+        "<li>  Skill three.</li>\n<br />" \
+        "</ul></p>\n\n<p>Penultimate paragraph. </p>\n\n" \
+        "<p>Last paragraph  </p>"
+      end
+
+      it "transforms badly formatted inline bullet point symbols into validly formatted <li> tags" do
+        expect(subject.public_send(field)).to eq(well_formatted_html)
+      end
+    end
+  end
+end
+
 RSpec.describe VacancyPresenter do
   subject { described_class.new(vacancy) }
 
   let(:vacancy) { build(:vacancy) }
 
   describe "#job_advert" do
-    let(:vacancy) { build_stubbed(:vacancy, job_advert: "<script> call();</script>Sanitized content") }
-
-    it "sanitizes and transforms the job_advert into HTML" do
-      expect(subject.job_advert).to eq("<p> call();Sanitized content</p>")
-    end
-
-    context "when the advert is well-formatted (has line breaks between list items)" do
-      let(:vacancy) do
-        build(:vacancy, job_advert:
-          "Deputy Head Teacher\n" \
-         "To be successful, you will:\n" \
-         "·   Have quality one\n" \
-         "·   Have quality two\n" \
-         "·   Have quality three\n" \
-         "Apply now. ")
-      end
-
-      it "does not reformat" do
-        expect(subject.job_advert).to eq(
-          "<p>Deputy Head Teacher\n<br />" \
-          "To be successful, you will:\n<br />" \
-          "•   Have quality one\n<br />" \
-          "•   Have quality two\n<br />" \
-          "•   Have quality three\n<br />" \
-          "Apply now. </p>",
-        )
-      end
-    end
-
-    context "when the advert is badly formatted (no line breaks between list items)" do
-      let(:vacancy) do
-        build(:vacancy, job_advert:
-          " Sentence one. Sentence two.\n" \
-          "Paragraph two. Qualifications:\n" \
-          "•    Skill one." \
-          "•    Skill two." \
-          "•    Skill three.\n" \
-          "Penultimate paragraph.\n" \
-          "Last paragraph \n")
-      end
-
-      it "transforms badly formatted inline bullet point symbols into validly formatted <li> tags" do
-        expect(subject.job_advert).to eq(
-          "<p> Sentence one. Sentence two.\n<br />" \
-          "Paragraph two. Qualifications:\n<br />" \
-          "<ul>\n<br /><li>    Skill one.</li>\n<br />" \
-          "<li>    Skill two.</li>\n<br />" \
-          "<li>    Skill three.</li>\n<br /></ul>" \
-          "\n<br />Penultimate paragraph.\n<br />" \
-          "Last paragraph </p>",
-        )
-      end
-    end
-
-    # For backwards compatibility. Rich-text editing was removed 16th August 2021.
-    context "when the advert was made using a rich text editor" do
-      context "when the advert is well-formatted (has line breaks between list items)" do
-        let(:vacancy) do
-          build_stubbed(:vacancy, job_advert:
-            "<div><!--block-->&nbsp;</div><div><!--block--><strong>Deputy Head Teacher</strong>&nbsp;<br><br></div>" \
-         "<div><!--block-->To be successful, you will:&nbsp;<br><br></div>" \
-         "<div><!--block-->· &nbsp; Have quality one;&nbsp;<br><br></div>" \
-         "<div><!--block-->· &nbsp; Have quality two;&nbsp;<br><br></div>" \
-         "<div><!--block-->· &nbsp; Have quality three;&nbsp;<br><br></div>" \
-         "<div><!--block-->Apply now.&nbsp;</div>")
-        end
-
-        it "does not reformat" do
-          expect(subject.job_advert).to eq(
-            "<p> <strong>Deputy Head Teacher</strong> </p>\n\n" \
-          "<p>To be successful, you will: </p>\n\n" \
-          "<p>•   Have quality one; </p>\n\n" \
-          "<p>•   Have quality two; </p>\n\n" \
-          "<p>•   Have quality three; </p>\n\n" \
-          "<p>Apply now. </p>",
-          )
-        end
-      end
-
-      context "when the advert is badly formatted" do
-        let(:vacancy) do
-          build_stubbed(:vacancy, job_advert:
-            "<div><!--block-->&nbsp;</div><div><!--block-->Sentence one. Sentence two.&nbsp;<br><br></div>" \
-          "<div><!--block-->Paragraph two. Qualifications:&nbsp;<br><br></div>" \
-          "<div><!--block-->•&nbsp; &nbsp; Skill one.&nbsp;</div>" \
-          "<div><!--block-->•&nbsp; &nbsp; Skill two.&nbsp;</div>" \
-          "<div><!--block-->•&nbsp; &nbsp; Skill three.&nbsp;<br><br></div>" \
-          "<div><!--block-->Penultimate paragraph.&nbsp;<br><br>" \
-          "Last paragraph &nbsp;<br><br></div><div>")
-        end
-
-        it "transforms badly formatted inline bullet point symbols into validly formatted <li> tags" do
-          expect(subject.job_advert).to eq(
-            "<p> Sentence one. Sentence two. </p>\n\n" \
-          "<p>Paragraph two. Qualifications: </p>\n\n" \
-          "<p><ul>\n<br /><li>  Skill one.</li>\n<br />" \
-          "<li>  Skill two.</li>\n<br />" \
-          "<li>  Skill three.</li>\n<br />" \
-          "</ul></p>\n\n<p>Penultimate paragraph. </p>\n\n" \
-          "<p>Last paragraph  </p>",
-          )
-        end
-      end
-    end
+    it_behaves_like "a fields that outputs the correct HTML", :job_advert
   end
 
   describe "#about_school" do
-    let(:vacancy) { build_stubbed(:vacancy, about_school: "<script> call();</script>Sanitized content") }
-
-    it "sanitizes and transforms about_school into HTML" do
-      expect(subject.about_school).to eq("<p> call();Sanitized content</p>")
-    end
+    it_behaves_like "a fields that outputs the correct HTML", :about_school
   end
 
   describe "#school_visits" do
-    let(:vacancy) { build_stubbed(:vacancy, school_visits: "<script> call();</script>Sanitized content") }
-
-    it "sanitizes and transforms school_visits into HTML" do
-      expect(subject.school_visits).to eq("<p> call();Sanitized content</p>")
-    end
+    it_behaves_like "a fields that outputs the correct HTML", :school_visits
   end
 
   describe "#how_to_apply" do
-    let(:vacancy) { build_stubbed(:vacancy, how_to_apply: "<script> call();</script>Sanitized content") }
+    it_behaves_like "a fields that outputs the correct HTML", :how_to_apply
+  end
 
-    it "sanitizes and transforms school_visits into HTML" do
-      expect(subject.how_to_apply).to eq("<p> call();Sanitized content</p>")
-    end
+  describe "#benefits" do
+    it_behaves_like "a fields that outputs the correct HTML", :benefits
   end
 
   describe "#readable_working_patterns" do
