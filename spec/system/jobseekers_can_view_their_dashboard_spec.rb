2d6f1ca9f892f63693a68c00db05efe4c9d80ce1
alexbowen <alexbowen123@googlemail.com>
2022-07-15 10:32:43 +0100

    hide jobseeker tabs with no content

--- spec/system/jobseekers_can_view_their_dashboard_spec.rb
+++ spec/system/jobseekers_can_view_their_dashboard_spec.rb
@@ -1,24 +1,20 @@
 require "rails_helper"
 
 RSpec.describe "Jobseekers can view their dashboard" do
-  let(:jobseeker) { create(:jobseeker, email: "jobseeker@example.com") }
+  let(:email) { "jobseeker@example.com" }
+  let(:jobseeker) { create(:jobseeker, email: email) }
 
   before do
     login_as(jobseeker, scope: :jobseeker)
     visit jobseekers_account_path
   end
 
-  it "shows their email in the dashboard header" do
-    expect(page).to have_css(".tabs-component") do |dashboard|
-      expect(dashboard).to have_css("h2", text: "jobseeker@example.com")
-    end
-  end
-
   it "shows `Applications` tab" do
-    expect(page).to have_css(".tabs-component") do |dashboard|
-      expect(dashboard).to have_css(".tabs-component-navigation__list") do |nav|
-        expect(nav).to have_link(I18n.t("jobseekers.job_applications.index.page_title"))
-      end
+    within ".tabs-component" do
+      expect(page).to have_link(I18n.t("jobseekers.accounts.show.page_title"))
+      expect(page).to_not have_link(I18n.t("jobseekers.job_applications.index.page_title"))
+      expect(page).to_not have_link(I18n.t("jobseekers.saved_jobs.index.page_title"))
+      expect(page).to_not have_link(I18n.t("jobseekers.subscriptions.index.page_title"))
     end
   end
 end
