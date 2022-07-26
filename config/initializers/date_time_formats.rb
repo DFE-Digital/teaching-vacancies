75b72d08e2a2a6a1ac356e271566d8d445cb123a
alexbowen <alexbowen123@googlemail.com>
2022-07-26 23:34:47 +0100

    Change all times across the service to govuk style

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -7,4 +7,18 @@ Date::DATE_FORMATS[:day_month] = "%-d %B"
 Time::DATE_FORMATS[:default] = "%e %B %Y %-l:%M%P"
 Time::DATE_FORMATS[:date_only] = "%e %B %Y"
 Time::DATE_FORMATS[:date_only_shorthand] = "%e %b %Y"
-Time::DATE_FORMATS[:time_only] = "%-l:%M%P"
+Time::DATE_FORMATS[:time_only] = lambda do |time|
+  minutes = time.strftime("%M")
+  hour = time.strftime("%-l")
+  meridian = time.strftime("%P")
+
+  if hour == "12" && minutes == "00" && meridian == "pm"
+    time.strftime("%-l%P (midday)")
+  elsif hour == "12" && minutes == "00" && meridian == "am"
+    time.strftime("%-l%P (midnight)")
+  elsif minutes == "00"
+    time.strftime("%-l%P")
+  else
+    time.strftime("%-l:%M%P")
+  end
+end
