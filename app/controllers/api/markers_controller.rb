766bdb438cb6c926adc0d5dc1a00e51e4941addb
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2022-08-25 13:50:44 +0100

    Fix map popup

--- app/controllers/api/markers_controller.rb
+++ app/controllers/api/markers_controller.rb
@@ -49,7 +49,7 @@ class Api::MarkersController < Api::ApplicationController
 
     [
       { label: t("jobs.annual_salary"), value: vacancy.salary },
-      { label: t("jobs.school_type"), value: organisation.readable_phase&.capitalize },
+      { label: t("jobs.school_type"), value: organisation_type(organisation) },
       { label: t("jobs.working_patterns"), value: working_patterns(vacancy) },
       { label: t("jobs.expires_at"), value: format_time_to_datetime_at(vacancy.expires_at) },
     ].select { |d| d[:value].present? }
