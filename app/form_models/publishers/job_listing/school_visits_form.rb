9a9d7cfc1b7fab3438ddcbb5d324ef2d082d2044
Rob Young <robertmatthewyoung@gmail.com>
2022-09-30 13:20:11 +0100

    Fix more specs

--- app/form_models/publishers/job_listing/school_visits_form.rb
+++ app/form_models/publishers/job_listing/school_visits_form.rb
@@ -1,6 +1,6 @@
 class Publishers::JobListing::SchoolVisitsForm < Publishers::JobListing::VacancyForm
   validates :school_visits, inclusion: { in: [true, false, "true", "false"] }
-  validates :school_visits_details, presence: true, if: -> { vacancy.school_visits_details.present? }
+  validates :school_visits_details, presence: true, if: -> { vacancy&.school_visits_details.present? }
 
   def self.fields
     %i[school_visits_details school_visits]
