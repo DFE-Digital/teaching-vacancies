6841cfd7225f1e54451fd702c364b804e7700171
BroiSatse <sklajn@gmail.com>
2022-10-26 12:28:18 +0100

    Adding stan to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,7 @@ users = [
   { email: "rose.mackworth-young@digital.education.gov.uk", family_name: "Mackworth-Young", given_name: "Rose" },
   { email: "sabrina.altieri@education.gov.uk", family_name: "Altieri", given_name: "Sabrina" },
   { email: "shahad.rahman@digital.education.gov.uk", family_name: "Rahman", given_name: "Shahad" },
+  { email: "stan.klajn@digital.education.gov.uk", family_name: "Klajn", given_name: "Stan" },
 ]
 
 users.each do |user|
