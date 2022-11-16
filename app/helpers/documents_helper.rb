c44c2f33cd48b3786fc3ecf3322f6610ea024a72
Joseph Hull <josephhull676@yahoo.co.uk>
2022-11-16 17:37:16 +0000

    Fix issue allowing no application form for published job
    Noticed this bug after seeing  errors seen in sentry (https://sentry.io/organizations/teaching-vacancies/issues/3725374099/?project=6212514).
    
    If a hiring staff user publishes a job, decides to change an application form and then deletes the application form without providing
    replacement, the error above is caused. This could be done because after deleting the file, the user could move away from the page,
    leaving the published job without an application form.
    
    In the event of a user wishing to delete an application form, we want to force them to provide a replacement.
    To do this, when clicking the "Delete" link, we reload the page with extra param application_form_staged_for_replacement: true
    (see app/helpers/documents_helper.rb:9). If this param is present, we hide the document
    (a sort of "soft" delete - see app/views/publishers/vacancies/build/application_form.html.slim:16). If it's present we also
    add a hidden field to the form. This added field forces validation to fail if present
    (see app/form_models/publishers/job_listing/application_form_form.rb:63), forcing the user to provide another application form to
    replace the existing one attached to the vacancy. This param is passed on to the view as a local variable below so that the vacancy's
    application form remains hidden after rendering the view as a result of failing validation. This also ensures that validation fails again
    if they don't provide a replacement application form.

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -6,10 +6,10 @@ module DocumentsHelper
     end
   end
 
-  def remove_application_form_link(application_form, vacancy)
-    govuk_link_to organisation_job_application_forms_path(id: application_form.id, job_id: vacancy.id, back_to_review: params[:back_to_review], back_to_show: params[:back_to_show]), method: :delete do
-      safe_join [t("jobs.upload_documents_table.actions.delete"),
-                 tag.span(" #{application_form.filename} supporting document", class: "govuk-visually-hidden")]
+  def remove_application_form_link
+    # See commit message for 1aa28cce3239c42b1af23d61ae08add3e8c51e5e for context
+    govuk_link_to organisation_job_build_path(vacancy.id, :application_form, application_form_staged_for_replacement: true, back_to_review: params[:back_to_review], back_to_show: params[:back_to_show]) do
+      safe_join [t("jobs.upload_documents_table.actions.delete"), tag.span("application form", class: "govuk-visually-hidden")]
     end
   end
 end
