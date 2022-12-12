b48ec8204251b219864ca04adda7a70fe3184235
georgeschena <georgeschena@hotmail.com>
2022-12-12 11:57:24 +0000

    Add config for schools_images_logos_s3_bucket
    Lint Terraform
    
    Fix duplicate name of resource
    
    Fix naming of new s3 bucket paths
    
    Rename storage back to amazon
    
    Rename path
    
    Add common iam role for ManageAttachmentImagesLogosBucketS3
    
    Lint terraform
    
    Rename s3 resource
    
    Change path name
    
    Add IAM user/key/policy management for new bucket

--- terraform/app/modules/paas/main.tf
+++ terraform/app/modules/paas/main.tf
@@ -40,8 +40,22 @@ resource "aws_s3_bucket" "documents_s3_bucket" {
   force_destroy = var.documents_s3_bucket_force_destroy
 }
 
+resource "aws_s3_bucket" "schools_images_logos_s3_bucket" {
+  bucket        = local.schools_images_logos_s3_bucket_name
+  force_destroy = var.schools_images_logos_s3_bucket_force_destroy
+}
+
 resource "aws_s3_bucket_public_access_block" "documents_s3_bucket_block" {
   bucket = aws_s3_bucket.documents_s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "schools_images_logos_s3_bucket_block" {
+  bucket = aws_s3_bucket.schools_images_logos_s3_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -53,6 +67,12 @@ resource "aws_iam_policy" "documents_s3_bucket_policy" {
   name   = "${local.documents_s3_bucket_name}-policy"
   path   = "/attachment_buckets_policies/"
   policy = data.aws_iam_policy_document.documents_s3_bucket_policy_document.json
+}
+
+resource "aws_iam_policy" "schools_images_logos_s3_bucket_policy" {
+  name   = "${local.schools_images_logos_s3_bucket_name}-policy"
+  path   = "/attachment_images_logos_buckets_policies/"
+  policy = data.aws_iam_policy_document.schools_images_logos_s3_bucket_policy_document.json
 }
 
 data "aws_iam_policy_document" "documents_s3_bucket_policy_document" {
@@ -67,18 +87,44 @@ data "aws_iam_policy_document" "documents_s3_bucket_policy_document" {
   }
 }
 
+data "aws_iam_policy_document" "schools_images_logos_s3_bucket_policy_document" {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject"
+    ]
+    resources = ["${aws_s3_bucket.schools_images_logos_s3_bucket.arn}/*"]
+    effect    = "Allow"
+  }
+}
+
 resource "aws_iam_user" "documents_s3_bucket_user" {
   name = "${local.documents_s3_bucket_name}-user"
   path = "/attachment_buckets_users/"
+}
+
+resource "aws_iam_user" "schools_images_logos_s3_bucket_user" {
+  name = "${local.schools_images_logos_s3_bucket_name}-user"
+  path = "/attachment_images_logos_buckets_users/"
 }
 
 resource "aws_iam_access_key" "documents_s3_bucket_access_key" {
   user = aws_iam_user.documents_s3_bucket_user.name
 }
 
+resource "aws_iam_access_key" "schools_images_logos_s3_bucket_access_key" {
+  user = aws_iam_user.schools_images_logos_s3_bucket_user.name
+}
+
 resource "aws_iam_user_policy_attachment" "attachment" {
   user       = aws_iam_user.documents_s3_bucket_user.name
   policy_arn = aws_iam_policy.documents_s3_bucket_policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "attachment_images_logos" {
+  user       = aws_iam_user.schools_images_logos_s3_bucket_user.name
+  policy_arn = aws_iam_policy.schools_images_logos_s3_bucket_policy.arn
 }
 
 resource "cloudfoundry_app" "web_app" {
--- terraform/common/.terraform.lock.hcl
+++ terraform/common/.terraform.lock.hcl
@@ -5,6 +5,7 @@ provider "registry.terraform.io/hashicorp/aws" {
   version     = "3.45.0"
   constraints = ">= 3.45.0, ~> 3.45.0"
   hashes = [
+    "h1:9l/yDPt/OPG6a0ITu7amfq1LjdnWHTsOgn/KOxM26HA=",
     "h1:u1fmVwS10RxbjJEMz43bmRYkhRUuWymNpf1Ci6bBjuM=",
     "zh:0fdbb3af75ff55807466533f97eb314556ec41a908a543d7cafb06546930f7c6",
     "zh:20656895744fa0f4607096b9681c77b2385f450b1577f9151d3070818378a724",
