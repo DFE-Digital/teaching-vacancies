5c9937988a527a9f568594de0d5dbe5ee5d182ac
georgeschena <georgeschena@hotmail.com>
2023-01-04 14:22:22 +0000

    Improve page_path by using proc

--- app/controllers/jobseekers/profile_controller.rb
+++ app/controllers/jobseekers/profile_controller.rb
@@ -3,42 +3,42 @@ class Jobseekers::ProfileController < Jobseekers::BaseController
     {
       title: "Personal details",
       link_text: "Add personal details",
-      page_path: "",
+      page_path: -> { "" },
     },
     {
       title: "Job preferences",
       link_text: "Add job preferences",
-      page_path: "",
+      page_path: -> { "" },
     },
     {
       title: "Qualified teaching status (QTS)",
       link_text: "Add qualified teaching status",
-      page_path: "",
+      page_path: -> { "" },
     },
     {
       title: "Qualifications",
       link_text: "Add qualifications",
-      page_path: "",
+      page_path: -> { "" },
     },
     {
       title: "Work history",
       link_text: "Add roles",
-      page_path: "",
+      page_path: -> { "" },
     },
     {
       title: "About you",
       link_text: "Add details about you",
-      page_path: "",
+      page_path: -> { "" },
     },
     {
       title: "Hide profile from schools or trusts",
       link_text: "Set up who cannot view your profile",
-      page_path: "",
+      page_path: -> { "" },
     },
     {
       title: "Preview and turn on profile",
       link_text: "Preview profile",
-      page_path: "",
+      page_path: -> { "" },
     },
   ].freeze
 
