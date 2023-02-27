9f02e46d6d90d0d3ba777b6eecfe7a339c41dbb4
georgeschena <georgeschena@hotmail.com>
2023-02-27 14:10:25 +0000

    Refactor

--- app/controllers/jobseekers/profiles/preview_controller.rb
+++ app/controllers/jobseekers/profiles/preview_controller.rb
@@ -2,8 +2,13 @@ class Jobseekers::Profiles::PreviewController < Jobseekers::ProfilesController
   include VacanciesHelper
 
   def show
-    @profile = JobseekerProfile.where(jobseeker_id: current_jobseeker.id).first
-    @personal_details = PersonalDetails.where(jobseeker_profile_id: @profile.id).first
-    @job_preferences = JobPreferences.where(jobseeker_profile_id: @profile.id).first
+    @personal_details = PersonalDetails.where(jobseeker_profile_id: jobseeker_profile.id).first
+    @job_preferences = JobPreferences.where(jobseeker_profile_id: jobseeker_profile.id).first
+  end
+
+  private
+
+  def jobseeker_profile
+    JobseekerProfile.where(jobseeker_id: current_jobseeker.id).first
   end
 end
