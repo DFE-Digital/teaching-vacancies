aa1a7917896f740359e171d1f00c40637f2135d0
georgeschena <georgeschena@hotmail.com>
2023-03-16 16:45:30 +0000

    Add job_preferences creation to jobseekers_profile factory bot

--- spec/factories/job_preferences.rb
+++ spec/factories/job_preferences.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :job_preferences do
-    association :jobseeker_profile
+    roles { ["senior_leader"] }
+    phases { ["nursery"] }
+    key_stages { ["early_years"] }
+    subjects { ["maths"] }
+    working_patterns { ["full_time"] }
+    builder_completed { true }
+    completed_steps do
+      {
+        roles: "completed",
+        phases: "completed",
+        key_stages: "completed",
+        subjects: "completed",
+        working_patterns: "completed",
+        locations: "completed",
+      }
+    end
+    jobseeker_profile
   end
 end
