6b4ffe4cb34e8d0a6174acd2adb2f3ce99b768fa
George Schena <40758489+georgeschena@users.noreply.github.com>
2023-03-21 11:24:01 +0000

    Create dummy Candidate Profiles for preview apps and local development (#5882)
    * Add job_preferences creation to jobseekers_profile factory bot
    
    * Add locations
    
    * Added missing job preference translations
    
    * Update tests to reflect factorybot jobseeker_profile creation
    
    ---------
    
    Co-authored-by: Stanislaw Klajn <sklajn@gmail.com>

--- spec/factories/job_preferences.rb
+++ spec/factories/job_preferences.rb
@@ -1,5 +1,23 @@
 FactoryBot.define do
   factory :job_preferences do
-    association :jobseeker_profile
+    roles { ["senior_leader"] }
+    phases { ["nursery"] }
+    key_stages { ["early_years"] }
+    subjects { ["maths"] }
+    working_patterns { ["full_time"] }
+    builder_completed { true }
+    completed_steps do
+      {
+        roles: "completed",
+        phases: "completed",
+        key_stages: "completed",
+        subjects: "completed",
+        working_patterns: "completed",
+        locations: "completed",
+      }
+    end
+
+    locations { build_list(:job_preferences_location, 1, radius: 200, job_preferences: nil) }
+    jobseeker_profile
   end
 end
