7a63808d2fa1a4cbd6bf2049ef6ea07f8b9ae117
Joseph Hull <30624173+josephhull676@users.noreply.github.com>
2023-03-23 18:05:20 +0000

    Merge current organisation into jobseeker_profile_search_params (#5899)

--- app/controllers/publishers/jobseeker_profiles_controller.rb
+++ app/controllers/publishers/jobseeker_profiles_controller.rb
@@ -1,6 +1,6 @@
 class Publishers::JobseekerProfilesController < Publishers::BaseController
   def index
-    @pagy, @jobseeker_profiles = pagy(Search::JobseekerProfileSearch.new(filters).jobseeker_profiles)
+    @pagy, @jobseeker_profiles = pagy(Search::JobseekerProfileSearch.new(jobseeker_profile_search_params).jobseeker_profiles)
     @form = Publishers::JobseekerProfileSearchForm.new(jobseeker_profile_search_params)
   end
 
@@ -11,15 +11,13 @@ class Publishers::JobseekerProfilesController < Publishers::BaseController
   private
 
   def jobseeker_profile_search_params
-    params.permit(locations: [], qualified_teacher_status: [], roles: [], working_patterns: [], education_phases: [], key_stages: [], subjects: []).transform_values(&:compact_blank)
+    params.permit(locations: [], qualified_teacher_status: [], roles: [], working_patterns: [], education_phases: [], key_stages: [], subjects: [])
+          .transform_values(&:compact_blank)
+          .merge(current_organisation:)
   end
 
   def profile
     @profile ||= JobseekerProfile.find(params[:id])
   end
   helper_method :profile
-
-  def filters
-    jobseeker_profile_search_params.merge(current_organisation:)
-  end
 end
