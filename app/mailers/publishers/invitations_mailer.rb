6fbb3c7d561f7607ec006710073ca850a73fffa4
Joseph Hull <josephhull676@yahoo.co.uk>
2023-04-03 18:19:41 +0100

    Correct pluralisation in application invite email

--- app/mailers/publishers/invitations_mailer.rb
+++ app/mailers/publishers/invitations_mailer.rb
@@ -1,17 +1,15 @@
 module Publishers
   class InvitationsMailer < BaseMailer
     def invite_to_apply(job_ids:, jobseeker_id:, publisher_id:, organisation_id:)
-      @jobs = Vacancy.where(id: job_ids)
+      @vacancies = Vacancy.where(id: job_ids)
 
       jobseeker = Jobseeker.find(jobseeker_id)
-      publisher = Publisher.find(publisher_id)
-      organisation = ::Organisation.find(organisation_id)
 
       @jobseeker_name = jobseeker.jobseeker_profile.personal_details.first_name
-      @publisher_name = publisher.given_name
-      @organisation_name = organisation.name
+      @publisher_name = Publisher.find(publisher_id).given_name
+      @organisation_name = ::Organisation.find(organisation_id).name
 
-      @subject = I18n.t("publishers.invitations_mailer.invite_to_apply.subject")
+      @subject = I18n.t("publishers.invitations_mailer.invite_to_apply.subject", count: @vacancies.count)
 
       view_mail(template, to: jobseeker.email, subject: @subject)
     end
