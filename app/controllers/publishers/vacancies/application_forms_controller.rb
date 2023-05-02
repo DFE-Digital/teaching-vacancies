d8126a3d8381acc770ff56226bfde463ca20ed1d
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2023-05-02 16:24:18 +0100

    Add supporting document replaced event

--- app/controllers/publishers/vacancies/application_forms_controller.rb
+++ app/controllers/publishers/vacancies/application_forms_controller.rb
@@ -8,6 +8,7 @@ class Publishers::Vacancies::ApplicationFormsController < Publishers::Vacancies:
       vacancy.application_form.attach(form.application_form) if application_form_uploaded?
       update_vacancy
       send_event if application_form_uploaded?
+      send_dfe_analytics_event if application_form_uploaded?
       redirect_to_next_step
     else
       # See commit message for 1aa28cce3239c42b1af23d61ae08add3e8c51e5e for context
@@ -52,6 +53,27 @@ class Publishers::Vacancies::ApplicationFormsController < Publishers::Vacancies:
         size: vacancy.application_form.byte_size,
         content_type: vacancy.application_form.content_type,
       )
+    end
+  end
+
+  def send_dfe_analytics_event
+    fail_safe do
+      event_data = {
+        vacancy_id: StringAnonymiser.new(vacancy.id),
+        document_type: "application_form",
+        name: vacancy.application_form.filename,
+        size: vacancy.application_form.byte_size,
+        content_type: vacancy.application_form.content_type,
+      }
+
+      event = DfE::Analytics::Event.new
+        .with_type(event_type)
+        .with_request_details(request)
+        .with_response_details(response)
+        .with_user(current_publisher)
+        .with_data(event_data)
+
+      DfE::Analytics::SendEvents.do([event])
     end
   end
 
