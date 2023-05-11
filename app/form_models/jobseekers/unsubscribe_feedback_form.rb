1b8c6949820e98104dfa47f81b32668db1237024
Kyle MacPherson <kaimac2010@gmail.com>
2023-05-11 15:41:27 +0100

    Add occupation to unsubscribe feedback form

--- app/form_models/jobseekers/unsubscribe_feedback_form.rb
+++ app/form_models/jobseekers/unsubscribe_feedback_form.rb
@@ -1,8 +1,9 @@
 class Jobseekers::UnsubscribeFeedbackForm < BaseForm
-  attr_accessor :comment, :email, :other_unsubscribe_reason_comment, :unsubscribe_reason, :user_participation_response
+  attr_accessor :comment, :email, :other_unsubscribe_reason_comment, :unsubscribe_reason, :user_participation_response, :occupation
 
   validates :comment, length: { maximum: 1200 }
   validates :email, presence: true, if: -> { user_participation_response == "interested" }
+  validates :occupation, presence: true, if: -> { user_participation_response == "interested" }
   validates :email, email_address: true, if: -> { email.present? }
   validates :other_unsubscribe_reason_comment, presence: true, if: -> { unsubscribe_reason == "other_reason" }
   validates :unsubscribe_reason, inclusion: { in: Feedback.unsubscribe_reasons.keys }
--- spec/form_models/jobseekers/unsubscribe_feedback_form_spec.rb
+++ spec/form_models/jobseekers/unsubscribe_feedback_form_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe Jobseekers::UnsubscribeFeedbackForm, type: :model do
   subject { described_class.new(params) }
+  let(:user_participation_response) {"interested"}
   let(:params) do
     {
       comment: "Found a job mate",
@@ -11,9 +12,6 @@ RSpec.describe Jobseekers::UnsubscribeFeedbackForm, type: :model do
     }
   end
 
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to allow_value("email@example.com").for(:email) }
-  it { is_expected.to_not allow_value("invalid@email@com").for(:email) }
   it { is_expected.to validate_inclusion_of(:unsubscribe_reason).in_array(Feedback.unsubscribe_reasons.keys) }
   it { is_expected.to validate_length_of(:comment).is_at_most(1200) }
   it {
@@ -32,4 +30,19 @@ RSpec.describe Jobseekers::UnsubscribeFeedbackForm, type: :model do
 
     it { is_expected.to validate_presence_of(:other_unsubscribe_reason_comment) }
   end
+
+  context "when the user_participation_response == 'interested'" do
+    it { is_expected.to validate_presence_of(:occupation) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to allow_value("email@example.com").for(:email) }
+    it { is_expected.to_not allow_value("invalid@email@com").for(:email) }
+  end
+
+  context "when the user_participation_response != 'interested'" do
+    let(:user_participation_response) { "uninterested" }
+
+    it { is_expected.not_to validate_presence_of(:email) }
+    it { is_expected.not_to validate_presence_of(:occupation) }
+  end
+
 end
