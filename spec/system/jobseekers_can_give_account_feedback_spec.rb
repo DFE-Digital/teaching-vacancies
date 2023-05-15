0b0c30d9c3f833dfcba4af3a9f3b209f2b0f38f3
Kyle MacPherson <kaimac2010@gmail.com>
2023-05-15 14:35:03 +0100

    Add occupation to account feedback form

--- spec/system/jobseekers_can_give_account_feedback_spec.rb
+++ spec/system/jobseekers_can_give_account_feedback_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe "Jobseekers can give account feedback" do
   let(:jobseeker) { create(:jobseeker) }
   let(:comment) { "amazing account features!" }
+  let(:occupation) { "Teaching assistant" }
 
   before do
     login_as(jobseeker, scope: :jobseeker)
@@ -16,9 +17,10 @@ RSpec.describe "Jobseekers can give account feedback" do
     choose I18n.t("helpers.label.jobseekers_account_feedback_form.rating_options.somewhat_satisfied")
     choose name: "jobseekers_account_feedback_form[user_participation_response]", option: "interested"
     fill_in "jobseekers_account_feedback_form[comment]", with: comment
+    fill_in "jobseekers_account_feedback_form[occupation]", with: occupation
 
     expect { click_button I18n.t("buttons.submit") }.to change {
-      jobseeker.feedbacks.where(comment: comment, email: jobseeker.email, rating: "somewhat_satisfied", feedback_type: "jobseeker_account", user_participation_response: "interested").count
+      jobseeker.feedbacks.where(comment: comment, email: jobseeker.email, rating: "somewhat_satisfied", feedback_type: "jobseeker_account", user_participation_response: "interested", occupation: occupation).count
     }.by(1)
     expect(current_path).to eq(jobseekers_account_path)
     expect(page).to have_content(I18n.t("jobseekers.account_feedbacks.create.success"))
