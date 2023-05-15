9fc856795fa68d1c6e23296d92c4a1a2d02146c5
Kyle MacPherson <kaimac2010@gmail.com>
2023-05-15 14:38:41 +0100

    Add occupation to publisher job feedback form

--- app/form_models/publishers/job_listing/feedback_form.rb
+++ app/form_models/publishers/job_listing/feedback_form.rb
@@ -1,8 +1,9 @@
 class Publishers::JobListing::FeedbackForm < BaseForm
-  attr_accessor :comment, :email, :rating, :report_a_problem, :user_participation_response
+  attr_accessor :comment, :email, :rating, :report_a_problem, :user_participation_response, :occupation
 
   validates :rating, inclusion: { in: Feedback.ratings.keys }
   validates :email, presence: true, if: -> { user_participation_response == "interested" }
+  validates :occupation, presence: true, if: -> { user_participation_response == "interested" }
   validates :email, email_address: true, if: -> { email.present? }
   validates :user_participation_response, inclusion: { in: Feedback.user_participation_responses.keys }
 end
