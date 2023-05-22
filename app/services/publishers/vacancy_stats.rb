b4a0288f7a73c07c0faae2b0e176aa945e80b2cd
Marc Sardon <marc@bichito.software>
2023-05-22 15:56:37 +0100

    Ensure number of vacancy views stat does not reset
    As we are pushing to BigQ using the real Vacancy id, and all the
    previous view stats data is associated with the anonymised vacancy id,
    we would lose the previous view counters for all the existing views
    prior to removing the Vacancy id anonymisation.
    
    We are ensuring to combine both previous and new views when retrieving
    the number from BigQ.

--- app/services/publishers/vacancy_stats.rb
+++ app/services/publishers/vacancy_stats.rb
@@ -7,21 +7,19 @@ class Publishers::VacancyStats
   end
 
   def number_of_unique_views
-    query_single_field(:number_of_unique_vacancy_views)
-  end
-
-  private
-
-  attr_reader :vacancy
-
-  def query_single_field(field)
+    field = :number_of_unique_vacancy_views
     fail_safe do
       Rails.cache.fetch([field, vacancy.id], expires_in: CACHE_DURATION, skip_nil: true) do
         # publish_on added to make query more efficient as the BigQuery table is partitioned by it
+        #
+        # Why do we search by both Vacancy real id and Anonymised vacancy id?
+        # We stopped anonymising the Vacancy id in the data pushed to BigQ, but the existing BigQ data is associated to
+        # the anonymised id. To reflect the real number of unique vacancy views, we are adding up any views associated
+        # with the Vacancy anonymised id to the new views associated with the real Vacancy id.
         sql = <<~SQL
-          SELECT #{field}
+          SELECT SUM(#{field}) AS #{field}
           FROM `#{Rails.configuration.big_query_dataset}.#{TABLE_NAME}`
-          WHERE id="#{vacancy.id}"
+          WHERE id IN ("#{vacancy.id}", "#{StringAnonymiser.new(vacancy.id)}")
           AND publish_on = "#{vacancy.publish_on.iso8601}"
         SQL
 
@@ -29,6 +27,10 @@ class Publishers::VacancyStats
       end
     end
   end
+
+  private
+
+  attr_reader :vacancy
 
   def big_query
     @big_query ||= Google::Cloud::Bigquery.new
--- spec/services/publishers/vacancy_stats_spec.rb
+++ spec/services/publishers/vacancy_stats_spec.rb
@@ -9,15 +9,16 @@ RSpec.describe Publishers::VacancyStats do
   before do
     allow(Google::Cloud::Bigquery).to receive(:new).and_return(big_query)
     allow(vacancy).to receive(:id).and_return("id")
+    allow(StringAnonymiser).to receive(:new).with("id").and_return("anonymous-id")
   end
 
   describe "#number_of_unique_views" do
     let(:response) { [{ number_of_unique_vacancy_views: 42 }] }
     let(:expected_sql) do
       <<~SQL
-        SELECT number_of_unique_vacancy_views
+        SELECT SUM(number_of_unique_vacancy_views) AS number_of_unique_vacancy_views
         FROM `test_dataset.vacancies_published`
-        WHERE id="id"
+        WHERE id IN ("id", "anonymous-id")
         AND publish_on = "1999-12-31"
       SQL
     end
