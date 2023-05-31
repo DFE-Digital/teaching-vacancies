43f37183fb0800df6e2031d797662cb7f0d720bd
Kyle MacPherson <kaimac2010@gmail.com>
2023-05-31 15:03:24 +0100

    Do not show purpose question for hiring staff users on general feedback form

--- app/controllers/general_feedbacks_controller.rb
+++ app/controllers/general_feedbacks_controller.rb
@@ -1,5 +1,6 @@
 class GeneralFeedbacksController < ApplicationController
   def new
+    @user_type = current_user.try(:class)
     @general_feedback_form = GeneralFeedbackForm.new
   end
 
@@ -8,6 +9,7 @@ class GeneralFeedbacksController < ApplicationController
     @feedback = Feedback.new(feedback_attributes)
 
     if @general_feedback_form.invalid?
+      @user_type = current_user.try(:class)
       render :new
     elsif recaptcha_is_invalid?
       redirect_to invalid_recaptcha_path(form_name: @general_feedback_form.class.name.underscore.humanize)
@@ -22,10 +24,10 @@ class GeneralFeedbacksController < ApplicationController
 
   def general_feedback_form_params
     params.require(:general_feedback_form)
-          .permit(:comment, :email, :report_a_problem, :user_participation_response, :visit_purpose, :rating, :visit_purpose_comment, :occupation)
+          .permit(:comment, :email, :report_a_problem, :user_participation_response, :visit_purpose, :rating, :visit_purpose_comment, :occupation, :user_type)
   end
 
   def feedback_attributes
-    general_feedback_form_params.except("report_a_problem").merge(feedback_type: "general", jobseeker_id: current_jobseeker&.id, publisher_id: current_publisher&.id)
+    general_feedback_form_params.except("report_a_problem", "user_type").merge(feedback_type: "general", jobseeker_id: current_jobseeker&.id, publisher_id: current_publisher&.id)
   end
 end
