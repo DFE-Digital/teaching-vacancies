29f5167d3db423024b99b80b12e3215c522417e3
Kyle MacPherson <kaimac2010@gmail.com>
2023-06-13 16:21:24 +0100

    Allow users to search for either special schools or faith schools on the schools page

--- app/services/search/school_search.rb
+++ app/services/search/school_search.rb
@@ -43,7 +43,7 @@ class Search::SchoolSearch
   end
 
   def clear_filters_params
-    active_criteria.merge({ education_phase: [], key_stage: [], special_school: [], job_availability: [], organisation_types: [] })
+    active_criteria.merge({ education_phase: [], key_stage: [], job_availability: [], organisation_types: [], school_types: [] })
   end
 
   private
@@ -108,20 +108,16 @@ class Search::SchoolSearch
 
     scope.where(school_type: selected_school_types)
   end
-  
+
   def apply_school_type_filter(scope)
-    scope = apply_special_school_filter(scope)
-    apply_faith_school_filter(scope)
-  end
+    return scope unless school_types.present?
 
-  def apply_special_school_filter(scope)
-    return scope unless school_types.present? && school_types.include?("special_school")
+    special_school_types = ["Community special school", "Foundation special school", "Non-maintained special school", "Academy special converter", "Academy special sponsor led", "Free schools special"]
     
-    scope.where(school_type: ["Community special school", "Foundation special school", "Non-maintained special school", "Academy special converter",  "Academy special sponsor led", "Free schools special"])
-  end
+    return scope.where("school_type IN (?) OR gias_data -> 'ReligiousCharacter (name)' IS NOT NULL", special_school_types) if school_types.include?("special_school") && school_types.include?("faith_school")
 
-  def apply_faith_school_filter(scope)
-    return scope unless school_types.present? && school_types.include?("faith_school")
+    return scope.where(school_type: special_school_types) if school_types.include?("special_school")
+      
     scope.where("gias_data -> 'ReligiousCharacter (name)' IS NOT NULL")
   end
 end
