2748ed917668688ec542b5d47010d46be3afad23
Marc Sardon <marc@bichito.software>
2023-07-12 14:36:48 +0100

    Add origin path to Jobseekers account feedback
    As done with the general feedback.

--- app/controllers/jobseekers/account_feedbacks_controller.rb
+++ app/controllers/jobseekers/account_feedbacks_controller.rb
@@ -1,14 +1,14 @@
 class Jobseekers::AccountFeedbacksController < Jobseekers::BaseController
   def new
-    @account_feedback_form = Jobseekers::AccountFeedbackForm.new(origin: params[:origin])
+    @account_feedback_form = Jobseekers::AccountFeedbackForm.new(origin_path: params[:origin])
   end
 
   def create
     @account_feedback_form = Jobseekers::AccountFeedbackForm.new(account_feedback_form_params)
 
     if @account_feedback_form.valid?
-      Feedback.create(feedback_attributes.except(:origin))
-      redirect_to @account_feedback_form.origin, success: t(".success")
+      Feedback.create(feedback_attributes)
+      redirect_to @account_feedback_form.origin_path, success: t(".success")
     else
       render :new
     end
@@ -17,7 +17,8 @@ class Jobseekers::AccountFeedbacksController < Jobseekers::BaseController
   private
 
   def account_feedback_form_params
-    params.require(:jobseekers_account_feedback_form).permit(:comment, :email, :origin, :rating, :report_a_problem, :user_participation_response, :occupation)
+    params.require(:jobseekers_account_feedback_form)
+          .permit(:comment, :email, :origin_path, :rating, :report_a_problem, :user_participation_response, :occupation)
   end
 
   def feedback_attributes
