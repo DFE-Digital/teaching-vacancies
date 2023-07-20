88c6f0643f4598d7e1398bbbe09fbe5e74b34dec
Marc Sardon <marc@bichito.software>
2023-07-21 00:22:17 +0100

    Add parsing of StartDate values from Sources
    Parses the different formats coming from the Vacancy Sources plus
    expected typical variations.
    
    When a date doesn't come in date format, we register it as free text.
    Trying to parse text dates in human language is prone to big errors, so
    leaving those cases as text.

---
+++ app/vacancy_sources/vacancy_source_start_date.rb
@@ -0,0 +1,64 @@
+# Parses, when possible, the start date values coming from the external vacancy sources
+class VacancySourceStartDate
+  # EG: "04-09-2023", "4-9-23", "04/09/2023", "4/9/23", "04.09.2023"
+  REGEXP_DATE_YEAR_END = %r{\d{1,2}[-\/\.]\d{1,2}[-\/\.][2-9]\d{1,3}}
+  # EG: "2023-09-04", "23-9-4", "2023/09/04", "23/9/4", "2023.09.04"
+  REGEXP_DATE_YEAR_START = %r{\d{2,4}[-\/\.]\d{1,2}[-\/\.]\d{1,2}}
+  # Matches either of the above
+  REGEXP_DATE = /(#{REGEXP_DATE_YEAR_START})|(#{REGEXP_DATE_YEAR_END})/
+  # EG: "T12:00:00", "12:00:00"
+  REGEXP_TIME = /T?\d\d:\d\d:\d\d/
+  # EG: "2023-09-04T12:00:00", "2023-09-04 12:00:00", "2023-09-04"
+  REGEXP_DATETIME = /#{REGEXP_DATE}\s?(#{REGEXP_TIME})?/
+  # Ensures the whole string matches the above and no extra info comes together with a datetime value
+  REGEXP_DATETIME_ONLY = /^#{REGEXP_DATETIME}$/
+
+  # Types based on Vacancy#start_date_type values
+  TYPE_SPECIFIC = "specific_date".freeze
+  TYPE_OTHER = "other".freeze
+
+  attr_reader :input, :type, :start_date
+
+  def initialize(date)
+    @input = date
+    return if date.blank?
+
+    parse_start_date(date)
+  end
+
+  private
+
+  def parse_start_date(date)
+    date.strip!
+    if date.match?(REGEXP_DATETIME_ONLY)
+      @type = TYPE_SPECIFIC
+      @start_date = Date.parse(year_first(remove_time(date))).to_s
+    else
+      @type = TYPE_OTHER
+      @start_date = date
+    end
+  rescue Date::Error
+    @type = TYPE_OTHER
+    @start_date = @input # Records the original value if something goes wrong during the parsing.
+  end
+
+  def remove_time(date)
+    time = date.match(REGEXP_TIME).try(:[], 0)
+    return date unless time
+
+    date.gsub(time, "").strip
+  end
+
+  # Turns "04-09-2023" into "2023-09-04"
+  def year_first(date)
+    return date unless date.match?(REGEXP_DATE_YEAR_END)
+
+    separator = separator(date)
+    date.split(separator).reverse.join(separator)
+  end
+
+  # Identifies the separator symbol used in the date ('-', '/' or '.')
+  def separator(date)
+    date.match(%r{[-\/\.]}).try(:[], 0)
+  end
+end
---
+++ spec/vacancy_sources/vacancy_source_start_date_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe VacancySourceStartDate do
+  describe "#new" do
+    it "doesnt't parse a null value" do
+      start_date = described_class.new(nil)
+      expect(start_date).to have_attributes(input: nil, type: nil, start_date: nil)
+    end
+
+    it "doesnt't parse an empty string" do
+      start_date = described_class.new("")
+      expect(start_date).to have_attributes(input: "", type: nil, start_date: nil)
+    end
+
+    it "doesnt't parse an empty string with spaces" do
+      start_date = described_class.new("   ")
+      expect(start_date).to have_attributes(input: "   ", type: nil, start_date: nil)
+    end
+
+    exact_dates = ["04-09-2023", "4-9-23", "04-09-23", "4-09-2023", "04-9-2023", " 04-09-2023 ",
+                   "04/09/2023", "04/09/23", "4/9/23", " 04/09/2023 ",
+                   "04.09.2023", "4.9.2023", "4.9.23", " 04.09.2023 ",
+                   "2023-09-04", "23-9-4", "23-09-04", "2023-09-4", "2023-9-04", " 2023-09-04 ",
+                   "2023/09/04", "23/09/04", "23/9/4", " 2023/09/04 ",
+                   "2023.09.04", "2023.9.4", "23.9.4", " 2023.09.04 ",
+                   "2023-09-04T00:00:00", "2023-09-04 T00:00:00", "2023-09-04 00:00:00",
+                   "04-09-2023T00:00:00", "04-09-2023 T00:00:00", "04-09-2023 00:00:00"]
+
+    exact_dates.each do |date|
+      it "parses '#{date}' as type 'specific_date' with value '2023-09-04'" do
+        start_date = described_class.new(date)
+        expect(start_date).to have_attributes(input: date, type: "specific_date", start_date: "2023-09-04")
+      end
+    end
+
+    freetext_dates = ["TBC", "ASAP", "Immediate", "Flexible", "Flexible start date", "Flexible start date - ASAP",
+                      "1st September 2023 / As soon as possible", "30th October 2023", "September 2023",
+                      "1st January 2024", "September 2023 or January 2024", "Sept 2023",
+                      "2023-09-04 or later", "From 2023-09-04", "2023/09/04 onwards",
+                      "2023-09-04T00:00:00 and next day", "From 2023-09-04T00:00:00"]
+
+    freetext_dates.each do |date|
+      it "parses '#{date}' as type 'other' without modifying the given date" do
+        start_date = described_class.new(date)
+        expect(start_date).to have_attributes(input: date, type: "other", start_date: date)
+      end
+    end
+  end
+end
