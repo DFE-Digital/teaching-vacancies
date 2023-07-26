d71684337fa5f8af7a682d417bf2fce36d18b78d
Marc Sardón <pringaohowto@gmail.com>
2023-07-26 11:54:13 +0100

    Revert "Reapply: Fix activerecord and db inconsistencies"

--- db/migrate/20230724180461_add_organisations_id_organisation_vacancies_organisation_id_fk.rb
+++
@@ -1,8 +0,0 @@
-class AddOrganisationsIdOrganisationVacanciesOrganisationIdFk < ActiveRecord::Migration[7.0]
-  disable_ddl_transaction!
-
-  def change
-    add_foreign_key :organisation_vacancies, :organisations, column: :organisation_id, primary_key: :id, validate: false
-    validate_foreign_key :organisation_vacancies, :organisations
-  end
-end
