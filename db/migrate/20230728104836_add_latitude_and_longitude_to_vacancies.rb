28fec731a85010407ba984edcb242bd6d9c3c480
Kyle MacPherson <kaimac2010@gmail.com>
2023-07-31 09:31:44 +0100

    Remove latitude and longitude

--- db/migrate/20230728104836_add_latitude_and_longitude_to_vacancies.rb
+++
@@ -1,6 +0,0 @@
-class AddLatitudeAndLongitudeToVacancies < ActiveRecord::Migration[7.0]
-  def change
-    add_column :vacancies, :latitude, :float
-    add_column :vacancies, :longitude, :float
-  end
-end
