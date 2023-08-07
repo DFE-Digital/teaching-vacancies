208d9ac99fc0ee587c055b76ee301bb74a891bd5
Marc Sardon <marc@bichito.software>
2023-08-07 16:44:04 +0100

    DO NOT MERGE: Enable ventrus import job for test

--- app/jobs/import_from_vacancy_sources_job.rb
+++ app/jobs/import_from_vacancy_sources_job.rb
@@ -9,9 +9,9 @@ class ImportFromVacancySourcesJob < ApplicationJob
   queue_as :default
 
   def perform
-    return if DisableExpensiveJobs.enabled?
+    # return if DisableExpensiveJobs.enabled?
 
-    SOURCES.each do |source_klass|
+    [VacancySource::Source::Ventrus].each do |source_klass|
       import_start_time = Time.zone.now
 
       source_klass.new.each do |vacancy|
