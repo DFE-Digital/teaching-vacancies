5e7e4e159d80a89761c7fb08ced2caf5d2000ebd
Kyle MacPherson <kaimac2010@gmail.com>
2023-08-22 16:29:33 +0100

    Only sort by distance if someone selects sort by distance

--- app/services/search/vacancy_sort.rb
+++ app/services/search/vacancy_sort.rb
@@ -1,16 +1,29 @@
 class Search::VacancySort < RecordSort
-  def initialize(keyword:)
+  def initialize(keyword:, location: nil)
     @keyword = keyword
+    @location = location
     super
   end
 
   def options
-    [publish_on_desc_option, closing_date_asc_option]
+    if location
+      [distance_option, publish_on_desc_option, closing_date_asc_option]
+    else
+      [publish_on_desc_option, closing_date_asc_option]
+    end
+  end
+
+  def default_sort_option
+    if location
+      distance_option
+    else
+      publish_on_desc_option
+    end
   end
 
   private
 
-  attr_reader :keyword
+  attr_reader :keyword, :location
 
   def publish_on_desc_option
     SortOption.new("publish_on", I18n.t("jobs.sort_by.publish_on.descending"), "desc")
@@ -18,5 +31,9 @@ class Search::VacancySort < RecordSort
 
   def closing_date_asc_option
     SortOption.new("expires_at", I18n.t("jobs.sort_by.expires_at.ascending.vacancy.jobseeker"), "asc")
+  end
+
+  def distance_option
+    SortOption.new("distance", "Distance", "asc")
   end
 end
