83ad5389b3886cde019f32be90897e4742127e29
Marc Sardon <marc@bichito.software>
2023-08-25 09:18:32 +0100

    Add Source tag for Importing Vacancy logs
    So we can also trace the logs for every source.

--- app/jobs/import_from_vacancy_sources_job.rb
+++ app/jobs/import_from_vacancy_sources_job.rb
@@ -27,7 +27,7 @@ class ImportFromVacancySourcesJob < ApplicationJob
 
       PaperTrail.request(whodunnit: "Import from external source") do
         if vacancy.save
-          log("Imported vacancy #{vacancy.id} from feed #{source_name}")
+          log(source_name, "Imported vacancy #{vacancy.id}.")
         else
           failure = create_failed_imported_vacancy(source_name, vacancy)
           errors[failure.external_reference] = failure.import_errors
@@ -40,7 +40,7 @@ class ImportFromVacancySourcesJob < ApplicationJob
 
   def mark_removed_vacancies_from_source(source_name, import_start_time)
     Vacancy.live.where(external_source: source_name, updated_at: (...import_start_time)).find_each do |v|
-      log("Set vacancy #{v.id} as removed from external system")
+      log(source_name, "Set vacancy #{v.id} as removed from external system.")
       v.update_attribute(:status, :removed_from_external_system)
     end
   end
@@ -49,9 +49,10 @@ class ImportFromVacancySourcesJob < ApplicationJob
     failed_imported_vacancy = FailedImportedVacancy.find_by(external_reference: vacancy.external_reference)
 
     if failed_imported_vacancy.present?
-      log("FailedImportedVacancy for #{vacancy.external_reference} from #{source_name} already exists.")
+      log(source_name, "FailedImportedVacancy for #{vacancy.external_reference} already exists.")
       failed_imported_vacancy
     else
+      log(source_name, "Creating FailedImportedVacancy for #{vacancy.external_reference}.")
       FailedImportedVacancy.create(source: source_name,
                                    external_reference: vacancy.external_reference,
                                    import_errors: vacancy.errors.to_json,
@@ -70,13 +71,14 @@ class ImportFromVacancySourcesJob < ApplicationJob
                                                  failed_percentage: })
       scope.set_context("Validation errors for each external source reference", errors.to_hash)
 
+      log(source_name, "#{errors.count} out of #{total_count} (#{failed_percentage}%) vacancies failed to import.")
       Sentry.capture_message("#{source_name} source: #{failed_percentage}% of vacancies failed to import",
                              level: :warning,
                              fingerprint: ["{{ tags.source }}"]) # Groups all the sentry messages for each source
     end
   end
 
-  def log(message)
-    Rails.logger.info("[ImportFromVacancySourcesJob] #{message}")
+  def log(source, message)
+    Rails.logger.info("[ImportFromVacancySourcesJob][#{source}] #{message}")
   end
 end
