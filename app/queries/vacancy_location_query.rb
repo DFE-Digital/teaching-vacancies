c8f8746c921950c865c3f531a66f1ceff3ae1653
Kyle MacPherson <kaimac2010@gmail.com>
2023-08-30 12:07:42 +0100

    Remove unnecessary *args parameter

--- app/queries/vacancy_location_query.rb
+++ app/queries/vacancy_location_query.rb
@@ -3,7 +3,7 @@ class VacancyLocationQuery < LocationQuery
     @scope = scope
   end
 
-  def call(*args)
-    super("vacancies.geolocation", *args)
+  def call(...)
+    super("vacancies.geolocation", ...)
   end
 end
