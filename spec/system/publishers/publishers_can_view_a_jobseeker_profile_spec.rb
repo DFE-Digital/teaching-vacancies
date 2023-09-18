d405f33c066b9b643870f8e0c489057df2e67b20
Kyle MacPherson <kaimac2010@gmail.com>
2023-09-18 14:31:30 +0100

    Add tests for removal of locations from hiring staff view of jobseeker profiles

--- spec/system/publishers/publishers_can_view_a_jobseeker_profile_spec.rb
+++ spec/system/publishers/publishers_can_view_a_jobseeker_profile_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Jobseeker profiles", type: :system do
   let(:publisher) { create(:publisher) }
   let(:organisation) { create(:school) }
-  let(:jobseeker_profile) { create(:jobseeker_profile, :completed) }
+  let(:jobseeker_profile) { create(:jobseeker_profile, :completed, :with_location_preferences) }
 
   scenario "A publisher can view a jobseeker's profile" do
     login_publisher(publisher:, organisation:)
@@ -13,5 +13,6 @@ RSpec.describe "Jobseeker profiles", type: :system do
     expect(page).to have_content(jobseeker_profile.jobseeker.email)
     expect(page).to have_content(jobseeker_profile.qualified_teacher_status_year)
     expect(page).to have_content(jobseeker_profile.about_you)
+    expect(page).not_to have_content("Location")
   end
 end
