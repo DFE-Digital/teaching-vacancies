3875942fe65ecdabe2476fbf8a9cee33fc0f070c
Kyle MacPherson <kaimac2010@gmail.com>
2023-09-18 15:24:29 +0100

    Fix tests

--- spec/support/vacancy_helpers.rb
+++ spec/support/vacancy_helpers.rb
@@ -257,7 +257,7 @@ module VacancyHelpers
     expect(page).to have_content(I18n.t("publishers.vacancies.steps.documents")) if vacancy.supporting_documents.any?
 
     if vacancy.enable_job_applications?
-      sponsorship_inset_text = vacancy.visa_sponsorship_available ? "Skilled worker sponsorship is available for this job." : "Skilled worker sponsorship is not available for this job."
+      sponsorship_inset_text = vacancy.visa_sponsorship_available ? "Skilled worker visa sponsorship is available for this job." : "Skilled worker visa sponsorship is not available for this job."
       expect(page).to have_content sponsorship_inset_text
       expect(page).to have_link(I18n.t("jobseekers.job_applications.apply"), href: new_jobseekers_job_job_application_path(vacancy.id))
     else
