d074f676c75eb17957480e02a9845f9d0356244e
Marc Sardon <marc@bichito.software>
2023-09-18 17:11:36 +0100

    Restor original threshold value

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   include DfE::Analytics::Requests
 
-  SUSPICIOUS_RECAPTCHA_THRESHOLD = 0.1
+  SUSPICIOUS_RECAPTCHA_THRESHOLD = 0.5
   VALID_CLICK_EVENT_TYPES = %w[vacancy_save_to_account_clicked].freeze
 
   add_flash_types :success, :warning
