015561d6cbdf6c9dea44564e5aea4d07c3204d2f
Marc Sardon <marc@bichito.software>
2023-09-22 13:01:30 +0100

    Fix phase mapping for 'Every' source

--- spec/vacancy_sources/vacancy_source/source/every_spec.rb
+++ spec/vacancy_sources/vacancy_source/source/every_spec.rb
@@ -217,4 +217,26 @@ RSpec.describe VacancySource::Source::Every do
       end
     end
   end
+
+  describe "phase mapping" do
+    let(:response_body) { super().gsub("primary", phase) }
+
+    %w[16-19 16_19].each do |phase|
+      context "when the phase is '#{phase}'" do
+        let(:phase) { phase }
+
+        it "maps the phase to '[sixth_form_or_college]' in the vacancy" do
+          expect(vacancy.phases).to eq(["sixth_form_or_college"])
+        end
+      end
+    end
+
+    context "when the phase is 'through_school'" do
+      let(:phase) { "through_school" }
+
+      it "maps the phase to '[through]' in the vacancy" do
+        expect(vacancy.phases).to eq(["through"])
+      end
+    end
+  end
 end
