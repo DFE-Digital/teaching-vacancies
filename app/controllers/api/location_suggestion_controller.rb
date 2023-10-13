df46af39786607d07ed11c77d310776b1e2c02ad
Kyle MacPherson <kaimac2010@gmail.com>
2023-10-13 11:15:54 +0100

    Proof of concept to show how we might be able to exclude non-english towns/cities from our autosuggestion locations

--- app/controllers/api/location_suggestion_controller.rb
+++ app/controllers/api/location_suggestion_controller.rb
@@ -8,10 +8,12 @@ class Api::LocationSuggestionController < Api::ApplicationController
     rescue HTTParty::ResponseError, LocationSuggestion::GooglePlacesAutocompleteError => e
       return render(json: { error: e }, status: :bad_request)
     end
+    
+    filtered_suggestions = exclude_welsh_locations(suggestions)
 
     render json: {
       query: location,
-      suggestions: suggestions,
+      suggestions: filtered_suggestions,
       matched_terms: matched_terms,
     }
   end
@@ -26,5 +28,20 @@ class Api::LocationSuggestionController < Api::ApplicationController
     return render(json: { error: "Missing location input" }, status: :bad_request) if location.nil?
 
     render(json: { error: "Insufficient location input" }, status: :bad_request) if location.length < 3
+  end
+
+  def exclude_welsh_locations(suggestions)
+    places_to_exclude = [
+      "Wales", "Cardiff", "Saint Asaph", "Swansea", "Denbigh", "Newport", "St Davids", "Caerlon", "Lampeter", "Bala", "Pembroke", "Rhayader",
+      "Bangor", "Welshpool", "Llanfyllin", "Rhondaa", "Gelligaer", "Machynlleth", "Kidwelly", "Aberaeron", "Cardigan", "Narberth", "Harlech", "Talgarth",
+      "Gwersyllt", "Aberaman", "Bagillt", "Flint", "Carmarthen", "Buckley", "Neath", "Aberystwyth", "Llantwit Major", "Milford Haven", "Caerphilly", "Tonyrefail",
+      "Maesteg", "Haverfordwest", "Aberdare", "Connah's Quay", "Barry", "Rhyl", "Risca", "Prestatyn", "Brackla", "Conwy", "Flintshire"
+    ]
+
+    postcode_pattern_to_exclude = /(LL|SY|LD|SA|CF|NP)\d/
+
+    suggestions.reject do |str|
+      str.match?(postcode_pattern_to_exclude) || places_to_exclude.include?(str)
+    end
   end
 end
