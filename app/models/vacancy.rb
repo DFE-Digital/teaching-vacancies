5e7f2750227c1ad65651f74bcc84bd402eedba7f
Kyle MacPherson <kaimac2010@gmail.com>
2023-10-24 17:08:59 +0100

    change expiry times

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -98,7 +98,7 @@ class Vacancy < ApplicationRecord
   after_save :reset_markers, if: -> { saved_change_to_status? && (listed? || pending?) }
 
   EQUAL_OPPORTUNITIES_PUBLICATION_THRESHOLD = 5
-  EXPIRY_TIME_OPTIONS = %w[9:00 12:00 17:00 23:59].freeze
+  EXPIRY_TIME_OPTIONS = %w[8:00 9:00 12:00 15:00 23:59].freeze
 
   # Class method added to help with the mapping of array_enums for paper_trail, which stores the changes
   # as an array of integers in the version.
