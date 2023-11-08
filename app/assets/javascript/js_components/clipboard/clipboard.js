d9a9143b089aaf7f4966b13f463d375d147af32e
Marc Sardon <marc@bichito.software>
2023-11-08 15:41:18 +0000

    Extend JS clipboard JS code
    So it has complete behaviour for showing a success message on the
    actioned element when the information has been copied to the clipboard.

--- app/assets/javascript/js_components/clipboard/clipboard.js
+++ app/assets/javascript/js_components/clipboard/clipboard.js
@@ -1,12 +1,22 @@
 import { Controller } from '@hotwired/stimulus';
 import { triggerEvent } from '../../lib/events';
 
+// Brought and adapted from this project:
+// https://github.com/stimulus-components/stimulus-clipboard/blob/master/src/index.ts
 export default class extends Controller {
-  static targets = ['source'];
+  static targets = ['button', 'source'];
+
+  static values = {
+    successContent: String,
+    successDuration: {
+      type: Number,
+      default: 3000,
+    },
+  };
 
   copy() {
-    let text = this.sourceTarget.value ? this.sourceTarget.value : this.sourceTarget.text;
-    navigator.clipboard.writeText(text);
+    const text = this.sourceTarget.value ? this.sourceTarget.value : this.sourceTarget.text;
+    navigator.clipboard.writeText(text).then(() => this.copied());
     triggerEvent(
       'copied_to_clipboard',
       {
@@ -14,5 +24,19 @@ export default class extends Controller {
         subject: this.sourceTarget.dataset.subject,
       },
     );
+  }
+
+  copied() {
+    if (!this.hasButtonTarget) return;
+
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+    this.originalContent = this.buttonTarget.innerHTML;
+    this.buttonTarget.innerHTML = this.successContentValue;
+
+    this.timeout = setTimeout(() => {
+      this.buttonTarget.innerHTML = this.originalContent;
+    }, this.successDurationValue);
   }
 }
