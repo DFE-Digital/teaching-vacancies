230bf8453ec11c3b069eab68a49b430a470bab41
Kyle MacPherson <kaimac2010@gmail.com>
2023-11-27 15:24:26 +0000

    Add visa_sponsorship_available to ventrus ats

--- spec/vacancy_sources/vacancy_source/source/ventrus_spec.rb
+++ spec/vacancy_sources/vacancy_source/source/ventrus_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe VacancySource::Source::Ventrus do
         working_patterns: %w[part_time],
         contract_type: "permanent",
         phases: %w[secondary],
-        visa_sponsorship_available: false,
+        visa_sponsorship_available: true,
       }
     end
 
@@ -185,6 +185,18 @@ RSpec.describe VacancySource::Source::Ventrus do
 
       it "assigns the vacancy job location to the central trust" do
         expect(vacancy.readable_job_location).to eq(school1.name)
+      end
+    end
+
+    context "when visa_sponsorship_available field is not supplied" do
+      before do
+        expect(HTTParty).to receive(:get).with("http://example.com/feed.xml").and_return(file_fixture("vacancy_sources/ventrus_without_visa_sponsorship_available.xml").read)
+      end
+  
+      let(:vacancy) { subject.first }
+  
+      it "defaults visa_sponsorship_available to false" do
+        expect(vacancy.visa_sponsorship_available).to eq false
       end
     end
   end
