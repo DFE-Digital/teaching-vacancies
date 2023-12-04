a5d3e1774ce2d94d4cc3ed8f1f80839d051b8a59
Kyle MacPherson <kaimac2010@gmail.com>
2023-12-04 12:23:03 +0000

    Do not import out of scope schools from every

--- app/vacancy_sources/vacancy_source/source/every.rb
+++ app/vacancy_sources/vacancy_source/source/every.rb
@@ -3,6 +3,20 @@ class VacancySource::Source::Every
 
   FEED_URL = ENV.fetch("VACANCY_SOURCE_EVERY_FEED_URL").freeze
   SOURCE_NAME = "every".freeze
+  EXCLUDED_DETAILED_SCHOOL_TYPES = [
+    "Further education",
+    "Other independent school",
+    "Online provider",
+    "British schools overseas",
+    "Institution funded by other government department",
+    "Miscellaneous",
+    "Offshore schools",
+    "Service children’s education",
+    "Special post 16 institution",
+    "Other independent special school",
+    "Higher education institutions",
+    "Welsh establishment",
+  ].freeze
 
   class EveryImportError < StandardError; end
 
@@ -14,6 +28,9 @@ class VacancySource::Source::Every
 
   def each
     results.each do |result|
+      schools = schools_for(result)
+      next if vacancy_listed_at_excluded_school_type?(schools)
+
       v = Vacancy.find_or_initialize_by(
         external_source: SOURCE_NAME,
         external_reference: result["reference"],
@@ -26,7 +43,7 @@ class VacancySource::Source::Every
       v.publish_on ||= Date.today
 
       begin
-        v.assign_attributes(attributes_for(result))
+        v.assign_attributes(attributes_for(result, schools))
       rescue ArgumentError => e
         v.errors.add(:base, e)
       end
@@ -37,7 +54,7 @@ class VacancySource::Source::Every
 
   private
 
-  def attributes_for(item)
+  def attributes_for(item, schools)
     {
       job_title: item["jobTitle"],
       job_advert: item["jobAdvert"],
@@ -55,7 +72,7 @@ class VacancySource::Source::Every
 
       # TODO: What about central office/multiple school vacancies?
       job_location: :at_one_school,
-    }.merge(organisation_fields(item))
+    }.merge(organisation_fields(item, schools))
      .merge(start_date_fields(item))
   end
 
@@ -70,12 +87,16 @@ class VacancySource::Source::Every
     end
   end
 
-  def organisation_fields(item)
+  def organisation_fields(item, schools)
     {
-      organisations: schools_for(item),
+      organisations: schools,
       readable_job_location: main_organisation(item)&.name,
       about_school: main_organisation(item)&.description,
     }
+  end
+
+  def vacancy_listed_at_excluded_school_type?(schools)
+    (schools.map(&:detailed_school_type) & EXCLUDED_DETAILED_SCHOOL_TYPES).present?
   end
 
   def schools_for(item)
