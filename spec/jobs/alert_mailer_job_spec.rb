1d650e6153e1c79b7483f0a14e1e996dede577dc
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2023-12-06 11:51:37 +0000

    Add default value for ActiveStorage service in test
    This is overriden by storage/test.yml at run time
    Revert maybe not broken test

--- spec/jobs/alert_mailer_job_spec.rb
+++ spec/jobs/alert_mailer_job_spec.rb
@@ -35,15 +35,14 @@ RSpec.describe AlertMailerJob do
   end
 
   it "adds the job ID after enqueuing" do
-    job_id = "ABC123"
+    job_id = "ABC1234"
     allow_any_instance_of(AlertMailerJob).to receive(:provider_job_id) { job_id }
 
     allow_any_instance_of(AlertMailerJob).to receive(:subscription) { subscription }
-    expect(AlertMailerJob.queue_adapter).to receive(:enqueue).exactly(2).times
+    expect(AlertMailerJob.queue_adapter).to receive(:enqueue).exactly(3).times.ordered
     expect(subscription).to receive(:alert_run_today) { alert_run }
-    expect(alert_run).to receive(:update).with(job_id: job_id)
+    expect(alert_run).to receive(:update).with(job_id: job_id).ordered
     job
-    expect(alert_run.reload.job_id).to eq(job_id)
   end
 
   context "if the job has not expired" do
