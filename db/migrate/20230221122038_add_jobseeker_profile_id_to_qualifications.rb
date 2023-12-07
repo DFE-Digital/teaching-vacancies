5d8727ce3ee4dba4b1c960f9cd0bc1fcfc142df3
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2023-12-07 13:45:05 +0000

    Remove unnecessary optional flag

--- db/migrate/20230221122038_add_jobseeker_profile_id_to_qualifications.rb
+++ db/migrate/20230221122038_add_jobseeker_profile_id_to_qualifications.rb
@@ -1,5 +1,5 @@
 class AddJobseekerProfileIdToQualifications < ActiveRecord::Migration[7.0]
   def change
-    add_reference :qualifications, :jobseeker_profile, index: true, optional: true, type: :uuid
+    add_reference :qualifications, :jobseeker_profile, index: true, type: :uuid
   end
 end
