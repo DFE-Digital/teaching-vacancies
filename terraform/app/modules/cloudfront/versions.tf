3f54b737b8c36ef28b4951737b51a84f20bdb010
Tope Emmanuel <topej.emmanuel@gmail.com>
2023-12-13 11:28:42 +0000

    Upgraded hashicorp/aws to latest version 5.29.0
    WHY: The upgrade is required to resolve the phantom changes being shown by terraform
    HOW: By updating the value of version in the provider files to 5.29.0

--- terraform/app/modules/cloudfront/versions.tf
+++ terraform/app/modules/cloudfront/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 3.45.0"
+      version               = "5.29.0"
       configuration_aliases = [aws.aws_us_east_1]
     }
   }
