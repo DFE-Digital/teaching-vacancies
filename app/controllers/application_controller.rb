831cf7abada5aeebe15ac09445ab6ac3b81e55af
Kyle MacPherson <kaimac2010@gmail.com>
2024-01-04 09:32:22 +0000

    Temporarily ignore recaptcha scores while sending out promotional emails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -84,7 +84,8 @@ class ApplicationController < ActionController::Base
   # https://github.com/ambethia/recaptcha#verify_recaptcha
   # https://github.com/ambethia/recaptcha#recaptcha_reply
   def recaptcha_is_invalid?(model = nil)
-    !verify_recaptcha(model: model, action: controller_name, minimum_score: SUSPICIOUS_RECAPTCHA_THRESHOLD) && recaptcha_reply
+    # temporarily ignore recaptcha scores until we can verify that issues with high recaptcha failure rate are fixed.
+    false
   end
 
   def request_event
