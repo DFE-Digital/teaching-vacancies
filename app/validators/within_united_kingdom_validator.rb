3199daca9f29a51e2ff0d652a63245f6b37cfd27
Kyle MacPherson <kaimac2010@gmail.com>
2024-01-05 12:15:45 +0000

    rubocop linting

--- app/validators/within_united_kingdom_validator.rb
+++ app/validators/within_united_kingdom_validator.rb
@@ -1,9 +1,9 @@
-class WithinUnitedKingdomValidator< ActiveModel::EachValidator
+class WithinUnitedKingdomValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    unless Geocoder.search(value).map(&:country).include?("United Kingdom")
-      record.errors.add(attribute, I18n.t("activemodel.errors.models.jobseekers/job_preferences_form/location_form.attributes.location.blank"))
-    end
+    return if Geocoder.search(value).map(&:country).include?("United Kingdom")
+
+    record.errors.add(attribute, I18n.t("activemodel.errors.models.jobseekers/job_preferences_form/location_form.attributes.location.blank"))
   end
 end
