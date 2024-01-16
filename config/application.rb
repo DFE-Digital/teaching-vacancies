54ea6f01161fad39b3f59b831fa4845892ebda47
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-01-16 20:07:34 +0000

    Update path

--- config/application.rb
+++ config/application.rb
@@ -92,7 +92,7 @@ module TeachingVacancies
 
     config.maintenance_mode = ActiveModel::Type::Boolean.new.cast(ENV.fetch("MAINTENANCE_MODE", nil))
 
-    config.assets.paths << Rails.root.join("node_modules/govuk-frontend/govuk/assets")
+    config.assets.paths << Rails.root.join("node_modules/govuk-frontend/dist/govuk/assets")
     config.assets.excluded_paths << Rails.root.join("app/assets/stylesheets")
 
     config.view_component.preview_paths << "#{Rails.root}/app/components/previews"
