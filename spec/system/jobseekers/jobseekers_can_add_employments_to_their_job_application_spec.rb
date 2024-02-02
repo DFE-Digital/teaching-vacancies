e2d71e448c9a14f4cfe04cf369dec718ed307804
Kyle MacPherson <kaimac2010@gmail.com>
2024-02-02 13:44:30 +0000

    Fix tests

--- spec/system/jobseekers/jobseekers_can_add_employments_to_their_job_application_spec.rb
+++ spec/system/jobseekers/jobseekers_can_add_employments_to_their_job_application_spec.rb
@@ -10,8 +10,6 @@ RSpec.describe "Jobseekers can add employments and breaks to their job applicati
   it "allows jobseekers to add a current role" do
     visit jobseekers_job_application_build_path(job_application, :employment_history)
 
-    expect(page).to have_content("No employment specified")
-
     click_on I18n.t("buttons.add_job")
     expect(page).to have_link(I18n.t("buttons.cancel"), href: jobseekers_job_application_build_path(job_application, :employment_history))
     validates_step_complete(button: I18n.t("buttons.save_employment"))
