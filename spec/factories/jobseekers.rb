4fdc5debf95996ef20130e5ddb49db18699c6bbf
Kyle MacPherson <kaimac2010@gmail.com>
2024-02-09 16:01:37 +0000

    Add test for editing account type

--- spec/factories/jobseekers.rb
+++ spec/factories/jobseekers.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :jobseeker do
     email { Faker::Internet.unique.email(domain: "example.com") }
     password { "passw0rd" }
+    account_type { "teacher" }
     confirmed_at { 1.hour.ago }
   end
 end
