13563ec093fba09f867137e93aa01290e57794ed
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-02-09 09:10:32 +0000

    Enable DFE::Analytics Entity Table Check job
    The last version od DFE Analytics added a job that sends entity tables
    info (row count, checksum and timestamp) for each entity sent to BigQ.
    
    This will be used to confirm production DB matching BigQ tables and help
    debugging.

--- spec/configuration/sidekiq_scheduled_jobs_spec.rb
+++ spec/configuration/sidekiq_scheduled_jobs_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "Sidekiq configuration" do
-  let(:all_jobs) do
+  let(:all_app_jobs) do
     # Require all jobs in case autoloading didn't get there
     Rails.root.join("app/jobs").glob("**/*.rb").map { |file| require file }
 
     ApplicationJob.descendants.map(&:name)
   end
-
+  let(:gem_jobs) { %w[DfE::Analytics::EntityTableCheckJob] }
   let(:scheduled_jobs) { YAML.load_file("./config/schedule.yml").map { |_, v| v["class"] } }
   let(:unscheduled_jobs) do
     %w[
@@ -31,6 +31,6 @@ RSpec.describe "Sidekiq configuration" do
   end
 
   it "includes all scheduled jobs in the schedule" do
-    expect(scheduled_jobs).to match_array(all_jobs - unscheduled_jobs)
+    expect(scheduled_jobs).to match_array(all_app_jobs + gem_jobs - unscheduled_jobs)
   end
 end
