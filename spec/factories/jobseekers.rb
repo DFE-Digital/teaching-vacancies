3f306bd9746a92d84522232701e9f331a57cddfb
Kyle MacPherson <kaimac2010@gmail.com>
2024-02-11 22:38:52 +0000

    Fix typo in jobseeker factory

--- spec/factories/jobseekers.rb
+++ spec/factories/jobseekers.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :jobseeker do
     email { Faker::Internet.unique.email(domain: "example.com") }
     password { "passw0rd" }
-    account_type { "teacher" }
+    account_type { "teaching" }
     confirmed_at { 1.hour.ago }
   end
 end
