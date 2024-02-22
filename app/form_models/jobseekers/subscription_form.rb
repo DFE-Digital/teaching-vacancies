f6f8d478d03d7484524e7a20e4bc53722e30204a
Marc Sardon <marc@bichito.software>
2024-02-22 17:40:09 +0000

    Include restored working pattterns on subscription form

--- app/form_models/jobseekers/subscription_form.rb
+++ app/form_models/jobseekers/subscription_form.rb
@@ -59,7 +59,7 @@ class Jobseekers::SubscriptionForm < BaseForm
     @job_role_options = Vacancy.job_roles.keys.map { |option| [option, I18n.t("helpers.label.publishers_job_listing_job_role_form.job_role_options.#{option}")] }
     @phase_options = Vacancy.phases.keys.map { |option| [option, I18n.t("helpers.label.publishers_job_listing_education_phases_form.phases_options.#{option}")] }
     @ect_status_options = [["ect_suitable", I18n.t("jobs.filters.ect_suitable")]]
-    @working_pattern_options = %w[full_time part_time].map do |option|
+    @working_pattern_options = Vacancy.working_patterns.keys.map do |option|
       [option, I18n.t("helpers.label.publishers_job_listing_working_patterns_form.working_patterns_options.#{option}")]
     end
   end
