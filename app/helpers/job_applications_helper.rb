e787ca5bf404dc535ce2ca1aacfd272a796f3b36
Kyle MacPherson <kaimac2010@gmail.com>
2024-03-11 13:58:19 +0000

    Fix visa sponsorship bug

--- app/helpers/job_applications_helper.rb
+++ app/helpers/job_applications_helper.rb
@@ -120,6 +120,14 @@ module JobApplicationsHelper
     job_application.in_progress_steps.include?(step.to_s)
   end
 
+  def visa_sponsorship_available_answer
+    if job_application.right_to_work_in_uk
+      t("jobseekers.profiles.personal_details.work.options.true")
+    else
+      t("jobseekers.profiles.personal_details.work.options.false")
+    end
+  end
+
   def radio_button_legend_hint
     if vacancy.visa_sponsorship_available?
       {
