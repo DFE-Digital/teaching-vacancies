0227789dadd154a4c23fa30f41dc7c883d494616
Kyle MacPherson <kaimac2010@gmail.com>
2024-03-13 15:23:34 +0000

    Add tests for the statuses after prefilling application form from jobseeker profile

---
+++ spec/system/jobseekers/jobseekers_applications_get_prefilled_with_profile_data_spec.rb
@@ -0,0 +1,66 @@
+require "rails_helper"
+
+RSpec.describe "Jobseekers applications are prefilled with profile data" do
+  let!(:jobseeker) { create(:jobseeker) }
+  let(:vacancy) { create(:vacancy, organisations: [school], visa_sponsorship_available: true) }
+  let(:school) { create(:school) }
+
+  context "when the jobseeker has a profile" do
+    context "when the jobseeker has completed details in their profile" do
+      let!(:jobseeker_profile) { create(:jobseeker_profile, :completed, jobseeker: jobseeker) }
+  
+      it "shows all sections with the status tag 'in progress'" do
+        login_as(jobseeker, scope: :jobseeker)
+        visit job_path(vacancy)
+        within ".banner-buttons" do
+          click_on I18n.t("jobseekers.job_applications.banner_links.apply")
+        end
+  
+        click_button "Start application"
+  
+        expect(page).to have_css('#personal_details .review-component__section__heading__status', text: 'in progress')
+        expect(page).to have_css('#professional_status .review-component__section__heading__status', text: 'in progress')
+        expect(page).to have_css('#qualifications .review-component__section__heading__status', text: 'in progress')
+        expect(page).to have_css('#employment_history .review-component__section__heading__status', text: 'in progress')
+      end
+    end
+
+    context "when the jobseeker has not completed any details in their profile" do
+      let!(:jobseeker_profile) { create(:jobseeker_profile, jobseeker: jobseeker, qualified_teacher_status: nil) }
+  
+      it "shows all sections with the status tag 'in progress'" do
+        login_as(jobseeker, scope: :jobseeker)
+        visit job_path(vacancy)
+        within ".banner-buttons" do
+          click_on I18n.t("jobseekers.job_applications.banner_links.apply")
+        end
+  
+        click_button "Start application"
+  
+        expect(page).to have_css('#personal_details .review-component__section__heading__status', text: 'not started')
+        expect(page).to have_css('#professional_status .review-component__section__heading__status', text: 'not started')
+        expect(page).to have_css('#qualifications .review-component__section__heading__status', text: 'not started')
+        expect(page).to have_css('#employment_history .review-component__section__heading__status', text: 'not started')
+      end
+    end
+
+    context "when the jobseeker has completed some details in their profile but not all of them" do
+      let!(:jobseeker_profile) { create(:jobseeker_profile, :with_qualifications, :with_employment_history, jobseeker: jobseeker, qualified_teacher_status: nil) }
+  
+      it "shows all sections that have been filled in with the status tag 'in progress' and shows empty sections with the status tag 'not started'" do
+        login_as(jobseeker, scope: :jobseeker)
+        visit job_path(vacancy)
+        within ".banner-buttons" do
+          click_on I18n.t("jobseekers.job_applications.banner_links.apply")
+        end
+  
+        click_button "Start application"
+  
+        expect(page).to have_css('#personal_details .review-component__section__heading__status', text: 'not started')
+        expect(page).to have_css('#professional_status .review-component__section__heading__status', text: 'not started')
+        expect(page).to have_css('#qualifications .review-component__section__heading__status', text: 'in progress')
+        expect(page).to have_css('#employment_history .review-component__section__heading__status', text: 'in progress')
+      end
+    end
+  end
+end
