29e1a5d06e34223df7c7ed12a97271a3566a2f35
Marc Sardon <marc@bichito.software>
2024-04-18 14:24:44 +0100

    Bump headers level on Timeline component
    To resolve an accessibility issue where the page headers are going from
    H1 to H3 & H4. We are changing the components header levels from H3 & H4
    to H2 & H3.
    This allows for a consistent linear progression of the pages from H1 ->
    H2 --> H3 headers.

--- app/components/timeline_component.rb
+++ app/components/timeline_component.rb
@@ -4,13 +4,13 @@ class TimelineComponent < ApplicationComponent
   end
 
   renders_one :heading, lambda { |title:|
-    tag.h3(class: "timeline-component__heading govuk-heading-s") { title }
+    tag.h2(class: "timeline-component__heading govuk-heading-s") { title }
   }
 
   renders_many :items, lambda { |key:, value:|
     tag.li(class: "timeline-component__item") do
       safe_join([
-        tag.h4(class: "timeline-component__key govuk-heading-s") { key },
+        tag.h3(class: "timeline-component__key govuk-heading-s") { key },
         tag.p(class: "timeline-component__value govuk-body") { value },
       ])
     end
