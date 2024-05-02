ef3c0dad5bb2af0cee3d32b2852727b95848268b
Kyle MacPherson <kaimac2010@gmail.com>
2024-05-02 16:28:18 +0100

    Allow users to update training and cpds data on profile from the job application form

--- spec/system/jobseekers/jobseekers_can_update_their_profile_from_job_applications_spec.rb
+++ spec/system/jobseekers/jobseekers_can_update_their_profile_from_job_applications_spec.rb
@@ -6,13 +6,15 @@ RSpec.describe "Jobseekers can update their profile from job applications" do
   let(:vacancy) { create(:vacancy) }
   let(:application_qualification) { create(:qualification, name: "Application qualification") }
   let(:application_employment) { create(:employment, job_title: "Application employment") }
-  let(:job_application) { create(:job_application, jobseeker: jobseeker, vacancy: vacancy, qualifications: [application_qualification], employments: [application_employment]) }
+  let(:application_training) { create(:training_and_cpd, name: "Application training") }
+  let(:job_application) { create(:job_application, jobseeker: jobseeker, vacancy: vacancy, qualifications: [application_qualification], employments: [application_employment], training_and_cpds: [application_training], training_and_cpds_section_completed: true) }
 
   context "when the jobseekers have a profile" do
     let(:jobseeker) { create(:jobseeker) }
     let(:profile_qualification) { create(:qualification, name: "Original profile qualification") }
     let(:profile_employment) { create(:employment, job_title: "Original profile employment") }
-    let!(:jobseeker_profile) { create(:jobseeker_profile, jobseeker: jobseeker, qualifications: [profile_qualification], employments: [profile_employment]) }
+    let(:profile_training) { create(:training_and_cpd, name: "Original profile training") }
+    let!(:jobseeker_profile) { create(:jobseeker_profile, jobseeker: jobseeker, qualifications: [profile_qualification], employments: [profile_employment], training_and_cpds: [profile_training]) }
 
     before do
       login_as(jobseeker, scope: :jobseeker)
@@ -32,6 +34,9 @@ RSpec.describe "Jobseekers can update their profile from job applications" do
 
       expect(page).to have_css("h3.govuk-summary-card__title", text: "Original profile employment")
       expect(page).not_to have_css("h3.govuk-summary-card__title", text: "Application employment")
+
+      expect(page).to have_css("h3.govuk-summary-card__title", text: "Original profile training")
+      expect(page).not_to have_css("h3.govuk-summary-card__title", text: "Application training")
     end
 
     scenario "jobseekers can update their profile work history using the job application information on submission" do
@@ -46,6 +51,26 @@ RSpec.describe "Jobseekers can update their profile from job applications" do
 
       expect(page).to have_css("h3.govuk-summary-card__title", text: "Application employment")
       expect(page).not_to have_css("h3.govuk-summary-card__title", text: "Original profile employment")
+
+      expect(page).to have_css("h3.govuk-summary-card__title", text: "Original profile training")
+      expect(page).not_to have_css("h3.govuk-summary-card__title", text: "Application training")
+    end
+
+    scenario "jobseekers can update their profile training and cpd using the job application information on submission" do
+      check I18n.t("helpers.label.jobseekers_job_application_review_form.update_profile_options.training_and_cpds")
+      click_on I18n.t("buttons.submit_application")
+      expect(page).to have_content(I18n.t("jobseekers.job_applications.submit.panel.title"))
+
+      click_link "Your profile"
+
+      expect(page).to have_css("h3.govuk-summary-card__title", text: "Original profile qualification")
+      expect(page).not_to have_css("h3.govuk-summary-card__title", text: "Application qualification")
+
+      expect(page).to have_css("h3.govuk-summary-card__title", text: "Original profile employment")
+      expect(page).not_to have_css("h3.govuk-summary-card__title", text: "Application employment")
+
+      expect(page).not_to have_css("h3.govuk-summary-card__title", text: "Original profile training")
+      expect(page).to have_css("h3.govuk-summary-card__title", text: "Application training")
     end
   end
 
