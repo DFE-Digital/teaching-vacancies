46e1f53e8633db220fad7ae910eca39ff2bae8a3
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-05-28 11:42:18 +0100

    Add mobile focus order for ayy1
    On iOS voiceover, the elements on the page are being skippedaa on the profiles
    question pages. The button appears to be the first element selected on load.
    This fixes the issue by forcing the focus of the mobile page on the
    first element of the page itself which is the skip to next section
    button

---
+++ app/assets/javascript/js_components/mobileFocus/mobileFocus.js
@@ -0,0 +1,26 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  connect() {
+    this.handleMobilePageFocus();
+  }
+
+  detectMobile() {
+    return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  }
+
+  handleMobilePageFocus() {
+    if (!this.detectMobile()) {
+      return;
+    }
+
+    if (window.location.href.includes("profile/personal-details")) {
+      let skipLinkElement = document.getElementsByClassName("govuk-skip-link");
+
+      if (skipLinkElement && skipLinkElement.length > 0) {
+        skipLinkElement[0].focus();
+        document.activeElement.blur();
+      }
+    }
+  }
+}
