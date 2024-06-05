43ba2778f0ace12dd1192fc2e6d3e5589f11b9fa
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-06-05 10:53:57 +0100

    Forces controller initialization on every personal details form

--- app/assets/javascript/js_components/mobileFocus/mobileFocus.js
+++ app/assets/javascript/js_components/mobileFocus/mobileFocus.js
@@ -1,21 +1,21 @@
 import { Controller } from '@hotwired/stimulus';
 
-export default class extends Controller {
+export default class MobileFocusController extends Controller {
   connect() {
     this.handleMobilePageFocus();
   }
 
   static detectMobile() {
-    return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }
 
   handleMobilePageFocus() {
-    if (!this.detectMobile()) {
+    if (!this.constructor.detectMobile()) {
       return;
     }
 
     if (window.location.href.includes('profile/personal-details')) {
-      const skipLinkElement = document.getElementsByClassName('govuk-skip-link');
+      const skipLinkElement = document.querySelector('.govuk-skip-link');
 
       if (skipLinkElement && skipLinkElement.length > 0) {
         skipLinkElement[0].focus();
