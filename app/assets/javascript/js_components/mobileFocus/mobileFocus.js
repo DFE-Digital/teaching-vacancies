d9187e77331d2b2b58ac6ae26f4712009da25c09
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-06-05 15:01:59 +0100

    Force the position to be at the top of that page

--- app/assets/javascript/js_components/mobileFocus/mobileFocus.js
+++ app/assets/javascript/js_components/mobileFocus/mobileFocus.js
@@ -14,6 +14,10 @@ export default class MobileFocusController extends Controller {
       return;
     }
 
+    window.onbeforeunload = () => {
+      window.scrollTo(0, 0);
+    };
+
     if (window.location.href.includes('profile/personal-details')) {
       const skipLinkElement = document.querySelector('.govuk-skip-link');
 
