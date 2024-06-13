4326e64005f77bc955f4bbd34c6db5bb53663d22
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-06-13 14:12:31 +0100

    Refine regexp

--- config/initializers/sanitised_mailer_logger.rb
+++ config/initializers/sanitised_mailer_logger.rb
@@ -1,6 +1,6 @@
 class SanitizedMailerLogger < ActiveSupport::Logger
   def format_message(severity, timestamp, progname, msg)
-    filtered_message = msg.gsub(/"to":\[.*?\]/, '"to":["[FILTERED]"]')
+    filtered_message = msg.gsub(/"to":\["[^"]+"\]/, '"to":["[FILTERED]"]')
     super(severity, timestamp, progname, filtered_message)
   end
 end
