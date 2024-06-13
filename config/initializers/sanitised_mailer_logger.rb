c99e1fe1c28763a925dd4637c76db2705e0710ad
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-06-13 11:36:25 +0100

    Add Custom Logger to Filter Email Recipients in ActionMailer Logs
    Tthe custom logger extends ActiveSupport::Logger and overrides the
    format_message method. It replace the email addresses with the string
    "[FILTERED]", before calling super to maintain the rest of the
    original logging behaviour.

---
+++ config/initializers/sanitised_mailer_logger.rb
@@ -0,0 +1,8 @@
+class SanitizedMailerLogger < ActiveSupport::Logger
+  def format_message(severity, timestamp, progname, msg)
+    filtered_message = msg.gsub(/"to":\[.*?\]/, '"to":["[FILTERED]"]')
+    super(severity, timestamp, progname, filtered_message)
+  end
+end
+
+ActionMailer::Base.logger = SanitizedMailerLogger.new($stdout)
