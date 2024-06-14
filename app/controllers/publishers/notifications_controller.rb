4c808a755b705cbcad684454a9957f446edf74d8
Kyle MacPherson <kaimac2010@gmail.com>
2024-06-14 15:26:06 +0100

    Change deprecated method

--- app/controllers/publishers/notifications_controller.rb
+++ app/controllers/publishers/notifications_controller.rb
@@ -11,12 +11,12 @@ class Publishers::NotificationsController < Publishers::BaseController
   private
 
   def notifications
-    @notifications ||= current_publisher.notifications
-                                        .created_within_data_access_period
+    @notifications ||= current_publisher.noticed_events
+                                        .where("created_at >= ?", Time.current - DATA_ACCESS_PERIOD_FOR_PUBLISHERS)
                                         .order(created_at: :desc)
   end
 
   def mark_notifications_as_read
-    notifications.mark_as_read!
+    notifications.mark_as_read
   end
 end
