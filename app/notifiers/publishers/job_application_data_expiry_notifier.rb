9625ee35a7063089a4796659a072380956fd4e29
Kyle MacPherson <kaimac2010@gmail.com>
2024-06-17 11:19:48 +0100

    Move notifier methods into the new notification_methods block

--- app/notifiers/publishers/job_application_data_expiry_notifier.rb
+++ app/notifiers/publishers/job_application_data_expiry_notifier.rb
@@ -1,34 +1,37 @@
 class Publishers::JobApplicationDataExpiryNotifier < Noticed::Event
-  include ActionView::Helpers::UrlHelper
-  include GovukLinkHelper
-  include GovukVisuallyHiddenHelper
-  include DatesHelper
 
   deliver_by :database
   deliver_by :email, mailer: "Publishers::JobApplicationDataExpiryMailer", method: :job_application_data_expiry
   delegate :created_at, to: :record
   param :vacancy, :publisher
 
-  def message
-    t("notifications.publishers/job_application_data_expiry_notification.message_html",
-      link: vacancy_applications_link, date: format_date(data_expiration_date))
-  end
+  notification_methods do
+    include ActionView::Helpers::UrlHelper
+    include GovukLinkHelper
+    include GovukVisuallyHiddenHelper
+    include DatesHelper
+    
+    def message
+      t("notifications.publishers/job_application_data_expiry_notification.message_html",
+        link: vacancy_applications_link, date: format_date(data_expiration_date))
+    end
+  
+    def timestamp
+      "#{day(created_at)} at #{format_time(created_at)}"
+    end
 
-  def timestamp
-    "#{day(created_at)} at #{format_time(created_at)}"
-  end
-
-  private
-
-  def data_expiration_date
-    (vacancy.expires_at + 1.year).to_date
-  end
-
-  def vacancy
-    params[:vacancy]
-  end
-
-  def vacancy_applications_link
-    govuk_link_to vacancy.job_title, organisation_job_job_applications_path(vacancy.id), class: "govuk-link--no-visited-state"
+    private
+  
+    def data_expiration_date
+      (vacancy.expires_at + 1.year).to_date
+    end
+  
+    def vacancy
+      params[:vacancy]
+    end
+  
+    def vacancy_applications_link
+      govuk_link_to vacancy.job_title, organisation_job_job_applications_path(vacancy.id), class: "govuk-link--no-visited-state"
+    end
   end
 end
--- app/notifiers/publishers/job_application_received_notifier.rb
+++ app/notifiers/publishers/job_application_received_notifier.rb
@@ -1,33 +1,35 @@
 class Publishers::JobApplicationReceivedNotifier < Noticed::Event
-  include ActionView::Helpers::UrlHelper
-  include GovukLinkHelper
-  include GovukVisuallyHiddenHelper
-  include DatesHelper
-
   deliver_by :database
   delegate :created_at, to: :record
   param :vacancy, :job_application
 
-  def message
-    t("notifications.publishers/job_application_received_notification.message_html",
-      link: application_link, job_title: vacancy.job_title, organisation: vacancy.organisation_name)
-  end
+  notification_methods do
+    include ActionView::Helpers::UrlHelper
+    include GovukLinkHelper
+    include GovukVisuallyHiddenHelper
+    include DatesHelper
+    
+    def message
+      t("notifications.publishers/job_application_received_notification.message_html",
+        link: application_link, job_title: vacancy.job_title, organisation: vacancy.organisation_name)
+    end
 
-  def timestamp
-    "#{day(created_at)} at #{format_time(created_at)}"
-  end
+    def timestamp
+      "#{day(created_at)} at #{format_time(created_at)}"
+    end
 
-  private
+    private
 
-  def application_link
-    govuk_link_to "an application", organisation_job_job_application_path(vacancy.id, job_application.id), class: "govuk-link--no-visited-state"
-  end
+    def application_link
+      govuk_link_to "an application", organisation_job_job_application_path(vacancy.id, job_application.id), class: "govuk-link--no-visited-state"
+    end
 
-  def job_application
-    params[:job_application]
-  end
+    def job_application
+      params[:job_application]
+    end
 
-  def vacancy
-    params[:vacancy]
+    def vacancy
+      params[:vacancy]
+    end
   end
 end
