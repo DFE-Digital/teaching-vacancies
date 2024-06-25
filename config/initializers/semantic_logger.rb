aeefaad9d53e5344f571476775da708df15aade0
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-06-25 15:42:30 +0100

    New config

--- config/initializers/semantic_logger.rb
+++ config/initializers/semantic_logger.rb
@@ -1,6 +1,3 @@
-require "semantic_logger"
-require "json"
-
 class CustomLogFormatter < SemanticLogger::Formatters::Raw
   def call(log, logger)
     super
@@ -12,12 +9,6 @@ class CustomLogFormatter < SemanticLogger::Formatters::Raw
 end
 
 unless Rails.env.local?
-  Rails.application.configure do
-    config.semantic_logger.application = "" # No need to send the application name as logstash reads it from Cloud Foundry log tags
-    config.active_record.logger = nil # Don't log SQL in production
-    config.semantic_logger.backtrace_level = :error
-  end
-
-  SemanticLogger.add_appender(io: $stdout, level: Rails.application.config.log_level, formatter: CustomLogFormatter.new)
+  SemanticLogger.add_appender(io: STDOUT, level: Rails.application.config.log_level, formatter: CustomLogFormatter.new)
   Rails.logger.info("Application logging to STDOUT")
 end
