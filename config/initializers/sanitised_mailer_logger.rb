31a8f11440d1dc4b6bf939f5c0e83916332d4a37
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-06-25 13:52:50 +0100

    Revert to a simpler solution

--- config/initializers/sanitised_mailer_logger.rb
+++ config/initializers/sanitised_mailer_logger.rb
@@ -1,12 +1,23 @@
 require "semantic_logger"
+require "json"
 
-class SanitisedMailerLogger < SemanticLogger::Appender::IO
-  def log(log)
-    log.message = SanitisedMailerMiddleware.sanitize(log.message)
+class CustomLogFormatter < SemanticLogger::Formatters::Raw
+  def call(log, logger)
     super
+
+    if hash["payload"].is_a?(Hash) && hash["payload"]["to"]
+      hash["payload"].delete("to")
+    end
+
+    hash.to_json
   end
 end
 
-wrapped_logger = SanitisedMailerLogger.new(io: $stdout, formatter: :json)
-ActionMailer::Base.logger = wrapped_logger
-SemanticLogger.add_appender(appender: wrapped_logger)
+unless Rails.env.local?
+  SemanticLogger.add_appender(
+    io: $stdout,
+    level: Rails.application.config.log_level,
+    formatter: CustomLogFormatter.new,
+  )
+  Rails.logger.info("Application logging to STDOUT")
+end
