654effdb377fc0747e8100be127fe7b82ddad188
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-07-09 15:36:14 +0100

    Fix sanitation for posts page

--- config/routes.rb
+++ config/routes.rb
@@ -243,11 +243,11 @@ Rails.application.routes.draw do
       as: :posts
 
   get "/:section/:subcategory" => "posts#subcategory",
-      constraints: { section: /(jobseeker-guides|get-help-hiring)/, subcategory: /[\w-]+/ },
+      constraints: { section: /(jobseeker-guides|get-help-hiring)/, subcategory: /get-help-applying-for-your-teaching-role|return-to-teaching-in-england|how-to-create-job-listings-and-accept-applications|how-to-setup-your-account/ },
       as: :subcategory
 
   get "/:section/:subcategory/:post_name" => "posts#show",
-      constraints: { section: /(jobseeker-guides|get-help-hiring)/, subcategory: /[\w-]+/, post_name: /[\w-]+/ },
+      constraints: { section: /(jobseeker-guides|get-help-hiring)/, subcategory: /get-help-applying-for-your-teaching-role|return-to-teaching-in-england|how-to-create-job-listings-and-accept-applications|how-to-setup-your-account/, post_name: /[\w-]+/ },
       as: :post
 
   get "/list-school-job" => "pages#show", defaults: { id: "list-school-job" }
