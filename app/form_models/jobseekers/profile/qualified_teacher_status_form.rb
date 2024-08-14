3949867348f4146579d23c7cde3b1e67af17414b
Kyle MacPherson <kaimac2010@gmail.com>
2024-08-14 16:32:31 +0100

    Add TRN and statutory induction period completed to jobseeker profile and jobseeker profile page

--- app/form_models/jobseekers/profile/qualified_teacher_status_form.rb
+++ app/form_models/jobseekers/profile/qualified_teacher_status_form.rb
@@ -3,7 +3,16 @@ class Jobseekers::Profile::QualifiedTeacherStatusForm < BaseForm
   validates :qualified_teacher_status_year, numericality: { less_than_or_equal_to: proc { Time.current.year } }, if: -> { qualified_teacher_status == "yes" }
 
   def self.fields
-    %i[qualified_teacher_status qualified_teacher_status_year]
+    %i[qualified_teacher_status qualified_teacher_status_year teacher_reference_number statutory_induction_complete]
   end
+
+  def statutory_induction_complete_options
+    [
+      ["yes", I18n.t("helpers.label.jobseekers_job_application_professional_status_form.statutory_induction_complete_options.yes")],
+      ["no", I18n.t("helpers.label.jobseekers_job_application_professional_status_form.statutory_induction_complete_options.no")],
+      ["on_track", I18n.t("helpers.label.jobseekers_job_application_professional_status_form.statutory_induction_complete_options.on_track")],
+    ]
+  end
+  
   attr_accessor(*fields)
 end
