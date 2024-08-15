414e2de060bf30e3bf9515c3340587d13f730f0b
Kyle MacPherson <kaimac2010@gmail.com>
2024-08-15 15:31:51 +0100

    Add validations for the qts form

--- app/form_models/jobseekers/profile/qualified_teacher_status_form.rb
+++ app/form_models/jobseekers/profile/qualified_teacher_status_form.rb
@@ -1,6 +1,10 @@
 class Jobseekers::Profile::QualifiedTeacherStatusForm < BaseForm
   validates :qualified_teacher_status, inclusion: { in: %w[yes no on_track non_teacher] }
   validates :qualified_teacher_status_year, numericality: { less_than_or_equal_to: proc { Time.current.year } }, if: -> { qualified_teacher_status == "yes" }
+  validates :teacher_reference_number, presence: true, if: -> { qualified_teacher_status == "yes" }
+  validates_format_of :teacher_reference_number, with: /\A\d{7}\z/, allow_blank: false, if: -> { qualified_teacher_status == "yes" }
+  validates_format_of :teacher_reference_number, with: /\A\d{7}\z/, allow_blank: true, if: -> { qualified_teacher_status == "no" || qualified_teacher_status == "on_track"}
+  validates :statutory_induction_complete, inclusion: { in: %w[yes no on_track] }, if: -> { qualified_teacher_status == "yes" }
 
   def self.fields
     %i[qualified_teacher_status qualified_teacher_status_year teacher_reference_number statutory_induction_complete]
