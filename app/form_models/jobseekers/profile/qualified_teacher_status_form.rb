71a90f64678be228f72a864652f71b42b951a957
Kyle MacPherson <kaimac2010@gmail.com>
2024-08-21 14:21:29 +0100

    Linting

--- app/form_models/jobseekers/profile/qualified_teacher_status_form.rb
+++ app/form_models/jobseekers/profile/qualified_teacher_status_form.rb
@@ -3,7 +3,7 @@ class Jobseekers::Profile::QualifiedTeacherStatusForm < BaseForm
   validates :qualified_teacher_status_year, numericality: { less_than_or_equal_to: proc { Time.current.year } }, if: -> { qualified_teacher_status == "yes" }
   validates :teacher_reference_number, presence: true, if: -> { qualified_teacher_status == "yes" }
   validates_format_of :teacher_reference_number, with: /\A\d{7}\z/, allow_blank: false, if: -> { qualified_teacher_status == "yes" }
-  validates_format_of :teacher_reference_number, with: /\A\d{7}\z/, allow_blank: true, if: -> { qualified_teacher_status == "no" || qualified_teacher_status == "on_track"}
+  validates_format_of :teacher_reference_number, with: /\A\d{7}\z/, allow_blank: true, if: -> { qualified_teacher_status == "no" || qualified_teacher_status == "on_track" }
   validates :statutory_induction_complete, inclusion: { in: %w[yes no on_track] }, if: -> { qualified_teacher_status == "yes" }
 
   def self.fields
@@ -17,6 +17,6 @@ class Jobseekers::Profile::QualifiedTeacherStatusForm < BaseForm
       ["on_track", I18n.t("helpers.label.jobseekers_job_application_professional_status_form.statutory_induction_complete_options.on_track")],
     ]
   end
-  
+
   attr_accessor(*fields)
 end
