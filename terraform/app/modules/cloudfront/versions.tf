39d821090b63f6d8a84f34764cf9c79a719e1045
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2024-08-22 11:14:03 +0100

    Update application terraform providers

--- terraform/app/modules/cloudfront/versions.tf
+++ terraform/app/modules/cloudfront/versions.tf
@@ -1,8 +1,6 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = "5.29.0"
       configuration_aliases = [aws.aws_us_east_1]
     }
   }
--- terraform/app/modules/cloudwatch/versions.tf
+++ terraform/app/modules/cloudwatch/versions.tf
@@ -1,8 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.29.0"
-    }
+    aws = {}
   }
 }
