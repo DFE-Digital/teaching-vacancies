5fed8b9dd027c7ab2a08ac1b5bf2b1b10c6a700c
Kyle MacPherson <kaimac2010@gmail.com>
2024-08-27 16:52:58 +0100

    Redirect existing users to account found page if it's their first login via gov.uk one login

--- app/controllers/jobseekers/accounts_controller.rb
+++ app/controllers/jobseekers/accounts_controller.rb
@@ -2,4 +2,6 @@ class Jobseekers::AccountsController < Jobseekers::BaseController
   def show; end
 
   def confirmation; end
+
+  def account_found; end
 end
--- app/controllers/jobseekers/govuk_one_login_callbacks_controller.rb
+++ app/controllers/jobseekers/govuk_one_login_callbacks_controller.rb
@@ -5,6 +5,10 @@ class Jobseekers::GovukOneLoginCallbacksController < Devise::OmniauthCallbacksCo
   def openid_connect
     if (govuk_one_login_user = Jobseekers::GovukOneLogin::UserFromAuthResponse.call(params, session))
       session[:govuk_one_login_id_token] = govuk_one_login_user.id_token
+      
+      if existing_jobseeker_first_sign_in_via_one_login(govuk_one_login_user)
+        session[:user_exists_first_log_in] = { value: 'true', path: '/', expires: 1.hour.from_now }
+      end
 
       jobseeker = Jobseeker.find_or_create_from_govuk_one_login(email: govuk_one_login_user.email,
                                                                 govuk_one_login_id: govuk_one_login_user.id)
@@ -32,6 +36,15 @@ class Jobseekers::GovukOneLoginCallbacksController < Devise::OmniauthCallbacksCo
   # Devise method to redirect the user after sign in.
   # We need to build our own logic to redirect the user to the correct pages.
   def after_sign_in_path_for(_resource)
-    jobseekers_job_applications_path
+    if session[:user_exists_first_log_in]
+      session.delete(:user_exists_first_log_in)
+      account_found_jobseekers_account_path
+    else
+      jobseekers_job_applications_path
+    end
+  end
+
+  def existing_jobseeker_first_sign_in_via_one_login(govuk_one_login_user)
+    Jobseeker.find_by(email: govuk_one_login_user.email, govuk_one_login_id: nil)
   end
 end
