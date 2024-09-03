9a22cf3e9aca20bac21f44514ccd2d2b443ca83f
Kyle MacPherson <kaimac2010@gmail.com>
2024-09-03 15:22:14 +0100

    validation to ensure that users cannot request confirmation codes too frequently

--- app/form_models/jobseekers/request_account_transfer_email_form.rb
+++ app/form_models/jobseekers/request_account_transfer_email_form.rb
@@ -3,4 +3,15 @@ class Jobseekers::RequestAccountTransferEmailForm < BaseForm
 
   validates :email, presence: true
   validates :email, email_address: true, if: -> { email.present? }
+  validate :validate_recent_code_request, if: -> { email.present? }
+
+
+  def validate_recent_code_request
+    jobseeker = Jobseeker.find_by(email: email)
+    return if jobseeker.nil?
+
+    if jobseeker.account_merge_confirmation_code_generated_at.present? && jobseeker.account_merge_confirmation_code_generated_at > 1.minute.ago
+      errors.add(:email, :recent_code_request, message: "Please wait 1 minute before requesting another code.")
+    end
+  end
 end
