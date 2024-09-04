c648a114fa5f1c060e7201bb1e7d232f701a1257
Kyle MacPherson <kaimac2010@gmail.com>
2024-09-04 15:57:03 +0100

    wip data transfer

--- app/controllers/jobseekers/account_transfers_controller.rb
+++ app/controllers/jobseekers/account_transfers_controller.rb
@@ -8,9 +8,10 @@ class Jobseekers::AccountTransfersController < Jobseekers::BaseController
     @account_transfer_form = Jobseekers::AccountTransferForm.new(request_account_transfer_email_form_params)
 
     if @account_transfer_form.valid?
-      # do some stuff to transfer over details
-      flash[:success] = "Your account details have been transferred successfully!"
-      redirect_to jobseekers_profile_path
+      if transfer_account_data
+        flash[:success] = "Your account details have been transferred successfully!"
+        redirect_to jobseekers_profile_path
+      end
     else
       render :new
     end
@@ -21,4 +22,39 @@ class Jobseekers::AccountTransfersController < Jobseekers::BaseController
   def request_account_transfer_email_form_params
     params.require(:jobseekers_account_transfer_form).permit(:account_merge_confirmation_code, :email)
   end
+
+  def transfer_account_data
+    account_to_transfer = Jobseeker.find_by(email: @account_transfer_form.email)
+
+    profile = account_to_transfer.jobseeker_profile
+    if profile
+      current_jobseeker.jobseeker_profile.destroy! if current_jobseeker.jobseeker_profile
+      profile.jobseeker_id = current_jobseeker.id
+      profile.save!
+    end
+
+    feedbacks = account_to_transfer.feedbacks
+    feedbacks.each do |feedback|
+      feedback.jobseeker_id = current_jobseeker.id
+      feedback.save!
+    end
+
+    job_applications = account_to_transfer.job_applications
+    job_applications.each do |job_application|
+      job_application.jobseeker_id = current_jobseeker.id
+      job_application.save!
+    end
+    
+    saved_jobs = account_to_transfer.saved_jobs
+    saved_jobs.each do |saved_job|
+      saved_job.jobseeker_id = current_jobseeker.id
+      saved_job.save!
+    end
+  end
 end
+
+
+# has_many :feedbacks, dependent: :destroy, inverse_of: :jobseeker
+# has_many :job_applications, dependent: :destroy
+# has_many :saved_jobs, dependent: :destroy
+# has_one :jobseeker_profile
