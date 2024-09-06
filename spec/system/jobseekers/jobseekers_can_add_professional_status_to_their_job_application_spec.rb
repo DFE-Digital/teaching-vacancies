e7e8ba9d552af7bc4a992c29993ca0c80065d544
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-09-06 14:36:38 +0100

    Update system spec

--- spec/system/jobseekers/jobseekers_can_add_professional_status_to_their_job_application_spec.rb
+++ spec/system/jobseekers/jobseekers_can_add_professional_status_to_their_job_application_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Jobseekers can add details about their qualified teacher status to a job application" do
   let(:jobseeker) { create(:jobseeker) }
+  let!(:jobseeker_profile) { create(:jobseeker_profile, jobseeker: jobseeker) }
   let(:vacancy) { create(:vacancy, organisations: [build(:school)]) }
   let!(:job_application) { create(:job_application, :status_draft, jobseeker: jobseeker, vacancy: vacancy) }
 
@@ -19,7 +20,7 @@ RSpec.describe "Jobseekers can add details about their qualified teacher status 
       expect(page).to have_link("Select yes if you have completed your statutory induction year", href: "#jobseekers-job-application-professional-status-form-statutory-induction-complete-field-error")
     end
 
-    choose("Yes")
+    choose "Yes", name: "jobseekers_job_application_professional_status_form[qualified_teacher_status]"
 
     click_on "Save and continue"
 
@@ -27,10 +28,20 @@ RSpec.describe "Jobseekers can add details about their qualified teacher status 
       expect(page).to have_link("Enter the year your QTS was awarded", href: "#jobseekers-job-application-professional-status-form-qualified-teacher-status-year-field-error")
     end
 
-    choose("Yes")
+    choose "Yes", name: "jobseekers_job_application_professional_status_form[qualified_teacher_status]"
     fill_in "Year QTS was awarded", with: "2022"
     fill_in "Please provide more detail (optional field)", with: "It was hard work but I made it"
     choose("I'm on track to complete it")
+
+    click_on "Save and continue"
+
+    within "ul.govuk-list.govuk-error-summary__list" do
+      expect(page).to have_link("Enter a teacher reference number (TRN)", href: "#jobseekers-job-application-professional-status-form-teacher-reference-number-field-error")
+      expect(page).to have_link("Select yes if you have a teacher reference number. If you have qualified teacher status (QTS) you must select yes.", href: "#jobseekers-job-application-professional-status-form-has-teacher-reference-number-field-error")
+    end
+
+    choose "Yes", name: "jobseekers_job_application_professional_status_form[has_teacher_reference_number]"
+    fill_in "What is your Teacher reference number (TRN)?", with: "1234567"
 
     click_on "Save and continue"
 
