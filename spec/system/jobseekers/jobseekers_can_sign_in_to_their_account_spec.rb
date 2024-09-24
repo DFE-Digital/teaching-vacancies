c4b3fa79b6bb6badcc3b4dce069df4b1dcb9700e
Marc Sardon <marc@bichito.software>
2024-09-24 14:53:53 +0100

    Add quick apply sign-in redirection to system tests

--- spec/system/jobseekers/jobseekers_can_sign_in_to_their_account_spec.rb
+++ spec/system/jobseekers/jobseekers_can_sign_in_to_their_account_spec.rb
@@ -44,4 +44,20 @@ RSpec.describe "Jobseekers can sign in to their account" do
       expect(page).to have_link(text: I18n.t("nav.sign_in"))
     end
   end
+
+  context "when the user sign-in following a vacancy quick apply link" do
+    let(:jobseeker) { create(:jobseeker) }
+    let(:old_vacancy) { create(:vacancy, organisations: [build(:school)]) }
+    let(:vacancy) { create(:vacancy, organisations: [build(:school)]) }
+    let!(:recent_job_application) { create(:job_application, :status_submitted, jobseeker: jobseeker, vacancy: old_vacancy) }
+
+    scenario "the user is sent to the quick application page" do
+      visit new_jobseekers_job_job_application_path(vacancy.id)
+      expect(current_path).to eq(new_jobseeker_session_path)
+
+      sign_in_jobseeker_govuk_one_login(jobseeker, navigate: false)
+      expect(current_path).to eq(new_quick_apply_jobseekers_job_job_application_path(vacancy.id))
+      expect(page).to have_css("h1", text: I18n.t("jobseekers.job_applications.new_quick_apply.heading"))
+    end
+  end
 end
