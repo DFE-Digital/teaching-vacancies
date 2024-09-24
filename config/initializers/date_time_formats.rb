62600907df98640719b097331dc4e54b155ca9fe
Kyle MacPherson <kaimac2010@gmail.com>
2024-09-24 14:31:30 +0100

    Linting

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -4,7 +4,6 @@ Date::DATE_FORMATS[:month_year] = "%B %Y"
 Date::DATE_FORMATS[:day_month] = "%-d %B"
 Date::DATE_FORMATS[:day_month_year] = "%d %B %Y"
 
-
 # Format times globally into these formats:
 Time::DATE_FORMATS[:default] = "%e %B %Y %-l:%M%P"
 Time::DATE_FORMATS[:date_only] = "%e %B %Y"
