5d3ffaa5f8b14f11f140b7e5d57415fd9d9006a7
starswan <github@starswan.com>
2024-09-25 14:59:29 +0100

    add test to check the URL link in the outgoing email

--- spec/jobs/send_email_for_draft_job_applications_job_spec.rb
+++ spec/jobs/send_email_for_draft_job_applications_job_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe SendEmailForDraftJobApplicationsJob, type: :job do
   let(:vacancy) { create(:vacancy, expires_at: Date.today + time_left + 2.hours) }
   let(:job_application) { JobApplication.last }
 
+  include Rails.application.routes.url_helpers
+
   before do
     create(:job_application, :status_draft, vacancy: vacancy, jobseeker: jobseeker)
   end
@@ -16,8 +18,11 @@ RSpec.describe SendEmailForDraftJobApplicationsJob, type: :job do
       expect(Jobseekers::VacancyMailer).to receive(:draft_application_only).with(job_application).at_least(:once).and_call_original
     end
 
-    it "sends an email" do
-      perform_enqueued_jobs { described_class.perform_later }
+    it "sends an email containing a link to review the job application" do
+      expect {
+        perform_enqueued_jobs { described_class.perform_later }
+      }.to change(ActionMailer::Base.deliveries, :count).by(1)
+      expect(ActionMailer::Base.deliveries.last.body).to include(jobseekers_job_application_review_url(job_application))
     end
   end
 
