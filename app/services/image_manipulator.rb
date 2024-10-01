5e3c805515da17ba6838ce21c9ed2a99efe362ec
starswan <github@starswan.com>
2024-10-01 21:32:26 +0100

    try to close image files

--- app/services/image_manipulator.rb
+++ app/services/image_manipulator.rb
@@ -4,6 +4,9 @@ class ImageManipulator
   end
 
   def alter_dimensions_and_preserve_aspect_ratio(width, height)
-    MiniMagick::Image.open(@image_file_path).resize "#{width}x#{height}"
+    MiniMagick::Image.open(@image_file_path).tap do |image|
+      image.resize "#{width}x#{height}"
+      image.close
+    end
   end
 end
