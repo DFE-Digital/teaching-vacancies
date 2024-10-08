b7f5222ed815b74bb725ad570f3be9a3fc66de78
Kyle MacPherson <kaimac2010@gmail.com>
2024-10-08 16:33:45 +0100

    Add success message to i18n files

--- app/controllers/jobseekers/request_account_transfer_emails_controller.rb
+++ app/controllers/jobseekers/request_account_transfer_emails_controller.rb
@@ -13,7 +13,7 @@ class Jobseekers::RequestAccountTransferEmailsController < Jobseekers::BaseContr
         Jobseekers::AccountMailer.request_account_transfer(jobseeker).deliver_now
       end
 
-      success_message = @request_account_transfer_email_form.email_resent ? "Email resent" : nil
+      success_message = @request_account_transfer_email_form.email_resent ? t(".success") : nil
       redirect_to new_jobseekers_account_transfer_path(email: @request_account_transfer_email_form.email), success: success_message
     else
       render :new
