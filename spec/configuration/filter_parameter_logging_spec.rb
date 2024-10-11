aa0fd08af2d5b80a1f692b449d4ed3a20da993ea
starswan <github@starswan.com>
2024-10-11 15:58:42 +0100

    put back factory bot rails in main gemfile as seeds uses it

--- spec/configuration/filter_parameter_logging_spec.rb
+++ spec/configuration/filter_parameter_logging_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Filter parameter logging configuration" do
   let(:analytics_hidden_pii) { Rails.application.config_for(:analytics_hidden_pii) }
-  let(:filter_params) { Rails.application.config.filter_parameters }
+  let(:filter_params) { Rails.application.config.filter_parameters + [:id] }
 
   specify "all anonymised analytics fields should be filtered from logs" do
     analytics_hidden_pii.each_value do |shared|
