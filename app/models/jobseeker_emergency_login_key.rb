7bebaa8fe82feee702f92e29adac17087113d3bb
Kyle MacPherson <kaimac2010@gmail.com>
2024-10-29 15:02:24 +0000

    Delete redundant jobseeker_emergency_login_key file

--- app/models/jobseeker_emergency_login_key.rb
+++
@@ -1,8 +0,0 @@
-class JobseekerEmergencyLoginKey < ApplicationRecord
-  belongs_to :jobseeker
-  validates :not_valid_after, presence: true
-
-  def expired?
-    Time.current > not_valid_after
-  end
-end
