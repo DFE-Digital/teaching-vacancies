fcab818db3ddf8e7da77231b3e8d97e33c67883b
Kyle MacPherson <kaimac2010@gmail.com>
2024-10-30 16:48:36 +0000

    rubocop linting

--- spec/system/jobseekers/jobseekers_can_sign_in_by_email_spec.rb
+++ spec/system/jobseekers/jobseekers_can_sign_in_by_email_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 require "dfe/analytics/rspec/matchers"
 
 RSpec.describe "Jobseekers can sign in with fallback email authentication" do
-  before { allow(AuthenticationFallbackForJobseekers).to receive(:enabled?) { true } }
+  before { allow(AuthenticationFallbackForJobseekers).to receive(:enabled?).and_return(true) }
 
   it "can reach email authentication page" do
     visit root_path
@@ -12,7 +12,7 @@ RSpec.describe "Jobseekers can sign in with fallback email authentication" do
     expect(page).to have_content(I18n.t("publishers.login_keys.new.notice"))
   end
 
-  context "jobseeker flow" do
+  context "when fallback authentication is enabled" do
     let(:jobseeker) { create(:jobseeker, confirmed_at: 1.day.ago) }
 
     let(:login_key) do
@@ -21,6 +21,7 @@ RSpec.describe "Jobseekers can sign in with fallback email authentication" do
 
     let(:message_delivery) { instance_double(ActionMailer::MessageDelivery) }
 
+    # rubocop:disable RSpec/AnyInstance
     before do
       allow_any_instance_of(Jobseekers::LoginKeysController)
         .to receive(:generate_login_key)
@@ -30,6 +31,7 @@ RSpec.describe "Jobseekers can sign in with fallback email authentication" do
         .with(login_key_id: login_key.id, jobseeker: jobseeker)
         .and_return(message_delivery)
     end
+    # rubocop:enable RSpec/AnyInstance
 
     context "when a jobseeker tries to sign in" do
       it "can sign in, sign out" do
@@ -62,17 +64,17 @@ RSpec.describe "Jobseekers can sign in with fallback email authentication" do
           visit consume_jobseekers_login_key_path(login_key)
           expect(page).to have_content("used")
           expect(page).to have_content("Sign in unsuccessful")
-          expect(page).not_to have_current_path(jobseeker_root_path)
+          expect(page).to have_no_current_path(jobseeker_root_path)
         end
       end
 
       it "cannot sign in if key has expired" do
         visit root_path
-          within(".govuk-header__navigation") { click_on I18n.t("buttons.sign_in") }
-          click_on I18n.t("buttons.sign_in_jobseeker")
-          expect(message_delivery).to receive(:deliver_later)
-          fill_in "jobseeker[email]", with: jobseeker.email
-          click_on I18n.t("buttons.submit")
+        within(".govuk-header__navigation") { click_on I18n.t("buttons.sign_in") }
+        click_on I18n.t("buttons.sign_in_jobseeker")
+        expect(message_delivery).to receive(:deliver_later)
+        fill_in "jobseeker[email]", with: jobseeker.email
+        click_on I18n.t("buttons.submit")
         travel 5.hours do
           visit consume_jobseekers_login_key_path(login_key)
           expect(page).to have_content("expired")
