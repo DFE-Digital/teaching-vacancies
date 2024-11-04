0cd201b44c1bf8d1614e0e03f7dcd2c10e8a7edd
starswan <github@starswan.com>
2024-11-04 12:09:36 +0000

    Refactor FormFileValidator

--- app/form_models/publishers/job_listing/vacancy_form.rb
+++ app/form_models/publishers/job_listing/vacancy_form.rb
@@ -1,6 +1,13 @@
 class Publishers::JobListing::VacancyForm < BaseForm
   attr_accessor :params, :vacancy, :completed_steps, :current_organisation
 
+  DOCUMENT_VALIDATION_OPTIONS = {
+    file_type: :document,
+    content_types_allowed: %w[application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document].freeze,
+    file_size_limit: 10.megabytes,
+    valid_file_types: %i[PDF DOC DOCX],
+  }.freeze
+
   def initialize(params = {}, vacancy = nil, current_publisher = nil)
     @params = params
     @vacancy = vacancy
--- app/validators/form_file_validator.rb
+++ app/validators/form_file_validator.rb
@@ -1,19 +1,21 @@
+# TODO - this whole class could be replaced with https://github.com/igorkasyanchuk/active_storage_validations
+# Possibly this could happen when we move over to Azure storage, which will do the virus check for us rather than the
+# upload/download from google cloud as we currently have it.
 class FormFileValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     # We only want to run the validations in this validator when the form is submitted to check that the file is virus free and the size and
     # type are valid. Every job listing form model's validations are run after a step is completed in all_steps_valid?
     # (see app/form_models/form_sequence.rb:13). At this point the custom validations in this validator do not need to be run.
-    return true unless validating_files_after_form_submission?(value)
-
-    @form = record
-    @file_upload_field_name = attribute
-    @form_field_value = value
-    record.file_type == :image ? validate_each_image : validate_each_document
+    if validating_files_after_form_submission?(value)
+      [value].flatten.each do |file|
+        valid_file_size?(record, attribute, file) &&
+          valid_file_type?(record, attribute, file) &&
+          virus_free?(record, attribute, file)
+      end
+    end
   end
 
   private
-
-  attr_reader :form, :file_upload_field_name, :form_field_value
 
   def validating_files_after_form_submission?(value)
     # If the class of the file attached to the form object is ActionDispatch::Http::UploadedFile, we know that these validations are being run after the submission
@@ -22,54 +24,38 @@ class FormFileValidator < ActiveModel::EachValidator
     [value].flatten.all?(ActionDispatch::Http::UploadedFile)
   end
 
-  def validate_each_image
-    [form_field_value].flatten.each do |file|
-      valid_file_size?(file) &&
-        valid_file_type?(file) &&
-        virus_free?(file)
-    end
-  end
+  def valid_file_size?(record, attribute, value)
+    return true if value.size <= options[:file_size_limit]
 
-  def validate_each_document
-    [form_field_value].flatten.each do |file|
-      valid_file_size?(file) &&
-        valid_file_type?(file) &&
-        virus_free?(file)
-    end
-  end
-
-  def valid_file_size?(file)
-    return true if file.size <= form.file_size_limit
-
-    form.errors.add(
-      file_upload_field_name,
+    record.errors.add(
+      attribute,
       I18n.t(
         "jobs.file_size_error_message",
-        filename: file.original_filename,
-        size_limit: ActiveSupport::NumberHelper.number_to_human_size(form.file_size_limit),
+        filename: value.original_filename,
+        size_limit: ActiveSupport::NumberHelper.number_to_human_size(options[:file_size_limit]),
       ),
     )
     false
   end
 
-  def valid_file_type?(file)
-    content_type = MimeMagic.by_magic(file.tempfile)&.type
-    return true if form.content_types_allowed.include?(content_type)
+  def valid_file_type?(record, attribute, value)
+    content_type = MimeMagic.by_magic(value.tempfile)&.type
+    return true if options[:content_types_allowed].include?(content_type)
 
-    Rails.logger.warn("Attempted to upload '#{file.original_filename}' with forbidden file type '#{content_type}'")
-    form.errors.add(file_upload_field_name, I18n.t("jobs.file_type_error_message", filename: file.original_filename, valid_file_types: valid_file_types_for_error_message))
+    Rails.logger.warn("Attempted to upload '#{value.original_filename}' with forbidden file type '#{content_type}'")
+    record.errors.add(attribute, I18n.t("jobs.file_type_error_message", filename: value.original_filename, valid_file_types: valid_file_types_for_error_message))
     false
   end
 
-  def virus_free?(file)
-    return true if Publishers::DocumentVirusCheck.new(file.tempfile).safe?
+  def virus_free?(record, attribute, value)
+    return true if Publishers::DocumentVirusCheck.new(value.tempfile).safe?
 
-    Rails.logger.warn("Attempted to upload '#{file.original_filename}' but Google Drive virus check failed")
-    form.errors.add(file_upload_field_name, I18n.t("jobs.file_virus_error_message", filename: file.original_filename))
+    Rails.logger.warn("Attempted to upload '#{value.original_filename}' but Google Drive virus check failed")
+    record.errors.add(attribute, I18n.t("jobs.file_virus_error_message", filename: value.original_filename))
     false
   end
 
   def valid_file_types_for_error_message
-    form.valid_file_types.to_sentence(two_words_connector: " or ", last_word_connector: " or ")
+    options[:valid_file_types].to_sentence(two_words_connector: " or ", last_word_connector: " or ")
   end
 end
