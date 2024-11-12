24cd1eb38d3799fbba943a5d6b0e66d29f01e4fd
Stephen Dicks <github@starswan.com>
2024-11-12 10:58:40 +0000

    Faith school catholic flow (#7222)

---
+++ app/components/job_application_review_component/religious_information_section.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class JobApplicationReviewComponent::ReligiousInformationSection < JobApplicationReviewComponent::Section
+  def initialize(job_application, allow_edit:, name:)
+    # only include the details form if we follow a religion
+    forms = if job_application.following_religion
+              %w[FollowingReligionForm ReligionDetailsForm]
+            else
+              %w[FollowingReligionForm]
+            end
+    super(job_application, forms: forms, name: name, allow_edit: allow_edit)
+  end
+end
