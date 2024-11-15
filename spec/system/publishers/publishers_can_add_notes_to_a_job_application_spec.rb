23c58d977805bff3979c33a9e725093cdd577c29
Kyle MacPherson <kaimac2010@gmail.com>
2024-11-15 16:14:54 +0000

    Update note tests

--- spec/system/publishers/publishers_can_add_notes_to_a_job_application_spec.rb
+++ spec/system/publishers/publishers_can_add_notes_to_a_job_application_spec.rb
@@ -12,34 +12,26 @@ RSpec.describe "Publishers can add notes to a job application", js: true do
 
   it "shows the current notes" do
     visit organisation_job_job_application_path(vacancy.id, job_application)
-    click_on I18n.t("publishers.vacancies.job_applications.header.tabs.notes")
 
-    expect(current_path).to eq(organisation_job_job_application_notes_path(vacancy.id, job_application))
     expect(page).to have_content(note.content)
   end
 
   it "allows notes to be added to job applications" do
-    visit organisation_job_job_application_notes_path(vacancy.id, job_application)
-
-    expect(page.has_field?("publishers-job-application-notes-form-content-field")).to eq(false)
-
-    click_on I18n.t("buttons.add_note")
-
-    expect(page.has_field?("publishers-job-application-notes-form-content-field")).to eq(true)
+    visit organisation_job_job_application_path(vacancy.id, job_application)
 
     fill_in "Add a note", with: ""
     click_on I18n.t("buttons.save")
 
-    expect(page).to have_content(I18n.t("activemodel.errors.models.publishers/job_application/notes_form.attributes.content.blank"))
+    expect(page).to have_content("Note did not save. Notes must not be blank or more than 150 words")
 
-    fill_in "Add a note", with: note_content
+    fill_in "Add a note", with: "ABCDEFG"
     click_on I18n.t("buttons.save")
 
-    expect(page).to have_content(note_content)
+    expect(page).to have_content("ABCDEFG")
   end
 
   it "allows notes to be deleted" do
-    visit organisation_job_job_application_notes_path(vacancy.id, job_application)
+    visit organisation_job_job_application_path(vacancy.id, job_application)
 
     expect(page).to have_content(note.content)
     click_on I18n.t("buttons.delete")
