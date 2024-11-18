b4a06e1217473c921b08f314276670ab4a3a9e76
starswan <github@starswan.com>
2024-11-18 11:39:40 +0000

    job downloads

--- app/controllers/publishers/vacancies/job_applications_controller.rb
+++ app/controllers/publishers/vacancies/job_applications_controller.rb
@@ -51,6 +51,11 @@ class Publishers::Vacancies::JobApplicationsController < Publishers::Vacancies::
     @form = Publishers::JobApplication::TagForm.new(tag_params.merge(job_applications: tag_params.fetch(:job_applications).compact_blank))
     if @form.valid?
       if params["download_selected"] == "true"
+        downloads = JobApplication
+                      .includes([:qualifications, :employments, :training_and_cpds, :references, { jobseeker: :jobseeker_profile }, {vacancy: [:organisations, :publisher_organisation]}])
+                      .where(vacancy: vacancy.id)
+                      .select { |job_application| @form.job_applications.include?(job_application.id) }
+        download_selected downloads
       else
         @job_applications = vacancy.job_applications.where(id: @form.job_applications)
         render "tag"
@@ -70,6 +75,24 @@ class Publishers::Vacancies::JobApplicationsController < Publishers::Vacancies::
   end
 
   private
+
+  require "zip"
+
+  def download_selected downloads
+    stringio = Zip::OutputStream.write_buffer do |zio|
+      downloads.each do |job_application|
+        zio.put_next_entry "#{job_application.first_name}_#{job_application.last_name}.pdf"
+        pdf = JobApplicationPdfGenerator.new(job_application, vacancy).generate
+        zio.write pdf.render
+      end
+    end
+    send_data(
+      stringio.string,
+      filename: "applications_#{vacancy.job_title}.zip",
+      type: "application/zip",
+      # disposition: "inline",
+      )
+  end
 
   def job_applications
     @job_applications ||= vacancy.job_applications.not_draft
