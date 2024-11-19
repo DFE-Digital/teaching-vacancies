ecd8f1cf0b64fe59a8368599fde2e96fc8a0a8b6
Kyle MacPherson <kaimac2010@gmail.com>
2024-11-19 15:11:09 +0000

    Fix/remove tests

--- app/helpers/job_applications_helper.rb
+++ app/helpers/job_applications_helper.rb
@@ -89,6 +89,7 @@ module JobApplicationsHelper
 
   def publisher_job_application_status_tag(status, classes: [])
     classes << "govuk-!-margin-bottom-2"
+    classes << "application-status"
     govuk_tag text: PUBLISHER_STATUS_MAPPINGS[status.to_sym],
               colour: JOB_APPLICATION_STATUS_TAG_COLOURS[status.to_sym],
               classes: classes.join(" ")
