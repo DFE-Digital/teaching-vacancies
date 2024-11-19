f33f7f7d0d4e364c037dddee2e8fb31c6b297cf0
Stephen Dicks <github@starswan.com>
2024-11-19 17:17:47 +0000

    tweak test to make it more robust (#7299)

--- spec/system/publishers/publishers_can_manage_job_applications_for_a_vacancy_spec.rb
+++ spec/system/publishers/publishers_can_manage_job_applications_for_a_vacancy_spec.rb
@@ -76,7 +76,7 @@ RSpec.describe "Publishers can manage job applications for a vacancy" do
         find(".govuk-checkboxes__item").click
       end
       click_on I18n.t("publishers.vacancies.job_applications.candidates.update_application_status")
-      find(".govuk-tag--purple").click
+      find("span.govuk-tag--purple").click
       click_on I18n.t("buttons.save_and_continue")
       expect(page).to have_content("New (3)")
     end
