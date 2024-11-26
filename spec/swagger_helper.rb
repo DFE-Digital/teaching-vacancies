e926caaa8d6d37eb0c13be60a40f15f6d538a3ba
Marc Sardon <marc@bichito.software>
2024-11-26 18:14:06 +0000

    Extract common Vacancy schema
    The vacancy schema will be shared across multiple responses:
    - Individual vacancies listed in the get all the vacancies operation.
    - Individual vacancy retrieval.
    - Response for creating a vacancy.
    - Response for updating a vacancy.

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -39,6 +39,172 @@ RSpec.configure do |config|
           },
         },
         schemas: {
+          vacancy: {
+            type: :object,
+            additionalProperties: false,
+            required: %i[id
+                         external_advert_url
+                         expires_at
+                         starts_on
+                         job_title
+                         skills_and_experience
+                         salary
+                         external_reference
+                         job_roles
+                         working_patterns
+                         contract_type
+                         phases
+                         publish_on
+                         schools],
+            properties: {
+              id: { type: :string, example: "9d8f5715-2e7c-4e64-8e34-35f510c12e66" },
+              external_advert_url: { type: :string, example: "https://example.com/jobs/123" },
+              publish_on: { type: :string, format: :date },
+              expires_at: { type: :string, format: :date },
+              job_title: { type: :string, example: "Teacher of Geography" },
+              skills_and_experience: { type: :string, example: "We're looking for a dedicated Teacher of Geography" },
+              salary: { type: :string, example: "£12,345 to £67, 890" },
+              benefits_details: { type: :string, example: "TLR2a" },
+              starts_on: { type: :string, example: "Summer Term" },
+              external_reference: { type: :string, example: "123GTZY" },
+              visa_sponsorship_available: { type: :boolean },
+              is_job_share: { type: :boolean },
+              schools: {
+                oneOf: [
+                  {
+                    type: :object,
+                    additionalProperties: false,
+                    required: %i[school_urns],
+                    properties: {
+                      school_urns: {
+                        type: :array,
+                        minItems: 1,
+                        items: {
+                          type: :integer,
+                          example: 12_345,
+                        },
+                      },
+                    },
+                  },
+                  {
+                    type: :object,
+                    additionalProperties: false,
+                    required: %i[trust_uid school_urns],
+                    properties: {
+                      trust_uid: {
+                        type: :integer,
+                        example: 12_345,
+                      },
+                      school_urns: {
+                        type: :array,
+                        minItems: 0,
+                        items: {
+                          type: :integer,
+                          example: 12_345,
+                        },
+                      },
+                    },
+                  },
+                  {
+                    type: :object,
+                    additionalProperties: false,
+                    required: %i[trust_uid],
+                    properties: {
+                      trust_uid: {
+                        type: :integer,
+                        example: 12_345,
+                      },
+                    },
+                  },
+                ],
+              },
+              job_roles: {
+                type: :array,
+                minItems: 1,
+                items: {
+                  type: :string,
+                  enum: Vacancy.job_roles.keys,
+                },
+              },
+              ect_suitable: {
+                type: :boolean,
+              },
+              working_patterns: {
+                type: :array,
+                minItems: 1,
+                items: {
+                  type: :string,
+                  enum: Vacancy.working_patterns.keys,
+                },
+              },
+              contract_type: {
+                type: :string,
+                enum: Vacancy.contract_types.keys,
+              },
+              phases: {
+                type: :array,
+                minItems: 1,
+                items: {
+                  type: :string,
+                  enum: Vacancy.phases.keys,
+                },
+              },
+              key_stages: {
+                type: :array,
+                minItems: 1,
+                items: {
+                  type: :string,
+                  enum: Vacancy.key_stages.keys,
+                },
+              },
+              subjects: {
+                type: :array,
+                minItems: 1,
+                items: {
+                  type: :string,
+                  enum: ["Accounting",
+                         "Art and design",
+                         "Biology",
+                         "Business Studies",
+                         "Chemistry",
+                         "Citizenship",
+                         "Classics",
+                         "Computing",
+                         "Dance",
+                         "Design And Technology",
+                         "Drama",
+                         "Economics",
+                         "Engineering",
+                         "English",
+                         "Food Technology",
+                         "French",
+                         "Geography",
+                         "German",
+                         "health_and_social_care",
+                         "history",
+                         "Humanities",
+                         "ICT",
+                         "Languages",
+                         "Law",
+                         "Mandarin",
+                         "Mathematics",
+                         "Media Studies",
+                         "Music",
+                         "Philosophy",
+                         "Physical Education",
+                         "Physics",
+                         "PSHE",
+                         "Psychology",
+                         "Religious Education",
+                         "Science",
+                         "Social Science",
+                         "Sociology",
+                         "Spanish",
+                         "Statistics"],
+                },
+              },
+            },
+          },
           unauthorized_error: {
             type: "object",
             properties: {
