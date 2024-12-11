03b74feb36dd8d21973d86a9795fe5d4388b8549
Stephen Dicks <github@starswan.com>
2024-12-11 17:06:27 +0000

    Religious form preview (#7333)

--- spec/support/vacancy_helpers.rb
+++ spec/support/vacancy_helpers.rb
@@ -104,11 +104,11 @@ module VacancyHelpers
   def fill_in_applying_for_the_job_form_fields(vacancy, local_authority_vacancy: false)
     return unless !local_authority_vacancy && vacancy.enable_job_applications?
 
-    choose strip_tags(I18n.t("helpers.label.publishers_job_listing_applying_for_the_job_form.enable_job_applications_options.true"))
+    find('label[for="publishers-job-listing-applying-for-the-job-form-application-form-type-no-religion-field"]').click
   end
 
   def fill_in_applying_for_the_job_disable_job_applications
-    choose strip_tags(I18n.t("helpers.label.publishers_job_listing_applying_for_the_job_form.enable_job_applications_options.false"))
+    choose strip_tags(I18n.t("helpers.label.publishers_job_listing_applying_for_the_job_form.application_form_type_options.other"))
   end
 
   def fill_in_how_to_receive_applications_form_fields(vacancy)
@@ -387,18 +387,18 @@ module VacancyHelpers
 
   def expect_correct_pay_package_options(vacancy)
     if vacancy.contract_type == "casual"
-      expect(page).not_to have_field("Full-time equivalent salary", type: "checkbox")
-      expect(page).not_to have_field("Actual salary", type: "checkbox")
-      expect(page).not_to have_field("Pay scale", type: "checkbox")
-      expect(page).to have_field("Hourly rate of pay", type: "checkbox")
+      expect(page).not_to have_content("Full-time equivalent salary")
+      expect(page).not_to have_content("Actual salary")
+      expect(page).not_to have_content("Pay scale")
+      expect(page).to have_content("Hourly rate of pay")
     else
-      expect(page).to have_field("Full-time equivalent salary", type: "checkbox")
-      expect(page).to have_field("Pay scale", type: "checkbox")
-      expect(page).to have_field("Hourly rate of pay", type: "checkbox")
+      expect(page).to have_content("Full-time equivalent salary")
+      expect(page).to have_content("Pay scale")
+      expect(page).to have_content("Hourly rate of pay")
       if vacancy.working_patterns.include? "part_time"
-        expect(page).to have_field("Actual salary", type: "checkbox")
+        expect(page).to have_content("Actual salary")
       else
-        expect(page).not_to have_field("Actual salary", type: "checkbox")
+        expect(page).not_to have_content("Actual salary")
       end
     end
   end
