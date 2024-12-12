1e318f902e44633505dde43a418b8773511d0b15
Davide Dippolito <davide.dippolito@digital.education.gov.uk>
2024-12-12 11:02:16 +0000

    Add linking to the errors

--- app/presenters/work_history_error_summary_presenter.rb
+++ app/presenters/work_history_error_summary_presenter.rb
@@ -1,14 +1,17 @@
 class WorkHistoryErrorSummaryPresenter
-  WORK_HISTORY_FIELD = "#jobseekers-job-application-employment-history-form-base-field-error".freeze
-
-  def initialize(error_messages)
+  def initialize(error_messages, unexplained_employment_gaps)
     @error_messages = error_messages
+    @unexplained_employment_gaps = unexplained_employment_gaps
   end
 
   def formatted_error_messages
     @error_messages.flat_map do |attribute, messages|
       if attribute == :base
-        messages.map { |message| [attribute, message, WORK_HISTORY_FIELD] }
+        messages.each_with_index.map do |message, index|
+          gap = @unexplained_employment_gaps.values[index]
+          gap_id = "gap-#{gap[:started_on].strftime('%Y%m%d')}-#{gap[:ended_on].strftime('%Y%m%d')}"
+          [attribute, message, "##{gap_id}"]
+        end
       else
         [[attribute, messages.first]]
       end
