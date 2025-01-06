1eff2a28769866587701bca3cbd2c08962a15303
Stephen Dicks <github@starswan.com>
2025-01-06 10:08:09 +0000

    Faith school candidate (#7335)

--- app/helpers/status_tag_helper.rb
+++ app/helpers/status_tag_helper.rb
@@ -1,20 +1,33 @@
 module StatusTagHelper
+  STATUS_COLOURS = {
+    optional: "grey",
+    not_started: "grey",
+    action_required: "red",
+    complete: "green",
+    in_progress: "yellow",
+    imported: "blue",
+  }.freeze
+  # rubocop:disable Lint/DuplicateBranch
   def review_section_tag(resource, form_classes)
     steps = form_classes.map(&:target_name)
-    if resource.is_a?(JobApplication) && steps.all? { |step| job_application_step_imported?(resource, step) }
-      imported
-    elsif form_classes.all?(&:optional?)
-      optional
-    elsif resource.is_a?(JobApplication) && steps.all? { |step| job_application_step_in_progress?(resource, step) }
-      in_progress
-    elsif steps.none? { |step| step_completed?(resource, step) }
-      not_started
-    elsif step_forms_contain_errors?(resource, form_classes)
-      action_required
-    else
-      complete
-    end
+    status = if resource.is_a?(JobApplication) && steps.all? { |step| job_application_step_imported?(resource, step) }
+               :imported
+             elsif form_classes.all?(&:optional?)
+               :optional
+             elsif resource.is_a?(JobApplication) && steps.all? { |step| job_application_step_in_progress?(resource, step) }
+               :in_progress
+             elsif steps.none? { |step| step_completed?(resource, step) }
+               :not_started
+             elsif step_forms_contain_errors?(resource, form_classes)
+               :action_required
+             elsif steps.all? { |step| step_completed?(resource, step) }
+               :complete
+             else
+               :in_progress
+             end
+    govuk_tag(text: t("shared.status_tags.#{status}"), colour: STATUS_COLOURS.fetch(status))
   end
+  # rubocop:enable Lint/DuplicateBranch
 
   private
 
@@ -26,29 +39,5 @@ module StatusTagHelper
     form_classes.any? do |form_class|
       form_class.fields.any? { |field| resource.errors.include?(field) }
     end
-  end
-
-  def optional
-    govuk_tag(text: t("shared.status_tags.optional"), colour: "grey")
-  end
-
-  def not_started
-    govuk_tag(text: t("shared.status_tags.not_started"), colour: "grey")
-  end
-
-  def action_required
-    govuk_tag(text: t("shared.status_tags.action_required"), colour: "red")
-  end
-
-  def complete
-    govuk_tag(text: t("shared.status_tags.complete"), colour: "green")
-  end
-
-  def in_progress
-    govuk_tag(text: t("shared.status_tags.in_progress"), colour: "yellow")
-  end
-
-  def imported
-    govuk_tag(text: t("shared.status_tags.imported"), colour: "blue")
   end
 end
