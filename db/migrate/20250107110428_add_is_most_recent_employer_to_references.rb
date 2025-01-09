7dc79e79769b430eee545e3ac2aeaa3e8d08be2b
Kyle MacPherson <kaimac2010@gmail.com>
2025-01-09 10:41:01 +0000

    Migration to use boolean type for is_most_recent_employer

--- db/migrate/20250107110428_add_is_most_recent_employer_to_references.rb
+++ db/migrate/20250107110428_add_is_most_recent_employer_to_references.rb
@@ -1,5 +1,5 @@
 class AddIsMostRecentEmployerToReferences < ActiveRecord::Migration[7.2]
   def change
-    add_column :references, :is_most_recent_employer, :string
+    add_column :references, :is_most_recent_employer, :boolean
   end
 end
