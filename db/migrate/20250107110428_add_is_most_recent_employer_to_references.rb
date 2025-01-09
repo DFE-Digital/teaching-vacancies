44f0c521117d483dd0bae0e10c00caca65258fbf
Kyle MacPherson <kaimac2010@gmail.com>
2025-01-09 10:53:24 +0000

    Disable Rails/ThreeStateBooleanColumn cop
    disabling the three state boolean rule which states boolean columns should always have not null constraint because in this case there is no reasonable way to accurately backfill this column for existing references.

--- db/migrate/20250107110428_add_is_most_recent_employer_to_references.rb
+++ db/migrate/20250107110428_add_is_most_recent_employer_to_references.rb
@@ -1,5 +1,9 @@
 class AddIsMostRecentEmployerToReferences < ActiveRecord::Migration[7.2]
+  # disabling the three state boolean rule which states boolean columns should always have not null constraint because in this case there is no reasonable way
+  # to accurately backfill this column for existing references.
+  # rubocop:disable Rails/ThreeStateBooleanColumn
   def change
     add_column :references, :is_most_recent_employer, :boolean
   end
+  # rubocop:enable Rails/ThreeStateBooleanColumn
 end
