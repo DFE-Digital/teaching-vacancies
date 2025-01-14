9af5aeb777a65ece5623f9ef537142868c72cd32
Marc Sardon <marc@bichito.software>
2025-01-14 13:42:37 +0000

    Clean Alerts with "delete_all"
    The destroy_all call was not adequate for the high volume of data, as
    instantiates each record in memory.
    
    Delete all executes everything with a single SQL query (what I
    originally intended and missed).

--- app/jobs/delete_old_alert_runs_job.rb
+++ app/jobs/delete_old_alert_runs_job.rb
@@ -2,6 +2,6 @@ class DeleteOldAlertRunsJob < ApplicationJob
   queue_as :low
 
   def perform
-    AlertRun.where(run_on: ...1.week.ago).destroy_all
+    AlertRun.where(run_on: ...1.week.ago).delete_all
   end
 end
