51546b139b0703f4439d234a235336f21a9fc34c
Kyle MacPherson <kaimac2010@gmail.com>
2025-01-15 14:47:41 +0000

    Improve performance of migration

--- app/services/qualifications_migration.rb
+++ app/services/qualifications_migration.rb
@@ -1,13 +1,11 @@
 class QualificationsMigration
   def self.perform
     ActiveRecord::Base.transaction do
-      qualifications = Qualification.where(category: "other_secondary")
-
-      qualifications.find_each do |qualification|
-        results = qualification.qualification_results
-        results.each do |result|
-          Qualification.create!(category: "other", subject: result.subject, grade: result.grade, institution: qualification.institution, name: qualification.name,
-                                year: qualification.year, job_application_id: qualification.job_application_id, jobseeker_profile_id: qualification.jobseeker_profile_id,
+      Qualification.includes(:qualification_results).where(category: "other_secondary").find_each(batch_size: 500) do |qualification|
+        qualification.qualification_results.each do |result|
+          Qualification.create!(category: "other", subject: result.subject, grade: result.grade,
+                                institution: qualification.institution, name: qualification.name, year: qualification.year,
+                                job_application_id: qualification.job_application_id, jobseeker_profile_id: qualification.jobseeker_profile_id,
                                 finished_studying: qualification.finished_studying)
         end
 
