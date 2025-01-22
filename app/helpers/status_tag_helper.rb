b96b63ab067a7faf541056e5ba556e654cbca5dd
starswan <github@starswan.com>
2025-01-22 15:46:22 +0000

    update job application status indicators

--- app/helpers/status_tag_helper.rb
+++ app/helpers/status_tag_helper.rb
@@ -11,22 +11,19 @@ module StatusTagHelper
   # rubocop:disable Lint/DuplicateBranch
   def review_section_tag(resource, form_classes)
     steps = form_classes.map(&:target_name)
-    status = if resource.is_a?(JobApplication) && steps.all? { |step| job_application_step_imported?(resource, step) }
-               :imported
-             elsif form_classes.all?(&:optional?)
-               :optional
-             elsif resource.is_a?(JobApplication) && steps.all? { |step| job_application_step_in_progress?(resource, step) }
-               :in_progress
-             elsif steps.none? { |step| step_completed?(resource, step) }
-               :not_started
-             elsif step_forms_contain_errors?(resource, form_classes)
-               :action_required
-             elsif steps.all? { |step| step_completed?(resource, step) }
-               :complete
-             else
-               :in_progress
-             end
-    govuk_tag(text: t("shared.status_tags.#{status}"), colour: STATUS_COLOURS.fetch(status))
+    if steps.all? { |step| resource.in_progress_steps.include?(step.to_s) }
+      incomplete
+    elsif steps.all? { |step| resource.imported_steps.include?(step.to_s) }
+      imported
+    elsif form_classes.all?(&:optional?)
+      optional
+    elsif steps.none? { |step| step_completed?(resource, step) }
+      incomplete
+    elsif step_forms_contain_errors?(resource, form_classes)
+      action_required
+    else
+      complete
+    end
   end
   # rubocop:enable Lint/DuplicateBranch
 
@@ -40,5 +37,29 @@ module StatusTagHelper
     form_classes.any? do |form_class|
       form_class.fields.any? { |field| resource.errors.include?(field) }
     end
+  end
+
+  def optional
+    govuk_tag(text: t("shared.status_tags.optional"), colour: "grey")
+  end
+
+  def not_started
+    govuk_tag(text: t("shared.status_tags.not_started"), colour: "grey")
+  end
+
+  def action_required
+    govuk_tag(text: t("shared.status_tags.action_required"), colour: "red")
+  end
+
+  def complete
+    govuk_tag(text: t("shared.status_tags.complete"), colour: "green")
+  end
+
+  def incomplete
+    govuk_tag(text: t("shared.status_tags.in_progress"), colour: "yellow")
+  end
+
+  def imported
+    govuk_tag(text: t("shared.status_tags.imported"), colour: "blue")
   end
 end
