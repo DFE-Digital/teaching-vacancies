05d0b1ecf3f14ac80919c98383c820e66e691266
Kyle MacPherson <kaimac2010@gmail.com>
2025-01-29 19:43:38 +0000

    rubocop linting

--- db/migrate/20250129180236_add_job_application_to_professional_body_memberships.rb
+++ db/migrate/20250129180236_add_job_application_to_professional_body_memberships.rb
@@ -1,6 +1,6 @@
 class AddJobApplicationToProfessionalBodyMemberships < ActiveRecord::Migration[7.2]
   disable_ddl_transaction!
-  
+
   def change
     add_reference_concurrently :professional_body_memberships, :job_application, type: :uuid, foreign_key: true
   end
