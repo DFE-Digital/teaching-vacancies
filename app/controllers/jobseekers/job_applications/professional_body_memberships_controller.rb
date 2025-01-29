25414affbc8d7dadb79fa5b087db34fd2ff92606
Kyle MacPherson <kaimac2010@gmail.com>
2025-01-29 18:24:58 +0000

    Allow creating/updating/deleting professional body memberships from job application

---
+++ app/controllers/jobseekers/job_applications/professional_body_memberships_controller.rb
@@ -0,0 +1,59 @@
+class Jobseekers::JobApplications::ProfessionalBodyMembershipsController < Jobseekers::ProfilesController
+  helper_method :back_path, :job_application, :professional_body_membership
+
+  def new
+    @form = Jobseekers::ProfessionalBodyMembershipForm.new
+  end
+
+  def edit
+    @form = Jobseekers::ProfessionalBodyMembershipForm.new(professional_body_membership.slice(:name, :membership_type, :membership_number, :year_membership_obtained, :exam_taken))
+  end
+
+  def create
+    @form = Jobseekers::ProfessionalBodyMembershipForm.new(professional_body_memberships_form_params)
+
+    if @form.valid?
+      job_application.professional_body_memberships.create!(professional_body_memberships_form_params)
+      redirect_to back_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @form = Jobseekers::ProfessionalBodyMembershipForm.new(professional_body_memberships_form_params)
+
+    if @form.valid?
+      professional_body_membership.update!(professional_body_memberships_form_params)
+      redirect_to back_path
+    else
+      render :edit
+    end
+  end
+
+  def confirm_destroy
+    @form = Jobseekers::ProfessionalBodyMembershipForm.new
+  end
+
+  def destroy
+    professional_body_membership.destroy!
+    redirect_to back_path, success: t(".success")
+  end
+
+  def professional_body_memberships_form_params
+    params.require(:jobseekers_professional_body_membership_form)
+          .permit(:name, :membership_type, :membership_number, :year_membership_obtained, :exam_taken)
+  end
+
+  def professional_body_membership
+    @professional_body_membership ||= job_application.professional_body_memberships.find(params[:id] || params[:professional_body_membership_id])
+  end
+
+  def job_application
+    @job_application ||= current_jobseeker.job_applications.draft.find(params[:job_application_id])
+  end
+
+  def back_path
+    @back_path ||= jobseekers_job_application_build_path(job_application, :professional_body_memberships)
+  end
+end
