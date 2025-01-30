ad8f215cafde8393b98435d207f6a2f4851d1932
starswan <github@starswan.com>
2025-01-30 11:47:10 +0000

    undo active job config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,8 +54,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job
   # (and separate queues per environment)
-  config.active_job.queue_adapter = :sidekiq
-
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "teaching-vacancies_#{Rails.env}"
   config.action_mailer.perform_caching = false
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,7 +38,6 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
 
-  config.active_job.queue_adapter = :test
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
