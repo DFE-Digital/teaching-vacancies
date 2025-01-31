53d3ab73a58ca1591e15a63450d1229f59eda037
starswan <github@starswan.com>
2025-01-31 11:09:11 +0000

    reduce target aftyer exclduing rake tasks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,7 +46,7 @@ if ENV.fetch("COVERAGE", 0).to_i.positive?
     add_group "Presenters", "app/presenters"
     add_group "Notifiers", "app/notifiers"
 
-    minimum_coverage line: 94.02, branch: 77.91
+    minimum_coverage line: 93.93, branch: 77.91
   end
 end
 
