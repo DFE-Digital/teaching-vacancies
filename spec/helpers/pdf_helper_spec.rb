8dce6d121c56ff4b15047c030597d5efa98563bc
Kyle MacPherson <kaimac2010@gmail.com>
2025-02-07 15:39:47 +0000

    Add additional tests to pdf_helper_spec

--- spec/helpers/pdf_helper_spec.rb
+++ spec/helpers/pdf_helper_spec.rb
@@ -52,9 +52,10 @@ RSpec.describe JobApplicationPdfGenerator, type: :service do
       expect(text_analysis.strings).to include(job_application.last_name)
       expect(text_analysis.strings).to include(job_application.email_address)
       expect(text_analysis.strings).to include(job_application.phone_number)
+      expect(text_analysis.strings).to include(job_application.national_insurance_number)
     end
 
-    context "when render professional body memberships" do
+    context "when rendering professional body memberships" do
       context "when professional body memberships exist" do
         let(:pdf_data) { text_analysis.strings.join(" ") }
 
@@ -79,6 +80,24 @@ RSpec.describe JobApplicationPdfGenerator, type: :service do
         it "displays the none message" do
           expect(pdf_data).to include(I18n.t("jobseekers.job_applications.show.professional_body_memberships.none"))
         end
+      end
+    end
+
+    context "when working_pattern_details is not present" do
+      let(:job_application_without_working_pattern_details) { create(:job_application, vacancy: vacancy, working_pattern_details: nil) }
+      let(:pdf_generator) { JobApplicationPdfGenerator.new(job_application_without_working_pattern_details, vacancy) }
+
+      it "does not show working pattern details" do
+        expect(text_analysis.strings).not_to include("orking pattern preference details")
+      end
+    end
+
+    context "when national insurance number is not present" do
+      let(:job_application_without_ni_number) { create(:job_application, vacancy: vacancy, national_insurance_number: nil) }
+      let(:pdf_generator) { JobApplicationPdfGenerator.new(job_application_without_ni_number, vacancy) }
+
+      it "does not show national insurance number" do
+        expect(text_analysis.strings).not_to include("National Insurance number")
       end
     end
   end
