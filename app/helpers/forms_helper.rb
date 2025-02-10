8309fd6867933943783dae425946c5e9724d62ee
Kyle MacPherson <kaimac2010@gmail.com>
2025-02-10 09:15:48 +0000

    Refactor forms helper to account for fields that are hashes

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,7 +1,6 @@
 module FormsHelper
   def render_divs_for_fields(form_model)
-    # Jobseekers::JobApplication::PersonalDetailsForm has a field { working_patterns: [] } so we need to use storable fields instead.
-    fields = form_model == Jobseekers::JobApplication::PersonalDetailsForm ? form_model.storable_fields : form_model.fields
+    fields = form_model.fields.map { |field| field.is_a?(Hash) ? field.keys.first : field }
     safe_join(fields.map { |field| tag.div(id: field) })
   end
 end
