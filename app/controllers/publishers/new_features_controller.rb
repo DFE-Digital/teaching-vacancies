4f8200cc817853060a925a9590e3bd811acd99c3
starswan <github@starswan.com>
2025-02-12 12:02:26 +0000

    test pass

--- app/controllers/publishers/new_features_controller.rb
+++ app/controllers/publishers/new_features_controller.rb
@@ -2,7 +2,7 @@ class Publishers::NewFeaturesController < Publishers::BaseController
   skip_before_action :check_terms_and_conditions
 
   # TODO: update when a new feature is introduced
-  NEW_FEATURES_PAGE_UPDATED_AT = DateTime.new(2025, 2, 13).freeze # This constant lives here so that we remember to update it.
+  NEW_FEATURES_PAGE_UPDATED_AT = DateTime.new(2025, 2, 12).freeze # This constant lives here so that we remember to update it.
 
   def reminder
     session[:visited_application_feature_reminder_page] = true
