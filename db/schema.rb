97193109f9c0babe80ae6299c9d4968f63a024e1
starswan <github@starswan.com>
2025-02-12 11:01:15 +0000

    rerbase schema

--- db/schema.rb
+++ db/schema.rb
@@ -264,6 +264,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_10_105445) do
     t.string "ethos_and_aims_ciphertext"
     t.integer "working_patterns", array: true
     t.string "working_pattern_details"
+    t.string "qts_age_range_and_subject"
     t.index ["jobseeker_id"], name: "index_job_applications_jobseeker_id"
     t.index ["vacancy_id"], name: "index_job_applications_on_vacancy_id"
   end
@@ -316,6 +317,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_10_105445) do
     t.string "statutory_induction_complete"
     t.string "has_teacher_reference_number"
     t.string "statutory_induction_complete_details"
+    t.string "qts_age_range_and_subject"
     t.index ["jobseeker_id"], name: "index_jobseeker_profiles_jobseeker_id", unique: true
   end
 
@@ -328,8 +330,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_10_105445) do
     t.datetime "updated_at", null: false
     t.date "account_closed_on"
     t.text "current_sign_in_ip_ciphertext"
-    t.text "last_sign_in_ip_ciphertext"
     t.string "govuk_one_login_id"
+    t.text "last_sign_in_ip_ciphertext"
     t.string "account_merge_confirmation_code"
     t.datetime "account_merge_confirmation_code_generated_at"
     t.index ["email"], name: "index_jobseekers_on_email", unique: true
