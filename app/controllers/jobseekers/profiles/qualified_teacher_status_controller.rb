362a424ede31407dee123b13f73b4ea1397d249d
starswan <github@starswan.com>
2025-02-13 17:08:30 +0000

    controller refactor

--- app/controllers/jobseekers/profiles/qualified_teacher_status_controller.rb
+++ app/controllers/jobseekers/profiles/qualified_teacher_status_controller.rb
@@ -1,43 +1,34 @@
-class Jobseekers::Profiles::QualifiedTeacherStatusController < Jobseekers::ProfilesController
-  def edit; end
-
-  def update
-    if form.valid?
-      year = form.qualified_teacher_status == "yes" ? form.qualified_teacher_status_year : ""
-      profile.update(qualified_teacher_status: JobseekerProfile.qualified_teacher_statuses[form.qualified_teacher_status],
-                     qualified_teacher_status_year: year,
-                     teacher_reference_number: form.updated_teacher_reference_number,
-                     statutory_induction_complete: form.statutory_induction_complete,
-                     has_teacher_reference_number: form.has_teacher_reference_number,
-                     statutory_induction_complete_details: form.statutory_induction_complete_details)
-      redirect_to jobseekers_profile_qualified_teacher_status_path
-    else
-      render :edit
+module Jobseekers
+  class Profiles::QualifiedTeacherStatusController < ProfilesController
+    def edit
+      @form = Profile::QualifiedTeacherStatusForm.new(profile.slice(*(form_class.fields - %i[qualified_teacher_status_details])))
     end
-  end
 
-  private
-
-  helper_method :form
-
-  def form
-    @form ||= Jobseekers::Profile::QualifiedTeacherStatusForm.new(form_attributes)
-  end
-
-  def form_attributes
-    case action_name
-    when "edit"
-      profile.slice(:qualified_teacher_status, :qualified_teacher_status_year, :teacher_reference_number, :statutory_induction_complete, :has_teacher_reference_number, :statutory_induction_complete_details)
-    when "update"
-      form_params
+    def update
+      @form = Profile::QualifiedTeacherStatusForm.new(form_params)
+      if @form.valid?
+        year = @form.qualified_teacher_status == "yes" ? @form.qualified_teacher_status_year : ""
+        profile.update(qualified_teacher_status: JobseekerProfile.qualified_teacher_statuses[@form.qualified_teacher_status],
+                       qualified_teacher_status_year: year,
+                       teacher_reference_number: @form.updated_teacher_reference_number,
+                       statutory_induction_complete: @form.statutory_induction_complete,
+                       qts_age_range_and_subject: @form.qts_age_range_and_subject,
+                       has_teacher_reference_number: @form.has_teacher_reference_number,
+                       statutory_induction_complete_details: @form.statutory_induction_complete_details)
+        redirect_to jobseekers_profile_qualified_teacher_status_path
+      else
+        render :edit
+      end
     end
-  end
 
-  def form_class
-    "jobseekers/profile/qualified_teacher_status_form".camelize.constantize
-  end
+    private
 
-  def form_params
-    params.require(:jobseekers_profile_qualified_teacher_status_form).permit(form_class.fields)
+    def form_class
+      Profile::QualifiedTeacherStatusForm
+    end
+
+    def form_params
+      params.require(:jobseekers_profile_qualified_teacher_status_form).permit(form_class.fields)
+    end
   end
 end
