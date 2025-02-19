579f5cf1c0adc27fd86aee212ee2379420fbddd2
Kyle MacPherson <kaimac2010@gmail.com>
2025-02-19 17:17:09 +0000

    Ensure flexi_working_details_provided is always a boolean

--- app/form_models/publishers/job_listing/about_the_role_form.rb
+++ app/form_models/publishers/job_listing/about_the_role_form.rb
@@ -1,4 +1,6 @@
 class Publishers::JobListing::AboutTheRoleForm < Publishers::JobListing::VacancyForm
+  include ActiveModel::Attributes
+
   validates :ect_status, inclusion: { in: Vacancy.ect_statuses.keys }, if: -> { vacancy&.job_roles&.include?("teacher") }
   validate :job_advert_presence, if: -> { vacancy.job_advert.present? }
   validate :about_school_presence, if: -> { vacancy.about_school.present? }
@@ -9,8 +11,10 @@ class Publishers::JobListing::AboutTheRoleForm < Publishers::JobListing::Vacancy
   validate :safeguarding_information_does_not_exceed_maximum_words, if: -> { safeguarding_information_provided == "true" }, unless: -> { vacancy.job_advert.present? || vacancy.about_school.present? }
   validates :further_details_provided, inclusion: { in: [true, false, "true", "false"] }, unless: -> { vacancy.job_advert.present? || vacancy.about_school.present? }
   validate :further_details_presence, if: -> { further_details_provided == "true" }, unless: -> { vacancy.job_advert.present? || vacancy.about_school.present? }
-  validates :flexi_working_details_provided, inclusion: { in: [true, false, "true", "false"] }
-  validate :flexi_working_presence, if: -> { flexi_working_details_provided == "true" }
+  validates :flexi_working_details_provided, inclusion: { in: [true, false] }
+  validate :flexi_working_presence, if: -> { flexi_working_details_provided }
+
+  attribute :flexi_working_details_provided, :boolean
 
   def self.fields
     %i[
@@ -24,7 +28,6 @@ class Publishers::JobListing::AboutTheRoleForm < Publishers::JobListing::Vacancy
       safeguarding_information
       further_details_provided
       further_details
-      flexi_working_details_provided
     ]
   end
   attr_accessor(*fields)
@@ -44,6 +47,13 @@ class Publishers::JobListing::AboutTheRoleForm < Publishers::JobListing::Vacancy
       flexi_working_details_provided:,
     }
   end
+
+  # Overriding load_form to include :flexi_working_details_provided since it's not part of `self.fields`. This is necessary because 
+  # :flexi_working_details_provided is explicitly defined as a boolean attribute.
+  def self.load_form(model)
+    fields = self.fields << :flexi_working_details_provided
+    model.slice(*fields)
+  end  
 
   private
 
