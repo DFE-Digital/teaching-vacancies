8030d0e680114254e7ed223128f250b75937854f
starswan <github@starswan.com>
2025-02-25 08:57:10 +0000

    add view spec

--- spec/views/vacancies/show.html.slim_spec.rb
+++ spec/views/vacancies/show.html.slim_spec.rb
@@ -1,56 +1,39 @@
 require "rails_helper"
 
-RSpec.describe "vacancies/show", type: :view do
-  let(:salary) { "27000" }
+RSpec.describe "vacancies/show" do
   before do
-    assign :vacancy, VacancyPresenter.new(create(:vacancy, hourly_rate: nil, salary: salary))
+    assign :vacancy, VacancyPresenter.new(create(:vacancy, hourly_rate: hourly_rate, salary: salary))
     render
   end
 
-  it "has salary" do
-    expect(rendered).to have_content(salary)
+  describe "job posting metadata" do
+    let(:json_ld) { JSON.parse(rendered.html.css("script.jobref").inner_text, symbolize_names: true) }
+
+    context "with hourly rate" do
+      let(:hourly_rate) { 25 }
+      let(:salary) { "27000" }
+
+      it "has hourly rate as salary" do
+        expect(json_ld.fetch(:baseSalary)).to eq({ :@type => "MonetaryAmount", :currency => "GBP", :value => { :@type => "QuantitativeValue", :unitText => "HOUR", :value => "25" } })
+      end
+    end
+
+    context "without hourly rate" do
+      let(:hourly_rate) { nil }
+      let(:salary) { "27000" }
+
+      it "has salary instead" do
+        expect(json_ld.fetch(:baseSalary)).to eq({ :@type => "MonetaryAmount", :currency => "GBP", :value => { :@type => "QuantitativeValue", :unitText => "YEAR", :value => "27000" } })
+      end
+    end
+
+    context "without money" do
+      let(:hourly_rate) { nil }
+      let(:salary) { nil }
+
+      it "has no salary" do
+        expect(json_ld.key?(:baseSalary)).to be(false)
+      end
+    end
   end
-
-  # def vacancy_json_ld(vacancy)
-  #   {
-  #     "@context": "http://schema.org",
-  #     "@type": "JobPosting",
-  #     baseSalary: {
-  #       :@type=>"MonetaryAmount",
-  #       :currency=>"GBP",
-  #       :value=>{:@type=>"QuantitativeValue", :unitText=>"HOUR", :value=>"£25 per hour"}
-  #     },
-  #     title: vacancy.job_title,
-  #     jobBenefits: vacancy.benefits_details,
-  #     datePosted: vacancy.publish_on.iso8601,
-  #     description: vacancy.skills_and_experience.present? ? vacancy.skills_and_experience : vacancy.job_advert,
-  #     occupationalCategory: vacancy.job_roles.first,
-  #     directApply: vacancy.enable_job_applications,
-  #     employmentType: vacancy.working_patterns_for_job_schema,
-  #     industry: "Education",
-  #     jobLocation: {
-  #       "@type": "Place",
-  #       address: {
-  #         "@type": "PostalAddress",
-  #         addressLocality: vacancy.organisation.town,
-  #         addressRegion: vacancy.organisation.region,
-  #         streetAddress: vacancy.organisation.address,
-  #         postalCode: vacancy.organisation.postcode,
-  #         addressCountry: "GB",
-  #       },
-  #     },
-  #     url: job_url(vacancy),
-  #     hiringOrganization: {
-  #       "@type": "Organization",
-  #       logo: image_path("images/govuk-icon-180.png"),
-  #       name: vacancy.organisation_name,
-  #       sameAs: vacancy.organisation.url,
-  #       identifier: vacancy.organisation.urn,
-  #       description: vacancy.about_school,
-  #     },
-  #     validThrough: vacancy.expires_at.to_time.iso8601,
-  #   }
-  # end
-
-
 end
