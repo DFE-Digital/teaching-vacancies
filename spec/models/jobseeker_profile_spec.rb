dd3d64c3263db1e46306d6e8fc5cfcd8a4ea0b3c
Kyle MacPherson <kaimac2010@gmail.com>
2025-02-26 11:00:24 +0000

    Ensure data is not copied from draft applications into jobseeker profiles

--- spec/models/jobseeker_profile_spec.rb
+++ spec/models/jobseeker_profile_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe JobseekerProfile, type: :model do
       end
     end
 
-    context "when the jobseeker has a previous application" do
+    context "when the jobseeker has a previously submitted application" do
       let!(:previous_application) { create(:job_application, :status_submitted, jobseeker:) }
 
       it "uses the details from the previous application" do
@@ -22,6 +22,24 @@ RSpec.describe JobseekerProfile, type: :model do
         expect(profile.qualified_teacher_status_year).to eq(previous_application.qualified_teacher_status_year)
         expect(profile.qualified_teacher_status).to eq(previous_application.qualified_teacher_status)
         expect(profile.personal_details.first_name).to eq(previous_application.first_name)
+        expect(profile.personal_details.last_name).to eq(previous_application.last_name)
+      end
+
+      it "creates a job preferences record" do
+        expect(profile.job_preferences).to be_present
+      end
+    end
+
+    context "when the jobseeker has a previous draft application" do
+      let!(:previous_application) { create(:job_application, :status_draft, jobseeker:) }
+
+      it "does not use the details from the draft application" do
+        expect(profile.employments.map(&:job_title).sort).to be_empty
+        expect(profile.qualifications.map(&:institution).sort).to be_empty
+        expect(profile.qualified_teacher_status_year).to be_nil
+        expect(profile.qualified_teacher_status).to be_nil
+        expect(profile.personal_details.first_name).to be_nil
+        expect(profile.personal_details.last_name).to be_nil
       end
 
       it "creates a job preferences record" do
--- spec/models/personal_details_spec.rb
+++ spec/models/personal_details_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe PersonalDetails do
       end
     end
 
-    context "when the profile has a previous application" do
+    context "when the profile has a previously submitted application" do
       let!(:previous_application) { create(:job_application, :status_submitted, jobseeker:) }
 
       it "uses the details from the previous application" do
@@ -30,6 +30,21 @@ RSpec.describe PersonalDetails do
 
       it "sets some steps to completed" do
         expect(personal_details.completed_steps).to include("name", "phone_number", "work")
+      end
+    end
+
+    context "when the jobseeker has a previous draft application" do
+      let!(:previous_application) { create(:job_application, :status_draft, jobseeker:) }
+
+      it "uses the details from the previous application" do
+        expect(personal_details.first_name).to be_nil
+        expect(personal_details.last_name).to be_nil
+        expect(personal_details.phone_number).to be_nil
+        expect(personal_details.right_to_work_in_uk).to be_nil
+      end
+
+      it "does not set steps to completed" do
+        expect(personal_details.completed_steps).to be_blank
       end
     end
 
