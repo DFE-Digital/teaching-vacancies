99afe4b48b7f94ee46e2bcfc0d99b918436f1ae8
Kyle MacPherson <kaimac2010@gmail.com>
2025-02-26 13:45:25 +0000

    Fix tests

--- spec/system/jobseekers/jobseekers_can_manage_a_profile_spec.rb
+++ spec/system/jobseekers/jobseekers_can_manage_a_profile_spec.rb
@@ -127,8 +127,8 @@ RSpec.describe "Jobseekers can manage their profile" do
       end
     end
 
-    describe "personal details if the jobseeker has a blank previous job application" do
-      let!(:previous_application) { create(:job_application, :status_draft, jobseeker:, first_name: nil, last_name: nil, phone_number: "01234567890") }
+    describe "personal details if the jobseeker has a blank previously submitted job application" do
+      let!(:previous_application) { create(:job_application, :status_submitted, jobseeker:, first_name: nil, last_name: nil, phone_number: "01234567890") }
 
       before do
         visit jobseekers_profile_path
@@ -136,6 +136,22 @@ RSpec.describe "Jobseekers can manage their profile" do
 
       it "prefills the form with the jobseeker's provided personal details" do
         expect(page).to have_content(previous_application.phone_number)
+      end
+
+      it "still shows the summary rows for the blank attributes" do
+        expect(page).to have_content("Name")
+      end
+    end
+
+    describe "personal details if the jobseeker has a blank previous draft job application" do
+      let!(:previous_application) { create(:job_application, :status_draft, jobseeker:, first_name: nil, last_name: nil, phone_number: "01234567890") }
+
+      before do
+        visit jobseekers_profile_path
+      end
+
+      it "prefills the form with the jobseeker's provided personal details" do
+        expect(page).not_to have_content(previous_application.phone_number)
       end
 
       it "still shows the summary rows for the blank attributes" do
