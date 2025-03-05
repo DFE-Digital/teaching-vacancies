bb92414ff16a67cdc972ab5b3b761d5089d47c98
Kyle MacPherson <kaimac2010@gmail.com>
2025-03-05 15:31:48 +0000

    wip

--- app/form_models/publishers/job_listing/contract_type_form.rb
+++ app/form_models/publishers/job_listing/contract_type_form.rb
@@ -8,8 +8,27 @@ class Publishers::JobListing::ContractTypeForm < Publishers::JobListing::Vacancy
   validates :contract_type, inclusion: { in: Vacancy.contract_types.keys }
   validates :fixed_term_contract_duration, presence: true, if: -> { contract_type == "fixed_term" }
   validates :is_parental_leave_cover, inclusion: { in: [true, false] }, if: -> { contract_type == "fixed_term" }
+  validates :working_patterns, presence: true, inclusion: { in: Vacancy.working_patterns.keys - ["job_share"] }
+  validates :is_job_share, inclusion: { in: [true, false, "true", "false"] }
+  validate :working_patterns_details_does_not_exceed_maximum_words
 
+  # probably needs changing
   def self.fields
-    %i[contract_type fixed_term_contract_duration is_parental_leave_cover]
+    %i[working_patterns working_patterns_details is_job_share contract_type fixed_term_contract_duration is_parental_leave_cover]
+  end
+  attr_accessor(*fields)
+
+  def params_to_save
+    { working_patterns:, working_patterns_details:, is_job_share:, contract_type:, fixed_term_contract_duration:, is_parental_leave_cover:}
+  end
+
+  private
+
+  def working_patterns_details_does_not_exceed_maximum_words
+    return unless number_of_words_exceeds_permitted_length?(75, working_patterns_details)
+
+    errors.add(:working_patterns_details,
+               :working_patterns_details_maximum_words,
+               message: I18n.t("working_patterns_errors.working_patterns_details.maximum_words"))
   end
 end
