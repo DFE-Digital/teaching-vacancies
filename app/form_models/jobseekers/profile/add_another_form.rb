58f44d675477d91b36a7a048cffdd7d4f42b38b0
starswan <github@starswan.com>
2025-03-05 11:48:39 +0000

    tidy up, remove old backfill task

--- app/form_models/jobseekers/profile/add_another_form.rb
+++ app/form_models/jobseekers/profile/add_another_form.rb
@@ -5,9 +5,4 @@ class Jobseekers::Profile::AddAnotherForm < BaseForm
   attribute :add_another, :boolean
 
   validates :add_another, inclusion: { in: [true, false] }
-
-  # def initialize(attributes = {})
-  #   self.add_another = ActiveModel::Type::Boolean.new.cast(attributes.delete(:add_another))
-  #   super
-  # end
 end
--- app/form_models/jobseekers/profile/hide_profile_form.rb
+++ app/form_models/jobseekers/profile/hide_profile_form.rb
@@ -5,9 +5,4 @@ class Jobseekers::Profile::HideProfileForm < BaseForm
   attribute :requested_hidden_profile, :boolean
 
   validates :requested_hidden_profile, inclusion: { in: [true, false] }
-
-  # def initialize(attributes = {})
-  #   self.requested_hidden_profile = ActiveModel::Type::Boolean.new.cast(attributes.delete(:requested_hidden_profile))
-  #   super
-  # end
 end
