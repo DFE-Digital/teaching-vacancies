2e5a87728c1ac8ac31beae8e21b89fa3268a08aa
starswan <github@starswan.com>
2025-03-05 16:04:34 +0000

    Tidy up

--- app/controllers/jobseekers/email_preferences_controller.rb
+++ app/controllers/jobseekers/email_preferences_controller.rb
@@ -1,32 +1,25 @@
-class Jobseekers::EmailPreferencesController < Jobseekers::BaseController
-  def edit
-    @form = Jobseekers::EmailPreferencesForm.new(current_jobseeker)
-  end
+module Jobseekers
+  class EmailPreferencesController < BaseController
+    before_action :set_model
 
-  def update
-    @form = Jobseekers::EmailPreferencesForm.new(current_jobseeker, email_preferences_params)
-    
-    if @form.valid?
-      current_jobseeker.update(
-        email_opt_out: @form.email_opt_out == "true",
-        email_opt_out_reason: @form.email_opt_out == "true" ? @form.email_opt_out_reason : nil,
-        email_opt_out_comment: @form.email_opt_out == "true" ? @form.email_opt_out_comment : nil,
-        email_opt_out_at: @form.email_opt_out == "true" ? Time.current : nil
-      )
-      
-      redirect_to jobseekers_account_path, success: t(".success")
-    else
-      render :edit
+    def edit; end
+
+    def update
+      if @model.update(email_preferences_params)
+        redirect_to jobseekers_account_path, success: t(".success")
+      else
+        render :edit
+      end
     end
-  end
 
-  private
+    private
 
-  def email_preferences_params
-    params.require(:jobseekers_email_preferences_form).permit(
-      :email_opt_out,
-      :email_opt_out_reason,
-      :email_opt_out_comment
-    )
+    def set_model
+      @model = current_jobseeker
+    end
+
+    def email_preferences_params
+      params.require(:jobseekers_email_preferences_form).permit(:email_opt_out, :email_opt_out_reason, :email_opt_out_comment)
+    end
   end
 end
