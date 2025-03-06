2dc52229ce8339029fd2ba14d76d8b5f1ada84b9
Marc Sardon <pringaohowto@gmail.com>
2025-03-06 13:42:23 +0000

    Add configuration tests for campaign filters
    Ensure each one of the values present in the campaigns configurations
    matches an:
    - Existing banner image.
    - Existing teaching job role.
    - Existing subject.
    
    So, if the wrong value is entered when adding/updating a campaign, will
    be caught by our tests.

--- spec/configuration/campaign_pages_configuration_spec.rb
+++ spec/configuration/campaign_pages_configuration_spec.rb
@@ -10,4 +10,34 @@ RSpec.describe "Landing page configuration" do
       end
     end
   end
+
+  it "each campaign page configuration points to an existing banner image" do
+    Rails.application.config.campaign_pages.each do |campaign, config|
+      expect { ActionController::Base.helpers.image_path(config[:banner_image]) }
+        .not_to raise_error, "Image asset for campaign '#{campaign}' does not exist at 'app/assets/images/#{config[:banner_image]}'"
+    end
+  end
+
+  it "each campaign page configuration with teaching job roles filters by valid roles" do
+    Rails.application.config.campaign_pages.each do |campaign, config|
+      next if config[:teaching_job_roles].blank?
+
+      config[:teaching_job_roles].each do |job_role| # rubocop:disable Rspect/IteratedExpectation
+        expect(job_role).to be_in(Vacancy::TEACHING_JOB_ROLES),
+                            "Invalid teaching job role '#{job_role}' for campaign '#{campaign}'" # Needed iterated expectation for this descriptive error
+      end
+    end
+  end
+
+  it "each campaign page configuration with subjects filters by valid subjects" do
+    valid_subjects = SUBJECT_OPTIONS.map(&:first) # List of available subjects in the service (from subjects.yml)
+    Rails.application.config.campaign_pages.each do |campaign, config|
+      next if config[:subjects].blank?
+
+      config[:subjects].each do |subject| # rubocop:disable Rspect/IteratedExpectation
+        expect(subject).to be_in(valid_subjects),
+                           "Invalid subject '#{subject}' for campaign '#{campaign}'" # Needed iterated expectation for this descriptive error
+      end
+    end
+  end
 end
