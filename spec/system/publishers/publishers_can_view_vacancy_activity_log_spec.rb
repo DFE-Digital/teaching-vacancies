9046165c7270709e6facd9cf99cf8fd4bb241df0
Kyle MacPherson <kaimac2010@gmail.com>
2025-03-07 10:13:29 +0000

    Refactor yml files

--- spec/system/publishers/publishers_can_view_vacancy_activity_log_spec.rb
+++ spec/system/publishers/publishers_can_view_vacancy_activity_log_spec.rb
@@ -27,10 +27,10 @@ RSpec.describe "Publishers can view a vacancy's activity log", versioning: true 
     click_review_page_change_link(section: "job_details", row: "working_patterns")
     expect(current_path).to eq(organisation_job_build_path(vacancy.id, :working_patterns))
 
-    choose I18n.t("helpers.label.publishers_job_listing_contract_type_form.contract_type_options.#{new_contract_type}")
+    choose I18n.t("helpers.label.publishers_job_listing_working_patterns_form.contract_type_options.#{new_contract_type}")
     within("#publishers-job-listing-working-patterns-form-contract-type-fixed-term-conditional") do
       choose "Yes"
-      fill_in I18n.t("helpers.label.publishers_job_listing_contract_type_form.fixed_term_contract_duration"), with: "6 months"
+      fill_in I18n.t("helpers.label.publishers_job_listing_working_patterns_form.fixed_term_contract_duration"), with: "6 months"
     end
 
     click_on I18n.t("buttons.save_and_continue")
