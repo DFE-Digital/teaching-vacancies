273acb2bdcb77134f0be0f7eeadb433a8c0409dc
Kyle MacPherson <kaimac2010@gmail.com>
2025-03-11 12:23:31 +0000

    Use booleans attributes for is_job_share and is_parental_leave_cover

--- app/form_models/publishers/job_listing/contract_information_form.rb
+++ app/form_models/publishers/job_listing/contract_information_form.rb
@@ -4,17 +4,17 @@ class Publishers::JobListing::ContractInformationForm < Publishers::JobListing::
 
   validates :working_patterns, presence: true, inclusion: { in: Vacancy::WORKING_PATTERNS }
   validates :fixed_term_contract_duration, presence: true, if: -> { contract_type == "fixed_term" }
-  validates :is_parental_leave_cover, inclusion: { in: ["true", "false", true, false] }, if: -> { contract_type == "fixed_term" }
-  validates :working_patterns, presence: true, inclusion: { in: Vacancy.working_patterns.keys - ["job_share"] }
+  validates :is_parental_leave_cover, inclusion: { in: [true, false] }, if: -> { contract_type == "fixed_term" }
+  validates :working_patterns, presence: true, inclusion: { in: Vacancy.working_patterns.keys - %w[job_share] }
   validates :is_job_share, inclusion: { in: [true, false] }
   validate :working_patterns_details_does_not_exceed_maximum_words
 
   attribute :working_patterns, array: true, default: []
   attribute :working_patterns_details
-  attribute :is_job_share
+  attribute :is_job_share, :boolean
   attribute :contract_type
   attribute :fixed_term_contract_duration
-  attribute :is_parental_leave_cover
+  attribute :is_parental_leave_cover, :boolean
 
   class << self
     # Overriding load_form because we use attributes in this form rather than defining the fields like we do in other forms.
