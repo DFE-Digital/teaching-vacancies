c1b13df3095de4a29386ea6acb82aa18ee8890c1
Kyle MacPherson <kaimac2010@gmail.com>
2025-03-14 11:20:02 +0000

    Linting

--- app/form_models/publishers/job_listing/contract_type_form.rb
+++ app/form_models/publishers/job_listing/contract_type_form.rb
@@ -8,7 +8,7 @@ class Publishers::JobListing::ContractTypeForm < Publishers::JobListing::Vacancy
   validates :contract_type, inclusion: { in: Vacancy.contract_types.keys }
   validates :fixed_term_contract_duration, presence: true, if: -> { contract_type == "fixed_term" }
   validates :is_parental_leave_cover, inclusion: { in: [true, false] }, if: -> { contract_type == "fixed_term" }
-  validates :working_patterns, presence: true, inclusion: { in: Vacancy.working_patterns.keys - ["job_share"] }
+  validates :working_patterns, presence: true, inclusion: { in: Vacancy.working_patterns.keys - %w[job_share] }
   validates :is_job_share, inclusion: { in: [true, false, "true", "false"] }
   validate :working_patterns_details_does_not_exceed_maximum_words
 
