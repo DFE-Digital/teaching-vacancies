2cfaa74a9a77be8f564c91e9304f81df0e47685c
Stephen Dicks <github@starswan.com>
2025-03-17 10:22:09 +0000

    rubocop-govuk 5.0.10 ugrade (#7627)

--- spec/support/application_helpers.rb
+++ spec/support/application_helpers.rb
@@ -1,5 +1,5 @@
 module ApplicationHelpers
   def script_tag_content(wrapper_class: "")
-    page.all("script#{wrapper_class}", visible: false).first.native.text.strip
+    page.first("script#{wrapper_class}", visible: false).native.text.strip
   end
 end
