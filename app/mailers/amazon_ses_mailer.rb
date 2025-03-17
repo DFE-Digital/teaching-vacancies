a1bccf8e231ba0fc9a817569357880ec84244b97
starswan <github@starswan.com>
2025-03-17 11:10:40 +0000

    spike SES

--- app/mailers/amazon_ses_mailer.rb
+++ app/mailers/amazon_ses_mailer.rb
@@ -14,6 +14,8 @@ class AmazonSesMailer < ActionMailer::Base
 
   default from: "ats.teachingvacancies@service.education.gov.uk"
 
+  default from: "ats.teachingvacancies@service.education.gov.uk"
+
   def send_email(to:, subject:)
     @to = to
     mail(to: to, subject: subject, delivery_method_options: {
