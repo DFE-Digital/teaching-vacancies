83c18c6cc2ce752027471fcf233d8c129f56f62b
Kyle MacPherson <kaimac2010@gmail.com>
2025-03-27 10:32:50 +0000

    Fix migration

--- db/migrate/20250325153836_create_vacancy_analytics.rb
+++ db/migrate/20250325153836_create_vacancy_analytics.rb
@@ -1,7 +1,7 @@
 class CreateVacancyAnalytics < ActiveRecord::Migration[7.2]
   def change
     create_table :vacancy_analytics, id: :uuid do |t|
-      t.uuid :vacancy_id, null: false, index: true
+      t.references :vacancy, null: false, foreign_key: true, type: :uuid, index: false
       t.string :referrer_url, null: false
       t.date :date, null: false
       t.integer :visit_count, default: 0, null: false
@@ -10,7 +10,6 @@ class CreateVacancyAnalytics < ActiveRecord::Migration[7.2]
 
       # Composite index to speed up querying and enforce uniqueness constraint
       t.index %i[vacancy_id referrer_url date], unique: true, name: "index_vacancy_referrer_stats_on_vacancy_referrer_and_date"
-      # Index to speed up querying by date range
       t.index :date
     end
   end
