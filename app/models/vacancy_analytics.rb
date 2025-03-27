13e24c7f5d1953bfc877b93c1fb51182542f4211
Kyle MacPherson <kaimac2010@gmail.com>
2025-03-27 11:39:27 +0000

    Remove unused methods

--- app/models/vacancy_analytics.rb
+++ app/models/vacancy_analytics.rb
@@ -3,18 +3,6 @@ class VacancyAnalytics < ApplicationRecord
 
   validates :referrer_url, presence: true
   validates :date, presence: true
-
   # Prevent duplicate entries for the same vacancy, referrer and date
   validates :referrer_url, uniqueness: { scope: %i[vacancy_id date] }
-
-  scope :for_date_range, ->(start_date, end_date) { where(date: start_date..end_date) }
-  scope :by_referrer, ->(referrer) { where(referrer_url: referrer) }
-
-  def track_vacancy_view(referrer)
-    # Track asynchronously to not impact response time
-    TrackVacancyViewJob.perform_later(
-      vacancy_id: id,
-      referrer_url: referrer,
-    )
-  end
 end
