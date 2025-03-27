b85b81d2177a63c3b2e460bb4f63c6aa0a250ba2
Kyle MacPherson <kaimac2010@gmail.com>
2025-03-27 15:13:06 +0000

    Refactor test

--- spec/services/vacancy_analytics_service_spec.rb
+++ spec/services/vacancy_analytics_service_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
 RSpec.describe VacancyAnalyticsService do
-  let(:vacancy_id) { SecureRandom.uuid }
+  let(:vacancy) { create(:vacancy) }
   let(:referrer_url) { "https://example.com/some/path?utm=source" }
   let(:normalized_referrer) { "example.com" }
-  let(:redis_key) { "vacancy_referrer_stats:#{vacancy_id}:#{normalized_referrer}" }
+  let(:redis_key) { "vacancy_referrer_stats:#{vacancy.id}:#{normalized_referrer}" }
 
   before do
     # Use fresh Redis DB (or clear key)
@@ -17,24 +17,14 @@ RSpec.describe VacancyAnalyticsService do
   describe ".track_visit" do
     it "increments the Redis counter for a normalized referrer" do
       expect {
-        described_class.track_visit(vacancy_id, referrer_url)
+        described_class.track_visit(vacancy.id, referrer_url)
       }.to change { Redis.current.get(redis_key).to_i }.by(1)
     end
 
-    it "does nothing if vacancy_id is blank" do
+    it "does nothing if vacancy id is blank" do
       expect {
         described_class.track_visit(nil, referrer_url)
       }.not_to(change { Redis.current.keys("vacancy_referrer_stats:*").count })
-    end
-
-    it "skips keys with zero counts" do
-      Redis.current.set(redis_key, 0)
-    
-      expect {
-        described_class.aggregate_and_save_stats
-      }.not_to change(VacancyAnalytics, :count)
-    
-      expect(Redis.current.get(key)).to eq("0") # key is not deleted
     end
   end
 
@@ -53,24 +43,29 @@ RSpec.describe VacancyAnalyticsService do
   end
 
   describe ".aggregate_and_save_stats" do
-    let(:key) { "vacancy_referrer_stats:#{Date.current}:#{vacancy_id}:#{normalized_referrer}" }
-
-    before do
-      Redis.current.set(key, 5)
-    end
+    let(:key) { "vacancy_referrer_stats:#{Date.current}:#{vacancy.id}:#{normalized_referrer}" }
 
     it "upserts the correct stat into the database and deletes the Redis key" do
+      Redis.current.set(key, 5)
       expect {
         described_class.aggregate_and_save_stats
       }.to change(VacancyAnalytics, :count).by(1)
 
-      expect(Redis.current.get(key)).to be_nil
+      expect(Redis.current.exists?(key)).to be(false)
 
       stat = VacancyAnalytics.last
-      expect(stat.vacancy_id).to eq(vacancy_id)
+      expect(stat.vacancy_id).to eq(vacancy.id)
       expect(stat.referrer_url).to eq(normalized_referrer)
       expect(stat.date).to eq(Date.current)
       expect(stat.visit_count).to eq(5)
+    end
+
+    it "skips keys with zero counts" do
+      Redis.current.set(redis_key, 0)
+
+      expect {
+        described_class.aggregate_and_save_stats
+      }.not_to change(VacancyAnalytics, :count)
     end
   end
 end
