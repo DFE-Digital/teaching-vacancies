ea9e44bd8214794b190e8820e2e99cb0eceb778b
Kyle MacPherson <kaimac2010@gmail.com>
2025-04-02 16:49:43 +0100

    Remove has_teacher_reference_number from jobseeker profiles

--- app/form_models/jobseekers/profiles/qualified_teacher_status_form.rb
+++ app/form_models/jobseekers/profiles/qualified_teacher_status_form.rb
@@ -6,12 +6,10 @@ module Jobseekers
       validates :qualified_teacher_status, inclusion: { in: %w[yes no on_track non_teacher] }
       validates :qualified_teacher_status_year, numericality: { less_than_or_equal_to: proc { Time.current.year } }, if: -> { qualified_teacher_status == "yes" }
       validates :teacher_reference_number, presence: true, if: -> { qualified_teacher_status == "yes" }
-      validates_format_of :teacher_reference_number, with: /\A\d{7}\z/, allow_blank: false, if: -> { qualified_teacher_status == "yes" || has_teacher_reference_number }
+      validates_format_of :teacher_reference_number, with: /\A\d{7}\z/, allow_blank: false, if: -> { qualified_teacher_status == "yes" }
       validates_format_of :teacher_reference_number, with: /\A\d{7}\z/, allow_blank: true, if: -> { %w[no on_track].include?(qualified_teacher_status) }
       validates :is_statutory_induction_complete, inclusion: { in: [true, false] }, if: -> { qualified_teacher_status == "yes" }
 
-      validates :has_teacher_reference_number, inclusion: { in: [true] }, if: -> { qualified_teacher_status == "yes" }
-      validates :has_teacher_reference_number, inclusion: { in: [true, false] }, if: -> { %w[no on_track].include?(qualified_teacher_status) }
 
       FIELDS = %i[qualified_teacher_status
                   qualified_teacher_status_year
@@ -26,33 +24,19 @@ module Jobseekers
         end
 
         def fields
-          storable_fields + [:has_teacher_reference_number]
+          storable_fields
         end
 
         def load_form_from_model(profile)
-          attributes = profile.slice(storable_fields)
-          has_trn = if attributes[:teacher_reference_number].present?
-                      "true"
-                    else
-                      (attributes[:teacher_reference_number].nil? ? nil : "false")
-                    end
-
-          new(attributes.merge(has_teacher_reference_number: has_trn))
+          new(attributes)
         end
       end
 
       def params_to_save
-        stored = attributes.symbolize_keys.slice(*self.class.storable_fields)
-        if has_teacher_reference_number
-          stored
-        else
-          # blank means we don't have one, nil means we don't know yet
-          stored.merge(teacher_reference_number: "")
-        end
+        attributes.symbolize_keys.slice(*self.class.storable_fields)
       end
 
       attribute :is_statutory_induction_complete, :boolean
-      attribute :has_teacher_reference_number, :boolean
 
       FIELDS.each do |field|
         attribute field
