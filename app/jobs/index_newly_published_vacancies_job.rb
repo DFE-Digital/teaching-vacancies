89999b1020d6fa3b190346cd95c1fee59f8f8f17
Kyle MacPherson <kaimac2010@gmail.com>
2025-04-07 16:57:00 +0100

    Linting

--- app/jobs/index_newly_published_vacancies_job.rb
+++ app/jobs/index_newly_published_vacancies_job.rb
@@ -1,8 +1,8 @@
-class IndexNewlyLiveVacanciesJob < ApplicationJob
+class IndexNewlyPublishedVacanciesJob < ApplicationJob
   queue_as :low
 
   def perform
-    Vacancy.published.where(publish_on: Date.today).find_each do |vacancy|
+    Vacancy.published.where(publish_on: Time.zone.today).find_each do |vacancy|
       UpdateGoogleIndexQueueJob.perform_later(Rails.application.routes.url_helpers.job_url(vacancy))
     end
   end
