268aaa6319a899c59b10e7aa67291560d16b8469
Kyle MacPherson <kaimac2010@gmail.com>
2025-04-07 16:53:17 +0100

    Update newly_published_vacancies_job to index jobs that are published today rather than yesterday

--- app/jobs/index_newly_published_vacancies_job.rb
+++ app/jobs/index_newly_published_vacancies_job.rb
@@ -2,13 +2,8 @@ class IndexNewlyLiveVacanciesJob < ApplicationJob
   queue_as :low
 
   def perform
-    Vacancy.published
-           .where("publish_on = ?", Date.yesterday)
-           .where("expires_at >= ?", Time.current)
-           .find_each do |vacancy|
-      next unless vacancy.listed?
-
-      UpdateGoogleIndexQueueJob.perform_later(job_url(vacancy))
+    Vacancy.published.where(publish_on: Date.today).find_each do |vacancy|
+      UpdateGoogleIndexQueueJob.perform_later(Rails.application.routes.url_helpers.job_url(vacancy))
     end
   end
 end
