78cc6abc9355a47a07c1d41b82a4885aa2c84b19
Kyle MacPherson <kaimac2010@gmail.com>
2025-04-09 10:24:24 +0100

    wip

--- db/migrate/20250325153836_create_vacancy_analytics.rb
+++ db/migrate/20250325153836_create_vacancy_analytics.rb
@@ -1,16 +1,14 @@
 class CreateVacancyAnalytics < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+  
   def change
     create_table :vacancy_analytics, id: :uuid do |t|
-      t.references :vacancy, null: false, foreign_key: true, type: :uuid, index: false
-      t.string :referrer_url, null: false
-      t.date :date, null: false
-      t.integer :visit_count, default: 0, null: false
+      t.references :vacancy, null: false, foreign_key: true, type: :uuid, index: { unique: true }
+      t.jsonb :referrer_counts, null: false, default: {}
 
       t.timestamps
 
-      # Composite index to speed up querying and enforce uniqueness constraint
-      t.index %i[vacancy_id referrer_url date], unique: true, name: "index_vacancy_referrer_stats_on_vacancy_referrer_and_date"
-      t.index :date
+      add_index :vacancy_analytics, :referrer_counts, using: :gin, algorithm: :concurrently
     end
   end
 end
