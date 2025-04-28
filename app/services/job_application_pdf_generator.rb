71994b1b5183aa334c9d9d217fc780eadb6c99a5
Jerome Riga <jriga@zemis.co.uk>
2025-04-28 15:41:58 +0100

    Refactored `JobApplicationPdfGenerator`
    Uses `PdfUiHelper` for rendering data from `JobApplicationPdf`

--- app/services/job_application_pdf_generator.rb
+++ app/services/job_application_pdf_generator.rb
@@ -1,32 +1,76 @@
 class JobApplicationPdfGenerator
-  include PdfHelper
+  include Prawn::View
+  include PdfUiHelper
 
   def initialize(job_application, vacancy)
-    @job_application = job_application
-    @vacancy = vacancy
+    @datasource = JobApplicationPdf.new(job_application, vacancy)
+    @document = Prawn::Document.new(page_size: "A4", margin: 1.cm)
   end
 
   def generate
-    Prawn::Document.new do |pdf|
-      update_font_family(pdf)
-      add_image_to_first_page(pdf)
-      add_headers(pdf)
-      pdf.stroke_horizontal_rule
-      add_personal_details(pdf)
-      add_professional_status(pdf)
-      add_qualifications(pdf)
-      add_training_and_cpds(pdf)
-      add_professional_body_memberships(pdf)
-      add_employment_history(pdf)
-      add_personal_statement(pdf)
-      add_references(pdf)
-      add_ask_for_support(pdf)
-      add_declarations(pdf)
-      add_footers(pdf)
-    end
+    page_style
+    job_application_page_header
+    page_footer(datasource.footer_text)
+
+    render_table_section(:personal_details)
+    render_table_section(:professional_status)
+    render_nested_section(:qualifications)
+    render_nested_section(:training_and_cpds)
+    render_nested_section(:professional_body_memberships)
+    render_nested_section(:employment_history)
+    render_page(:personal_statement)
+    render_nested_section(:references)
+    render_table_section(:ask_for_support)
+    render_table_section(:declarations)
+
+    number_pages "<page> of <total>",
+                 at: [bounds.right - 50, bounds.bottom - 10],
+                 size: 8
+
+    document
   end
 
   private
 
-  attr_reader :job_application, :vacancy
+  attr_reader :datasource, :document
+
+  def job_application_page_header
+    page_header do
+      text datasource.header_text, size: 12, style: :italic
+      move_down 0.5.cm
+      text datasource.applicant_name, size: 22, style: :bold
+    end
+  end
+
+  def render_table_section(section_name)
+    page_section do
+      page_title(section_name.to_s.titleize)
+      page_table(datasource.public_send(section_name).to_a)
+    end
+  end
+
+  def render_nested_section(section_name)
+    page_section do
+      page_title(section_name.to_s.titleize)
+      datasource.public_send(section_name).each do |sub_title, group|
+        page_sub_title(sub_title) if sub_title.present?
+        next if group.blank?
+
+        group.each do
+          page_table(it.to_a)
+          move_down 1.cm
+        end
+      end
+    end
+  end
+
+  def render_page(page_name)
+    start_new_page
+    page_section do
+      page_title(page_name.to_s.titleize)
+      move_down 0.3.cm
+      text datasource.public_send(page_name), size: 12, leading: 4
+    end
+    start_new_page
+  end
 end
---
+++ spec/services/job_application_pdf_generator_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+require "pdf/inspector"
+
+RSpec.describe JobApplicationPdfGenerator do
+  let(:vacancy) { build(:vacancy, :published, :at_one_school) }
+  let(:job_application) { build(:job_application, :status_submitted, vacancy: vacancy) }
+  let(:generator) { described_class.new(job_application, vacancy) }
+
+  describe "#generate" do
+    subject(:document) { generator.generate }
+
+    let(:pdf) { PDF::Inspector::Text.analyze(document.render).strings }
+
+    it { is_expected.to be_a(Prawn::Document) }
+
+    it "includes page header" do
+      expect(pdf).to include(I18n.t("jobseekers.job_applications.caption", job_title: vacancy.job_title, organisation: vacancy.organisation_name))
+    end
+
+    it "includes section titles" do
+      expect(pdf).to include("Personal Details")
+      expect(pdf).to include("Professional Status")
+      expect(pdf).to include("Qualifications")
+      expect(pdf).to include("Training And Cpds")
+      expect(pdf).to include("Professional Body Memberships")
+      expect(pdf).to include("Employment History")
+      expect(pdf).to include("Personal Statement")
+      expect(pdf).to include("References")
+      expect(pdf).to include("Ask For Support")
+      expect(pdf).to include("Declarations")
+    end
+
+    it "includes page footer" do
+      expect(pdf).to include("#{job_application.name} | #{vacancy.organisation_name}")
+    end
+
+    it "includes page number" do
+      expect(pdf).to include("1 of 4")
+    end
+  end
+end
