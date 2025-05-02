6aaedaaad923141f81559c22caf69c6fa1c2fc62
starswan <github@starswan.com>
2025-05-02 15:40:51 +0100

    fix link on active jobs to be 'live' rather than 'published'

--- spec/system/publishers/publishers_can_manage_job_applications_for_a_vacancy_spec.rb
+++ spec/system/publishers/publishers_can_manage_job_applications_for_a_vacancy_spec.rb
@@ -183,7 +183,7 @@ RSpec.describe "Publishers can manage job applications for a vacancy" do
     describe "the summary section" do
       it "shows breadcrumb with link to active jobs in dashboard" do
         within(".govuk-breadcrumbs") do
-          expect(page).to have_link(I18n.t("jobs.dashboard.published.tab_heading"), href: organisation_jobs_with_type_path(:published))
+          expect(page).to have_link(I18n.t("jobs.dashboard.published.tab_heading"), href: organisation_jobs_with_type_path(:live))
         end
       end
 
