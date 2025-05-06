9f1e990d0ae964f00cf64c76121fec2324942235
Kyle MacPherson <kaimac2010@gmail.com>
2025-05-06 16:27:57 +0100

    Remove unused code

--- spec/page_objects/pages/publisher/job_location_page.rb
+++ spec/page_objects/pages/publisher/job_location_page.rb
@@ -4,17 +4,11 @@ module PageObjects
       class JobLocationPage < CommonPage
         set_url "/organisation/jobs/{vacancy_id}/build/job_location"
 
-        element :continue_button, "input[value='#{I18n.t('buttons.continue')}']"
-
         def fill_in_and_submit_form(vacancy)
           vacancy.organisations.each do |organisation|
             check(organisation.school? ? organisation.name : I18n.t("organisations.job_location_heading.central_office"))
           end
           click_on I18n.t("buttons.continue")
-        end
-
-        def continue
-          continue_button.click
         end
       end
     end
