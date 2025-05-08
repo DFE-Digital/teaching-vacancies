9d19b3049b7942c9b862a5d73b1b197d8023881e
Kyle MacPherson <kaimac2010@gmail.com>
2025-05-08 14:22:21 +0100

    Make system tests into view specs

---
+++ spec/views/publishers/vacancies/review.html.slim_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe "publishers/vacancies/review" do
+  let(:school) { build(:school) }
+  let(:vacancy) { create(:vacancy, publish_on: publish_date) }
+  let(:presenter) { VacancyPresenter.new(vacancy) }
+  let(:step_process) { Publishers::Vacancies::VacancyStepProcess.new(:review, vacancy: vacancy, organisation: school) }
+
+  before do
+    p = presenter
+    s = school
+    sp = step_process
+
+    view.define_singleton_method(:vacancy) { p }
+    view.define_singleton_method(:current_organisation) { s }
+    view.define_singleton_method(:step_process) { sp }
+
+    render
+  end
+
+  context "when the vacancy is published today" do
+    let(:publish_date) { Date.current }
+
+    it "shows the immediate publish button" do
+      expect(rendered).to include(I18n.t("publishers.vacancies.show.heading_component.action.publish"))
+    end
+  end
+
+  context "when the vacancy is scheduled for future" do
+    let(:publish_date) { Date.current + 2.days }
+
+    it "shows the scheduled publish button" do
+      expect(rendered).to include(I18n.t("publishers.vacancies.show.heading_component.action.scheduled_complete_draft"))
+    end
+  end
+end
