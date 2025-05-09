7b2a2d53853fa89659ad1c1d7035fc5096522c24
Kyle MacPherson <kaimac2010@gmail.com>
2025-05-09 12:18:13 +0100

    remove system test

--- spec/views/publishers/vacancies/show.html.slim_spec.rb
+++ spec/views/publishers/vacancies/show.html.slim_spec.rb
@@ -19,10 +19,9 @@ RSpec.describe "publishers/vacancies/show" do
   let(:about_the_role) { rendered.html.css("#about_the_role") }
   let(:important_dates) { rendered.html.css("#important_dates") }
   let(:application_process) { rendered.html.css("#application_process") }
-  let(:publish_on) { nil }
 
   context "with a minimal vacancy" do
-    let(:vacancy) { create(:vacancy, :without_contract_details, publish_on: publish_on) }
+    let(:vacancy) { create(:vacancy, :without_contract_details) }
     let(:next_invalid_step) { :job_role }
 
     it "show first section as in-progress, and the rest as not startable" do
@@ -42,14 +41,6 @@ RSpec.describe "publishers/vacancies/show" do
       expect(about_the_role).to have_content "Not started"
       expect(important_dates).to have_content "Cannot start yet"
       expect(application_process).to have_content "Cannot start yet"
-    end
-  end
-
-  context "when the vacancy is published today" do
-    let(:publish_on) { Date.current }
-
-    it "shows the immediate publish button" do
-      expect(rendered).to include(I18n.t("publishers.vacancies.show.heading_component.action.publish"))
     end
   end
 end
