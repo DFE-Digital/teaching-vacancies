a5eaf8ec8b7920ad2ea69704e694954ebe9b708d
starswan <github@starswan.com>
2025-05-09 08:46:57 +0100

    Pre-interview checks base branch

---
+++ app/form_models/publishers/job_application/collect_references_form.rb
@@ -0,0 +1,10 @@
+class Publishers::JobApplication::CollectReferencesForm
+  include ActiveModel::Model
+  include ActiveModel::Validations
+  include ActiveModel::Attributes
+
+  attr_accessor :job_applications
+
+  attribute :collect_references_and_declarations, :boolean
+  validates :collect_references_and_declarations, inclusion: { in: [true, false], allow_nil: false }
+end
