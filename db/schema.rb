a05b71a509eb992319bf72ee814a7f0e10b1ac24
Kyle MacPherson <kaimac2010@gmail.com>
2025-05-13 10:44:07 +0100

    Add migration to add type to job applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_30_141711) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_09_154018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "citext"
@@ -259,6 +259,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_30_141711) do
     t.boolean "has_close_relationships"
     t.boolean "has_right_to_work_in_uk"
     t.boolean "has_safeguarding_issue"
+    t.string "type"
     t.index ["jobseeker_id"], name: "index_job_applications_jobseeker_id"
     t.index ["vacancy_id"], name: "index_job_applications_on_vacancy_id"
   end
