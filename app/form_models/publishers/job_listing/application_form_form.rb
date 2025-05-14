2830eb956833beef5066816ce17c8f65d9898861
Kyle MacPherson <kaimac2010@gmail.com>
2025-05-14 11:12:41 +0100

    Allow publishers to create vacancies with uploaded_form type

--- app/form_models/publishers/job_listing/application_form_form.rb
+++ app/form_models/publishers/job_listing/application_form_form.rb
@@ -2,54 +2,20 @@ class Publishers::JobListing::ApplicationFormForm < Publishers::JobListing::Vaca
   validate :application_form_presence
   validates :application_form, form_file: Vacancy::DOCUMENT_VALIDATION_OPTIONS
 
-  validates :application_email, presence: true
-  validate :other_application_email_presence
-  validate :other_application_email_valid
-
   def self.fields
-    %i[application_email]
-  end
-  attr_accessor(:application_form, :application_form_staged_for_replacement, *fields)
-  attr_writer(:other_application_email)
-
-  def application_email
-    return unless @vacancy.application_email || params[:application_email]
-
-    if params[:application_email].present?
-      return params[:application_email] if params[:application_email] == @current_publisher&.email
-
-      return "other"
-    end
-
-    return @current_publisher&.email if @vacancy.application_email == @current_publisher&.email
-
-    "other"
+    []
   end
 
-  def other_application_email
-    return params[:other_application_email] if params[:other_application_email]
 
-    @vacancy.application_email unless @vacancy.application_email == @current_publisher&.email
-  end
+  attr_accessor(:application_form, :application_form_staged_for_replacement)
 
   def params_to_save
     {
-      application_email: params[:application_email] == "other" ? params[:other_application_email] : params[:application_email],
       completed_steps: params[:completed_steps],
     }
   end
 
   private
-
-  def other_application_email_presence
-    errors.add(:other_application_email, :blank) if params[:application_email] == "other" && params[:other_application_email].blank?
-  end
-
-  def other_application_email_valid
-    return unless params[:other_application_email].present? && params[:application_email] == "other"
-
-    errors.add(:other_application_email, :invalid) unless params[:other_application_email].match? URI::MailTo::EMAIL_REGEXP
-  end
 
   def application_form_presence
     return if application_form.present?
