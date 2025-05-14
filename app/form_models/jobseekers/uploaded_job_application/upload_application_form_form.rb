d988a1dc3c6a0e4cadcc4a15c5ef967798007431
Kyle MacPherson <kaimac2010@gmail.com>
2025-05-14 16:51:32 +0100

    Allowing jobseeker to submit new type of application

---
+++ app/form_models/jobseekers/uploaded_job_application/upload_application_form_form.rb
@@ -0,0 +1,34 @@
+module Jobseekers
+  module UploadedJobApplication
+    class UploadApplicationFormForm < Jobseekers::JobApplication::BaseForm
+      include ActiveModel::Model
+      include ActiveModel::Attributes
+      include Jobseekers::JobApplication::CompletedFormAttribute
+
+      validates :application_form, form_file: DOCUMENT_VALIDATION_OPTIONS
+      validate :application_form_presence
+
+      completed_attribute(:upload_application_form)
+
+      attr_accessor :application_form
+
+      class << self
+        def fields
+          [:application_form]
+        end
+
+        def load_form(model)
+          (completed_attrs(model, :upload_application_form)).merge(application_form: model.application_form)
+        end
+      end
+
+      private
+
+      def application_form_presence
+        return if application_form.present?
+
+        errors.add(:application_form, :blank)
+      end
+    end
+  end
+end
--- app/models/jobseeker.rb
+++ app/models/jobseeker.rb
@@ -8,6 +8,7 @@ class Jobseeker < ApplicationRecord
 
   has_many :feedbacks, dependent: :destroy, inverse_of: :jobseeker
   has_many :job_applications, dependent: :destroy
+  has_many :uploaded_job_applications
   has_many :saved_jobs, dependent: :destroy
   has_many :emergency_login_keys, as: :owner
   has_one :jobseeker_profile
