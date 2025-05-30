d89fe656ac9b7d6b7b0ca67429d3fa818027bb7c
Stephen Dicks <github@starswan.com>
2025-05-30 11:43:33 +0100

    put back small integration test

---
+++ spec/system/publishers/publishers_can_preview_a_profile_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "Publishers can preview an organisation or school profile" do
+  let(:publisher) { create(:publisher, organisations: [organisation]) }
+
+  before do
+    Organisation.subclasses.each do |klass|
+      allow_any_instance_of(klass).to receive(:geopoint?).and_return(true)
+    end
+
+    login_publisher(publisher: publisher, organisation: organisation)
+    visit publisher_root_path
+  end
+
+  after { logout }
+
+  context "when the publisher is signed in as a school" do
+    let(:organisation) { create(:school) }
+
+    before do
+      click_link I18n.t("nav.school_profile")
+      click_link I18n.t("publishers.organisations.show.preview_link_text", organisation_type: "school")
+    end
+
+    it "displays a profile summary" do
+      has_profile_summary?(page, organisation)
+    end
+  end
+end
