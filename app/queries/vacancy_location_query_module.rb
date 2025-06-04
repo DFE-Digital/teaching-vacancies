b1f75a745a97f092117906e900f8b6c7c7696ba9
Stephen Dicks <github@starswan.com>
2025-06-04 16:48:42 +0100

    lint

--- app/queries/vacancy_location_query_module.rb
+++ app/queries/vacancy_location_query_module.rb
@@ -2,6 +2,6 @@
 
 module VacancyLocationQueryModule
   def vacancy_location_query(location_query, radius_in_miles, polygon:, sort_by_distance:)
-    VacancyLocationQuery.new(all).call(location_query, radius_in_miles, polygon: polygon, sort_by_distance:sort_by_distance)
+    VacancyLocationQuery.new(all).call(location_query, radius_in_miles, polygon: polygon, sort_by_distance: sort_by_distance)
   end
 end
