b2b31cf70caf148f243279a22dd5a53ddbc48675
Jerome Riga <jriga@zemis.co.uk>
2025-06-18 09:16:52 +0100

    Add CTA banner for self disclosure request (#7837)
    # Add self-disclosure request banner
    
    ## Trello card URL
    [1916](https://trello.com/c/aBz8Ylya)
    
    ## Changes in this PR:
    
    - Add banner on jobseekers/job_applications/show for self disclosure
    request
    
    - Add tag on jobseekers/job_applications/index for self disclosure
    request
    
    ## Screenshots of UI changes:
    
    ### Before
    
    ### After
    
    ![image](https://github.com/user-attachments/assets/201b347a-c5ef-4c28-a797-81bb67a18163)
    
    ![image](https://github.com/user-attachments/assets/7054e386-95bc-4b15-9132-fe6b16ee5547)

--- spec/views/jobseekers/job_applications/index.html.slim_spec.rb
+++ spec/views/jobseekers/job_applications/index.html.slim_spec.rb
@@ -2,84 +2,33 @@ require "rails_helper"
 
 RSpec.describe "jobseekers/job_applications/index" do
   let(:jobseeker) { create(:jobseeker) }
-  let(:index_view) { Capybara.string(rendered) }
-  let(:selectors) do
-    {
-      header: "h1",
-      job_applications: "#applications-results .card-component",
-      job_application_header: ".card-component__header a",
-      job_application_tag: ".card-component__action .govuk-tag",
-    }
-  end
+  let(:other_job_application) { create(:job_application, jobseeker:) }
+  let(:self_disclosure_request) { nil }
 
   before do
-    job_applications
-    without_partial_double_verification do
-      allow(view).to receive(:current_jobseeker).and_return(jobseeker.reload)
-    end
-
+    jobseeker.job_applications << self_disclosure_request.job_application if self_disclosure_request
+    allow(view).to receive(:current_jobseeker).and_return(jobseeker)
     render
   end
 
-  describe "job applications rendering" do
-    context "when there is no job application" do
-      let(:job_applications) { nil }
+  describe "self disclosure request tag" do
+    let(:tag_text) { "action required" }
 
-      it "renders empty content" do
-        expect(index_view).to have_css(selectors[:header], text: "Applications (0)")
-        expect(index_view.all(selectors[:job_applications])).to be_empty
-      end
+    context "when self_disclosure_request sent" do
+      let(:job_application) { create(:job_application, :status_interviewing) }
+      let(:self_disclosure_request) { create(:self_disclosure_request, :sent, job_application:) }
+
+      it { expect(rendered).to have_content(tag_text) }
     end
 
-    context "when there are some job applications" do
-      let(:draft) { create(:job_application, :status_draft, jobseeker:) }
-      let(:submitted) { create(:job_application, :status_submitted, jobseeker:) }
-      let(:reviewed) { create(:job_application, :status_reviewed, jobseeker:) }
-      let(:shortlisted) { create(:job_application, :status_shortlisted, jobseeker:) }
-      let(:unsuccessful) { create(:job_application, :status_unsuccessful, jobseeker:) }
-      let(:withdrawn) { create(:job_application, :status_withdrawn, jobseeker:) }
-      let(:interviewing) { create(:job_application, :status_interviewing, jobseeker:) }
-      let(:expired) { create(:job_application, :status_draft, jobseeker:, vacancy: vacancy_expired) }
-      let(:vacancy_expired) { create(:vacancy, :at_one_school, expires_at: 1.week.ago) }
+    context "when self_disclosure_request manual" do
+      let(:self_disclosure_request) { create(:self_disclosure_request, :manual) }
 
-      let(:job_applications) do
-        [draft, submitted, reviewed, shortlisted, unsuccessful, withdrawn, interviewing, expired]
-      end
+      it { expect(rendered).to have_no_content(tag_text) }
+    end
 
-      it "renders job application list" do
-        expect(index_view).to have_css(selectors[:header], text: "Applications (8)")
-        expect(index_view.all(selectors[:job_applications]).count).to eq(7)
-      end
-
-      describe "applications groups" do
-        let(:groups) { index_view.all(selectors[:job_applications]) }
-
-        it "renders draft applications group first" do
-          expect(groups[0]).to have_css(selectors[:job_application_header], text: draft.vacancy.job_title)
-          # expect(groups[0]).to have_link(selectors[:job_application_header], href: jobseekers_job_application_apply_path(draft))
-          expect(groups[0].find(selectors[:job_application_header])["href"]).to eq(jobseekers_job_application_apply_path(draft))
-          expect(groups[0]).to have_css(selectors[:job_application_tag], text: draft.status)
-        end
-
-        %i[submitted reviewed shortlisted unsuccessful withdrawn].each.with_index do |app, idx|
-          it "renders #{app} applications group" do
-            application = public_send(app)
-            expect(groups[idx + 1]).to have_css(selectors[:job_application_header], text: application.vacancy.job_title)
-            expect(groups[idx + 1].find(selectors[:job_application_header])["href"]).to eq(jobseekers_job_application_path(application))
-            if app == :reviewed
-              expect(groups[idx + 1]).to have_css(selectors[:job_application_tag], text: "submitted")
-            else
-              expect(groups[idx + 1]).to have_css(selectors[:job_application_tag], text: application.status)
-            end
-          end
-        end
-
-        it "renders expired applications group last" do
-          expect(groups.last).to have_css(selectors[:job_application_header], text: expired.vacancy.job_title)
-          expect(groups.last.find(selectors[:job_application_header])["href"]).to eq(jobseekers_job_application_apply_path(expired))
-          expect(groups.last).to have_css(selectors[:job_application_tag], text: "deadline passed")
-        end
-      end
+    context "when self_disclosure_request missing" do
+      it { expect(rendered).to have_no_content(tag_text) }
     end
   end
 end
--- spec/views/jobseekers/job_applications/show.html.slim_spec.rb
+++ spec/views/jobseekers/job_applications/show.html.slim_spec.rb
@@ -1,153 +1,52 @@
 require "rails_helper"
 
 RSpec.describe "jobseekers/job_applications/show" do
-  let(:show_view) { Capybara.string(rendered) }
-  let(:jobseeker) { build_stubbed(:jobseeker) }
-  let(:vacancy) { build_stubbed(:vacancy) }
-  let(:job_application) { build_stubbed(:job_application, :status_shortlisted, jobseeker:, vacancy:) }
-  let(:current_jobseeker) { jobseeker }
+  let(:jobseeker) { create(:jobseeker) }
+  let(:other_job_application) { create(:job_application, jobseeker:) }
+  let(:self_disclosure_request) { nil }
+  let(:job_application) { create(:job_application) }
 
   before do
+    job_application.self_disclosure_request = self_disclosure_request
     without_partial_double_verification do
-      allow(view).to receive_messages(current_jobseeker:, job_application:, vacancy:)
+      allow(view).to receive_messages(job_application:, vacancy: job_application.vacancy)
     end
-
     render
   end
 
-  describe "banner" do
-    subject(:banner) { show_view.find(".review-banner") }
-
-    let(:selectors) do
-      {
-        header: "h1",
-        tag: ".status-tag",
-        delete_btn: ".delete-application",
-        withdraw_btn: ".withdraw-application",
-        download_btn: ".print-application",
-        view_link: ".view-listing-link",
-      }
+  describe "self disclosure request banner" do
+    let(:scope) { "jobseekers.job_applications.show.self_disclosure" }
+    let(:call_to_action) do
+      I18n.t(
+        ".cta_html",
+        scope:,
+        organisation: job_application.vacancy.organisation.name,
+        link: "self-disclosure declaration",
+      )
+    end
+    let(:form_path) do
+      jobseekers_job_application_self_disclosure_path(job_application, Wicked::FIRST_STEP)
     end
 
-    it "renders section" do
-      expect(banner).to have_css(selectors[:header], text: "#{vacancy.job_title} at #{vacancy.organisation.name}")
-      expect(banner).to have_css(selectors[:tag], text: "shortlisted")
+    context "when self_disclosure_request sent" do
+      let(:job_application) { create(:job_application, :status_interviewing) }
+      let(:self_disclosure_request) { create(:self_disclosure_request, :sent, job_application:) }
 
-      expect(banner).to have_css(selectors[:view_link], text: "View this listing (opens in new tab)")
-      expect(banner).to have_link("View this listing (opens in new tab)", href: job_path(vacancy))
-
-      expect(banner).to have_css(selectors[:download_btn])
-      expect(banner).to have_css(selectors[:withdraw_btn])
-      expect(banner).to have_link("Withdraw", href: jobseekers_job_application_confirm_withdraw_path(job_application))
-
-      expect(banner).to have_no_css(selectors[:delete_btn])
-    end
-  end
-
-  describe "quick links" do
-    subject(:quick_links) { show_view.all(".navigation-list-component .navigation-list-component__anchor a") }
-
-    context "with religious vacancy" do
-      let(:jobseeker) { create(:jobseeker) }
-      let(:vacancy) { create(:vacancy, :catholic) }
-      let(:job_application) do
-        create(:job_application, :status_shortlisted, :with_baptism_certificate, jobseeker:, vacancy:)
-      end
-      let(:expected_sections) do
-        [
-          "Personal details",
-          "Professional status",
-          "Qualifications",
-          "Training and continuing professional development (CPD)",
-          "Professional body memberships",
-          "Work history",
-          "Personal statement",
-          "Religious information",
-          "References",
-          "Ask for support if you have a disability or other needs",
-          "Declarations",
-        ]
-      end
-
-      it "renders nav links" do
-        expect(quick_links.map(&:text)).to match_array(expected_sections)
-      end
-
-      describe "sections" do
-        subject(:sections) { show_view.all(".govuk-summary-card__title") }
-
-        let(:expected_sections) do
-          [
-            "Personal details",
-            "Professional status",
-            "Qualifications",
-            "Training and continuing professional development (CPD)",
-            "Professional body memberships (optional)",
-            "Work history",
-            "Personal statement",
-            "Religious information",
-            "References",
-            "Ask for support if you have a disability or other needs",
-            "Declarations",
-          ]
-        end
-
-        it "renders each section" do
-          expect(sections.map(&:text)).to match_array(expected_sections)
-        end
-      end
+      it { expect(rendered).to have_content(call_to_action) }
+      it { expect(rendered).to have_link(I18n.t(".form", scope:), href: form_path) }
     end
 
-    context "with other vacancy" do
-      let(:expected_sections) do
-        [
-          "Personal details",
-          "Professional status",
-          "Qualifications",
-          "Training and continuing professional development (CPD)",
-          "Professional body memberships",
-          "Work history",
-          "Personal statement",
-          "References",
-          "Ask for support if you have a disability or other needs",
-          "Declarations",
-        ]
-      end
+    context "when self_disclosure_request manual" do
+      let(:job_application) { create(:job_application, :status_interviewing) }
+      let(:self_disclosure_request) { create(:self_disclosure_request, :manual) }
 
-      it "renders nav links" do
-        expect(quick_links.map(&:text)).to match_array(expected_sections)
-      end
-
-      describe "sections" do
-        subject(:sections) { show_view.all(".govuk-summary-card__title") }
-
-        let(:expected_sections) do
-          [
-            "Personal details",
-            "Professional status",
-            "Qualifications",
-            "Training and continuing professional development (CPD)",
-            "Professional body memberships (optional)",
-            "Work history",
-            "Personal statement",
-            "References",
-            "Ask for support if you have a disability or other needs",
-            "Declarations",
-          ]
-        end
-
-        it "renders each section" do
-          expect(sections.map(&:text)).to match_array(expected_sections)
-        end
-      end
+      it { expect(rendered).to have_no_content(call_to_action) }
+      it { expect(rendered).to have_no_link(I18n.t(".form", scope:), href: form_path) }
     end
-  end
 
-  describe "timeline" do
-    subject(:timeline_items) { show_view.all(".timeline-component .timeline-component__item") }
-
-    it "renders" do
-      expect(timeline_items.map(&:text)).to contain_exactly("Application submitted")
+    context "when job application not status interviewing" do
+      it { expect(rendered).to have_no_content(call_to_action) }
+      it { expect(rendered).to have_no_link(I18n.t(".form", scope:), href: form_path) }
     end
   end
 end
