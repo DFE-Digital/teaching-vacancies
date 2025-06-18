a91ad9407e93100e5a1b3b0b8fa49b1fc5c5d7fb
Jerome Riga <jriga@zemis.co.uk>
2025-06-18 09:16:52 +0100

    Add CTA banner for self disclosure request (#7837)
    # Add self-disclosure request banner
    
    ## Trello card URL
    [1916](https://trello.com/c/aBz8Ylya)
    
    ## Changes in this PR:
    
    - Add banner on jobseekers/job_applications/show for self disclosure
    request
    
    - Add tag on jobseekers/job_applications/index for self disclosure
    request
    
    ## Screenshots of UI changes:
    
    ### Before
    
    ### After
    
    ![image](https://github.com/user-attachments/assets/201b347a-c5ef-4c28-a797-81bb67a18163)
    
    
    
    ![image](https://github.com/user-attachments/assets/7054e386-95bc-4b15-9132-fe6b16ee5547)

---
+++ spec/views/jobseekers/job_applications/index.html.slim_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe "jobseekers/job_applications/index" do
+  let(:jobseeker) { create(:jobseeker) }
+  let(:other_job_application) { create(:job_application, jobseeker:) }
+  let(:self_disclosure_request) { nil }
+
+  before do
+    jobseeker.job_applications << self_disclosure_request.job_application if self_disclosure_request
+    allow(view).to receive(:current_jobseeker).and_return(jobseeker)
+    render
+  end
+
+  describe "self disclosure request tag" do
+    let(:tag_text) { "action required" }
+
+    context "when self_disclosure_request sent" do
+      let(:job_application) { create(:job_application, :status_interviewing) }
+      let(:self_disclosure_request) { create(:self_disclosure_request, :sent, job_application:) }
+
+      it { expect(rendered).to have_content(tag_text) }
+    end
+
+    context "when self_disclosure_request manual" do
+      let(:self_disclosure_request) { create(:self_disclosure_request, :manual) }
+
+      it { expect(rendered).to have_no_content(tag_text) }
+    end
+
+    context "when self_disclosure_request missing" do
+      it { expect(rendered).to have_no_content(tag_text) }
+    end
+  end
+end
