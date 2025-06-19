cc452a768008de0bad3cbdd475335a7fe66a6b2d
Kyle MacPherson <kaimac2010@gmail.com>
2025-06-19 19:17:29 +0100

    Add assertions to existing flow to avoid duplication

--- spec/system/jobseekers/jobseekers_can_complete_a_job_application_spec.rb
+++ spec/system/jobseekers/jobseekers_can_complete_a_job_application_spec.rb
@@ -18,11 +18,13 @@ RSpec.describe "Jobseekers can complete a job application" do
     validates_step_complete
     fill_in_personal_details
     click_on I18n.t("buttons.save_and_continue")
+    expect(page).to have_css("#personal_details", text: I18n.t("shared.status_tags.complete"))
 
     click_on(I18n.t("jobseekers.job_applications.build.professional_status.heading"))
     validates_step_complete
     fill_in_professional_status
     click_on I18n.t("buttons.save_and_continue")
+    expect(page).to have_css("#professional_status", text: I18n.t("shared.status_tags.complete"))
 
     click_on(I18n.t("jobseekers.job_applications.build.qualifications.heading"))
     validates_step_complete
@@ -40,6 +42,7 @@ RSpec.describe "Jobseekers can complete a job application" do
     click_on I18n.t("buttons.save_qualification.one")
     choose "Yes, I've completed this section"
     click_on I18n.t("buttons.save_and_continue")
+    expect(page).to have_css("#qualifications", text: I18n.t("shared.status_tags.complete"))
 
     click_on(I18n.t("jobseekers.job_applications.build.training_and_cpds.heading"))
     expect(page).to have_content("No training or CPD specified")
@@ -51,6 +54,7 @@ RSpec.describe "Jobseekers can complete a job application" do
     click_on "Save and continue"
     choose "Yes, I've completed this section"
     click_on "Save and continue"
+    expect(page).to have_css("#training_and_cpds", text: I18n.t("shared.status_tags.complete"))
 
     click_on(I18n.t("jobseekers.job_applications.build.professional_body_memberships.list_heading"))
     expect(page).to have_content("No memberships")
@@ -62,6 +66,7 @@ RSpec.describe "Jobseekers can complete a job application" do
     click_on "Save and continue"
     choose "Yes, I've completed this section"
     click_on "Save and continue"
+    expect(page).to have_css("#professional_body_memberships", text: I18n.t("shared.status_tags.complete"))
 
     click_on(I18n.t("jobseekers.job_applications.build.employment_history.heading"))
     validates_step_complete
@@ -75,11 +80,13 @@ RSpec.describe "Jobseekers can complete a job application" do
     click_on I18n.t("buttons.continue")
     choose I18n.t("helpers.label.jobseekers_job_application_employment_history_form.employment_history_section_completed_options.true")
     click_on I18n.t("buttons.save_and_continue")
+    expect(page).to have_css("#employment_history", text: I18n.t("shared.status_tags.complete"))
 
     click_on(I18n.t("jobseekers.job_applications.build.personal_statement.heading"))
     validates_step_complete
     fill_in_personal_statement
     click_on I18n.t("buttons.save_and_continue")
+    expect(page).to have_css("#personal_statement", text: I18n.t("shared.status_tags.complete"))
 
     click_on(I18n.t("jobseekers.job_applications.build.referees.heading"))
 
@@ -93,21 +100,25 @@ RSpec.describe "Jobseekers can complete a job application" do
     click_on I18n.t("buttons.save_reference")
     choose I18n.t("helpers.label.jobseekers_job_application_referees_form.referees_section_completed_options.true")
     click_on I18n.t("buttons.save_and_continue")
+    expect(page).to have_css("#referees", text: I18n.t("shared.status_tags.complete"))
 
     click_on(I18n.t("jobseekers.job_applications.build.equal_opportunities.heading"))
     validates_step_complete
     fill_in_equal_opportunities
     click_on I18n.t("buttons.save_and_continue")
+    expect(page).to have_css("#equal_opportunities", text: I18n.t("shared.status_tags.complete"))
 
     click_on(I18n.t("jobseekers.job_applications.build.ask_for_support.heading"))
     validates_step_complete
     fill_in_ask_for_support
     click_on I18n.t("buttons.save_and_continue")
+    expect(page).to have_css("#ask_for_support", text: I18n.t("shared.status_tags.complete"))
 
     click_on(I18n.t("jobseekers.job_applications.build.declarations.heading"))
     validates_step_complete
     fill_in_declarations
     click_on I18n.t("buttons.save_and_continue")
+    expect(page).to have_css("#declarations", text: I18n.t("shared.status_tags.complete"))
     click_on "Review application"
 
     expect(current_path).to eq(jobseekers_job_application_review_path(job_application))
