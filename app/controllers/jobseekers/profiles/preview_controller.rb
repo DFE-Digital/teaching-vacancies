4ab47abcf48bb9361c9ca9b33e5bcaa714bed6d0
Stephen Dicks <github@starswan.com>
2025-06-19 16:35:01 +0100

    Deactivate inactive profiles over 6 months

--- app/controllers/jobseekers/profiles/preview_controller.rb
+++ app/controllers/jobseekers/profiles/preview_controller.rb
@@ -1,8 +1,6 @@
 class Jobseekers::Profiles::PreviewController < Jobseekers::ProfilesController
-  include VacanciesHelper
-
   def show
-    @personal_details = profile.personal_details
-    @job_preferences = profile.job_preferences
+    @personal_details = @profile.personal_details
+    @job_preferences = @profile.job_preferences
   end
 end
