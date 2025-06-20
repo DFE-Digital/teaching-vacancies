00d88fa2840166b8e449342f90e0d1857cbef4fe
Stephen Dicks <github@starswan.com>
2025-06-20 10:55:47 +0100

    remove stray helper

--- app/controllers/jobseekers/profiles/preview_controller.rb
+++ app/controllers/jobseekers/profiles/preview_controller.rb
@@ -1,6 +1,4 @@
 class Jobseekers::Profiles::PreviewController < Jobseekers::ProfilesController
-  include VacanciesHelper
-
   def show
     @personal_details = @profile.personal_details
     @job_preferences = @profile.job_preferences
