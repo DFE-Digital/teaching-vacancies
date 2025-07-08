3fc54fbbc5afc8b862aed9316ed58c3718d31f59
Stephen Dicks <github@starswan.com>
2025-07-08 11:45:18 +0100

    tweak layout

--- app/mailers/publishers/collect_references_mailer.rb
+++ app/mailers/publishers/collect_references_mailer.rb
@@ -5,9 +5,10 @@ module Publishers
     def collect_references(reference_request)
       @reference_request = reference_request
       @job_application = reference_request.referee.job_application
-      send_email(to: reference_request.referee.email, subject: t(".subject", name: @job_application.name,
-                                                                             job_title: @job_application.vacancy.job_title,
-                                                                             organisation_name: @job_application.vacancy.organisation_name))
+      send_email(to: reference_request.referee.email,
+                 subject: t(".subject", name: @job_application.name,
+                                        job_title: @job_application.vacancy.job_title,
+                                        organisation_name: @job_application.vacancy.organisation_name))
     end
 
     def inform_applicant_about_references(job_application)
