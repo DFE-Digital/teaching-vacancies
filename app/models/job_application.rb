9e6e9c84bcd1f973729304512e3a1b0125b68080
Stephen Dicks <github@starswan.com>
2025-07-15 15:58:55 +0100

    simplify code

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -88,6 +88,8 @@ class JobApplication < ApplicationRecord
   scope :after_submission, -> { where.not(status: :draft) }
   scope :draft, -> { where(status: "draft") }
 
+  scope :active_for_selection, -> { where.not(status: %w[draft withdrawn]) }
+
   validates :email_address, email_address: true, if: -> { email_address_changed? } # Allows data created prior to validation to still be valid
 
   has_one_attached :baptism_certificate, service: :amazon_s3_documents
