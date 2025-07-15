5f879a7627c90822d92ca64775471c5aa955dd54
Stephen Dicks <github@starswan.com>
2025-07-15 10:23:14 +0100

    declined reference flow

--- app/form_models/referees/can_give_reference_form.rb
+++ app/form_models/referees/can_give_reference_form.rb
@@ -10,6 +10,14 @@ module Referees
       validates field, inclusion: { in: [true, false], allow_nil: false }
     end
 
+    def params_to_save
+      if can_give_reference
+        super
+      else
+        super.merge(complete: true)
+      end
+    end
+
     class << self
       def storable_fields
         ATTRIBUTES
