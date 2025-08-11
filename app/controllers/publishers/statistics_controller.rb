11628d7c9d76cd6c96dfe4f77931594c4952d98c
Stephen Dicks <github@starswan.com>
2025-08-11 11:25:02 +0100

    tweak

--- app/controllers/publishers/statistics_controller.rb
+++ app/controllers/publishers/statistics_controller.rb
@@ -15,12 +15,12 @@ module Publishers
     end
 
     def vacancies
-      PublishedVacancy.in_organisation_ids(current_publisher.accessible_organisations(current_organisation))
+      PublishedVacancy.in_organisation_ids(current_publisher.accessible_organisations(current_organisation).map(&:id))
     end
 
-    # convert hash like {"age_thing" => 28, "age_other" => 24, "wibble" => 4, "age_other_descriptions"}
-    #              into {"thing" => 28, "other" => 24}
     def filter_reports_by(reports, value)
+      # convert hash like {"age_thing" => 28, "age_other" => 24, "wibble" => 4, "age_other_descriptions"}
+      #              into {"thing" => 28, "other" => 24}
       reports.map do |hash|
         hash.select { |k, _v| k.starts_with?(value) && k != "#{value}other_descriptions" }
                                .transform_keys { |k| k[value.length..] }
@@ -28,6 +28,7 @@ module Publishers
     end
 
     def merge_sum(values)
+      # use extended merge syntax to merge hashes by adding up the values
       values.reduce({}) do |hash, referrer_counts_hash|
         hash.merge(referrer_counts_hash) do |_key, v1, v2|
           v1 + v2
