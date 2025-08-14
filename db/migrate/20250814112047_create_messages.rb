62ff0652890d566e1847de63cea53b1c1989e49f
Kyle MacPherson <kaimac2010@gmail.com>
2025-08-14 12:17:42 +0100

    Allow sending of messages

--- db/migrate/20250814112047_create_messages.rb
+++ db/migrate/20250814112047_create_messages.rb
@@ -1,7 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[7.2]
   def change
     create_table :messages, id: :uuid, default: -> { "gen_random_uuid()" } do |t|
+<<<<<<< HEAD
       t.text :content, null: false
+=======
+      t.text :content
+>>>>>>> 7e5386bbf (Allow sending of messages)
       t.string :sender_type, null: false
       t.uuid :sender_id, null: false
       t.uuid :conversation_id, null: false
@@ -9,7 +13,11 @@ class CreateMessages < ActiveRecord::Migration[7.2]
     end
 
     add_index :messages, :conversation_id
+<<<<<<< HEAD
     add_index :messages, %i[sender_type sender_id], name: "index_messages_on_sender"
+=======
+    add_index :messages, [:sender_type, :sender_id], name: "index_messages_on_sender"
+>>>>>>> 7e5386bbf (Allow sending of messages)
     add_foreign_key :messages, :conversations
   end
 end
