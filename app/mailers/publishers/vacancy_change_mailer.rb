29f5a51a5fd799203d43a413f06671d12692fccb
Stephen Dicks <github@starswan.com>
2025-08-18 08:41:12 +0100

    Revert one off mailer job (#7977)
    ## Trello card URL
    
    
    https://trello.com/c/JRNLyz1v/2076-remove-code-related-to-publishers-one-off-upload-download-changes-notification-emails
    
    ## Changes in this PR:
    
    Revert one-off mailer job

--- app/mailers/publishers/vacancy_change_mailer.rb
+++
@@ -1,8 +0,0 @@
-class Publishers::VacancyChangeMailer < Publishers::BaseMailer
-  def notify(publisher:)
-    @publisher = publisher
-    @subject = I18n.t("publishers.vacancy_change_mailer.notify.subject")
-
-    send_email(to: publisher.email, subject: @subject)
-  end
-end
