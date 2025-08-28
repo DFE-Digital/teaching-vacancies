ff3fdcbd9e9dad9f9f85afe7d446cf5d0c5dc43e
Kyle MacPherson <kaimac2010@gmail.com>
2025-08-28 15:34:35 +0100

    Allow archiving and unarchiving of messages

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,6 +2,9 @@ class Conversation < ApplicationRecord
   belongs_to :job_application
   has_many :messages, dependent: :destroy
 
+  scope :inbox, -> { where(archived: false) }
+  scope :archived, -> { where(archived: true) }
+
   def self.default_title_for(job_application)
     "Regarding application: #{job_application.vacancy.job_title}"
   end
