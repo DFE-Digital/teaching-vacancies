847b35a2b535f05cb660502314184dd2a9c44d20
Stephen Dicks <github@starswan.com>
2025-09-04 19:51:07 +0100

    added test for slightly odd scenario

--- spec/system/publishers/publishers_can_view_candidate_interstitial_spec.rb
+++ spec/system/publishers/publishers_can_view_candidate_interstitial_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.describe "Publishers can see the Candidate profiles interstitial page" do
   let(:school) { create(:school) }
 
-  context "the user has logged in for the first time" do
+  before { login_publisher(publisher: publisher, organisation: school) }
+
+  after { logout }
+
+  context "the user has not accepted terms" do
     let(:publisher) { create(:publisher, acknowledged_candidate_profiles_interstitial: false, accepted_terms_at: nil) }
-
-    before { login_publisher(publisher: publisher, organisation: school) }
-
-    after { logout }
 
     scenario "they will see the interstitial page only once" do
       visit organisation_jobs_with_type_path
@@ -18,11 +18,22 @@ RSpec.describe "Publishers can see the Candidate profiles interstitial page" do
 
       expect(page).to have_content("You can now view candidate profiles and invite them to apply to jobs")
 
-      visit "/"
-
       visit organisation_jobs_with_type_path
 
       expect(page).to_not have_content("You can now view candidate profiles and invite them to apply to jobs")
+    end
+  end
+
+  context "the user has accepted terms, but somehow has not seen the interstitial" do
+    let(:publisher) { create(:publisher, acknowledged_candidate_profiles_interstitial: false) }
+
+    scenario "they will see the interstitial page only once" do
+      visit organisation_jobs_with_type_path
+
+      expect(page).to have_current_path publishers_candidate_profiles_interstitial_path
+
+      click_on "View candidate profiles"
+      expect(page).to have_current_path publishers_jobseeker_profiles_path
     end
   end
 end
