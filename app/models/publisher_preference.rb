93bf3c6deff8da71e63b5c20bc5e811f50822fb3
Stephen Dicks <github@starswan.com>
2025-09-08 09:01:39 +0100

    add commments to publisher profile model (#8035)

--- app/models/publisher_preference.rb
+++ app/models/publisher_preference.rb
@@ -1,3 +1,16 @@
+# This class serves 2 distinct purposes:
+#
+# 1. For Local authorities (LAs) it stores the 'filter' for which schools
+# each publisher wants to manage - these are stored in local_authority_publisher_schools
+# and mostly accessed via the 'schools' property
+#
+# 2. For all organisations, the 'organisation_publisher_preferences' relationship
+# ()mostly accessed via organisations)
+# is a database-backed organisation filter - this allows the filter to be preserved
+# between pages when nvaigating between different tabs. The filters are managed via
+# the PublisherPreferencesController, often by removing and adding single filters
+# and then redisplaying publisher/vacancies#index page
+#
 class PublisherPreference < ApplicationRecord
   belongs_to :publisher
   belongs_to :organisation
