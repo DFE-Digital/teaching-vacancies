7b6af0fcc6c63a9fe00a9987fad1265bc725785c
Jerome Riga <jriga@zemis.co.uk>
2025-09-09 17:13:16 +0100

    Add note timestamp format

---
+++ spec/components/publisher_notes_on_job_application_component_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe PublisherNotesOnJobApplicationComponent, type: :component do
+  let(:notes_component) { Capybara.string(render_component) }
+  let(:component) { described_class.new(job_application:, vacancy:, notes_form:) }
+  let(:render_component) { render_inline(component) }
+  let(:notes_form) { Publishers::JobApplication::NotesForm.new }
+  let(:note) { build_stubbed(:note, created_at:) }
+  let(:job_application) { note.job_application }
+  let(:vacancy) { job_application.vacancy }
+  let(:created_at) { Time.zone.now }
+
+  before do
+    allow(job_application).to receive(:notes).and_return([note])
+    notes_component
+  end
+
+  it "renders formatted note timestamp" do
+    expect(notes_component.find("p.govuk-body-s").text).to include(created_at.to_fs)
+  end
+end
