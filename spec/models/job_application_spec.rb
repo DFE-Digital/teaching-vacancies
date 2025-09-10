87670527936526c8e82920fbce580699de1ea569
Kyle MacPherson <kaimac2010@gmail.com>
2025-09-10 16:23:42 +0100

    Add missing tests

--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -314,6 +314,110 @@ RSpec.describe JobApplication do
     end
   end
 
+  describe "#can_jobseeker_initiate_message?" do
+    subject { job_application.can_jobseeker_initiate_message? }
+
+    context "when status allows jobseeker to initiate messages" do
+      %w[interviewing unsuccessful_interview offered declined].each do |allowed_status|
+        context "when status is #{allowed_status}" do
+          let(:job_application) { create(:job_application, status: allowed_status) }
+
+          it { is_expected.to be true }
+        end
+      end
+    end
+
+    context "when status does not allow jobseeker to initiate messages" do
+      %w[submitted shortlisted unsuccessful withdrawn].each do |disallowed_status|
+        context "when status is #{disallowed_status}" do
+          let(:job_application) { create(:job_application, status: disallowed_status) }
+
+          it { is_expected.to be false }
+        end
+      end
+    end
+  end
+
+  describe "#can_jobseeker_reply_to_message?" do
+    subject { job_application.can_jobseeker_reply_to_message? }
+
+    context "when status allows jobseeker to reply to messages" do
+      %w[submitted shortlisted interviewing unsuccessful_interview offered declined].each do |allowed_status|
+        context "when status is #{allowed_status}" do
+          let(:job_application) { create(:job_application, status: allowed_status) }
+
+          it { is_expected.to be true }
+        end
+      end
+    end
+
+    context "when status does not allow jobseeker to reply to messages" do
+      %w[unsuccessful withdrawn].each do |disallowed_status|
+        context "when status is #{disallowed_status}" do
+          let(:job_application) { create(:job_application, status: disallowed_status) }
+
+          it { is_expected.to be false }
+        end
+      end
+    end
+  end
+
+  describe "#can_publisher_send_message?" do
+    subject { job_application.can_publisher_send_message? }
+
+    context "when status allows publisher to send messages" do
+      %w[submitted shortlisted interviewing unsuccessful_interview offered declined unsuccessful].each do |allowed_status|
+        context "when status is #{allowed_status}" do
+          let(:job_application) { create(:job_application, status: allowed_status) }
+
+          it { is_expected.to be true }
+        end
+      end
+    end
+
+    context "when status does not allow publisher to send messages" do
+      context "when status is withdrawn" do
+        let(:job_application) { create(:job_application, status: "withdrawn") }
+
+        it { is_expected.to be false }
+      end
+    end
+  end
+
+  describe "#can_jobseeker_send_message?" do
+    subject { job_application.can_jobseeker_send_message? }
+
+    context "when no conversations exist" do
+      context "when jobseeker can initiate messages" do
+        let(:job_application) { create(:job_application, status: "interviewing") }
+
+        it { is_expected.to be true }
+      end
+
+      context "when jobseeker cannot initiate messages" do
+        let(:job_application) { create(:job_application, status: "submitted") }
+
+        it { is_expected.to be false }
+      end
+    end
+
+    context "when conversations exist" do
+      before { create(:conversation, job_application: job_application) }
+
+      context "when jobseeker can reply to messages" do
+        let(:job_application) { create(:job_application, status: "submitted") }
+
+        it { is_expected.to be true }
+      end
+
+      context "when jobseeker cannot reply to messages" do
+        let(:job_application) { create(:job_application, status: "withdrawn") }
+
+        it { is_expected.to be false }
+      end
+    end
+  end
+
   context "when saving change to status" do
     subject { create(:job_application) }
 
