c4b3739f5bd0e1c58e9f0a7a2844000363c8ff91
Kyle MacPherson <kaimac2010@gmail.com>
2025-09-10 16:23:42 +0100

    Add missing tests

--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -320,7 +320,11 @@ RSpec.describe JobApplication do
     context "when status allows jobseeker to initiate messages" do
       %w[interviewing unsuccessful_interview offered declined].each do |allowed_status|
         context "when status is #{allowed_status}" do
+<<<<<<< HEAD
           let(:job_application) { build_stubbed(:job_application, status: allowed_status) }
+=======
+          let(:job_application) { create(:job_application, status: allowed_status) }
+>>>>>>> 876705279 (Add missing tests)
 
           it { is_expected.to be true }
         end
@@ -330,7 +334,11 @@ RSpec.describe JobApplication do
     context "when status does not allow jobseeker to initiate messages" do
       %w[submitted shortlisted unsuccessful withdrawn].each do |disallowed_status|
         context "when status is #{disallowed_status}" do
+<<<<<<< HEAD
           let(:job_application) { build_stubbed(:job_application, status: disallowed_status) }
+=======
+          let(:job_application) { create(:job_application, status: disallowed_status) }
+>>>>>>> 876705279 (Add missing tests)
 
           it { is_expected.to be false }
         end
@@ -344,7 +352,11 @@ RSpec.describe JobApplication do
     context "when status allows jobseeker to reply to messages" do
       %w[submitted shortlisted interviewing unsuccessful_interview offered declined].each do |allowed_status|
         context "when status is #{allowed_status}" do
+<<<<<<< HEAD
           let(:job_application) { build_stubbed(:job_application, status: allowed_status) }
+=======
+          let(:job_application) { create(:job_application, status: allowed_status) }
+>>>>>>> 876705279 (Add missing tests)
 
           it { is_expected.to be true }
         end
@@ -354,7 +366,11 @@ RSpec.describe JobApplication do
     context "when status does not allow jobseeker to reply to messages" do
       %w[unsuccessful withdrawn].each do |disallowed_status|
         context "when status is #{disallowed_status}" do
+<<<<<<< HEAD
           let(:job_application) { build_stubbed(:job_application, status: disallowed_status) }
+=======
+          let(:job_application) { create(:job_application, status: disallowed_status) }
+>>>>>>> 876705279 (Add missing tests)
 
           it { is_expected.to be false }
         end
@@ -368,7 +384,11 @@ RSpec.describe JobApplication do
     context "when status allows publisher to send messages" do
       %w[submitted shortlisted interviewing unsuccessful_interview offered declined unsuccessful].each do |allowed_status|
         context "when status is #{allowed_status}" do
+<<<<<<< HEAD
           let(:job_application) { build_stubbed(:job_application, status: allowed_status) }
+=======
+          let(:job_application) { create(:job_application, status: allowed_status) }
+>>>>>>> 876705279 (Add missing tests)
 
           it { is_expected.to be true }
         end
@@ -377,7 +397,11 @@ RSpec.describe JobApplication do
 
     context "when status does not allow publisher to send messages" do
       context "when status is withdrawn" do
+<<<<<<< HEAD
         let(:job_application) { build_stubbed(:job_application, status: "withdrawn") }
+=======
+        let(:job_application) { create(:job_application, status: "withdrawn") }
+>>>>>>> 876705279 (Add missing tests)
 
         it { is_expected.to be false }
       end
@@ -389,19 +413,28 @@ RSpec.describe JobApplication do
 
     context "when no conversations exist" do
       context "when jobseeker can initiate messages" do
+<<<<<<< HEAD
         let(:job_application) { build_stubbed(:job_application, status: "interviewing") }
+=======
+        let(:job_application) { create(:job_application, status: "interviewing") }
+>>>>>>> 876705279 (Add missing tests)
 
         it { is_expected.to be true }
       end
 
       context "when jobseeker cannot initiate messages" do
+<<<<<<< HEAD
         let(:job_application) { build_stubbed(:job_application, status: "submitted") }
+=======
+        let(:job_application) { create(:job_application, status: "submitted") }
+>>>>>>> 876705279 (Add missing tests)
 
         it { is_expected.to be false }
       end
     end
 
     context "when conversations exist" do
+<<<<<<< HEAD
       let(:job_application) { build_stubbed(:job_application, status: status) }
 
       before do
@@ -411,12 +444,22 @@ RSpec.describe JobApplication do
 
       context "when jobseeker can reply to messages" do
         let(:status) { "submitted" }
+=======
+      before { create(:conversation, job_application: job_application) }
+
+      context "when jobseeker can reply to messages" do
+        let(:job_application) { create(:job_application, status: "submitted") }
+>>>>>>> 876705279 (Add missing tests)
 
         it { is_expected.to be true }
       end
 
       context "when jobseeker cannot reply to messages" do
+<<<<<<< HEAD
         let(:status) { "withdrawn" }
+=======
+        let(:job_application) { create(:job_application, status: "withdrawn") }
+>>>>>>> 876705279 (Add missing tests)
 
         it { is_expected.to be false }
       end
