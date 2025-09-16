d5366ee27ccfbb3d146a049829f9ac74f3de4b53
Kyle MacPherson <kaimac2010@gmail.com>
2025-09-16 14:56:50 +0100

    Fix notification

--- app/models/jobseekers/message_received_notifier.rb
+++ app/models/jobseekers/message_received_notifier.rb
@@ -12,19 +12,22 @@ module Jobseekers
 
     # rubocop:disable Metrics/BlockLength
     notification_methods do
-      def message_text
+      def message
         if unsuccessful_application?
           t("notifications.jobseekers/message_received_notification.unsuccessful.message_html",
+            link: message_link,
             job_title: job_title,
             school_name: school_name)
         else
           t("notifications.jobseekers/message_received_notification.default.message_html",
+            link: message_link,
             job_title: job_title)
         end
       end
 
       include DatesHelper
       include ActionView::Helpers::UrlHelper
+      include GovukVisuallyHiddenHelper
       include GovukLinkHelper
 
       def timestamp
@@ -32,6 +35,10 @@ module Jobseekers
       end
 
       private
+
+      def message_link
+        govuk_link_to "a message", jobseekers_job_application_path(job_application, tab: "messages"), class: "govuk-link--no-visited-state"
+      end
 
       def unsuccessful_application?
         job_application.status == "unsuccessful"
@@ -48,10 +55,6 @@ module Jobseekers
       def school_name
         job_application.vacancy.organisation.name
       end
-    end
-
-    def message(*)
-      record
     end
   end
   # rubocop:enable Metrics/BlockLength
