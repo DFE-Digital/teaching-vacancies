5998f79f96baf6db4e8bd800302e5e4ce8f12152
Marc Sardon <pringaohowto@gmail.com>
2025-09-18 15:25:02 +0100

    Invalidate existing additional cookies consent
    Given the new tracking features. Invalidate the existing consent for
    additional cookies from the users.
    
    Users will need to re-consent for additional cookies.
    
    (cherry picked from commit b46a2ac51980ed4526f22319efa1585965f4f5c6)

--- app/assets/javascript/clarity_cookies.js
+++ app/assets/javascript/clarity_cookies.js
@@ -5,7 +5,7 @@ const cookies = document.cookie.split(';').map(Cookie.parse);
 // change clarity consent cookie value from default 'yes' to 'clarity'
 // to remind ourselves that we have called the clarity consent API
 cookies.forEach((cookie) => {
-  if (cookie && cookie.name === 'consented-to-additional-cookies-v2' && cookie.value === 'yes') {
+  if (cookie && cookie.name === 'consented-to-additional-cookies-v3' && cookie.value === 'yes') {
     cookie.value = 'clarity';
     window.clarity('consent');
     document.cookie = cookie.toString();
