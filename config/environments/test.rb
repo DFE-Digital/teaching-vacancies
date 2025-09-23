0c33eb3523ce8e640834c779f31870d8a6a78c79
Stephen Dicks <github@starswan.com>
2025-09-23 15:28:06 +0100

    added daily log rotation for development and test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.logger = Logger.new(config.paths["log"].first, "daily")
 end
 
 # Avoid OmniAuth output in tests:
