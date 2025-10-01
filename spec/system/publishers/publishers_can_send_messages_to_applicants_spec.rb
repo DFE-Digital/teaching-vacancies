62df9cd37d33b67adac93feddfe28c7ee64d7494
Stephen Dicks <github@starswan.com>
2025-10-01 16:06:49 +0100

    accessibility tests

--- spec/system/publishers/publishers_can_send_messages_to_applicants_spec.rb
+++ spec/system/publishers/publishers_can_send_messages_to_applicants_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Publishers can send messages to job applicants" do
   let(:publisher) { create(:publisher, organisations: [organisation]) }
   let(:vacancy) { create(:vacancy, :live, organisations: [organisation]) }
   let(:job_application) { create(:job_application, :submitted, vacancy: vacancy) }
+  let(:jobseeker) { job_application.jobseeker }
 
   context "when logged in as a publisher" do
     before do
@@ -14,7 +15,6 @@ RSpec.describe "Publishers can send messages to job applicants" do
     after { logout }
 
     context "with messages from 2 different publishers" do
-      let(:jobseeker) { job_application.jobseeker }
       let(:another_publisher) { create(:publisher) }
       let!(:conversation) { create(:conversation, job_application: job_application) }
 
@@ -30,15 +30,17 @@ RSpec.describe "Publishers can send messages to job applicants" do
       # but we want the namespaces in this instance
       # https://github.com/rspec/rspec-rails/issues/396 suggests setting the partial lookup path
       # view.lookup_context.prefixes << 'intranet/application' << 'application' but it didn't seem to work
-      it "shows the correct headers for each message", :js do
+      it "shows the correct headers for each message" do
         expect(all(".govuk-summary-card__title-wrapper").map { |x| x[:class].split.last }).to eq(["message-header--recipient", "message-header--sender", "message-header--sender"])
       end
     end
 
     context "when viewing a job application messages tab" do
-      it "shows download messages button on the page" do
+      before do
         visit messages_organisation_job_job_application_path(vacancy.id, job_application.id)
+      end
 
+      it "shows download messages button on the page" do
         expect(page).to have_link("Download messages")
 
         # Check that the download link has the correct path
@@ -47,8 +49,6 @@ RSpec.describe "Publishers can send messages to job applicants" do
       end
 
       it "allows publisher to send a message to the job applicant", :js do
-        visit messages_organisation_job_job_application_path(vacancy.id, job_application.id)
-
         expect(page).to have_link("Download messages")
         expect(page).to have_link("Send message to candidate")
 
@@ -75,13 +75,19 @@ RSpec.describe "Publishers can send messages to job applicants" do
       end
     end
 
-    context "when messages already exist", :js do
+    context "when messages already exist" do
       let!(:conversation) { create(:conversation, job_application: job_application) }
       let!(:message) { create(:publisher_message, conversation: conversation, sender: publisher, content: "Previous message content") }
 
-      it "displays existing messages and allows sending additional messages" do
+      before do
         visit messages_organisation_job_job_application_path(vacancy.id, job_application.id)
+      end
 
+      it "passes accessibility checks", :a11y do
+        expect(page).to be_axe_clean
+      end
+
+      it "displays existing messages and allows sending additional messages", :js do
         expect(page).to have_text("#{publisher.given_name} #{publisher.family_name}")
         expect(page).to have_text("Previous message content")
         expect(page).to have_text(message.created_at.strftime("%d %B %Y at %I:%M %p"))
@@ -101,8 +107,6 @@ RSpec.describe "Publishers can send messages to job applicants" do
       end
 
       it "shows validation errors with existing messages when sending blank message" do
-        visit messages_organisation_job_job_application_path(vacancy.id, job_application.id)
-
         expect(page).to have_text("Previous message content")
 
         click_link "Send message to candidate"
@@ -115,64 +119,85 @@ RSpec.describe "Publishers can send messages to job applicants" do
     end
   end
 
-  context "when jobseeker replies to messages" do
-    let(:jobseeker) { job_application.jobseeker }
+  context "with messages" do
     let!(:conversation) { create(:conversation, job_application: job_application) }
     let!(:publisher_message) { create(:publisher_message, conversation: conversation, sender: publisher, content: "Hello from publisher") }
-    let!(:job_application_without_messages) { create(:job_application, :submitted, vacancy: vacancy, jobseeker: jobseeker) }
 
+    context "with a jobseeker" do
+      before do
+        login_as(jobseeker, scope: :jobseeker)
+        visit jobseekers_job_application_path(job_application, tab: "messages")
+      end
+
+      after { logout }
+
+      it "passes accessibility checks", :a11y do
+        expect(page).to be_axe_clean
+      end
+
+      it "displays existing publisher messages to jobseeker and allows jobseeker to reply to publisher messages", :js do
+        expect(page).to have_text("#{publisher.given_name} #{publisher.family_name}")
+        expect(page).to have_text("Hello from publisher")
+        expect(page).to have_text(publisher_message.created_at.strftime("%d %B %Y at %I:%M %p"))
+        click_link "Send message to hiring staff"
+        expect(page).to have_css(".trix-content")
+        expect(page).to have_button("Send message")
+        expect(page).to have_link("Cancel")
+        expect(page).to have_text("How will this message be sent?")
+
+        # cannot send empty message
+        click_button "Send message"
+
+        expect(page).to have_css("h2.govuk-error-summary__title", text: "There is a problem")
+        within(".govuk-error-summary__body") do
+          expect(page).to have_link("Please enter your message")
+        end
+
+        jobseeker_reply = "Thank you for your message. I look forward to hearing from you."
+        fill_in_trix_editor "publishers_job_application_messages_form_content", with: jobseeker_reply
+
+        click_button "Send message"
+
+        expect(page).to have_text("Message sent successfully")
+        expect(page).to have_text(job_application.name.to_s)
+        expect(conversation.reload.messages.count).to eq(2)
+        message_cards = all(".govuk-summary-card")
+        expect(message_cards.first).to have_text(jobseeker_reply)
+        expect(message_cards.last).to have_text("Hello from publisher")
+      end
+    end
+
+    context "with a publisher" do
+      before do
+        create(:jobseeker_message, conversation: conversation, sender: jobseeker, content: "Jobseeker reply content")
+
+        login_publisher(publisher: publisher, organisation: organisation)
+
+        visit messages_organisation_job_job_application_path(vacancy.id, job_application.id)
+      end
+
+      after { logout }
+
+      it "passes accessibility checks", :a11y do
+        expect(page).to be_axe_clean
+      end
+
+      it "shows jobseeker reply in publisher interface" do
+        expect(page).to have_text("Hello from publisher")
+        expect(page).to have_text("Jobseeker reply content")
+        expect(page).to have_text(job_application.name.to_s)
+        expect(page).to have_text("#{publisher.given_name} #{publisher.family_name}")
+      end
+    end
+  end
+
+  context "without jobseeker messages" do
     before { login_as(jobseeker, scope: :jobseeker) }
 
     after { logout }
 
-    it "displays existing publisher messages to jobseeker and allows jobseeker to reply to publisher messages", :js do
-      visit jobseekers_job_application_path(job_application, tab: "messages")
-
-      expect(page).to have_text("#{publisher.given_name} #{publisher.family_name}")
-      expect(page).to have_text("Hello from publisher")
-      expect(page).to have_text(publisher_message.created_at.strftime("%d %B %Y at %I:%M %p"))
-      click_link "Send message to hiring staff"
-      expect(page).to have_css(".trix-content")
-      expect(page).to have_button("Send message")
-      expect(page).to have_link("Cancel")
-      expect(page).to have_text("How will this message be sent?")
-
-      # cannot send empty message
-      click_button "Send message"
-
-      expect(page).to have_css("h2.govuk-error-summary__title", text: "There is a problem")
-      within(".govuk-error-summary__body") do
-        expect(page).to have_link("Please enter your message")
-      end
-
-      jobseeker_reply = "Thank you for your message. I look forward to hearing from you."
-      fill_in_trix_editor "publishers_job_application_messages_form_content", with: jobseeker_reply
-
-      click_button "Send message"
-
-      expect(page).to have_text("Message sent successfully")
-      expect(page).to have_text(job_application.name.to_s)
-      expect(conversation.reload.messages.count).to eq(2)
-      message_cards = all(".govuk-summary-card")
-      expect(message_cards.first).to have_text(jobseeker_reply)
-      expect(message_cards.last).to have_text("Hello from publisher")
-    end
-
-    it "shows jobseeker reply in publisher interface" do
-      create(:jobseeker_message, conversation: conversation, sender: jobseeker, content: "Jobseeker reply content")
-
-      login_publisher(publisher: publisher, organisation: organisation)
-
-      visit messages_organisation_job_job_application_path(vacancy.id, job_application.id)
-
-      expect(page).to have_text("Hello from publisher")
-      expect(page).to have_text("Jobseeker reply content")
-      expect(page).to have_text(job_application.name.to_s)
-      expect(page).to have_text("#{publisher.given_name} #{publisher.family_name}")
-    end
-
     it "shows no messages interface when no conversation exists" do
-      visit jobseekers_job_application_path(job_application_without_messages, tab: "messages")
+      visit jobseekers_job_application_path(job_application, tab: "messages")
 
       expect(page).to have_text(I18n.t("jobseekers.job_applications.messages.no_messages_yet"))
       expect(page).to have_no_css(".trix-content")
