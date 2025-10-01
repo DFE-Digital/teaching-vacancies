e8a2024ca9fd0e2634eee99257a77d70c27ce09f
Stephen Dicks <github@starswan.com>
2025-10-01 09:23:52 +0100

    New job statistics page (#8105)

--- app/services/publishers/export_vacancy_to_csv.rb
+++ app/services/publishers/export_vacancy_to_csv.rb
@@ -1,39 +1,29 @@
 require "csv"
 
 class Publishers::ExportVacancyToCsv
-  attr_reader :vacancy
+  class << self
+    def call(vacancy, job_application_counts)
+      columns = base_columns vacancy
+      columns.merge!(job_application_columns(job_application_counts)) if vacancy.can_receive_job_applications?
 
-  def initialize(vacancy, number_of_unique_views)
-    @vacancy = vacancy
-    @number_of_unique_views = number_of_unique_views
-  end
-
-  def call
-    columns = base_columns
-    columns.merge!(job_application_columns) if vacancy.can_receive_job_applications?
-
-    CSV.generate(headers: true) do |csv|
-      csv << columns.keys
-      csv << columns.values
+      CSV.generate(headers: true) do |csv|
+        csv << columns.keys
+        csv << columns.values
+      end
     end
-  end
 
-  private
+    private
 
-  def base_columns
-    {
-      "Organisation" => vacancy.organisation_name,
-      I18n.t("jobs.job_title") => vacancy.job_title,
-      I18n.t("publishers.vacancies.statistics.show.views_by_jobseeker") => @number_of_unique_views,
-      I18n.t("publishers.vacancies.statistics.show.saves_by_jobseeker") => vacancy.saved_jobs.count,
-    }
-  end
+    def base_columns(vacancy)
+      {
+        "Organisation" => vacancy.organisation_name,
+        I18n.t("jobs.job_title") => vacancy.job_title,
+      }
+    end
 
-  def job_application_columns
-    { I18n.t("publishers.vacancies.statistics.show.total_applications") => vacancy.job_applications.not_draft.count,
-      I18n.t("publishers.vacancies.statistics.show.unread_applications") => vacancy.job_applications.submitted.count,
-      I18n.t("publishers.vacancies.statistics.show.shortlisted_applications") => vacancy.job_applications.shortlisted.count,
-      I18n.t("publishers.vacancies.statistics.show.rejected_applications") => vacancy.job_applications.unsuccessful.count,
-      I18n.t("publishers.vacancies.statistics.show.withdrawn_applications") => vacancy.job_applications.withdrawn.count }
+    def job_application_columns(job_application_counts)
+      { I18n.t("publishers.vacancies.statistics.show.total_applications") => job_application_counts.sum }
+        .merge(job_application_counts.map.with_index { |count, index| [I18n.t(".applications.#{index}", scope: "publishers.vacancies.statistics.show"), count] }.to_h)
+    end
   end
 end
