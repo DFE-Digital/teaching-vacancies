b6b87bab958ba42492c337c3a749119c456a47ef
Kyle MacPherson <kaimac2010@gmail.com>
2025-10-02 16:55:02 +0100

    Allow searching of candidate messages

---
+++ app/form_models/publishers/candidate_messages_search_form.rb
@@ -0,0 +1,19 @@
+class Publishers::CandidateMessagesSearchForm
+  include ActiveModel::Model
+
+  attr_reader :keyword
+
+  def initialize(params = {})
+    @keyword = params[:keyword].to_s.strip if params[:keyword].present?
+  end
+
+  def to_hash
+    {
+      keyword: @keyword,
+    }.compact_blank
+  end
+
+  def active_criteria?
+    to_hash.any?
+  end
+end
