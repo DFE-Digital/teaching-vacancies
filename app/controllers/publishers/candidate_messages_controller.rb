90ab0cd045e03355e1d8deff2670dc1b3ebd1020
Kyle MacPherson <kaimac2010@gmail.com>
2025-10-02 12:13:11 +0100

    Refactor

--- app/controllers/publishers/candidate_messages_controller.rb
+++ app/controllers/publishers/candidate_messages_controller.rb
@@ -1,14 +1,15 @@
 class Publishers::CandidateMessagesController < Publishers::BaseController
   def index
     @tab = params[:tab] || "inbox"
+    organisation_ids = current_publisher.accessible_organisations(current_organisation).map(&:id)
 
-    base_conversations = Conversation.for_organisation(current_publisher.accessible_organisations(current_organisation).map(&:id))
+    base_conversations = Conversation.for_organisations(organisation_ids)
                                     .includes(job_application: :vacancy, messages: :sender)
                                     .ordered_by_unread_and_latest_message
 
     @conversations = @tab == "archive" ? base_conversations.archived : base_conversations.inbox
 
-    @inbox_count = Conversation.for_organisation(current_publisher.accessible_organisations(current_organisation).map(&:id))
+    @inbox_count = Conversation.for_organisations(organisation_ids)
                                .inbox
                                .with_unread_jobseeker_messages
                                .count
@@ -16,8 +17,9 @@ class Publishers::CandidateMessagesController < Publishers::BaseController
 
   def toggle_archive
     conversation_ids = params[:conversations] || []
+    organisation_ids = current_publisher.accessible_organisations(current_organisation).map(&:id)
 
-    Conversation.for_organisation(current_publisher.accessible_organisations(current_organisation).map(&:id))
+    Conversation.for_organisations(organisation_ids)
                 .where(id: conversation_ids)
                 .update_all(archived: params[:archive_action] == "archive")
 
