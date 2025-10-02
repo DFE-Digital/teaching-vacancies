1cbea62df23d979257889772611e826bc35e03d1
Jerome Riga <jriga@zemis.co.uk>
2025-10-02 08:29:59 +0100

    Add intermediary landing page `leadership-roles`
    * add new layout
    * tracked CTA link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,23 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
 
+  INTERMEDIARY_LANDING_PAGE_IDS = %w[leadership-roles].freeze
+
+  layout :layout_for_page
+
   def invalid_page
     not_found
   end
 
   private
+
+  def layout_for_page
+    if params[:id].in?(INTERMEDIARY_LANDING_PAGE_IDS)
+      "application_intermediary"
+    else
+      "application"
+    end
+  end
 
   def set_headers
     response.set_header("X-Robots-Tag", "index, nofollow")
