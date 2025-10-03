f063b737a80d667539d2820dddbb38eed293c5a4
starswan <github@starswan.com>
2025-10-03 16:15:09 +0100

    2nd attempt at fixing flakey test

--- spec/services/messages_pdf_generator_spec.rb
+++ spec/services/messages_pdf_generator_spec.rb
@@ -2,14 +2,10 @@ require "rails_helper"
 require "pdf/inspector"
 
 RSpec.describe MessagesPdfGenerator do
-  let(:publisher) { create(:publisher, given_name: "John", family_name: "Smith") }
-  let(:vacancy) { create(:vacancy, :at_one_school, publisher: publisher) }
-  let(:job_application) { create(:job_application, :status_submitted, vacancy: vacancy) }
-  let(:conversation) { create(:conversation, job_application: job_application) }
-  let(:publisher_message) { create(:publisher_message, conversation: conversation, sender: publisher) }
-  let(:jobseeker_message) { create(:jobseeker_message, conversation: conversation) }
-  let(:messages) { [publisher_message, jobseeker_message] }
-  let(:generator) { described_class.new(job_application, messages) }
+  let(:publisher) { build_stubbed(:publisher, given_name: "John", family_name: "Smith") }
+  let!(:vacancy) { build_stubbed(:vacancy, :at_one_school, publisher: publisher, job_applications: [job_application]) }
+  let(:job_application) { build_stubbed(:job_application, :status_submitted, conversations: [conversation]) }
+  let(:generator) { described_class.new(job_application, conversation.messages) }
 
   describe "#generate" do
     subject(:document) { generator.generate }
@@ -19,23 +15,29 @@ RSpec.describe MessagesPdfGenerator do
     end
     let(:pdf_text) { pdf.join(" ") }
 
-    it { is_expected.to be_a(Prawn::Document) }
-
-    it "includes page header and footer information" do
-      expect(pdf_text).to include("Messages for #{vacancy.job_title}")
-      expect(pdf_text).to include("1 of 1")
-      expect(pdf_text).to include("#{job_application.first_name} #{job_application.last_name} | #{vacancy.organisation_name}")
-    end
-
     context "when there are no messages" do
-      let(:messages) { [] }
+      let(:conversation) { build_stubbed(:conversation) }
+
+      it { is_expected.to be_a(Prawn::Document) }
 
       it "displays no messages text" do
         expect(pdf_text).to include("No messages yet.")
       end
+
+      it "includes page header and footer information" do
+        expect(pdf_text).to include("Messages")
+        expect(pdf_text).to include("Messages for #{vacancy.job_title}")
+        expect(pdf_text).to include("#{job_application.first_name} #{job_application.last_name}")
+        expect(pdf_text).to include("1 of 1")
+        expect(pdf_text).to include("#{job_application.first_name} #{job_application.last_name} | #{vacancy.organisation_name}")
+      end
     end
 
     context "when there are messages" do
+      let(:conversation) { build_stubbed(:conversation, messages: [publisher_message, jobseeker_message]) }
+      let(:publisher_message) { build_stubbed(:publisher_message, sender: publisher) }
+      let(:jobseeker_message) { build_stubbed(:jobseeker_message) }
+
       it "includes message content, sender names and timestamps in table format" do
         expect(pdf_text).to include(publisher_message.content.to_plain_text)
         expect(pdf_text).to include(jobseeker_message.content.to_plain_text)
