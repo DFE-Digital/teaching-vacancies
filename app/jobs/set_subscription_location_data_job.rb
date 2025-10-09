00a86cb1ad9a0cfb6e3110fa9aabebd2664dbdb1
Marc Sardon <pringaohowto@gmail.com>
2025-10-09 18:47:41 +0100

    Cover new job with unit tests

--- app/jobs/set_subscription_location_data_job.rb
+++ app/jobs/set_subscription_location_data_job.rb
@@ -4,9 +4,6 @@ class SetSubscriptionLocationDataJob < ApplicationJob
   # Extracted as a job to avoid slowing down subscription creation
   # by making potentially slow external API calls or database geometry operations.
   def perform(subscription_id)
-    subscription = Subscription.find_by(id: subscription_id)
-    return unless subscription
-
-    subscription.set_location_data!
+    Subscription.find_by(id: subscription_id)&.set_location_data!
   end
 end
