f7d0e6a41dbf1650b094458ca1a64579f1303bf0
Stephen Dicks <github@starswan.com>
2025-10-17 15:55:52 +0100

    typo

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -1,4 +1,4 @@
-# The default is Rails::HTML5::Sanitizer buit that seems to only allow a very small subset of HTML tags
+# The default is Rails::HTML5::Sanitizer but that seems to only allow a very small subset of HTML tags
 # and attrs - specifically no divs and no class attributes.
 Rails.application.config.after_initialize do
   ActionText::ContentHelper.sanitizer = Rails::HTML4::Sanitizer.safe_list_sanitizer.new
