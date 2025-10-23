c10fb7472d889b0c110fe7580e78043468267346
Kyle MacPherson <kaimac2010@gmail.com>
2025-10-23 12:42:31 +0100

    send email wip

---
+++ app/mailers/publishers/account_invitation_mailer.rb
@@ -0,0 +1,13 @@
+class Publishers::AccountInvitationMailer < Publishers::BaseMailer
+  include MailerAnalyticsEvents
+
+  def invite_user(contact_email:, publisher_email:)
+    @contact_email = contact_email
+    @publisher_email = publisher_email
+    @publisher = Publisher.find_by(email: publisher_email) # For analytics
+    
+    @subject = I18n.t("publishers.account_invitation_mailer.invite_user.subject")
+
+    send_email(to: contact_email, subject: @subject)
+  end
+end
