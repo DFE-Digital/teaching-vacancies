5233f731c2d969bdf3b710aee3c81a0a99c46178
Jerome Riga <jriga@zemis.co.uk>
2025-10-23 09:42:17 +0100

    Self-disclosure request reminder (#8169)
    ## Trello card URL
    
    https://trello.com/c/vd53obkd/2194-add-the-ability-to-send-a-reminder-onto-the-self-disclosure-flow-mirror-references-flow
    
    ## Changes in this PR:
    
    - After 2 working days if self-disclosure request is still pending
    Hiring
    Staff (HS) have the ability to send a reminder to the jobseeker.
    
    
    ## Screenshots of UI changes:
    
    ### Before
    
    ### After
    <img width="953" height="546" alt="image"
    src="https://github.com/user-attachments/assets/b35dce79-a3f0-4c56-906e-d3925fe86f48"
    />
    
    
    ## Checklists:
    
    ### Data & Schema Changes
    
    If this PR modifies data structures or validations, check the following:
    
    - [ ] Adds/removes model validations
    - [ ] Adds/removes database fields
    - [ ] Modifies Vacancy enumerables (phases, working patterns, job roles,
    key stages, etc.)
    
    <details>
    <summary>If any of the above options has changed then the author must
    check/resolve all of the following...</summary>
    
    ### Integration Impact
    
    Does this change affect any of these integrations?
    - [ ] DfE Analytics platform
    - [ ] Legacy imports mappings
    - [ ] DWP Find a Job export mappings
    - [ ] Publisher ATS API (may require mapping updates or API versioning)
    
    ### User Experience & Data Integrity
    
    Could this change impact:
    - [ ] Existing subscription alerts (will legacy subscription search
    filters break?)
    - [ ] Legacy vacancy copying (will copied vacancies fail new
    validations?)
    - [ ] In-progress drafts for Vacancies or Job Applications
    </details>

---
+++ spec/system/publishers/publishers_can_send_self_disclosure_reminder_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe "Publishers can send reminder for pending self-disclosure request", :perform_enqueued do
+  include ActiveJob::TestHelper
+
+  let(:publisher) { create(:publisher, :with_organisation, email: "publisher@contoso.com") }
+  let(:vacancy) { create(:vacancy, publisher:, organisations: publisher.organisations) }
+  let(:job_application) { create(:job_application, :status_submitted, email_address: "jobseeker@contoso.com", vacancy:) }
+  let(:self_disclosure_request) { create(:self_disclosure_request, :sent, updated_at:, job_application:) }
+
+  before do
+    create(:self_disclosure, self_disclosure_request:)
+  end
+
+  context "when viewed 1 business days after request sent" do
+    let(:updated_at) { 1.business_day.ago }
+
+    scenario "page has no reminder button" do
+      run_with_publisher(publisher) do
+        publisher_ats_self_disclosure_page.load(vacancy_id: vacancy.id, job_application_id: job_application.id)
+        expect(publisher_ats_self_disclosure_page).not_to have_reminder_btn
+      end
+    end
+  end
+
+  context "when viewed 2 business days after request sent" do
+    let(:updated_at) { 2.business_days.ago }
+
+    scenario "send reminder" do
+      run_with_publisher(publisher) do
+        publisher_ats_self_disclosure_page.load(vacancy_id: vacancy.id, job_application_id: job_application.id)
+        expect(publisher_ats_self_disclosure_page).to have_reminder_btn
+        publisher_ats_self_disclosure_page.reminder_btn.click
+
+        expect(publisher_ats_self_disclosure_page).to be_displayed(vacancy_id: vacancy.id, job_application_id: job_application.id)
+        expect(publisher_ats_self_disclosure_page).to have_text("Success")
+        expect(publisher_ats_self_disclosure_page).not_to have_reminder_btn
+        expect(ActionMailer::Base.deliveries.flat_map(&:to))
+          .to contain_exactly("jobseeker@contoso.com")
+      end
+    end
+  end
+end
