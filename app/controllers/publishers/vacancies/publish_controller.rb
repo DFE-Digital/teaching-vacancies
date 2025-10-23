51f267bfde60ed2afc8f6810169b4b6f3e5bd9b6
Kyle MacPherson <kaimac2010@gmail.com>
2025-10-23 16:18:29 +0100

    wip

--- app/controllers/publishers/vacancies/publish_controller.rb
+++ app/controllers/publishers/vacancies/publish_controller.rb
@@ -4,8 +4,15 @@ class Publishers::Vacancies::PublishController < Publishers::Vacancies::WizardBa
       redirect_to organisation_job_path(vacancy.id), notice: t("messages.jobs.already_published")
     elsif all_steps_valid? && PublishVacancy.new(vacancy, current_publisher, current_organisation).call
       update_google_index(vacancy) if PublishedVacancy.find(vacancy.id).live?
-      # send the email here?
-      # some kind of logic to check if vacancy.contact_email is a publisher already, if not send email
+      
+      # Send invitation email if contact email doesn't belong to an existing publisher
+      unless vacancy.contact_email_belongs_to_a_publisher?
+        Publishers::AccountInvitationMailer.invite_user(
+          contact_email: vacancy.contact_email,
+          publisher_email: current_publisher.email
+        ).deliver_now
+      end
+      
       redirect_to organisation_job_summary_path(vacancy.id)
     else
       redirect_to organisation_job_path(vacancy.id)
