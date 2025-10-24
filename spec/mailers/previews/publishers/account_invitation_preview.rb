34b5edc402916999d52aadbed275c66772dbd8a4
Kyle MacPherson <kaimac2010@gmail.com>
2025-10-24 14:58:08 +0100

    Linting

--- spec/mailers/previews/publishers/account_invitation_preview.rb
+++ spec/mailers/previews/publishers/account_invitation_preview.rb
@@ -2,7 +2,7 @@ class Publishers::AccountInvitationPreview < ActionMailer::Preview
   def invite_user
     Publishers::AccountInvitationMailer.invite_user(
       contact_email: "new.contact@example.com",
-      publisher_email: "publisher@school.edu"
+      publisher_email: "publisher@school.edu",
     )
   end
 end
