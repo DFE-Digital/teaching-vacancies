5da8876ed0084cd7936a7c6b156342b0ffacba9a
Stephen Dicks <github@starswan.com>
2025-10-24 17:03:03 +0100

    stet trait

--- spec/factories/job_applications.rb
+++ spec/factories/job_applications.rb
@@ -272,15 +272,15 @@ FactoryBot.define do
     end
   end
 
-  # trait :status_interviewing_with_pre_checks do
-  #   transient do
-  #     submitted_at { 4.days.ago }
-  #     interviewing_at { 2.days.ago }
-  #     status { :interviewing }
-  #     create_self_disclosure { true }
-  #     create_references { true }
-  #   end
-  # end
+  trait :status_interviewing_with_pre_checks do
+    transient do
+      submitted_at { 4.days.ago }
+      interviewing_at { 2.days.ago }
+      status { :interviewing }
+      create_self_disclosure { true }
+      create_references { true }
+    end
+  end
 
   trait :status_unsuccessful_interview do
     transient do
