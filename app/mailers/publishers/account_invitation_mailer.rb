130043993a9e7980095069acd10888030a4e86e2
Kyle MacPherson <kaimac2010@gmail.com>
2025-10-27 11:53:14 +0000

    Allow confirm contact email page to behave properly when edited

--- app/mailers/publishers/account_invitation_mailer.rb
+++ app/mailers/publishers/account_invitation_mailer.rb
@@ -4,7 +4,7 @@ class Publishers::AccountInvitationMailer < Publishers::BaseMailer
   def invite_user(contact_email:, publisher_email:)
     @contact_email = contact_email
     @publisher_email = publisher_email
-    @publisher = Publisher.find_by(email: publisher_email) # For analytics
+    @publisher = Publisher.find_by(email: publisher_email)
 
     @subject = I18n.t("publishers.account_invitation_mailer.invite_user.subject")
 
