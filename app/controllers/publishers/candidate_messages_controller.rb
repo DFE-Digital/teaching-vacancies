c655cb237b51d29225a9f23881e68a2f46de9337
Stephen Dicks <github@starswan.com>
2025-10-28 15:50:17 +0000

    message pagination with content changes

--- app/controllers/publishers/candidate_messages_controller.rb
+++ app/controllers/publishers/candidate_messages_controller.rb
@@ -9,8 +9,9 @@ class Publishers::CandidateMessagesController < Publishers::BaseController
 
     filtered_conversations = @tab == "archive" ? base_conversations.archived : base_conversations.inbox
 
-    @conversations = Search::CandidateMessagesSearch.new(@search_form.to_hash, sort: @sort, scope: filtered_conversations).conversations
-    @total_count = @conversations.count
+    conversations = Search::CandidateMessagesSearch.new(@search_form.to_hash, sort: @sort, scope: filtered_conversations).conversations
+    @total_count = conversations.count
+    @pagy, @conversations = pagy(conversations, items: 15)
 
     @inbox_count = Conversation.for_organisations(organisation_ids).inbox.with_unread_jobseeker_messages.count
   end
--- spec/views/publishers/candidate_messages/index.html.slim_spec.rb
+++ spec/views/publishers/candidate_messages/index.html.slim_spec.rb
@@ -5,13 +5,27 @@ RSpec.describe "publishers/candidate_messages/index" do
     assign :tab, "inbox"
     assign :search_form, Publishers::CandidateMessagesSearchForm.new
     assign :sort, Publishers::CandidateMessagesSort.new
-    assign :total_count, 1
-    assign :conversations, conversations
+
+    assign :total_count, 16
+    assign :conversations, conversations.first(15)
+    assign :pagy, Pagy.new(count: 15, page: 1, items: 15)
+
     assign :inbox_count, 1
 
-    allow(view).to receive_messages(current_organisation: organisation)
+    allow(view).to receive_messages(current_organisation: build_stubbed(:school))
 
     render
+  end
+
+  let(:conversations) { build_stubbed_list(:conversation, 16, last_message_at: Time.current) }
+
+  it "shows inbox tab with correct count" do
+    expect(rendered).to have_content("Inbox (1)")
+    expect(rendered).to have_content("Archive")
+  end
+
+  it "paginates all the conversations" do
+    expect(rendered).to have_content("Showing 1 to 15 of 16 conversations")
   end
 
   describe "reading messages" do
