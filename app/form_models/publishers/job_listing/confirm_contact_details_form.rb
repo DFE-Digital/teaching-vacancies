9f47f21f359cd7043b9db661cb35a93c9f961886
Marc Sardon <pringaohowto@gmail.com>
2025-10-31 09:44:07 +0000

    Refactor and test ConfirmContactDetailsForm

--- app/form_models/publishers/job_listing/confirm_contact_details_form.rb
+++ app/form_models/publishers/job_listing/confirm_contact_details_form.rb
@@ -1,27 +1,27 @@
 class Publishers::JobListing::ConfirmContactDetailsForm < Publishers::JobListing::VacancyForm
   include ActiveModel::Attributes
 
-  validates :confirm_contact_email, presence: true, if: -> { unconfirmed_non_publisher_email? }
   attr_accessor(:confirm_contact_email)
 
+  validates :confirm_contact_email, presence: true, if: -> { !@vacancy.contact_email_belongs_to_a_publisher? }
+
   class << self
-    # confirm_contact_email is not a value that we store, only used for confirmation of contact_email, and navigation purposes.
     def fields
       %i[confirm_contact_email]
     end
 
-    def load_form(_model)
-      {}
+    def load_form(model)
+      # If the step is recorded as completed means they confirmed previously.
+      if model.completed_steps.include?("confirm_contact_details")
+        { confirm_contact_email: true }
+      else
+        {}
+      end
     end
   end
 
+  # confirm_contact_email is not a value that we store, only used for confirmation of contact_email, and navigation purposes.
   def params_to_save
     {}
-  end
-
-  private
-
-  def unconfirmed_non_publisher_email?
-    !@vacancy.contact_email_belongs_to_a_publisher? && @vacancy.completed_steps.exclude?("confirm_contact_details")
   end
 end
