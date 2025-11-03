bd0252d5de5b0dde1851cc8d9e8860b5eb05c6d3
Kyle MacPherson <kaimac2010@gmail.com>
2025-11-03 16:12:41 +0000

    Add test to ensure confirm contact details flow works on editing of vacancy

--- spec/system/publishers/publishers_can_edit_a_published_vacancy_as_a_school_spec.rb
+++ spec/system/publishers/publishers_can_edit_a_published_vacancy_as_a_school_spec.rb
@@ -217,11 +217,19 @@ RSpec.describe "Publishers can edit a vacancy" do
       it "can be successfully edited and adds a job to update the Google index in the queue" do
         expect(publisher_contact_details_page).to be_displayed
 
-        expect_any_instance_of(Publishers::Vacancies::BaseController)
-          .to receive(:update_google_index).with(vacancy)
-
         choose I18n.t("helpers.label.publishers_job_listing_contact_details_form.contact_email_options.other")
-        fill_in "publishers_job_listing_contact_details_form[other_contact_email]", with: contact_email
+        fill_in "publishers_job_listing_contact_details_form[other_contact_email]", with: "not_a_publisher_email@contoso.com"
+        click_on I18n.t("buttons.save_and_continue")
+
+        expect(publisher_confirm_contact_details_page).to be_displayed
+
+        publisher_confirm_contact_details_page.click_change_email_button
+
+        expect(publisher_contact_details_page).to be_displayed
+
+        expect_any_instance_of(Publishers::Vacancies::BaseController).to receive(:update_google_index).with(vacancy)
+
+        choose publisher.email
         click_on I18n.t("buttons.save_and_continue")
 
         expect(publisher_vacancy_page).to be_displayed
