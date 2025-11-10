b579d72616a40535c496c5b8fa96c2e194d46d4b
Kyle MacPherson <kaimac2010@gmail.com>
2025-11-10 13:15:52 +0000

    Add test coverage

--- spec/models/self_disclosure_spec.rb
+++ spec/models/self_disclosure_spec.rb
@@ -48,4 +48,37 @@ RSpec.describe SelfDisclosure do
     it { expect(self_disclosure.name).to eq(job_application.name) }
     it { expect(self_disclosure.address_line_1).to eq(job_application.street_address) }
   end
+
+  describe "#mark_as_received" do
+    let(:organisation) { create(:school) }
+    let(:publisher) { create(:publisher) }
+    let(:vacancy) { create(:vacancy, contact_email: contact_email, publisher: publisher) }
+    let(:job_application) { create(:job_application, vacancy: vacancy) }
+    let(:self_disclosure_request) { create(:self_disclosure_request, job_application: job_application) }
+    let(:self_disclosure) { create(:self_disclosure, self_disclosure_request: self_disclosure_request) }
+
+    context "when there is a registered publisher user" do
+      let(:contact_email) { publisher.email }
+
+      it "sends a notification to the registered user" do
+        allow(Publishers::SelfDisclosureReceivedNotifier).to receive(:with).with(record: self_disclosure).and_return(instance_double(Publishers::SelfDisclosureReceivedNotifier, deliver: true))
+
+        self_disclosure.mark_as_received
+
+        expect(self_disclosure_request.reload.status).to eq("received")
+      end
+    end
+
+    context "when there is no registered publisher user" do
+      let(:contact_email) { "unregistered@contoso.com" }
+
+      it "sends an email via mailer" do
+        allow(Publishers::CollectReferencesMailer).to receive(:self_disclosure_received).with(job_application).and_return(instance_double(ActionMailer::MessageDelivery, deliver_later: true))
+
+        self_disclosure.mark_as_received
+
+        expect(self_disclosure_request.reload.status).to eq("received")
+      end
+    end
+  end
 end
