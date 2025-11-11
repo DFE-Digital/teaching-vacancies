8e04a772697dfcd02484765c31eabb0bf60de0c2
Kyle MacPherson <kaimac2010@gmail.com>
2025-11-11 13:49:13 +0000

    Allow users to tab through trix buttons on messages (#8260)
    Prior to this change users couldn't tab through the formatting buttons
    (bold/italic etc) on the messages text box. This change allows them to
    tab through them by removing the tab index of -1 which trix adds to
    these buttons and adds styling to help user see which button they are
    tabbing through, in line with the rest of the service.

--- app/assets/javascript/trix-url-normalizer.js
+++ app/assets/javascript/trix-url-normalizer.js
@@ -1,6 +1,23 @@
 export function initTrixURLNormalizer() {
   document.addEventListener('trix-initialize', () => {
     const trixToolbar = document.querySelector('trix-toolbar');
+
+    // Make toolbar buttons keyboard accessible by removing tabindex="-1"
+    const toolbarButtons = trixToolbar.querySelectorAll('button[tabindex="-1"]');
+    toolbarButtons.forEach((button) => {
+      button.removeAttribute('tabindex');
+      // Add keyboard activation support - use keyup to avoid conflicts with Trix
+      button.addEventListener('keyup', (e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          e.preventDefault();
+          e.stopPropagation();
+          // simulate mouse down/up events to trigger button functionality
+          button.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
+          button.dispatchEvent(new MouseEvent('mouseup', { bubbles: true }));
+        }
+      });
+    });
+
     const normalizeWebEmbedURL = () => {
       const url = urlInput.value.trim();
       // return when no value
