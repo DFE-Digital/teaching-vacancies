805ca8a9cafbdfb6a1a6623f13219b21ad2a4b3a
Stephen Dicks <github@starswan.com>
2025-11-17 11:10:56 +0000

    limit govuk notify sending to 3000 mails per minute (#8287)

--- app/mailers/govuk_notify_mailer.rb
+++ app/mailers/govuk_notify_mailer.rb
@@ -1,4 +1,8 @@
 class GovukNotifyMailer < Mail::Notify::Mailer
+  SIDEKIQ_WORKER_COUNT = 4
+  # This is the maximum rate we can send through production Govuk Notify
+  GOVUK_NOTIFY_SEND_LIMIT_PER_MINUTE = 3000
+
   helper NotifyViewsHelper
   include MailerAnalyticsEvents
 
@@ -8,6 +12,13 @@ class GovukNotifyMailer < Mail::Notify::Mailer
 
   # :nocov:
   self.delivery_method = :notify unless Rails.env.test?
+
+  # inspired by https://mattbrictson.com/blog/applying-a-rate-limit-in-sidekiq
+  extend Limiter::Mixin
+
+  # limit sending emails to 3000 per minute - but send in a burst, don't balance across the minute
+  # calculate as 3000 / worker_count to make it clear what's going on
+  limit_method :send_email, rate: GOVUK_NOTIFY_SEND_LIMIT_PER_MINUTE / SIDEKIQ_WORKER_COUNT, balanced: false
 
   def send_email(to:, subject:)
     @to = to
