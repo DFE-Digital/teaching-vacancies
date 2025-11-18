1830258bd31bbd89225965f60068d18d89a6c653
Kyle MacPherson <kaimac2010@gmail.com>
2025-11-18 15:19:38 +0000

    Fix tests

--- spec/lib/tasks/migrate_personal_statement_to_content_spec.rb
+++ spec/lib/tasks/migrate_personal_statement_to_content_spec.rb
@@ -5,16 +5,14 @@ RSpec.describe "migrate_personal_statement_to_content" do
 
   include_context "rake"
 
-  let!(:job_application_with_statement) do
-    create(:job_application, personal_statement: "This is my personal statement")
-  end
+  let(:job_application_with_statement_but_no_content) { create(:job_application, personal_statement: "This is my personal statement") }
 
   before do
     create(:job_application, personal_statement: "Some statement").tap do |ja|
       ja.update!(content: "Some statement")
     end
     create(:job_application, personal_statement: "")
-
+    job_application_with_statement_but_no_content.content.destroy!
     allow(MigratePersonalStatementBatchJob).to receive(:perform_later)
   end
 
@@ -22,7 +20,7 @@ RSpec.describe "migrate_personal_statement_to_content" do
     rake_task.invoke
 
     expect(MigratePersonalStatementBatchJob).to have_received(:perform_later)
-      .with([job_application_with_statement.id])
+      .with([job_application_with_statement_but_no_content.id])
       .once
   end
 end
