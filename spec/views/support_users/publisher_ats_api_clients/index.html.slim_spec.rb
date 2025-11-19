88e78cd2ce882d4fd39c902c0c92668c7aba8f4c
Marc Sardon <pringaohowto@gmail.com>
2025-11-19 18:23:50 +0000

    Adapt view test to DB querying

--- spec/views/support_users/publisher_ats_api_clients/index.html.slim_spec.rb
+++ spec/views/support_users/publisher_ats_api_clients/index.html.slim_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "support_users/publisher_ats_api_clients/index" do
-  let(:api_client) { build_stubbed(:publisher_ats_api_client, vacancies: build_stubbed_list(:vacancy, 2)) }
+  let(:organisation) { create(:school) }
+  let(:vacancy_one) { create(:vacancy, :external, organisations: [organisation], created_at: 2.days.ago) }
+  let(:vacancy_two) { create(:vacancy, :external, organisations: [organisation], created_at: 1.day.ago) }
+  let(:api_client) { create(:publisher_ats_api_client, vacancies: [vacancy_one, vacancy_two], created_at: 3.days.ago) }
 
   before do
     assign :api_clients, [api_client]
@@ -13,9 +16,9 @@ RSpec.describe "support_users/publisher_ats_api_clients/index" do
     expect(rendered.html.css("td").map(&:text))
       .to eq(
         ["Big MAT ATS",
-         api_client.created_at.to_fs,
-         "0",
+         vacancy_one.created_at.to_fs,
          "2",
+         "1",
          api_client.created_at.to_fs,
          " 6 November 2024 2:31pm"],
       )
