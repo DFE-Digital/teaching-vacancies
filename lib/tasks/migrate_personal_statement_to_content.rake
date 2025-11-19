8f0d92c41386fbcd431141a3a65d0ac3ab704edc
Kyle MacPherson <kaimac2010@gmail.com>
2025-11-19 10:30:46 +0000

    Fix test coverage

--- lib/tasks/migrate_personal_statement_to_content.rake
+++ lib/tasks/migrate_personal_statement_to_content.rake
@@ -7,6 +7,4 @@ task migrate_personal_statement_to_content: :environment do
   scope.find_in_batches(batch_size: 1000) do |batch|
     MigratePersonalStatementBatchJob.perform_later(batch.pluck(:id))
   end
-
-  puts "Migration jobs queued"
 end
--- spec/lib/tasks/migrate_personal_statement_to_content_spec.rb
+++ spec/lib/tasks/migrate_personal_statement_to_content_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "migrate_personal_statement_to_content" do
-  subject(:rake_task) { rake[task_name] }
-
   include_context "rake"
+
+  let(:task_name) { "migrate_personal_statement_to_content" }
 
   let(:job_application_with_statement_but_no_content) { create(:job_application, personal_statement: "This is my personal statement") }
 
@@ -15,12 +15,14 @@ RSpec.describe "migrate_personal_statement_to_content" do
     job_application_with_statement_but_no_content.content.destroy!
     allow(MigratePersonalStatementBatchJob).to receive(:perform_later)
   end
-
+  
+  # rubocop:disable RSpec/NamedSubject
   it "queues migration jobs only for applications with personal_statement but no content" do
-    rake_task.invoke
+    subject.invoke
 
     expect(MigratePersonalStatementBatchJob).to have_received(:perform_later)
       .with([job_application_with_statement_but_no_content.id])
       .once
   end
+  # rubocop:enable RSpec/NamedSubject
 end
