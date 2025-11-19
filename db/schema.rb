d94b42f0132224529e6455e16bb892ece4fb4af4
Kyle MacPherson <kaimac2010@gmail.com>
2025-11-19 15:48:23 +0000

    drop unencrypted column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_19_142650) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_19_154619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "citext"
@@ -23,7 +23,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_19_142650) do
 
   create_table "action_text_rich_texts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
-    t.text "body"
     t.string "record_type", null: false
     t.uuid "record_id", null: false
     t.datetime "created_at", null: false
