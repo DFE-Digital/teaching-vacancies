1bb92e166f5fb01eb73dfc4907b3da0b714b4f51
Stephen Dicks <github@starswan.com>
2025-11-20 15:56:35 +0000

    support user fallback test tweaks

--- spec/system/support_users/fallback_sign_in_spec.rb
+++ spec/system/support_users/fallback_sign_in_spec.rb
@@ -3,21 +3,26 @@ require "rails_helper"
 RSpec.describe "Fallback Support User sign in" do
   include ActiveJob::TestHelper
 
-  let!(:support_user) { create(:support_user, email: "rachael@example.com") }
+  let(:support_user) { create(:support_user, email: "rachael@example.com") }
 
   before do
     allow(AuthenticationFallback).to receive(:enabled?).and_return(true)
   end
 
-  it "allows support users to sign in" do
-    visit new_support_user_session_path
-    fill_in "support_user[email]", with: "rachael@example.com"
+  it "allows support users to sign in", :perform_enqueued do
+    visit support_user_root_path
+    fill_in "support_user[email]", with: support_user.email
 
-    expect { perform_enqueued_jobs { click_on "Submit" } }
+    expect { click_on "Submit" }
       .to change { delivered_emails.count }
       .by(1)
 
-    visit first_link_from_last_mail
+    # make email link work even when :js is enabled for debugging
+    if page.server
+      visit first_link_from_last_mail.gsub("3000", page.server.port.to_s)
+    else
+      visit first_link_from_last_mail
+    end
     expect(current_path).to eq(support_user_root_path)
   end
 end
