c65d56cf3bcc034a11b4b74b2872e55cbed183af
Stephen Dicks <github@starswan.com>
2025-11-24 15:52:53 +0000

    move to after commit hook

--- config/initializers/active_job.rb
+++ config/initializers/active_job.rb
@@ -1,10 +1,8 @@
-module ActiveJob
-  class Base
-    # Controls whether Active Job's `#perform_later` and similar methods automatically defer
-    # the job queuing to after the current Active Record transaction is committed.
-    # Moved here in Rails 8.0 as global setting was deprecated.
-    # Needed here to cover sending emails inside a transaction (which inherits from ActiveJob::Base)
-    # but also probably useful for the Notification gem which might have similar issues
-    self.enqueue_after_transaction_commit = true
-  end
+# Controls whether Active Job's `#perform_later` and similar methods automatically defer
+# the job queuing to after the current Active Record transaction is committed.
+# Moved here in Rails 8.0 as global setting was deprecated.
+# Needed here to cover sending emails inside a transaction (which inherits from ActiveJob::Base)
+# but also probably useful for the Notification gem which might have similar issues
+Rails.application.config.after_initialize do
+  ActiveJob::Base.enqueue_after_transaction_commit = true
 end
