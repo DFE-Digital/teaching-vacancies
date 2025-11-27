221472a9bbd39e61c14018af02807087f7c8a1c9
Kyle MacPherson <kaimac2010@gmail.com>
2025-11-27 09:15:59 +0000

    initial commit to create deploy app so we can create messages and ensure that previously stored messages still work as expected after change

--- spec/lib/tasks/migrate_lockbox_action_text_rake_task_spec.rb
+++ spec/lib/tasks/migrate_lockbox_action_text_rake_task_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "lockbox:migrate_action_text" do
 
   let(:task_path) { "lib/tasks/migrate_lockbox_action_text" }
 
+  
   # rubocop:disable RSpec/NamedSubject
   it "calls Lockbox.migrate with ActionText::RichText" do
     expect(Lockbox).to receive(:migrate).with(ActionText::RichText)
