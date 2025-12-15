c97ade83cef2d6832d377ce50bd774831dab8d26
Marc Sardon <pringaohowto@gmail.com>
2025-12-15 17:49:27 +0000

    Bugfix: Notification loading before authentication
    When the jobseekers try to access their notifications without being signed in,
    the code tries to load the notifications for the user, but the user is
    still not set and it raises an exception.
    
    Fixed it by ensuring the authentication is prepended before any other
    before action hook. And add some request specs to cover the behaviour.

--- spec/requests/publishers/notifications_spec.rb
+++ spec/requests/publishers/notifications_spec.rb
@@ -6,23 +6,32 @@ RSpec.describe "Publisher notifications" do
   let(:vacancy) { create(:vacancy, organisations: [organisation]) }
   let(:job_application) { create(:job_application, vacancy: vacancy) }
 
-  before do
-    Publishers::JobApplicationReceivedNotifier.with(vacancy: vacancy, job_application: job_application).deliver(publisher)
-    allow_any_instance_of(Publishers::NotificationsController).to receive(:current_organisation).and_return(organisation)
-    sign_in(publisher, scope: :publisher)
-  end
-
-  after { sign_out(publisher) }
-
   describe "GET #index" do
-    it "renders the index page" do
-      expect(get(publishers_notifications_path)).to render_template(:index)
+    context "when signed in" do
+      before do
+        Publishers::JobApplicationReceivedNotifier.with(vacancy: vacancy, job_application: job_application).deliver(publisher)
+        allow_any_instance_of(Publishers::NotificationsController).to receive(:current_organisation).and_return(organisation)
+        sign_in(publisher, scope: :publisher)
+      end
+
+      after { sign_out(publisher) }
+
+      it "renders the index page" do
+        expect(get(publishers_notifications_path)).to render_template(:index)
+      end
+
+      it "marks notifications as read" do
+        notification = publisher.notifications.first
+        freeze_time do
+          expect { get publishers_notifications_path }.to change { notification.reload.read_at }.from(nil).to(Time.current)
+        end
+      end
     end
 
-    it "marks notifications as read" do
-      notification = publisher.notifications.first
-      freeze_time do
-        expect { get publishers_notifications_path }.to change { notification.reload.read_at }.from(nil).to(Time.current)
+    context "when signed out" do
+      it "redirects to the publisher sign in page" do
+        get publishers_notifications_path
+        expect(response).to redirect_to(new_publisher_session_path(redirected: true))
       end
     end
   end
