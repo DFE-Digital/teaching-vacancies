8d688c1442edd5de0efdb575c6ba7dbffef98f2b
Stephen Dicks <github@starswan.com>
2026-01-15 14:59:54 +0000

    rubocop reversible

--- db/migrate/20260115140453_change_subscription_uk_area_type.rb
+++ db/migrate/20260115140453_change_subscription_uk_area_type.rb
@@ -4,7 +4,7 @@ class ChangeSubscriptionUkAreaType < ActiveRecord::Migration[8.0]
   def change
     add_column :subscriptions, :new_uk_area, :geometry, srid: 27_700
     add_index :subscriptions, :new_uk_area, using: :gist, algorithm: :concurrently
-    remove_index :subscriptions, name: :index_subscriptions_on_uk_area, algorithm: :concurrently
+    remove_index :subscriptions, :uk_area, algorithm: :concurrently
     safety_assured do
       remove_column :subscriptions, :uk_area, :geometry, srid: 27_700
       rename_column :subscriptions, :new_uk_area, :uk_area
