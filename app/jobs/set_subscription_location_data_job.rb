7a807bd4b0ff69d58e84959c5e80518705e19f2b
Marc Sardon <pringaohowto@gmail.com>
2026-01-16 12:45:12 +0000

    Set specific Sidekiq queues for each purpose
    Better granular control of each queue purpose, priority and concurrency
    limits.

--- app/jobs/set_subscription_location_data_job.rb
+++ app/jobs/set_subscription_location_data_job.rb
@@ -1,7 +1,7 @@
 class SetSubscriptionLocationDataJob < ApplicationJob
   # Uses dbintensive queue as it causes very High DB load when many subscriptions are updated/created at once
   # and we want to limit how many of these jobs run at the same time (see config/sidekiq.yml)
-  queue_as :verylow
+  queue_as :dbintensive
 
   # Extracted as a job to avoid slowing down subscription creation
   # by making potentially slow external API calls or database geometry operations.
