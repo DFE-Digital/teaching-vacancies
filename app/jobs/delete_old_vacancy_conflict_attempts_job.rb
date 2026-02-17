441fa6b9ab1c7f69119f54930f666c5e525cef65
Kyle MacPherson <kaimac2010@gmail.com>
2026-02-17 13:52:04 +0000

    Add job to delete vacancy conflicts every september if they are older than 13 months

---
+++ app/jobs/delete_old_vacancy_conflict_attempts_job.rb
@@ -0,0 +1,8 @@
+class DeleteOldVacancyConflictAttemptsJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    # Delete records older than 13 months (September to September retention period)
+    VacancyConflictAttempt.where("created_at <= ?", 13.months.ago).delete_all
+  end
+end
