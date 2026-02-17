3a5882938f99b711eb0b73d40bc50cb61e17554d
Stephen Dicks <github@starswan.com>
2026-02-17 13:19:13 +0000

    only avoid updating searchable_content when vacancy is expired (#8532)
    ## Changes in this PR:
    
    Only avoid update_searchable_content when vacancy has expired

--- spec/services/publish_vacancy_spec.rb
+++ spec/services/publish_vacancy_spec.rb
@@ -3,25 +3,28 @@ require "rails_helper"
 RSpec.describe PublishVacancy do
   let(:organisation) { create(:school) }
   let(:user) { create(:publisher) }
-  let(:vacancy) { create(:draft_vacancy, publisher: nil) }
+  let(:vacancy) { create(:draft_vacancy, publisher: nil, searchable_content: nil) }
+  let(:published_vacancy) { Vacancy.find(vacancy.id) }
 
   describe "#call" do
-    it "updates the vacancy's status to published" do
+    before do
       PublishVacancy.new(vacancy, user, organisation).call
+    end
 
+    it "updates the vacancy's status to published" do
       expect(vacancy.type).to eq("PublishedVacancy")
     end
 
     it "updates the id of the user who confirmed the publishing of a vacancy" do
-      PublishVacancy.new(vacancy, user, organisation).call
-
-      expect(Vacancy.find(vacancy.id).publisher_id).to eq(user.id)
+      expect(published_vacancy.publisher_id).to eq(user.id)
     end
 
     it "updates the id of the organisation of the user who confirmed the publishing of a vacancy" do
-      PublishVacancy.new(vacancy, user, organisation).call
+      expect(published_vacancy.publisher_organisation_id).to eq(organisation.id)
+    end
 
-      expect(Vacancy.find(vacancy.id).publisher_organisation_id).to eq(organisation.id)
+    it "updates the searchable content" do
+      expect(published_vacancy.searchable_content).not_to be_nil
     end
   end
 end
