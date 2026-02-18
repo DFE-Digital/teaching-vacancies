896547b1428c1bea407aabc0cb77f803ee3db86c
Stephen Dicks <github@starswan.com>
2026-02-18 17:37:07 +0000

    linter

--- spec/lib/tasks/migrate_legacy_job_preferences_task_spec.rb
+++ spec/lib/tasks/migrate_legacy_job_preferences_task_spec.rb
@@ -7,11 +7,13 @@ RSpec.describe "migrate_legacy_job_preferences" do
   let!(:pref_b) { create(:job_preferences, working_patterns: %w[flexible full_time]) }
   let!(:pref_c) { create(:job_preferences, working_patterns: %w[term_time]) }
 
+  # rubocop:disable RSpec/NamedSubject
   before do
     create(:job_preferences)
     create(:job_preferences, working_patterns: nil)
     subject.invoke
   end
+  # rubocop:enable RSpec/NamedSubject
 
   it "fixes up term time flexible" do
     expect(pref_a.reload.working_patterns).to match_array(%w[full_time part_time])
