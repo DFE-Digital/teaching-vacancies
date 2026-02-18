4cb12235c05a319116a6179a6e6a19ce46d08b5f
Stephen Dicks <github@starswan.com>
2026-02-18 14:06:19 +0000

    stet

--- app/form_models/publishers/job_listing/about_the_role_form.rb
+++ app/form_models/publishers/job_listing/about_the_role_form.rb
@@ -1,7 +1,7 @@
 class Publishers::JobListing::AboutTheRoleForm < Publishers::JobListing::VacancyForm
   include ActiveModel::Attributes
 
-  validates :ect_status, inclusion: { in: Vacancy.ect_statuses.keys }, if: -> { vacancy.job_roles&.include?("teacher") }
+  validates :ect_status, inclusion: { in: Vacancy.ect_statuses.keys }, if: -> { vacancy&.job_roles&.include?("teacher") }
   validate :skills_and_experience_presence
   validate :school_offer_presence
   validates :further_details_provided, inclusion: { in: [true, false] }
