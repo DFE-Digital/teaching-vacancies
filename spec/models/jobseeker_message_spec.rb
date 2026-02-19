6ce45fb88a27b621861bce6c4becb45aaacf1c4c
Stephen Dicks <github@starswan.com>
2026-02-19 11:22:06 +0000

    Only validate message type on create (#8525)

--- spec/models/jobseeker_message_spec.rb
+++ spec/models/jobseeker_message_spec.rb
@@ -1,12 +1,13 @@
 require "rails_helper"
 
 RSpec.describe JobseekerMessage do
-  let(:conversation) { build(:conversation) }
+  let(:conversation) { build(:conversation, job_application: job_application) }
   let(:jobseeker) { build(:jobseeker) }
 
   describe "validations" do
     context "without a sender" do
-      let(:message)  { build(:jobseeker_message, conversation: conversation, sender: nil) }
+      let(:job_application) { build(:job_application) }
+      let(:message) { build(:jobseeker_message, conversation: conversation, sender: nil) }
 
       it "has a strict validation" do
         expect { message.valid? }.to raise_error(ActiveModel::StrictValidationFailed, "Sender is mandatory")
@@ -20,26 +21,32 @@ RSpec.describe JobseekerMessage do
 
       let(:job_application) { instance_double(JobApplication) }
 
-      context "when jobseeker can send message" do
-        before do
-          allow(job_application).to receive(:can_jobseeker_send_message?).and_return(true)
-        end
+      context "when jobseeker can initiate a message" do
+        let(:job_application) { create(:job_application, :status_interviewing) }
+        let(:message) { build(:jobseeker_message, conversation: conversation, sender: jobseeker) }
 
         it "allows message creation" do
-          message = build(:jobseeker_message, conversation: conversation, sender: jobseeker)
-
           expect(message).to be_valid
+        end
+
+        context "when application is subsequently withdrawn" do
+          before do
+            message.save!
+            job_application.assign_attributes(status: "withdrawn")
+            job_application.save!(validate: false)
+          end
+
+          it "stays valid" do
+            expect(message).to be_valid
+          end
         end
       end
 
-      context "when jobseeker cannot send message" do
-        before do
-          allow(job_application).to receive(:can_jobseeker_send_message?).and_return(false)
-        end
+      context "when jobseeker cannot initiate a message" do
+        let(:job_application) { create(:job_application, :status_withdrawn) }
+        let(:message) { build(:jobseeker_message, conversation: conversation, sender: jobseeker) }
 
         it "prevents message creation with validation error" do
-          message = build(:jobseeker_message, conversation: conversation, sender: jobseeker)
-
           expect(message).not_to be_valid
           expect(message.errors[:base]).to include("Cannot send message for this job application status")
         end
