1160f3fcc7266d99c19da707cd54a1b2f54c2f73
Stephen Dicks <github@starswan.com>
2026-02-19 08:54:32 +0000

    Update app/jobs/update_single_dsi_user_in_db_job.rb
    Co-authored-by: Marc Sardón <marc.sardon@education.gov.uk>

--- app/jobs/update_single_dsi_user_in_db_job.rb
+++ app/jobs/update_single_dsi_user_in_db_job.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UpdateSingleDSIUserInDbJob < ApplicationJob
+  queue_as :low
+
   def perform(dsi_user)
     Publishers::DfeSignIn::UpdateUsersInDb.convert_to_user(dsi_user)
   end
