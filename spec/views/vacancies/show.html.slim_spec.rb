b0935c22c972ee420495cb193df44289560cb80d
Kyle MacPherson <kaimac2010@gmail.com>
2026-02-20 10:17:51 +0000

    Add link to personal statement tool (#8399)
    ## Trello card URL
    
    
    https://trello.com/c/mM286qCi/2433-build-initial-testing-integration-with-external-personal-statement-tool
    
    ## Changes in this PR:
    
    This PR adds a link to the personal statement builder
    
    ## Screenshots of UI changes:
    job page:
    <img width="1064" height="404" alt="Screenshot 2026-01-29 at 16 53 51"
    src="https://github.com/user-attachments/assets/6686b295-f39c-4bc0-b2eb-67731ff02be0"
    />
    
    
    job application page:
    <img width="997" height="305" alt="Screenshot 2026-01-29 at 16 50 59"
    src="https://github.com/user-attachments/assets/cec0fef3-b9e1-4a3d-99b1-b9e38cdc353d"
    />
    
    ## Checklists:
    
    ### Data & Schema Changes
    
    If this PR modifies data structures or validations, check the following:
    
    - [ ] Adds/removes model validations
    - [ ] Adds/removes database fields
    - [ ] Modifies Vacancy enumerables (phases, working patterns, job roles,
    key stages, etc.)
    
    <details>
    <summary>If any of the above options has changed then the author must
    check/resolve all of the following...</summary>
    
    ### Integration Impact
    
    Does this change affect any of these integrations?
    - [ ] DfE Analytics platform
    - [ ] Legacy imports mappings
    - [ ] DWP Find a Job export mappings
    - [ ] Publisher ATS API (may require mapping updates or API versioning)
    
    ### User Experience & Data Integrity
    
    Could this change impact:
    - [ ] Existing subscription alerts (will legacy subscription search
    filters break?)
    - [ ] Legacy vacancy copying (will copied vacancies fail new
    validations?)
    - [ ] In-progress drafts for Vacancies or Job Applications
    </details>

--- spec/views/vacancies/show.html.slim_spec.rb
+++ spec/views/vacancies/show.html.slim_spec.rb
@@ -37,4 +37,42 @@ RSpec.describe "vacancies/show" do
       end
     end
   end
+
+  describe "personal statement help" do
+    context "when vacancy is ect_suitable but does not have enable_job_applications" do
+      let(:vacancy) { create(:vacancy, ect_status: :ect_suitable, enable_job_applications: false) }
+
+      it "renders the personal statement help section" do
+        expect(rendered).to have_css(".sidebar-info-box")
+        expect(rendered).to have_content(I18n.t("jobs.personal_statement_help.heading"))
+      end
+    end
+
+    context "when vacancy has enable_job_applications but is not ect_suitable" do
+      let(:vacancy) { create(:vacancy, ect_status: :ect_unsuitable, enable_job_applications: true) }
+
+      it "renders the personal statement help section" do
+        expect(rendered).to have_css(".sidebar-info-box")
+        expect(rendered).to have_content(I18n.t("jobs.personal_statement_help.heading"))
+      end
+    end
+
+    context "when vacancy is ect_suitable and has enable_job_applications" do
+      let(:vacancy) { create(:vacancy, ect_status: :ect_suitable, enable_job_applications: true) }
+
+      it "renders the personal statement help section" do
+        expect(rendered).to have_css(".sidebar-info-box")
+        expect(rendered).to have_content(I18n.t("jobs.personal_statement_help.heading"))
+      end
+    end
+
+    context "when vacancy is not ect_suitable and does not have enable_job_applications" do
+      let(:vacancy) { create(:vacancy, ect_status: :ect_unsuitable, enable_job_applications: false) }
+
+      it "does not render the personal statement help section" do
+        expect(rendered).to have_no_css(".sidebar-info-box")
+        expect(rendered).to have_no_content(I18n.t("jobs.personal_statement_help.heading"))
+      end
+    end
+  end
 end
