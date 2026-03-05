27bfb9da18a44ac8002e236d0edf292f9de34675
Kyle MacPherson <kaimac2010@gmail.com>
2026-03-05 14:51:55 +0000

    Refactor tests

---
+++ spec/lib/tasks/db_migrate_ignore_concurrent_migration_exceptions_rake_task_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+# rubocop:disable RSpec/NamedSubject
+RSpec.describe "db:migrate:ignore_concurrent_migration_exceptions" do
+  include_context "rake"
+
+  let(:task_path) { "lib/tasks/migrate_swallowing_concurrent_migration_exceptions" }
+
+  before do
+    Rake::Task.define_task("db:migrate")
+  end
+
+  it "invokes db:migrate" do
+    allow(Rake::Task["db:migrate"]).to receive(:invoke)
+
+    subject.invoke
+
+    expect(Rake::Task["db:migrate"]).to have_received(:invoke)
+  end
+
+  it "swallows ActiveRecord::ConcurrentMigrationError" do
+    allow(Rake::Task["db:migrate"]).to receive(:invoke).and_raise(ActiveRecord::ConcurrentMigrationError)
+
+    expect { subject.invoke }.not_to raise_error
+  end
+end
+# rubocop:enable RSpec/NamedSubject
