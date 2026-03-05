2902d2118d833858df8bbe9d30df0a883d7ec563
Kyle MacPherson <kaimac2010@gmail.com>
2026-03-05 14:30:09 +0000

    Fix tests

--- spec/lib/tasks/migrate_swallowing_concurrent_migration_exceptions_rake_task_spec.rb
+++ spec/lib/tasks/migrate_swallowing_concurrent_migration_exceptions_rake_task_spec.rb
@@ -1,41 +1,53 @@
 require "rails_helper"
 
-RSpec.describe "db:migrate:ignore_concurrent_migration_exceptions" do
-  include_context "rake"
+RSpec.describe "migrate_swallowing_concurrent_migration_exceptions" do
+  # rubocop:disable RSpec/NamedSubject
+  describe "db:migrate:ignore_concurrent_migration_exceptions" do
+    include_context "rake"
 
-  let(:task_path) { "lib/tasks/migrate_swallowing_concurrent_migration_exceptions" }
+    let(:task_path) { "lib/tasks/migrate_swallowing_concurrent_migration_exceptions" }
 
-  it "invokes db:migrate and swallows concurrent migration errors" do
-    allow(Rake::Task["db:migrate"]).to receive(:invoke)
+    before do
+      Rake::Task.define_task("db:migrate")
+    end
 
-    subject.invoke
+    it "invokes db:migrate" do
+      allow(Rake::Task["db:migrate"]).to receive(:invoke)
 
-    expect(Rake::Task["db:migrate"]).to have_received(:invoke)
+      subject.invoke
+
+      expect(Rake::Task["db:migrate"]).to have_received(:invoke)
+    end
+
+    it "swallows ActiveRecord::ConcurrentMigrationError" do
+      allow(Rake::Task["db:migrate"]).to receive(:invoke).and_raise(ActiveRecord::ConcurrentMigrationError)
+
+      expect { subject.invoke }.not_to raise_error
+    end
   end
 
-  it "swallows ActiveRecord::ConcurrentMigrationError" do
-    allow(Rake::Task["db:migrate"]).to receive(:invoke).and_raise(ActiveRecord::ConcurrentMigrationError)
+  describe "db:prepare:ignore_concurrent_migration_exceptions" do
+    include_context "rake"
 
-    expect { subject.invoke }.not_to raise_error
+    let(:task_path) { "lib/tasks/migrate_swallowing_concurrent_migration_exceptions" }
+
+    before do
+      Rake::Task.define_task("db:prepare")
+    end
+
+    it "invokes db:prepare" do
+      allow(Rake::Task["db:prepare"]).to receive(:invoke)
+
+      subject.invoke
+
+      expect(Rake::Task["db:prepare"]).to have_received(:invoke)
+    end
+
+    it "swallows ActiveRecord::ConcurrentMigrationError" do
+      allow(Rake::Task["db:prepare"]).to receive(:invoke).and_raise(ActiveRecord::ConcurrentMigrationError)
+
+      expect { subject.invoke }.not_to raise_error
+    end
   end
-end
-
-RSpec.describe "db:prepare:ignore_concurrent_migration_exceptions" do
-  include_context "rake"
-
-  let(:task_path) { "lib/tasks/migrate_swallowing_concurrent_migration_exceptions" }
-
-  it "invokes db:prepare and swallows concurrent migration errors" do
-    allow(Rake::Task["db:prepare"]).to receive(:invoke)
-
-    subject.invoke
-
-    expect(Rake::Task["db:prepare"]).to have_received(:invoke)
-  end
-
-  it "swallows ActiveRecord::ConcurrentMigrationError" do
-    allow(Rake::Task["db:prepare"]).to receive(:invoke).and_raise(ActiveRecord::ConcurrentMigrationError)
-
-    expect { subject.invoke }.not_to raise_error
-  end
+  # rubocop:enable RSpec/NamedSubject
 end
