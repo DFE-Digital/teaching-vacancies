8a1587c3324cf0a466c94cc2c0a30e71643d541b
Stephen Dicks <github@starswan.com>
2026-03-06 15:26:32 +0000

    m,erge

--- spec/factories/job_preferences.rb
+++ spec/factories/job_preferences.rb
@@ -31,13 +31,6 @@ FactoryBot.define do
       locations { [build(:job_preferences_location, name: Faker::Address.postcode, radius: Faker::Number.between(from: 1, to: 200), job_preferences: instance)] }
     end
 
-    trait :for_seed_data do
-      working_patterns { factory_rand_sample(Vacancy.working_patterns.keys, 1..2) }
-      roles { factory_rand_sample(Vacancy.job_roles.keys, 1..Vacancy.job_roles.keys.count) }
-      phases { factory_rand_sample(Vacancy.phases.keys, 1..Vacancy.phases.keys.count) }
-      subjects { factory_rand_sample(SUBJECT_OPTIONS.map(&:first), 1..3) }
-    end
-
     trait :incomplete do
       builder_completed { false }
       completed_steps { {} }
