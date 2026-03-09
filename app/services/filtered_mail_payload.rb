c9047e45aa8c52c0920a2a4c0c985dad2cc16870
Kyle MacPherson <kaimac2010@gmail.com>
2026-03-09 14:16:57 +0000

    Test filtering

--- app/services/filtered_mail_payload.rb
+++ app/services/filtered_mail_payload.rb
@@ -23,7 +23,7 @@ class FilteredMailPayload
       h[:date]               = @parameter_filter.filter_param("mailer.date", date)
       h[:duration]           = @parameter_filter.filter_param("mailer.duration", @event.duration.round(2)) if log_duration?
 
-      # Serialise args to ensure ActiveRecord objects are converted to hashes before filtering
+      # Serialise args to ensure ActiveRecord objects are converted to hashes before filtering otherwise filtering won't work.
       serialised_args = @event.payload[:args]&.map do |arg|
         arg.is_a?(ApplicationRecord) ? arg.attributes : arg
       end
