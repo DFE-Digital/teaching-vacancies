bd41b060a776564d38cd4028e607f113968a084d
Stephen Dicks <github@starswan.com>
2026-03-11 14:00:39 +0000

    wip

--- app/controllers/publishers/vacancies_controller.rb
+++ app/controllers/publishers/vacancies_controller.rb
@@ -1,5 +1,5 @@
 class Publishers::VacanciesController < Publishers::Vacancies::WizardBaseController
-  before_action :set_vacancy, except: %i[index]
+  before_action :set_vacancy, except: %i[index start]
 
   before_action :invent_job_alert_search_criteria, only: %i[show preview]
   before_action :redirect_to_new_features_reminder, only: %i[create]
