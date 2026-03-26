b4bab5783aa3ba6a2d4e78cefbe602b91cbdfc48
Stephen Dicks <github@starswan.com>
2026-03-26 14:21:18 +0000

    sidekiq upgrade

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,20 +6,20 @@ options = {
 
 # Redis concurrency must be plus 5 https://github.com/mperham/sidekiq/wiki/Using-Redis#complete-control
 Sidekiq.configure_server do |config|
-  config.merge!(options)
+  config[:concurrency] = options.fetch(:concurrency)
   config.logger.level = Logger::WARN
-  config.redis = { url: Rails.configuration.redis_queue_url, network_timeout: 5, size: config.options[:concurrency] + 5 }
+  config.redis = { url: Rails.configuration.redis_queue_url, network_timeout: 5, size: config[:concurrency] + 5 }
 end
 
 Sidekiq.configure_client do |config|
-  config.merge!(options)
-  config.redis = { url: Rails.configuration.redis_queue_url, network_timeout: 5, size: config.options[:concurrency] + 5 }
+  config[:concurrency] = options.fetch(:concurrency)
+  config.redis = { url: Rails.configuration.redis_queue_url, network_timeout: 5, size: config[:concurrency] + 5 }
 end
 
-schedule_file = "config/schedule.yml"
-
-if File.exist?(schedule_file) && Sidekiq.server?
-  Rails.application.config.after_initialize do
-    Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
-  end
-end
+# schedule_file = "config/schedule.yml"
+#
+# if File.exist?(schedule_file) && Sidekiq.server?
+#   Rails.application.config.after_initialize do
+#     Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
+#   end
+# end
