28e30e5ed39a9fd9dd7d4b2c1403ef06c0fa5e4f
Stephen Dicks <github@starswan.com>
2026-04-10 10:47:58 +0100

    add tests for new validations

--- spec/form_models/jobseekers/job_preferences_form_spec.rb
+++ spec/form_models/jobseekers/job_preferences_form_spec.rb
@@ -12,4 +12,22 @@ RSpec.describe Jobseekers::JobPreferencesForm, type: :model do
                                                            locations: :locations })
     end
   end
+
+  describe "validations on roles form" do
+    let(:roles) { Jobseekers::JobPreferencesForm::RolesForm.new(roles: %w[a b c d e]) }
+
+    it "has a custom error for too many roles" do
+      expect(roles).not_to be_valid
+      expect(roles.errors.messages).to eq({ roles: ["You can only select a maximum of 4 roles"] })
+    end
+  end
+
+  describe "validations on key stages form" do
+    let(:key_stages) { Jobseekers::JobPreferencesForm::KeyStagesForm.new(key_stages: %w[a b c d]) }
+
+    it "has a custom error for too many roles" do
+      expect(key_stages).not_to be_valid
+      expect(key_stages.errors.messages).to eq({ key_stages: ["You can only select a maximum of 3 key stages"] })
+    end
+  end
 end
