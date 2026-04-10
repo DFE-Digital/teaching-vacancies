0310b9faf9f6891a77cfa27b0d446e9dd018a3a8
Stephen Dicks <github@starswan.com>
2026-04-10 16:11:59 +0100

    remove custom religious sections

--- app/components/job_application_review_component/non_catholic_religious_information_section.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class JobApplicationReviewComponent::NonCatholicReligiousInformationSection < JobApplicationReviewComponent::Section
-  def initialize(job_application)
-    super(job_application, :non_catholic, forms: %w[NonCatholicForm])
-  end
-end
