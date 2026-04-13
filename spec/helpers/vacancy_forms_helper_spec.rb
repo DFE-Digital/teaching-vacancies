bafde99c33375819e2fac03c316a297dc5a37c52
Stephen Dicks <github@starswan.com>
2026-04-13 10:13:46 +0100

    Interstitials: Job Title page (school) (#8689)

--- spec/helpers/vacancy_forms_helper_spec.rb
+++ spec/helpers/vacancy_forms_helper_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe VacancyFormsHelper do
   describe "#vacancy_job_title_form_hint_text" do
-    let(:vacancy) { build(:vacancy, job_roles: [job_role], phases: [phase]) }
+    let(:vacancy) { build_stubbed(:vacancy, job_roles: [job_role], phases: [phase]) }
 
     context "with teaching role" do
       let(:job_role) { "teacher" }
@@ -11,7 +11,7 @@ RSpec.describe VacancyFormsHelper do
         let(:phase) { "primary" }
 
         it "shows a KS1 example" do
-          expect(helper.vacancy_job_title_form_hint_text(vacancy)).to eq("For example ‘Teacher of KS1’")
+          expect(helper.vacancy_job_title_form_hint_text(vacancy)).to eq(I18n.t("helpers.hint.publishers_job_listing_job_title_form.job_title.teacher.primary"))
         end
       end
 
@@ -19,7 +19,7 @@ RSpec.describe VacancyFormsHelper do
         let(:phase) { "secondary" }
 
         it "shows a Maths example" do
-          expect(helper.vacancy_job_title_form_hint_text(vacancy)).to eq("For example ‘Teacher of Maths’")
+          expect(helper.vacancy_job_title_form_hint_text(vacancy)).to eq(I18n.t("helpers.hint.publishers_job_listing_job_title_form.job_title.teacher.middle_secondary_or_sixth_form_or_college"))
         end
       end
     end
@@ -31,15 +31,15 @@ RSpec.describe VacancyFormsHelper do
         let(:phase) { "primary" }
 
         it "shows a KS1 example" do
-          expect(helper.vacancy_job_title_form_hint_text(vacancy)).to eq("For example ‘KS1 phase leader’")
+          expect(helper.vacancy_job_title_form_hint_text(vacancy)).to eq(I18n.t("helpers.hint.publishers_job_listing_job_title_form.job_title.middle_leader.primary"))
         end
       end
 
       context "when secondary" do
         let(:phase) { "secondary" }
 
-        it "shows a Maths example" do
-          expect(helper.vacancy_job_title_form_hint_text(vacancy)).to eq("For example ’Head of modern foreign languages’")
+        it "shows a modern languages example" do
+          expect(helper.vacancy_job_title_form_hint_text(vacancy)).to eq(I18n.t("helpers.hint.publishers_job_listing_job_title_form.job_title.middle_leader.middle_secondary_or_sixth_form_or_college"))
         end
       end
     end
