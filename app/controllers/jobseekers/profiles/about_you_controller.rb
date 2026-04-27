76a843b9c6c34766e7fa652fec65301f63d65058
Stephen Dicks <github@starswan.com>
2026-04-27 16:14:53 +0100

    wip

--- app/controllers/jobseekers/profiles/about_you_controller.rb
+++ app/controllers/jobseekers/profiles/about_you_controller.rb
@@ -1,23 +1,20 @@
 class Jobseekers::Profiles::AboutYouController < Jobseekers::ProfilesController
   def edit; end
 
-  # :nocov:
   def update
     @profile.assign_attributes(about_you_richtext: form_params.fetch(:about_you))
+    # :nocov:
     if @profile.save(context: :about_you)
       redirect_to jobseekers_profile_about_you_path
     else
       render :edit
     end
+    # :nocov:
   end
-  # :nocov:
 
   private
 
-  # the new version doesn't allow empty submissions
-  # rubocop:disable Rails/StrongParametersExpect
   def form_params
-    params.permit(jobseekers_profile_about_you_form: form_class.fields).require(:jobseekers_profile_about_you_form)
+    params.expect(jobseeker_profile: :about_you)
   end
-  # rubocop:enable Rails/StrongParametersExpect
 end
