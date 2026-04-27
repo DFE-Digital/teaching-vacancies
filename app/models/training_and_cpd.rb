65467645aaf8fc10083c24399762dc3dc4f9878d
Kyle MacPherson <kaimac2010@gmail.com>
2026-04-27 14:39:06 +0100

    Allow duplication of training and cpds

--- app/models/training_and_cpd.rb
+++ app/models/training_and_cpd.rb
@@ -2,4 +2,8 @@ class TrainingAndCpd < ApplicationRecord
   self.ignored_columns += %w[jobseeker_profile_id]
 
   belongs_to :job_application
+
+  def duplicate
+    dup.tap { |record| record.assign_attributes(job_application: nil) }
+  end
 end
