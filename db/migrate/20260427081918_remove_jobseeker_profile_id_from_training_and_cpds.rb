90ffd2665b265834731933c5ebe055e3981954c0
Kyle MacPherson <kaimac2010@gmail.com>
2026-04-29 11:58:20 +0100

    Fix migration issue

--- db/migrate/20260427081918_remove_jobseeker_profile_id_from_training_and_cpds.rb
+++ db/migrate/20260427081918_remove_jobseeker_profile_id_from_training_and_cpds.rb
@@ -2,8 +2,11 @@ class RemoveJobseekerProfileIdFromTrainingAndCpds < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    remove_foreign_key :training_and_cpds, :jobseeker_profiles
-    remove_index :training_and_cpds, column: :jobseeker_profile_id, algorithm: :concurrently
+    remove_foreign_key :training_and_cpds, :jobseeker_profiles, if_exists: true
+    remove_index :training_and_cpds, column: :jobseeker_profile_id, algorithm: :concurrently, if_exists: true
+    add_not_null_constraint :training_and_cpds, :job_application_id, name: "training_and_cpds_job_application_id_null", validate: false
+    validate_not_null_constraint :training_and_cpds, :job_application_id, name: "training_and_cpds_job_application_id_null"
     change_column_null :training_and_cpds, :job_application_id, false
+    remove_check_constraint :training_and_cpds, name: "training_and_cpds_job_application_id_null"
   end
 end
