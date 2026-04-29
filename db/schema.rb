d6b5d82b9a80a54257e1394002c500b98d8c4412
Kyle MacPherson <kaimac2010@gmail.com>
2026-04-29 12:38:47 +0100

    Fix migration issue (#8773)
    ## Trello card URL
    
    ## Changes in this PR:
    
    This PR fixes [a migration
    issue](https://teaching-vacancies.sentry.io/issues/7448926700/?environment=production&environment=prod&query=is%3Aunresolved&referrer=issue-stream)
    on production.
    
    ## Screenshots of UI changes:
    
    ### Before
    
    ### After
    
    
    ## Checklists:
    
    ### Data & Schema Changes
    
    If this PR modifies data structures or validations, check the following:
    
    - [ ] Adds/removes model validations
    - [ ] Adds/removes database fields
    - [ ] Modifies Vacancy enumerables (phases, working patterns, job roles,
    key stages, etc.)
    
    <details>
    <summary>If any of the above options has changed then the author must
    check/resolve all of the following...</summary>
    
    ### Integration Impact
    
    Does this change affect any of these integrations?
    - [ ] DfE Analytics platform
    - [ ] Legacy imports mappings
    - [ ] DWP Find a Job export mappings
    - [ ] Publisher ATS API (may require mapping updates or API versioning)
    
    ### User Experience & Data Integrity
    
    Could this change impact:
    - [ ] Existing subscription alerts (will legacy subscription search
    filters break?)
    - [ ] Legacy vacancy copying (will copied vacancies fail new
    validations?)
    - [ ] In-progress drafts for Vacancies or Job Applications
    </details>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_04_27_081918) do
+ActiveRecord::Schema[8.0].define(version: 2026_04_27_103037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "citext"
