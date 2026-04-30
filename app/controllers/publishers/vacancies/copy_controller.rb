84d1beb68fdcb8541b47485803749ebea4309d20
Stephen Dicks <github@starswan.com>
2026-04-30 16:49:21 +0100

    create template from legacy vacancy

--- app/controllers/publishers/vacancies/copy_controller.rb
+++ app/controllers/publishers/vacancies/copy_controller.rb
@@ -8,15 +8,7 @@ module Publishers
       end
 
       def create
-        @form = current_organisation.vacancy_templates.create(
-          vacancy.attributes
-                 .except(*(VacancyTemplate::IGNORED_ATTRIBUTES + %w[job_roles key_stages phases working_patterns]))
-                 .merge(copy_vacancy_params)
-                 .merge(job_roles: vacancy.job_roles,
-                        phases: vacancy.phases,
-                        working_patterns: vacancy.working_patterns,
-                        key_stages: vacancy.key_stages),
-        )
+        @form = create_template vacancy
         if @form.valid?
           redirect_to organisation_vacancy_templates_path, success: t("publishers.vacancies.show.copied.success")
         else
@@ -25,6 +17,26 @@ module Publishers
       end
 
       private
+
+      # rubocop can't seem to see that the caller check the return from this method
+      # rubocop:disable Rails/SaveBang
+      def create_template(vacancy)
+        receive_applications = if VacancyTemplate.receive_applications.key?(vacancy.receive_applications)
+                                 vacancy.receive_applications
+                               end
+
+        current_organisation.vacancy_templates.create(
+          vacancy.attributes
+                 .except(*(VacancyTemplate::IGNORED_ATTRIBUTES + %w[job_roles key_stages phases working_patterns receive_applications]))
+                 .merge(copy_vacancy_params)
+                 .merge(job_roles: vacancy.job_roles,
+                        phases: vacancy.phases,
+                        working_patterns: vacancy.working_patterns,
+                        key_stages: vacancy.key_stages,
+                        receive_applications: receive_applications),
+        )
+      end
+      # rubocop:enable Rails/SaveBang
 
       def copy_vacancy_params
         params.expect(vacancy_template: [:name])
