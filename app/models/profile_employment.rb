ef8ec71e563726eb0faf76c23a40cd205cf1b707
Marc Sardon <marc.sardon@education.gov.uk>
2026-04-30 13:01:04 +0100

    Remove ignoring columns from Profile Employments
    Having two models using the same DB table and one of the models ignoring
    a subset of the table attributes results in DfE Analytics entities
    missing the keys in some of the employments rows, but not on others.
    What is effectively an inconsisten schema for the employments table in
    BigQ.
    
    The PA alerted us about the issue and how BigQ as it is configured in
    Analytics cannot deal with this, breaking the analytics pipelines.

--- app/models/profile_employment.rb
+++ app/models/profile_employment.rb
@@ -4,7 +4,4 @@ class ProfileEmployment < EmploymentRecord
   has_encrypted :organisation, :job_title
 
   self.table_name = "employments"
-
-  # these fields are not used in the profile version
-  self.ignored_columns += %i[main_duties subjects reason_for_leaving]
 end
