4d32ada37815863768f70050a3964252832fa39c
Stephen Dicks <github@starswan.com>
2026-05-05 15:53:08 +0100

    create template from legacy vacancy (#8779)

---
+++ spec/requests/publishers/vacancies/copy_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Publishers::Vacancies::CopyController" do
+  let(:organisation) { create(:school) }
+  let(:publisher) { create(:publisher, accepted_terms_at: 1.day.ago) }
+
+  # rubocop:disable RSpec/AnyInstance
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:current_organisation).and_return(organisation)
+    sign_in(publisher, scope: :publisher)
+  end
+  # rubocop:enable RSpec/AnyInstance
+
+  after { sign_out(publisher) }
+
+  describe "POST #create" do
+    let(:name) { Faker::CryptoCoin.coin_name }
+    let(:new_template) { VacancyTemplate.last }
+
+    context "with a simple vacancy" do
+      let(:vacancy) { create(:vacancy, :no_tv_applications, organisations: [organisation]) }
+
+      it "copies a vacancy into a template" do
+        expect {
+          post organisation_job_copy_path(vacancy.id, params: { vacancy_template: { name: name } })
+        }.to change(VacancyTemplate, :count).by(1)
+        expect(new_template).to have_attributes(name: name, receive_applications: "website")
+      end
+    end
+
+    context "with a legacy email vacancy" do
+      let(:vacancy) { create(:vacancy, :legacy_email_application, organisations: [organisation]) }
+
+      it "copies a vacancy into a template" do
+        expect {
+          post organisation_job_copy_path(vacancy.id, params: { vacancy_template: { name: name } })
+        }.to change(VacancyTemplate, :count).by(1)
+        expect(new_template).to have_attributes(name: name, receive_applications: nil)
+      end
+    end
+  end
+end
