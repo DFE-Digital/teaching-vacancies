6be0935ccbf4ac1313205bf91e613ab4aa38cf03
Kyle MacPherson <kaimac2010@gmail.com>
2026-05-05 13:43:18 +0100

    Refactor

--- app/mailers/jobseekers/peak_times_mailer.rb
+++ app/mailers/jobseekers/peak_times_mailer.rb
@@ -1,8 +1,19 @@
 class Jobseekers::PeakTimesMailer < Jobseekers::BaseMailer
   helper_method :jobseeker, :first_name, :campaign_url, :campaign_key
 
-  MARCH_PEAK_TIMES_TEMPLATE_ID = "ebad6edf-99a5-4072-951f-f01c1178cdae".freeze
-  MAY_PEAK_TIMES_TEMPLATE_ID = "084ff736-1802-4409-87b3-2826ee04eac3".freeze
+  PEAK_TIMES_CAMPAIGNS = {
+    "march" => {
+      template_id: "ebad6edf-99a5-4072-951f-f01c1178cdae",
+      url: "https://teaching-vacancies.service.gov.uk/jobs?utm_source=notify&utm_medium=email&utm_campaign=peak_email&utm_content=march_2026",
+    },
+    "may" => {
+      template_id: "084ff736-1802-4409-87b3-2826ee04eac3",
+      url: "https://teaching-vacancies.service.gov.uk/jobs?utm_source=notify&utm_medium=email&utm_campaign=peak_email&utm_content=may_2026",
+    },
+    "november" => {
+      url: "https://teaching-vacancies.service.gov.uk/jobs?utm_source=notify&utm_medium=email&utm_campaign=notify_november_2025&utm_content=tuesday_2025",
+    },
+  }.freeze
 
   def reminder(jobseeker_id)
     @jobseeker = Jobseeker.includes(jobseeker_profile: :personal_details).find_by(id: jobseeker_id)
@@ -17,9 +28,8 @@ class Jobseekers::PeakTimesMailer < Jobseekers::BaseMailer
   private
 
   def send_notify_template_email
-    template_id = current_month == "march" ? MARCH_PEAK_TIMES_TEMPLATE_ID : MAY_PEAK_TIMES_TEMPLATE_ID
     template_mail(
-      template_id,
+      PEAK_TIMES_CAMPAIGNS[current_month][:template_id],
       to: @jobseeker.email,
       personalisation: {
         campaign_url: campaign_url,
@@ -39,16 +49,7 @@ class Jobseekers::PeakTimesMailer < Jobseekers::BaseMailer
   end
 
   def campaign_url
-    case current_month
-    when "may"
-      "https://teaching-vacancies.service.gov.uk/jobs?utm_source=notify&utm_medium=email&utm_campaign=peak_email&utm_content=may_2026"
-    when "november"
-      "https://teaching-vacancies.service.gov.uk/jobs?utm_source=notify&utm_medium=email&utm_campaign=notify_november_2025&utm_content=tuesday_2025"
-    when "march"
-      "https://teaching-vacancies.service.gov.uk/jobs?utm_source=notify&utm_medium=email&utm_campaign=peak_email&utm_content=march_2026"
-    else
-      "https://teaching-vacancies.service.gov.uk/"
-    end
+    PEAK_TIMES_CAMPAIGNS.fetch(current_month, {}).fetch(:url, "https://teaching-vacancies.service.gov.uk/")
   end
 
   def campaign_key
