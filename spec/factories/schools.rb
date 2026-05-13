999b3a3c176aba7afbfc3e62640c58b7b82605d4
Stephen Dicks <github@starswan.com>
2026-05-13 13:55:04 +0100

    map religious_character from GIAS

--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -26,6 +26,7 @@ FactoryBot.define do
         "TypeOfEstablishment (code)": "02",
       }
     end
+    religious_character { "None" }
     detailed_school_type { "Voluntary aided school" }
     minimum_age { 11 }
     maximum_age { 18 }
@@ -74,22 +75,7 @@ FactoryBot.define do
     end
 
     trait :catholic do
-      gias_data do
-        {
-          CloseDate: nil,
-          HeadFirstName: Faker::Name.first_name,
-          HeadLastName: Faker::Name.last_name.delete("'"),
-          HeadPreferredJobTitle: Faker::Name.prefix.delete("."),
-          DateOfLastInspectionVisit: Faker::Date.between(from: 999.days.ago, to: 5.days.ago),
-          NumberOfPupils: Faker::Number.number(digits: 3),
-          "OfstedRating (name)": factory_sample(ofsted_ratings),
-          OpenDate: Faker::Date.between(from: 10_000.days.ago, to: 1000.days.ago),
-          "ReligiousCharacter (name)": "Roman Catholic",
-          SchoolCapacity: Faker::Number.number(digits: 4),
-          TelephoneNum: Faker::Number.number(digits: 11).to_s,
-          "Trusts (name)": "#{Faker::Company.name.delete("'")} Trust",
-        }
-      end
+      religious_character { "Roman Catholic" }
     end
 
     trait :no_geolocation do
