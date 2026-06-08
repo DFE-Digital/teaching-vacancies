79923e0ebd7e09045ddd544db974f7dad4602d50
Stephen Dicks <github@starswan.com>
2026-06-08 16:23:20 +0100

    fix variable name

--- spec/services/vacancies/import/sources/fusion_spec.rb
+++ spec/services/vacancies/import/sources/fusion_spec.rb
@@ -118,9 +118,9 @@ RSpec.describe Vacancies::Import::Sources::Fusion do
     describe "working_patterns mapping" do
       context "when working_patterns includes `flexible`" do
         let(:response_body) do
-          hash = JSON.parse(super())
-          hash[0]["workingPatterns"] = "full_time,flexible"
-          hash.to_json
+          response_array = JSON.parse(super())
+          response_array[0]["workingPatterns"] = "full_time,flexible"
+          response_array.to_json
         end
 
         it "maps flexible to part time" do
@@ -130,9 +130,9 @@ RSpec.describe Vacancies::Import::Sources::Fusion do
 
       context "when working_patterns includes `flexible` and `part_time`" do
         let(:response_body) do
-          hash = JSON.parse(super())
-          hash[0]["workingPatterns"] = "full_time,part_time,flexible"
-          hash.to_json
+          response_array = JSON.parse(super())
+          response_array[0]["workingPatterns"] = "full_time,part_time,flexible"
+          response_array.to_json
         end
 
         it "maps flexible to part time" do
@@ -142,9 +142,9 @@ RSpec.describe Vacancies::Import::Sources::Fusion do
 
       context "when working_patterns includes `term_time`" do
         let(:response_body) do
-          hash = JSON.parse(super())
-          hash[0]["workingPatterns"] = "full_time,term_time"
-          hash.to_json
+          response_array = JSON.parse(super())
+          response_array[0]["workingPatterns"] = "full_time,term_time"
+          response_array.to_json
         end
 
         it "maps term_time to part time" do
@@ -154,9 +154,9 @@ RSpec.describe Vacancies::Import::Sources::Fusion do
 
       context "when working_patterns includes `term_time` and `part_time`" do
         let(:response_body) do
-          hash = JSON.parse(super())
-          hash[0]["workingPatterns"] = "full_time,part_time,term_time"
-          hash.to_json
+          response_array = JSON.parse(super())
+          response_array[0]["workingPatterns"] = "full_time,part_time,term_time"
+          response_array.to_json
         end
 
         it "maps term_time to part time" do
@@ -166,9 +166,9 @@ RSpec.describe Vacancies::Import::Sources::Fusion do
 
       context "when working pattern includes `job_share`" do
         let(:response_body) do
-          hash = JSON.parse(super())
-          hash[0]["workingPatterns"] = "job_share"
-          hash.to_json
+          response_array = JSON.parse(super())
+          response_array[0]["workingPatterns"] = "job_share"
+          response_array.to_json
         end
 
         it "maps job_share to part time" do
@@ -182,9 +182,9 @@ RSpec.describe Vacancies::Import::Sources::Fusion do
 
       context "when the working patterns list contains spaces" do
         let(:response_body) do
-          hash = JSON.parse(super())
-          hash[0]["workingPatterns"] = "full_time, part_time, term_time"
-          hash.to_json
+          response_array = JSON.parse(super())
+          response_array[0]["workingPatterns"] = "full_time, part_time, term_time"
+          response_array.to_json
         end
 
         it "records both working patterns in the vacancy" do
@@ -376,9 +376,9 @@ RSpec.describe Vacancies::Import::Sources::Fusion do
 
     context "when visa_sponsorship_available is not provided" do
       let(:response_body) do
-        hash = JSON.parse(super())
-        hash[0].delete("visaSponsorshipAvailable")
-        hash.to_json
+        response_array = JSON.parse(super())
+        response_array[0].delete("visaSponsorshipAvailable")
+        response_array.to_json
       end
 
       it "sets visa_sponsorship_available to false" do
@@ -388,9 +388,9 @@ RSpec.describe Vacancies::Import::Sources::Fusion do
 
     describe "when external reference is not provided" do
       let(:response_body) do
-        hash = JSON.parse(super())
-        hash[0].delete("reference")
-        hash.to_json
+        response_array = JSON.parse(super())
+        response_array[0].delete("reference")
+        response_array.to_json
       end
 
       it "sets it as nil" do
