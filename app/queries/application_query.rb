8f0dc718459ffe43e256d4a93e939e719e1593ca
Stephen Dicks <github@starswan.com>
2026-06-10 16:47:21 +0100

    rebase

--- app/queries/application_query.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationQuery
-  # Delegate class-level call to a new instance - allows using class name as an argument
-  # to scope declaration, e.g:
-  #   scope :foo, FooQuery
-  def self.call(...)
-    new.call(...)
-  end
-end
