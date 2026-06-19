0e36faf3f2b67e4d2f8da637d391027cbc930463
Kyle MacPherson <kaimac2010@gmail.com>
2026-06-19 11:24:25 +0100

    Add validations to address form

--- app/form_models/publishers/job_listing/confirm_job_address_form.rb
+++ app/form_models/publishers/job_listing/confirm_job_address_form.rb
@@ -1,7 +1,17 @@
 class Publishers::JobListing::ConfirmJobAddressForm < Publishers::JobListing::JobListingForm
   attr_accessor :job_address_line1, :job_address_line2, :job_address_town, :job_address_county, :job_address_postcode
 
+  validates :job_address_line1, presence: true, if: :any_address_field_present?
+  validates :job_address_town, presence: true, if: :any_address_field_present?
+  validates :job_address_postcode, presence: true, if: :any_address_field_present?
+
   def self.fields
     %i[job_address_line1 job_address_line2 job_address_town job_address_county job_address_postcode]
+  end
+
+  private
+
+  def any_address_field_present?
+    [job_address_line1, job_address_line2, job_address_town, job_address_county, job_address_postcode].any?(&:present?)
   end
 end
