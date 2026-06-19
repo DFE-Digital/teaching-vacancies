2c3afe05fb706ec23a2f823a749b5142915c74f5
Stephen Dicks <github@starswan.com>
2026-06-19 12:42:22 +0200

    FE subjects for job alerts (#8881)

--- config/initializers/subjects.rb
+++ config/initializers/subjects.rb
@@ -2,6 +2,9 @@ base_path = Rails.root.join("config/data")
 
 SUBJECT_OPTIONS = YAML.load_file(base_path.join("subjects.yml"))
 FURTHER_EDUCATION_SUBJECT_OPTIONS = YAML.load_file(base_path.join("further_education_subjects.yml"))
+
+# de-duplicated list - use this for JebSeeker side when we don't know FE/School,
+# other lists Hiring staff side when we do (vacancy organisations contains an FE college)
 VACANCY_SEARCH_SUBJECT_OPTIONS = (SUBJECT_OPTIONS + FURTHER_EDUCATION_SUBJECT_OPTIONS)
   .uniq { |subject, _hint| subject.downcase }
   .sort_by { |subject, _hint| subject.downcase }
