b4e000d085c1645ce3a035d189e98867e900412e
Stephen Dicks <github@starswan.com>
2026-06-22 15:46:47 +0200

    Add uniqueness constraint to publisher to convert crashes into failures (#8941)

--- app/jobs/update_single_dsi_user_in_db_job.rb
+++ app/jobs/update_single_dsi_user_in_db_job.rb
@@ -3,6 +3,9 @@
 class UpdateSingleDSIUserInDbJob < ApplicationJob
   queue_as :low
 
+  # Race conditions may result in validation failures due to duplicates
+  retry_on ActiveModel::ValidationError
+
   def perform(dsi_user)
     Publishers::DfeSignIn::UpdateUsersInDb.convert_to_user(dsi_user)
   end
