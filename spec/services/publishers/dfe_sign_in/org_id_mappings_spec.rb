19a122a549edeefca1843f01da725b2eab5797b6
Marc Sardon <marc@bichito.software>
2026-06-26 15:56:15 +0100

    Block unsupported eatablishment types from DfE Sign-in
    If DfE Sign-in policies allow publishers to add TV as a service, they
    would inmediately be able to sign-in as publisher in our service as long
    as their org category is accepted by us and our service has the
    organisation in our DB.
    
    This is a problem in some situations, like enabling Further Education
    orgs at DfE Sign-in policy level in preparation for our integration with
    FE colleges, being done before we actually want to enable them in our
    service.
    
    To mitigate these issues, we're adding a new level of filtering the
    incoming publishers from DfE Sign in.
    
    Upon signing-in for an establishment, we will check that the
    establishment type is not one of the out of scope/excluded by our
    service. And if it is one of those, we will send them to a tailored
    error page, following the same format as done with the orgs with
    unsupported category.

---
+++ spec/services/publishers/dfe_sign_in/org_id_mappings_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe Publishers::DfeSignIn::OrgIdMappings do
+  describe "OUT_OF_SCOPE_TYPES" do
+    it "contains exactly the same organisation types as Organisation::OUT_OF_SCOPE_DETAILED_SCHOOL_TYPES" do
+      dsi_out_of_scope_names = described_class::OUT_OF_SCOPE_TYPES.flat_map(&:values)
+
+      expect(dsi_out_of_scope_names).to match_array(Organisation::OUT_OF_SCOPE_DETAILED_SCHOOL_TYPES)
+    end
+  end
+
+  describe ".out_of_scope_type?" do
+    it "returns true for an out-of-scope type id" do
+      expect(described_class.out_of_scope_type?("18")).to be true # Further education
+    end
+
+    it "returns false for an in-scope type id" do
+      expect(described_class.out_of_scope_type?("01")).to be false
+    end
+
+    it "returns false for nil" do
+      expect(described_class.out_of_scope_type?(nil)).to be false
+    end
+  end
+end
