63e883ec5d25007537693d81926296d63b3614bf
Stephen Dicks <github@starswan.com>
2026-07-10 17:10:16 +0100

    use unique first_name to reduce test flakiness

--- spec/factories/personal_details.rb
+++ spec/factories/personal_details.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :personal_details do
-    first_name { FFaker::Name.first_name }
+    first_name { Faker::Name.unique.first_name }
     last_name { FFaker::Name.html_safe_last_name }
     has_right_to_work_in_uk { true }
     jobseeker_profile
