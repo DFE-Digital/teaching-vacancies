9a652554f83cfe2d68dbbda31027ede5e5dd6aab
Stephen Dicks <github@starswan.com>
2026-07-13 08:20:51 +0100

    ensure SolidQueueJob uses solid queue adapter (#9039)

--- app/jobs/solid_queue_job.rb
+++ app/jobs/solid_queue_job.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class SolidQueueJob < ApplicationJob
+  # :nocov:
+  # Set the adapter explicitly, otherwise this uses the default one (which might be sidekiq)
+  self.queue_adapter = :sidekiq unless Rails.env.test?
+  # :nocov:
+
   retry_on StandardError, attempts: 10
 end
