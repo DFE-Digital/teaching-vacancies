c56fa774a867bbe063c05f9a81b88cbdf7f5dfd6
Stephen Dicks <github@starswan.com>
2026-07-16 16:47:28 +0100

    wip

--- app/jobs/import_polygon_data_job.rb
+++ app/jobs/import_polygon_data_job.rb
@@ -2,12 +2,17 @@ class ImportPolygonDataJob < SidekiqJob
   queue_as :low
 
   def perform
+    load_data_set("cities") { |tolerance| OnsDataImport::ImportCities.call(tolerance) }
+    load_data_set("counties") { |tolerance| OnsDataImport::ImportCounties.call(tolerance * 2) }
+    load_data_set("regions") { |tolerance| OnsDataImport::ImportRegions.call(tolerance * 2) }
+    load_data_set("composites") { |tolerance| OnsDataImport::CreateComposites.call(tolerance * 3) }
+  end
+
+  def load_data_set name, &block
     (0..).each do |tolerance_multiplier|
       tolerance = OnsDataImport::Base::TOLERANCE_100M + (tolerance_multiplier / 10_000.0)
-      OnsDataImport::ImportCounties.call(tolerance * 2)
-      OnsDataImport::ImportCities.call(tolerance)
-      OnsDataImport::ImportRegions.call(tolerance * 2)
-      OnsDataImport::CreateComposites.call(tolerance)
+
+      block.call(tolerance)
 
       invalid_names = LocationPolygon.order(:name).reject { |p|
         begin
@@ -17,9 +22,12 @@ class ImportPolygonDataJob < SidekiqJob
         end
       }.map(&:name)
 
-      break if invalid_names.empty?
-
-      logger.info "Invalid names #{invalid_names} for tolerance #{tolerance_multiplier} (#{tolerance})"
+      if invalid_names.empty?
+        logger.info "Loading #{name} tolerance #{tolerance_multiplier} (#{tolerance})"
+        break
+      else
+        logger.info "Loading #{name} Invalid names #{invalid_names} for tolerance #{tolerance_multiplier} (#{tolerance})"
+      end
     end
   end
 end
