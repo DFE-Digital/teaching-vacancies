b905bfbe860b847662aba53f963fb26855fd4dd6
Stephen Dicks <github@starswan.com>
2026-07-16 13:53:58 +0100

    linter

--- app/jobs/import_polygon_data_job.rb
+++ app/jobs/import_polygon_data_job.rb
@@ -3,15 +3,22 @@ class ImportPolygonDataJob < SidekiqJob
 
   def perform
     (0..).each do |tolerance_multiplier|
-      tolerance = OnsDataImport::Base::TOLERANCE_100M + (tolerance_multiplier / 10000.0)
+      tolerance = OnsDataImport::Base::TOLERANCE_100M + (tolerance_multiplier / 10_000.0)
       OnsDataImport::ImportCounties.call(tolerance)
       OnsDataImport::ImportCities.call(tolerance)
       OnsDataImport::ImportRegions.call(tolerance)
       OnsDataImport::CreateComposites.call(tolerance)
 
-      invalid_names = LocationPolygon.all.order(:name).reject { |p| p.area.invalid_reason.nil? && p.uk_area.invalid_reason.nil? rescue false }.map(&:name)
+      invalid_names = LocationPolygon.order(:name).reject { |p|
+        begin
+          p.area.invalid_reason.nil? && p.uk_area.invalid_reason.nil?
+        rescue StandardError
+          false
+        end
+      }.map(&:name)
 
       break if invalid_names.empty?
+
       logger.info "Invalid names #{invalid_names} for tolerance #{tolerance_multiplier} (#{tolerance})"
     end
   end
