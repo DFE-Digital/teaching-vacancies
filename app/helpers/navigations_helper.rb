2e0954df42643ec519ab5159b09c50da725d89e7
Kyle MacPherson <kaimac2010@gmail.com>
2026-07-16 14:24:46 +0100

    Fix publisher nav tab highlight for messages, candidate profiles, statistics and notifications

--- app/helpers/navigations_helper.rb
+++ app/helpers/navigations_helper.rb
@@ -7,6 +7,22 @@ module NavigationsHelper
     current_page?(organisation_jobs_with_type_path) || request.original_fullpath =~ %r{^/organisation/jobs}
   end
 
+  def messages_active?
+    request.original_fullpath =~ %r{^/publishers/candidate_messages}
+  end
+
+  def candidate_profiles_active?
+    request.original_fullpath =~ %r{^/publishers/jobseeker_profiles}
+  end
+
+  def statistics_active?
+    request.original_fullpath =~ %r{^/publishers/current_year_statistics}
+  end
+
+  def notifications_active?
+    request.original_fullpath =~ %r{^/publishers/notifications}
+  end
+
   def schools_in_your_trust_active?
     request.original_fullpath =~ %r{^/publishers/schools}
   end
