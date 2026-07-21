15f8222fdf3b835454b37ea1479f86e7ab413807
Stephen Dicks <github@starswan.com>
2026-07-21 12:08:59 +0100

    linter

--- app/controllers/jobseekers/job_applications/professional_body_memberships_controller.rb
+++ app/controllers/jobseekers/job_applications/professional_body_memberships_controller.rb
@@ -1,49 +1,48 @@
 module Jobseekers
-class JobApplications::ProfessionalBodyMembershipsController < BaseController
-  before_action :set_job_application, only: %i[create edit new update destroy]
+  class JobApplications::ProfessionalBodyMembershipsController < BaseController
+    before_action :set_job_application, only: %i[create edit new update destroy]
 
-  before_action :set_model, only: %i[edit update destroy]
+    before_action :set_model, only: %i[edit update destroy]
 
-  def new
-    @model = @job_application.professional_body_memberships.build
-  end
+    def new
+      @model = @job_application.professional_body_memberships.build
+    end
 
-  def edit; end
+    def edit; end
 
-  def create
-    @model = @job_application.professional_body_memberships.build(professional_body_memberships_form_params)
+    def create
+      @model = @job_application.professional_body_memberships.build(professional_body_memberships_form_params)
 
-    if @model.save
-      redirect_to back_path
-    else
-      render :new
+      if @model.save
+        redirect_to back_path
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @model.update(professional_body_memberships_form_params)
+        redirect_to back_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @model.destroy!
+      redirect_to back_path, success: t(".success")
+    end
+
+    def professional_body_memberships_form_params
+      params.expect(jobseekers_professional_body_membership_form: %i[name membership_type membership_number year_membership_obtained exam_taken])
+    end
+
+    def set_model
+      @model = @job_application.professional_body_memberships.find(params[:id])
+    end
+
+    def set_job_application
+      @job_application = current_jobseeker.job_applications.draft.find(params[:job_application_id])
     end
   end
-
-  def update
-    if @model.update(professional_body_memberships_form_params)
-      redirect_to back_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @model.destroy!
-    redirect_to back_path, success: t(".success")
-  end
-
-  def professional_body_memberships_form_params
-    params.expect(jobseekers_professional_body_membership_form: %i[name membership_type membership_number year_membership_obtained exam_taken])
-  end
-
-  def set_model
-    @model = @job_application.professional_body_memberships.find(params[:id])
-  end
-
-  def set_job_application
-    @job_application = current_jobseeker.job_applications.draft.find(params[:job_application_id])
-  end
-
-end
 end
--- app/controllers/jobseekers/job_applications/training_and_cpds_controller.rb
+++ app/controllers/jobseekers/job_applications/training_and_cpds_controller.rb
@@ -1,52 +1,52 @@
 module Jobseekers
-class JobApplications::TrainingAndCpdsController < BaseController
-  before_action :set_job_application, only: %i[create edit new update destroy]
-  before_action :set_training_and_cpd, only: %i[edit update destroy]
+  class JobApplications::TrainingAndCpdsController < BaseController
+    before_action :set_job_application, only: %i[create edit new update destroy]
+    before_action :set_training_and_cpd, only: %i[edit update destroy]
 
-  def new
-    @training_and_cpd = @job_application.training_and_cpds.build
-  end
+    def new
+      @training_and_cpd = @job_application.training_and_cpds.build
+    end
 
-  def edit; end
+    def edit; end
 
-  def create
-    @training_and_cpd = @job_application.training_and_cpds.build(training_and_cpd_form_params)
-    if @training_and_cpd.save
-      redirect_to back_path
-    else
-      render :new
+    def create
+      @training_and_cpd = @job_application.training_and_cpds.build(training_and_cpd_form_params)
+      if @training_and_cpd.save
+        redirect_to back_path
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @training_and_cpd.update(training_and_cpd_form_params)
+        redirect_to back_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @training_and_cpd.destroy!
+      redirect_to back_path, success: t(".success")
+    end
+
+    private
+
+    def training_and_cpd_form_params
+      params.expect(jobseekers_training_and_cpd_form: %i[name provider grade year_awarded course_length])
+    end
+
+    def set_training_and_cpd
+      @training_and_cpd = @job_application.training_and_cpds.find(params[:id])
+    end
+
+    def set_job_application
+      @job_application = current_jobseeker.job_applications.draft.find(params[:job_application_id])
+    end
+
+    def back_path
+      @back_path ||= jobseekers_job_application_build_path(@job_application, :training_and_cpds)
     end
   end
-
-  def update
-    if @training_and_cpd.update(training_and_cpd_form_params)
-      redirect_to back_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @training_and_cpd.destroy!
-    redirect_to back_path, success: t(".success")
-  end
-
-  private
-
-  def training_and_cpd_form_params
-    params.expect(jobseekers_training_and_cpd_form: %i[name provider grade year_awarded course_length])
-  end
-
-  def set_training_and_cpd
-    @training_and_cpd = @job_application.training_and_cpds.find(params[:id])
-  end
-
-  def set_job_application
-    @job_application = current_jobseeker.job_applications.draft.find(params[:job_application_id])
-  end
-
-  def back_path
-    @back_path ||= jobseekers_job_application_build_path(@job_application, :training_and_cpds)
-  end
-end
 end
