2c2f7342b219b5c72f7e1e58b3413c981135d12d
Stephen Dicks <github@starswan.com>
2026-07-21 13:06:02 +0100

    redo subscription query

--- app/queries/subscription_vacancies_matching_query.rb
+++ app/queries/subscription_vacancies_matching_query.rb
@@ -1,29 +1,7 @@
 class SubscriptionVacanciesMatchingQuery
-  # attr_reader :scope
-
-  # British National Grid SRID (EPSG:27700) is a projected coordinate system used for mapping in Great Britain.
-  # It provides coordinates in meters, which is useful for distance calculations, which we need
-  # for radius-based searches.
-  # It is significantly more accurate for distance calculations in Great Britain that EPSG:3857 (Web Mercator).
-  # EPSG:3857 distort distances and areas, especially as you move away from the equator. What would cause a multiplier
-  # between 1.5x and 1.7x for radius/buffer distances in our case to get the matches we would expect.
-  # BRITISH_NATIONAL_GRID_SRID = 27700
-
-  # Builds the query to find the IDs for the vacancies matching the subscription criteria.
-  # The subquery is needed to be able to combine our requirements into a valid single SQL statement:
-  # - retrieve unique DISTINCT ON vacancy ids: As the location filter may return the same vacancy multiple times (vacancy for multiple orgs).
-  # - order by publish_on DESC to get the most recent vacancies first.
-  # - apply a limit if needed over the already ordered vacancies.
-  # - be able to call pluck(:id) over the resulting query while combining it by distinct and order by a different attribute.
-  #
-  # If we didn't need the order by published date, we could just call distinct over the filtered query and be done without
-  # a subquery.
-  #
   # PERFORMANCE:
-  # We do not want to return the whole Vacancy into memory as this query is used by a job that iterates through
-  # hundreds of thousands of subscriptions to retrieve the matching ids.
-  # Directly retrieving the ids from SQL is way faster and more efficient than loading the Vacancy AR objects into memory
-  # and then calling map(&:id).
+  # The caller of this method just needs to add .pluck(:id) (rather than map(&:id))
+  # to ensure that vacancies doesn't get loaded into RAM by mistake
   class << self
     def call(scope:, subscription:, limit:)
       search_criteria = sanitise_search_criteria(subscription.search_criteria).symbolize_keys
@@ -55,164 +33,17 @@ class SubscriptionVacanciesMatchingQuery
       criteria.except(*Subscription::JOB_ROLE_ALIASES)
     end
 
-    # Goes through the search criteria filters and applies them to the scope one by one, building up the SQL query used
-    # to find the matching vacancies for the subscription.
-    # def build_query_from_search_criteria(scope, search_criteria, subscription)
-    #   search_criteria.each do |criterion, value|
-    #     scope =
-    #       case criterion
-    #       when :job_roles then job_roles_filter(scope, value)
-    #       when :visa_sponsorship_availability then visa_sponsorship_filter(scope, value)
-    #       when :ect_statuses then ect_status_filter(scope, value)
-    #       when :newly_qualified_teacher then newly_qualified_teacher_filter(scope, value)
-    #       when :subjects then subjects_filter(scope, value)
-    #       when :subject then subjects_filter(scope, [value]) # TO DO: This is a legacy single subject criteria. Backfill and remove criteria
-    #       when :phases then phases_filter(scope, value)
-    #       when :working_patterns then working_patterns_filter(scope, value)
-    #       when :organisation_slug then organisation_slug_filter(scope, value)
-    #       when :keyword then keyword_filter(scope, value)
-    #       when :location then location_filter(scope, value, subscription)
-    #       else scope # Ignore unknown criteria
-    #       end
-    #   end
-    #   scope
-    # end
     # Turns the job_roles from multiple possible criteria keys (aliases) into single job_roles list transformed into
     # array_enum to match DB values (integers) that will be used in the query.
     def sanitise_job_roles(criteria)
       criteria.slice(*Subscription::JOB_ROLE_ALIASES)
               .values
               .flatten
-      # .filter_map { |jr| Vacancy::JOB_ROLES[jr.to_s] }
     end
-
-    # Turns the phases from string values to to array_enum to match DB values (integers) that will be used in the query.
-    # def sanitise_phases(criteria)
-    #   Array(criteria[:phases]).filter_map { |ph| Vacancy.phases[ph.to_s] }
-    # end
-
-    # Handle job_share as a special string, not in enum.
-    # def sanitise_working_patterns(criteria)
-    #   patterns = Array(criteria[:working_patterns])
-    #   patterns_int = patterns.reject { |p| p == "job_share" }
-    #                          .filter_map { |wp| Vacancy.working_patterns[wp.to_s] } # Map to integer DB values for SQL query
-    #   # Keep job_share string if present, otherwise just use integer array
-    #   if patterns.include?("job_share")
-    #     patterns_int + %w[job_share]
-    #   else
-    #     patterns_int
-    #   end
-    # end
-
-    # If any of the vacancy's job roles match any of the subscription's job roles
-    # def job_roles_filter(scope, subscription_job_roles)
-    #   scope.where("job_roles && ARRAY[?]::integer[]", subscription_job_roles)
-    # end
-
-    # def visa_sponsorship_filter(scope, visa_sponsorship_available)
-    #   scope.where(visa_sponsorship_available: visa_sponsorship_available)
-    # end
-
-    # Where any of the subscription's ect_statuses match the vacancy's ect_status
-    # def ect_status_filter(scope, subscription_ect_statuses)
-    #   scope.where(ect_status: subscription_ect_statuses)
-    # end
-
-    # legacy criteria:  value always 'true' if present
-    # TO DO: Backfil it to ect_statuses array in the subscriptions table and remove this legacy criteria
-    # def newly_qualified_teacher_filter(scope, newly_qualified_teacher)
-    #   if newly_qualified_teacher == "true"
-    #     scope.where(ect_status: "ect_suitable")
-    #   else
-    #     scope
-    #   end
-    # end
-
-    # If any of the vacancy's subjects match any of the subscription's subjects
-    # def subjects_filter(scope, subscription_subjects)
-    #   scope.where("(subjects && ARRAY[?]::varchar[])", subscription_subjects)
-    # end
-
-    # If any of the vacancy's phases match any of the subscription's phases
-    # def phases_filter(scope, subscription_phases)
-    #   scope.where("phases && ARRAY[?]::integer[]", subscription_phases)
-    # end
-
-    # def working_patterns_filter(scope, subscription_patterns)
-    #   if subscription_patterns == %w[job_share] # If only job_share working pattern. Use specific filter.
-    #     scope.where(is_job_share: true)
-    #   elsif subscription_patterns.include?("job_share") # Either is job_share or any of the other working patterns match.
-    #     patterns = subscription_patterns - %w[job_share]
-    #     scope.where("is_job_share = TRUE OR working_patterns && ARRAY[?]::integer[]", patterns)
-    #   else # If any of the vacancy's working patterns match any of the subscription's working patterns
-    #     scope.where("working_patterns && ARRAY[?]::integer[]", subscription_patterns)
-    #   end
-    # end
 
     # When subscriptions are for a particular org new vacancies. The search criteria contains the exact org. slug.
     def organisation_slug_filter(scope, organisation_slug)
       scope.joins(:organisations).where(organisations: { slug: organisation_slug })
     end
-
-    # Filter ensuring that for the subscription keywords, all keywords are present in the vacancy's searchable_content
-    # Using 'simple' config in the tsquery instead of 'english' to avoid some words not being matched.
-    # We only need a "is every keyword included in the searchable content?" match.
-    # def keyword_filter(scope, subscription_keywords)
-    #   scope.where("vacancies.searchable_content @@ plainto_tsquery('simple', ?)", subscription_keywords.downcase.strip)
-    # end
-
-    # If the subscription search criteria has a 'within this distance from this location' filter,
-    # we need to filter the vacancies based on whether their organisations locations fall within the subscription's area
-    # or radius distance from subscription geopoint. Depending if the subscription location matches a polygon or not will
-    #  have either an area (polygon with radius buffer) or a geopoint + radius to filter by.
-    # def location_filter(scope, subscription_location, subscription)
-    #   location = subscription_location.to_s.strip.downcase
-    #   return scope.none if location.blank? # Invalid location filter returns no matches
-    #   return scope if LocationQuery::NATIONWIDE_LOCATIONS.include?(location) # Nationwide location ignores location filtering
-    #
-    #   # 'area_before_type_cast' and 'geopoint_before_type_cast' are used to avoid casting the fields into RGeo objects.
-    #   # This reduces memory usage and speeds up the query. As we don't need to use the actual objects in Ruby code,
-    #   # just need to know if they are present in DB.
-    #   if subscription.area_before_type_cast.present?
-    #     location_by_area_filter(scope, subscription)
-    #   elsif subscription.geopoint_before_type_cast.present? && subscription.radius_in_metres.present?
-    #     location_by_geopoint_filter(scope, subscription)
-    #   else
-    #     scope.none # Invalid location filter (having no area or geopoint) returns no matches
-    #   end
-    # end
-
-    # Filter vacancies where their organisations' geopoints fall within the subscription's area polygon.
-    # The subscription area is already buffered by the subscription radius when created/updated.
-    # So a "london" + 10 miles subscription will have an area polygon that is London polygon + 10 miles buffer.
-    # Any vacancy with an organisation geopoint within that buffered area polygon will match.
-    #
-    # PERFORMANCE: We cast the organisations.geopoint to geometry to ensure the ST_Contains is a geometry operation.
-    # This is significantly faster than geography operations for this type of query.
-    #
-    # Including 'publish_on' in the distinct: The provided scope may be ordered by publish on date. When using distinct
-    # with ordering, Postgres requires all selected columns to be included in the distinct clause or will fail during execution.
-    # def location_by_area_filter(scope, subscription)
-    #   scope.joins("INNER JOIN subscriptions ON subscriptions.id = '#{subscription.id}'")
-    #        .joins(:organisations)
-    #        .where("ST_Contains(subscriptions.area, organisations.geopoint::geometry)")
-    # end
-
-    # Filter vacancies where their organisations' geopoints fall within the subscription's radius from the subscription's
-    # geopoint.
-    # PERFORMANCE:
-    #  - We transform both geopoints to British National Grid SRID (27700) to ensure the ST_DWithin is done in metres.
-    #  - We cast the organisations.geopoint (stored as geography) to geometry to ensure the ST_DWithin is a geometry
-    #    operation. This is significantly faster than using geography types for this type of query.
-    #
-    # Including 'publish_on' in the distinct: The provided scope may be ordered by publish on date. When using distinct
-    # with ordering, Postgres requires all selected columns to be included in the distinct clause or will fail during execution.
-    # def location_by_geopoint_filter(scope, subscription)
-    #   scope.joins("INNER JOIN subscriptions ON subscriptions.id = '#{subscription.id}'")
-    #        .joins(:organisations)
-    #        .where("ST_DWithin(ST_Transform(organisations.geopoint::geometry, #{BRITISH_NATIONAL_GRID_SRID}),
-    #                         ST_Transform(subscriptions.geopoint, #{BRITISH_NATIONAL_GRID_SRID}),
-    #                         subscriptions.radius_in_metres)")
-    # end
   end
 end
--- spec/queries/subscription_vacancies_matching_query_spec.rb
+++ spec/queries/subscription_vacancies_matching_query_spec.rb
@@ -440,6 +440,181 @@ RSpec.describe SubscriptionVacanciesMatchingQuery do
     end
   end
 
+  describe "location matching" do
+    let(:subscription) { create(:daily_subscription, location: location, radius: radius) }
+
+    let(:liverpool_vacancy) { Vacancy.find_by!(job_title: "liv") }
+    let(:basildon_vacancy) { Vacancy.find_by!(job_title: "bas") }
+    let(:st_albans_vacancy) { Vacancy.find_by!(job_title: "sta") }
+    let(:basildon_stalbans_vacancy) { Vacancy.find_by!(job_title: "bas-sta") }
+
+    # rubocop:disable RSpec/BeforeAfterAll
+    before(:all) do
+      YAML.unsafe_load_file(Rails.root.join("spec/fixtures/polygons.yml")).map(&:attributes).each { |s| LocationPolygon.create!(s) }
+      YAML.unsafe_load_file(Rails.root.join("spec/fixtures/liverpool_schools.yml")).map(&:attributes).each { |s| School.create!(s) }
+      YAML.unsafe_load_file(Rails.root.join("spec/fixtures/basildon_schools.yml")).map(&:attributes).each { |s| School.create!(s) }
+      YAML.unsafe_load_file(Rails.root.join("spec/fixtures/st_albans_schools.yml")).map(&:attributes).each { |s| School.create!(s) }
+      liverpool_org = School.find_by!(town: "Liverpool")
+      basildon_org = School.find_by!(town: "Basildon")
+      st_albans_org = School.find_by!(town: "St Albans")
+
+      create(:vacancy, :published_slugged, job_title: "liv", organisations: [liverpool_org])
+      create(:vacancy, :published_slugged, job_title: "bas", organisations: [basildon_org])
+      create(:vacancy, :published_slugged, job_title: "sta", organisations: [st_albans_org])
+      create(:vacancy, :published_slugged, job_title: "bas-sta", organisations: [basildon_org, st_albans_org])
+    end
+
+    after(:all) do
+      Vacancy.destroy_all
+      School.destroy_all
+      Publisher.destroy_all
+      Subscription.destroy_all
+      LocationPolygon.destroy_all
+    end
+    # rubocop:enable RSpec/BeforeAfterAll
+
+    context "with a subscription containing a nationwide location" do
+      let(:subscription) { create(:daily_subscription, location: "england") }
+
+      it "finds all the vacancies regardless their location" do
+        expect(query_results).to match_array([liverpool_vacancy, basildon_vacancy, st_albans_vacancy, basildon_stalbans_vacancy].map(&:job_title))
+      end
+    end
+
+    context "with a subscription containing a polygon area (Basildon)" do
+      let(:subscription) { create(:daily_subscription, location: "Basildon", radius: radius).tap(&:set_location_data!) }
+
+      context "with a small radius" do
+        let(:radius) { 4 }
+
+        it "finds just basildon vacancy" do
+          expect(query_results).to contain_exactly(basildon_vacancy.job_title, basildon_stalbans_vacancy.job_title)
+        end
+      end
+
+      context "with a medium radius" do
+        let(:radius) { 50 }
+
+        it "finds basildon and st albans vacancies" do
+          expect(query_results).to contain_exactly(st_albans_vacancy.job_title, basildon_vacancy.job_title, basildon_stalbans_vacancy.job_title)
+        end
+      end
+
+      context "with a large radius" do
+        let(:radius) { 200 }
+
+        it "finds all vacancies" do
+          expect(query_results).to contain_exactly(liverpool_vacancy.job_title, st_albans_vacancy.job_title, basildon_vacancy.job_title, basildon_stalbans_vacancy.job_title)
+        end
+      end
+
+      context "when the vacancy does not match the non-location criteria" do
+        let(:subscription) { create(:daily_subscription, location: "Basildon", teaching_job_roles: %w[sendco], radius: 200) }
+
+        it "does not find the vacancy" do
+          expect(query_results).to be_empty
+        end
+      end
+
+      context "when the vacancy belongs to multiple organisations matching the location filter" do
+        let(:subscription) { create(:daily_subscription, location: "Basildon", radius: 50).tap(&:set_location_data!) }
+
+        it "returns the vacancy once" do
+          expect(query_results).to match_array([basildon_stalbans_vacancy, basildon_vacancy, st_albans_vacancy].map(&:job_title))
+        end
+      end
+    end
+
+    context "with a subscription containing a geopoint (basildon postcode)" do
+      let(:subscription) { create(:daily_subscription, location: "Basildon SS14 3WB", radius: radius).tap(&:set_location_data!) }
+      let(:geocoding_for_basildon) { instance_double(Geocoding, coordinates: [51.58521140000001, 0.4631542]) }
+
+      before do
+        allow(Geocoding).to receive(:new).and_return(geocoding_for_basildon)
+      end
+
+      context "with a small radius" do
+        let(:radius) { 5 }
+
+        it "finds just basildon vacancy" do
+          expect(query_results).to contain_exactly(basildon_vacancy.job_title, basildon_stalbans_vacancy.job_title)
+        end
+      end
+
+      context "with a medium radius" do
+        let(:radius) { 50 }
+
+        it "finds basildon and st albans vacancies" do
+          expect(query_results).to contain_exactly(st_albans_vacancy.job_title, basildon_vacancy.job_title, basildon_stalbans_vacancy.job_title)
+        end
+      end
+
+      context "with a large radius" do
+        let(:radius) { 200 }
+
+        it "finds all vacancies" do
+          expect(query_results).to match_array([liverpool_vacancy,
+                                                st_albans_vacancy,
+                                                basildon_stalbans_vacancy,
+                                                basildon_vacancy].map(&:job_title))
+        end
+      end
+
+      context "when the vacancy does not match the non-location criteria" do
+        let(:subscription) { create(:daily_subscription, location: "Basildon SS14 3WB", teaching_job_roles: %w[pastoral_health_and_welfare], radius: 200) }
+
+        it "does not find the vacancy" do
+          expect(query_results).to be_empty
+        end
+      end
+
+      context "when the vacancy does not match the non-location subjects" do
+        let(:subscription) { create(:daily_subscription, location: "Basildon SS14 3WB", subjects: %w[French German], radius: 200) }
+
+        it "does not find the vacancy" do
+          expect(query_results).to be_empty
+        end
+      end
+
+      context "when the vacancy belongs to multiple organisations matching the location filter" do
+        let(:subscription) { create(:daily_subscription, location: "Basildon SS14 3WB", radius: 50).tap(&:set_location_data!) }
+
+        it "returns the vacancy once" do
+          expect(query_results).to match_array([basildon_vacancy, st_albans_vacancy, basildon_stalbans_vacancy].map(&:job_title))
+        end
+      end
+    end
+
+    context "with a subscription containing location search criteria but neither a polygon area nor a geopoint" do
+      let(:subscription) { create(:daily_subscription, location: "Basildon", radius: radius) }
+      let(:radius) { 50 }
+
+      it "finds no vacancies" do
+        pending("Is this correct behaviour?")
+
+        expect(query_results).to be_empty
+      end
+    end
+
+    context "with a subscription containing no location search criteria" do
+      let(:subscription) { create(:daily_subscription) }
+
+      it "finds all the vacancies regardless their location" do
+        expect(query_results).to match_array([liverpool_vacancy, basildon_vacancy, st_albans_vacancy, basildon_stalbans_vacancy].map(&:job_title))
+      end
+    end
+
+    context "with a subscription containing blanks search criteria" do
+      let(:subscription) { create(:daily_subscription, location: "", radius: 10) }
+
+      it "filters out all the vacancies" do
+        pending("Is this correct behaviour?")
+
+        expect(query_results).to be_empty
+      end
+    end
+  end
+
   describe "combined criteria matching" do
     let(:subscription) do
       build_stubbed(
@@ -449,7 +624,7 @@ RSpec.describe SubscriptionVacanciesMatchingQuery do
         phases: %w[secondary],
         working_patterns: %w[full_time],
         keyword: "nice",
-        )
+      )
     end
 
     let!(:matching_job) do
