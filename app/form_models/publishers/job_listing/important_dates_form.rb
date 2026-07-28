685cd9d7b4cf1f0c70fd3117e2ad0524a029c5ec
starswan <github@starswan.com>
2026-07-28 08:46:44 +0100

    wip

--- app/form_models/publishers/job_listing/important_dates_form.rb
+++ app/form_models/publishers/job_listing/important_dates_form.rb
@@ -1,44 +1,48 @@
-class Publishers::JobListing::ImportantDatesForm < Publishers::JobListing::ExpiryDateTimeForm
-  attr_accessor :publish_on_day
-  attr_reader :publish_on
+module Publishers
+  module JobListing
+    class ImportantDatesForm < ExpiryDateTimeForm
+      attr_accessor :publish_on_day
+      attr_reader :publish_on
 
-  #  ;publish_on_day is a radio, so this validation is skipped if it hasn't been selected
-  validates :publish_on, date: { on_or_after: :today, on_or_before: :far_future }, unless: -> { publish_on_day.blank? }
-  validates :publish_on_day, inclusion: { in: %w[today tomorrow another_day] }
+      #  ;publish_on_day is a radio, so this validation is skipped if it hasn't been selected
+      validates :publish_on, date: { on_or_after: :today, on_or_before: :far_future }, unless: -> { publish_on_day.blank? }
+      validates :publish_on_day, inclusion: { in: %w[today tomorrow another_day] }
 
-  def self.fields
-    %i[publish_on expires_at]
-  end
-
-  class << self
-    def load_from_model(vacancy, current_publisher:) # rubocop:disable Lint/UnusedMethodArgument
-      params = args_from_vacancy(vacancy)
-                 .merge(publish_on: vacancy.publish_on)
-                 .merge(publish_on_day: extract_publish_on_day(vacancy))
-      new(params)
-    end
-  end
-
-  def params_to_save
-    { expires_at: expires_at,
-      publish_on: publish_on }
-  end
-
-  def publish_on=(value)
-    @publish_on =
-      case @publish_on_day
-      when "today" then Date.today
-      when "tomorrow" then Date.tomorrow
-      else date_from_multiparameter_hash(value)
+      def self.fields
+        %i[publish_on expires_at]
       end
-  end
 
-  class << self
-    def extract_publish_on_day(params)
-      return "today" if params[:publish_on_day] == "today" || params[:publish_on] == Date.today
-      return "tomorrow" if params[:publish_on_day] == "tomorrow" || params[:publish_on] == Date.tomorrow
+      class << self
+        def load_from_model(vacancy, current_publisher:) # rubocop:disable Lint/UnusedMethodArgument
+          params = args_from_vacancy(vacancy)
+                     .merge(publish_on: vacancy.publish_on)
+                     .merge(publish_on_day: extract_publish_on_day(vacancy))
+          new(params)
+        end
+      end
 
-      "another_day" if params[:publish_on_day] == "another_day" || params[:publish_on].is_a?(Date)
+      def params_to_save
+        { expires_at: expires_at,
+          publish_on: publish_on }
+      end
+
+      def publish_on=(value)
+        @publish_on =
+          case @publish_on_day
+          when "today" then Date.today
+          when "tomorrow" then Date.tomorrow
+          else date_from_multiparameter_hash(value)
+          end
+      end
+
+      class << self
+        def extract_publish_on_day(params)
+          return "today" if params[:publish_on_day] == "today" || params[:publish_on] == Date.today
+          return "tomorrow" if params[:publish_on_day] == "tomorrow" || params[:publish_on] == Date.tomorrow
+
+          "another_day" if params[:publish_on_day] == "another_day" || params[:publish_on].is_a?(Date)
+        end
+      end
     end
   end
 end
