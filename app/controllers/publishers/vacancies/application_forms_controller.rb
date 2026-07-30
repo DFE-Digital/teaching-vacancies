abeb1fca1478baf92dc12d339d933bbd391d31aa
starswan <github@starswan.com>
2026-07-30 14:57:33 +0100

    wip

--- app/controllers/publishers/vacancies/application_forms_controller.rb
+++ app/controllers/publishers/vacancies/application_forms_controller.rb
@@ -1,18 +1,21 @@
-require "google/apis/drive_v3"
-
 class Publishers::Vacancies::ApplicationFormsController < Publishers::Vacancies::WizardBaseController
   helper_method :form
+  before_action :set_vacancy
 
   def create
     if form.valid?
       vacancy.application_form.attach(form.application_form) if application_form_uploaded?
       update_vacancy
-      send_dfe_analytics_event if application_form_uploaded?
+      send_dfe_analytics_event
       redirect_to_next_step
     else
-      # See commit message for 1aa28cce3239c42b1af23d61ae08add3e8c51e5e for context
-      render "publishers/vacancies/build/application_form", locals: { application_form_staged_for_replacement: application_form_staged_for_replacement? }
+      render "publishers/vacancies/build/application_form"
     end
+  end
+
+  def destroy
+    vacancy.application_form.purge_later
+    redirect_to organisation_job_build_path(vacancy.id, :application_form)
   end
 
   private
@@ -26,8 +29,7 @@ class Publishers::Vacancies::ApplicationFormsController < Publishers::Vacancies:
   end
 
   def application_form_params
-    params.expect(publishers_job_listing_application_form_form: %i[application_form application_form_staged_for_replacement])
-          .merge(completed_steps: completed_steps)
+    params.fetch(:publishers_job_listing_application_form_form, {}).permit(:application_form).merge(completed_steps: completed_steps)
   end
 
   def update_vacancy
@@ -36,8 +38,6 @@ class Publishers::Vacancies::ApplicationFormsController < Publishers::Vacancies:
   end
 
   def event_type
-    return :supporting_document_replaced if application_form_staged_for_replacement?
-
     :supporting_document_created
   end
 
@@ -52,11 +52,11 @@ class Publishers::Vacancies::ApplicationFormsController < Publishers::Vacancies:
       }
 
       event = DfE::Analytics::Event.new
-        .with_type(event_type)
-        .with_request_details(request)
-        .with_response_details(response)
-        .with_user(current_publisher)
-        .with_data(data: event_data)
+                                   .with_type(event_type)
+                                   .with_request_details(request)
+                                   .with_response_details(response)
+                                   .with_user(current_publisher)
+                                   .with_data(data: event_data)
 
       DfE::Analytics::SendEvents.do([event])
     end
@@ -70,11 +70,7 @@ class Publishers::Vacancies::ApplicationFormsController < Publishers::Vacancies:
     end
   end
 
-  def application_form_staged_for_replacement?
-    params[:publishers_job_listing_application_form_form][:application_form_staged_for_replacement].present?
-  end
-
   def application_form_uploaded?
-    params[:publishers_job_listing_application_form_form][:application_form].present?
+    params.dig(:publishers_job_listing_application_form_form, :application_form).present?
   end
 end
