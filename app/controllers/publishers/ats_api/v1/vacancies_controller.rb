2d923501f6fbb98631a99e843caf128e2fb06561
Marc Sardon <marc@bichito.software>
2026-08-05 08:31:54 +0100

    Rack upgrade: Rename unprocessable entity status
    The unprocessable_entity is legacy. Unprocessable content returns the
    same status code so no real changes on APi response.

--- app/controllers/publishers/ats_api/v1/vacancies_controller.rb
+++ app/controllers/publishers/ats_api/v1/vacancies_controller.rb
@@ -8,7 +8,7 @@ class Publishers::AtsApi::V1::VacanciesController < Api::ApplicationController
 
   rescue_from StandardError, with: :render_server_error
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-  rescue_from Publishers::AtsApi::OrganisationFetcher::InvalidOrganisationError, with: :render_unprocessable_entity
+  rescue_from Publishers::AtsApi::OrganisationFetcher::InvalidOrganisationError, with: :render_unprocessable_content
   # If there is an issue with the parameter parsing we return a controlled 400 error with a messagge rather than a default 500 error.
   rescue_from ActionController::ParameterMissing, with: :render_bad_request
 
@@ -159,7 +159,7 @@ class Publishers::AtsApi::V1::VacanciesController < Api::ApplicationController
     render json: { errors: [exception.message] }, status: :bad_request
   end
 
-  def render_unprocessable_entity(exception)
+  def render_unprocessable_content(exception)
     render json: { errors: [exception.message] }, status: :unprocessable_content
   end
 end
