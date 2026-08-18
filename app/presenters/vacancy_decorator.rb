8523089927156d6304f619c5c0de45547a5b6ea0
Stephen Dicks <github@starswan.com>
2026-08-18 14:38:22 +0100

    Add tests for untested vacancy decorator methods

--- app/presenters/vacancy_decorator.rb
+++ app/presenters/vacancy_decorator.rb
@@ -6,7 +6,7 @@ class VacancyDecorator < Draper::Decorator
            :publish_on, :skills_and_experience, :job_roles,
            :hourly_rate?, :hourly_rate, :salary?, :actual_salary?, :pay_scale?, :salary, :pay_scale, :actual_salary,
            :about_school, :expires_at, :expired?, :enable_job_applications?,
-           :for_an_fe_college?, :live?, :uploaded_form?,:ect_suitable?,
+           :for_an_fe_college?, :live?, :uploaded_form?, :ect_suitable?,
            :slug, :allow_key_stages?, :allow_subjects?, :contract_type, :ect_status,
            :visa_sponsorship_available, :fixed_term_contract_duration, :school_offer, :flexi_working,
            :further_details, :include_additional_documents, :central_office?, :contact_email, :contact_number,
