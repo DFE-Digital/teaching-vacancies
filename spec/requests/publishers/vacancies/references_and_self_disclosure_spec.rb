69efe3a5652a2bb018aec0764dac9e6f5baf9bf0
Kyle MacPherson <kaimac2010@gmail.com>
2026-08-19 18:25:52 +0100

    Linting

--- spec/requests/publishers/vacancies/references_and_self_disclosure_spec.rb
+++ spec/requests/publishers/vacancies/references_and_self_disclosure_spec.rb
@@ -14,13 +14,24 @@ RSpec.describe "Publishers::Vacancies::ReferencesAndSelfDisclosure" do
   end
 
   before do
+    # rubocop:disable RSpec/AnyInstance
     allow_any_instance_of(ApplicationController).to receive(:current_organisation).and_return(organisation)
+    # rubocop:enable RSpec/AnyInstance
     sign_in(publisher, scope: :publisher)
   end
 
   after { sign_out(publisher) }
 
   describe "PATCH #update on collect_self_disclosure step" do
+    subject(:request) do
+      patch(
+        organisation_job_job_application_batch_references_and_self_disclosure_path(
+          vacancy.id, batch.id, :collect_self_disclosure
+        ),
+        params: params,
+      )
+    end
+
     let(:params) do
       {
         publishers_job_application_collect_self_disclosure_form: {
@@ -29,15 +40,6 @@ RSpec.describe "Publishers::Vacancies::ReferencesAndSelfDisclosure" do
           contact_applicants: false,
         },
       }
-    end
-
-    subject(:request) do
-      patch(
-        organisation_job_job_application_batch_references_and_self_disclosure_path(
-          vacancy.id, batch.id, :collect_self_disclosure
-        ),
-        params: params,
-      )
     end
 
     context "when job application has a religious referee and no existing religious reference request" do
