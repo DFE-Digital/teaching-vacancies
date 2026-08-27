f4e9ab79f5768ef75e84733432690edec8d431ea
Stephen Dicks <github@starswan.com>
2026-08-27 13:53:37 +0100

    add test for weekly job filter

--- spec/jobs/send_weekly_alert_email_job_spec.rb
+++ spec/jobs/send_weekly_alert_email_job_spec.rb
@@ -11,4 +11,26 @@ RSpec.describe SendWeeklyAlertEmailJob do
       job.subscriptions
     end
   end
+
+  context "with multiple vacancies", :perform_enqueued do
+    before do
+      create(:vacancy, :published_slugged, publish_on: Date.current - 8)
+    end
+
+    # rubocop:disable RSpec/VerifiedDoubles
+    let(:mail) { double(:mail) }
+    # rubocop:enable RSpec/VerifiedDoubles
+
+    let!(:one_week_ago) { create(:vacancy, :published_slugged, publish_on: Date.current - 7) }
+    let!(:two_days_ago) { create(:vacancy, :published_slugged, publish_on: Date.yesterday - 1) }
+    let!(:yesterday) { create(:vacancy, :published_slugged, publish_on: Date.yesterday) }
+
+    let!(:subscription) { create(:weekly_subscription) }
+
+    it "sends an email" do
+      expect(Jobseekers::AlertMailer).to receive(:alert).with(subscription.id, [yesterday, two_days_ago, one_week_ago].map(&:id)) { mail }
+      expect(mail).to receive(:deliver_later) { ActionMailer::MailDeliveryJob.new }
+      described_class.perform_later
+    end
+  end
 end
