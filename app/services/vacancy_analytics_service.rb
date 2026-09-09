5fbb681b62b19fc335d093f087c55fab9bc25909
Stephen Dicks <github@starswan.com>
2026-09-09 15:39:48 +0100

    Rework vacancy stats collection

--- app/services/vacancy_analytics_service.rb
+++ app/services/vacancy_analytics_service.rb
@@ -1,58 +1,47 @@
 class VacancyAnalyticsService
-  REDIS_KEY_PREFIX = "vacancy_referrer_stats".freeze
+  class << self
+    def track_visit(vacancy_id:, referrer_url:, hostname:, params:)
+      return if vacancy_id.blank?
 
-  def self.track_visit(redis, vacancy_id, referrer_url, hostname, params)
-    return if vacancy_id.blank?
+      referrer = normalize_referrer(referrer_url, hostname, params)
 
-    # Generate a Redis key for this vacancy and referrer
-    redis_key = "#{REDIS_KEY_PREFIX}:#{vacancy_id}:#{normalize_referrer(referrer_url, hostname, params)}"
-    # Increment the counter in Redis
-    redis.incr(redis_key)
-  end
-
-  def self.aggregate_and_save_stats(redis)
-    keys_pattern = "#{REDIS_KEY_PREFIX}:*"
-
-    redis.scan_each(match: keys_pattern).each_slice(100) do |keys_batch|
-      # Creates hash that, when accessed with a missing key, assigns a new nested hash as the value. This nested hash defaults to 0 for any missing keys.
-      updates_by_vacancy = Hash.new { |h, k| h[k] = Hash.new(0) }
-      keys_to_delete = []
-
-      keys_batch.each do |key|
-        count = redis.get(key).to_i
-        next if count.zero?
-
-        # Parse key to extract vacancy_id and referrer
-        _, vacancy_id, referrer = key.split(":", 3)
-
-        updates_by_vacancy[vacancy_id][referrer] += count
-        keys_to_delete << key
-      end
-
-      update_stats_in_database(updates_by_vacancy) if updates_by_vacancy.any?
-      redis.del(*keys_to_delete)
-    end
-  end
-
-  def self.update_stats_in_database(vacancy_updates)
-    vacancy_updates.each do |vacancy_id, new_referrer_counts|
-      # Skip if the associated vacancy does not exist. This may happen if the vacancy was deleted after the visit was tracked.
-      next unless PublishedVacancy.exists?(id: vacancy_id)
-
+      # VacancyAnalytics.connection.exec_query(<<~SQL.squish, "track_visit", [vacancy_id, referrer])
+      #   INSERT INTO vacancy_analytics (id, vacancy_id, referrer_counts, created_at, updated_at)
+      #   VALUES (gen_random_uuid(), $1, jsonb_build_object($2::text, 1), now(), now())
+      #   ON CONFLICT (vacancy_id) DO UPDATE
+      #   SET referrer_counts = jsonb_set(
+      #         vacancy_analytics.referrer_counts,
+      #         ARRAY[$2::text],
+      #         to_jsonb(COALESCE((vacancy_analytics.referrer_counts ->> $2::text)::bigint, 0) + 1)),
+      #       updated_at = now()
+      # SQL
       VacancyAnalytics.transaction do
         analytics = VacancyAnalytics.where(vacancy_id: vacancy_id).lock(true).first_or_initialize
 
-        merged_counts = analytics.referrer_counts.merge(new_referrer_counts) do |_, old_count, new_count|
-          old_count.to_i + new_count.to_i
-        end
-
-        analytics.referrer_counts = merged_counts
+        new_count = analytics.referrer_counts.fetch(referrer, 0) + 1
+        analytics.referrer_counts[referrer] = new_count
         analytics.save!
       end
     end
-  end
 
-  class << self
+    # def self.update_stats_in_database(vacancy_updates)
+    #   vacancy_updates.each do |vacancy_id, new_referrer_counts|
+    #     # Skip if the associated vacancy does not exist. This may happen if the vacancy was deleted after the visit was tracked.
+    #     next unless PublishedVacancy.exists?(id: vacancy_id)
+    #
+    #     VacancyAnalytics.transaction do
+    #       analytics = VacancyAnalytics.where(vacancy_id: vacancy_id).lock(true).first_or_initialize
+    #
+    #       merged_counts = analytics.referrer_counts.merge(new_referrer_counts) do |_, old_count, new_count|
+    #         old_count.to_i + new_count.to_i
+    #       end
+    #
+    #       analytics.referrer_counts = merged_counts
+    #       analytics.save!
+    #     end
+    #   end
+    # end
+
     def normalize_referrer(referrer, hostname, params)
       if params[:utm_medium] == "email"
         "jobalert"
