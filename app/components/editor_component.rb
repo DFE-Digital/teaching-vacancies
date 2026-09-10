526f20b79b7bc4b4e2c3ff9b6bb2d551cb835362
Stephen Dicks <github@starswan.com>
2026-09-10 17:05:12 +0100

    reduce scope for 'classes' work-around

--- app/components/editor_component.rb
+++ app/components/editor_component.rb
@@ -1,5 +1,4 @@
-# seems ok - used in 4 places
-class EditorComponent < ApplicationComponent
+class EditorComponent < TvsClassesComponent
   def initialize(form_input:, value:, field_name:, hint: nil, label: {}, classes: [], html_attributes: {})
     super(classes: classes, html_attributes: html_attributes.merge({ data: { controller: "editor" } }))
 
