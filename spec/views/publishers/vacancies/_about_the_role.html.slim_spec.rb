3f01942a32f6deec5b0acf2759ee4c4ee0b42f99
Stephen Dicks <github@starswan.com>
2026-09-17 08:59:36 +0100

    move partials out of nested directory

--- spec/views/publishers/vacancies/_about_the_role.html.slim_spec.rb
+++ spec/views/publishers/vacancies/_about_the_role.html.slim_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-RSpec.describe "publishers/vacancies/vacancy_review_sections/_about_the_role" do
-  before { render partial: "publishers/vacancies/vacancy_review_sections/about_the_role", locals: { vacancy: vacancy.decorate } }
+RSpec.describe "publishers/vacancies/_about_the_role" do
+  before { render partial: "publishers/vacancies/about_the_role", locals: { vacancy: vacancy.decorate } }
 
   context "when published with documents" do
     # TODO: can't currently stub a vacancy with documents
