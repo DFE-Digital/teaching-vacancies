2e162e51ca6ee18b52c5b9124bf4c9896d664743
Kyle MacPherson <kaimac2010@gmail.com>
2026-09-18 13:55:30 +0100

    wip

--- spec/mailers/jobseekers/alert_mailer_spec.rb
+++ spec/mailers/jobseekers/alert_mailer_spec.rb
@@ -27,6 +27,7 @@ RSpec.describe Jobseekers::AlertMailer do
   # The array of vacancies is set to length 1 because the order varies, making it hard to test url parameters.
   let(:vacancies) { create_list(:vacancy, 1, organisations: [school]).map(&:decorate) }
   let(:utm_params) { { utm_source: "a_unique_identifier", utm_medium: "email", utm_campaign: "#{frequency}_alert" } }
+  let(:expected_job_title_link) { notify_link(job_url(vacancies.first, **utm_params), vacancies.first.job_title) }
   let(:relevant_job_alert_feedback_url) do
     subscription_submit_feedback_url(
       subscription.token,
@@ -104,8 +105,7 @@ RSpec.describe Jobseekers::AlertMailer do
                                         school_name: vacancies.first.organisation_name))
       expect(mail.to).to eq([subscription.email])
       expect(body).to include(I18n.t("jobseekers.alert_mailer.alert.summary.daily", count: 1))
-                  .and include(vacancies.first.job_title)
-                  .and include(job_url(vacancies.first, **utm_params))
+                  .and include(expected_job_title_link)
                   .and include(I18n.t("jobseekers.alert_mailer.alert.working_pattern", working_pattern: vacancies.first.readable_working_patterns_with_details))
                   .and include(I18n.t("jobseekers.alert_mailer.alert.title"))
                   .and include("Keyword: English")
@@ -162,8 +162,7 @@ RSpec.describe Jobseekers::AlertMailer do
                                         school_name: vacancies.first.organisation_name))
       expect(mail.to).to eq([subscription.email])
       expect(body).to include(I18n.t("jobseekers.alert_mailer.alert.summary.weekly", count: 1))
-                  .and include(vacancies.first.job_title)
-                  .and include(job_url(vacancies.first, **utm_params))
+                  .and include(expected_job_title_link)
                   .and include(I18n.t("jobseekers.alert_mailer.alert.working_pattern",
                                       working_pattern: vacancies.first.readable_working_patterns_with_details))
                   .and include(I18n.t("jobseekers.alert_mailer.alert.title"))
