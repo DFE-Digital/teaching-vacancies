67f3edc21372b9ff673601f6b4e54f821b079189
Stephen Dicks <github@starswan.com>
2026-09-25 12:45:36 +0100

    Remove old aggregate stats job (#9313)
    ## Trello card URL
    
    
    https://trello.com/c/6pjoUUoS/3256-redis-removal-step-2-remove-redis-queue

--- app/jobs/track_vacancy_view_job.rb
+++
@@ -1,7 +0,0 @@
-class TrackVacancyViewJob < ApplicationJob
-  queue_as :low
-
-  def perform(vacancy_id:, referrer_url:, hostname:, params:)
-    VacancyAnalyticsService.track_visit(vacancy_id: vacancy_id, referrer_url: referrer_url, hostname: hostname, params: params)
-  end
-end
