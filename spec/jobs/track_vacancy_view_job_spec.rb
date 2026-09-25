245daa549a650ae81152fa26752c5a73bf81e877
Stephen Dicks <github@starswan.com>
2026-09-25 10:25:29 +0100

    rework test

--- spec/jobs/track_vacancy_view_job_spec.rb
+++ spec/jobs/track_vacancy_view_job_spec.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+require "rails_helper"
+
 RSpec.describe TrackVacancyViewJob do
   let(:vacancy) { create(:vacancy) }
   let(:referrer_url) { "https://www.example.com/some/path?utm=source" }
   let(:hostname) { "www.tvs.service.gov.uk" }
 
   it "calls track_visits" do
-    expect(VacancyAnalyticsService).to receive(:track_visit).and_call_original
-    described_class.perform_now(vacancy_id: vacancy.id, referrer_url: referrer_url, hostname: hostname, params: {})
+    expect {
+      described_class.perform_now(vacancy_id: vacancy.id, referrer_url: referrer_url, hostname: hostname, params: {})
+    }.to change(VacancyAnalytics, :count).by(1)
   end
 end
